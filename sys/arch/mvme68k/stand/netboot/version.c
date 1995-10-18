7caf0a25a5e44381b16975229b5c564df1e7690f
cvs-fast-export <cvs-fast-export>
1995-10-18 10:39:23 +0000

    Synthetic commit for incomplete tag pica_1

--- sys/arch/mvme68k/stand/netboot/version.c
+++
@@ -1,9 +0,0 @@
-/*	$NetBSD: version.c,v 1.1.1.1 1995/07/25 23:12:26 chuck Exp $ */
-
-/*
- *	NOTE ANY CHANGES YOU MAKE TO THE BOOTBLOCKS HERE.
- *
- *	1.1
- */
-
-char *version = "$Revision: 1.1.1.1 $";
