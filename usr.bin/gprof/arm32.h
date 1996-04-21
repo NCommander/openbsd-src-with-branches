4177635f39a4ba434730316f00d88a36d6939d44
deraadt <deraadt>
1996-04-21 23:47:38 +0000

    sync to netbsd 960418

--- usr.bin/gprof/arm32.h
+++ usr.bin/gprof/arm32.h
@@ -1,7 +1,7 @@
-/*	$NetBSD: pass3.c,v 1.8 1995/03/18 14:55:54 cgd Exp $	*/
+/*	$NetBSD: arm32.h,v 1.1 1996/04/01 21:51:26 mark Exp $	*/
 
-/*
- * Copyright (c) 1980, 1986, 1993
+/*-
+ * Copyright (c) 1991, 1993
  *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -33,47 +33,13 @@
  * SUCH DAMAGE.
  */
 
-#ifndef lint
-#if 0
-static char sccsid[] = "@(#)pass3.c	8.1 (Berkeley) 6/5/93";
-#else
-static char rcsid[] = "$NetBSD: pass3.c,v 1.8 1995/03/18 14:55:54 cgd Exp $";
-#endif
-#endif /* not lint */
+/*
+ *	offset (in bytes) of the code from the entry address of a routine.
+ *	(see asgnsamples for use and explanation.)
+ */
 
-#include <sys/param.h>
-#include <sys/time.h>
-#include <ufs/ufs/dinode.h>
-#include <ufs/ffs/fs.h>
-#include "fsck.h"
-#include "extern.h"
+#define OFFSET_OF_CODE	0
+#define	UNITS_TO_CODE	(OFFSET_OF_CODE / sizeof(UNIT))
 
-void
-pass3()
-{
-	register struct inoinfo **inpp, *inp;
-	ino_t orphan;
-	int loopcnt;
-
-	for (inpp = &inpsort[inplast - 1]; inpp >= inpsort; inpp--) {
-		inp = *inpp;
-		if (inp->i_number == ROOTINO ||
-		    !(inp->i_parent == 0 || statemap[inp->i_number] == DSTATE))
-			continue;
-		if (statemap[inp->i_number] == DCLEAR)
-			continue;
-		for (loopcnt = 0; ; loopcnt++) {
-			orphan = inp->i_number;
-			if (inp->i_parent == 0 ||
-			    statemap[inp->i_parent] != DSTATE ||
-			    loopcnt > numdirs)
-				break;
-			inp = getinoinfo(inp->i_parent);
-		}
-		(void)linkup(orphan, inp->i_dotdot);
-		inp->i_parent = inp->i_dotdot = lfdir;
-		lncntp[lfdir]--;
-		statemap[orphan] = DFOUND;
-		propagate();
-	}
-}
+enum opermodes { dummy };
+typedef enum opermodes	operandenum;
