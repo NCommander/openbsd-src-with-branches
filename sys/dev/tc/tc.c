3c5983f4f8197375190e20741426aa9ecd76f6c9
deraadt <deraadt>
1996-05-07 07:31:39 +0000

    add :

--- sys/dev/tc/tc.c
+++ sys/dev/tc/tc.c
@@ -1,5 +1,5 @@
 /*	$OpenBSD: tc.c,v 1.3 1996/04/21 22:26:28 deraadt Exp $	*/
-/*	$NetBSD: tc.c,v 1.14 1996/04/18 01:12:41 cgd Exp $	*/
+/*	$NetBSD: tc.c,v 1.15 1996/04/29 16:06:38 cgd Exp $	*/
 
 /*
  * Copyright (c) 1994, 1995 Carnegie-Mellon University.
@@ -94,7 +94,7 @@ tcattach(parent, self, aux)
 	tc_addr_t tcaddr;
 	int i;
 
-	printf("%s MHz clock\n",
+	printf(": %s MHz clock\n",
 	    tba->tba_speed == TC_SPEED_25_MHZ ? "25" : "12.5");
 
 	/*
