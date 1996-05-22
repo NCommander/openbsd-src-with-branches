11fe19319900e936024b1e0009d71a993a50eae6
deraadt <deraadt>
1996-05-22 12:02:43 +0000

    sync

--- sys/compat/common/tty_43.c
+++ sys/compat/common/tty_43.c
@@ -1,5 +1,5 @@
 /*	$OpenBSD: tty_43.c,v 1.1 1996/02/26 23:26:53 niklas Exp $	*/
-/*	$NetBSD: tty_43.c,v 1.3 1996/03/14 19:31:49 christos Exp $	*/
+/*	$NetBSD: tty_43.c,v 1.4 1996/05/18 22:17:49 veego Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1991, 1993
@@ -50,6 +50,11 @@
 #include <sys/kernel.h>
 #include <sys/syslog.h>
 #include <sys/ioctl_compat.h>
+
+/*
+ * XXX libcompat files should be included with config attributes
+ */
+#ifdef COMPAT_OLDTTY
 
 int ttydebug = 0;
 
@@ -475,3 +480,5 @@ ttcompatsetlflags(tp, t)
 	t->c_lflag = lflag;
 	t->c_cflag = cflag;
 }
+
+#endif /* COMPAT_OLDTTY */
