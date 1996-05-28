3bcd7ffa6553068ea57092ad5f6680bb40b555a5
chuck <chuck>
1996-05-28 15:16:57 +0000

    new version of bootst (from sun3 tapeboot)

---
+++ sys/arch/mvme68k/stand/bootst/version.c
@@ -0,0 +1,8 @@
+/*	$OpenBSD: version.c,v 1.5 1996/05/16 02:46:57 chuck Exp $ */
+
+/*
+ * make a random change to this file when you want the bootblock
+ * revision to increase. like change this x to a z, or something.
+ */
+
+char *version = "$Revision: 0.0 $";
