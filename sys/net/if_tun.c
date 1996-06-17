0062b50d9a2bfb5ba8866745f8f33d4b7f295c0c
deraadt <deraadt>
1996-06-17 11:06:18 +0000

    bad splx, bad bad bad

--- sys/net/if_tun.c
+++ sys/net/if_tun.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_tun.c,v 1.12 1996/06/02 16:27:44 niklas Exp $	*/
+/*	$OpenBSD: if_tun.c,v 1.13 1996/06/02 17:24:58 niklas Exp $	*/
 /*	$NetBSD: if_tun.c,v 1.24 1996/05/07 02:40:48 thorpej Exp $	*/
 
 /*
@@ -625,7 +625,6 @@ tunwrite(dev, uio, ioflag)
 #endif
 	default:
 		m_freem(top);
-		splx(s);
 		return EAFNOSUPPORT;
 	}
 
