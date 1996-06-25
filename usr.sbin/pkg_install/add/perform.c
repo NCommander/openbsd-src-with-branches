16898881cd7be0e85afef3a518d3cc80f7e77898
dm <dm>
1996-06-25 04:51:53 +0000

    our tar has no --fast-read

--- usr.sbin/pkg_install/add/perform.c
+++ usr.sbin/pkg_install/add/perform.c
@@ -1,7 +1,7 @@
-/*	$OpenBSD: perform.c,v 1.1 1996/06/04 07:56:04 niklas Exp $	*/
+/*	$OpenBSD: perform.c,v 1.2 1996/06/04 08:43:34 niklas Exp $	*/
 
 #ifndef lint
-static const char *rcsid = "$OpenBSD: perform.c,v 1.1 1996/06/04 07:56:04 niklas Exp $";
+static const char *rcsid = "$OpenBSD: perform.c,v 1.2 1996/06/04 08:43:34 niklas Exp $";
 #endif
 
 /*
@@ -115,7 +115,11 @@ pkg_do(char *pkg)
 		    whinge("Can't stat package file '%s'.", pkg_fullname);
 		    goto bomb;
 		}
+#if 0
 		sprintf(extract_contents, "--fast-read %s", CONTENTS_FNAME);
+#else
+		sprintf(extract_contents, "%s", CONTENTS_FNAME);
+#endif
 		extract = extract_contents;
 	    }
 	    else
