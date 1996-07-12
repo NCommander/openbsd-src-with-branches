40a8223b61e2cee3d88c527557642fe95272d6c7
pefo <pefo>
1996-07-12 08:57:59 +0000

    Fixed wrong type of argument passing for SCIOCREASSIGN. (thanks Theo).

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sd.c,v 1.15 1996/06/16 03:07:21 downsj Exp $	*/
+/*	$OpenBSD: sd.c,v 1.16 1996/07/11 22:17:46 pefo Exp $	*/
 /*	$NetBSD: sd.c,v 1.100.4.1 1996/06/04 23:14:08 thorpej Exp $	*/
 
 /*
@@ -751,7 +751,7 @@ sdioctl(dev, cmd, addr, flag, p)
 	case SCIOCREASSIGN:
 		if ((flag & FWRITE) == 0)
 			return EBADF;
-		error = sd_reassign_blocks(sd, *(*(int **)addr));
+		error = sd_reassign_blocks(sd, (*(int *)addr));
 		return error;
 
 	default:
