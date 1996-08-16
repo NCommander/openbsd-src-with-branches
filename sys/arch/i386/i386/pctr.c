7179ecac91f6d9c196472b10230a3ffac7f7a897
dm <dm>
1996-08-16 00:02:36 +0000

    Some mostly cosmetic bug fixes.

--- sys/arch/i386/i386/pctr.c
+++ sys/arch/i386/i386/pctr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pctr.c,v 1.4 1996/08/14 03:02:54 dm Exp $	*/
+/*	$OpenBSD: pctr.c,v 1.5 1996/08/14 22:03:15 dm Exp $	*/
 
 /*
  * Pentium performance counter driver for OpenBSD.
@@ -182,7 +182,7 @@ pctrioctl (dev_t dev, int cmd, caddr_t data, int fflag, struct proc *p)
       return p6ctrsel (fflag, cmd, *(u_int *) data);
     if (usep5ctr)
       return p5ctrsel (fflag, cmd, *(u_int *) data);
-    return EINVAL;
+    return ENODEV;
   default:
     return EINVAL;
   }
