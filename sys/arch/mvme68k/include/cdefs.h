61a212320c5527b21e98fd91451bd0459a2d5af1
cvs-fast-export <cvs-fast-export>
1996-09-14 20:58:17 +0000

    Synthetic commit for incomplete tag gnu

--- sys/arch/mvme68k/include/cdefs.h
+++
@@ -1,8 +0,0 @@
-/*	$OpenBSD$ */
-
-#ifndef _MACHINE_CDEFS_H_
-#define _MACHINE_CDEFS_H_
-
-#include <m68k/cdefs.h>
-
-#endif
--- usr.sbin/tcpdump/version.c
+++
@@ -1,4 +0,0 @@
-/*	$OpenBSD: version.c,v 1.3 1996/06/10 07:48:00 deraadt Exp $	*/
-/*	$NetBSD: version.c,v 1.3 1996/05/20 00:41:20 fvdl Exp $	*/
-
-char version[] = "3.2.0";
