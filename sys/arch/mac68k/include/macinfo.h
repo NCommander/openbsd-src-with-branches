92a388b46a59c6cc7754c8a83a079ddb8cb98147
briggs <briggs>
1996-10-28 15:00:49 +0000

    Remove a blank line.

--- sys/arch/mac68k/include/macinfo.h
+++ sys/arch/mac68k/include/macinfo.h
@@ -1,5 +1,4 @@
-
-/*	$OpenBSD: scsi96reg.h,v 1.4 1996/10/23 04:52:03 briggs Exp $	*/
+/*	$OpenBSD: macinfo.h,v 1.1 1996/10/28 14:34:07 briggs Exp $	*/
 /*	$NetBSD: scsi96reg.h,v 1.5 1996/05/05 06:18:02 briggs Exp $	*/
 
 /*
