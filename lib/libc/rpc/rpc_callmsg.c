e6131c193407c0e22b1860ffa23712cf2e120949
etheisen <etheisen>
1996-11-14 06:27:18 +0000

    memcpy needs string.h.

--- lib/libc/rpc/rpc_callmsg.c
+++ lib/libc/rpc/rpc_callmsg.c
@@ -28,7 +28,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char *rcsid = "$OpenBSD: rpc_callmsg.c,v 1.2 1996/07/20 06:12:37 deraadt Exp $";
+static char *rcsid = "$OpenBSD: rpc_callmsg.c,v 1.3 1996/08/19 08:31:45 tholo Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 /*
@@ -39,6 +39,7 @@ static char *rcsid = "$OpenBSD: rpc_callmsg.c,v 1.2 1996/07/20 06:12:37 deraadt 
  */
 
 #include <stdlib.h>
+#include <string.h>
 #include <sys/param.h>
 
 #include <rpc/rpc.h>
