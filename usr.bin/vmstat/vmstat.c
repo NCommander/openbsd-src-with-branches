0eb6f6e527e6bfd27d1929be4283e297cec83855
deraadt <deraadt>
1996-12-04 10:04:44 +0000

    roundup error; netbsd pr#2944, augustss@cs.chalmers.se

--- usr.bin/vmstat/vmstat.c
+++ usr.bin/vmstat/vmstat.c
@@ -1,5 +1,5 @@
 /*	$NetBSD: vmstat.c,v 1.29.4.1 1996/06/05 00:21:05 cgd Exp $	*/
-/*	$OpenBSD: vmstat.c,v 1.12 1996/06/22 17:38:14 tholo Exp $	*/
+/*	$OpenBSD: vmstat.c,v 1.13 1996/06/24 23:51:59 tholo Exp $	*/
 
 /*
  * Copyright (c) 1980, 1986, 1991, 1993
@@ -437,7 +437,7 @@ dovmstat(interval, reps)
 		 * We round upward to avoid losing low-frequency events
 		 * (i.e., >= 1 per interval but < 1 per second).
 		 */
-		halfuptime = (uptime + 1) / 2;
+		halfuptime = uptime == 1 ? 0 : (uptime + 1) / 2;
 		(void)sleep(interval);
 	}
 }
