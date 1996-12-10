5ee4f3123e889015699fb0b6d988e5dcf50022b4
millert <millert>
1996-12-10 22:22:03 +0000

    Flex 2.5.4

--- usr.bin/lex/libyywrap.c
+++ usr.bin/lex/libyywrap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: libyywrap.c,v 1.2 1996/06/26 05:35:37 deraadt Exp $	*/
+/*	$OpenBSD: libyywrap.c,v 1.3 1996/07/13 22:22:06 millert Exp $	*/
 
 /* libyywrap - flex run-time support library "yywrap" function */
 
@@ -8,7 +8,8 @@
 
 int yywrap __P((void));
 
-int yywrap()
+int
+yywrap()
 	{
 	return 1;
 	}
