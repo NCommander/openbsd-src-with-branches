d6a9324b132715d5b90b40b31047017141d16894
niklas <niklas>
1996-12-17 15:05:30 +0000

    Tagged wrong version, brought in the changes from 1.12 int the branch

--- sys/arch/atari/include/endian.h
+++
@@ -1,9 +0,0 @@
-/*	$OpenBSD: endian.h,v 1.1.1.1 1995/03/26 07:12:06 leo Exp $	*/
-/*	$NetBSD: endian.h,v 1.1.1.1 1995/03/26 07:12:06 leo Exp $	*/
-
-#ifndef _MACHINE_ENDIAN_H_
-#define _MACHINE_ENDIAN_H_
-
-#include <m68k/endian.h>
-
-#endif
--- sys/arch/mac68k/include/stdarg.h
+++
@@ -1,5 +0,0 @@
-/*	$OpenBSD$	*/
-/*	$NetBSD: stdarg.h,v 1.5 1994/10/26 08:46:48 cgd Exp $	*/
-
-/* Just use the common m68k definition */
-#include <m68k/stdarg.h>
