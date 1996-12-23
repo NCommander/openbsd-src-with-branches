f44cfcd0a258f791f06be3dcd3d66d8843888f09
mickey <mickey>
1996-12-23 13:22:49 +0000

    pppd 2.3b3 import. some minor buf oflow fixes and so. try it out ppl, but i've got it running talking to cisco w/ all the AFs enabled in kernel.

--- usr.sbin/pppd/pathnames.h
+++ usr.sbin/pppd/pathnames.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pathnames.h,v 1.2 1996/03/25 15:55:53 niklas Exp $	*/
+/*	$OpenBSD: pathnames.h,v 1.3 1996/07/20 12:02:13 joshd Exp $	*/
 
 /*
  * define path names
@@ -22,6 +22,7 @@
 #define _PATH_TTYOPT	"/etc/ppp/options."
 #define _PATH_CONNERRS	"/etc/ppp/connect-errors"
 #define _PATH_USEROPT	".ppprc"
+#define _PATH_PEERFILES	"/etc/ppp/peers/"
 
 #ifdef IPX_CHANGE
 #define _PATH_IPXUP	"/etc/ppp/ipx-up"
