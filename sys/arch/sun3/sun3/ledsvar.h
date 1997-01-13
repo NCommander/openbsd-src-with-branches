d117015f821b74271605c2565e6f7d8f3fec0b31
kstailey <kstailey>
1997-01-13 00:29:28 +0000

    back-panel LED control device derived from code by der Mouse

---
+++ sys/arch/sun3/sun3/ledsvar.h
@@ -0,0 +1,7 @@
+/*	$OpenBSD$	*/
+
+extern volatile unsigned int led_n_patterns;
+extern volatile unsigned int led_countmax;
+extern volatile const unsigned char * volatile led_patterns;
+extern volatile unsigned int led_countdown;
+extern volatile unsigned int led_px;
