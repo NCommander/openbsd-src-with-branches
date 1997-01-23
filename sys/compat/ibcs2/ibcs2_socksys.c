214933f513598d025b3739946fa8fab58142280a
niklas <niklas>
1997-01-23 16:22:57 +0000

    Sync with NetBSD 970118, missed...

--- sys/compat/ibcs2/ibcs2_socksys.c
+++ sys/compat/ibcs2/ibcs2_socksys.c
@@ -1,5 +1,5 @@
 /*	$OpenBSD: ibcs2_socksys.c,v 1.3 1996/08/02 20:35:10 niklas Exp $	*/
-/*	$NetBSD: ibcs2_socksys.c,v 1.4 1996/05/03 17:05:29 christos Exp $	*/
+/*	$NetBSD: ibcs2_socksys.c,v 1.7 1996/10/13 00:46:51 christos Exp $	*/
 
 /*
  * Copyright (c) 1994, 1995 Scott Bartram
@@ -131,8 +131,8 @@ ibcs2_socksys(p, v, retval)
 
 	default:
 		printf("socksys unknown %08x %08x %08x %08x %08x %08x %08x\n",
-		    realargs[0], realargs[1], realargs[2], realargs[3], 
-		    realargs[4], realargs[5], realargs[6]);
+                       realargs[0], realargs[1], realargs[2], realargs[3], 
+                       realargs[4], realargs[5], realargs[6]);
 		return EINVAL;
 	}
 	/* NOTREACHED */
