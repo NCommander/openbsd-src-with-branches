5a992833befd6d1fa47f2767c6d983cdd0796fc5
tholo <tholo>
1997-01-26 01:23:46 +0000

    Make ip_len and ip_off unsigned values; don't transmit or accept packets larger than the maximum IP packet size.  From NetBSD.

--- sys/netinet/udp_usrreq.c
+++ sys/netinet/udp_usrreq.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: udp_usrreq.c,v 1.6 1996/05/15 09:03:12 mickey Exp $	*/
+/*	$OpenBSD: udp_usrreq.c,v 1.7 1996/07/05 20:42:18 deraadt Exp $	*/
 /*	$NetBSD: udp_usrreq.c,v 1.28 1996/03/16 23:54:03 christos Exp $	*/
 
 /*
@@ -458,6 +458,15 @@ udp_output(m, va_alist)
 	if (m == 0) {
 		error = ENOBUFS;
 		goto bail;
+	}
+
+	/*
+	 * Compute the packet length of the IP header, and
+	 * punt if the length looks bogus.
+	 */
+	if ((len + sizeof(struct udpiphdr)) > IP_MAXPACKET) {
+		error = EMSGSIZE;
+		goto release;
 	}
 
 	/*
