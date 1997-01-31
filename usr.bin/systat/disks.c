c25583d8e2db1f5b3b659d8579f3518c7675a408
deraadt <deraadt>
1997-01-31 10:08:37 +0000

    need string.h

--- usr.bin/systat/disks.c
+++ usr.bin/systat/disks.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: disks.c,v 1.5 1996/06/26 05:40:05 deraadt Exp $	*/
+/*	$OpenBSD: disks.c,v 1.6 1997/01/17 07:13:23 millert Exp $	*/
 /*	$NetBSD: disks.c,v 1.4 1996/05/10 23:16:33 thorpej Exp $	*/
 
 /*-
@@ -38,9 +38,10 @@
 #if 0
 static char sccsid[] = "@(#)disks.c	8.1 (Berkeley) 6/6/93";
 #endif
-static char rcsid[] = "$OpenBSD: disks.c,v 1.5 1996/06/26 05:40:05 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: disks.c,v 1.6 1997/01/17 07:13:23 millert Exp $";
 #endif /* not lint */
 
+#include <string.h>
 #include "systat.h"
 #include "extern.h"
 static void dkselect(char *args, int truefalse, int selections[]);
