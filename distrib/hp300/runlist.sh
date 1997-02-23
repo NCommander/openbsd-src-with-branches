af64e5241a2d75289a8277092c764f9a1b316bdb
downsj <downsj>
1997-02-23 19:10:55 +0000

    Checkpoint.  This should be pretty close now, just gotta fix kernel bugs.

--- distrib/hp300/runlist.sh
+++ distrib/hp300/runlist.sh
@@ -1,11 +1,11 @@
 #	$OpenBSD$
-#	$NetBSD: runlist.sh,v 1.1 1995/07/18 04:13:01 briggs Exp $
+#	$NetBSD: runlist.sh,v 1.1 1995/10/03 22:47:57 thorpej Exp $
 
 if [ "X$1" = "X-d" ]; then
 	SHELLCMD=cat
 	shift
 else
-	SHELLCMD="sh"
+	SHELLCMD="sh -e"
 fi
 
 ( while [ "X$1" != "X" ]; do
