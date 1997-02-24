ac9852139aa3b45f2aeb8492a473f97bca861f99
niklas <niklas>
1997-02-24 14:49:39 +0000

    OpenBSD tag

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -
 #
+#	$OpenBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
