f4ca64dc2b5d4b9a093ebf0e930c1956e914768f
niklas <niklas>
1997-03-21 00:48:50 +0000

    New FIXUP_PC_AFTER_BREAK convention from NetBSD

--- sys/ddb/db_run.c
+++ sys/ddb/db_run.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: db_run.c,v 1.3 1996/03/11 11:16:20 mickey Exp $	*/
+/*	$OpenBSD: db_run.c,v 1.4 1996/04/21 22:19:10 deraadt Exp $	*/
 /*	$NetBSD: db_run.c,v 1.8 1996/02/05 01:57:12 christos Exp $	*/
 
 /* 
@@ -79,7 +79,7 @@ db_stop_at_pc(regs, is_breakpoint)
 	     * Breakpoint trap.  Fix up the PC if the
 	     * machine requires it.
 	     */
-	    FIXUP_PC_AFTER_BREAK
+	    FIXUP_PC_AFTER_BREAK(regs);
 	    pc = PC_REGS(regs);
 	}
 #endif
