497e4b1d6982228714f8b9f691901f66ba7987eb
millert <millert>
1997-04-05 22:36:19 +0000

    Strip leading '/' of pathnames (only in tar mode).  -S option turns this off like GNU tar.

--- bin/pax/extern.h
+++ bin/pax/extern.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: extern.h,v 1.10 1997/02/27 23:32:57 michaels Exp $	*/
+/*	$OpenBSD: extern.h,v 1.11 1997/03/02 09:46:43 tholo Exp $	*/
 /*	$NetBSD: extern.h,v 1.5 1996/03/26 23:54:16 mrg Exp $	*/
 
 /*-
@@ -232,6 +232,7 @@ extern int pmtime;
 extern int nodirs;
 extern int pmode;
 extern int pids;
+extern int rmleadslash;
 extern int exit_val;
 extern int docrc;
 extern char *dirptr;
