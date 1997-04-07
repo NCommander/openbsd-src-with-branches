b6b816f8a1a6ea2d71a1a2e73706b0afc61c2785
weingart <weingart>
1997-04-07 02:07:06 +0000

    Fix device name.

--- sys/arch/i386/stand/libsa/dev_i386.c
+++ sys/arch/i386/stand/libsa/dev_i386.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dev_i386.c,v 1.3 1997/03/31 23:06:28 mickey Exp $	*/
+/*	$OpenBSD: dev_i386.c,v 1.4 1997/04/06 20:08:25 mickey Exp $	*/
 
 /*
  * Copyright (c) 1996 Michael Shalayeff
@@ -95,8 +95,17 @@ devboot(bootdev, p)
 	else
 		*p++ = 'f';
 	*p++ = 'd';
+#ifndef _TEST
+	*p++ = '(';
+#endif
 	*p++ = '0' + (bootdev & 0x7f);
+#ifndef _TEST
+	*p++ = ',';
+#endif
 	*p++ = 'a';
+#ifndef _TEST
+	*p++ = ')';
+#endif
 	*p = '\0';
 }
 
