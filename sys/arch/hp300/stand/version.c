afa92582f1e2497c590b428c97abeb22b566e368
downsj <downsj>
1997-04-16 12:38:26 +0000

    Add a proper version number ala other ports, and clean up various parts of the Makefiles.  Lots more to do...

---
+++ sys/arch/hp300/stand/version.c
@@ -0,0 +1,12 @@
+/*	$OpenBSD$	*/
+
+/*
+ * Record major changes in the boot code here, and increment the version
+ * number.
+ */
+
+/*
+ * 2.0			INITIAL REVISION
+ */
+
+const char version[] = "2.0";
