e2084577a61c64726277681091f9c089c2183053
deraadt <deraadt>
1997-04-18 09:57:29 +0000

    indent

--- sys/nfs/nfs_vnops.c
+++ sys/nfs/nfs_vnops.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: nfs_vnops.c,v 1.13 1997/01/31 10:33:46 deraadt Exp $	*/
+/*	$OpenBSD: nfs_vnops.c,v 1.14 1997/04/08 22:46:50 deraadt Exp $	*/
 /*	$NetBSD: nfs_vnops.c,v 1.62.4.1 1996/07/08 20:26:52 jtc Exp $	*/
 
 /*
@@ -1542,7 +1542,7 @@ nfs_rename(v)
 	 * rename of the new file over it.
 	 */
 	if (tvp && tvp->v_usecount > 1 && !VTONFS(tvp)->n_sillyrename &&
-		tvp->v_type != VDIR && !nfs_sillyrename(tdvp, tvp, tcnp)) {
+	    tvp->v_type != VDIR && !nfs_sillyrename(tdvp, tvp, tcnp)) {
 		vrele(tvp);
 		tvp = NULL;
 	}
