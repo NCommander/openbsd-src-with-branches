bfaa098c1813d1d1ed535253dcab5dbb2e0886a9
gvf <gvf>
1997-04-22 16:13:44 +0000

    Add libz and tick version # to reflect change.

--- sys/arch/mvme68k/stand/bootxx/version.c
+++ sys/arch/mvme68k/stand/bootxx/version.c
@@ -1,8 +1,8 @@
-/*	$OpenBSD$ */
+/*	$OpenBSD: version.c,v 1.1 1996/05/16 02:37:26 chuck Exp $ */
 
 /*
  * make a random change to this file when you want the bootblock
- * revision to increase. like change this x to a y, or something.
+ * revision to increase. like change this y to an x, or something.
  */
 
-char *version = "$Revision: 0.0 $";
+char *version = "$Revision: 1.1 $";
