2a403f905792b5555550256f078c4d96603310f7
millert <millert>
1997-04-28 20:45:02 +0000

    - cast usages of *printf() to void since we don't check return val - move an assert to be *before* a strcpy() where it can do some good. - integrate NetBSD fixes for 64-bit machines (NetBSD PR #3450, Ross Harvey) - add lite2 tags

--- lib/libc/regex/regfree.c
+++ lib/libc/regex/regfree.c
@@ -33,10 +33,16 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
+ *
+ *	@(#)regfree.c	8.3 (Berkeley) 3/20/94
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD: regfree.c,v 1.4 1995/02/27 13:29:56 cgd Exp $";
+#if 0
+static char sccsid[] = "@(#)regfree.c	8.3 (Berkeley) 3/20/94";
+#else
+static char rcsid[] = "$OpenBSD: regfree.c,v 1.2 1996/08/19 08:31:16 tholo Exp $";
+#endif
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
