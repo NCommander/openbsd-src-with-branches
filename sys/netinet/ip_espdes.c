5ed0a4e6a1e82661d5f4109e16947d8e67a4f216
provos <provos>
1997-06-24 20:57:29 +0000

    use ntohs instead of NTOHS for counters

--- sys/netinet/ip_espdes.c
+++ sys/netinet/ip_espdes.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_espdes.c,v 1.5 1997/06/21 00:09:18 deraadt Exp $	*/
+/*	$OpenBSD: ip_espdes.c,v 1.6 1997/06/24 12:15:23 provos Exp $	*/
 
 /*
  * The author of this code is John Ioannidis, ji@tla.org,
@@ -288,7 +288,7 @@ espdes_input(struct mbuf *m, struct tdb *tdb)
 
     /* Update the counters */
     tdb->tdb_packets++;
-    tdb->tdb_bytes += NTOHS(ip->ip_len) - (ip->ip_hl << 2) + blk[6] + 2;
+    tdb->tdb_bytes += ntohs(ip->ip_len) - (ip->ip_hl << 2) + blk[6] + 2;
 
     return m;
 }
