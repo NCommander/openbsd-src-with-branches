9023e870436fdbf6a4e74dbb10728da3f03a2f3c
tholo <tholo>
1997-06-28 03:29:57 +0000

    Latest version from Cyclic Software

--- gnu/usr.bin/cvs/os2/rcmd.h
+++ gnu/usr.bin/cvs/os2/rcmd.h
@@ -1,6 +1,16 @@
 /* rcmd.h --- interface to executing commands on remote hosts
    Karl Fogel <kfogel@cyclic.com> --- November 1995  */
 
+/* This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.  */
+
 /* Run the command CMD on the host *AHOST, and return a file descriptor for
    a bidirectional stream socket connected to the command's standard input
    and output.
