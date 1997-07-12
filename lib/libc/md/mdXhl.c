c54dcf177b44fd54ecf7d3c4c7d53f1e6c5aa811
millert <millert>
1997-07-12 20:30:57 +0000

    #include <md5.h> not "md5.h"

--- lib/libc/md/mdXhl.c
+++ lib/libc/md/mdXhl.c
@@ -8,7 +8,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD: mdXhl.c,v 1.5 1996/11/24 02:25:59 niklas Exp $";
+static char rcsid[] = "$OpenBSD: mdXhl.c,v 1.6 1997/03/18 06:36:51 flipk Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <stdlib.h>
@@ -18,7 +18,7 @@ static char rcsid[] = "$OpenBSD: mdXhl.c,v 1.5 1996/11/24 02:25:59 niklas Exp $"
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
-#include "mdX.h"
+#include <mdX.h>
 
 /* ARGSUSED */
 char *
