f3987b17583e53d5ac048feb346394c1fe7badde
kstailey <kstailey>
1997-07-23 14:45:15 +0000

    tabify

--- bin/mkdir/mkdir.c
+++ bin/mkdir/mkdir.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mkdir.c,v 1.3 1996/08/02 12:40:58 deraadt Exp $	*/
+/*	$OpenBSD: mkdir.c,v 1.4 1996/08/14 03:49:14 tholo Exp $	*/
 /*	$NetBSD: mkdir.c,v 1.14 1995/06/25 21:59:21 mycroft Exp $	*/
 
 /*
@@ -44,7 +44,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)mkdir.c	8.2 (Berkeley) 1/25/94";
 #else
-static char rcsid[] = "$OpenBSD: mkdir.c,v 1.3 1996/08/02 12:40:58 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: mkdir.c,v 1.4 1996/08/14 03:49:14 tholo Exp $";
 #endif
 #endif /* not lint */
 
@@ -154,7 +154,7 @@ mkpath(path, mode, dir_mode)
 				return (-1);
 			}
 		} else if (!S_ISDIR(sb.st_mode)) {
-		        warnx("%s: %s", path, strerror(ENOTDIR));
+			warnx("%s: %s", path, strerror(ENOTDIR));
 			return (-1);
 		}
 		    
