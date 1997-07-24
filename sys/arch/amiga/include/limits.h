17ed467f1dace716ba97aa6d12dae9162d5b520e
deraadt <deraadt>
1997-07-24 14:48:57 +0000

    multiple inclusion protection; brenner@umr.edu

--- sys/arch/amiga/include/limits.h
+++ sys/arch/amiga/include/limits.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: limits.h,v 1.8 1994/10/26 02:06:16 cgd Exp $	*/
+/*	$OpenBSD: limits.h,v 1.2 1997/01/16 09:25:56 niklas Exp $	*/
 /*	$NetBSD: limits.h,v 1.8 1994/10/26 02:06:16 cgd Exp $	*/
 
 #ifndef _MACHINE_LIMITS_H_
@@ -6,4 +6,4 @@
 
 #include <m68k/limits.h>
 
-#endif
+#endif /* _MACHINE_LIMITS_H_ */
--- sys/arch/mvme68k/include/limits.h
+++ sys/arch/mvme68k/include/limits.h
@@ -1,4 +1,9 @@
 /*	$OpenBSD$ */
 
+#ifndef _MACHINE_LIMITS_H_
+#define _MACHINE_LIMITS_H_
+
 /* Just use the common m68k definition */
 #include <m68k/limits.h>
+
+#endif /* _MACHINE_LIMITS_H_ */
