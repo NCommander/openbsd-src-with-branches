770c5bf336e38e5fa9fb199cbc26d9ab3e77ecbd
deraadt <deraadt>
1997-08-05 22:22:58 +0000

    make signal handlers errno safe

--- bin/csh/csh.c
+++ bin/csh/csh.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: csh.c,v 1.6 1997/07/23 14:36:48 kstailey Exp $	*/
+/*	$OpenBSD: csh.c,v 1.7 1997/07/25 18:57:59 mickey Exp $	*/
 /*	$NetBSD: csh.c,v 1.14 1995/04/29 23:21:28 mycroft Exp $	*/
 
 /*-
@@ -44,7 +44,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)csh.c	8.2 (Berkeley) 10/12/93";
 #else
-static char rcsid[] = "$OpenBSD: csh.c,v 1.6 1997/07/23 14:36:48 kstailey Exp $";
+static char rcsid[] = "$OpenBSD: csh.c,v 1.7 1997/07/25 18:57:59 mickey Exp $";
 #endif
 #endif /* not lint */
 
@@ -930,7 +930,10 @@ void
 pintr(notused)
 	int notused;
 {
+    int save_errno = errno;
+
     pintr1(1);
+    errno = save_errno;
 }
 
 void
