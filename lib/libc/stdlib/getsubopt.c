298420a1d344c2922ff3e154fd7ea542c7d59937
millert <millert>
1997-08-20 04:02:18 +0000

    Move getsubopt(3) to stdlib like lite2 and XPG4.2 say... Update man page from lite2 while I'm at it.

--- lib/libc/stdlib/getsubopt.c
+++ lib/libc/stdlib/getsubopt.c
@@ -1,6 +1,8 @@
+/*	$OpenBSD$	*/
+
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -31,9 +33,13 @@
  * SUCH DAMAGE.
  */
 
-#if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD: getsubopt.c,v 1.5 1995/06/16 07:15:18 jtc Exp $";
-#endif /* LIBC_SCCS and not lint */
+#ifndef lint
+#if 0
+static char sccsid[] = "@(#)getsubopt.c	8.1 (Berkeley) 6/4/93";
+#else
+static char rcsid[] = "$OpenBSD: getsubopt.c,v 1.2 1996/08/19 08:24:06 tholo Exp $";
+#endif
+#endif /* not lint */
 
 #include <unistd.h>
 #include <stdlib.h>
