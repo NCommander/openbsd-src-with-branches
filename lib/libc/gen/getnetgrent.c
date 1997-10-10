8ec6bc33c4a128c6f035723bc4b53cdab8fb1d74
deraadt <deraadt>
1997-10-10 23:07:30 +0000

    NO abort() CALLS IN LIBC

--- lib/libc/gen/getnetgrent.c
+++ lib/libc/gen/getnetgrent.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: getnetgrent.c,v 1.5 1997/02/03 00:10:08 millert Exp $	*/
+/*	$OpenBSD: getnetgrent.c,v 1.6 1997/07/23 21:04:05 kstailey Exp $	*/
 
 /*
  * Copyright (c) 1994 Christos Zoulas
@@ -32,7 +32,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char *rcsid = "$OpenBSD: getnetgrent.c,v 1.5 1997/02/03 00:10:08 millert Exp $";
+static char *rcsid = "$OpenBSD: getnetgrent.c,v 1.6 1997/07/23 21:04:05 kstailey Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -299,10 +299,6 @@ lookup(ypdom, name, line, bywhat)
 		case _NG_KEYBYHOST:
 			map = "netgroup.byhost";
 			break;
-
-		default:
-			abort();
-			break;
 		}
 
 
@@ -415,10 +411,6 @@ addgroup(ypdom, sl, grp)
 
 		case _NG_ERROR:
 			return;
-
-		default:
-			abort();
-			return;
 		}
 	}
 }
@@ -517,10 +509,6 @@ in_find(ypdom, sl, grp, host, user, domain)
 
 		case _NG_ERROR:
 			free(line);
-			return 0;
-
-		default:
-			abort();
 			return 0;
 		}
 	}
