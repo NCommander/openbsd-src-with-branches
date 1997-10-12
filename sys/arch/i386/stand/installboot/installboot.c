08c8b4896f555def063a0b132271764f78dfbaee
weingart <weingart>
1997-10-12 19:39:48 +0000

    One more sanity check.  If no blocks to load, complain bitterly.

--- sys/arch/i386/stand/installboot/installboot.c
+++ sys/arch/i386/stand/installboot/installboot.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: installboot.c,v 1.17 1997/10/03 21:36:07 deraadt Exp $	*/
+/*	$OpenBSD: installboot.c,v 1.18 1997/10/07 08:56:18 mickey Exp $	*/
 /*	$NetBSD: installboot.c,v 1.5 1995/11/17 23:23:50 gwr Exp $ */
 
 /*
@@ -425,6 +425,8 @@ loadblocknums(boot, devfd, dl)
 	 * Have the inode.  Figure out how many blocks we need.
 	 */
 	ndb = howmany(ip->di_size, fs->fs_bsize);
+	if (ndb <= 0)
+		errx(1, "No blocks to load");
 	if (ndb > maxblocknum)
 		errx(1, "Too many blocks");
 	if (verbose)
