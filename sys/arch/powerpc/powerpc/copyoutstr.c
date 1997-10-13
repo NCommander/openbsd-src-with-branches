f3418aa397b9e111203848a205db58d2279b0fd5
pefo <pefo>
1997-10-13 13:43:02 +0000

    Monolithic (No OpenFirmware driver crap...) PowerPC kernel. This is the first release based on Wolfgang Solfrank, TooLs GmbH, work. Most OFW stuff has been ripped out. Only a few functions for memory probing and halt/reboot functionality has been kept. The kernel currently works with V.I's power.4e PowerPC604 board, but more will come.

--- sys/arch/powerpc/powerpc/copyoutstr.c
+++ sys/arch/powerpc/powerpc/copyoutstr.c
@@ -1,5 +1,4 @@
-/*	$OpenBSD:$	*/
-/*	$NetBSD: copyoutstr.c,v 1.1 1996/09/30 16:34:42 ws Exp $	*/
+/*	$OpenBSD: copyoutstr.c,v 1.2 1996/12/28 06:21:45 rahnds Exp $	*/
 
 /*-
  * Copyright (C) 1995 Wolfgang Solfrank.
