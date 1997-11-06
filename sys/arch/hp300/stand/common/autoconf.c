fb1e49962bbd876db9faa6c4fb8a90d3604d20b7
millert <millert>
1997-11-06 19:42:45 +0000

    Idnetify/support hp385 (33Mhz 68040)

--- sys/arch/hp300/stand/common/autoconf.c
+++ sys/arch/hp300/stand/common/autoconf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: autoconf.c,v 1.3 1997/02/03 04:48:01 downsj Exp $	*/
+/*	$OpenBSD: autoconf.c,v 1.1 1997/07/14 08:14:07 downsj Exp $	*/
 /*	$NetBSD: autoconf.c,v 1.12 1997/01/30 10:32:51 thorpej Exp $	*/
 
 /*
@@ -107,6 +107,7 @@ configure()
 	case HP_380:
 		cpuspeed = MHZ_25 * 2;	/* XXX */
 		break;
+	case HP_385:
 	case HP_433:
 		cpuspeed = MHZ_33 * 2;	/* XXX */
 		break;
