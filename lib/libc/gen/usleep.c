55774c82e158c5d542cbd522541362212306aa1f
millert <millert>
1997-11-06 01:25:49 +0000

    XPG4.2 compat:     1) usleep() and ualarm() use useconds_t not u_int     2) usleep() returns an int

--- lib/libc/gen/usleep.c
+++ lib/libc/gen/usleep.c
@@ -32,22 +32,23 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD: usleep.c,v 1.4 1996/09/15 09:31:11 tholo Exp $";
+static char rcsid[] = "$OpenBSD: usleep.c,v 1.5 1997/04/25 04:20:42 tholo Exp $";
 #endif /* LIBC_SCCS and not lint */
 
+#include <sys/types.h>
 #include <sys/time.h>
 
-void
+int
 usleep(useconds)
-	unsigned int useconds;
+	useconds_t useconds;
 {
 	struct timespec rqt;
 
 	if (useconds == 0)
-		return;
+		return(0);
 
 	rqt.tv_sec = useconds / 1000000;
 	rqt.tv_nsec = (useconds % 1000000) * 1000;
 
-	nanosleep(&rqt, NULL);
+	return(nanosleep(&rqt, NULL));
 }
