70c44870faaa0a91adaf28d08f7a3e6669fc5ce4
niklas <niklas>
1997-11-16 12:07:28 +0000

    Revert one of the $_ changes made 970102, as it makes sh crash when mail arrives and mailchecking is on.  I doubt this case was meant to be altered at that time judging from the documentation fix it went along with. However to be really sure I would like to see what POSIX has to say about the mail notification message format.  Is $_ meant to be expanded to the mailbox path?  If so, this revertion is correct, otherwise, another fix ought to be made.  I have no POSIX spec though.

--- bin/pdksh/mail.c
+++ bin/pdksh/mail.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mail.c,v 1.3 1997/01/02 17:37:27 downsj Exp $	*/
+/*	$OpenBSD: mail.c,v 1.5 1997/06/19 13:58:44 kstailey Exp $	*/
 
 /*
  * Mailbox checking code by Robert J. Gibson, adapted for PD ksh by
@@ -180,7 +180,15 @@ mbox_t	*mbp;
 {
 	struct tbl	*vp;
 
+#if 0
+	/*
+	 * I doubt this $_ overloading is bad in /bin/sh mode.  Anyhow, we
+	 * crash as the code looks now if we do not set vp.  Now, this is
+	 * easy to fix too, but I'd like to see what POSIX says before doing
+	 * a change like that.
+	 */
 	if (!Flag(FSH))
+#endif
 		setstr((vp = local("_", FALSE)), mbp->mb_path);
 
 	shellf("%s\n", substitute(mbp->mb_msg ? mbp->mb_msg : MBMESSAGE, 0));
