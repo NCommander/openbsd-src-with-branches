235d2b75728229da85a785cb03c682dabc8e9d1d
gene <gene>
1997-11-30 06:12:36 +0000

    Canonify the _*_H_ namespace appropriately. Also, clean up comments and wrap with _KERNEL correctly.

--- sys/arch/mac68k/include/cdefs.h
+++ sys/arch/mac68k/include/cdefs.h
@@ -1,9 +1,9 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: cdefs.h,v 1.3 1996/05/26 18:35:45 briggs Exp $	*/
 /*	$NetBSD: cdefs.h,v 1.2 1996/05/05 06:17:34 briggs Exp $	*/
 
-#ifndef _CDEFS_MACHINE_
-#define _CDEFS_MACHINE_
+#ifndef _MAC68K_CDEFS_H_
+#define _MAC68K_CDEFS_H_
 
 #include <m68k/cdefs.h>
 
-#endif /* _CDEFS_MACHINE_ */
+#endif /* _MAC68K_CDEFS_H_ */
