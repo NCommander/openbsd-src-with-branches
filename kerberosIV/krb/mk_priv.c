531495a1951a8ce25b4e973a37cdae8739d88f56
art <art>
1997-12-12 05:30:34 +0000

    Another upgrade of kth-krb + many fixes of strcpys and unchecked pointers.

--- kerberosIV/krb/mk_priv.c
+++ kerberosIV/krb/mk_priv.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: mk_priv.c,v 1.4 1997/12/09 07:57:29 art Exp $	*/
 /* $KTH: mk_priv.c,v 1.18 1997/04/01 08:18:37 joda Exp $ */
 
 /*
@@ -42,7 +42,7 @@
 #include "krb_locl.h"
 
 /* application include files */
-#include "lsb_addr_comp.h"
+#include "krb-archaeology.h"
 
 /*
  * krb_mk_priv() constructs an AUTH_MSG_PRIVATE message.  It takes
