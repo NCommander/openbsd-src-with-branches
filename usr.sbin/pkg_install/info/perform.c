6e995b13452bc39457fc8184e3560db86548445d
gene <gene>
1997-12-14 09:42:19 +0000

    Invoke raging paranoia on strcpy's that should be strncpy's. These should be checked in all but one case anyway, but you never know.

--- usr.sbin/pkg_install/info/perform.c
+++ usr.sbin/pkg_install/info/perform.c
@@ -1,7 +1,7 @@
-/*	$OpenBSD: perform.c,v 1.1 1996/06/04 07:56:10 niklas Exp $	*/
+/*	$OpenBSD: perform.c,v 1.2 1996/06/04 08:43:39 niklas Exp $	*/
 
 #ifndef lint
-static const char *rcsid = "$OpenBSD: perform.c,v 1.1 1996/06/04 07:56:10 niklas Exp $";
+static const char *rcsid = "$OpenBSD: perform.c,v 1.2 1996/06/04 08:43:39 niklas Exp $";
 #endif
 
 /*
@@ -91,7 +91,7 @@ pkg_do(char *pkg)
 
     if (isURL(pkg)) {
 	if ((cp = fileGetURL(NULL, pkg)) != NULL) {
-	    strcpy(fname, cp);
+	    strncpy(fname, cp, FILENAME_MAX);
 	    isTMP = TRUE;
 	}
     }
@@ -105,7 +105,7 @@ pkg_do(char *pkg)
 	    snprintf(&fname[len], FILENAME_MAX - len, "/%s", pkg);
 	}
 	else
-	    strcpy(fname, pkg);
+	    strncpy(fname, pkg, FILENAME_MAX);
 	cp = fname;
     }
     else {
