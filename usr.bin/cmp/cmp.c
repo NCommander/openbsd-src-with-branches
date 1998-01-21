6a127e8097493c18d4cfd0d1a6bedb69b855289f
deraadt <deraadt>
1998-01-21 22:16:32 +0000

    error

--- usr.bin/cmp/cmp.c
+++ usr.bin/cmp/cmp.c
@@ -1,4 +1,4 @@
-/*      $OpenBSD: cmp.c,v 1.5 1997/01/15 23:42:19 millert Exp $      */
+/*      $OpenBSD: cmp.c,v 1.6 1997/06/12 19:45:21 deraadt Exp $      */
 /*      $NetBSD: cmp.c,v 1.7 1995/09/08 03:22:56 tls Exp $      */
 
 /*
@@ -44,7 +44,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)cmp.c	8.3 (Berkeley) 4/2/94";
 #else
-static char rcsid[] = "$OpenBSD: cmp.c,v 1.5 1997/01/15 23:42:19 millert Exp $";
+static char rcsid[] = "$OpenBSD: cmp.c,v 1.6 1997/06/12 19:45:21 deraadt Exp $";
 #endif
 #endif /* not lint */
 
@@ -164,6 +164,6 @@ usage()
 {
 
 	(void)fprintf(stderr,
-	    "usage: cmp [-l | s] file1 file2 [skip1 [skip2]]\n");
+	    "usage: cmp [-l | -s] file1 file2 [skip1 [skip2]]\n");
 	exit(ERR_EXIT);
 }
