73111102c78f8ddd2d9953bbb01f9fba34cb9db5
niklas <niklas>
1998-02-22 01:06:10 +0000

    KNF

--- sys/compat/svr4/svr4_ncr.c
+++ sys/compat/svr4/svr4_ncr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	 */
+/*	$OpenBSD: svr4_ncr.c,v 1.2 1996/10/17 19:15:50 niklas Exp $	 */
 
 /*
  * Copyright (c) 1995, 1996 Niklas Hallqvist
@@ -47,7 +47,7 @@ svr4_ncr_sys_rdebug(p, v, retval)
 	void *v;
 	register_t *retval;
 {
-	return ENXIO;
+	return (ENXIO);
 }
 
 #endif /* COMPAT_SVR4_NCR */
