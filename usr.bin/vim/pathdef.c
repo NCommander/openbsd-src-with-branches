5837961c0104474ab5f33d3864e510652cf6e918
downsj <downsj>
1998-02-23 12:07:48 +0000

    This belongs in ports.

--- usr.bin/vim/pathdef.c
+++
@@ -1,7 +0,0 @@
-/*	$OpenBSD$	*/
-#include "vim.h"
-char_u *sys_compatrc_fname = (char_u *)SYS_COMPATRC_FILE;
-char_u *sys_vimrc_fname = (char_u *)SYS_VIMRC_FILE;
-char_u *sys_gvimrc_fname = (char_u *)SYS_GVIMRC_FILE;
-char_u *help_fname = (char_u *)VIM_HLP;
-char_u *all_cflags = (char_u *)"";
