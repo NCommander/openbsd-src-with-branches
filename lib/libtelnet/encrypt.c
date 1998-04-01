977460ba5c9fe7936535cc3f5d3f8122b98a61ed
deraadt <deraadt>
1998-04-01 11:32:02 +0000

    eavesdropping messages, no sorry, no way

--- lib/libtelnet/encrypt.c
+++ lib/libtelnet/encrypt.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: encrypt.c,v 1.3 1998/03/12 04:48:48 art Exp $	*/
 /* $KTH: encrypt.c,v 1.19 1997/11/02 03:58:03 assar Exp $ */
 
 /*-
@@ -502,11 +502,6 @@ EncryptAutoDec(int on)
 void
 encrypt_not(void)
 {
-    if (encrypt_verbose)
-  	printf("[ Connection is NOT encrypted ]\r\n");
-    else
-  	printf("\r\n*** Connection not encrypted! "
-	       "Communication may be eavesdropped. ***\r\n");
 }
 
 /*
