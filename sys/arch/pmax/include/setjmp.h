cb6191208fa4417c30d47c7b1f0c12a9aaa50d7b
millert <millert>
1998-05-08 17:34:53 +0000

    use what we can from mips/include

--- sys/arch/pmax/include/setjmp.h
+++ sys/arch/pmax/include/setjmp.h
@@ -1,7 +1,3 @@
-/*	$NetBSD: setjmp.h,v 1.1 1994/12/20 10:37:05 cgd Exp $	*/
+/*	$OpenBSD$	*/
 
-/*
- * machine/setjmp.h: machine dependent setjmp-related information.
- */
-
-#define	_JBLEN	83		/* size, in longs, of a jmp_buf */
+#include <mips/setjmp.h>
