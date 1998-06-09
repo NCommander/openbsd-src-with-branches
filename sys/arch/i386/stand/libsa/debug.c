f7c4e9492449bd1ef51cab1f62305fe20a05447c
mickey <mickey>
1998-06-09 13:45:07 +0000

    there is no such thing as dprintf''

--- sys/arch/i386/stand/libsa/debug.c
+++ sys/arch/i386/stand/libsa/debug.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: debug.c,v 1.5 1998/04/18 07:39:44 deraadt Exp $	*/
+/*	$OpenBSD: debug.c,v 1.6 1998/04/18 22:24:25 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1997 Michael Shalayeff
@@ -35,12 +35,6 @@
 #include <lib/libsa/stand.h>
 #include <debug.h>
 #include <dev/cons.h>
-
-#ifdef __STDC__
-void dprintf __P((const char *fmt, ...));
-#else
-void dprintf ();
-#endif
 
 #define	VBASE	(0xb8000)
 
