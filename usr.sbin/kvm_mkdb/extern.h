a553d479e9b53ecd4e19718d1aca50c5887b897d
millert <millert>
1998-08-19 06:47:55 +0000

    Make kvm_mkdb work again  o testdb() needs to take as an arg the name of the kernel so it can do    the version test correctly.  o add undocumented verbose flag to tell when we are rebuilding the .db file  o DO_* -> _NLIST_DO_*, this prevented kvm_mkdb from doing anything at all.  o preserve a few error messages so you get error output when none of    the exec types match the target.  o check malloc return values  o get_kerntext should not subtract the sizeof(struct exec) from kernel_text

--- usr.sbin/kvm_mkdb/extern.h
+++ usr.sbin/kvm_mkdb/extern.h
@@ -31,9 +31,8 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)extern.h	8.1 (Berkeley) 6/6/93
- *	$OpenBSD: extern.h,v 1.1.1.1 1995/10/18 08:47:39 deraadt Exp $
+ *	$OpenBSD: extern.h,v 1.2 1997/01/15 22:08:15 millert Exp $
  */
 
 void	create_knlist __P((char *, DB *));
-void	error __P((char *));
-int	testdb __P(());
+int	testdb __P((char *));
