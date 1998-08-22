c9856eb90b6c74ae272e6f39327dd06e4fd9306f
smurph <smurph>
1998-08-22 07:17:23 +0000

    Changed bootsd to work with bootxx bootblocks.

--- sys/arch/mvme88k/stand/bootsd/version.c
+++ sys/arch/mvme88k/stand/bootsd/version.c
@@ -1,8 +1,8 @@
-/*	$Id: version.c,v 1.3 1995/12/06 10:54:56 deraadt Exp $ */
+/*	$OpenBSD: version.c,v 1.7 1997/04/22 16:13:39 gvf Exp $ */
 
 /*
  * make a random change to this file when you want the bootblock
- * revision to increase. like change this x to a y, or something.
+ * revision to increase. like change this q to an x, or something.
  */
 
-char *version = "$Revision: 1.3 $";
+char *version = "$Revision: 1.2 $";
