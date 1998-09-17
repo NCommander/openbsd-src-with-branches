aa410ce4c270ba4d3c90d5148455867d053508f1
marc <marc>
1998-09-17 20:34:41 +0000

    add NO_APMPRINT conditional

--- sys/arch/i386/i386/apm.c
+++ sys/arch/i386/i386/apm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: apm.c,v 1.20 1998/04/27 23:39:31 deraadt Exp $	*/
+/*	$OpenBSD: apm.c,v 1.21 1998/07/18 02:40:33 marc Exp $	*/
 
 /*-
  * Copyright (c) 1995 John T. Kohl.  All rights reserved.
@@ -208,6 +208,7 @@ apm_power_print (sc, regs)
 	struct apm_softc *sc;
 	struct apmregs *regs;
 {
+#if !defined(APM_NOPRINT)
 	sc->batt_life = BATT_LIFE(regs);
 	if (BATT_LIFE(regs) != APM_BATT_LIFE_UNKNOWN) {
 		printf("%s: battery life expectancy %d%%\n",
@@ -275,6 +276,7 @@ apm_power_print (sc, regs)
 	}
 
 	printf("\n");
+#endif
 }
 
 /*
