28215fc1c1b3bd491a3e1c111200d0932156f15d
art <art>
1998-11-15 16:45:16 +0000

    GC unnecessary declaration

--- sys/kern/kern_synch.c
+++ sys/kern/kern_synch.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_synch.c,v 1.11 1997/11/06 05:58:19 csapuntz Exp $	*/
+/*	$OpenBSD: kern_synch.c,v 1.12 1998/02/03 19:06:25 deraadt Exp $	*/
 /*	$NetBSD: kern_synch.c,v 1.37 1996/04/22 01:38:37 christos Exp $	*/
 
 /*-
@@ -294,7 +294,6 @@ tsleep(ident, priority, wmesg, timo)
 	register int s;
 	int sig, catch = priority & PCATCH;
 	extern int cold;
-	void endtsleep __P((void *));
 
 #ifdef KTRACE
 	if (KTRPOINT(p, KTR_CSW))
