4c71e45a46be0413df4737aa2ea15765f162d9ee
art <art>
1998-11-18 10:43:04 +0000

    atoi does not do error checking on numbers, change to strtol. pr system/641 From <lha@stacken.kth.se>

--- sbin/modunload/modunload.c
+++ sbin/modunload/modunload.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: modunload.c,v 1.4 1996/08/05 11:05:21 mickey Exp $	*/
+/*	$OpenBSD: modunload.c,v 1.5 1997/01/15 23:41:14 millert Exp $	*/
 /*	$NetBSD: modunload.c,v 1.9 1995/05/28 05:23:05 jtc Exp $	*/
 
 /*
@@ -74,12 +74,15 @@ main(argc, argv)
 	int c;
 	int modnum = -1;
 	char *modname = NULL;
+	char *endptr;
 	struct lmc_unload ulbuf;
 
 	while ((c = getopt(argc, argv, "i:n:")) != -1) {
 		switch (c) {
 		case 'i':
-			modnum = atoi(optarg);
+			modnum = strtol(optarg, &endptr, 0);
+			if (*endptr != '\0')
+                                errx(1, "not a valid number");
 			break;	/* number */
 		case 'n':
 			modname = optarg;
