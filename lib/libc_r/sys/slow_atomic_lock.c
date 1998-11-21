839fe1ba5527e2f6e13e72be74ce2d97d2d3a7ce
d <d>
1998-11-21 14:02:10 +0000

    missed an include

--- lib/libc_r/sys/slow_atomic_lock.c
+++ lib/libc_r/sys/slow_atomic_lock.c
@@ -1,5 +1,6 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: slow_atomic_lock.c,v 1.1 1998/11/20 11:15:38 d Exp $	*/
 
+#include <pthread.h>
 #include "pthread_private.h"
 #include "spinlock.h"
 #include <signal.h>
