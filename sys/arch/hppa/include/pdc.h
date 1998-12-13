699841da6b4ed11c0353124d9bb805e55106f9ee
mickey <mickey>
1998-12-13 06:39:10 +0000

    define PDC arguments alignment

--- sys/arch/hppa/include/pdc.h
+++ sys/arch/hppa/include/pdc.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pdc.h,v 1.6 1998/11/23 03:28:22 mickey Exp $	*/
+/*	$OpenBSD: pdc.h,v 1.7 1998/12/05 17:34:04 mickey Exp $	*/
 
 /*
  * Copyright (c) 1990 mt Xinu, Inc.  All rights reserved.
@@ -91,6 +91,7 @@
 #define	MINIOSIZ	64		/* minimum buffer size for IODC call */
 #define	MAXIOSIZ	(64 * 1024)	/* maximum buffer size for IODC call */
 
+#define	PDC_ALIGNMENT	__attribute__ ((aligned(8)))
 
 /*
  * The PDC Entry Points and their arguments...
