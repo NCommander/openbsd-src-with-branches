eab2959d588e47ad6d941e8dabf21c9342e93509
marc <marc>
1998-12-16 19:26:31 +0000

    add mirror-distfiles as a known target

--- share/mk/bsd.port.subdir.mk
+++ share/mk/bsd.port.subdir.mk
@@ -1,5 +1,5 @@
 #	from: @(#)bsd.subdir.mk	5.9 (Berkeley) 2/1/91
-#	$OpenBSD: bsd.port.subdir.mk,v 1.1 1996/06/03 22:47:11 niklas Exp $
+#	$OpenBSD: bsd.port.subdir.mk,v 1.3 1997/09/09 15:11:29 imp Exp $
 #	FreeBSD Id: bsd.port.subdir.mk,v 1.20 1997/08/22 11:16:15 asami Exp
 #
 # The include file <bsd.port.subdir.mk> contains the default targets
@@ -80,7 +80,8 @@ ${SUBDIR}::
 	${MAKE} all
 
 .for __target in all fetch fetch-list package extract configure \
-		 build clean depend describe distclean reinstall tags checksum
+		 build clean depend describe distclean reinstall \
+		 tags checksum mirror-distfiles
 .if !target(__target)
 ${__target}: _SUBDIRUSE
 .endif
