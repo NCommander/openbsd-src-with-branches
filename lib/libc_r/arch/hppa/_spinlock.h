618c2a4fb315b6466aef47f31420e3992226f131
d <d>
1999-01-08 08:25:37 +0000

    move spinlock.h into the real world

--- lib/libc_r/arch/hppa/_spinlock.h
+++
@@ -1,6 +0,0 @@
-/*	$OpenBSD$	*/
-
-#define _SPINLOCK_UNLOCKED      (1)
-#define _SPINLOCK_LOCKED        (0)
-typedef int _spinlock_lock_t;
-
--- lib/libc_r/arch/powerpc/_spinlock.h
+++
@@ -1,6 +0,0 @@
-/*	$OpenBSD$	*/
-
-#define _SPINLOCK_UNLOCKED	(0)
-#define _SPINLOCK_LOCKED	(1)
-typedef register_t _spinlock_lock_t;
-
