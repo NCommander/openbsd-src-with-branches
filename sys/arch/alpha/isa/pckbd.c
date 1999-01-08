fcf708b9ca7e66290b744eb1660638f6e33ac272
niklas <niklas>
1999-01-08 03:16:16 +0000

    s/pcppi/pckbc/ due to conflict with "real" pcppi and as a step towards sys/dev/pckbc.  Will probably still not coexist with pcppi but now alpha at least configs.  I will come back here anyday and improve.

--- sys/arch/alpha/isa/pckbd.c
+++ sys/arch/alpha/isa/pckbd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pckbd.c,v 1.13 1998/02/05 16:47:59 deraadt Exp $	*/
+/*	$OpenBSD: pckbd.c,v 1.14 1998/02/05 22:57:51 deraadt Exp $	*/
 /*	$NetBSD: pckbd.c,v 1.14 1996/12/05 01:39:30 cgd Exp $	*/
 
 /*-
@@ -65,7 +65,7 @@
 #include <alpha/isa/spkrreg.h>
 #include <alpha/isa/timerreg.h>
 #include <machine/wsconsio.h>
-#include <alpha/isa/pcppivar.h>
+#include <alpha/isa/pckbcvar.h>
 
 #include <dev/wscons/wsconsvar.h>
 #include <dev/wscons/kbd.h>
@@ -323,10 +323,10 @@ pckbdprobe(parent, match, aux)
 #endif
 	void *aux;
 {
-	struct pcppi_attach_args *pa = aux;
+	struct pckbc_attach_args *pa = aux;
 	u_int i, rv;
 
-	if (pa->pa_slot != PCPPI_KBD_SLOT)
+	if (pa->pa_slot != PCKBC_KBD_SLOT)
 		return 0;
 
 	rv = 0;
@@ -418,7 +418,7 @@ pckbdattach(parent, self, aux)
 	void *aux;
 {
 	struct pckbd_softc *sc = (void *)self;
-	struct pcppi_attach_args *pa = aux;
+	struct pckbc_attach_args *pa = aux;
 
 	pckbd_iot = pa->pa_iot;
 	pckbd_ic = pa->pa_ic;
