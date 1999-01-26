5316012ffa07358dfb5f4dcd30c69d4165293136
imp <imp>
1999-01-26 20:36:15 +0000

    pmax is also mips based

--- sys/arch/pmax/include/spinlock.h
+++ sys/arch/pmax/include/spinlock.h
@@ -1,3 +1,3 @@
-/*	$OpenBSD: spinlock.h,v 1.1 1999/01/21 02:59:33 millert Exp $	*/
+/*	$OpenBSD: spinlock.h,v 1.1 1999/01/21 03:02:00 millert Exp $	*/
 
-#include <m68k/spinlock.h>
+#include <mips/spinlock.h>
