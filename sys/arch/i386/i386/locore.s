91cb700d03677d201d41c2f7d9401f28a0358e98
deraadt <deraadt>
1999-02-12 19:40:12 +0000

    do not accept T_TRCTRAP DDB inside the osyscall stub; weingart

--- sys/arch/i386/i386/locore.s
+++ sys/arch/i386/i386/locore.s
@@ -1,4 +1,4 @@
-/*	$OpenBSD: locore.s,v 1.44 1998/08/18 22:29:10 marc Exp $	*/
+/*	$OpenBSD: locore.s,v 1.45 1998/09/06 20:09:58 millert Exp $	*/
 /*	$NetBSD: locore.s,v 1.145 1996/05/03 19:41:19 christos Exp $	*/
 
 /*-
@@ -2067,6 +2067,7 @@ IDTVEC(osyscall)
 	popfl
 	pushl	$7		# size of instruction for restart
 	jmp	syscall1
+IDTVEC(osyscall_end)
 
 /*
  * Trap gate entry for syscall
