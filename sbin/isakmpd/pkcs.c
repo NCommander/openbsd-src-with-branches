a60c6f3c34836411a96a4dfe3912cdcaafd66bde
niklas <niklas>
1999-02-26 03:49:16 +0000

    Merge from the Ericsson repository | revision 1.12 | date: 1999/02/25 11:39:18;  author: niklas;  state: Exp;  lines: +3 -2 | include sysdep.h everywhere | ----------------------------

--- sbin/isakmpd/pkcs.c
+++ sbin/isakmpd/pkcs.c
@@ -1,5 +1,5 @@
-/*	$OpenBSD: pkcs.c,v 1.4 1998/11/16 21:07:17 niklas Exp $	*/
-/*	$EOM: pkcs.c,v 1.11 1998/08/20 14:08:59 provos Exp $	*/
+/*	$OpenBSD: pkcs.c,v 1.5 1998/11/17 11:10:18 niklas Exp $	*/
+/*	$EOM: pkcs.c,v 1.12 1999/02/25 11:39:18 niklas Exp $	*/
 
 /*
  * Copyright (c) 1998 Niels Provos.  All rights reserved.
@@ -39,9 +39,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "sysdep.h"
+
 #include "gmp_util.h"
 #include "log.h"
-#include "sysdep.h"
 #include "asn.h"
 #include "asn_useful.h"
 #include "pkcs.h"
