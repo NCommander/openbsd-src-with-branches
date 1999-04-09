49043a62d65bb6d9a518affb469930a883fc54f5
niklas <niklas>
1999-04-09 23:28:45 +0000

    The kernel parts of a sysctl that can switch on/off IP-in-IP (protocol 4) support, when IPSEC is compiled in.  The default is disabled.  Turn on with: sysctl -w net.inet.ip4.allow=1 ***Only*** do this if you are really knowing what you do! This control does not control the tunnel modes of ESP and AH.

--- sys/netinet/in.h
+++ sys/netinet/in.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: in.h,v 1.20 1999/03/24 02:31:03 cmetz Exp $	*/
+/*	$OpenBSD: in.h,v 1.21 1999/03/27 21:04:21 provos Exp $	*/
 /*	$NetBSD: in.h,v 1.20 1996/02/13 23:41:47 christos Exp $	*/
 
 /*
@@ -463,7 +463,7 @@ struct in6_pktinfo {
 	{ "icmp", CTLTYPE_NODE }, \
 	{ "igmp", CTLTYPE_NODE }, \
 	{ "ggp", CTLTYPE_NODE }, \
-	{ 0, 0 }, \
+	{ "ip4", CTLTYPE_NODE }, \
 	{ 0, 0 }, \
 	{ "tcp", CTLTYPE_NODE }, \
 	{ 0, 0 }, \
