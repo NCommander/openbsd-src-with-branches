3338048b16ec03baebb6b8efbb09c330b58c961f
niklas <niklas>
1999-05-16 21:48:37 +0000

    spltdb introduced, protection for tdb lists and related structures, so they won't disappear behind our back by an expiration.  Cleanup expiration logic too.

--- sys/netinet/in_pcb.c
+++ sys/netinet/in_pcb.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: in_pcb.c,v 1.32 1999/03/27 21:04:18 provos Exp $	*/
+/*	$OpenBSD: in_pcb.c,v 1.33 1999/04/28 09:28:16 art Exp $	*/
 /*	$NetBSD: in_pcb.c,v 1.25 1996/02/13 23:41:53 christos Exp $	*/
 
 /*
@@ -529,8 +529,10 @@ in_pcbdetach(v)
 		ip_freemoptions(inp->inp_moptions);
 #ifdef IPSEC
 	/* XXX IPsec cleanup here */
+	s = spltdb();
 	if (inp->inp_tdb)
 		TAILQ_REMOVE(&inp->inp_tdb->tdb_inp, inp, inp_tdb_next);
+	splx(s);
 #endif
 	s = splnet();
 	LIST_REMOVE(inp, inp_hash);
