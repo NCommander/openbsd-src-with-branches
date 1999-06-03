3a5ef4985c0f050ae0b917948c74f821fba6a694
espie <espie>
1999-06-03 21:52:02 +0000

    Repair amiga builds (int schedules need some machine-specific macros)

--- sys/net/if_bridge.c
+++ sys/net/if_bridge.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_bridge.c,v 1.8 1999/03/19 22:47:33 jason Exp $	*/
+/*	$OpenBSD: if_bridge.c,v 1.9 1999/05/24 23:09:10 jason Exp $	*/
 
 /*
  * Copyright (c) 1999 Jason L. Wright (jason@thought.net)
@@ -43,6 +43,7 @@
 #include <sys/errno.h>
 #include <sys/device.h>
 #include <sys/kernel.h>
+#include <machine/cpu.h>
 
 #include <net/if.h>
 #include <net/if_types.h>
