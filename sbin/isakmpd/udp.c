9315dbdf6026a605815fa67af5429ea3712b2dae
niklas <niklas>
1999-06-05 23:11:19 +0000

    Merge with EOM 1.41, RCSIds

--- sbin/isakmpd/udp.c
+++ sbin/isakmpd/udp.c
@@ -1,5 +1,5 @@
-/*	$OpenBSD: udp.c,v 1.13 1999/04/30 11:46:59 niklas Exp $	*/
-/*	$EOM: udp.c,v 1.40 1999/04/29 18:46:54 niklas Exp $	*/
+/*	$OpenBSD: udp.c,v 1.14 1999/06/05 22:54:42 niklas Exp $	*/
+/*	$EOM: udp.c,v 1.41 1999/06/05 23:08:53 niklas Exp $	*/
 
 /*
  * Copyright (c) 1998, 1999 Niklas Hallqvist.  All rights reserved.
