8ca2e453611d52d9da55da9534fc6663f2122b3f
deraadt <deraadt>
1999-06-06 15:39:16 +0000

    use __sparc__

--- sys/compat/svr4/svr4_stat.c
+++ sys/compat/svr4/svr4_stat.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: svr4_stat.c,v 1.10 1997/11/13 06:16:27 deraadt Exp $	 */
+/*	$OpenBSD: svr4_stat.c,v 1.11 1998/02/09 04:32:13 tholo Exp $	 */
 /*	$NetBSD: svr4_stat.c,v 1.21 1996/04/22 01:16:07 christos Exp $	 */
 
 /*
@@ -60,7 +60,7 @@
 #include <compat/svr4/svr4_time.h>
 #include <compat/svr4/svr4_socket.h>
 
-#ifdef sparc
+#ifdef __sparc__
 /* 
  * Solaris-2.4 on the sparc has the old stat call using the new
  * stat data structure...
--- sys/dev/ic/midway.c
+++ sys/dev/ic/midway.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: midway.c,v 1.23 1999/01/11 02:30:59 aaron Exp $	*/
+/*	$OpenBSD: midway.c,v 1.24 1999/01/11 05:12:17 millert Exp $	*/
 /*	(sync'd to midway.c 1.68)	*/
 
 /*
@@ -144,7 +144,7 @@
 #endif
 
 
-#if !defined(sparc) && !defined(__FreeBSD__)
+#if !defined(__sparc__) && !defined(__FreeBSD__)
 #include <machine/bus.h>
 #endif
 
