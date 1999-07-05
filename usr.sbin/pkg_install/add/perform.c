210d7948bb68dbfb24b12ae41a4cae41aeb8036b
espie <espie>
1999-07-05 09:55:40 +0000

    oops

--- usr.sbin/pkg_install/add/perform.c
+++ usr.sbin/pkg_install/add/perform.c
@@ -1,7 +1,7 @@
-/*	$OpenBSD: perform.c,v 1.6 1998/10/13 23:09:49 marc Exp $	*/
+/*	$OpenBSD: perform.c,v 1.7 1999/07/04 18:23:51 espie Exp $	*/
 
 #ifndef lint
-static const char *rcsid = "$OpenBSD: perform.c,v 1.6 1998/10/13 23:09:49 marc Exp $";
+static const char *rcsid = "$OpenBSD: perform.c,v 1.7 1999/07/04 18:23:51 espie Exp $";
 #endif
 
 /*
@@ -137,7 +137,7 @@ pkg_do(char *pkg)
 		    warnx("can't stat package file '%s'", pkg_fullname);
 		    goto bomb;
 		}
-		snprintf(extract_contents, sizeof( extract_comments ),
+		snprintf(extract_contents, sizeof( extract_contents ),
 			 "-q %s", CONTENTS_FNAME);
 		extract = extract_contents;
 	    }
