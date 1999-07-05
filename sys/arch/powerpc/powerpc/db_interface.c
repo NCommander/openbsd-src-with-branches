a5969c90c57c1c3074407bec635b8ec6f491e290
rahnds <rahnds>
1999-07-05 20:23:08 +0000

    Use a breakpoint to cause an exception to cause the registers to be saved for debugging purposes.

--- sys/arch/powerpc/powerpc/db_interface.c
+++ sys/arch/powerpc/powerpc/db_interface.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD:$	*/
+/*	$OpenBSD: db_interface.c,v 1.2 1996/12/28 06:21:50 rahnds Exp $	*/
 
 #include <sys/param.h>
 #include <sys/proc.h>
@@ -7,8 +7,8 @@
 void
 Debugger()
 {
-	db_trap(T_BREAKPOINT);
 /*
-	__asm volatile ("tw 4,2,2");
+	db_trap(T_BREAKPOINT);
 */
+	__asm volatile ("tw 4,2,2");
 }
