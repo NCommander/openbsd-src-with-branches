bd0e07d15649ab42093cb1672360fc7c6cbf2ad4
art <art>
1999-08-16 18:38:50 +0000

    add a missing strerror. (there was already a %s in the format string)

--- sbin/savecore/savecore.c
+++ sbin/savecore/savecore.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: savecore.c,v 1.17 1999/02/23 07:40:29 deraadt Exp $	*/
+/*	$OpenBSD: savecore.c,v 1.18 1999/07/01 15:41:59 deraadt Exp $	*/
 /*	$NetBSD: savecore.c,v 1.26 1996/03/18 21:16:05 leo Exp $	*/
 
 /*-
@@ -44,7 +44,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)savecore.c	8.3 (Berkeley) 1/2/94";
 #else
-static char rcsid[] = "$OpenBSD: savecore.c,v 1.17 1999/02/23 07:40:29 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: savecore.c,v 1.18 1999/07/01 15:41:59 deraadt Exp $";
 #endif
 #endif /* not lint */
 
@@ -431,7 +431,7 @@ err1:			syslog(LOG_WARNING, "%s: %s", path, strerror(errno));
 		ofd = Create(path, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 		fp  = fdopen(ofd, "w");
 		if (fp == NULL) {
-			syslog(LOG_ERR, "%s: fdopen: %s", path);
+			syslog(LOG_ERR, "%s: fdopen: %s", path, strerror(errno));
 			exit(1);
 		}
 	}
