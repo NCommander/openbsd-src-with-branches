35dac0890dfd5bb31ca08e48e2caacdd671de70b
itojun <itojun>
1999-12-08 15:58:30 +0000

    s/ipv6intr/ip6intr/

--- sys/net/netisr.h
+++ sys/net/netisr.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: netisr.h,v 1.12 1999/01/07 23:15:49 deraadt Exp $	*/
+/*	$OpenBSD: netisr.h,v 1.13 1999/05/24 23:09:11 jason Exp $	*/
 /*	$NetBSD: netisr.h,v 1.12 1995/08/12 23:59:24 mycroft Exp $	*/
 
 /*
@@ -74,7 +74,7 @@ int	netisr;				/* scheduling bits for network */
 
 void	arpintr __P((void));
 void	ipintr __P((void));
-void	ipv6intr __P((void));
+void	ip6intr __P((void));
 void	atintr __P((void));
 void	nsintr __P((void));
 void	clnlintr __P((void));
