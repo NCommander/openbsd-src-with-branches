c44ab9424815b9c70dbb26345f60f47dd627660a
angelos <angelos>
2000-01-05 19:53:12 +0000

    Config file should be in /etc

--- usr.sbin/rtadvd/pathnames.h
+++ usr.sbin/rtadvd/pathnames.h
@@ -1,3 +1,3 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: pathnames.h,v 1.2 1999/12/11 10:33:28 itojun Exp $	*/
 
-#define _PATH_RTADVDCONF "/usr/local/v6/etc/rtadvd.conf"
+#define _PATH_RTADVDCONF "/etc/rtadvd.conf"
