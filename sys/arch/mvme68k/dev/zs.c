070b85296689622f2c897e4ef5457f2016132512
smurph <smurph>
2000-01-27 03:28:38 +0000

    goof.

--- sys/arch/mvme68k/dev/zs.c
+++ sys/arch/mvme68k/dev/zs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: zs.c,v 1.5 1996/06/11 10:17:40 deraadt Exp $ */
+/*	$OpenBSD: zs.c,v 1.6 2000/01/27 03:06:50 smurph Exp $ */
 
 /*
  * Copyright (c) 1995 Theo de Raadt
@@ -172,7 +172,7 @@ zsmatch(parent, vcf, args)
     * this is what we test with for now. XXX - smurph
     */
    if (!badvaddr(ca->ca_vaddr, 1))
-      if (*zstest != 0xFF)
+      if (*zstest == 0xFF)
          return(0);
       else 
          return(1);
