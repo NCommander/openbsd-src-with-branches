41930ba545b1d8546588362ebea7877a35944c91
itojun <itojun>
2000-02-08 12:50:25 +0000

    in6.h is not necessary (in.h will bring it in)

--- sbin/ipsecadm/ipsecadm.c
+++ sbin/ipsecadm/ipsecadm.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ipsecadm.c,v 1.30 2000/01/13 04:46:18 angelos Exp $ */
+/* $OpenBSD: ipsecadm.c,v 1.31 2000/01/27 08:04:02 angelos Exp $ */
 /*
  * The authors of this code are John Ioannidis (ji@tla.org),
  * Angelos D. Keromytis (kermit@csd.uch.gr) and 
@@ -50,10 +50,6 @@
 #include <netccitt/x25.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-
-#ifdef INET6
-#include <netinet6/in6.h>
-#endif /* INET6 */
 
 #include <errno.h>
 #include <unistd.h>
