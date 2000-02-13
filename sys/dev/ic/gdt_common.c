8a74037ba07d6ad0a435f33f6239b0d62e07a69d
niklas <niklas>
2000-02-13 11:23:15 +0000

    Handle out-of-bounds errors correctly

--- sys/dev/ic/gdt_common.c
+++ sys/dev/ic/gdt_common.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: gdt_common.c,v 1.2 2000/02/12 09:45:49 niklas Exp $	*/
+/*	$OpenBSD: gdt_common.c,v 1.3 2000/02/13 10:56:41 niklas Exp $	*/
 
 /*
  * Copyright (c) 1999, 2000 Niklas Hallqvist.  All rights reserved.
@@ -555,9 +555,11 @@ gdt_scsi_cmd(xs)
 		}
 		if (blockno >= gdt->sc_hdr[target].hd_size ||
 		    blockno + blockcnt > gdt->sc_hdr[target].hd_size) {
+			GDT_UNLOCK_GDT(gdt);
 			printf("%s: out of bounds %u-%u >= %u\n",
 			    gdt->sc_dev.dv_xname, blockno, blockcnt,
 			    gdt->sc_hdr[target].hd_size);
+			scsi_done(xs);
 			xs->error = XS_DRIVER_STUFFUP;
 			return (COMPLETE);
 		}
