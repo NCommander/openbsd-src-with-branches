cf8066094754d203251202ad085d2408eee20fcf
cvs-fast-export <cvs-fast-export>
2000-02-14 05:53:19 +0000

    Synthetic commit for incomplete tag SMP_BASE

--- lib/libc/arch/m88k/DEFS.h
+++
@@ -1,3 +0,0 @@
-/*	$Id: DEFS.h,v 1.1 1997/03/25 17:06:47 rahnds Exp $ */
-
-#include <machine/asm.h>
--- usr.sbin/rtadvd/pathnames.h
+++
@@ -1,3 +0,0 @@
-/*	$OpenBSD: pathnames.h,v 1.2 1999/12/11 10:33:28 itojun Exp $	*/
-
-#define _PATH_RTADVDCONF "/etc/rtadvd.conf"
