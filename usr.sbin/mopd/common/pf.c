e3297b1cf1466cb688fd87f55820135a306a91b9
bitblt <bitblt>
2000-02-20 17:45:34 +0000

    string manipluation paranoia changes.  Most probably not exploitable.

--- usr.sbin/mopd/common/pf.c
+++ usr.sbin/mopd/common/pf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf.c,v 1.2 1996/09/21 19:11:52 maja Exp $ */
+/*	$OpenBSD: pf.c,v 1.3 1997/08/18 03:11:31 millert Exp $ */
 
 /*
  * Copyright (c) 1993-95 Mats O Jansson.  All rights reserved.
@@ -34,7 +34,7 @@
  */
 
 #ifndef LINT
-static char rcsid[] = "$OpenBSD: pf.c,v 1.2 1996/09/21 19:11:52 maja Exp $";
+static char rcsid[] = "$OpenBSD: pf.c,v 1.3 1997/08/18 03:11:31 millert Exp $";
 #endif
 
 #include <stdio.h>
@@ -176,7 +176,8 @@ pfAddMulti(s, interface, addr)
 	struct ifreq ifr;
 	int	fd;
 	
-	strcpy(ifr.ifr_name, interface);
+	strncpy(ifr.ifr_name, interface,sizeof(ifr.ifr_name) - 1);
+	ifr.ifr_name[sizeof(ifr.ifr_name)] = 0;
 
 	ifr.ifr_addr.sa_family = AF_UNSPEC;
 	bcopy(addr, ifr.ifr_addr.sa_data, 6);
@@ -211,7 +212,8 @@ pfDelMulti(s, interface, addr)
 	struct ifreq ifr;
 	int	fd;
 	
-	strcpy(ifr.ifr_name, interface);
+	strncpy(ifr.ifr_name, interface, sizeof (ifr.ifr_name) - 1);
+	ifr.ifr_name[sizeof(ifr.ifr_name)] = 0;
 	
 	ifr.ifr_addr.sa_family = AF_UNSPEC;
 	bcopy(addr, ifr.ifr_addr.sa_data, 6);
--- usr.sbin/mopd/otherOS/pf-snit.c
+++ usr.sbin/mopd/otherOS/pf-snit.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$ */
+/*	$OpenBSD: pf-snit.c,v 1.2 1996/09/21 19:12:50 maja Exp $ */
 
 /*
  * Copyright (c) 1993-96 Mats O Jansson.  All rights reserved.
@@ -30,7 +30,7 @@
  */
 
 #ifndef LINT
-static char rcsid[] = "$OpenBSD: pf-snit.c,v 1.1.1.1 1996/09/21 13:49:17 maja Exp $";
+static char rcsid[] = "$OpenBSD: pf-snit.c,v 1.2 1996/09/21 19:12:50 maja Exp $";
 #endif
 
 #include <stdio.h>
@@ -239,7 +239,8 @@ pfAddMulti(s, interface, addr)
 	struct ifreq ifr;
 	int fd;
 	
-	strcpy(ifr.ifr_name, interface);
+	strncpy(ifr.ifr_name, interface, sizeof (ifr.ifr_name) -1);
+	ifr.ifr_name[sizeof(ifr.ifr_name)] = 0;
 	
 	ifr.ifr_addr.sa_family = AF_UNSPEC;
 	bcopy(addr, ifr.ifr_addr.sa_data, 6);
@@ -274,7 +275,8 @@ char *interface, *addr;
 	struct ifreq ifr;
 	int fd;
 	
-	strcpy(ifr.ifr_name, interface);
+	strncpy(ifr.ifr_name, interface, sizeof (ifr.ifr_name) -1);
+	ifr.ifr_name[sizeof(ifr.ifr_name)] = 0;
 	
 	ifr.ifr_addr.sa_family = AF_UNSPEC;
 	bcopy(addr, ifr.ifr_addr.sa_data, 6);
