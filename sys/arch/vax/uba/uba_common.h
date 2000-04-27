5db0d5c9382bf6b9eaa484a22696e1d646993972
bjc <bjc>
2000-04-27 03:14:52 +0000

    sync w/netbsd

--- sys/arch/vax/uba/uba_common.h
+++ sys/arch/vax/uba/uba_common.h
@@ -1,12 +1,13 @@
-/*	$OpenBSD: udareg.h,v 1.3 1996/07/01 21:24:50 ragge Exp $	*/
-/*	$NetBSD: udareg.h,v 1.3 1996/07/01 21:24:50 ragge Exp $	*/
-
-/*
- * Copyright (c) 1988 Regents of the University of California.
+/*	$OpenBSD$	*/
+/*	$NetBSD: uba_common.h,v 1.2 1999/06/21 16:23:01 ragge Exp $ */
+/*-
+ * Copyright (c) 1982, 1986 The Regents of the University of California.
  * All rights reserved.
- *
- * This code is derived from software contributed to Berkeley by
- * Chris Torek.
+ * (c) UNIX System Laboratories, Inc.
+ * All or some portions of this file are derived from material licensed
+ * to the University of California by American Telephone and Telegraph
+ * Co. or Unix System Laboratories, Inc. and are reproduced herein with
+ * the permission of UNIX System Laboratories, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -36,29 +37,52 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)udareg.h	7.3 (Berkeley) 5/8/91
+ *	@(#)ubareg.h	7.8 (Berkeley) 5/9/91
  */
 
 /*
- * UDA50 registers and structures
+ * VAX-specific parts of the Unibus softc.
  */
+struct uba_regs;
 
-/*
- * Writing any value to udaip starts initialisation.  Reading from it
- * when the UDA is running makes the UDA look through the command ring
- * to find any new commands.  Reading udasa gives status; writing it
- * during initialisation sets things up.
- */
-struct udadevice {
-	u_short	udaip;		/* initialisation and polling */
-	u_short	udasa;		/* status and address */
+struct	uba_vsoftc {
+	struct	uba_softc uv_sc;/* Common vars from arch/vax/qbus/ubavar.h */
+	struct	vax_bus_dma_tag uv_dmat;
+	struct	vax_sgmap uv_sgmap;
+	int	uv_size;	/* Size of UBA addressable memory */
+	paddr_t uv_addr;	/* Physical address of map registers */
+	struct	uba_regs *uv_uba;/* Where applicable */
+	int	uh_ibase;
+	int	uh_zvtime;
+	int	uh_zvtotal;
+	int	uh_zvcnt;
 };
 
 /*
- * Bits in UDA status register after initialisation
+ * Size of unibus memory address space in pages
+ * (also number of map registers).
  */
-#define	UDA_GO		0x0001	/* run */
+#define UBAPAGES	496
+#define UBAIOADDR	0760000	 /* start of I/O page */
+#define UBAIOPAGES	16
 
-#define	UDASR_BITS \
-"\20\20ERR\17STEP4\16STEP3\15STEP2\14STEP1\13NV\12QB\11DI\10IE\1GO"
+/*
+ * DW780/DW750 hardware registers
+ */
+struct uba_regs {
+	int	uba_cnfgr;		/* configuration register */
+	int	uba_cr;			/* control register */
+	int	uba_sr;			/* status register */
+	int	uba_dcr;		/* diagnostic control register */
+	int	uba_fmer;		/* failed map entry register */
+	int	uba_fubar;		/* failed UNIBUS address register */
+	int	pad1[2];
+	int	uba_brsvr[4];
+	int	uba_brrvr[4];		/* receive vector registers */
+	int	uba_dpr[16];		/* buffered data path register */
+	int	pad2[480];
+	struct pte uba_map[UBAPAGES];	/* unibus map register */
+	int	pad3[UBAIOPAGES];	/* no maps for device address space */
+};
 
+void	uba_dma_init __P((struct uba_vsoftc *));
