da526ca7d7c7f9c25128681f5b5294ee42b9eb38
markus <markus>
2000-04-27 15:23:02 +0000

    cleanup logging for sshd/2, respect PasswordAuth no

--- usr.bin/ssh/session.c
+++ usr.bin/ssh/session.c
@@ -8,7 +8,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: session.c,v 1.4 2000/04/14 10:30:33 markus Exp $");
+RCSID("$OpenBSD: session.c,v 1.5 2000/04/19 09:24:39 markus Exp $");
 
 #include "xmalloc.h"
 #include "ssh.h"
@@ -1402,6 +1402,5 @@ do_authenticated2(void)
 	 * authentication.
 	 */
 	alarm(0);
-	log("do_authenticated2");
 	server_loop2();
 }
