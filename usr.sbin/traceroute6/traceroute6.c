39cdb422775e0fd0925342d5f88474ca2d7b22b7
itojun <itojun>
2000-06-05 12:58:42 +0000

    correct option parser typo.  -f, not -b.

--- usr.sbin/traceroute6/traceroute6.c
+++ usr.sbin/traceroute6/traceroute6.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: traceroute6.c,v 1.7 2000/04/20 07:57:41 angelos Exp $	*/
+/*	$OpenBSD: traceroute6.c,v 1.8 2000/04/20 17:43:46 angelos Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -429,7 +429,7 @@ main(argc, argv)
 #endif
 			freehostent(hp);
 			break;
-		case 'b':
+		case 'f':
 			first_hop = atoi(optarg);
 			if (first_hop > max_hops) {
 				Fprintf(stderr,
