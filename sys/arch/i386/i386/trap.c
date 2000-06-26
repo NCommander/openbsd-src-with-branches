eb75476c124c31e6af3622a7964f8161f6ca391d
art <art>
2000-06-26 22:45:53 +0000

    Make the check for !usermode in syscal a DIAGNOSTIC.

--- sys/arch/i386/i386/trap.c
+++ sys/arch/i386/i386/trap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: trap.c,v 1.32 2000/06/08 21:11:58 niklas Exp $	*/
+/*	$OpenBSD: trap.c,v 1.32 2000/06/08 22:25:19 niklas Exp $	*/
 /*	$NetBSD: trap.c,v 1.95 1996/05/05 06:50:02 mycroft Exp $	*/
 
 #undef DEBUG
@@ -612,8 +612,10 @@ syscall(frame)
 #else
 	cnt.v_syscall++;
 #endif
+#ifdef DIAGNOSTIC
 	if (!USERMODE(frame.tf_cs, frame.tf_eflags))
 		panic("syscall");
+#endif
 	p = curproc;
 	sticks = p->p_sticks;
 	p->p_md.md_regs = &frame;
