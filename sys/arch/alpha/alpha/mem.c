88444e467f66f3ed7e6cfdb6c2ef6d1de2cbf0f2
ericj <ericj>
2000-07-05 18:30:32 +0000

    make dmesg work on alpha

--- sys/arch/alpha/alpha/mem.c
+++ sys/arch/alpha/alpha/mem.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mem.c,v 1.8 1998/08/31 17:42:25 millert Exp $	*/
+/*	$OpenBSD: mem.c,v 1.9 1999/11/22 19:21:55 matthieu Exp $	*/
 /*	$NetBSD: mem.c,v 1.10 1996/11/13 21:13:10 cgd Exp $	*/
 
 /*
@@ -122,7 +122,7 @@ mmrw(dev, uio, flags)
 kmemphys:
 			/* allow reads only in RAM (except for DEBUG) */
 			if (v < ctob(firstusablepage) ||
-			    v > ctob(lastusablepage + 1))
+			    v > ctob(lastusablepage + 1 + btoc(MSGBUFSIZE)))
 				return (EFAULT);
 			o = uio->uio_offset & PGOFSET;
 			c = min(uio->uio_resid, (int)(NBPG - o));
