3a98537c0060e01b9ea68134967b15748f25614f
itojun <itojun>
2000-07-06 10:14:49 +0000

    sync with router renumber struct decl change. more logs.  make it very sure to close temporary socket.

--- usr.sbin/rtadvd/config.c
+++ usr.sbin/rtadvd/config.c
@@ -1,5 +1,5 @@
-/*	$OpenBSD: config.c,v 1.5 2000/03/13 06:16:11 itojun Exp $	*/
-/*	$KAME: config.c,v 1.11 2000/05/16 13:34:13 itojun Exp $	*/
+/*	$OpenBSD: config.c,v 1.6 2000/05/23 11:23:22 itojun Exp $	*/
+/*	$KAME: config.c,v 1.12 2000/05/22 22:23:07 itojun Exp $	*/
 
 /*
  * Copyright (C) 1998 WIDE Project.
