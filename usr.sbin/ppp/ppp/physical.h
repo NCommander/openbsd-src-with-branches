470290318ea41ad2f88b014396a69df84a140c6e
brian <brian>
2000-09-14 22:02:50 +0000

    Support PPPoATM, disabled for now as /usr/include/netnatm doesn't exist
    Submitted by: Jakob Stoklund Olesen <stoklund@taxidriver.dk>

--- usr.sbin/ppp/ppp/physical.h
+++ usr.sbin/ppp/ppp/physical.h
@@ -16,7 +16,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *  $OpenBSD: physical.h,v 1.15 2000/02/27 00:21:09 brian Exp $
+ *  $OpenBSD: physical.h,v 1.16 2000/02/27 01:38:27 brian Exp $
  *
  */
 
@@ -35,6 +35,7 @@ struct cmdargs;
 #define UDP_DEVICE	4
 #define ETHER_DEVICE	5
 #define EXEC_DEVICE	6
+#define ATM_DEVICE	7
 
 /* Returns from awaitcarrier() */
 #define CARRIER_PENDING	1
