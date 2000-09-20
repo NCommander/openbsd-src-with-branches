be58bfbd6ee3cb7b783cb4f10f88f0ec8c30e074
espie <espie>
2000-09-20 22:57:20 +0000

    Avoid erasing a GOT definition. This is needed for a -fPIC libgcc.

--- gnu/usr.bin/ld/ld.c
+++ gnu/usr.bin/ld/ld.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ld.c,v 1.14 2000/02/21 16:01:04 deraadt Exp $	*/
+/*	$OpenBSD: ld.c,v 1.15 2000/02/21 16:56:56 art Exp $	*/
 /*	$NetBSD: ld.c,v 1.52 1998/02/20 03:12:51 jonathan Exp $	*/
 
 /*-
@@ -1469,7 +1469,7 @@ enter_global_ref(lsp, name, entry)
 			if (com)
 				common_defined_global_count--;
 			sp->common_size = 0;
-			if (sp != dynamic_symbol)
+			if (sp != dynamic_symbol && sp != got_symbol)
 				sp->defined = 0;
 		}
 
