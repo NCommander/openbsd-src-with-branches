a541936df6c7d0c78e93961fd1c76d73aeac0092
millert <millert>
2000-10-02 17:33:47 +0000

    Don't look in ~/.terminfo if setugid.  This is consistent with the behavior of checking ~/.terminfo.db and ~/.termcap.db

--- lib/libcurses/tinfo/home_terminfo.c
+++ lib/libcurses/tinfo/home_terminfo.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: home_terminfo.c,v 1.2 1999/03/02 06:23:28 millert Exp $	*/
+/*	$OpenBSD: home_terminfo.c,v 1.3 2000/08/02 04:10:47 millert Exp $	*/
 
 /****************************************************************************
  * Copyright (c) 1998 Free Software Foundation, Inc.                        *
@@ -51,7 +51,7 @@ _nc_home_terminfo(void)
 	char *home;
 	static char *temp = 0;
 
-	if (temp == 0) {
+	if (temp == 0 && !issetugid()) {
 		if ((home = getenv("HOME")) != 0 && *home != '\0'
 		 && my_length <= PATH_MAX) {
 			temp = typeMalloc(char, my_length);
