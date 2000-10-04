1a2d4cc293bfdcaafc61b5559179f61b45f4f41d
d <d>
2000-10-04 05:55:35 +0000

    switch to _machdep_switch() instead of setjmp/longjmp. For some reason this fixes sparc threads.

--- lib/libc_r/arch/mips/uthread_machdep.h
+++ lib/libc_r/arch/mips/uthread_machdep.h
@@ -1,38 +1,6 @@
-/*
- * OpenBSD/mips machine-dependent thread macros
- *
- * $OpenBSD: uthread_machdep.h,v 1.3 1999/01/17 23:49:49 d Exp $
- */
+/*	$OpenBSD$	*/
+/* David Leonard, <d@csee.uq.edu.au>. Public domain. */
 
-#include <machine/regnum.h>
-#include <machine/signal.h>
-
-/* floating point state is saved by setjmp/longjmp */
-
-#define _thread_machdep_save_float_state(thr) 		/* no need */
-#define _thread_machdep_restore_float_state(thr) 	/* no need */
-
-/* initialise the jmpbuf stack frame so it continues from entry */
-#define _thread_machdep_thread_create(thr, entry, pattr)	\
-	{							\
-	    struct sigcontext *j = &(thr)->saved_jmp_buf;	\
-								\
-	    /* initialise to sane values */			\
-	    _thread_machdep_setjmp(j);				\
-	    /* entry */						\
-	    j->sc_regs[RA] = j->sc_pc; /* for gdb */		\
-	    j->sc_pc = (int)entry;				\
-	    /* stack */						\
-	    j->sc_regs[SP] = (int) (thr)->stack->base		\
-				+ (thr)->stack->size		\
-				- sizeof(double);		\
-	}
-
-#define _thread_machdep_longjmp(a,v)	longjmp(a,v)
-#define _thread_machdep_setjmp(a)	setjmp(a)
-
-typedef jmp_buf _machdep_jmp_buf;
-
-struct _machdep_struct {
-	/* nothing needed */
+struct _machdep_state {
+	int	frame;
 };
--- lib/libpthread/arch/m68k/uthread_machdep.h
+++ lib/libpthread/arch/m68k/uthread_machdep.h
@@ -1,40 +1,6 @@
-/*
- * OpenBSD/m68k machine-dependent thread macros
- *
- * $OpenBSD: uthread_machdep.h,v 1.2 1999/01/17 23:49:49 d Exp $
- */
+/*	$OpenBSD$	*/
+/* David Leonard, <d@csee.uq.edu.au>. Public domain. */
 
-/* save the floating point state of a thread */
-#define _thread_machdep_save_float_state(thr) 		\
-	{						\
-		/* fsave privileged instr */		\
-	}
-
-/* restore the floating point state of a thread */
-#define _thread_machdep_restore_float_state(thr) 	\
-	{						\
-		/* frestore privileged instr */		\
-	}
-
-/* initialise the jmpbuf stack frame so it continues from entry */
-
-#define _thread_machdep_thread_create(thr, entry, pattr)	\
-	{						\
-	    /* entry */					\
-	    (thr)->saved_jmp_buf[5] = (long) entry;	\
-	    /* stack */					\
-	    (thr)->saved_jmp_buf[2] = (long) (thr)->stack->base \
-				+ (thr)->stack->size	\
-				- sizeof(double);	\
-	}
-
-#define	_thread_machdep_longjmp(a,v)	_longjmp(a,v)
-#define	_thread_machdep_setjmp(a)	_setjmp(a)
-
-typedef jmp_buf _machdep_jmp_buf;
-
-struct _machdep_struct {
-        /* char            saved_fp[108]; */
-	int	dummy;
+struct _machdep_state {
+	int	sp;
 };
-
