21406af43f84dd90be246ad12316ee9036b72292
fgsch <fgsch>
2000-10-19 17:15:26 +0000

    Increment version; deraadt@ ok.

--- sys/arch/i386/stand/boot/conf.c
+++ sys/arch/i386/stand/boot/conf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: conf.c,v 1.13 1998/07/20 18:14:52 mickey Exp $	*/
+/*	$OpenBSD: conf.c,v 1.14 2000/05/30 22:00:24 mickey Exp $	*/
 
 /*
  * Copyright (c) 1996 Michael Shalayeff
@@ -48,7 +48,7 @@
 #include <dev/cons.h>
 #include <lib/libsa/exec.h>
 
-const char version[] = "1.24";
+const char version[] = "1.25";
 int	debug = 1;
 
 const struct x_sw execsw[] = {
