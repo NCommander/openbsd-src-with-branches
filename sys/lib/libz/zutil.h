da55d36184d7e56bb18c129b192fe455a740286a
art <art>
2000-11-08 15:39:10 +0000

    Include stand.h from lib/libsa/stand.h

--- sys/lib/libz/zutil.h
+++ sys/lib/libz/zutil.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: zutil.h,v 1.9 1999/02/14 20:06:05 mickey Exp $	*/
+/*	$OpenBSD: zutil.h,v 1.10 2000/03/03 00:54:57 todd Exp $	*/
 /* zutil.h -- internal interface and configuration of the compression library
  * Copyright (C) 1995-1998 Jean-loup Gailly.
  * For conditions of distribution and use, see copyright notice in zlib.h
@@ -16,7 +16,7 @@
 #include "zlib.h"
 
 #ifdef _STANDALONE
-#include <stand.h>
+#include <lib/libsa/stand.h>
 #else
 #ifdef STDC
 #  include <stddef.h>
