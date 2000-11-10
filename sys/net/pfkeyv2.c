cdc7be7df6a0aac81ed7d5732b509cbeb7e8aa89
jason <jason>
2000-11-10 21:48:41 +0000

    Pull in patch from current: Fix (angelos): Send the message to registered promiscuous listeners.

--- sys/net/pfkeyv2.c
+++ sys/net/pfkeyv2.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: pfkeyv2.c,v 1.47 2000/10/09 22:18:29 angelos Exp $ */
+/* $OpenBSD: pfkeyv2.c,v 1.48 2000/10/14 06:23:51 angelos Exp $ */
 /*
 %%% copyright-nrl-97
 This software is Copyright 1997-1998 by Randall Atkinson, Ronald Lee,
@@ -715,7 +715,7 @@ pfkeyv2_sendmessage(void **headers, int mode, struct socket *socket,
 	    /* Send to all registered promiscuous listeners */
 	    for (s = pfkeyv2_sockets; s; s = s->next)
 	      if ((s->flags & PFKEYV2_SOCKETFLAGS_PROMISC) &&
-		  (s->flags & PFKEYV2_SOCKETFLAGS_REGISTERED))
+		  !(s->flags & PFKEYV2_SOCKETFLAGS_REGISTERED))
 		pfkey_sendup(s->socket, packet, 1);
 
 	    m_freem(packet);
