5235fd0eadf3af97b617a34e8835f48aa9fd8247
art <art>
2000-11-10 18:15:49 +0000

    Change the ktrace interface functions from taking the trace vnode to taking the traced proc. The vnode is in the proc and all functions need the proc.

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_exec.c,v 1.43 2000/09/26 14:01:39 art Exp $	*/
+/*	$OpenBSD: kern_exec.c,v 1.44 2000/09/28 13:41:39 art Exp $	*/
 /*	$NetBSD: kern_exec.c,v 1.75 1996/02/09 18:59:28 christos Exp $	*/
 
 /*-
@@ -618,7 +618,7 @@ sys_execve(p, v, retval)
 
 #ifdef KTRACE
 	if (KTRPOINT(p, KTR_EMUL))
-		ktremul(p->p_tracep, p->p_emul->e_name);
+		ktremul(p, p->p_emul->e_name);
 #endif
 	return (0);
 
