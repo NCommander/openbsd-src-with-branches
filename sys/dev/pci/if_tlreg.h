e1ada4df137b0ad46b51eed0fdb807779a652322
jason <jason>
2000-12-23 06:14:33 +0000

    Pull in patch from current: Fix (jason): remove diagnostic around TL_INTR_INVALID (this is a valid condition when two devices are on the same interrupt).

--- sys/dev/pci/if_tlreg.h
+++ sys/dev/pci/if_tlreg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_tlreg.h,v 1.3 1999/09/13 20:41:40 jason Exp $	*/
+/*	$OpenBSD: if_tlreg.h,v 1.4 1999/09/13 20:45:26 jason Exp $	*/
 
 /*
  * Copyright (c) 1997, 1998
@@ -122,7 +122,6 @@ struct tl_softc {
 	u_int8_t		tl_ctlr;	/* chip number */
 	u_int8_t		tl_unit;	/* interface number */
 	u_int8_t		tl_eeaddr;
-	u_int8_t		tl_empty_intr;	/* expecting empty interrupt */
 	u_int8_t		tl_phy_addr;	/* PHY address */
 	u_int8_t		tl_tx_pend;	/* TX pending */
 	u_int8_t		tl_want_auto;	/* autoneg scheduled */
