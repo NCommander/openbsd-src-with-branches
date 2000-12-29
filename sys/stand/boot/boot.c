99643a6910427d3c47ea05e7f35208ec500d4fc1
mickey <mickey>
2000-12-29 19:24:44 +0000

    OpenBSD/machine banner, not machine_arch

--- sys/stand/boot/boot.c
+++ sys/stand/boot/boot.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: boot.c,v 1.21 1998/05/30 05:45:24 deraadt Exp $	*/
+/*	$OpenBSD: boot.c,v 1.22 1998/07/08 19:24:52 mickey Exp $	*/
 
 /*
  * Copyright (c) 1997,1998 Michael Shalayeff
@@ -57,7 +57,7 @@ boot(bootdev)
 
 	machdep();
 
-	printf(">> OpenBSD/" MACHINE_ARCH " BOOT %s\n", version);
+	printf(">> OpenBSD/" MACHINE " BOOT %s\n", version);
 
 	devboot(bootdev, cmd.bootdev);
 	strncpy(cmd.image, bootfile, sizeof(cmd.image));
