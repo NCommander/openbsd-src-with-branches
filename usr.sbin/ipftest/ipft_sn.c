e02256ba55f0e9c25e2e9dcf06a63d6f5f4b4111
fgsch <fgsch>
2001-01-17 06:01:27 +0000

    2nd part of the ipf code; need more testing.

--- usr.sbin/ipftest/ipft_sn.c
+++ usr.sbin/ipftest/ipft_sn.c
@@ -1,7 +1,7 @@
 /*	$OpenBSD$	*/
 
 /*
- * Copyright (C) 1993-1998 by Darren Reed.
+ * Copyright (C) 1993-2000 by Darren Reed.
  *
  * Redistribution and use in source and binary forms are permitted
  * provided that this notice is preserved and due credit is given
@@ -32,14 +32,14 @@
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
 #include <net/if.h>
-#include <netinet/ip_fil_compat.h>
+#include <netinet/ip_compat.h>
 #include <netinet/tcpip.h>
 #include "ipf.h"
 #include "snoop.h"
 #include "ipt.h"
 
 #if !defined(lint)
-static const char rcsid[] = "@(#)$IPFilter: ipft_sn.c,v 2.1 1999/08/04 17:30:04 darrenr Exp $";
+static const char rcsid[] = "@(#)$IPFilter: ipft_sn.c,v 2.2 2000/03/13 22:10:24 darrenr Exp $";
 #endif
 
 struct	llc	{
--- usr.sbin/ipftest/snoop.h
+++ usr.sbin/ipftest/snoop.h
@@ -1,7 +1,7 @@
 /*	$OpenBSD$	*/
 
 /*
- * Copyright (C) 1993-1998 by Darren Reed.
+ * Copyright (C) 1993-2000 by Darren Reed.
  *
  * Redistribution and use in source and binary forms are permitted
  * provided that this notice is preserved and due credit is given
@@ -13,7 +13,7 @@
 
 /*
  * written to comply with the RFC (1761) from Sun.
- * $IPFilter: snoop.h,v 2.1 1999/08/04 17:30:19 darrenr Exp $
+ * $IPFilter: snoop.h,v 2.2 2000/03/13 22:10:27 darrenr Exp $
  */
 struct	snoophdr	{
 	char	s_id[8];
