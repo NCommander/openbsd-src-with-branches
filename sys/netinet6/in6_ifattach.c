1ac974f98c7f88338350efea9727a885ab145af0
itojun <itojun>
2001-01-18 06:48:25 +0000

    do not remove default route by mistake, on interface removal.

--- sys/netinet6/in6_ifattach.c
+++ sys/netinet6/in6_ifattach.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: in6_ifattach.c,v 1.12 2000/10/18 18:49:39 itojun Exp $	*/
+/*	$OpenBSD: in6_ifattach.c,v 1.13 2000/10/25 22:01:02 jason Exp $	*/
 /*	$KAME: in6_ifattach.c,v 1.68 2000/10/18 18:44:24 itojun Exp $	*/
 
 /*
@@ -889,7 +889,8 @@ in6_ifdetach(ifp)
 	sin6.sin6_family = AF_INET6;
 	sin6.sin6_addr = in6addr_linklocal_allnodes;
 	sin6.sin6_addr.s6_addr16[1] = htons(ifp->if_index);
-	if ((rt = rtalloc1((struct sockaddr *)&sin6, 0)) != NULL) {
+	rt = rtalloc1((struct sockaddr *)&sin6, 0);
+	if (rt && rt->rt_ifp == ifp) {
 		rtrequest(RTM_DELETE, (struct sockaddr *)rt_key(rt),
 			rt->rt_gateway, rt_mask(rt), rt->rt_flags, 0);
 		rtfree(rt);
