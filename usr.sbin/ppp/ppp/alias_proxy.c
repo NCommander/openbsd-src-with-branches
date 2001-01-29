3fd40a7c5ea78d8959e3df6a08f258a0da8b7deb
brian <brian>
2001-01-29 00:29:32 +0000

    Ignore leading witespace in the string given to PacketAliasProxyRule().

--- usr.sbin/ppp/ppp/alias_proxy.c
+++ usr.sbin/ppp/ppp/alias_proxy.c
@@ -22,7 +22,7 @@
 
     Initial development: April, 1998 (cjm)
 
-    $OpenBSD: alias_proxy.c,v 1.5 2000/02/27 01:38:24 brian Exp $
+    $OpenBSD: alias_proxy.c,v 1.6 2000/09/02 22:12:26 brian Exp $
 */
 
 
@@ -533,6 +533,7 @@ PacketAliasProxyRule(const char *cmd)
     struct proxy_entry *proxy_entry;
 
 /* Copy command line into a buffer */
+    cmd += strspn(cmd, " \t");
     cmd_len = strlen(cmd);
     if (cmd_len > (sizeof(buffer) - 1))
         return -1;
