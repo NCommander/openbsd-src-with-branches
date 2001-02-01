72934654159a23d1f5be5c8917f78d48232c1ed8
smurph <smurph>
2001-02-01 03:38:23 +0000

    Major changes to get MVME188 working.  More header and code cleanups.  The kernel is tested on MVME188A/2P256 and MVME188A/1P64.

--- sys/arch/mvme88k/dev/siop.c
+++ sys/arch/mvme88k/dev/siop.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: siop.c,v 1.3 1999/05/29 04:41:44 smurph Exp $ */
+/*	$OpenBSD: siop.c,v 1.4 2001/01/13 05:18:58 smurph Exp $ */
 
 /*
  * Copyright (c) 1994 Michael L. Hitch
@@ -229,9 +229,9 @@ siop_scsicmd(xs)
 	slp = xs->sc_link;
 	sc = slp->adapter_softc;
 	flags = xs->flags;
-   xs->error = XS_NOERROR;
+	xs->error = XS_NOERROR;
 	
-   /* XXXX ?? */
+	/* XXXX ?? */
 	if (flags & SCSI_DATA_UIO)
 		panic("siop: scsi data uio requested");
 
