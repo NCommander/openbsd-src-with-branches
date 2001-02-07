f061968160dacaa8fd0bdea45b56e69cb920385e
itojun <itojun>
2001-02-07 18:04:50 +0000

    fix size_t -> int cast (use u_long).  markus ok

--- usr.bin/ssh/ssh-keyscan.c
+++ usr.bin/ssh/ssh-keyscan.c
@@ -8,7 +8,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: ssh-keyscan.c,v 1.11 2001/01/21 19:05:57 markus Exp $");
+RCSID("$OpenBSD: ssh-keyscan.c,v 1.12 2001/02/04 15:32:26 stevesk Exp $");
 
 #include <sys/queue.h>
 #include <errno.h>
@@ -470,7 +470,7 @@ conloop(void)
 		seltime = c->c_tv;
 		seltime.tv_sec -= now.tv_sec;
 		seltime.tv_usec -= now.tv_usec;
-		if ((int) seltime.tv_usec < 0) {
+		if (seltime.tv_usec < 0) {
 			seltime.tv_usec += 1000000;
 			seltime.tv_sec--;
 		}
