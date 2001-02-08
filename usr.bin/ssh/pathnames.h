a72e41710dd853f002d579e9e7065b975850b823
markus <markus>
2001-02-08 00:04:52 +0000

    allow sftp over ssh protocol 1; ok djm@

--- usr.bin/ssh/pathnames.h
+++ usr.bin/ssh/pathnames.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: pathnames.h,v 1.2 2001/01/29 01:58:17 niklas Exp $	*/
 
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
@@ -104,3 +104,6 @@
 
 /* for scp */
 #define _PATH_CP			"cp"
+
+/* for sftp */
+#define _PATH_SFTP_SERVER		"/usr/libexec/sftp-server"
