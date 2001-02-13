d44f481f1e9740cd383f1b5f22b8f6475cdb9003
art <art>
2001-02-13 19:51:49 +0000

    Use MIN, not min when counting the pages we steal. min is a function taking u_int arguments and we are counting signed longs here.

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vfs_bio.c,v 1.26 1999/12/05 08:09:01 art Exp $	*/
+/*	$OpenBSD: vfs_bio.c,v 1.27 2000/06/23 02:14:38 mickey Exp $	*/
 /*	$NetBSD: vfs_bio.c,v 1.44 1996/06/11 11:15:36 pk Exp $	*/
 
 /*-
@@ -678,7 +678,7 @@ allocbuf(bp, size)
 		binshash(nbp, &invalhash);
 
 		/* and steal its pages, up to the amount we need */
-		amt = min(nbp->b_bufsize, (desired_size - bp->b_bufsize));
+		amt = MIN(nbp->b_bufsize, (desired_size - bp->b_bufsize));
 		pagemove((nbp->b_data + nbp->b_bufsize - amt),
 			 bp->b_data + bp->b_bufsize, amt);
 		bp->b_bufsize += amt;
