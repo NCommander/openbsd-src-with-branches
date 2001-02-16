7ff6e39957020434875b2902f506aeca58e9bb53
mickey <mickey>
2001-02-16 19:20:13 +0000

    db_elf definitions

--- sys/arch/hppa/include/db_machdep.h
+++ sys/arch/hppa/include/db_machdep.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: db_machdep.h,v 1.3 1999/08/14 03:31:50 mickey Exp $	*/
+/*	$OpenBSD: db_machdep.h,v 1.4 2000/04/06 19:43:46 todd Exp $	*/
 
 /*
  * Copyright (c) 1998 Michael Shalayeff
@@ -34,6 +34,9 @@
 #define	_MACHINE_DB_MACHDEP_H_
 
 #include <vm/vm.h>
+
+#define	DB_ELF_SYMBOLS
+#define	DB_ELFSIZE	32
 
 /* types the generic ddb module needs */
 typedef	vaddr_t db_addr_t;
