dc040b6fb0ae5e9c50965bc34cc45dec1b7ddf03
markus <markus>
2001-02-28 08:54:55 +0000

    make sure remote stderr does not get truncated. remove closed fd's from the select mask.

--- usr.bin/ssh/nchan.h
+++ usr.bin/ssh/nchan.h
@@ -22,7 +22,7 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/* RCSID("$OpenBSD: nchan.h,v 1.8 2000/06/20 01:39:43 markus Exp $"); */
+/* RCSID("$OpenBSD: nchan.h,v 1.9 2000/09/07 20:27:52 deraadt Exp $"); */
 
 #ifndef NCHAN_H
 #define NCHAN_H
@@ -84,7 +84,7 @@ extern chan_event_fn	*chan_rcvd_ieof;
 extern chan_event_fn	*chan_write_failed;
 extern chan_event_fn	*chan_obuf_empty;
 
-extern chan_event_fn	*chan_delete_if_full_closed;
+int chan_is_dead(Channel * c);
 
 void    chan_init_iostates(Channel * c);
 void	chan_init(void);
