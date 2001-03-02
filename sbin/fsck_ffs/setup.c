b32e7413b4860ba476906215b2327ffabfc60557
art <art>
2001-03-02 08:33:55 +0000

    Make fsck aware of soft updates. We had this in the tree 1997, but it went away because it was too close to release. Ok: csapuntz@ (as a size note: It's really good to test fsck when you are torture testing the filesystems)

--- sbin/fsck_ffs/setup.c
+++ sbin/fsck_ffs/setup.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: setup.c,v 1.6 1997/10/06 20:22:36 deraadt Exp $	*/
+/*	$OpenBSD: setup.c,v 1.7 1999/08/17 09:13:14 millert Exp $	*/
 /*	$NetBSD: setup.c,v 1.27 1996/09/27 22:45:19 christos Exp $	*/
 
 /*
@@ -38,7 +38,7 @@
 #if 0
 static char sccsid[] = "@(#)setup.c	8.5 (Berkeley) 11/23/94";
 #else
-static char rcsid[] = "$OpenBSD: setup.c,v 1.6 1997/10/06 20:22:36 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: setup.c,v 1.7 1999/08/17 09:13:14 millert Exp $";
 #endif
 #endif /* not lint */
 
@@ -336,8 +336,10 @@ setup(dev)
 		    fsbtodb(&sblock, sblock.fs_csaddr + j * sblock.fs_frag),
 		    size) != 0 && !asked) {
 			pfatal("BAD SUMMARY INFORMATION");
-			if (reply("CONTINUE") == 0)
+			if (reply("CONTINUE") == 0) {
+				ckfini(0);
 				errexit("%s", "");
+			}
 			asked++;
 		}
 	}
@@ -382,6 +384,10 @@ setup(dev)
 		goto badsblabel;
 	}
 	bufinit();
+	if (sblock.fs_flags & FS_DOSOFTDEP)
+		usedsoftdep = 1;
+	else
+		usedsoftdep = 0;
 	return (1);
 
 badsblabel:
