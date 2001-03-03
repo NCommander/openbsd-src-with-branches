319d5ad8738ac7b90b179300e825015374b65b25
art <art>
2001-03-03 12:28:55 +0000

    Allow the syncer to get pages from the pagedaemon reserve. Otherwise we can end up in a situation where the syncer waits for pages and the pagedaemon waits for buffers.

--- sys/uvm/uvm_page.c
+++ sys/uvm/uvm_page.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_page.c,v 1.10 2001/01/29 02:07:47 niklas Exp $	*/
+/*	$OpenBSD: uvm_page.c,v 1.11 2001/03/02 09:07:39 art Exp $	*/
 /*	$NetBSD: uvm_page.c,v 1.23 1999/05/25 01:34:13 thorpej Exp $	*/
 
 /* 
@@ -874,7 +874,8 @@ uvm_pagealloc_strat(obj, off, anon, flags, strat, free_list)
 		(obj && UVM_OBJ_IS_KERN_OBJECT(obj));
 	if ((uvmexp.free <= uvmexp.reserve_kernel && !use_reserve) ||
 	    (uvmexp.free <= uvmexp.reserve_pagedaemon &&
-	     !(use_reserve && curproc == uvm.pagedaemon_proc)))
+	     !(use_reserve && (curproc == uvm.pagedaemon_proc ||
+				curproc == syncerproc))))
 		goto fail;
 
  again:
