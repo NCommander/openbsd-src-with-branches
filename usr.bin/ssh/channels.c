8f1c656d56ff43f3244e9344863e78caa88ba5d1
markus <markus>
2001-03-04 00:03:59 +0000

    debug1->2

--- usr.bin/ssh/channels.c
+++ usr.bin/ssh/channels.c
@@ -40,7 +40,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: channels.c,v 1.95 2001/02/28 21:27:48 markus Exp $");
+RCSID("$OpenBSD: channels.c,v 1.96 2001/02/28 21:31:32 markus Exp $");
 
 #include <openssl/rsa.h>
 #include <openssl/dsa.h>
@@ -1069,7 +1069,7 @@ channel_output_poll()
 		if (compat20 &&
 		    (c->flags & (CHAN_CLOSE_SENT|CHAN_CLOSE_RCVD))) {
 			/* XXX is this true? */
-			debug("channel %d: no data after CLOSE", c->self);
+			debug2("channel %d: no data after CLOSE", c->self);
 			continue;
 		}
 
