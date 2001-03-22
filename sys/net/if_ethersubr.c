a19670e82351c4ee3bcaddc1770e81acf298da34
jason <jason>
2001-03-22 05:26:35 +0000

    remove vlan_proto variable, replace with ETHERTYPE_8021Q define if_type for vlan interfaces (IFT_8021_VLAN) #ifdef DEBUG some debugging printf's protect against other uses of M_PROTO1

--- sys/net/if_ethersubr.c
+++ sys/net/if_ethersubr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ethersubr.c,v 1.41 2001/02/01 01:32:51 jason Exp $	*/
+/*	$OpenBSD: if_ethersubr.c,v 1.42 2001/02/20 19:39:27 mickey Exp $	*/
 /*	$NetBSD: if_ethersubr.c,v 1.19 1996/05/07 02:40:30 thorpej Exp $	*/
 
 /*
@@ -615,7 +615,7 @@ decapsulate:
 	etype = ntohs(eh->ether_type);
 
 #if NVLAN > 0
-	if (etype == vlan_proto) {
+	if (etype == ETHERTYPE_8021Q) {
 		if (vlan_input(eh, m) < 0)
 			ifp->if_data.ifi_noproto++;
 		return;
