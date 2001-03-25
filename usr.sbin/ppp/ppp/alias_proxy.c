62ca2b8483113cc8e445c1f208005fd1112b3d2c
brian <brian>
2001-03-25 12:34:34 +0000

    Remove an extraneous decl

--- usr.sbin/ppp/ppp/alias_proxy.c
+++ usr.sbin/ppp/ppp/alias_proxy.c
@@ -22,7 +22,7 @@
 
     Initial development: April, 1998 (cjm)
 
-    $OpenBSD: alias_proxy.c,v 1.6 2000/09/02 22:12:26 brian Exp $
+    $OpenBSD: alias_proxy.c,v 1.7 2001/01/29 00:29:32 brian Exp $
 */
 
 
@@ -704,7 +704,6 @@ PacketAliasProxyRule(const char *cmd)
                 }
                 else
                 {
-                    int n;
                     int nbits;
                     char s[sizeof(buffer)];
 
