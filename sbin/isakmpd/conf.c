7ec83a7cbfd537ce14961d584534da4db65761aa
ho <ho>
2001-03-27 15:46:29 +0000

    (c)-2001

--- sbin/isakmpd/conf.c
+++ sbin/isakmpd/conf.c
@@ -1,9 +1,9 @@
-/*	$OpenBSD: conf.c,v 1.28 2001/02/28 08:49:43 angelos Exp $	*/
+/*	$OpenBSD: conf.c,v 1.29 2001/03/13 17:56:31 ho Exp $	*/
 /*	$EOM: conf.c,v 1.48 2000/12/04 02:04:29 angelos Exp $	*/
 
 /*
  * Copyright (c) 1998, 1999, 2000, 2001 Niklas Hallqvist.  All rights reserved.
- * Copyright (c) 2000 Håkan Olsson.  All rights reserved.
+ * Copyright (c) 2000, 2001 Håkan Olsson.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
