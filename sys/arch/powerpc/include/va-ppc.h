bc80be08958f830108e07e99dc80fb3e5d38d81e
drahn <drahn>
2001-03-29 18:52:19 +0000

    Add OpenBSD tag to headers missing them.

--- sys/arch/powerpc/include/va-ppc.h
+++ sys/arch/powerpc/include/va-ppc.h
@@ -1,3 +1,4 @@
+/*	$OpenBSD:$	*/
 /* GNU C varargs support for the PowerPC with either the V.4 or Windows NT calling sequences */
 
 #ifndef _WIN32
--- sys/arch/powerpc/include/varargs.h
+++ sys/arch/powerpc/include/varargs.h
@@ -1,3 +1,4 @@
+/*	$OpenBSD:$	*/
 /*	$NetBSD: varargs.h,v 1.1 1996/09/30 16:34:37 ws Exp $	*/
 
 /*-
