ae967ac0815e684ff2694a07cb07a40d2ed0e523
markus <markus>
2001-04-04 22:04:35 +0000

    parse full kexinit packet. make server-side more robust, too.

--- usr.bin/ssh/kexgex.c
+++ usr.bin/ssh/kexgex.c
@@ -24,7 +24,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: kexgex.c,v 1.2 2001/04/03 23:32:12 markus Exp $");
+RCSID("$OpenBSD: kexgex.c,v 1.3 2001/04/04 09:48:34 markus Exp $");
 
 #include <openssl/bn.h>
 
@@ -286,7 +286,7 @@ kexgex_server(Kex *kex)
 		/* unused for old GEX */
 		break;
 	default:
-		fatal("protocol error during kex, no DH_GEX_REQUEST");
+		fatal("protocol error during kex, no DH_GEX_REQUEST: %d", type);
 	}
 	packet_done();
 
