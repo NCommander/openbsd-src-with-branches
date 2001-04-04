af67c04ed0376f235a18571e9d7a60196528f12b
markus <markus>
2001-04-04 15:50:55 +0000

    f-secure 1.3.2 does not handle IGNORE; from milliondl@ornl.gov

--- usr.bin/ssh/compat.c
+++ usr.bin/ssh/compat.c
@@ -23,7 +23,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: compat.c,v 1.41 2001/03/27 10:57:00 markus Exp $");
+RCSID("$OpenBSD: compat.c,v 1.42 2001/03/27 17:46:49 provos Exp $");
 
 #include <regex.h>
 
@@ -93,6 +93,7 @@ compat_datafellows(const char *version)
 		{ "^1\\.7 SecureFX",	SSH_OLD_SESSIONID },
 		{ "^1\\.2\\.1[89]",	SSH_BUG_IGNOREMSG },
 		{ "^1\\.2\\.2[012]",	SSH_BUG_IGNOREMSG },
+		{ "^1\\.3\\.2",		SSH_BUG_IGNOREMSG },	/* f-secure */
 		{ "^SSH Compatible Server",			/* Netscreen */
 					SSH_BUG_PASSWORDPAD },
 		{ "^OSU_0",		SSH_BUG_PASSWORDPAD },
