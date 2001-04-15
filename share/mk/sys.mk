bddc1db710b01f8b737728aeaf63625aa98aba6f
deraadt <deraadt>
2001-04-15 02:35:10 +0000

    move to 2.9-beta

--- share/mk/sys.mk
+++ share/mk/sys.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: sys.mk,v 1.24 2000/08/21 21:13:40 deraadt Exp $
+#	$OpenBSD: sys.mk,v 1.25 2000/10/27 07:57:43 hugh Exp $
 #	$NetBSD: sys.mk,v 1.27 1996/04/10 05:47:19 mycroft Exp $
 #	@(#)sys.mk	5.11 (Berkeley) 3/13/91
 
@@ -10,7 +10,7 @@
 
 unix=		We run OpenBSD.
 OSMAJOR=	2
-OSMINOR=	8
+OSMINOR=	9
 OSREV=		$(OSMAJOR).$(OSMINOR)
 OSrev=		$(OSMAJOR)$(OSMINOR)
 
