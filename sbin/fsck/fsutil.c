2c466387003e56bd3812027fc449d2e40ba9c884
deraadt <deraadt>
2001-05-15 22:03:03 +0000

    do not permit emalloc/erealloc of 0 sized objects

--- sbin/fsck/fsutil.c
+++ sbin/fsck/fsutil.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fsutil.c,v 1.1 1996/10/20 08:36:25 tholo Exp $	*/
+/*	$OpenBSD: fsutil.c,v 1.2 1997/07/25 19:13:18 mickey Exp $	*/
 /*	$NetBSD: fsutil.c,v 1.2 1996/10/03 20:06:31 christos Exp $	*/
 
 /*
@@ -291,7 +291,11 @@ void *
 emalloc(s)
 	size_t s;
 {
-	void *p = malloc(s);
+	void *p;
+
+	if (s == 0)
+		err(1, "malloc failed");
+	p = malloc(s);
 	if (p == NULL)
 		err(1, "malloc failed");
 	return p;
@@ -303,6 +307,8 @@ erealloc(p, s)
 	void *p;
 	size_t s;
 {
+	if (s == 0)
+		err(1, "realloc failed");
 	p = realloc(p, s);
 	if (p == NULL)
 		err(1, "realloc failed");
