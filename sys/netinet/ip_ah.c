80391480848cff10107172875288ef705694f610
angelos <angelos>
2001-05-27 03:48:34 +0000

    Pass a NULL packet tag for now to ipsp_common_input_cb().

--- sys/netinet/ip_ah.c
+++ sys/netinet/ip_ah.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_ah.c,v 1.52 2001/05/13 15:39:27 deraadt Exp $ */
+/*	$OpenBSD: ip_ah.c,v 1.53 2001/05/17 18:41:46 provos Exp $ */
 
 /*
  * The authors of this code are John Ioannidis (ji@tla.org),
@@ -1208,7 +1208,7 @@ ah_output_cb(void *op)
     FREE(ptr, M_XDATA);
     crypto_freereq(crp);
 
-    err =  ipsp_process_done(m, tdb, tdb2);
+    err =  ipsp_process_done(m, tdb, tdb2, NULL);
     splx(s);
     return err;
 
--- sys/netinet/ip_esp.c
+++ sys/netinet/ip_esp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_esp.c,v 1.58 2001/05/13 15:39:27 deraadt Exp $ */
+/*	$OpenBSD: ip_esp.c,v 1.59 2001/05/17 18:41:47 provos Exp $ */
 
 /*
  * The authors of this code are John Ioannidis (ji@tla.org),
@@ -663,7 +663,7 @@ esp_input_cb(void *op)
     m_copyback(m, protoff, sizeof(u_int8_t), lastthree + 2);
 
     /* Back to generic IPsec input processing */
-    err = ipsec_common_input_cb(m, tdb, skip, protoff);
+    err = ipsec_common_input_cb(m, tdb, skip, protoff, NULL);
     splx(s);
     return err;
 
