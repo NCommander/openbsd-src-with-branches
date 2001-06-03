849f77f691222b83fb1640a3a923fe58fc0bcb96
angelos <angelos>
2001-06-03 04:18:22 +0000

    Style.

--- bin/ps/nlist.c
+++ bin/ps/nlist.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: nlist.c,v 1.5 1999/08/16 18:18:34 art Exp $	*/
+/*	$OpenBSD: nlist.c,v 1.6 2001/06/03 04:07:59 angelos Exp $	*/
 /*	$NetBSD: nlist.c,v 1.11 1995/03/21 09:08:03 cgd Exp $	*/
 
 /*-
@@ -38,7 +38,7 @@
 #if 0
 static char sccsid[] = "@(#)nlist.c	8.4 (Berkeley) 4/2/94";
 #else
-static char rcsid[] = "$OpenBSD: nlist.c,v 1.5 1999/08/16 18:18:34 art Exp $";
+static char rcsid[] = "$OpenBSD: nlist.c,v 1.6 2001/06/03 04:07:59 angelos Exp $";
 #endif
 #endif /* not lint */
 
@@ -106,8 +106,7 @@ donlist()
 			eval = rval = 1;
 		}
 	}
-	else
-	{
+	else {
 		siz = sizeof (fscale);
 		mib[0] = CTL_KERN;
 		mib[1] = KERN_FSCALE;
