662d288541f3b3986e02839ed45050f35042fd56
miod <miod>
2001-06-12 22:47:36 +0000

    Pull in patch from current: Errata: sshd(8) allows users to delete arbitrary files named "cookies" if X11 forwarding is enabled. X11 forwarding is disabled by default. Fix (markus): - use fatal_register_cleanup instead of atexit, sync with x11 authdir   handling - switch uid when cleaning up tmp files and sockets; reported by   zen-parse@gmx.net on bugtraq

--- usr.bin/ssh/channels.h
+++ usr.bin/ssh/channels.h
@@ -32,7 +32,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-/* RCSID("$OpenBSD: channels.h,v 1.31 2001/04/13 22:46:53 beck Exp $"); */
+/* RCSID("$OpenBSD: channels.h,v 1.22.2.4 2001/05/07 21:09:27 jason Exp $"); */
 
 #ifndef CHANNELS_H
 #define CHANNELS_H
@@ -292,6 +292,8 @@ void    auth_request_forwarding(void);
  * a static buffer.
  */
 char   *auth_get_socket_name(void);
+
+void	auth_sock_cleanup_proc(void *_pw);
 
 /*
  * This is called to process SSH_CMSG_AGENT_REQUEST_FORWARDING on the server.
