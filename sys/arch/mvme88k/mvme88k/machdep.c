7cbd0c5396524cdd9a0872bcf20d898bc6a1693e
miod <miod>
2001-06-13 21:24:52 +0000

    Actually, because of multi-interrupts devices, use the opposite loop direction. My bad, doh !

--- sys/arch/mvme88k/mvme88k/machdep.c
+++ sys/arch/mvme88k/mvme88k/machdep.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: machdep.c,v 1.37 2001/06/05 23:07:50 miod Exp $	*/
+/* $OpenBSD: machdep.c,v 1.38 2001/06/12 23:09:45 miod Exp $	*/
 /*
  * Copyright (c) 1998, 1999, 2000, 2001 Steve Murphree, Jr.
  * Copyright (c) 1996 Nivas Madhur
@@ -1498,7 +1498,7 @@ intr_findvec(start, end)
 		panic("intr_findvec(): bad parameters");
 #endif
 
-	for (vec = end; vec > start; --vec){
+	for (vec = start; vec < end; vec++){
 		if (intr_handlers[vec] == NULL)
 			return (vec);
 	}
