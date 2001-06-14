79baab85b93baf20fcc739f8319df1765f12c9a3
jj <jj>
2001-06-14 10:41:50 +0000

    Fix for pmap_extract on amiga. int -> paddr_t fix.

--- sys/arch/amiga/amiga/sys_machdep.c
+++ sys/arch/amiga/amiga/sys_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sys_machdep.c,v 1.8 2000/06/23 02:14:35 mickey Exp $	*/
+/*	$OpenBSD: sys_machdep.c,v 1.9 2001/06/08 08:08:42 art Exp $	*/
 /*	$NetBSD: sys_machdep.c,v 1.16 1997/05/19 10:14:47 veego Exp $	*/
 
 /*
@@ -73,8 +73,9 @@ cachectl(req, addr, len)
 #if defined(M68040) || defined(M68060)
 	if (mmutype == MMU_68040) {
 		register int inc = 0;
-		int pa = 0, doall = 0;
+		int doall = 0;
 		caddr_t end = 0;
+		paddr_t pa = 0;
 
 		if (addr == 0 ||
 		    ((req & ~CC_EXTPURGE) != CC_PURGE && len > 2*NBPG))
