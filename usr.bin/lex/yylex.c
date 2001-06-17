c56f48edc520508356c93e7166f2e57866fbb98c
deraadt <deraadt>
2001-06-17 07:30:42 +0000

    Vern says: Yes, this came up a while ago, and I put together a flex release with a modified copyright that begins:      Redistribution and use in source and binary forms, with or      without modification, are permitted provided that: [...] feel free to update the copyrights in each of the files with the one in COPYING.

--- usr.bin/lex/yylex.c
+++ usr.bin/lex/yylex.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: yylex.c,v 1.2 1996/06/26 05:35:44 deraadt Exp $	*/
+/*	$OpenBSD: yylex.c,v 1.3 1996/07/13 22:22:15 millert Exp $	*/
 
 /* yylex - scanner front-end for flex */
 
@@ -13,22 +13,22 @@
  * to contract no. DE-AC03-76SF00098 between the United States
  * Department of Energy and the University of California.
  *
- * Redistribution and use in source and binary forms are permitted provided
- * that: (1) source distributions retain this entire copyright notice and
- * comment, and (2) distributions including binaries display the following
- * acknowledgement:  ``This product includes software developed by the
- * University of California, Berkeley and its contributors'' in the
- * documentation or other materials provided with the distribution and in
- * all advertising materials mentioning features or use of this software.
- * Neither the name of the University nor the names of its contributors may
- * be used to endorse or promote products derived from this software without
- * specific prior written permission.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that: (1) source distributions
+ * retain this entire copyright notice and comment, and (2) distributions
+ * including binaries display the following acknowledgement:  ``This product
+ * includes software developed by the University of California, Berkeley
+ * and its contributors'' in the documentation or other materials provided
+ * with the distribution and in all advertising materials mentioning
+ * features or use of this software. Neither the name of the University nor
+ * the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
  * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  */
 
-/* $Header: /home/daffy/u0/vern/flex/RCS/yylex.c,v 2.13 95/03/04 16:10:41 vern Exp $ */
+/* $Header: /cvs/src/usr.bin/lex/yylex.c,v 1.3 1996/07/13 22:22:15 millert Exp $ */
 
 #include <ctype.h>
 #include "flexdef.h"
