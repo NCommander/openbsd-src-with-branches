632df6d0d69bb07a154dd8c95062b97bce49a5e9
deraadt <deraadt>
2001-06-18 08:34:45 +0000

    oops, backwards check

--- sys/crypto/criov.c
+++ sys/crypto/criov.c
@@ -1,4 +1,4 @@
-/*      $OpenBSD: criov.c,v 1.3 2001/06/08 08:09:35 art Exp $	*/
+/*      $OpenBSD: criov.c,v 1.4 2001/06/18 07:57:40 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1999 Theo de Raadt
@@ -66,8 +66,8 @@ iov2pages(uio, np, pp, lp, maxp, nicep)
 
 next_page:
 		
-		if (pmap_extract(pmap_kernel(), va, &pa) == TRUE)
-			panic("iov2pages: unmapped pages 0x%x", va);
+		if (pmap_extract(pmap_kernel(), va, &pa) == FALSE)
+			panic("iov2pages: unmapped pages");
 
 		pa += off;
 
