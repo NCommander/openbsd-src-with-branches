112ac61b722dd42433e472e058cbb0c53f0337ec
provos <provos>
2001-06-25 01:00:35 +0000

    do not drop unsupported protocol. default to pass. needs to be configurable later.

--- sys/net/pf.c
+++ sys/net/pf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf.c,v 1.18 2001/06/25 00:02:54 dhartmei Exp $ */
+/*	$OpenBSD: pf.c,v 1.19 2001/06/25 00:14:57 niklas Exp $ */
 
 /*
  * Copyright (c) 2001, Daniel Hartmeier
@@ -1788,9 +1788,7 @@ pf_test(int direction, struct ifnet *ifp, struct mbuf **m)
 	}
 
 	default:
-		printf("packetfilter: dropping unsupported protocol");
-		print_ip(ifp, h);
-		action = PF_DROP;
+		action = PF_PASS;
 		break;
 	}
 
