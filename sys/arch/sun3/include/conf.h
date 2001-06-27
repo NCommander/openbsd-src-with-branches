220e609dbfb15705eb70c5b7b0c392c3f40fbeee
angelos <angelos>
2001-06-27 06:24:05 +0000

    IPF cleanup; openbsd@davidkrause.com

--- sys/arch/sun3/include/conf.h
+++ sys/arch/sun3/include/conf.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: conf.h,v 1.7 1997/02/22 21:09:16 kstailey Exp $	*/
+/*	$OpenBSD: conf.h,v 1.8 2000/03/02 23:01:45 todd Exp $	*/
 
 /*-
  * Copyright (c) 1996 Kenneth Stailey.  All rights reserved.
@@ -74,14 +74,5 @@ cdev_decl(bw2);
 cdev_decl(cg2);
 #include "cgfour.h"
 cdev_decl(cg4);
-
-/* IP packet filtering */
-/* open, close, read, ioctl */
-cdev_decl(ipl);
-#ifdef IPFILTER
-#define NIPF 1
-#else
-#define NIPF 0
-#endif
 
 cdev_decl(ksyms);
