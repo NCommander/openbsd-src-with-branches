f232a26b4cb12cacd271d1d3f1081e59f50bac91
provos <provos>
2001-06-27 02:45:58 +0000

    make it understand protocol numbers, too

--- sbin/pfctl/pfctl_parser.c
+++ sbin/pfctl/pfctl_parser.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pfctl_parser.c,v 1.16 2001/06/26 22:36:28 dhartmei Exp $ */
+/*	$OpenBSD: pfctl_parser.c,v 1.17 2001/06/26 22:56:01 dugsong Exp $ */
 
 /*
  * Copyright (c) 2001, Daniel Hartmeier
@@ -658,6 +658,11 @@ parse_rule(int n, char *l, struct pf_rule *r)
 		struct protoent *p;
 		w = next_word(&l);
 		p = getprotobyname(w);
+		if (p == NULL) {
+			int proto = atoi(w);
+			if (proto > 0)
+				p = getprotobynumber(proto);
+		}
 		if (p == NULL) {
 			error(n, "unknown protocol %s\n", w);
 			return (0);
