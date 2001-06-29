5fd109c7c19e4142a64eee0490a855baf364b0dd
ho <ho>
2001-06-29 18:12:07 +0000

    A few more direct sa_len usages.

--- sbin/isakmpd/sa.c
+++ sbin/isakmpd/sa.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sa.c,v 1.42 2001/05/31 20:25:37 angelos Exp $	*/
+/*	$OpenBSD: sa.c,v 1.43 2001/06/07 03:20:21 angelos Exp $	*/
 /*	$EOM: sa.c,v 1.112 2000/12/12 00:22:52 niklas Exp $	*/
 
 /*
@@ -203,7 +203,8 @@ sa_check_peer (struct sa *sa, void *v_addr)
     return 0;
 
   sa->transport->vtbl->get_dst (sa->transport, &dst, &dstlen);
-  return dstlen == addr->len && memcmp (dst, addr->addr, dstlen) == 0;
+  return dst->sa_len == addr->len
+    && memcmp (dst, addr->addr, dst->sa_len) == 0;
 }
 
 struct dst_isakmpspi_arg {
