0c42f70cb66c72a53522cecbbff1fdfd8dbc7def
niklas <niklas>
2001-07-09 23:10:04 +0000

    sync

--- sys/dev/cardbus/cardbusdevs.h
+++ sys/dev/cardbus/cardbusdevs.h
@@ -4,7 +4,7 @@
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	OpenBSD: cardbusdevs,v 1.9 2001/07/09 19:41:38 mickey Exp 
+ *	OpenBSD: cardbusdevs,v 1.10 2001/07/09 23:09:29 niklas Exp 
  */
 /*	$NetBSD: cardbusdevs,v 1.7 1999/12/11 22:22:34 explorer Exp $	*/
 
@@ -97,4 +97,4 @@
 /* this is the device id `indicating 21143 driver compatibility' */
 #define	CARDBUS_PRODUCT_XIRCOM_X3201_3_21143	0x0003		/* X3201-3 (21143) */
 #define	CARDBUS_PRODUCT_XIRCOM_MODEM56	0x0103		/* 56k Modem */
-#define	CARDBUS_PRODUCT_XIRCOM_MODEM56	0x0105		/* CBEM56G Modem */
+#define	CARDBUS_PRODUCT_XIRCOM_CBEM56G	0x0105		/* CBEM56G Modem */
