753563c1ec216f8e59d86cb4095191877544e190
brian <brian>
2001-07-09 00:08:55 +0000

    Add a ``nat proto'' command -- similar to natd(8)'s -redirect_proto switch.

--- usr.sbin/ppp/ppp/nat_cmd.h
+++ usr.sbin/ppp/ppp/nat_cmd.h
@@ -24,13 +24,14 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$OpenBSD: nat_cmd.h,v 1.4 2000/06/23 09:47:05 brian Exp $
+ *	$OpenBSD: nat_cmd.h,v 1.5 2001/06/07 09:32:55 brian Exp $
  */
 
 struct cmdargs;
 
 extern int nat_RedirectPort(struct cmdargs const *);
 extern int nat_RedirectAddr(struct cmdargs const *);
+extern int nat_RedirectProto(struct cmdargs const *);
 extern int nat_ProxyRule(struct cmdargs const *);
 extern int nat_SetTarget(struct cmdargs const *);
 
