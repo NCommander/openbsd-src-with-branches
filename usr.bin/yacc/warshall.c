62bf8e5b743abbbcf770f7a123ea6d42132c386c
pvalchev <pvalchev>
2001-07-16 06:29:45 +0000

    -Wall cleanup; ok deraadt

--- usr.bin/yacc/warshall.c
+++ usr.bin/yacc/warshall.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: warshall.c,v 1.4 1996/03/19 03:21:51 jtc Exp $	*/
+/*	$OpenBSD: warshall.c,v 1.3 1996/06/26 05:44:40 deraadt Exp $	*/
 /*	$NetBSD: warshall.c,v 1.4 1996/03/19 03:21:51 jtc Exp $	*/
 
 /*
@@ -41,12 +41,15 @@
 #if 0
 static char sccsid[] = "@(#)warshall.c	5.4 (Berkeley) 5/24/93";
 #else
-static char rcsid[] = "$OpenBSD: warshall.c,v 1.4 1996/03/19 03:21:51 jtc Exp $";
+static char rcsid[] = "$OpenBSD: warshall.c,v 1.3 1996/06/26 05:44:40 deraadt Exp $";
 #endif
 #endif /* not lint */
 
 #include "defs.h"
 
+void transitive_closure __P((unsigned *, int));
+
+void
 transitive_closure(R, n)
 unsigned *R;
 int n;
@@ -99,6 +102,7 @@ int n;
     }
 }
 
+void
 reflexive_transitive_closure(R, n)
 unsigned *R;
 int n;
