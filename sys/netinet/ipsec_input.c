7c8f2a3d3e750836b5b79b6594b803c097f1f15f
deraadt <deraadt>
2001-08-07 14:07:47 +0000

    enable ah & esp by default, now that we trust the code more

--- sys/netinet/ipsec_input.c
+++ sys/netinet/ipsec_input.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ipsec_input.c,v 1.49 2001/07/05 16:45:55 jjbg Exp $	*/
+/*	$OpenBSD: ipsec_input.c,v 1.50 2001/07/06 19:35:38 jjbg Exp $	*/
 /*
  * The authors of this code are John Ioannidis (ji@tla.org),
  * Angelos D. Keromytis (kermit@csd.uch.gr) and
@@ -84,8 +84,8 @@ void *ipsec_common_ctlinput(int, struct sockaddr *, void *, int);
 #endif
 
 /* sysctl variables */
-int esp_enable = 0;
-int ah_enable = 0;
+int esp_enable = 1;
+int ah_enable = 1;
 int ipcomp_enable = 0;
 
 #ifdef INET6
