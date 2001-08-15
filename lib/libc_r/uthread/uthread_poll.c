4cc6cb9e5054e5d096384355400b300dd7193f62
fgsch <fgsch>
2001-08-15 15:47:56 +0000

    Use INFTIM.

--- lib/libc_r/uthread/uthread_poll.c
+++ lib/libc_r/uthread/uthread_poll.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uthread_poll.c,v 1.3 1999/11/25 07:01:41 d Exp $	*/
+/*	$OpenBSD: uthread_poll.c,v 1.4 2000/01/06 08:28:53 d Exp $	*/
 /*
  * Copyright (c) 1999 Daniel Eischen <eischen@vigrid.com>
  * All rights reserved.
@@ -56,7 +56,7 @@ poll(struct pollfd fds[], int nfds, int timeout)
 		numfds = _thread_dtablesize;
 	}
 	/* Check if a timeout was specified: */
-	if (timeout == -1) {
+	if (timeout == INFTIM) {
 		/* Wait for ever: */
 		_thread_kern_set_timeout(NULL);
 	} else if (timeout > 0) {
