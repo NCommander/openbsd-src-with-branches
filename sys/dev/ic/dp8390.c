b529d34ca3ade81282881707ae8dd02fb0a90f7b
aaron <aaron>
2001-08-18 16:50:03 +0000

    Make use of ax88190_stop_card and dl10019_stop_card.

--- sys/dev/ic/dp8390.c
+++ sys/dev/ic/dp8390.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dp8390.c,v 1.16 2001/07/08 23:38:05 fgsch Exp $	*/
+/*	$OpenBSD: dp8390.c,v 1.17 2001/07/09 17:22:12 fgsch Exp $	*/
 /*	$NetBSD: dp8390.c,v 1.13 1998/07/05 06:49:11 jonathan Exp $	*/
 
 /*
@@ -221,6 +221,9 @@ dp8390_stop(sc)
 	while (((NIC_GET(regt, regh,
 	    ED_P0_ISR) & ED_ISR_RST) == 0) && --n)
 		;
+
+	if (sc->stop_card != NULL)
+		(*sc->stop_card)(sc);
 }
 
 /*
