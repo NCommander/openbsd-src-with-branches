a40a531ee43e3299edac875885e790a3b437b9e2
mickey <mickey>
2001-09-10 16:44:52 +0000

    move vtophys proto into a port-area in order to remove it for macppc eventually; drahn@ ok

--- sys/arch/macppc/include/pmap.h
+++ sys/arch/macppc/include/pmap.h
@@ -1,3 +1,5 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: pmap.h,v 1.1 2001/09/01 15:49:06 drahn Exp $	*/
 
 #include <powerpc/pmap.h>
+
+paddr_t vtophys __P((vaddr_t));
--- sys/arch/mvmeppc/include/pmap.h
+++ sys/arch/mvmeppc/include/pmap.h
@@ -1,3 +1,5 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: pmap.h,v 1.3 2001/09/02 19:40:24 miod Exp $	*/
 
 #include <powerpc/pmap.h>
+
+paddr_t vtophys __P((vaddr_t));
