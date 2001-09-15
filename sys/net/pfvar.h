01d48f4d05a7b9fa0acc40b20f88469ca8603151
dhartmei <dhartmei>
2001-09-15 16:47:07 +0000

    Don't use m_pkthdr.rcvif in pflog_packet(), it doesn't work for outgoing packets and is obviously invalid (and not NULL) for IPv6 packets (hence crashed). Pass ifp down instead.
    sizeof(ih) instead of sizeof(&ih) for pf_pull_hdr() from pf_test6().

--- sys/net/pfvar.h
+++ sys/net/pfvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pfvar.h,v 1.50 2001/09/06 18:05:46 jasoni Exp $ */
+/*	$OpenBSD: pfvar.h,v 1.51 2001/09/15 03:54:40 frantzen Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -558,7 +558,8 @@ int	pf_tree_insert(struct pf_tree_node **, struct pf_tree_node *,
 int	pf_tree_remove(struct pf_tree_node **, struct pf_tree_node *,
 	    struct pf_tree_key *);
 
-int	pflog_packet(struct mbuf *, int, u_short, u_short, struct pf_rule *);
+int	pflog_packet(struct ifnet *, struct mbuf *, int, u_short, u_short,
+	    struct pf_rule *);
 int	pf_match_addr(u_int8_t, struct pf_addr *, struct pf_addr *,
 	    struct pf_addr *, int);
 int	pf_match_port(u_int8_t, u_int16_t, u_int16_t, u_int16_t);
