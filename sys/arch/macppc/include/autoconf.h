f5f9835cdc151b9f80816c24dd0381809773d704
mickey <mickey>
2001-09-15 01:42:55 +0000

    pass the dma tag to obio; drahn@ ok

--- sys/arch/macppc/include/autoconf.h
+++ sys/arch/macppc/include/autoconf.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: autoconf.h,v 1.10 2001/07/09 02:14:05 mickey Exp $ */
+/*	$OpenBSD: autoconf.h,v 1.1 2001/09/01 15:49:06 drahn Exp $ */
 
 /*
  * Copyright (c) 1997 Per Fogelstrom
@@ -86,6 +86,7 @@ struct confargs {
 	/* macobio hooks ?? */
 	bus_space_tag_t ca_iot;
 	bus_space_tag_t ca_memt; /* XXX */
+	bus_dma_tag_t ca_dmat;
 	u_int32_t ca_node;
 	int ca_nreg;
 	u_int32_t *ca_reg;
