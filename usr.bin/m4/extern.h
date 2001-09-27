4d1175f38a6a20066ffe1a91a4f58ce475b05ab4
espie <espie>
2001-09-27 11:40:33 +0000

    traceon/traceoff built-ins.

--- usr.bin/m4/extern.h
+++ usr.bin/m4/extern.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: extern.h,v 1.25 2001/09/18 14:55:52 espie Exp $	*/
+/*	$OpenBSD: extern.h,v 1.26 2001/09/19 13:14:18 espie Exp $	*/
 /*	$NetBSD: extern.h,v 1.3 1996/01/13 23:25:24 pk Exp $	*/
 
 /*-
@@ -117,7 +117,7 @@ extern char *endpbb;
 extern char *endest;
 
 /* trace.c */
-extern void mark_traced __P((const char *));
+extern void mark_traced __P((const char *, int));
 extern int is_traced __P((const char *));
 extern void trace_file __P((const char *));
 extern ssize_t trace __P((const char **, int, struct input_file *));
