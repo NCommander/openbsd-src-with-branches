8a940fe227b365ac1d5367f35b5506868881d2e5
millert <millert>
2001-09-28 01:42:54 +0000

    sysctl() support for getting the SYSV *info structs and the associated SYSV structs.  To be used by ipcs(1).  Based on work by simonb@netbsd.org

--- sys/sys/msg.h
+++ sys/sys/msg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: msg.h,v 1.5 1998/06/11 19:21:28 deraadt Exp $	*/
+/*	$OpenBSD: msg.h,v 1.6 2001/08/12 22:50:12 millert Exp $	*/
 /*	$NetBSD: msg.h,v 1.9 1996/02/09 18:25:18 christos Exp $	*/
 
 /*
@@ -114,7 +114,12 @@ struct msginfo {
 		msgssz,		/* size of a message segment (see notes above) */
 		msgseg;		/* number of message segments */
 };
-struct msginfo	msginfo;
+struct msginfo	msginfo;	/* XXX */
+
+struct msg_sysctl_info {
+	struct msginfo msginfo;
+	struct msqid_ds msgids[1];
+};
 
 #ifndef MSGSSZ
 #define MSGSSZ	8		/* Each segment must be 2^N long */
