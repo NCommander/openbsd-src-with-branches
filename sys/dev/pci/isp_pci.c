002467d8f91d047409c7a43f48bc191189ef968c
mjacob <mjacob>
2001-09-29 01:23:54 +0000

    If we field an interrupt, return to the caller that we did so. Otherwise, on some platforms, after a while they'll think it's all spurious and disconnect the interrupt routing. Thereupon Bad Things Happen (tm).
    Approved by: Theo.

--- sys/dev/pci/isp_pci.c
+++ sys/dev/pci/isp_pci.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: isp_pci.c,v 1.22 2001/08/26 03:32:22 jason Exp $	*/
+/*	$OpenBSD: isp_pci.c,v 1.23 2001/09/01 07:16:40 mjacob Exp $	*/
 /*
  * PCI specific probe and attach routines for Qlogic ISP SCSI adapters.
  *
@@ -1091,7 +1091,7 @@ isp_pci_intr(void *arg)
 		isp->isp_osinfo.onintstack = 1;
 		isp_intr(isp, isr, sema, mbox);
 		isp->isp_osinfo.onintstack = 0;
-		return (0);
+		return (1);
 	}
 }
 
