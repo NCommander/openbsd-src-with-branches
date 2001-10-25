0149f018fbe39df579cdea8634a91c4030889156
drahn <drahn>
2001-10-25 14:30:43 +0000

    If the console keyboard goes away, reinit wskbd_console_device. Will allow future console keyboard attaches to occur.

--- sys/dev/wscons/wskbd.c
+++ sys/dev/wscons/wskbd.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: wskbd.c,v 1.25 2001/09/26 01:21:37 jcs Exp $ */
+/* $OpenBSD: wskbd.c,v 1.26 2001/09/30 05:49:58 mickey Exp $ */
 /* $NetBSD: wskbd.c,v 1.38 2000/03/23 07:01:47 thorpej Exp $ */
 
 /*
@@ -546,6 +546,11 @@ wskbd_detach(self, flags)
 		timeout_del(&sc->sc_repeat_ch);
 	}
 #endif
+
+	if (sc->sc_isconsole) {
+		KASSERT(wskbd_console_device == sc);
+		wskbd_console_device = NULL;
+	}
 
 	evar = &sc->sc_events;
 	if (evar->io) {
