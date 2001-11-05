8f1f33c5152e01bc6969e06a002c1acbf58e125c
art <art>
2001-11-05 22:20:18 +0000

    No need to include vm/vm_param.h here, sys/sysctl.h does that for us.

--- lib/libc/gen/getloadavg.c
+++ lib/libc/gen/getloadavg.c
@@ -32,14 +32,13 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD: getloadavg.c,v 1.5 1995/02/25 08:51:15 cgd Exp $";
+static char rcsid[] = "$OpenBSD: getloadavg.c,v 1.2 1996/08/19 08:23:39 tholo Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <sys/sysctl.h>
-#include <vm/vm_param.h>
 
 #include <stdlib.h>
 
