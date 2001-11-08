d3fb3b4e45e99db3220ba07a8f7109e52978b434
itojun <itojun>
2001-11-08 09:52:02 +0000

    use strncpy() to set ifname arg to ioctl

--- usr.sbin/ndp/ndp.c
+++ usr.sbin/ndp/ndp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ndp.c,v 1.10 2001/03/21 15:32:27 itojun Exp $	*/
+/*	$OpenBSD: ndp.c,v 1.11 2001/07/23 18:42:32 itojun Exp $	*/
 /*	$KAME: ndp.c,v 1.69 2001/07/23 14:46:31 itojun Exp $	*/
 
 /*
@@ -915,7 +915,7 @@ ifinfo(argc, argv)
 		/* NOTREACHED */
 	}
 	bzero(&nd, sizeof(nd));
-	strcpy(nd.ifname, ifname);
+	strncpy(nd.ifname, ifname, sizeof(nd.ifname));
 	if (ioctl(s, SIOCGIFINFO_IN6, (caddr_t)&nd) < 0) {
  		err(1, "ioctl(SIOCGIFINFO_IN6)");
 		/* NOTREACHED */
