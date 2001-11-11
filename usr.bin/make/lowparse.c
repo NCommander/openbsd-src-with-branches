d4e7a2061dceee642e8a4d16a50e2c4d32b166f3
espie <espie>
2001-11-11 12:35:03 +0000

    Fixed version... don't see how this could work on i386, since it didn't initialize create in main.c.

--- usr.bin/make/lowparse.c
+++ usr.bin/make/lowparse.c
@@ -1,5 +1,5 @@
 /*	$OpenPackages$ */
-/*	$OpenBSD: lowparse.c,v 1.13 2001/07/18 14:49:13 espie Exp $ */
+/*	$OpenBSD: lowparse.c,v 1.15 2001/11/11 06:02:06 deraadt Exp $ */
 
 /* low-level parsing functions. */
 
@@ -443,7 +443,7 @@ Parse_Getfilename()
 void
 LowParse_Init()
 {
-    Lst_Init(&input_stack);
+    Static_Lst_Init(&input_stack);
     current = NULL;
 }
 
