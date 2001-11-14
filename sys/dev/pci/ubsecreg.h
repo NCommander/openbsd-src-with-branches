dba348d40b3437a98f02a5aa43c58e6868016463
jason <jason>
2001-11-14 00:29:25 +0000

    Define the TRDY timeout and RETRY timeout registers (not used... yet)

--- sys/dev/pci/ubsecreg.h
+++ sys/dev/pci/ubsecreg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ubsecreg.h,v 1.13 2001/05/30 02:26:14 jason Exp $	*/
+/*	$OpenBSD: ubsecreg.h,v 1.14 2001/06/29 21:52:42 jason Exp $	*/
 
 /*
  * Copyright (c) 2000 Theo de Raadt
@@ -33,7 +33,9 @@
  * datasheet.
  */
 
-#define BS_BAR		0x10	/* DMA and status base address register */
+#define BS_BAR			0x10	/* DMA base address register */
+#define	BS_TRDY_TIMEOUT		0x40	/* TRDY timeout */
+#define	BS_RETRY_TIMEOUT	0x41	/* DMA retry timeout */
 
 /*
  * DMA Control & Status Registers (offset from BS_BAR)
