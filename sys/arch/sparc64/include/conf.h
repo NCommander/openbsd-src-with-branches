27d5f9b61915fc04c37752541209ce57146b7aa2
jason <jason>
2001-11-15 03:59:08 +0000

    cdev glue for crypto(9) [Not known to work, but will as soon as interrupt issues are ironed out]

--- sys/arch/sparc64/include/conf.h
+++ sys/arch/sparc64/include/conf.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: conf.h,v 1.3 2001/09/20 20:50:03 jason Exp $	*/
+/*	$OpenBSD: conf.h,v 1.4 2001/10/28 03:14:59 jason Exp $	*/
 /*	$NetBSD: conf.h,v 1.9 2001/03/26 12:33:26 lukem Exp $	*/
 
 /*-
@@ -40,6 +40,8 @@
 #define mmread mmrw
 #define mmwrite mmrw
 cdev_decl(mm);
+
+cdev_decl(crypto);
 
 cdev_decl(ksyms);
 
