62f8c145e2616b2978748201ab3e37c96fe1ba58
deraadt <deraadt>
2001-11-19 04:21:41 +0000

    no signals, no need for signal.h

--- usr.sbin/ypserv/yppush/yppush_svc.c
+++ usr.sbin/ypserv/yppush/yppush_svc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: yppush_svc.c,v 1.3 2000/06/28 23:58:24 deraadt Exp $ */
+/*	$OpenBSD: yppush_svc.c,v 1.4 2000/10/12 09:47:28 deraadt Exp $ */
 
 /*
  * Copyright (c) 1996 Mats O Jansson <moj@stacken.kth.se>
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char rcsid[] = "$OpenBSD: yppush_svc.c,v 1.3 2000/06/28 23:58:24 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: yppush_svc.c,v 1.4 2000/10/12 09:47:28 deraadt Exp $";
 #endif /* not lint */
 
 #include "yppush.h"
@@ -44,7 +44,6 @@ static char rcsid[] = "$OpenBSD: yppush_svc.c,v 1.3 2000/06/28 23:58:24 deraadt 
 #include <stdio.h>
 #include <stdlib.h>
 #include <netdb.h>
-#include <signal.h>
 #include <memory.h>
 #include <syslog.h>
 
