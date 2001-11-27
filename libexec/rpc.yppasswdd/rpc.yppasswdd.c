a22193fa43fb3d7ee6ce80ec3f0e85c8a33af513
millert <millert>
2001-11-27 01:02:48 +0000

    Use strlcat() when adding to 'make_arg' via the -m flag to avoid overflowing.  Closes PR 2201.

--- libexec/rpc.yppasswdd/rpc.yppasswdd.c
+++ libexec/rpc.yppasswdd/rpc.yppasswdd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: rpc.yppasswdd.c,v 1.9 1997/08/19 07:00:51 niklas Exp $	*/
 
 /*
  * Copyright (c) 1994 Mats O Jansson <moj@stacken.kth.se>
@@ -32,7 +32,7 @@
  */
 
 #ifndef LINT
-static char rcsid[] = "$OpenBSD: rpc.yppasswdd.c,v 1.8 1997/08/05 02:26:50 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: rpc.yppasswdd.c,v 1.9 1997/08/19 07:00:51 niklas Exp $";
 #endif
 
 #include <sys/types.h>
@@ -90,8 +90,16 @@ main(argc, argv)
 			} else if (strcmp("-m", argv[i]) == 0) {
 				domake = 1;
 				while (i < argc) {
-					strcat(make_arg, " ");
-					strcat(make_arg, argv[i]);
+					if (strlcat(make_arg, " ",
+					    sizeof(make_arg)) >=
+					    sizeof(make_arg) ||
+					    strlcat(make_arg, argv[i],
+					    sizeof(make_arg)) >=
+					    sizeof(make_arg)) {
+						(void) fprintf(stderr,
+						    "-m argument too long.\n");
+						exit(1);
+					}
 					i++;
 				}
 			} else if (strcmp("-d", argv[i]) == 0
