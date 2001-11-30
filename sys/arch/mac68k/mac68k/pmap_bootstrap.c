d42957bc354423b604668d45615faf26f541981a
miod <miod>
2001-11-30 20:58:18 +0000

    Switch to pmap_motorola.

--- sys/arch/mac68k/mac68k/pmap_bootstrap.c
+++ sys/arch/mac68k/mac68k/pmap_bootstrap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pmap_bootstrap.c,v 1.14 2001/05/08 17:30:41 aaron Exp $	*/
+/*	$OpenBSD: pmap_bootstrap.c,v 1.15 2001/11/06 19:53:15 miod Exp $	*/
 /*	$NetBSD: pmap_bootstrap.c,v 1.50 1999/04/07 06:14:33 scottr Exp $	*/
 
 /* 
@@ -641,4 +641,23 @@ bootstrap_mac68k(tc)
 		zs_init();
 
 	videoaddr = newvideoaddr;
+}
+
+void
+pmap_init_md()
+{
+	vaddr_t addr;
+
+	/*
+	 * Mark as unavailable the regions which we have mapped in
+	 * pmap_bootstrap().
+	 */
+	addr = (vaddr_t)IOBase;
+	if (uvm_map(kernel_map, &addr,
+		    m68k_ptob(IIOMAPSIZE + ROMMAPSIZE + VIDMAPSIZE),
+		    NULL, UVM_UNKNOWN_OFFSET, 0,
+		    UVM_MAPFLAG(UVM_PROT_NONE, UVM_PROT_NONE,
+				UVM_INH_NONE, UVM_ADV_RANDOM,
+				UVM_FLAG_FIXED)))
+		panic("pmap_init: bogons in the VM system!\n");
 }
