69800fd5cadda43c1dab6b9930b15f10cd0b463a
art <art>
2001-11-30 16:37:57 +0000

    Don't brelse a random pointer in ffs_realloccg when filesystem is full and bpp == NULL.

--- sys/ufs/ffs/ffs_alloc.c
+++ sys/ufs/ffs/ffs_alloc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ffs_alloc.c,v 1.35 2001/11/21 21:23:56 csapuntz Exp $	*/
+/*	$OpenBSD: ffs_alloc.c,v 1.36 2001/11/27 05:27:12 art Exp $	*/
 /*	$NetBSD: ffs_alloc.c,v 1.11 1996/05/11 18:27:09 mycroft Exp $	*/
 
 /*
@@ -170,7 +170,7 @@ ffs_realloccg(ip, lbprev, bpref, osize, nsize, cred, bpp, blknop)
 	ufs_daddr_t *blknop;
 {
 	struct fs *fs;
-	struct buf *bp;
+	struct buf *bp = NULL;
 	ufs_daddr_t quota_updated = 0;
 	int cg, request, error;
 	daddr_t bprev, bno;
