a91ac1fcdc0fa0b7f2caf3b68eccea320549bf18
deraadt <deraadt>
2001-12-01 23:42:22 +0000

    spelling; krapht@secureops.com

--- sys/sys/proc.h
+++ sys/sys/proc.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: proc.h,v 1.49 2001/11/06 18:41:10 art Exp $	*/
+/*	$OpenBSD: proc.h,v 1.50 2001/11/11 22:30:56 art Exp $	*/
 /*	$NetBSD: proc.h,v 1.44 1996/04/22 01:23:21 christos Exp $	*/
 
 /*-
@@ -107,8 +107,8 @@ struct	emul {
  * control, known in UN*X as a process; it has references to substructures
  * containing descriptions of things that the process uses, but may share
  * with related processes.  The process structure and the substructures
- * are always addressible except for those marked "(PROC ONLY)" below,
- * which might be addressible only on a processor on which the process
+ * are always addressable except for those marked "(PROC ONLY)" below,
+ * which might be addressable only on a processor on which the process
  * is running.
  */
 struct	proc {
