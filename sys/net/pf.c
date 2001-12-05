4ede9ec55d10156fdab9249d5a0b852812eb6a43
jason <jason>
2001-12-05 19:47:39 +0000

    Pull in patch from current: Fix (dhartmei): Use pf_pull_hdr() instead of manual mbuf traversal. Fixes potential crashes in pf_test6() for IPv6 packets with options.

--- sys/net/pf.c
+++ sys/net/pf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf.c,v 1.166 2001/11/13 17:45:26 frantzen Exp $ */
+/*	$OpenBSD: pf.c,v 1.163.2.1 2001/11/13 18:45:11 jason Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -4513,11 +4513,6 @@ pf_test6(int dir, struct ifnet *ifp, struct mbuf **m0)
 	off = ((caddr_t)h - m->m_data) + sizeof(struct ip6_hdr);
 	pd.proto = h->ip6_nxt;
 	do {			
-		while (off >= m->m_len) {
-			off -= m->m_len;
-			m = m->m_next;
-		}
-
 		switch (pd.proto) {
 		case IPPROTO_FRAGMENT: 
 			/* XXX we don't handle fragments yet */
@@ -4529,11 +4524,19 @@ pf_test6(int dir, struct ifnet *ifp, struct mbuf **m0)
 		case IPPROTO_ROUTING:
 		case IPPROTO_DSTOPTS: {
 			/* get next header and header length */
-			struct _opt6 *opt6;
+			struct _opt6 opt6;
 
-			opt6 = (struct _opt6 *)(mtod(m, caddr_t) + off);
-			pd.proto = opt6->opt6_nxt;
-			off += (opt6->opt6_hlen + 1) * 8;
+			if (!pf_pull_hdr(m, off, &opt6, sizeof(opt6),
+			    NULL, NULL, pd.af)) {
+				DPFPRINTF(PF_DEBUG_MISC,
+				    ("pf: IPv6 short opt\n"));
+				action = PF_DROP;
+				REASON_SET(&reason, PFRES_SHORT);
+				log = 1;
+				goto done;
+			}
+			pd.proto = opt6.opt6_nxt;
+			off += (opt6.opt6_hlen + 1) * 8;
 			/* goto the next header */
 			break;
 		}
