de588a6a1728685ef649882c910ad266057e1c78
nate <nate>
2001-12-14 00:44:59 +0000

    Don't use a single static struct alpha_bus_space since there are machines that can have more than one system bus attached to mainbus

--- sys/arch/alpha/pci/pci_2100_a50.c
+++ sys/arch/alpha/pci/pci_2100_a50.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pci_2100_a50.c,v 1.15 2001/08/17 22:43:19 mickey Exp $	*/
+/*	$OpenBSD: pci_2100_a50.c,v 1.16 2001/11/06 19:53:13 miod Exp $	*/
 /*	$NetBSD: pci_2100_a50.c,v 1.12 1996/11/13 21:13:29 cgd Exp $	*/
 
 /*
@@ -66,7 +66,7 @@ void
 pci_2100_a50_pickintr(acp)
 	struct apecs_config *acp;
 {
-	bus_space_tag_t iot = acp->ac_iot;
+	bus_space_tag_t iot = &acp->ac_iot;
 	pci_chipset_tag_t pc = &acp->ac_pc;
 	pcireg_t sioclass;
 	int sioII;
--- sys/arch/alpha/pci/pci_550.c
+++ sys/arch/alpha/pci/pci_550.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: pci_550.c,v 1.5 2001/08/17 22:26:58 mickey Exp $ */
+/* $OpenBSD: pci_550.c,v 1.6 2001/11/06 19:53:13 miod Exp $ */
 /* $NetBSD: pci_550.c,v 1.18 2000/06/29 08:58:48 mrg Exp $ */
 
 /*-
@@ -136,7 +136,7 @@ void
 pci_550_pickintr(ccp)
 	struct cia_config *ccp;
 {
-	bus_space_tag_t iot = ccp->cc_iot;
+	bus_space_tag_t iot = &ccp->cc_iot;
 	pci_chipset_tag_t pc = &ccp->cc_pc;
 #if 0
 	char *cp;
