2126f4fa0ed0d4eb47d670e3bf06842037aadc59
jason <jason>
2001-12-27 15:39:32 +0000

    Pull in patch from current: Fix (itojun): revision 1.12 lacks paren around && within || (gcc warns and compilation stops).

--- sys/net/if_spppsubr.c
+++ sys/net/if_spppsubr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_spppsubr.c,v 1.10 2001/07/10 11:09:07 espie Exp $	*/
+/*	$OpenBSD: if_spppsubr.c,v 1.10.2.1 2001/12/26 23:51:36 jason Exp $	*/
 /*
  * Synchronous PPP/Cisco link level subroutines.
  * Keepalive protocol implemented in both Cisco and PPP modes.
@@ -703,10 +703,10 @@ sppp_output(struct ifnet *ifp, struct mbuf *m,
 
 		if (! IF_QFULL (&sp->pp_fastq) &&
 		    ((ip->ip_tos & IPTOS_LOWDELAY) ||
-	    	    ((ip->ip_p == IPPROTO_TCP &&
-	    	    m->m_len >= sizeof (struct ip) + sizeof (struct tcphdr) &&
-	    	    (INTERACTIVE (ntohs (tcp->th_sport))) ||
-	    	     INTERACTIVE (ntohs (tcp->th_dport))))))
+	    	     (ip->ip_p == IPPROTO_TCP &&
+	    	      m->m_len >= sizeof (struct ip) + sizeof (struct tcphdr) &&
+	    	      (INTERACTIVE (ntohs (tcp->th_sport)) ||
+	    	       INTERACTIVE (ntohs (tcp->th_dport))))))
 			ifq = &sp->pp_fastq;
 	}
 #endif
