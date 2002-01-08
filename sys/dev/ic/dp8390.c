5ca119149194cf4fcdb66daa18223c862c827d8a
fgsch <fgsch>
2002-01-08 05:38:53 +0000

    SIOCSIFMTU support; patches by Claudio Castiglia <ccastig@softhome.net>

--- sys/dev/ic/dp8390.c
+++ sys/dev/ic/dp8390.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dp8390.c,v 1.18 2001/08/18 16:50:03 aaron Exp $	*/
+/*	$OpenBSD: dp8390.c,v 1.19 2001/09/20 17:02:31 mpech Exp $	*/
 /*	$NetBSD: dp8390.c,v 1.13 1998/07/05 06:49:11 jonathan Exp $	*/
 
 /*
@@ -846,6 +846,14 @@ dp8390_ioctl(ifp, cmd, data)
 		default:
 			dp8390_init(sc);
 			break;
+		}
+		break;
+
+	case SIOCSIFMTU:
+		if (ifr->ifr_mtu > ETHERMTU || ifr->ifr_mtu < ETHERMIN) {
+			error = EINVAL;
+		} else if (ifp->if_mtu != ifr->ifr_mtu) {
+			ifp->if_mtu = ifr->ifr_mtu;
 		}
 		break;
 
