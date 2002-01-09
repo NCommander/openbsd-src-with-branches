5c8fbfa703374953da35a610f672888876c045fe
ericj <ericj>
2002-01-09 18:20:52 +0000

    streams gone

--- usr.bin/modstat/modstat.c
+++ usr.bin/modstat/modstat.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: modstat.c,v 1.15 2002/01/08 04:59:24 ericj Exp $	*/
+/*	$OpenBSD: modstat.c,v 1.16 2002/01/08 17:16:38 ericj Exp $	*/
 
 /*
  * Copyright (c) 1993 Terrence R. Lambert.
@@ -54,7 +54,6 @@ static char *type_names[] = {
 	"SYSCALL",
 	"VFS",
 	"DEV",
-	"STRMOD",
 	"EXEC",
 	"MISC"
 };
