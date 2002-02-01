4ce8b0499b4317d16aa1db59310391f8129f09fb
mickey <mickey>
2002-02-01 08:47:43 +0000

    redefine COM_FREQ to 7.272727MHz

--- sys/arch/hppa/include/param.h
+++ sys/arch/hppa/include/param.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: param.h,v 1.15 2002/01/10 01:26:49 mickey Exp $	*/
+/*	$OpenBSD: param.h,v 1.16 2002/01/25 21:25:55 mickey Exp $	*/
 
 /* 
  * Copyright (c) 1988-1994, The University of Utah and
@@ -139,6 +139,7 @@
 #ifndef _LOCORE
 #define	CONADDR	conaddr
 #define	CONUNIT	conunit
+#define	COM_FREQ	7272727
 extern hppa_hpa_t conaddr;
 extern int conunit;
 #endif
