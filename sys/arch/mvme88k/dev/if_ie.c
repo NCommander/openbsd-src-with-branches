c28d62f5f97074ca8c3ab3aebdd0ad638fceaf9d
miod <miod>
2002-02-05 23:15:33 +0000

    I wonder how those two were not commited among the "volatile police" two weeks ago.

--- sys/arch/mvme88k/dev/if_ie.c
+++ sys/arch/mvme88k/dev/if_ie.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ie.c,v 1.19 2001/12/22 18:45:35 smurph Exp $ */
+/*	$OpenBSD: if_ie.c,v 1.20 2002/01/14 21:34:38 miod Exp $ */
 
 /*-
  * Copyright (c) 1998 Steve Murphree, Jr. 
@@ -252,7 +252,7 @@ void iereset __P((struct ie_softc *));
 void ie_readframe __P((struct ie_softc *, int));
 void ie_drop_packet_buffer __P((struct ie_softc *));
 int command_and_wait __P((struct ie_softc *, int,
-    void volatile *, int));
+    void *volatile, int));
 void ierint __P((struct ie_softc *));
 void ietint __P((struct ie_softc *));
 int ieget __P((struct ie_softc *, struct mbuf **,
