5d656b878f81003925b27c77656e8681b4db177b
art <art>
2002-02-12 16:38:39 +0000

    Fix my total blackout of the brain. I rearrange half of this function to properly handle unlocking in one error case, then I forget to handle that error case. I even had a regression test written for this!
    In other words: fix accept(2) to unlock when it runs out of fds.

--- sys/kern/uipc_syscalls.c
+++ sys/kern/uipc_syscalls.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uipc_syscalls.c,v 1.49 2002/02/11 12:34:30 art Exp $	*/
+/*	$OpenBSD: uipc_syscalls.c,v 1.50 2002/02/12 13:05:31 art Exp $	*/
 /*	$NetBSD: uipc_syscalls.c,v 1.19 1996/02/09 19:00:48 christos Exp $	*/
 
 /*
@@ -226,8 +226,7 @@ sys_accept(p, v, retval)
 		so->so_onq = &head->so_q;
 		TAILQ_INSERT_HEAD(so->so_onq, so, so_qe);
 		wakeup_one(&head->so_timeo);
-		splx(s);
-		return (error);
+		goto bad;
 	}
 	*retval = tmpfd;
 
