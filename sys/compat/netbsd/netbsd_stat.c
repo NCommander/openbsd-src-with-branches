31e8e1e3dfe47c3087f75b1347f9f9041e1ede3b
art <art>
2002-02-13 19:08:06 +0000

    More FREF/FRELE on relevant places. Now, only sys_mmap and a bunch of ioctl functions in sys/compat are left.

--- sys/compat/netbsd/netbsd_stat.c
+++ sys/compat/netbsd/netbsd_stat.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: netbsd_stat.c,v 1.11 2001/05/15 08:04:31 deraadt Exp $	*/
+/*	$OpenBSD: netbsd_stat.c,v 1.12 2001/10/26 12:03:27 art Exp $	*/
 /*
  * Copyright (c) 1989, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -195,7 +195,9 @@ netbsd_sys___fstat13(p, v, retval)
 	if ((fp = fd_getfile(fdp, fd)) == NULL)
 		return (EBADF);
 
+	FREF(fp);
 	error = (*fp->f_ops->fo_stat)(fp, &sb, p);
+	FRELE(fp);
 	if (error)
 		return (error);
 	openbsd_to_netbsd_stat(&sb, &nsb);
