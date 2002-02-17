efc1125eb931359316c99a495fea41b1221d246b
marc <marc>
2002-02-17 01:10:15 +0000

    skip tests that require root (REGRESSROOTTARGETS) unless the user is root or SUDO is not empty.  OK art@

--- share/mk/bsd.regress.mk
+++ share/mk/bsd.regress.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.regress.mk,v 1.7 2002/01/03 02:20:29 art Exp $
+#	$OpenBSD: bsd.regress.mk,v 1.8 2002/01/03 16:54:38 brad Exp $
 # No man pages for regression tests.
 NOMAN=
 
@@ -36,6 +36,14 @@ REGRESSSKIPSLOW?=no
 
 .if (${REGRESSSKIPSLOW:L} == "yes") && defined(REGRESSSLOWTARGETS)
 REGRESSSKIPTARGETS+=${REGRESSSLOWTARGETS}
+.endif
+
+.if defined(REGRESSROOTTARGETS)
+ROOTUSER!=id -g
+SUDO?=
+. if (${ROOTUSER} != 0) && empty(SUDO)
+REGRESSSKIPTARGETS+=${REGRESSROOTTARGETS}
+. endif
 .endif
 
 REGRESSSKIPTARGETS?=
