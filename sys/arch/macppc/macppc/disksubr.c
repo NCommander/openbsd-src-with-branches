e0779f7c5bc643d7b9bf67549885e0f1d4a618d5
deraadt <deraadt>
2002-02-19 02:46:40 +0000

    correct extended partition botch; gluk, in PR 1449

--- sys/arch/macppc/macppc/disksubr.c
+++ sys/arch/macppc/macppc/disksubr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: disksubr.c,v 1.16 2001/06/24 05:06:13 drahn Exp $	*/
+/*	$OpenBSD: disksubr.c,v 1.1 2001/09/01 15:44:20 drahn Exp $	*/
 /*	$NetBSD: disksubr.c,v 1.21 1996/05/03 19:42:03 christos Exp $	*/
 
 /*
@@ -213,6 +213,8 @@ hfs_done:
 		while (wander && n < 8 && loop < 8) {
 		        loop++;
 			wander = 0;
+			if (part_blkno < extoff)
+				part_blkno = extoff;
 
 			/* read boot record */
 			bp->b_blkno = part_blkno;
@@ -318,8 +320,10 @@ donot:
 				case DOSPTYP_EXTEND:
 				case DOSPTYP_EXTENDL:
 					part_blkno = get_le(&dp2->dp_start) + extoff;
-					if (!extoff)
+					if (!extoff) {
 						extoff = get_le(&dp2->dp_start);
+						part_blkno = 0;
+					}
 					wander = 1;
 					break;
 				default:
