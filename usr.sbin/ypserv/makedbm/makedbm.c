bc2164481d73bcb19cafef597d86c4bf21ad247c
maja <maja>
2002-03-20 14:19:30 +0000

    Improve error message in makedbm. -moj from Kenneth Ingham <ingham@i-pi.com>

--- usr.sbin/ypserv/makedbm/makedbm.c
+++ usr.sbin/ypserv/makedbm/makedbm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: makedbm.c,v 1.15 2001/10/24 14:40:00 deraadt Exp $ */
+/*	$OpenBSD: makedbm.c,v 1.16 2002/02/13 23:05:48 deraadt Exp $ */
 
 /*
  * Copyright (c) 1994-97 Mats O Jansson <moj@stacken.kth.se>
@@ -32,7 +32,7 @@
  */
 
 #ifndef LINT
-static char rcsid[] = "$OpenBSD: makedbm.c,v 1.15 2001/10/24 14:40:00 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: makedbm.c,v 1.16 2002/02/13 23:05:48 deraadt Exp $";
 #endif
 
 #include <stdio.h>
@@ -152,7 +152,8 @@ list_database(database,Uflag)
 		if (Uflag != 0)
 			if (db_hash_list_database(database)) return;
 
-		fprintf(stderr, "%s: can't open database %s\n", __progname, database);
+		fprintf(stderr, "%s: can't open database %s: %s\n", __progname,
+		    database, strerror(errno));
 		exit(1);
 	}
 	
