2e34c28cf779042cd229c168113a59c457773d11
itojun <itojun>
2002-03-23 00:50:59 +0000

    correct arg to bcmp().  need to compare 15 bytes, not 3 bytes.  sync w/kame

--- sys/netinet6/in6.c
+++ sys/netinet6/in6.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: in6.c,v 1.30 2001/12/07 09:16:07 itojun Exp $	*/
+/*	$OpenBSD: in6.c,v 1.31 2002/03/14 01:27:11 millert Exp $	*/
 /*	$KAME: in6.c,v 1.198 2001/07/18 09:12:38 itojun Exp $	*/
 
 /*
@@ -1646,7 +1646,7 @@ struct in6_addr *addr;
 		}
 	}
 
-	if (bcmp(&in6addr_loopback, addr, sizeof(addr) - 1) == 0) {
+	if (bcmp(&in6addr_loopback, addr, sizeof(*addr) - 1) == 0) {
 		if (addr->s6_addr8[15] == 1) /* loopback */
 			return IPV6_ADDR_SCOPE_NODELOCAL;
 		if (addr->s6_addr8[15] == 0) /* unspecified */
