1bed5f9512d5edfb890121608c2568edaac9cbb1
millert <millert>
2002-04-02 03:57:05 +0000

    3Com AirConnect 3CRWE777A is really just a 3CRWE737A in disguise. They use 3CRWE777A because that is the part number for the PCI adapter it comes with but the card can be used w/o it.

--- sys/dev/pcmcia/if_wi_pcmcia.c
+++ sys/dev/pcmcia/if_wi_pcmcia.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: if_wi_pcmcia.c,v 1.19 2002/03/28 16:45:52 provos Exp $ */
+/* $OpenBSD: if_wi_pcmcia.c,v 1.20 2002/03/29 18:54:07 millert Exp $ */
 /* $NetBSD: if_wi_pcmcia.c,v 1.14 2001/11/26 04:34:56 ichiro Exp $ */
 
 /*
@@ -107,6 +107,11 @@ static const struct wi_pcmcia_product {
 	{ PCMCIA_VENDOR_3COM,
 	  PCMCIA_PRODUCT_3COM_3CRWE737A,
 	  PCMCIA_CIS_3COM_3CRWE737A,
+	  "3Com AirConnect Wireless LAN"
+	},
+	{ PCMCIA_VENDOR_3COM,
+	  PCMCIA_PRODUCT_3COM_3CRWE777A,
+	  PCMCIA_CIS_3COM_3CRWE777A,
 	  "3Com AirConnect Wireless LAN"
 	},
 	{ PCMCIA_VENDOR_COREGA,
