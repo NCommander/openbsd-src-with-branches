46c24c4808ebf12ca2578f6df52c11538e8a8e4b
jason <jason>
2002-04-16 21:29:54 +0000

    move prototypes of dc_eeprom_width, dc_read_srom, and dc_parse_21143_srom to dcreg.h and out of the bus specific drivers

--- sys/dev/cardbus/if_dc_cardbus.c
+++ sys/dev/cardbus/if_dc_cardbus.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_dc_cardbus.c,v 1.8 2001/12/22 02:32:48 fgsch Exp $	*/
+/*	$OpenBSD: if_dc_cardbus.c,v 1.9 2002/03/14 01:26:53 millert Exp $	*/
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -56,10 +56,6 @@ void dc_cardbus_attach(struct device *, struct device *,void *);
 int dc_cardbus_detach(struct device *, int);
 
 void dc_cardbus_setup(struct dc_cardbus_softc *csc);
-
-extern void dc_eeprom_width(struct dc_softc *);
-extern void dc_read_srom(struct dc_softc *, int);
-extern void dc_parse_21143_srom(struct dc_softc *);
 
 struct cfattach dc_cardbus_ca = {
 	sizeof(struct dc_cardbus_softc), dc_cardbus_match, dc_cardbus_attach,
