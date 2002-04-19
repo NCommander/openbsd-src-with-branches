4f6572857f28d4c5255cc5f63dff28ed662926ac
deraadt <deraadt>
2002-04-19 15:48:35 +0000

    avoid cranking libc minors for another few weeks

--- lib/libc/gen/assert.c
+++ lib/libc/gen/assert.c
@@ -51,13 +51,3 @@ __assert(file, line, failedexpr)
 	abort();
 	/* NOTREACHED */
 }
-
-void
-__assert2(const char *file, int line, const char *func, const char *failedexpr)
-{
-	(void)fprintf(stderr,
-	    "assertion \"%s\" failed: file \"%s\", line %d, function \"%s\"\n",
-	    failedexpr, file, line, func);
-	abort();
-	/* NOTREACHED */
-}
