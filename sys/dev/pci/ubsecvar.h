2bb76376de00cded7c7a5d929b943d7f066eba4a
jason <jason>
2002-04-26 05:06:03 +0000

    1024 bit max

--- sys/dev/pci/ubsecvar.h
+++ sys/dev/pci/ubsecvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ubsecvar.h,v 1.26 2002/04/08 17:49:42 jason Exp $	*/
+/*	$OpenBSD: ubsecvar.h,v 1.27 2002/04/26 04:24:17 jason Exp $	*/
 
 /*
  * Copyright (c) 2000 Theo de Raadt
@@ -172,4 +172,3 @@ struct ubsec_stats {
 	u_int32_t hst_mcrerr;
 	u_int32_t hst_nodmafree;
 };
-
