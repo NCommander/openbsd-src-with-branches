eafa1391b0bab251eaee6403fc1bc5ca672f5d97
pvalchev <pvalchev>
2002-04-28 20:55:14 +0000

    IEEE 754 floating point completion code, and implementation of the FP_C (Floating Point Control Quadword).
    From ross@NetBSD.  Added a way to disable it with option NO_IEEE,
    which appears on the ramdisks to save space.  This affects only
    programs compiled with -mieee, and what it essentially does is
    enabling infinities and NaNs, instead of generating SIGFPE on
    division by zero, overflow, etc.
    ok art, deraadt

--- sys/arch/alpha/include/pcb.h
+++ sys/arch/alpha/include/pcb.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pcb.h,v 1.4 1997/01/24 19:57:15 niklas Exp $	*/
+/*	$OpenBSD: pcb.h,v 1.5 2002/03/14 01:26:27 millert Exp $	*/
 /*	$NetBSD: pcb.h,v 1.5 1996/11/13 22:21:00 cgd Exp $	*/
 
 /*
@@ -52,6 +52,7 @@ struct pcb {
 	struct fpreg	pcb_fp;			/* FP registers		[SW] */
 	unsigned long	pcb_onfault;		/* for copy faults	[SW] */
 	unsigned long	pcb_accessaddr;		/* for [fs]uswintr	[SW] */
+	struct cpu_info *__volatile pcb_fpcpu;	/* CPU with our FP state[SW] */
 };
 
 /*
