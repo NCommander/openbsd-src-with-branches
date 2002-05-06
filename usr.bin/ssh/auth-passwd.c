96e9f7d83941f0121841fedcee3268f2b65355d8
mouring <mouring>
2002-05-06 23:00:59 +0000

    When options.permit_empty_passwd == 0  ensure you are checking pw->passwd not password.

--- usr.bin/ssh/auth-passwd.c
+++ usr.bin/ssh/auth-passwd.c
@@ -36,7 +36,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: auth-passwd.c,v 1.23 2001/06/26 16:15:23 dugsong Exp $");
+RCSID("$OpenBSD: auth-passwd.c,v 1.24 2002/03/04 12:43:06 markus Exp $");
 
 #include "packet.h"
 #include "log.h"
@@ -61,7 +61,7 @@ auth_password(Authctxt *authctxt, const char *password)
 		return 0;
 	if (pw->pw_uid == 0 && options.permit_root_login != PERMIT_YES)
 		return 0;
-	if (*password == '\0' && options.permit_empty_passwd == 0)
+	if (*pw->pw_passwd == '\0' && options.permit_empty_passwd == 0)
 		return 0;
 #ifdef KRB5
 	if (options.kerberos_authentication == 1) {
