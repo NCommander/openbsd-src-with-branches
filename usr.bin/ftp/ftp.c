216495dac1b322dc4cdd4d5acb559ff1afa5f382
miod <miod>
2002-05-16 21:45:31 +0000

    MFC, requested by millert, diff by itojun: avoid buffer overrun on PASV from malicious server. http://online.securityfocus.com/archive/1/269356/2002-04-22/2002-04-28/0

--- usr.bin/ftp/ftp.c
+++ usr.bin/ftp/ftp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ftp.c,v 1.38 2000/06/30 16:00:14 millert Exp $	*/
+/*	$OpenBSD: ftp.c,v 1.39 2000/10/18 06:53:24 itojun Exp $	*/
 /*	$NetBSD: ftp.c,v 1.27 1997/08/18 10:20:23 lukem Exp $	*/
 
 /*
@@ -67,7 +67,7 @@
 #if 0
 static char sccsid[] = "@(#)ftp.c	8.6 (Berkeley) 10/27/94";
 #else
-static char rcsid[] = "$OpenBSD: ftp.c,v 1.38 2000/06/30 16:00:14 millert Exp $";
+static char rcsid[] = "$OpenBSD: ftp.c,v 1.39 2000/10/18 06:53:24 itojun Exp $";
 #endif
 #endif /* not lint */
 
@@ -423,9 +423,10 @@ getreply(expecteof)
 			if (dig > 4 && pflag == 1 && isdigit(c))
 				pflag = 2;
 			if (pflag == 2) {
-				if (c != '\r' && c != ')')
-					*pt++ = c;
-				else {
+				if (c != '\r' && c != ')') {
+					if (pt < &pasv[sizeof(pasv) - 1])
+						*pt++ = c;
+				} else {
 					*pt = '\0';
 					pflag = 3;
 				}
