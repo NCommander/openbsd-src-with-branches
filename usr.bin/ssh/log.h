9d3ce4d949255ab3ae9ea4096b1fd9d768f353e0
deraadt <deraadt>
2002-05-19 20:54:52 +0000

    extra commas in enum not 100% portable

--- usr.bin/ssh/log.h
+++ usr.bin/ssh/log.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: log.h,v 1.5 2002/02/04 12:15:25 markus Exp $	*/
+/*	$OpenBSD: log.h,v 1.6 2002/02/22 12:20:34 markus Exp $	*/
 
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
@@ -28,7 +28,7 @@ typedef enum {
 	SYSLOG_FACILITY_LOCAL5,
 	SYSLOG_FACILITY_LOCAL6,
 	SYSLOG_FACILITY_LOCAL7,
-	SYSLOG_FACILITY_NOT_SET = -1,
+	SYSLOG_FACILITY_NOT_SET = -1
 }       SyslogFacility;
 
 typedef enum {
@@ -40,7 +40,7 @@ typedef enum {
 	SYSLOG_LEVEL_DEBUG1,
 	SYSLOG_LEVEL_DEBUG2,
 	SYSLOG_LEVEL_DEBUG3,
-	SYSLOG_LEVEL_NOT_SET = -1,
+	SYSLOG_LEVEL_NOT_SET = -1
 }       LogLevel;
 
 void     log_init(char *, LogLevel, SyslogFacility, int);
