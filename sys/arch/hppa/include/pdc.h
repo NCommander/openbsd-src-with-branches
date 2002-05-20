040054d699ceeabc0c593b1a7ee5a363ae6b63d1
mickey <mickey>
2002-05-20 03:27:42 +0000

    minimum pdc stack is 9k, define it to be 16k here

--- sys/arch/hppa/include/pdc.h
+++ sys/arch/hppa/include/pdc.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pdc.h,v 1.18 2002/02/08 21:15:35 miod Exp $	*/
+/*	$OpenBSD: pdc.h,v 1.19 2002/03/14 01:26:32 millert Exp $	*/
 
 /*
  * Copyright (c) 1990 mt Xinu, Inc.  All rights reserved.
@@ -92,6 +92,7 @@
 #define	IODC_MAXIOSIZ	(64 * 1024)	/* maximum buffer size for IODC call */
 
 #define	PDC_ALIGNMENT	__attribute__ ((__aligned__(64)))
+#define	PDC_STACKSIZE	(4*NBPG)
 
 /*
  * The PDC Entry Points and their arguments...
