c603f7a889aa5ab27f9bca19a2c7558c8d523162
mickey <mickey>
2002-05-22 21:34:56 +0000

    add reminder and roundings

---
+++ lib/libm/arch/hppa/s_rint.c
@@ -0,0 +1,17 @@
+/*
+ * Written by Michael Shalayeff. Public Domain
+ */
+
+#if defined(LIBM_SCCS) && !defined(lint)
+static char rcsid[] = "$OpenBSD$";
+#endif
+
+#include "math.h"
+
+double
+__ieee754_rint(double x)
+{
+	__asm__ __volatile__("frnd,dbl %0,%0" : "+f" (x));
+
+	return (x);
+}
