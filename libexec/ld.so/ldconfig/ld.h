77924bfe6fb9adae4d8cb9424b745076dfccedb5
ericj <ericj>
2002-05-24 00:25:42 +0000

    replace some wrapper functions w/ no license w/ some public domain ones. deraadt@ ok

--- libexec/ld.so/ldconfig/ld.h
+++ libexec/ld.so/ldconfig/ld.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: ld.h,v 1.3 2001/01/30 02:39:04 brad Exp $ */
+/* $OpenBSD: ld.h,v 1.4 2002/02/16 21:27:30 millert Exp $ */
 /*
  * Header file to make code compatible with ELF version 
  * ldconfig was taken from the a.out ld.
@@ -7,7 +7,7 @@
 
 extern int	n_search_dirs;
 extern char	**search_dirs;
-char	*xstrdup(char *);
+char	*xstrdup(const char *);
 void	*xmalloc(size_t);
 void	*xrealloc(void *, size_t);
 char	*concat(const char *, const char *, const char *);
