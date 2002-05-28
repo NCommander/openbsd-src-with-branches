c34ba9661c9c7fc6e1d766d2e88a74a6eef7f913
jasoni <jasoni>
2002-05-28 15:44:28 +0000

    Factor out IP fragmentation code into its own function so it can be reused. - ok jason@, dhartmei@

--- sys/netinet/ip_var.h
+++ sys/netinet/ip_var.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_var.h,v 1.21 2002/01/24 22:42:48 provos Exp $	*/
+/*	$OpenBSD: ip_var.h,v 1.22 2002/03/14 01:27:11 millert Exp $	*/
 /*	$NetBSD: ip_var.h,v 1.16 1996/02/13 23:43:20 christos Exp $	*/
 
 /*
@@ -172,6 +172,7 @@ int	 ip_dooptions(struct mbuf *);
 void	 ip_drain(void);
 void	 ip_flush(void);
 void	 ip_forward(struct mbuf *, int);
+int	 ip_fragment(struct mbuf *, struct ifnet *);
 void	 ip_freef(struct ipq *);
 void	 ip_freemoptions(struct ip_moptions *);
 int	 ip_getmoptions(int, struct ip_moptions *, struct mbuf **);
