f8f290525faaac92b508befaed1a849e4379080b
pjanzen <pjanzen>
2002-05-31 05:18:56 +0000

    Don't include cases for characters not in optstring; ericj.  Snuck in on my last commit here.

--- bin/sleep/sleep.c
+++ bin/sleep/sleep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sleep.c,v 1.11 2001/09/06 13:29:08 mpech Exp $	*/
+/*	$OpenBSD: sleep.c,v 1.12 2002/02/16 21:27:07 millert Exp $	*/
 /*	$NetBSD: sleep.c,v 1.8 1995/03/21 09:11:11 cgd Exp $	*/
 
 /*
@@ -44,7 +44,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)sleep.c	8.3 (Berkeley) 4/2/94";
 #else
-static char rcsid[] = "$OpenBSD: sleep.c,v 1.11 2001/09/06 13:29:08 mpech Exp $";
+static char rcsid[] = "$OpenBSD: sleep.c,v 1.12 2002/02/16 21:27:07 millert Exp $";
 #endif
 #endif /* not lint */
 
@@ -80,8 +80,6 @@ main(argc, argv)
 
 	while ((ch = getopt(argc, argv, "")) != -1)
 		switch(ch) {
-		case '?':
-		case 'h':
 		default:
 			usage();
 		}
