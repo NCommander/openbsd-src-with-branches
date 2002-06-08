a6be8b38cbecd21c8a0626cd820d56b048ece702
jasoni <jasoni>
2002-06-08 21:53:53 +0000

    Break up icmp_error() so we can have the icmp error mbuf returned instead of it being sent to ip_output().

--- sys/netinet/ip_icmp.h
+++ sys/netinet/ip_icmp.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_icmp.h,v 1.15 2002/01/12 00:51:59 ericj Exp $	*/
+/*	$OpenBSD: ip_icmp.h,v 1.16 2002/03/14 01:27:11 millert Exp $	*/
 /*	$NetBSD: ip_icmp.h,v 1.10 1996/02/13 23:42:28 christos Exp $	*/
 
 /*
@@ -209,6 +209,8 @@ struct icmp {
 	(type) == ICMP_MASKREQ || (type) == ICMP_MASKREPLY)
 
 #ifdef _KERNEL
+struct mbuf *
+	icmp_do_error(struct mbuf *, int, int, n_long, struct ifnet *);
 void	icmp_error(struct mbuf *, int, int, n_long, struct ifnet *);
 void	icmp_input(struct mbuf *, ...);
 void	icmp_init(void);
