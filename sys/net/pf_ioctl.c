6b55a023e9fc6c93c3785ddf819938e23571b9a6
dhartmei <dhartmei>
2002-06-10 18:52:44 +0000

    #include <sys/timeout.h>, from Chris Kuethe

--- sys/net/pf_ioctl.c
+++ sys/net/pf_ioctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf_ioctl.c,v 1.2 2002/06/09 20:20:58 dhartmei Exp $ */
+/*	$OpenBSD: pf_ioctl.c,v 1.3 2002/06/10 17:05:11 dhartmei Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -43,6 +43,7 @@
 #include <sys/socketvar.h>
 #include <sys/kernel.h>
 #include <sys/time.h>
+#include <sys/timeout.h>
 #include <sys/pool.h>
 
 #include <net/if.h>
