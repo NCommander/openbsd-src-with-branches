badd08c6d3fd242da6a2caa991a05a7f55458660
jsyn <jsyn>
2002-06-11 05:22:40 +0000

    no more err(3) newlines in games; ok pjanzen@

--- games/trek/damage.c
+++ games/trek/damage.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: damage.c,v 1.2 1998/08/19 07:41:21 pjanzen Exp $	*/
+/*	$OpenBSD: damage.c,v 1.3 2002/05/31 04:21:30 pjanzen Exp $	*/
 /*	$NetBSD: damage.c,v 1.3 1995/04/22 10:58:40 cgd Exp $	*/
 
 /*
@@ -38,7 +38,7 @@
 #if 0
 static char sccsid[] = "@(#)damage.c	8.1 (Berkeley) 5/31/93";
 #else
-static char rcsid[] = "$OpenBSD: damage.c,v 1.2 1998/08/19 07:41:21 pjanzen Exp $";
+static char rcsid[] = "$OpenBSD: damage.c,v 1.3 2002/05/31 04:21:30 pjanzen Exp $";
 #endif
 #endif /* not lint */
 
@@ -97,5 +97,5 @@ damage(dev1, dam)
 		reschedule(e, e->date - Now.date + dam);
 		return;
 	}
-	errx(1, "Cannot find old damages %d\n", dev);
+	errx(1, "Cannot find old damages %d", dev);
 }
