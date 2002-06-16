271c830de180743ef32553173f6b662eaabcba64
mickey <mickey>
2002-06-16 01:36:22 +0000

    implement kqueue for audio; tested on wiggy and niels@ ok

--- sys/sys/conf.h
+++ sys/sys/conf.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: conf.h,v 1.56 2002/06/09 22:03:44 niklas Exp $	*/
+/*	$OpenBSD: conf.h,v 1.57 2002/06/11 05:15:15 miod Exp $	*/
 /*	$NetBSD: conf.h,v 1.33 1996/05/03 20:03:32 christos Exp $	*/
 
 /*-
@@ -343,7 +343,7 @@ extern struct cdevsw cdevsw[];
 	dev_init(c,n,open), dev_init(c,n,close), dev_init(c,n,read), \
 	dev_init(c,n,write), dev_init(c,n,ioctl), \
 	(dev_type_stop((*))) enodev, 0, dev_init(c,n,select), \
-	dev_init(c,n,mmap) }
+	dev_init(c,n,mmap), D_KQFILTER, dev_init(c,n,kqfilter) }
 
 /* open, close, read, write, ioctl, poll */
 #define cdev_midi_init(c,n) { \
