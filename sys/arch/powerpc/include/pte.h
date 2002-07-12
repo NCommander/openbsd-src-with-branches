ac70646feb46492206f9afcba42f0a1d25abb7b1
drahn <drahn>
2002-07-12 20:28:55 +0000

    Cleanup: use less _t typedefs, use the structure itself.
    pmap_t is the exception, it is required by the MI code so pmap_t will
    be used instead of using 'struct pmap *' in the code.  (consistency)

--- sys/arch/powerpc/include/pte.h
+++ sys/arch/powerpc/include/pte.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pte.h,v 1.4 2002/03/13 18:27:36 drahn Exp $	*/
+/*	$OpenBSD: pte.h,v 1.6 2002/07/12 20:26:10 drahn Exp $	*/
 /*	$NetBSD: pte.h,v 1.1 1996/09/30 16:34:32 ws Exp $	*/
 
 /*-
@@ -79,7 +79,7 @@ typedef	struct pte pte_t;
 
 #ifndef	_LOCORE
 #ifdef	_KERNEL
-extern pte_t *ptable;
+extern struct pte *ptable;
 extern int ptab_cnt;
 #endif	/* _KERNEL */
 #endif	/* _LOCORE */
