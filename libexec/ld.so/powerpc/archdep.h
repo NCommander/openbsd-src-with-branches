5df8bedf240284326edd22d0f3d631a8bd329c3c
drahn <drahn>
2002-07-12 20:18:30 +0000

    Change ld.so search order/method to match the a.out ld.so.
    run destructors on dlclose()
    
    Move more symbols into _dl_ private space, so that the proper (libc)
    version of the function will be used.
    
    Add readdir() functionality to perform the proper library searching.
    
    Support DL_PRELOAD
    
    Do not relocate symbols if ld.so is being traced (and will exit).
    
    Misc lint cleanup.
    
    ok art@

--- libexec/ld.so/powerpc/archdep.h
+++ libexec/ld.so/powerpc/archdep.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: archdep.h,v 1.6 2002/05/24 03:44:38 deraadt Exp $ */
+/*	$OpenBSD: archdep.h,v 1.7 2002/05/24 04:17:01 deraadt Exp $ */
 
 /*
  * Copyright (c) 1998 Per Fogelstrom, Opsycon AB
@@ -57,6 +57,7 @@
  *	The following functions are declared inline so they can
  *	be used before bootstrap linking has been finished.
  */
+
 static inline void
 _dl_dcbf(Elf32_Addr *addr)
 {
