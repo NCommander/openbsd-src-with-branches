6de258d25e7fc0031b36558dd4e1e26d19f9b317
deraadt <deraadt>
2002-08-03 21:49:24 +0000

    xdr_pri_free() ansi; pvalchev ok

--- usr.sbin/amd/amd/host_ops.c
+++ usr.sbin/amd/amd/host_ops.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: host_ops.c,v 1.7 2002/07/18 02:03:00 deraadt Exp $	*/
+/*	$OpenBSD: host_ops.c,v 1.8 2002/08/03 08:29:31 pvalchev Exp $	*/
 
 /*
  * Copyright (c) 1990 Jan-Simon Pendry
@@ -120,7 +120,7 @@ host_init(mntfs *mf)
 #ifndef HOST_EXEC
 
 static bool_t
-xdr_pri_free(xdrproc_t xdr_args, caddr_t args_ptr)
+xdr_pri_free(xdrproc_t xdr_args, void *args_ptr)
 {
 	XDR xdr;
 
