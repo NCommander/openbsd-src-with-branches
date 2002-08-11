009fa8896975b3b88ed3f0a38385fb8129fc3b86
art <art>
2002-08-11 23:11:22 +0000

    ELF support in sparc bootblocks. Loadfile is from alpha, but heaviliy hacked here. The build is done by building elf versions of boot and bootxx, then merging the .rodata and .text sections into .text with a horrible hack and then using objcopy to convert that into a.out.
    Maybe someone will want to fix installboot to deal with ELF instead, but
    I won't be that someone in the nearest future.

--- sys/arch/sparc/stand/bootxx/bootxx.c
+++ sys/arch/sparc/stand/bootxx/bootxx.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bootxx.c,v 1.1 1997/09/17 10:46:16 downsj Exp $	*/
+/*	$OpenBSD: bootxx.c,v 1.2 2002/03/14 01:26:44 millert Exp $	*/
 /*	$NetBSD: bootxx.c,v 1.2 1997/09/14 19:28:17 pk Exp $	*/
 
 /*
@@ -45,7 +45,7 @@ int netif_debug;
 /*
  * Boot device is derived from ROM provided information.
  */
-const char		progname[] = "bootxx";
+char		progname[] = "bootxx";
 struct open_file	io;
 
 /*
