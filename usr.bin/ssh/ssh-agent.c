6949a3672ad9711e325d3ea19cbb8f7ee75124e1
markus <markus>
2002-08-12 10:46:35 +0000

    make ssh-agent setgid, disallow ptrace.

--- usr.bin/ssh/ssh-agent.c
+++ usr.bin/ssh/ssh-agent.c
@@ -35,7 +35,7 @@
 
 #include "includes.h"
 #include <sys/queue.h>
-RCSID("$OpenBSD: ssh-agent.c,v 1.97 2002/06/24 14:55:38 markus Exp $");
+RCSID("$OpenBSD: ssh-agent.c,v 1.98 2002/07/21 18:07:45 stevesk Exp $");
 
 #include <openssl/evp.h>
 #include <openssl/md5.h>
@@ -933,6 +933,10 @@ main(int ac, char **av)
 	extern char *optarg;
 	pid_t pid;
 	char pidstrbuf[1 + 3 * sizeof pid];
+
+	/* drop */
+	setegid(getgid());
+	setgid(getgid());
 
 	SSLeay_add_all_algorithms();
 
