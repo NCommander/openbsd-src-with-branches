98e4ee02d4b22e757fc86117ea4386115b400d12
cvs-fast-export <cvs-fast-export>
2002-08-21 12:53:35 +0000

    Synthetic commit for incomplete tag strip

--- sys/xfs/xfs_global.h
+++
@@ -1,9 +0,0 @@
-#ifndef __XFS_GLOBAL_H
-#define __XFS_GLOBAL_H
-
-#ifndef RCSID
-#define RCSID(msg) \
-static /**/const char *const rcsid[] = { (char *)rcsid, "\100(#)" msg }
-#endif /* RCSID */
-
-#endif  /* __XFS_GLOBAL_H */
