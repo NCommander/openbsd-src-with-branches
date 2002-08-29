172e1f19b110dd9d06a674e2ee0b66cc9bfeb8c4
stevesk <stevesk>
2002-08-29 15:57:25 +0000

    pass addrlen with sockaddr *; from Hajimu UMEMOTO <ume@FreeBSD.org> NOTE: there are also p-specific parts to this patch. ok markus@

--- usr.bin/ssh/sshlogin.c
+++ usr.bin/ssh/sshlogin.c
@@ -39,7 +39,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: sshlogin.c,v 1.3 2001/12/19 07:18:56 deraadt Exp $");
+RCSID("$OpenBSD: sshlogin.c,v 1.4 2002/06/23 03:30:17 deraadt Exp $");
 
 #include <util.h>
 #include <utmp.h>
@@ -84,7 +84,7 @@ get_last_login_time(uid_t uid, const char *logname,
  */
 void
 record_login(pid_t pid, const char *ttyname, const char *user, uid_t uid,
-    const char *host, struct sockaddr * addr)
+    const char *host, struct sockaddr * addr, socklen_t addrlen)
 {
 	int fd;
 	struct lastlog ll;
