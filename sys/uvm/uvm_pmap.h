cda1eaa6b7907d07b353852ce45486be74878cef
art <art>
2002-09-10 18:29:44 +0000

    Change the pmap_zero_page and pmap_copy_page API to take the struct vm_page * instead of the pa. Most callers already had it handy and those who didn't only called it for managed pages and were outside time-critical code.
    This will allow us to make those functions clean and fast on sparc and
    sparc64 letting us to avoid unnecessary cache flushes.
    
    deraadt@ miod@ drahn@ ok.

--- sys/uvm/uvm_pmap.h
+++ sys/uvm/uvm_pmap.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_pmap.h,v 1.13 2002/03/18 07:42:45 hugh Exp $	*/
+/*	$OpenBSD: uvm_pmap.h,v 1.14 2002/03/18 07:48:11 hugh Exp $	*/
 /*	$NetBSD: uvm_pmap.h,v 1.1 2000/06/27 09:00:14 mrg Exp $	*/
 
 /* 
@@ -125,7 +125,7 @@ void		 pmap_collect(pmap_t);
 void		 pmap_copy(pmap_t, pmap_t, vaddr_t, vsize_t, vaddr_t);
 #endif
 #if !defined(pmap_copy_page)
-void		 pmap_copy_page(paddr_t, paddr_t);
+void		 pmap_copy_page(struct vm_page *, struct vm_page *);
 #endif
 struct pmap 	 *pmap_create(void);
 void		 pmap_destroy(pmap_t);
@@ -164,7 +164,7 @@ void		 pmap_remove(pmap_t, vaddr_t, vaddr_t);
 void		 pmap_update(void);
 #endif
 #if !defined(pmap_zero_page)
-void		 pmap_zero_page(paddr_t);
+void		 pmap_zero_page(struct vm_page *);
 #endif
 
 void		 pmap_virtual_space(vaddr_t *, vaddr_t *);
