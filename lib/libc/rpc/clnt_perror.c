12afe28e3f7b87311dd1ae898ae4f1596c571c89
deraadt <deraadt>
2002-09-10 05:39:07 +0000

    remove extra output of \n in clnt_perror()

--- lib/libc/rpc/clnt_perror.c
+++ lib/libc/rpc/clnt_perror.c
@@ -28,7 +28,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char *rcsid = "$OpenBSD: clnt_perror.c,v 1.13 2001/09/15 13:51:00 deraadt Exp $";
+static char *rcsid = "$OpenBSD: clnt_perror.c,v 1.14 2002/05/29 18:53:14 deraadt Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 /*
@@ -152,7 +152,7 @@ clnt_perror(rpch, s)
 	CLIENT *rpch;
 	char *s;
 {
-	(void) fprintf(stderr, "%s\n", clnt_sperror(rpch, s));
+	(void) fprintf(stderr, "%s", clnt_sperror(rpch, s));
 }
 
 static const char *const rpc_errlist[] = {
