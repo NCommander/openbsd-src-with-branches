ba1473b401f37db561a0dd67fc973d68790694da
itojun <itojun>
2002-09-17 04:57:53 +0000

    daemon should not change the directory.  from provos

--- bin/systrace/systrace.c
+++ bin/systrace/systrace.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: systrace.c,v 1.32 2002/08/05 23:27:53 provos Exp $	*/
+/*	$OpenBSD: systrace.c,v 1.33 2002/09/16 04:34:46 itojun Exp $	*/
 /*
  * Copyright 2002 Niels Provos <provos@citi.umich.edu>
  * All rights reserved.
@@ -512,7 +512,7 @@ main(int argc, char **argv)
 			err(1, "attachpid");
 
 		if (background) {
-			if (daemon(0, 1) == -1)
+			if (daemon(1, 1) == -1)
 				err(1, "daemon");
 		}
 	}
