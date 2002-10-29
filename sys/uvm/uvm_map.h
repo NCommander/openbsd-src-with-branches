7b57e5308933398ce29696e687711632b1d7ffce
art <art>
2002-10-29 18:30:21 +0000

    Since memory deallocation can't fail, remove the error return from uvm_unmap, uvm_deallocate and a few other functions. Simplifies some code and reduces diff to the UBC branch.

--- sys/uvm/uvm_map.h
+++ sys/uvm/uvm_map.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_map.h,v 1.26 2002/03/14 01:27:18 millert Exp $	*/
+/*	$OpenBSD: uvm_map.h,v 1.27 2002/06/11 06:38:01 art Exp $	*/
 /*	$NetBSD: uvm_map.h,v 1.24 2001/02/18 21:19:08 chs Exp $	*/
 
 /* 
@@ -348,9 +348,9 @@ int		uvm_map_reserve(vm_map_t, vsize_t, vaddr_t, vsize_t,
 void		uvm_map_setup(vm_map_t, vaddr_t, vaddr_t, int);
 int		uvm_map_submap(vm_map_t, vaddr_t, vaddr_t, vm_map_t);
 MAP_INLINE
-int		uvm_unmap(vm_map_t, vaddr_t, vaddr_t);
+void		uvm_unmap(vm_map_t, vaddr_t, vaddr_t);
 void		uvm_unmap_detach(vm_map_entry_t,int);
-int		uvm_unmap_remove(vm_map_t, vaddr_t, vaddr_t,
+void		uvm_unmap_remove(vm_map_t, vaddr_t, vaddr_t,
 				      vm_map_entry_t *);
 
 #endif /* _KERNEL */
