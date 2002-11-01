fd216dacee7cdd065a3122b4acf3244b11444292
mickey <mickey>
2002-11-01 00:05:45 +0000

    md part for hppa should look like this, at the first look

--- lib/libc_r/arch/hppa/uthread_machdep.h
+++ lib/libc_r/arch/hppa/uthread_machdep.h
@@ -1,5 +1,7 @@
-/*	$OpenBSD: uthread_machdep.h,v 1.4 2000/10/04 05:55:33 d Exp $	*/
+/*	$OpenBSD: uthread_machdep.h,v 1.5 2002/02/08 16:45:17 mickey Exp $	*/
 
 struct _machdep_state {
 	u_long	sp;
+	u_long	fp;
+	u_int64_t fpregs[32];
 };
