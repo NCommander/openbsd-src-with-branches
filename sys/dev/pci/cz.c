70607fe8e730bde7be672a82bab699d0554859c0
jason <jason>
2002-11-19 18:40:17 +0000

    Add a simplistic table driven lookup routine and use it where appropriate.

--- sys/dev/pci/cz.c
+++ sys/dev/pci/cz.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cz.c,v 1.4 2001/09/04 04:01:42 nate Exp $ */
+/*	$OpenBSD: cz.c,v 1.5 2002/01/30 20:45:34 nordin Exp $ */
 /*	$NetBSD: cz.c,v 1.15 2001/01/20 19:10:36 thorpej Exp $	*/
 
 /*-
@@ -275,13 +275,9 @@ cz_match(parent, match, aux)
 {
 	struct pci_attach_args *pa = aux;
 
-	if (PCI_VENDOR(pa->pa_id) == PCI_VENDOR_CYCLADES) {
-		switch (PCI_PRODUCT(pa->pa_id)) {
-		case PCI_PRODUCT_CYCLADES_CYCLOMZ_2:
-			return (1);
-		}
-	}
-
+	if (PCI_VENDOR(pa->pa_id) == PCI_VENDOR_CYCLADES &&
+	    PCI_PRODUCT(pa->pa_id) == PCI_PRODUCT_CYCLADES_CYCLOMZ_2)
+		return (1);
 	return (0);
 }
 
--- sys/dev/pci/if_fpa.c
+++ sys/dev/pci/if_fpa.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_fpa.c,v 1.19 2002/06/02 22:50:00 deraadt Exp $	*/
+/*	$OpenBSD: if_fpa.c,v 1.20 2002/06/09 03:14:18 todd Exp $	*/
 /*	$NetBSD: if_fpa.c,v 1.15 1996/10/21 22:56:40 thorpej Exp $	*/
 
 /*-
@@ -94,11 +94,10 @@ pdq_pci_match(parent, match, aux)
 {
 	struct pci_attach_args *pa = (struct pci_attach_args *)aux;
 
-	if (PCI_VENDOR(pa->pa_id) != PCI_VENDOR_DEC)
-		return (0);
-	if (PCI_PRODUCT(pa->pa_id) != PCI_PRODUCT_DEC_DEFPA)
-		return (0);
-	return (1);
+	if (PCI_VENDOR(pa->pa_id) == PCI_VENDOR_DEC &&
+	    PCI_PRODUCT(pa->pa_id) == PCI_PRODUCT_DEC_DEFPA)
+		return (1);
+	return (0);
 }
 
 void
