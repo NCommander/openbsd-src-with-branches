056c886d0c31d59320276251fa11b4eb5faa959a
ho <ho>
2002-11-21 09:40:34 +0000

    hmac_init and prf_alloc should use unsigned lengths. Problem pointed out by Patrick Latifi.

--- sbin/isakmpd/hash.c
+++ sbin/isakmpd/hash.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: hash.c,v 1.9 2002/06/09 08:13:06 todd Exp $	*/
+/*	$OpenBSD: hash.c,v 1.10 2002/08/23 18:17:17 ho Exp $	*/
 /*	$EOM: hash.c,v 1.10 1999/04/17 23:20:34 niklas Exp $	*/
 
 /*
@@ -50,7 +50,7 @@
 #include "hash.h"
 #include "log.h"
 
-void hmac_init (struct hash *, unsigned char *, int);
+void hmac_init (struct hash *, unsigned char *, unsigned int);
 void hmac_final (unsigned char *, struct hash *);
 
 /* Temporary hash contexts.  */
@@ -100,9 +100,9 @@ hash_get (enum hashes hashtype)
  */
 
 void
-hmac_init (struct hash *hash, unsigned char *okey, int len)
+hmac_init (struct hash *hash, unsigned char *okey, unsigned int len)
 {
-  int i, blocklen = HMAC_BLOCKLEN;
+  unsigned int i, blocklen = HMAC_BLOCKLEN;
   unsigned char key[HMAC_BLOCKLEN];
 
   memset (key, 0, blocklen);
--- sbin/isakmpd/hash.h
+++ sbin/isakmpd/hash.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: hash.h,v 1.3 1998/11/17 11:10:11 niklas Exp $	*/
+/*	$OpenBSD: hash.h,v 1.4 2002/09/11 09:50:43 ho Exp $	*/
 /*	$EOM: hash.h,v 1.6 1998/07/25 22:04:36 niklas Exp $	*/
 
 /*
@@ -59,7 +59,7 @@ struct hash {
   void (*Init) (void *);
   void (*Update) (void *, unsigned char *, unsigned int);
   void (*Final) (unsigned char *, void *);
-  void (*HMACInit) (struct hash *, unsigned char *, int);
+  void (*HMACInit) (struct hash *, unsigned char *, unsigned int);
   void (*HMACFinal) (unsigned char *, struct hash *);
 };
 
@@ -70,6 +70,6 @@ struct hash {
 #define HMAC_BLOCKLEN	64
 
 extern struct hash *hash_get (enum hashes);
-extern void hmac_init (struct hash *, unsigned char *, int);
+extern void hmac_init (struct hash *, unsigned char *, unsigned int);
 
 #endif /* _HASH_H_ */
