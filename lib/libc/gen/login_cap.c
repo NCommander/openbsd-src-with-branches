a58dbadcd82f53c5260e53f2c325ed21c4d1365c
deraadt <deraadt>
2002-11-22 19:47:03 +0000

    more snprintf; ok millert

--- lib/libc/gen/login_cap.c
+++ lib/libc/gen/login_cap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: login_cap.c,v 1.12 2002/06/27 10:21:35 deraadt Exp $	*/
+/*	$OpenBSD: login_cap.c,v 1.13 2002/10/09 20:36:19 millert Exp $	*/
 
 /*-
  * Copyright (c) 1995,1997 Berkeley Software Design, Inc. All rights reserved.
@@ -523,8 +523,8 @@ gsetrl(lc, what, name, type)
 	if (lc->lc_cap == NULL)
 		return (0);
 
-	sprintf(name_cur, "%s-cur", name);
-	sprintf(name_max, "%s-max", name);
+	snprintf(name_cur, sizeof name_cur, "%s-cur", name);
+	snprintf(name_max, sizeof name_max, "%s-max", name);
 
 	if (getrlimit(what, &r)) {
 		syslog(LOG_ERR, "getting resource limit: %m");
