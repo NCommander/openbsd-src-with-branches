4dd76a897d8324724698a5eb92cada6eb9d4a8f5
mickey <mickey>
2002-11-27 13:31:09 +0000

    fix an underflow in socket timeout calculations. (see http://www.freebsd.org/cgi/query-pr.cgi?pr=kern/32827). itojun@ ok

--- sys/kern/uipc_socket.c
+++ sys/kern/uipc_socket.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uipc_socket.c,v 1.44 2002/08/08 17:07:32 provos Exp $	*/
+/*	$OpenBSD: uipc_socket.c,v 1.46 2002/08/08 19:18:12 provos Exp $	*/
 /*	$NetBSD: uipc_socket.c,v 1.21 1996/02/04 02:17:52 christos Exp $	*/
 
 /*
@@ -1084,6 +1084,8 @@ sosetopt(so, level, optname, m0)
 				goto bad;
 			}
 			val = tv->tv_sec * hz + tv->tv_usec / tick;
+			if (val == 0 && tv->tv_usec != 0)
+				val = 1;
 
 			switch (optname) {
 
