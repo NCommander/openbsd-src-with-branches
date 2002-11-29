f592b19882d9dd5804138835123df0794aacf0b5
miod <miod>
2002-11-29 01:00:51 +0000

    Upon detection of an international Sun keyboard with a recognized layout, attach it with the appropriate keymap table, rather than the default KB_US table.
    If this is not the expected behaviour, users can still revert to US layout
    via "kbd us" or "wsconsctl -w keyboard.encoding=us".
    
    As the installation media uses the prom console, which will honor the
    international keyboard layout, this will definitely help users with
    international keyboards and fancy characters in their passwords...
    
    Note that there is still some Sun keyboard tables missing at the moment.

--- sys/dev/sun/sunkbdvar.h
+++ sys/dev/sun/sunkbdvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sunkbdvar.h,v 1.3 2002/05/29 20:43:43 maja Exp $	*/
+/*	$OpenBSD: sunkbdvar.h,v 1.4 2002/08/12 10:43:56 miod Exp $	*/
 
 /*
  * Copyright (c) 2002 Jason L. Wright (jason@thought.net)
@@ -47,3 +47,6 @@ extern struct wskbd_mapdata sunkbd5_keymapdata;
  * here's a way to distinct types 5 and 6 from type 4.
  */
 #define ISTYPE5(layout) ((layout) > 0x20)
+
+#define	MAXSUNLAYOUT	0x040
+extern const int sunkbd_layouts[MAXSUNLAYOUT];
