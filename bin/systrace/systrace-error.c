10791fba6965e69de8369a8e9101a24f7e41047e
fgsch <fgsch>
2002-12-05 19:39:27 +0000

    spelling; niels ok.

--- bin/systrace/systrace-error.c
+++ bin/systrace/systrace-error.c
@@ -3,12 +3,12 @@
  * Copyright 2002 Niels Provos <provos@citi.umich.edu>
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or withou
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 1. Redistributions of source code must retain the above copyrigh
+ * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyrigh
+ * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
