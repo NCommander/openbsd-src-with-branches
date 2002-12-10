84c472354511f04542aacd4b2a0b35715a189b42
miod <miod>
2002-12-10 23:43:48 +0000

    No need to hardclock IPL_STATCLOCK value anymore.

--- sys/arch/sparc/sparc/intr.c
+++ sys/arch/sparc/sparc/intr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: intr.c,v 1.21 2002/05/22 01:01:19 art Exp $ */
+/*	$OpenBSD: intr.c,v 1.22 2002/08/12 10:44:04 miod Exp $ */
 /*	$NetBSD: intr.c,v 1.20 1997/07/29 09:42:03 fair Exp $ */
 
 /*
@@ -105,7 +105,7 @@ strayintr(fp)
 }
 
 static struct intrhand level10 = { clockintr, NULL, (IPL_CLOCK << 8) };
-static struct intrhand level14 = { statintr, NULL, (14 << 8) /* XXX - IPL_STATCLOCK */ };
+static struct intrhand level14 = { statintr, NULL, (IPL_STATCLOCK << 8) };
 union sir sir;
 /*
  * Level 1 software interrupt (could also be Sbus level 1 interrupt).
