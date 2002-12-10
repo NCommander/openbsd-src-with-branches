3849ef31ee7d02b8ebaf332e0eb794096b19fa70
fgsch <fgsch>
2002-12-10 08:00:16 +0000

    fix setsockopt SO_REUSEADDR under linux emul; from NetBSD. thanks to jpmp@fibertel.com.ar for the testbed.  ish@ ok

--- sys/compat/linux/linux_socket.c
+++ sys/compat/linux/linux_socket.c
@@ -793,7 +793,14 @@ linux_to_bsd_so_sockopt(lopt)
 	case LINUX_SO_DEBUG:
 		return SO_DEBUG;
 	case LINUX_SO_REUSEADDR:
-		return SO_REUSEADDR;
+		/*
+		 * Linux does not implement SO_REUSEPORT, but allows reuse
+		 * of a host:port pair through SO_REUSEADDR even if the
+		 * address is not a multicast-address.  Effectively, this
+		 * means that we should use SO_REUSEPORT to allow Linux
+		 * applications to not exit with EADDRINUSE.
+		 */
+		return SO_REUSEPORT;
 	case LINUX_SO_TYPE:
 		return SO_TYPE;
 	case LINUX_SO_ERROR:
