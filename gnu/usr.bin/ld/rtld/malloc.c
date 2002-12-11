d58cd5df69b6b36c40c2e84fbfecbd933005ab22
millert <millert>
2002-12-11 23:24:39 +0000

    o Check mmap() return value against MAP_FAILED, not -1. o Prevent close() from clobbering errno. Patch from Peter Werner; closes PR 2995

--- gnu/usr.bin/ld/rtld/malloc.c
+++ gnu/usr.bin/ld/rtld/malloc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: malloc.c,v 1.6 2002/09/07 01:25:34 marc Exp $	*/
+/*	$OpenBSD: malloc.c,v 1.7 2002/11/29 22:31:41 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1983 Regents of the University of California.
@@ -35,7 +35,7 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 /*static char *sccsid = "from: @(#)malloc.c	5.11 (Berkeley) 2/23/91";*/
-static char *rcsid = "$OpenBSD: malloc.c,v 1.6 2002/09/07 01:25:34 marc Exp $";
+static char *rcsid = "$OpenBSD: malloc.c,v 1.7 2002/11/29 22:31:41 deraadt Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 /*
@@ -463,7 +463,7 @@ morepages(int n)
 
 	if ((pagepool_start = mmap(0, n * pagesz,
 			PROT_READ|PROT_WRITE,
-			MAP_ANON|MAP_COPY, fd, 0)) == (caddr_t)-1) {
+			MAP_ANON|MAP_COPY, fd, 0)) == (caddr_t)MAP_FAILED) {
 		xprintf("Cannot map anonymous memory");
 		return 0;
 	}
