43d9df85e1268fc3a09f97a57bc97650b12dc6fc
henning <henning>
2002-12-13 21:48:31 +0000

    add pqueue and pqid to pf_rule. this allows for a second queue on pf_rule. assign packets with tos 0x10 (lowdelay) to this one. if the second queue isn't specified set pqid = qid
    idea dhartmei@
    ok dhartmei@ frantzen@ deraadt@

--- sys/net/pfvar.h
+++ sys/net/pfvar.h
@@ -293,6 +293,7 @@ struct pf_rule {
 #define PF_QNAME_SIZE		 16
 	char			 ifname[IFNAMSIZ];
 	char			 qname[PF_QNAME_SIZE];
+	char			 pqname[PF_QNAME_SIZE];
 #define	PF_ANCHOR_NAME_SIZE	 16
 	char			 anchorname[PF_ANCHOR_NAME_SIZE];
 	TAILQ_ENTRY(pf_rule)	 entries;
@@ -308,6 +309,7 @@ struct pf_rule {
 	u_int32_t		 states;
 	u_int32_t		 max_states;
 	u_int32_t		 qid;
+	u_int32_t		 pqid;
 	u_int32_t		 rt_listid;
 
 	u_int16_t		 nr;
