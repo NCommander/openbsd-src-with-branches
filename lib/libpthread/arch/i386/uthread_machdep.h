a6589e22d19a318ac4c681e44ba23fa1709158dd
marc <marc>
2003-01-24 20:58:23 +0000

    save i386 fpu state

--- lib/libpthread/arch/i386/uthread_machdep.h
+++ lib/libpthread/arch/i386/uthread_machdep.h
@@ -1,10 +1,10 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: uthread_machdep.h,v 1.7 2000/10/04 05:55:34 d Exp $	*/
 /* David Leonard, <d@csee.uq.edu.au>. Public domain. */
 
-#include <machine/reg.h>
+#include <machine/npx.h>
 
 struct _machdep_state {
 	int		esp;
-	struct fpreg	fpreg;
+	struct save87	fpreg;
 };
 
