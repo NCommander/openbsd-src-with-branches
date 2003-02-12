e084c2cc0b8f33fd76bf1bcb2489fd60ab70ef9b
deraadt <deraadt>
2003-02-12 23:07:28 +0000

    move to 3.3-beta

--- share/mk/sys.mk
+++ share/mk/sys.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: sys.mk,v 1.31 2003/01/14 22:41:38 mickey Exp $
+#	$OpenBSD: sys.mk,v 1.32 2003/02/12 04:45:19 mickey Exp $
 #	$NetBSD: sys.mk,v 1.27 1996/04/10 05:47:19 mycroft Exp $
 #	@(#)sys.mk	5.11 (Berkeley) 3/13/91
 
@@ -10,7 +10,7 @@
 
 unix=		We run OpenBSD.
 OSMAJOR=	3
-OSMINOR=	2
+OSMINOR=	3
 OSREV=		$(OSMAJOR).$(OSMINOR)
 OSrev=		$(OSMAJOR)$(OSMINOR)
 
