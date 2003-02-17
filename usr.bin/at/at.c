cbb2d29a69af32317b1b21912a9d8eaafff86900
millert <millert>
2003-02-17 21:47:19 +0000

    Missing #include <limits.h>

--- usr.bin/at/at.c
+++ usr.bin/at/at.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: at.c,v 1.31 2002/11/20 19:57:19 millert Exp $	*/
+/*	$OpenBSD: at.c,v 1.32 2003/01/02 15:48:40 mpech Exp $	*/
 /*	$NetBSD: at.c,v 1.4 1995/03/25 18:13:31 glass Exp $	*/
 
 /*
@@ -43,6 +43,7 @@
 #include <err.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <limits.h>
 #include <locale.h>
 #include <pwd.h>
 #include <signal.h>
@@ -73,7 +74,7 @@
 #define TIMESIZE 50		/* Size of buffer passed to strftime() */
 
 #ifndef lint
-static const char rcsid[] = "$OpenBSD: at.c,v 1.31 2002/11/20 19:57:19 millert Exp $";
+static const char rcsid[] = "$OpenBSD: at.c,v 1.32 2003/01/02 15:48:40 mpech Exp $";
 #endif
 
 /* Variables to remove from the job's environment. */
