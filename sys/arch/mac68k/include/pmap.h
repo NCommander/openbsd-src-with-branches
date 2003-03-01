23990e89038538934f54d7ae0fbfd47f4d565de4
miod <miod>
2003-03-01 00:28:48 +0000

    - only declare and provide pmap_map() for mac68k, until it bites the dust.   Other m68k-based arches do not need it. - do not wait to allocate struct pv_page. Either pmap_enter() has been   invoked with PMAP_CANFAIL and can live with this, or we are close to   fandagoland anyways.

--- sys/arch/mac68k/include/pmap.h
+++ sys/arch/mac68k/include/pmap.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pmap.h,v 1.16 2002/01/10 22:25:40 miod Exp $	*/
+/*	$OpenBSD: pmap.h,v 1.17 2002/03/14 01:26:35 millert Exp $	*/
 
 #ifndef	_MAC68K_PMAP_H_
 #define	_MAC68K_PMAP_H_
@@ -7,7 +7,8 @@
 
 #ifdef	_KERNEL
 
-void mac68k_set_pte(vaddr_t va, paddr_t pge);
+vaddr_t	pmap_map(vaddr_t, paddr_t, paddr_t, int);
+void mac68k_set_pte(vaddr_t, paddr_t);
 
 void pmap_init_md(void);
 #define	PMAP_INIT_MD()	pmap_init_md()
