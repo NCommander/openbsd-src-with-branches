9ad5414bf0ac43d398fc5afac8fd57b3c182f1e3
deraadt <deraadt>
2003-03-04 19:11:37 +0000

    told you it was not knf

--- sys/arch/powerpc/powerpc/trap.c
+++ sys/arch/powerpc/powerpc/trap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: trap.c,v 1.56 2003/02/26 21:54:44 drahn Exp $	*/
+/*	$OpenBSD: trap.c,v 1.57 2003/03/04 19:10:31 drahn Exp $	*/
 /*	$NetBSD: trap.c,v 1.3 1996/10/13 03:31:37 christos Exp $	*/
 
 /*
@@ -267,8 +267,7 @@ userret(register struct proc *p, int pc, u_quad_t oticks)
 	if (p->p_flag & P_PROFIL) {
 		extern int psratio;
 
-		addupc_task(p, pc,
-			    (int)(p->p_sticks - oticks) * psratio);
+		addupc_task(p, pc, (int)(p->p_sticks - oticks) * psratio);
 	}
 
 	curpriority = p->p_priority;
