b92e0eeb65f71b3ce9be48ee4fc63ecf94401b83
millert <millert>
2003-03-09 01:27:50 +0000

    Make the semantics of the P_SUGIDEXEC flag match the issetugid(2) man page.  Instead of just clearing P_SUGIDEXEC if real and effective uids/gids matched, we now set P_SUGIDEXEC if there is a mismatch in the real, effective, or saved uid/gid and clear it otherwise. deraadt@ and tholo@ OK.

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_exec.c,v 1.74 2002/12/04 04:31:20 art Exp $	*/
+/*	$OpenBSD: kern_exec.c,v 1.75 2002/12/11 00:08:08 miod Exp $	*/
 /*	$NetBSD: kern_exec.c,v 1.75 1996/02/09 18:59:28 christos Exp $	*/
 
 /*-
@@ -453,11 +453,15 @@ sys_execve(p, v, retval)
 	}
 
 	/*
-	 * If process does execve() while it has euid/uid or egid/gid
-	 * which are mismatched, it remains P_SUGIDEXEC.
+	 * If process does execve() while it has a mismatched real,
+	 * effective, or saved uid/gid, we set P_SUGIDEXEC.
 	 */
-	if (p->p_ucred->cr_uid == p->p_cred->p_ruid &&
-	    p->p_ucred->cr_gid == p->p_cred->p_rgid)
+	if (p->p_ucred->cr_uid != p->p_cred->p_ruid ||
+	    p->p_ucred->cr_uid != p->p_cred->p_svuid ||
+	    p->p_ucred->cr_gid != p->p_cred->p_rgid ||
+	    p->p_ucred->cr_gid != p->p_cred->p_svgid)
+		p->p_flag |= P_SUGIDEXEC;
+	else
 		p->p_flag &= ~P_SUGIDEXEC;
 
 	/*
