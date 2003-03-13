350961ca9fc74cd33938ea1c2e72b59c5c8eeb21
drahn <drahn>
2003-03-13 01:06:58 +0000

    Initialize the timeout for the non-console case of PS2 keyboard. Fixes a panic on alpha with serial console. ok miod@

--- sys/dev/ic/pckbc.c
+++ sys/dev/ic/pckbc.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: pckbc.c,v 1.4 2002/04/30 20:56:15 miod Exp $ */
+/* $OpenBSD: pckbc.c,v 1.5 2002/06/09 00:58:03 nordin Exp $ */
 /* $NetBSD: pckbc.c,v 1.5 2000/06/09 04:58:35 soda Exp $ */
 
 /*
@@ -305,6 +305,9 @@ pckbc_attach(sc)
 	iot = t->t_iot;
 	ioh_d = t->t_ioh_d;
 	ioh_c = t->t_ioh_c;
+
+	if (pckbc_console == 0)
+		timeout_set(&t->t_cleanup, pckbc_cleanup, t);
 
 	/* flush */
 	(void) pckbc_poll_data1(iot, ioh_d, ioh_c, PCKBC_KBD_SLOT, 0);
