5010ad25efc32c0d4467e7f3b207ec770c7e6f00
millert <millert>
2003-03-14 04:29:04 +0000

    Replace strcpy() of a constant w/ strlcpy() for easy grepping; from Raymond M Schneider

--- usr.bin/skeyaudit/skeyaudit.c
+++ usr.bin/skeyaudit/skeyaudit.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: skeyaudit.c,v 1.13 2002/05/16 03:50:42 millert Exp $	*/
+/*	$OpenBSD: skeyaudit.c,v 1.14 2002/05/29 15:32:10 millert Exp $	*/
 
 /*
  * Copyright (c) 1997, 2000 Todd C. Miller <Todd.Miller@courtesan.com>
@@ -135,7 +135,7 @@ notify(struct passwd *pw, int seq, int interactive)
 
 	/* Only set this once */
 	if (hostname[0] == '\0' && gethostname(hostname, sizeof(hostname)) == -1)
-		strcpy(hostname, "unknown");
+		strlcpy(hostname, "unknown", sizeof(hostname));
 
 	if (interactive)
 		out = stdout;
