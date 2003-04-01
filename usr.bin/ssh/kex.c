cc64e3e7d4dbacd247094a71c82b00ff88c6ca26
markus <markus>
2003-04-01 10:31:26 +0000

    bugfix causes stalled connections for ssh.com < 3.0; noticed by ho@; tested by ho@ and myself

--- usr.bin/ssh/kex.c
+++ usr.bin/ssh/kex.c
@@ -23,7 +23,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: kex.c,v 1.53 2003/02/02 10:56:08 markus Exp $");
+RCSID("$OpenBSD: kex.c,v 1.54 2003/02/16 17:09:57 markus Exp $");
 
 #include <openssl/crypto.h>
 
@@ -392,7 +392,8 @@ kex_choose_conf(Kex *kex)
 	kex->we_need = need;
 
 	/* ignore the next message if the proposals do not match */
-	if (first_kex_follows && !proposals_match(my, peer)) {
+	if (first_kex_follows && !proposals_match(my, peer) && 
+	   !(datafellows & SSH_BUG_FIRSTKEX)) {
 		type = packet_read();
 		debug2("skipping next packet (type %u)", type);
 	}
