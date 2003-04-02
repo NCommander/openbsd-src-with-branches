24c01e3aae2674e95989e2dcbb0ccc464d78de97
mickey <mickey>
2003-04-02 18:51:37 +0000

    make it compile after tcpstates changes elsewhere

--- sys/netipx/spx_debug.h
+++ sys/netipx/spx_debug.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: spx_debug.h,v 1.2 1996/10/26 09:34:56 mickey Exp $	*/
+/*	$OpenBSD: spx_debug.h,v 1.3 2002/03/14 01:27:12 millert Exp $	*/
 
 /*-
  *
@@ -71,7 +71,6 @@ int	spx_debx;
 #ifdef _KERNEL
 extern char *prurequests[];
 extern char *sanames[];
-extern char *tcpstates[];
 
 void	spx_trace(int act, int ostate, struct spxpcb *sp, struct spx *si,
 		       int req);
