71d59a1a921784466c5d988a960c309bd436c33c
mickey <mickey>
2003-04-08 20:48:39 +0000

    just wate that debug altogether

--- sys/arch/hppa/dev/wax.c
+++ sys/arch/hppa/dev/wax.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: wax.c,v 1.4 2003/04/07 16:49:02 mickey Exp $	*/
+/*	$OpenBSD: wax.c,v 1.5 2003/04/08 20:37:39 miod Exp $	*/
 
 /*
  * Copyright (c) 1998,2003 Michael Shalayeff
@@ -102,12 +102,8 @@ waxattach(parent, self, aux)
 	int s, in;
 
 	if (bus_space_map(ca->ca_iot, ca->ca_hpa + 0xc000,
-			  IOMOD_HPASIZE, 0, &ioh)) {
-#ifdef DEBUG
+	    IOMOD_HPASIZE, 0, &ioh)) {
 		printf(": can't map IO space\n");
-#else
-		printf("\n");
-#endif
 		return;
 	}
 
