713ec628267da4701ae3211e95f9aa2ddd8aa9ed
deraadt <deraadt>
2003-04-16 01:45:36 +0000

    string cleaning

--- gnu/usr.bin/ld/ldconfig/ldconfig.c
+++ gnu/usr.bin/ld/ldconfig/ldconfig.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ldconfig.c,v 1.14 2002/09/07 01:25:34 marc Exp $	*/
+/*	$OpenBSD: ldconfig.c,v 1.15 2002/12/08 16:27:17 millert Exp $	*/
 
 /*
  * Copyright (c) 1993,1995 Paul Kranenburg
@@ -193,7 +193,7 @@ dodir(char *dir, int silent)
 			continue;
 
 		/* Copy the entry minus prefix */
-		(void)strcpy(name, dp->d_name + 3);
+		(void)strlcpy(name, dp->d_name + 3, sizeof name);
 		n = strlen(name);
 		if (n < 4)
 			continue;
