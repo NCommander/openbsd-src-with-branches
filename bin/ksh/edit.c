18866abe14679a8cda71accde5d2e7c458d976a7
fgsch <fgsch>
2003-05-05 22:02:33 +0000

    in emacs or vi mode, if vi-tabcomplete or vi-esccomplete mode is on, this escapes "`" so by instance cd works correctly; millert@ and pvalchev@ ok.

--- bin/ksh/edit.c
+++ bin/ksh/edit.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: edit.c,v 1.14 2002/02/16 17:18:08 millert Exp $	*/
+/*	$OpenBSD: edit.c,v 1.15 2002/06/09 05:47:27 todd Exp $	*/
 
 /*
  * Command line editing - common code
@@ -1058,7 +1058,7 @@ x_escape(s, len, putbuf_func)
 	int rval=0;
 
 	for (add = 0, wlen = len; wlen - add > 0; add++) {
-		if (strchr("\\$(){}*&;#|<>\"'", s[add]) || strchr(ifs, s[add])) {
+		if (strchr("\\$(){}*&;#|<>\"'`", s[add]) || strchr(ifs, s[add])) {
 			if (putbuf_func(s, add) != 0) {
 				rval = -1;
 				break;
