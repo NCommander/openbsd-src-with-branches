38594d43eaecbdfc19cd41899f57ea2d701fd759
margarida <margarida>
2003-05-12 19:26:42 +0000

    It's 3.3-stable now. Pointed by Chad Dougherty <chad@stalphonsos.com>

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.50 2003/02/13 19:51:30 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.51 2003/03/04 18:35:06 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -74,7 +74,7 @@ const char osversion[] = "${id}#${v}";
 const char sccs[] =
     "    @(#)${ost} ${osr} (${id}) #${v}: ${t}\n";
 const char version[] =
-    "${ost} ${osr} (${id}) #${v}: ${t}\n    ${u}@${h}:${d}\n";
+    "${ost} ${osr}-stable (${id}) #${v}: ${t}\n    ${u}@${h}:${d}\n";
 eof
 
 expr ${v} + 1 > version
