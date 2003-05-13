8c1f699019f98015b99731c5e9db0063200fef85
millert <millert>
2003-05-13 16:40:37 +0000

    De-hackify.  Use proper defines now that we have EI_OSABI and ELFOSABI_FREEBSD

--- sys/compat/freebsd/freebsd_exec.c
+++ sys/compat/freebsd/freebsd_exec.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: freebsd_exec.c,v 1.12 2001/11/06 19:53:17 miod Exp $	*/
+/*	$OpenBSD: freebsd_exec.c,v 1.13 2003/03/11 22:43:23 millert Exp $	*/
 /*	$NetBSD: freebsd_exec.c,v 1.2 1996/05/18 16:02:08 christos Exp $	*/
 
 /*
@@ -157,8 +157,12 @@ freebsd_elf_probe(p, epp, itp, pos, os)
 	int error;
 	size_t len;
 
+	/*
+	 * Older FreeBSD ELF binaries use a brand; newer ones use EI_OSABI
+	 */
 	brand = elf32_check_brand(eh);
-	if ((brand == NULL || strcmp(brand, "FreeBSD")) && eh->e_ident[EI_PAD] != 9)
+	if ((brand == NULL || strcmp(brand, "FreeBSD")) &&
+	    eh->e_ident[EI_OSABI] != ELFOSABI_FREEBSD)
 		return (EINVAL);
 	if (itp[0]) {
 		if ((error = emul_find(p, NULL, freebsd_emul_path, itp, &bp, 0)))
