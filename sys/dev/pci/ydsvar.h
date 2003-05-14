bfa140f71da72045621c51564408946baae6cfd8
jason <jason>
2003-05-14 09:04:59 +0000

    Nuke more commons: ac97_id2, tinfo_t

--- sys/dev/pci/ydsvar.h
+++ sys/dev/pci/ydsvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ydsvar.h,v 1.3 2001/12/20 04:50:06 jcs Exp $	*/
+/*	$OpenBSD: ydsvar.h,v 1.4 2002/01/04 20:42:18 ericj Exp $	*/
 /*	$NetBSD$	*/
 
 /*
@@ -119,7 +119,5 @@ struct yds_softc {
 };
 #define sc_opl_iot	sc_legacy_iot
 #define sc_mpu_iot	sc_legacy_iot
-
-int ac97_id2;
 
 #endif /* _DEV_PCI_YDSVAR_H_ */
