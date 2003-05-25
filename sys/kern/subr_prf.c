437c289ac6d7fbd38d9113c266290214cb8ec0de
ho <ho>
2003-05-25 19:26:49 +0000

    Add kern.splassert = 3, which works like level 2 but also drops into the debugger. niklas@ ok.

--- sys/kern/subr_prf.c
+++ sys/kern/subr_prf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: subr_prf.c,v 1.26.2.13 2003/05/16 00:29:43 niklas Exp $	*/
+/*	$OpenBSD: subr_prf.c,v 1.26.2.14 2003/05/18 18:16:45 niklas Exp $	*/
 /*	$NetBSD: subr_prf.c,v 1.45 1997/10/24 18:14:25 chuck Exp $	*/
 
 /*-
@@ -286,6 +286,12 @@ splassert_fail(int wantipl, int haveipl, const char *func)
 		db_stack_dump();
 #endif
 		break;
+	case 3:
+#ifdef DDB
+		db_stack_dump();
+		Debugger();
+		break;
+#endif
 	default:
 		panic("spl assertion failure in %s", func);
 	}
