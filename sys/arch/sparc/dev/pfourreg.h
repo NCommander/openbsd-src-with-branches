07ddf4b3470cb915c5adc401a322c93dd04fcd3a
deraadt <deraadt>
2003-06-02 18:47:58 +0000

    remove terms 3 (and 4 in some places) where i am the author

--- sys/arch/sparc/dev/pfourreg.h
+++ sys/arch/sparc/dev/pfourreg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: pfourreg.h,v 1.4 1997/08/08 08:25:21 downsj Exp $	*/
 /*	$NetBSD: pfourreg.h,v 1.1 1996/02/27 22:09:36 thorpej Exp $	*/
 
 /*
@@ -14,9 +14,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Theo de Raadt.
  * 4. The name of the author may not be used to endorse or promote products
  *    derived from this software without specific prior written permission.
  *
