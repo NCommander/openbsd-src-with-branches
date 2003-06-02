3fd7961b7343977050598af098409e78f2fa8acc
jason <jason>
2003-06-02 18:40:59 +0000

    nuke my clause 3 & 4 (and a couple jointly (c) with Theo).

--- sys/arch/sparc/dev/be.c
+++ sys/arch/sparc/dev/be.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: be.c,v 1.32 2002/04/30 01:12:28 art Exp $	*/
+/*	$OpenBSD: be.c,v 1.33 2002/08/08 03:32:00 jason Exp $	*/
 
 /*
  * Copyright (c) 1998 Theo de Raadt and Jason L. Wright.
@@ -12,8 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. The name of the authors may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
