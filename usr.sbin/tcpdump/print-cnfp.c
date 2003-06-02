5a90a62fcb64462bdec75f89b376c925db71320a
mickey <mickey>
2003-06-02 20:37:14 +0000

    three four kill ...

--- usr.sbin/tcpdump/print-cnfp.c
+++ usr.sbin/tcpdump/print-cnfp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: print-cnfp.c,v 1.3 2000/10/03 14:21:56 ho Exp $	*/
+/*	$OpenBSD: print-cnfp.c,v 1.4 2001/11/06 03:11:40 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1998 Michael Shalayeff
@@ -12,11 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Michael Shalayeff.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
