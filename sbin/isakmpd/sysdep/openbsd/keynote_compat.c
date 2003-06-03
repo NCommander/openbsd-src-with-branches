1dca7a6639a4a2a1cc3b9233b92f22cc5a638977
ho <ho>
2003-06-03 14:53:11 +0000

    Remove clauses 3 and 4. Approved by markus@ and niklas@.

--- sbin/isakmpd/sysdep/openbsd/keynote_compat.c
+++ sbin/isakmpd/sysdep/openbsd/keynote_compat.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: keynote_compat.c,v 1.3 2000/12/12 01:49:48 niklas Exp $	*/
+/*	$OpenBSD: keynote_compat.c,v 1.4 2002/06/09 08:13:07 todd Exp $	*/
 /*	$EOM: keynote_compat.c,v 1.1 2000/10/15 19:18:26 niklas Exp $	*/
 
 /*
@@ -12,11 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Ericsson Radio Systems.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
