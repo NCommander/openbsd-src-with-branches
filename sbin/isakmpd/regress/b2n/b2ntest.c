c06baf40f3a32e8c00444d325418bf2d542954ca
ho <ho>
2003-06-03 14:39:51 +0000

    Remove clauses 3 and 4. Approved by Niklas Hallqvist and Niels Provos.

--- sbin/isakmpd/regress/b2n/b2ntest.c
+++ sbin/isakmpd/regress/b2n/b2ntest.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: b2ntest.c,v 1.6 2002/01/23 17:43:24 ho Exp $	*/
+/*	$OpenBSD: b2ntest.c,v 1.7 2002/06/09 08:13:07 todd Exp $	*/
 /*	$EOM: b2ntest.c,v 1.4 1998/07/16 19:31:55 provos Exp $	*/
 
 /*
@@ -13,11 +13,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Ericsson Radio Systems.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
