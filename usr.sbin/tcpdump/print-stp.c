65a6dcf245234206a06cb6d18267e01d00d5bd3f
jason <jason>
2003-06-03 00:21:04 +0000

    last bit of clause 3 & 4 nuking for me.

--- usr.sbin/tcpdump/print-stp.c
+++ usr.sbin/tcpdump/print-stp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: print-stp.c,v 1.1 2000/10/19 16:31:42 jason Exp $	*/
+/*	$OpenBSD: print-stp.c,v 1.2 2002/02/19 19:39:40 millert Exp $	*/
 
 /*
  * Copyright (c) 2000 Jason L. Wright (jason@thought.net)
@@ -12,11 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Jason L. Wright
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
