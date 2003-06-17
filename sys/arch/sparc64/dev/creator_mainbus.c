36ba5b971ac7df6f392fbcfe886c5701c4cc83ab
miod <miod>
2003-06-17 17:35:43 +0000

    Extend the sunfb structure to take care of the PROM cursor position pointers, and adapt creator to the fb api. ok jason@

--- sys/arch/sparc64/dev/creator_mainbus.c
+++ sys/arch/sparc64/dev/creator_mainbus.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: creator_mainbus.c,v 1.6 2003/03/27 18:17:58 jason Exp $	*/
+/*	$OpenBSD: creator_mainbus.c,v 1.7 2003/06/02 20:24:09 jason Exp $	*/
 
 /*
  * Copyright (c) 2002 Jason L. Wright (jason@thought.net),
@@ -43,6 +43,7 @@
 #include <dev/wscons/wsdisplayvar.h>
 #include <dev/wscons/wscons_raster.h>
 #include <dev/rasops/rasops.h>
+#include <machine/fbvar.h>
 
 #include <sparc64/dev/creatorreg.h>
 #include <sparc64/dev/creatorvar.h>
