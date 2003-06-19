f173a4af605bb540d21d9da9df2c497002f23ec6
pvalchev <pvalchev>
2003-06-19 07:21:20 +0000

    plug memory leak; ok solar, camield

--- usr.sbin/popa3d/virtual.c
+++ usr.sbin/popa3d/virtual.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: virtual.c,v 1.3 2002/03/27 14:08:43 camield Exp $ */
+/* $OpenBSD: virtual.c,v 1.4 2003/04/02 00:07:53 deraadt Exp $ */
 
 /*
  * Virtual domain support.
@@ -117,7 +117,10 @@ struct passwd *virtual_userpass(char *user, char *pass, int *known)
 		return NULL;
 	}
 
-	if (!(address = strdup(address))) return NULL;
+	if (!(address = strdup(address))) {
+		free(pathname);
+		return NULL;
+	}
 	virtual_domain = address;
 
 	free(pathname);
