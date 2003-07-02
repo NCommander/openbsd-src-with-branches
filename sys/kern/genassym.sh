f902f211f4cc19dca9deb5c701a4c729db69d174
avsm <avsm>
2003-07-02 00:37:31 +0000

    bump mktemp randomness from 6->10 X's

--- sys/kern/genassym.sh
+++ sys/kern/genassym.sh
@@ -1,4 +1,4 @@
-#	$OpenBSD: genassym.sh,v 1.8 2002/02/21 02:49:18 miod Exp $
+#	$OpenBSD: genassym.sh,v 1.9 2003/02/08 10:19:30 pvalchev Exp $
 #	$NetBSD: genassym.sh,v 1.9 1998/04/25 19:48:27 matthias Exp $
 
 #
@@ -43,8 +43,8 @@ else
 	ccode=0
 fi
 
-TMPC=`mktemp /tmp/genassym_c.XXXXXX` || exit 1
-TMP=`mktemp /tmp/genassym.XXXXXX` || {
+TMPC=`mktemp /tmp/genassym_c.XXXXXXXXXX` || exit 1
+TMP=`mktemp /tmp/genassym.XXXXXXXXXX` || {
 	rm -f ${TMPC}
 	exit 1
 }
