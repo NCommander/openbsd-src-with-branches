97539c650529a5497f07f3d2dfcdc44a06299871
cedric <cedric>
2003-07-11 08:29:34 +0000

    Better parsing and -v support for tables:
      - remove the tableaddrs and tableaddr yacc production and reuse
        host_list instead.
      - produce better error messages.
      - do not load addresses from external file when it is not
        required (like with -R option).
      - store initializers in a new node_tinit linked list before
        putting them into the address buffer (see next point).
      - add a new print_tabledef() function, which makes "pfctl -nvf"
        print something useful for table definitions, which in turn
        makes it possible to write better regress tests (see first chunk
        of the diff) and bring table definition consistant with other
        parsed rules.
    
    ok dhartmei@

--- usr.sbin/authpf/authpf.c
+++ usr.sbin/authpf/authpf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: authpf.c,v 1.64 2003/07/03 21:09:13 cedric Exp $	*/
+/*	$OpenBSD: authpf.c,v 1.65 2003/07/08 11:31:27 dhartmei Exp $	*/
 
 /*
  * Copyright (C) 1998 - 2002 Bob Beck (beck@openbsd.org).
@@ -848,9 +848,8 @@ pfctl_set_limit(struct pfctl *pf, const char *opt, unsigned int limit)
 }
 
 int
-pfctl_define_table(char *name, int flags, int addrs, int noaction,
-    const char *anchor, const char *ruleset, struct pfr_buffer *ab,
-    int ticket)
+pfctl_define_table(char *name, int flags, int addrs, const char *anchor,
+    const char *ruleset, struct pfr_buffer *ab, int ticket)
 {
 	fprintf(stderr, "table definitions not yet supported in authpf\n");
 	return (1);
