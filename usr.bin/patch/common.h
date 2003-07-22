9f6454a87664b4005d4c3b4a08e48040e7e1e417
millert <millert>
2003-07-22 21:50:21 +0000

    Missed this in the rejname commit.

--- usr.bin/patch/common.h
+++ usr.bin/patch/common.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: common.h,v 1.16 2003/07/21 14:32:21 deraadt Exp $	*/
+/*	$OpenBSD: common.h,v 1.17 2003/07/22 17:18:49 otto Exp $	*/
 
 #define DEBUGGING
 
@@ -63,7 +63,6 @@ EXT bool ok_to_create_file INIT(FALSE);
 EXT char       *bestguess INIT(NULL);	/* guess at correct filename */
 
 EXT char       *outname INIT(NULL);
-EXT char        rejname[128];
 
 EXT char       *origprae INIT(NULL);
 
