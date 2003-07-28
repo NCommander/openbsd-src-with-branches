b0bb513eeae9e301de151488c8189418a68b3031
deraadt <deraadt>
2003-07-28 20:38:31 +0000

    cleanup library parts

--- usr.bin/lex/libyywrap.c
+++ usr.bin/lex/libyywrap.c
@@ -1,15 +1,15 @@
-/*	$OpenBSD: libyywrap.c,v 1.4 1996/12/10 22:22:03 millert Exp $	*/
+/*	$OpenBSD: libyywrap.c,v 1.5 2002/02/16 21:27:47 millert Exp $	*/
 
 /* libyywrap - flex run-time support library "yywrap" function */
 
-/* $Header: /cvs/src/usr.bin/lex/libyywrap.c,v 1.4 1996/12/10 22:22:03 millert Exp $ */
+/* $Header: /cvs/src/usr.bin/lex/libyywrap.c,v 1.5 2002/02/16 21:27:47 millert Exp $ */
 
 #include <sys/cdefs.h>
 
 int yywrap(void);
 
 int
-yywrap()
-	{
+yywrap(void)
+{
 	return 1;
-	}
+}
