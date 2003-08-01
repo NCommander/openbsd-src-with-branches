027e8fd4ecc7fc2ee9a203a47f5c456b46c249c4
millert <millert>
2003-08-01 19:50:23 +0000

    Go back to using ITIMER_PROF for the non-profiled libpthread and use ITIMER_VIRTUAL for the profiled version.  Fixes problems when programs linked with libpthread try to use ITIMER_VIRTUAL (and have no way of knowing that this will cause conflicts with the threads lib). marc@ OK

--- lib/libpthread/uthread/pthread_private.h
+++ lib/libpthread/uthread/pthread_private.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pthread_private.h,v 1.46 2003/05/13 16:49:32 marc Exp $	*/
+/*	$OpenBSD: pthread_private.h,v 1.47 2003/07/08 00:17:18 marc Exp $	*/
 /*
  * Copyright (c) 1995-1998 John Birrell <jb@cimlogic.com.au>.
  * All rights reserved.
@@ -169,7 +169,7 @@
 /*
  * Define the signals to be used for scheduling.
  */
-#if defined(_PTHREADS_COMPAT_SCHED)
+#if defined(_PTHREADS_COMPAT_SCHED) || defined(PROF)
 #define _ITIMER_SCHED_TIMER	ITIMER_VIRTUAL
 #define _SCHED_SIGNAL		SIGVTALRM
 #else
