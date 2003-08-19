24d2898bde772f3e4f4f9133651331cda221a173
mpech <mpech>
2003-08-19 14:01:35 +0000

    missing break.
    jason@ ok

--- sys/dev/pci/if_tl.c
+++ sys/dev/pci/if_tl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_tl.c,v 1.29 2003/01/15 06:31:24 art Exp $	*/
+/*	$OpenBSD: if_tl.c,v 1.30 2003/06/30 02:52:51 avsm Exp $	*/
 
 /*
  * Copyright (c) 1997, 1998
@@ -1800,6 +1800,7 @@ int tl_ioctl(ifp, command, data)
 			tl_init(sc);
 			break;
 		}
+		break;
 	case SIOCSIFFLAGS:
 		if (ifp->if_flags & IFF_UP) {
 			if (ifp->if_flags & IFF_RUNNING &&
