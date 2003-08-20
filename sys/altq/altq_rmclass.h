0ddf9e4e64c4cfcf060b92a44858714fd38b3cb9
henning <henning>
2003-08-20 12:33:17 +0000

    move the #define RM_MAXPRIO out of #ifdef _KERNEL

--- sys/altq/altq_rmclass.h
+++ sys/altq/altq_rmclass.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: altq_rmclass.h,v 1.5 2002/11/26 01:03:34 henning Exp $	*/
+/*	$OpenBSD: altq_rmclass.h,v 1.6 2002/12/16 17:27:20 henning Exp $	*/
 /*	$KAME: altq_rmclass.h,v 1.6 2000/12/09 09:22:44 kjc Exp $	*/
 
 /*
@@ -44,6 +44,8 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#define	RM_MAXPRIO	8	/* Max priority */
 
 #ifdef _KERNEL
 
@@ -113,7 +115,6 @@ struct red;
 #else
 #define	RM_MAXQUEUED	16	/* Max number of packets downstream of CBQ */
 #endif
-#define	RM_MAXPRIO	8	/* Max priority */
 #define	RM_MAXQUEUE	64	/* Max queue length */
 #define	RM_FILTER_GAIN	5	/* log2 of gain, e.g., 5 => 31/32 */
 #define	RM_POWER	(1 << RM_FILTER_GAIN)
