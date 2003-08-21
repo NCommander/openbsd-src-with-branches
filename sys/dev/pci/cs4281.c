22d1ff5f0cc7f0ccc56a9caff3b934a4705929e2
mickey <mickey>
2003-08-21 04:57:40 +0000

    sys/types.h is not really needed w/ sys/param.h

--- sys/dev/pci/cs4281.c
+++ sys/dev/pci/cs4281.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cs4281.c,v 1.13 2003/04/27 11:22:53 ho Exp $ */
+/*	$OpenBSD: cs4281.c,v 1.14 2003/08/21 04:52:52 kevlo Exp $ */
 /*	$Tera: cs4281.c,v 1.18 2000/12/27 14:24:45 tacha Exp $	*/
 
 /*
@@ -41,7 +41,6 @@
  *   1: midi and FM support
  */
 
-#include <sys/types.h>
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/kernel.h>
