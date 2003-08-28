c501f7bf6d9b8f80c23c45b3454c3eb3a47dd16e
krw <krw>
2003-08-28 02:24:04 +0000

    Fix use of uninitialized variable by porting NetBSD patch to correctly initialize it.
    Said variable is used as the value of the define DOWNLOAD_CONST_COUNT
    in the generated file aic7xxxreg.h.
    
    This define is currently not used anywhere.
    
    Spotted and ok mickey@.

--- sys/dev/microcode/aic7xxx/aicasm_symbol.c
+++ sys/dev/microcode/aic7xxx/aicasm_symbol.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: aicasm_symbol.c,v 1.5 2002/06/30 18:25:58 smurph Exp $ */
+/* $OpenBSD: aicasm_symbol.c,v 1.6 2002/11/19 18:36:18 jason Exp $ */
 /*
  * Aic7xxx SCSI host adapter firmware asssembler symbol table implementation
  *
@@ -474,8 +474,7 @@ symtable_dump(ofile)
 		
 		fprintf(ofile, "\n\n/* Downloaded Constant Definitions */\n");
 
-		while (SLIST_FIRST(&download_constants) !=
-		    SLIST_END(&download_constants)) {
+		for (i = 0; SLIST_FIRST(&download_constants) != NULL; i++) {
 			symbol_node_t *curnode;
 
 			curnode = SLIST_FIRST(&download_constants);
