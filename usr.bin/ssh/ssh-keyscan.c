985eeae4061a8f863cdec8b800ab11d6bbcb2bf4
markus <markus>
2003-09-19 11:30:39 +0000

    avoid fatal_cleanup, just call exit(); ok deraadt

--- usr.bin/ssh/ssh-keyscan.c
+++ usr.bin/ssh/ssh-keyscan.c
@@ -7,7 +7,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: ssh-keyscan.c,v 1.43 2003/04/26 04:29:49 deraadt Exp $");
+RCSID("$OpenBSD: ssh-keyscan.c,v 1.44 2003/06/28 16:23:06 deraadt Exp $");
 
 #include <sys/queue.h>
 #include <errno.h>
@@ -660,7 +660,7 @@ fatal(const char *fmt,...)
 	if (nonfatal_fatal)
 		longjmp(kexjmp, -1);
 	else
-		fatal_cleanup();
+		exit(255);
 }
 
 static void
