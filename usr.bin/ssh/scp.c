97671aa4a80a1774617a2fcef88fcb71f27cb9b1
markus <markus>
2003-09-19 17:40:20 +0000

    error handling for remote-remote copy; #638; report Harald Koenig; ok millert, fgs, henning, deraadt

--- usr.bin/ssh/scp.c
+++ usr.bin/ssh/scp.c
@@ -71,7 +71,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: scp.c,v 1.107 2003/06/28 16:23:06 deraadt Exp $");
+RCSID("$OpenBSD: scp.c,v 1.108 2003/07/18 01:54:25 deraadt Exp $");
 
 #include "xmalloc.h"
 #include "atomicio.h"
@@ -417,7 +417,8 @@ toremote(char *targ, int argc, char **argv)
 			}
 			if (verbose_mode)
 				fprintf(stderr, "Executing: %s\n", bp);
-			(void) system(bp);
+			if (system(bp) != 0)
+				errs = 1;
 			(void) xfree(bp);
 		} else {	/* local to remote */
 			if (remin == -1) {
