32b031ae52a36725859d91035b3a889c5c48c5e4
brad <brad>
2003-09-24 18:59:11 +0000

    MFC: Fixes by tedu@ and beck@
    fp leak.  from Patrick Latifi.
    and
    millert's sup fix, tested here on client and server.
    
    ok millert@

--- usr.bin/sup/src/supcparse.c
+++ usr.bin/sup/src/supcparse.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: supcparse.c,v 1.7 2001/05/04 22:16:16 millert Exp $	*/
+/*	$OpenBSD: supcparse.c,v 1.8 2002/02/16 21:27:54 millert Exp $	*/
 
 /*
  * Copyright (c) 1992 Carnegie Mellon University
@@ -266,8 +266,10 @@ putwhen(fname, tstamp)
 	FILE *fp;
 	if ((fp = fopen(fname, "w")) == NULL)
 		return (0);
-	if (fprintf(fp, "%u\n", tstamp) < 0)
+	if (fprintf(fp, "%u\n", tstamp) < 0) {
+		(void) fclose(fp);
 		return (0);
+	}
 	if (fclose(fp) != 0)
 		return (0);
 	return (1);
