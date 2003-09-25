fe93a6954192b5f85f34da7226a37e35246f281a
deraadt <deraadt>
2003-09-25 04:19:39 +0000

    whack commons as wanted by ho

--- sbin/fsck_ffs/fsck.h
+++ sbin/fsck_ffs/fsck.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fsck.h,v 1.12 2003/06/02 20:06:15 millert Exp $	*/
+/*	$OpenBSD: fsck.h,v 1.13 2003/08/25 23:28:15 tedu Exp $	*/
 /*	$NetBSD: fsck.h,v 1.13 1996/10/11 20:15:46 thorpej Exp $	*/
 
 /*
@@ -160,7 +160,8 @@ struct inoinfo {
 	u_int	i_numblks;		/* size of block array in bytes */
 	daddr_t	i_blks[1];		/* actually longer */
 } **inphead, **inpsort;
-long numdirs, listmax, inplast;
+
+extern long numdirs, listmax, inplast;
 
 long	dev_bsize;		/* computed value of DEV_BSIZE */
 long	secsize;		/* actual disk sector size */
--- sbin/fsck_ffs/setup.c
+++ sbin/fsck_ffs/setup.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: setup.c,v 1.19 2003/06/02 20:06:15 millert Exp $	*/
+/*	$OpenBSD: setup.c,v 1.20 2003/06/26 08:01:54 tedu Exp $	*/
 /*	$NetBSD: setup.c,v 1.27 1996/09/27 22:45:19 christos Exp $	*/
 
 /*
@@ -34,7 +34,7 @@
 #if 0
 static char sccsid[] = "@(#)setup.c	8.5 (Berkeley) 11/23/94";
 #else
-static const char rcsid[] = "$OpenBSD: setup.c,v 1.19 2003/06/02 20:06:15 millert Exp $";
+static const char rcsid[] = "$OpenBSD: setup.c,v 1.20 2003/06/26 08:01:54 tedu Exp $";
 #endif
 #endif /* not lint */
 
@@ -67,6 +67,8 @@ int calcsb(char *, int, struct fs *);
 static struct disklabel *getdisklabel(char *, int);
 static int readsb(int);
 static int cmpsb(struct fs *, struct fs *);
+
+long numdirs, listmax, inplast;
 
 int
 setup(char *dev)
