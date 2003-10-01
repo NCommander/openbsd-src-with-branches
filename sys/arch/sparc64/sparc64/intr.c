a5d393194f89947ae03078d200219d18dd53b5f3
cloder <cloder>
2003-10-01 06:01:55 +0000

    Fix off-by-one. OK jason@, henric@

--- sys/arch/sparc64/sparc64/intr.c
+++ sys/arch/sparc64/sparc64/intr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: intr.c,v 1.16 2003/05/17 03:53:39 jason Exp $	*/
+/*	$OpenBSD: intr.c,v 1.17 2003/06/12 01:07:31 deraadt Exp $	*/
 /*	$NetBSD: intr.c,v 1.39 2001/07/19 23:38:11 eeh Exp $ */
 
 /*
@@ -236,7 +236,7 @@ intr_establish(level, ih)
 	}
 #endif
 
-	if (ih->ih_number <= 0 || ih->ih_number > MAXINTNUM)
+	if (ih->ih_number <= 0 || ih->ih_number >= MAXINTNUM)
 		panic("intr_establish: bad intr number %x", ih->ih_number);
 
 	q = intrlev[ih->ih_number];
