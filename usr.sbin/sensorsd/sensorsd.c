660486a1ac08ff0f5c2fb93a9b180f00e61c5d8a
kevlo <kevlo>
2003-10-01 02:43:17 +0000

    sys/types.h is not really needed with sys/param.h. ok henning@

--- usr.sbin/sensorsd/sensorsd.c
+++ usr.sbin/sensorsd/sensorsd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sensorsd.c,v 1.3 2003/09/30 00:18:40 jose Exp $ */
+/*	$OpenBSD: sensorsd.c,v 1.4 2003/09/30 15:06:29 henning Exp $ */
 
 /*
  * Copyright (c) 2003 Henning Brauer <henning@openbsd.org>
@@ -16,7 +16,6 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include <sys/types.h>
 #include <sys/param.h>
 #include <sys/sysctl.h>
 #include <sys/sensors.h>
