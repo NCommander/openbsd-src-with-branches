159e77204324f1e6a5e79f924c5824dd10784d30
miod <miod>
2003-10-08 19:10:05 +0000

    delay() is actually a void function.

--- sys/arch/mvme88k/include/param.h
+++ sys/arch/mvme88k/include/param.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: param.h,v 1.29 2003/08/01 07:47:00 miod Exp $ */
+/*	$OpenBSD: param.h,v 1.30 2003/08/10 07:26:33 miod Exp $ */
 /*
  * Copyright (c) 1999 Steve Murphree, Jr.
  * Copyright (c) 1988 University of Utah.
@@ -160,7 +160,7 @@
 #include <machine/intr.h>
 
 #ifdef   _KERNEL
-extern int delay(int);
+extern void delay(int);
 #define  DELAY(x)             delay(x)
 
 extern int cputyp;
