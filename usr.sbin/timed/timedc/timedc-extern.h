2b611afc09e12fa46dd9a1ac4522f07ba53d3e8f
deraadt <deraadt>
2003-10-12 23:44:13 +0000

    buf oflow, fixed badly in freebsd by tjr, i changed it to give a nice warning instead of blind truncation; millert ok

--- usr.sbin/timed/timedc/timedc-extern.h
+++ usr.sbin/timed/timedc/timedc-extern.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: timedc-extern.h,v 1.5 2003/06/02 23:36:55 millert Exp $	*/
+/*	$OpenBSD: timedc-extern.h,v 1.6 2003/06/26 21:36:40 deraadt Exp $	*/
 
 /*-
  * Copyright (c) 1993 The Regents of the University of California.
@@ -37,7 +37,7 @@ void	bytehostorder(struct tsp *);
 void	bytenetorder(struct tsp *);
 void	clockdiff(int, char *[]);
 void	help(int, char *[]);
-void	makeargv(void);
+int	makeargv(void);
 void	msite(int, char *[]);
 void	quit(int, char *[]);
 void	testing(int, char *[]);
