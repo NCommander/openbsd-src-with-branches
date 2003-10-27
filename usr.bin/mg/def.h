c10836931a8d7a98df293cf7bf44842104299eed
vincent <vincent>
2003-10-27 11:21:12 +0000

    fix crash on startup by using the good allocation routine in edinit()
    spotted and ok by henning

--- usr.bin/mg/def.h
+++ usr.bin/mg/def.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: def.h,v 1.48 2003/08/15 23:23:18 vincent Exp $	*/
+/*	$OpenBSD: def.h,v 1.49 2003/10/21 22:48:07 vincent Exp $	*/
 
 #include <sys/queue.h>
 
@@ -360,6 +360,8 @@ int	 kinsert(int, int);
 int	 kremove(int);
 
 /* window.c X */
+MGWIN	*new_window(BUFFER *);
+void	 free_window(MGWIN *);
 int	 reposition(int, int);
 int	 refresh(int, int);
 int	 nextwind(int, int);
