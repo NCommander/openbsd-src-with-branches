f247ed498c83868b0bf0e71528bcc64e8c7fced8
drahn <drahn>
2003-10-31 04:07:10 +0000

    Use isa iot for pegasos com console attachment, perhaps this could be adopted MI?

--- sys/dev/ic/com.c
+++ sys/dev/ic/com.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: com.c,v 1.93 2003/09/23 16:51:12 millert Exp $	*/
+/*	$OpenBSD: com.c,v 1.92.4.1 2003/10/10 17:04:17 drahn Exp $	*/
 /*	$NetBSD: com.c,v 1.82.4.1 1996/06/02 09:08:00 mrg Exp $	*/
 
 /*
@@ -1460,6 +1460,8 @@ comcnprobe(cp)
 	bus_space_tag_t iot = &arc_bus_io;
 #elif defined(hppa)
 	bus_space_tag_t iot = &hppa_bustag;
+#elif defined(__pegasos__)
+	bus_space_tag_t iot = MD_ISA_IOT;
 #else
 	bus_space_tag_t iot = 0;
 #endif
