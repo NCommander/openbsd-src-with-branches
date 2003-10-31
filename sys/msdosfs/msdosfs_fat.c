123bc7607fd21c3a6a6e1b244e2d7adab703b99b
brad <brad>
2003-10-31 06:05:28 +0000

    MFC: Fix by tedu@
    from netbsd:
    Fix a panic that occurred when trying to traverse a corrupt msdosfs
    filesystem.  With this particular corruption, the code in pcbmap()
    would compute an offset into an array that was way out of bounds,
    so check the bounds before trying to access and return an error if
    the offset would be out of bounds.
    
    ok deraadt@

--- sys/msdosfs/msdosfs_fat.c
+++ sys/msdosfs/msdosfs_fat.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: msdosfs_fat.c,v 1.10 2001/12/19 08:58:06 art Exp $	*/
+/*	$OpenBSD: msdosfs_fat.c,v 1.11 2002/03/14 01:27:09 millert Exp $	*/
 /*	$NetBSD: msdosfs_fat.c,v 1.26 1997/10/17 11:24:02 ws Exp $	*/
 
 /*-
@@ -232,6 +232,11 @@ pcbmap(dep, findcn, bnp, cnp, sp)
 			bp_bn = bn;
 		}
 		prevcn = cn;
+		if (bo >= bsize) {
+			if (bp)
+				brelse(bp);
+			return (EIO);
+		}
 		if (FAT32(pmp))
 			cn = getulong(&bp->b_data[bo]);
 		else
