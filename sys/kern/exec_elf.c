e46604ec03301ae58db3d91eb5773d6fbc129726
margarida <margarida>
2003-11-03 22:07:49 +0000

    Pull patch from -current: Fixed by tedu@
    verify exec headers before attempting malloc.  reported by Georgi Guninski.
    
    ok millert@

--- sys/kern/exec_elf.c
+++ sys/kern/exec_elf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: exec_elf.c,v 1.43 2002/11/22 22:10:21 drahn Exp $	*/
+/*	$OpenBSD: exec_elf.c,v 1.44 2003/02/18 03:54:40 drahn Exp $	*/
 
 /*
  * Copyright (c) 1996 Per Fogelstrom
@@ -786,6 +786,7 @@ ELFNAME(os_pt_note)(struct proc *p, struct exec_package *epp, Elf_Ehdr *eh,
 
 	for (ph = hph;  ph < &hph[eh->e_phnum]; ph++) {
 		if (ph->p_type != PT_NOTE ||
+		    ph->p_filesz > 1024 ||
 		    ph->p_filesz < sizeof(Elf_Note) + name_size)
 			continue;
 
