6d365b0462d3352fa1e4bd9666a270f599dfe57e
pvalchev <pvalchev>
2003-12-08 08:25:17 +0000

    Correct what seems to be a typo: __builtin_va_alist vs __builtin_va_list in va_dcl declaration. Fixes lots of varargs-using ports OK mickey

--- sys/arch/hppa/include/varargs.h
+++ sys/arch/hppa/include/varargs.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: varargs.h,v 1.1 2002/03/11 00:19:28 miod Exp $	*/
 
 #ifndef _MACHINE_VARARGS_H_
 #define	_MACHINE_VARARGS_H_
@@ -8,7 +8,7 @@
 #define	__va_ellipsis	...
 
 #define	va_alist	__builtin_va_alist
-#define	va_dcl		long __builtin_va_list; __va_ellipsis
+#define	va_dcl		long __builtin_va_alist; __va_ellipsis
 
 #undef	va_start
 #define	va_start(ap) \
