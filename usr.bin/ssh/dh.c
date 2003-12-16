3508dfe2c6fbeaa5716ca2e535ce0216f9f131c9
markus <markus>
2003-12-16 15:51:54 +0000

    use <= instead of < in dh_estimate; ok provos/hshoexer; do not return < DH_GRP_MIN

--- usr.bin/ssh/dh.c
+++ usr.bin/ssh/dh.c
@@ -23,7 +23,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: dh.c,v 1.24 2003/04/08 20:21:28 itojun Exp $");
+RCSID("$OpenBSD: dh.c,v 1.25 2003/09/18 13:02:21 miod Exp $");
 
 #include "xmalloc.h"
 
@@ -279,11 +279,9 @@ int
 dh_estimate(int bits)
 {
 
-	if (bits < 64)
-		return (512);	/* O(2**63) */
-	if (bits < 128)
+	if (bits <= 128)
 		return (1024);	/* O(2**86) */
-	if (bits < 192)
+	if (bits <= 192)
 		return (2048);	/* O(2**116) */
 	return (4096);		/* O(2**156) */
 }
