b1b249f9a1039ee2317d28786a2849b6cc35cf43
deraadt <deraadt>
2003-12-31 19:17:41 +0000

    workaround:  during npx exception testing, delay(1).  For some reason amd64 (in 32 bit mode) and the new transmeta cpus want this, and lock up otherwise.  very odd.

--- sys/arch/i386/isa/npx.c
+++ sys/arch/i386/isa/npx.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: npx.c,v 1.28 2003/07/25 18:31:25 jason Exp $	*/
+/*	$OpenBSD: npx.c,v 1.29 2003/10/24 09:03:20 grange Exp $	*/
 /*	$NetBSD: npx.c,v 1.57 1996/05/12 23:12:24 mycroft Exp $	*/
 
 #if 0
@@ -203,6 +203,7 @@ npxprobe1(ia)
 			fldcw(&control);
 			npx_traps_while_probing = npx_intrs_while_probing = 0;
 			fp_divide_by_0();
+			delay(1);
 			if (npx_traps_while_probing != 0) {
 				/*
 				 * Good, exception 16 works.
