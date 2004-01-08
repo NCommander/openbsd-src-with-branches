4ae008640e215e2e893f8085cfbb76f85e48a964
drahn <drahn>
2004-01-08 14:59:15 +0000

    __init/__fini handling on ELF has not been correct. It is supposed to be a section which code stubs (branches) can be added to initialize/destructor This adds MD stubs to allow this to operate as expected. should fix wine and behave according to ELF specs. ok miod@

--- lib/csu/crtendS.c
+++ lib/csu/crtendS.c
@@ -1,9 +1,13 @@
-/*	$OpenBSD: crtendS.c,v 1.1 2001/05/28 21:38:13 drahn Exp $	*/
+/*	$OpenBSD: crtendS.c,v 1.2 2002/02/16 21:27:20 millert Exp $	*/
 /*	$NetBSD: crtend.c,v 1.1 1997/04/16 19:38:24 thorpej Exp $	*/
 
 #include <sys/cdefs.h>
+#include "md_init.h"
 
 static void (*__CTOR_LIST__[1])(void)
     __attribute__((section(".ctors"))) = { (void *)0 };		/* XXX */
 static void (*__DTOR_LIST__[1])(void)
     __attribute__((section(".dtors"))) = { (void *)0 };		/* XXX */
+
+MD_SECTION_EPILOGUE(".init");
+MD_SECTION_EPILOGUE(".fini");
