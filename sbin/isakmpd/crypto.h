76556c283f391628a51c0c14ee7a405d23174eff
brad <brad>
2004-01-13 22:50:07 +0000

    Fixes a few message handling flaws in isakmpd as reported by Thomas Walpuski.
    ok deraadt@ hshoexer@

--- sbin/isakmpd/crypto.h
+++ sbin/isakmpd/crypto.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: crypto.h,v 1.8 2003/06/03 14:28:16 ho Exp $	*/
+/*	$OpenBSD: crypto.h,v 1.12 2003/12/22 18:13:58 markus Exp $	*/
 /*	$EOM: crypto.h,v 1.12 2000/10/15 21:56:41 niklas Exp $	*/
 
 /*
@@ -32,16 +32,72 @@
 #ifndef _CRYPTO_H_
 #define _CRYPTO_H_
 
-#include <openssl/evp.h>
+#if defined (__APPLE__)
+
+#include <openssl/des.h>
+#ifdef USE_BLOWFISH
+#include <openssl/blowfish.h>
+#endif
+#ifdef USE_CAST
+#include <openssl/cast.h>
+#endif
+
+#else
+
+#include <des.h>
+#ifdef USE_BLOWFISH
+#include <blf.h>
+#endif
+#ifdef USE_CAST
+#include <cast.h>
+#endif
+
+#endif /* __APPLE__ */
+
+#ifdef USE_AES
+#include <openssl/aes.h>
+#endif
+
+#define USE_32BIT
+#if defined (USE_64BIT)
+
+#define XOR64(x,y) *(u_int64_t *)(x) ^= *(u_int64_t *)(y);
+#define SET64(x,y) *(u_int64_t *)(x) = *(u_int64_t *)(y);
+
+#elif defined (USE_32BIT)
+
+#define XOR64(x,y) *(u_int32_t *)(x) ^= *(u_int32_t *)(y); \
+   *(u_int32_t *)((u_int8_t *)(x) + 4) ^= *(u_int32_t *)((u_int8_t *)(y) + 4);
+#define SET64(x,y) *(u_int32_t *)(x) = *(u_int32_t *)(y); \
+   *(u_int32_t *)((u_int8_t *)(x) + 4) = *(u_int32_t *)((u_int8_t *)(y) + 4);
+
+#else
+
+#define XOR8(x,y,i) (x)[i] ^= (y)[i];
+#define XOR64(x,y) XOR8(x,y,0); XOR8(x,y,1); XOR8(x,y,2); XOR8(x,y,3); \
+   XOR8(x,y,4); XOR8(x,y,5); XOR8(x,y,6); XOR8(x,y,7);
+#define SET8(x,y,i) (x)[i] = (y)[i];
+#define SET64(x,y) SET8(x,y,0); SET8(x,y,1); SET8(x,y,2); SET8(x,y,3); \
+   SET8(x,y,4); SET8(x,y,5); SET8(x,y,6); SET8(x,y,7);
+
+#endif /* USE_64BIT */
+
+#define SET_32BIT_BIG(x,y) (x)[3]= (y); (x)[2]= (y) >> 8; \
+    (x)[1] = (y) >> 16; (x)[0]= (y) >> 24;
+#define GET_32BIT_BIG(x) (u_int32_t)(x)[3] | ((u_int32_t)(x)[2] << 8) | \
+    ((u_int32_t)(x)[1] << 16)| ((u_int32_t)(x)[0] << 24);
 
 /*
  * This is standard for all block ciphers we use at the moment.
- * Theoretically this could increase in future, e.g. for TwoFish.
- * Keep MAXBLK uptodate
+ * Keep MAXBLK uptodate.
  */
 #define BLOCKSIZE	8
 
-#define MAXBLK		(2*BLOCKSIZE)
+#ifdef USE_AES
+#define MAXBLK		AES_BLOCK_SIZE
+#else
+#define MAXBLK		BLOCKSIZE
+#endif
 
 struct keystate {
   struct crypto_xf *xf;			/* Back pointer */
@@ -51,13 +107,24 @@ struct keystate {
   u_int8_t	iv[MAXBLK];		/* Next IV to use */
   u_int8_t	iv2[MAXBLK];
   u_int8_t	*riv, *liv;
-  struct {
-      EVP_CIPHER_CTX enc, dec;
-  } evp;
+  union {
+    des_key_schedule desks[3];
+#ifdef USE_BLOWFISH
+    blf_ctx blfks;
+#endif
+#ifdef USE_CAST
+    cast_key castks;
+#endif
+#ifdef USE_AES
+    AES_KEY aesks[2];
+#endif
+  } keydata;
 };
 
-#define ks_evpenc	evp.enc
-#define ks_evpdec	evp.dec
+#define ks_des	keydata.desks
+#define ks_blf	keydata.blfks
+#define ks_cast	keydata.castks
+#define ks_aes	keydata.aesks
 
 /*
  * Information about the cryptotransform.
--- sbin/isakmpd/exchange.c
+++ sbin/isakmpd/exchange.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: exchange.c,v 1.83 2003/06/15 10:32:15 ho Exp $	*/
+/*	$OpenBSD: exchange.c,v 1.84 2003/08/08 08:46:59 ho Exp $	*/
 /*	$EOM: exchange.c,v 1.143 2000/12/04 00:02:25 angelos Exp $	*/
 
 /*
@@ -220,8 +220,10 @@ exchange_validate (struct message *msg)
 	      && !TAILQ_FIRST (&msg->payload[ISAKMP_PAYLOAD_HASH])
 	      && !TAILQ_FIRST (&msg->payload[ISAKMP_PAYLOAD_SIG]))
 	  || (*pc == EXCHANGE_SCRIPT_INFO
-	      && !TAILQ_FIRST (&msg->payload[ISAKMP_PAYLOAD_NOTIFY])
-	      && !TAILQ_FIRST (&msg->payload[ISAKMP_PAYLOAD_DELETE])))
+	      && ((!TAILQ_FIRST (&msg->payload[ISAKMP_PAYLOAD_NOTIFY])
+		   && !TAILQ_FIRST (&msg->payload[ISAKMP_PAYLOAD_DELETE]))
+	          || (TAILQ_FIRST (&msg->payload[ISAKMP_PAYLOAD_DELETE])
+		      && !TAILQ_FIRST (&msg->payload[ISAKMP_PAYLOAD_HASH])))))
 	{
 	  /* Missing payload.  */
 	  LOG_DBG ((LOG_MESSAGE, 70,
