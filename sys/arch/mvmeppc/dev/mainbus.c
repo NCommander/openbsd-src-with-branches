f06f0efba03bff321614f539eb5c792c53600d12
miod <miod>
2004-01-26 22:57:20 +0000

    Asking for a too short softc size in your cfattach structure is known to cause premature death in laboratory animals. Don't do that.

--- sys/arch/mvmeppc/dev/mainbus.c
+++ sys/arch/mvmeppc/dev/mainbus.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mainbus.c,v 1.4 2002/03/14 03:15:58 millert Exp $	*/
+/*	$OpenBSD: mainbus.c,v 1.5 2004/01/14 20:50:49 miod Exp $	*/
 
 /*
  * Copyright (c) 1994, 1995 Carnegie-Mellon University.
@@ -45,7 +45,7 @@ static void	mbattach(struct device *, struct device *, void *);
 static int	mbprint(void *, const char *);
 
 struct cfattach mainbus_ca = {
-	sizeof(struct device), mbmatch, mbattach
+	sizeof(struct mainbus_softc), mbmatch, mbattach
 };
 struct cfdriver mainbus_cd = {
 	NULL, "mainbus", DV_DULL
