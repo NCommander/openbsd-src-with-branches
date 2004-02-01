473f4ebe205fd24696ad23721b90b55c169a6807
drahn <drahn>
2004-02-01 06:13:17 +0000

    tag...

--- sys/arch/cats/include/exec.h
+++ sys/arch/cats/include/exec.h
@@ -1,5 +1,4 @@
-/*	$OpenBSD: exec.h,v 1.1 2004/01/15 01:18:53 drahn Exp $	*/
-/*	$OpenBSD: exec.h,v 1.1 2004/01/15 01:18:53 drahn Exp $	*/
+/*	$OpenBSD: exec.h,v 1.1 2004/02/01 05:12:54 drahn Exp $	*/
 /*	$NetBSD: exec.h,v 1.6 1994/10/27 04:16:05 cgd Exp $	*/
 
 /*
