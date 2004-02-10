246442198581b2c382dcebbf70bff269e0cf9b5b
tom <tom>
2004-02-10 01:01:13 +0000

    Allow /boot to load read-only data segments in ELF kernels.
    Thanks to miod@ for testing on many archs.
    
    ok deraadt@.

--- sys/lib/libsa/loadfile.c
+++ sys/lib/libsa/loadfile.c
@@ -1,5 +1,5 @@
 /* $NetBSD: loadfile.c,v 1.10 2000/12/03 02:53:04 tsutsui Exp $ */
-/* $OpenBSD: loadfile.c,v 1.7 2003/08/11 06:23:09 deraadt Exp $ */
+/* $OpenBSD: loadfile.c,v 1.8 2003/08/11 06:37:39 mickey Exp $ */
 
 /*-
  * Copyright (c) 1997 The NetBSD Foundation, Inc.
@@ -285,11 +285,11 @@ elf_exec(int fd, Elf_Ehdr *elf, u_long *marks, int flags)
 	for (first = 1, i = 0; i < elf->e_phnum; i++) {
 
 		if (phdr[i].p_type != PT_LOAD ||
-		    (phdr[i].p_flags & (PF_W|PF_X)) == 0)
+		    (phdr[i].p_flags & (PF_W|PF_R|PF_X)) == 0)
 			continue;
 
 #define IS_TEXT(p)	(p.p_flags & PF_X)
-#define IS_DATA(p)	(p.p_flags & PF_W)
+#define IS_DATA(p)	((p.p_flags & PF_X) == 0)
 #define IS_BSS(p)	(p.p_filesz < p.p_memsz)
 		/*
 		 * XXX: Assume first address is lowest
