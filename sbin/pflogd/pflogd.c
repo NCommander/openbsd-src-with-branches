8abe0799933fa16d0b21537dd6040ab35d4eb5dd
otto <otto>
2004-02-13 19:01:57 +0000

    cleanup signal handling; close descriptors. ok avsm@ millert@ canacar@

--- sbin/pflogd/pflogd.c
+++ sbin/pflogd/pflogd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pflogd.c,v 1.25 2004/01/15 20:15:14 canacar Exp $	*/
+/*	$OpenBSD: pflogd.c,v 1.26 2004/01/16 10:45:49 jmc Exp $	*/
 
 /*
  * Copyright (c) 2001 Theo de Raadt
@@ -502,6 +502,8 @@ main(int argc, char **argv)
 	struct pcap_stat pstat;
 	int ch, np, Xflag = 0;
 	pcap_handler phandler = dump_packet;
+
+	closefrom(STDERR_FILENO + 1);
 
 	while ((ch = getopt(argc, argv, "Dxd:s:f:")) != -1) {
 		switch (ch) {
