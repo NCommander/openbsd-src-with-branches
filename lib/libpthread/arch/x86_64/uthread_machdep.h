8318a2114e1ac72838313ff022fbe87b714d5094
deraadt <deraadt>
2004-02-25 03:48:36 +0000

    initial working pthreads support from sparc64, done with drahn.  there are some `debugging features' and other optimizations (such as not saving all registers) that we can still do perhaps

--- lib/libpthread/arch/x86_64/uthread_machdep.h
+++ lib/libpthread/arch/x86_64/uthread_machdep.h
@@ -1,5 +1,10 @@
-/* dummy */
+/*	$OpenBSD$	*/
+
+#include <sys/types.h>
+#include <machine/fpu.h>
 
 struct _machdep_state {
+	long	rsp;
+	/* must be 128-bit aligned */
+	struct savefpu   fpreg __attribute__ ((aligned (16)));
 };
-
