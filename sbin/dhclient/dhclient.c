2835e018b957cc1b7a082edb0606af42d16651aa
deraadt <deraadt>
2004-03-05 23:57:16 +0000

    remove even more goo

--- sbin/dhclient/dhclient.c
+++ sbin/dhclient/dhclient.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dhclient.c,v 1.29 2004/03/02 15:58:41 henning Exp $	*/
+/*	$OpenBSD: dhclient.c,v 1.30 2004/03/02 18:49:21 deraadt Exp $	*/
 
 /* DHCP Client. */
 
@@ -301,11 +301,6 @@ usage(void)
 	fprintf(stderr, "usage: %s [-dDqu] ", __progname);
 	fprintf(stderr, "[-c conffile] [-l leasefile] interface\n");
 	exit(1);
-}
-
-void
-cleanup(void)
-{
 }
 
 /*
