9632666a77fa28ed21bd15097b7c2c360f34ac2b
djm <djm>
2004-03-15 22:18:15 +0000

    more "if (foo);" ok millert@

--- bin/pax/ar_subs.c
+++ bin/pax/ar_subs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ar_subs.c,v 1.24 2003/06/02 23:32:08 millert Exp $	*/
+/*	$OpenBSD: ar_subs.c,v 1.25 2003/06/26 00:10:17 deraadt Exp $	*/
 /*	$NetBSD: ar_subs.c,v 1.5 1995/03/21 09:07:06 cgd Exp $	*/
 
 /*-
@@ -38,7 +38,7 @@
 #if 0
 static const char sccsid[] = "@(#)ar_subs.c	8.2 (Berkeley) 4/18/94";
 #else
-static const char rcsid[] = "$OpenBSD: ar_subs.c,v 1.24 2003/06/02 23:32:08 millert Exp $";
+static const char rcsid[] = "$OpenBSD: ar_subs.c,v 1.25 2003/06/26 00:10:17 deraadt Exp $";
 #endif
 #endif /* not lint */
 
@@ -109,7 +109,7 @@ list(void)
 			 * we need to read, to get the real filename
 			 */
 			off_t cnt;
-			if (!(*frmt->rd_data)(arcn, -1, &cnt));
+			if (!(*frmt->rd_data)(arcn, -1, &cnt))
 				(void)rd_skip(cnt + arcn->pad);
 			continue;
 		}
@@ -201,7 +201,7 @@ extract(void)
 			/*
 			 * we need to read, to get the real filename
 			 */
-			if (!(*frmt->rd_data)(arcn, -1, &cnt));
+			if (!(*frmt->rd_data)(arcn, -1, &cnt))
 				(void)rd_skip(cnt + arcn->pad);
 			continue;
 		}
