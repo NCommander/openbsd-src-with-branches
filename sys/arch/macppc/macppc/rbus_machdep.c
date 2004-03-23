98cc7c767f5a6646e56b46dd7a3536f01abf9491
drahn <drahn>
2004-03-23 03:25:46 +0000

    Add TI 1510 to recognized list of chips to configure. ok deraadt@

--- sys/arch/macppc/macppc/rbus_machdep.c
+++ sys/arch/macppc/macppc/rbus_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rbus_machdep.c,v 1.4 2003/10/15 17:50:16 drahn Exp $ */
+/*	$OpenBSD: rbus_machdep.c,v 1.5 2003/10/25 12:35:32 drahn Exp $ */
 /*	$NetBSD: rbus_machdep.c,v 1.2 1999/10/15 06:43:06 haya Exp $	*/
 
 /*
@@ -141,7 +141,8 @@ macppc_cardbus_init(pci_chipset_tag_t pc, pcitag_t tag)
 	}
 
 	if (PCI_VENDOR(x) == PCI_VENDOR_TI &&
-	    PCI_PRODUCT(x) == PCI_PRODUCT_TI_PCI1410) {
+	    (PCI_PRODUCT(x) == PCI_PRODUCT_TI_PCI1410 ||
+	    PCI_PRODUCT(x) == PCI_PRODUCT_TI_PCI1510)) {
 		/* dont mess with the bus numbers or latency timer */
 
 		/* Route INTA to MFUNC0 */
