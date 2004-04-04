bdd4217825faac350f0304d0f32b31dfeb77db02
beck <beck>
2004-04-04 22:18:16 +0000

    Can't resist. <deraadt> don't they recognize their moral responsibility to entertain me?

--- usr.bin/mg/theo.c
+++ usr.bin/mg/theo.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: theo.c,v 1.59 2004/01/03 00:41:26 henning Exp $	*/
+/*	$OpenBSD: theo.c,v 1.60 2004/02/27 23:20:52 mickey Exp $	*/
 /*
  * Copyright (c) 2002 Artur Grabowski <art@openbsd.org>
  * All rights reserved.
@@ -144,6 +144,7 @@ static const char *talk[] = {
 	"germans are not allowed to get involved there",
 	"gprs sucks camel dick dryer than the gobi desert",
 	"I AM NEVER SATISFIED"
+	"don't they recognize their moral responsibility to entertain me?"
 };
 
 static const int ntalk = sizeof(talk)/sizeof(talk[0]);
