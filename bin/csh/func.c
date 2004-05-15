090a540f319b507d853ffe705f3a62b28b755129
tedu <tedu>
2004-05-15 01:49:35 +0000

    print the whole hint for bad limit modifiers. ok otto pedro

--- bin/csh/func.c
+++ bin/csh/func.c
@@ -1,4 +1,4 @@
-/*    $OpenBSD: func.c,v 1.19 2003/06/23 16:42:15 deraadt Exp $       */
+/*    $OpenBSD: func.c,v 1.20 2003/06/25 21:12:39 deraadt Exp $       */
 /*    $NetBSD: func.c,v 1.11 1996/02/09 02:28:29 christos Exp $       */
 
 /*-
@@ -34,7 +34,7 @@
 #if 0
 static char sccsid[] = "@(#)func.c	8.1 (Berkeley) 5/31/93";
 #else
-static char rcsid[] = "$OpenBSD: func.c,v 1.19 2003/06/23 16:42:15 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: func.c,v 1.20 2003/06/25 21:12:39 deraadt Exp $";
 #endif
 #endif /* not lint */
 
@@ -1215,10 +1215,12 @@ badscal:
 static void
 limtail(Char *cp, char *str)
 {
+    char *origstr = str;
+
     while (*cp && *cp == *str)
 	cp++, str++;
     if (*cp)
-	stderror(ERR_BADSCALE, str);
+	stderror(ERR_BADSCALE, origstr);
 }
 
 /*ARGSUSED*/
