c5dfb9f7d4f2563f477fdc5a124da8081c967a52
mickey <mickey>
2004-05-25 18:07:21 +0000

    return object* the symbol is in for _dl_find_symbol*; drahn@ ok

--- libexec/ld.so/dlfcn.c
+++ libexec/ld.so/dlfcn.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dlfcn.c,v 1.34 2004/05/22 19:42:52 drahn Exp $ */
+/*	$OpenBSD: dlfcn.c,v 1.35 2004/05/25 15:56:18 deraadt Exp $ */
 
 /*
  * Copyright (c) 1998 Per Fogelstrom, Opsycon AB
@@ -137,7 +137,7 @@ dlsym(void *handle, const char *name)
 		return(0);
 	}
 
-	retval = (void *)_dl_find_symbol(name, object, &sym,
+	retval = (void *)_dl_find_symbol(name, object, &sym, NULL,
 	    SYM_SEARCH_SELF|SYM_NOWARNNOTFOUND|SYM_NOTPLT, 0, object);
 	if (sym != NULL)
 		retval += sym->st_value;
