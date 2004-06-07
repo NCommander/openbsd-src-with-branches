85c71d4849aa6670f415e65425fb4e7be849980a
tedu <tedu>
2004-06-07 01:00:12 +0000

    p_pstat = SONPROC

--- sys/arch/sparc64/sparc64/locore.s
+++ sys/arch/sparc64/sparc64/locore.s
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: locore.s,v 1.9.4.8 2004/06/05 23:11:00 niklas Exp $	*/
 /*	$NetBSD: locore.s,v 1.137 2001/08/13 06:10:10 jdolecek Exp $	*/
 
 /*
@@ -5879,10 +5879,8 @@ Lsw_scan:
 	 * p->p_cpu = curcpu();
 	 */
 #endif	/* defined(MULTIPROCESSOR) */
-#ifdef notyet
 	mov	SONPROC, %o0			! p->p_stat = SONPROC
 	stb	%o0, [%l3 + P_STAT]
-#endif	/* notyet */
 	sethi	%hi(_C_LABEL(want_resched)), %o0
 	st	%g0, [%o0 + %lo(_C_LABEL(want_resched))]	! want_resched = 0;
 	ldx	[%l3 + P_ADDR], %l1		! newpcb = p->p_addr;
