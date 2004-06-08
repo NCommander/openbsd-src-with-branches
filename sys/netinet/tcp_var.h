265986577f5b4b89e98d7d1a4b7b236342e56cae
markus <markus>
2004-06-08 19:47:24 +0000

    factor out md5 code; ok+tests henning@, djm@, hshoexer@

--- sys/netinet/tcp_var.h
+++ sys/netinet/tcp_var.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tcp_var.h,v 1.62 2004/04/20 20:05:29 markus Exp $	*/
+/*	$OpenBSD: tcp_var.h,v 1.63 2004/04/25 04:34:05 markus Exp $	*/
 /*	$NetBSD: tcp_var.h,v 1.17 1996/02/13 23:44:24 christos Exp $	*/
 
 /*
@@ -618,6 +618,8 @@ u_long	 tcp_seq_subtract(u_long, u_long );
 #endif /* TCP_SACK */
 #ifdef TCP_SIGNATURE
 int	tcp_signature_apply(caddr_t, caddr_t, unsigned int);
+int	tcp_signature(struct tdb *, int, struct mbuf *, struct tcphdr *,
+	    int, int, char *);
 #endif /* TCP_SIGNATURE */
 void	tcp_rndiss_init(void);
 tcp_seq	tcp_rndiss_next(void);
