3fa230a933ee84c8b18b6ccd6784875ae84f232a
drahn <drahn>
2004-06-11 19:06:40 +0000

    Treat SONPROC as runnable for nrun (uptime) too. pointed out by millert@

--- sys/uvm/uvm_meter.c
+++ sys/uvm/uvm_meter.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_meter.c,v 1.4.2.8 2004/06/05 23:13:13 niklas Exp $	*/
+/*	$OpenBSD: uvm_meter.c,v 1.4.2.9 2004/06/11 19:03:23 drahn Exp $	*/
 /*	$NetBSD: uvm_meter.c,v 1.21 2001/07/14 06:36:03 matt Exp $	*/
 
 /*
@@ -110,6 +110,7 @@ uvm_loadav(avg)
 		/* fall through */
 		case SRUN:
 		case SIDL:
+		case SONPROC:
 			nrun++;
 		}
 	}
