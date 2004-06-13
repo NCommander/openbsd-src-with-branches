14538a18e2557c9f81999852b07a538e7f779642
djm <djm>
2004-06-13 15:03:02 +0000

    implement session multiplexing in the client (the server has supported this since 2.0); ok markus@

--- usr.bin/ssh/includes.h
+++ usr.bin/ssh/includes.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: includes.h,v 1.16 2001/12/19 15:43:11 stevesk Exp $	*/
+/*	$OpenBSD: includes.h,v 1.17 2002/01/26 16:44:22 stevesk Exp $	*/
 
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
@@ -38,6 +38,7 @@ static /**/const char *const rcsid[] = { (char *)rcsid, "\100(#)" msg }
 #include <arpa/inet.h>
 #include <netdb.h>
 
+#include <stddef.h>
 #include <netgroup.h>
 #include <stdio.h>
 #include <ctype.h>
