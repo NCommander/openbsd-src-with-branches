a5d9e0808325fbe5173fc88c7a0eaa5a2d2c73b5
otto <otto>
2004-07-02 15:48:36 +0000

    Take TMPDIR into account when creating a temp mountpoint for populating. ok avsm@ millert@; man page ok jmc@

--- sbin/newfs/pathnames.h
+++ sbin/newfs/pathnames.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: pathnames.h,v 1.1 1996/12/04 08:38:59 deraadt Exp $	*/
 /*	$NetBSD: pathnames.h,v 1.1 1996/09/11 20:27:15 christos Exp $	*/
 
 /*
@@ -32,3 +32,4 @@
 
 #define _PATH_SBIN	"/sbin"
 #define _PATH_USRSBIN	"/usr/sbin"
+#define _PATH_MNT	"/mnt"
