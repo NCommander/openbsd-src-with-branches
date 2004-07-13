33feef7c3552555d9be9c68aad73b018cabc773e
marc <marc>
2004-07-13 21:03:39 +0000

    errno changes, lib major version bumps, and general flag day To build you must: cd /usr/src && make obj && make includes cd lib/libc && make depend && make && NOMAN=1 sudo make install cd /usr/src && make build

--- lib/libc/gen/errno.c
+++ lib/libc/gen/errno.c
@@ -1,5 +1,22 @@
 #if defined(LIBC_SCCS) && !defined(lint)
-static char rcsid[] = "$OpenBSD$";
+static char rcsid[] = "$OpenBSD: errno.c,v 1.2 1996/08/19 08:22:34 tholo Exp $";
 #endif /* LIBC_SCCS and not lint */
 
+#include <errno.h>
+#undef errno
+
+/*
+ * global errno for unthreaded programs.
+ */
 int errno;
+
+/*
+ * weak version of function used by unthreaded programs.
+ */
+int *
+___errno(void)
+{
+	return &errno;
+}
+
+__weak_alias(__errno, ___errno);
