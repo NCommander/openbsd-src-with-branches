de24f6c9eaae10720747d90cf255cf53c7c50bcd
espie <espie>
2004-07-14 10:44:03 +0000

    Rework @dirrm: keep them all, and remove them at the end of pkg_delete. Add @dir, more powerful than @dirrm, so that we can get rid of those pesky @exec mkdir -p.
    okay naddy@

--- usr.sbin/pkg_add/OpenBSD/PackingList.pm
+++ usr.sbin/pkg_add/OpenBSD/PackingList.pm
@@ -1,4 +1,4 @@
-# $OpenBSD: PackingList.pm,v 1.9 2004/07/05 09:22:40 espie Exp $
+# $OpenBSD: PackingList.pm,v 1.10 2004/07/13 00:35:39 espie Exp $
 #
 # Copyright (c) 2003 Marc Espie.
 # 
@@ -73,7 +73,7 @@ sub DirrmOnly
 	my ($fh, $cont) = @_;
 	local $_;
 	while (<$fh>) {
-		next unless m/^\@cwd\b/ || m/^\@dirrm\b/ || m/^\@name\b/;
+		next unless m/^\@cwd\b/ || m/^\@dirrm\b/ || m/^\@dir\b/ || m/^\@name\b/;
 		&$cont($_);
 	}
 }
