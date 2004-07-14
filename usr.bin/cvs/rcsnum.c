899ac37fa281ec7ca59504d54d59f1091fdbdb73
vincent <vincent>
2004-07-14 05:08:20 +0000

    fix memleak on rcsnum reuse initialize new members to 0

--- usr.bin/cvs/rcsnum.c
+++ usr.bin/cvs/rcsnum.c
@@ -191,11 +191,14 @@ rcsnum_aton(const char *str, char **ep, RCSNUM *nump)
 	if (!isdigit(*str))
 		return (-1);
 
-	nump->rn_id = (u_int16_t *)malloc(sizeof(u_int16_t));
-	if (nump->rn_id == NULL)
-		return (-1);
+	if (nump->rn_id == NULL) {
+		nump->rn_id = (u_int16_t *)malloc(sizeof(u_int16_t));
+		if (nump->rn_id == NULL)
+			return (-1);
+	}
 
 	nump->rn_len = 0;
+	nump->rn_id[nump->rn_len] = 0;
 
 	for (sp = str; ; sp++) {
 		if (!isdigit(*sp) && (*sp != '.')) {
@@ -216,6 +219,7 @@ rcsnum_aton(const char *str, char **ep, RCSNUM *nump)
 				return (-1);
 			}
 			nump->rn_id = (u_int16_t *)tmp;
+			nump->rn_id[nump->rn_len] = 0;
 			continue;
 		}
 
