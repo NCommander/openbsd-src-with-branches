894dd76117457478093d0762058f03320a057e26
dtucker <dtucker>
2004-07-17 05:31:41 +0000

    Move "Last logged in at.." message generation to the monitor, right before recording the new login.  Fixes missing lastlog message when /var/log/lastlog is not world-readable and incorrect datestamp when multiple sessions are used (bz #463);  much assistance & ok markus@

--- usr.bin/ssh/sshd.c
+++ usr.bin/ssh/sshd.c
@@ -42,7 +42,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: sshd.c,v 1.297 2004/06/26 20:07:16 avsm Exp $");
+RCSID("$OpenBSD: sshd.c,v 1.298 2004/07/11 17:48:47 deraadt Exp $");
 
 #include <openssl/dh.h>
 #include <openssl/bn.h>
@@ -207,6 +207,9 @@ struct monitor *pmonitor = NULL;
 
 /* global authentication context */
 Authctxt *the_authctxt = NULL;
+
+/* message to be displayed after login */
+Buffer loginmsg;
 
 /* Prototypes for various functions defined later in this file. */
 void destroy_sensitive_data(void);
@@ -1603,6 +1606,9 @@ main(int ac, char **av)
 	if (use_privsep)
 		if (privsep_preauth(authctxt) == 1)
 			goto authenticated;
+
+	/* prepare buffer to collect messages to display to user after login */
+	buffer_init(&loginmsg);
 
 	/* perform the key exchange */
 	/* authenticate user and start session */
