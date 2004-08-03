587a202b930f3c5725682b6009f654e3ad3c6ad6
mjc <mjc>
2004-08-03 04:11:49 +0000

    make pstat -f show locking
    ok art@ tedu@
    much help from pedro@

--- usr.sbin/pstat/pstat.c
+++ usr.sbin/pstat/pstat.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pstat.c,v 1.45 2003/09/08 23:31:55 deraadt Exp $	*/
+/*	$OpenBSD: pstat.c,v 1.46 2004/03/16 23:11:20 jmc Exp $	*/
 /*	$NetBSD: pstat.c,v 1.27 1996/10/23 22:50:06 cgd Exp $	*/
 
 /*-
@@ -40,7 +40,7 @@ static char copyright[] =
 #if 0
 from: static char sccsid[] = "@(#)pstat.c	8.9 (Berkeley) 2/16/94";
 #else
-static char *rcsid = "$OpenBSD: pstat.c,v 1.45 2003/09/08 23:31:55 deraadt Exp $";
+static char *rcsid = "$OpenBSD: pstat.c,v 1.46 2004/03/16 23:11:20 jmc Exp $";
 #endif
 #endif /* not lint */
 
@@ -916,12 +916,8 @@ filemode(void)
 			*fbp++ = 'W';
 		if (fp.f_flag & FAPPEND)
 			*fbp++ = 'A';
-#ifdef FSHLOCK	/* currently gone */
-		if (fp.f_flag & FSHLOCK)
-			*fbp++ = 'S';
-		if (fp.f_flag & FEXLOCK)
-			*fbp++ = 'X';
-#endif
+		if (fp.f_flag & FHASLOCK)
+			*fbp++ = 'L';
 		if (fp.f_flag & FASYNC)
 			*fbp++ = 'I';
 		*fbp = '\0';
