560b9885c667052c487b2624d4aa442ec10bfc52
claudio <claudio>
2004-08-03 13:34:06 +0000

    Make the code more portable by #ifdef-ing RTF_MPATH. OK henning@

--- usr.sbin/bgpd/kroute.c
+++ usr.sbin/bgpd/kroute.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kroute.c,v 1.103 2004/07/05 16:54:53 henning Exp $ */
+/*	$OpenBSD: kroute.c,v 1.104 2004/07/13 17:57:20 jaredy Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -1166,8 +1166,10 @@ fetchtable(void)
 		if (rtm->rtm_flags & RTF_LLINFO)	/* arp cache */
 			continue;
 
+#ifdef RTF_MPATH
 		if (rtm->rtm_flags & RTF_MPATH)		/* multipath */
 			continue;
+#endif
 
 		if ((kr = calloc(1, sizeof(struct kroute_node))) == NULL) {
 			log_warn("fetchtable");
