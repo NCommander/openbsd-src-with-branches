a49c7b3e5f13d8f98d5356afe38bc87284400451
aoyama <aoyama>
2004-08-18 13:29:46 +0000

    Add 'irq6/clock0' to evcount on luna88k. ok miod@

--- sys/arch/luna88k/dev/timekeeper.c
+++ sys/arch/luna88k/dev/timekeeper.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: timekeeper.c,v 1.1.1.1 2004/04/21 15:23:56 aoyama Exp $ */
+/* $OpenBSD: timekeeper.c,v 1.2 2004/05/07 14:11:05 aoyama Exp $ */
 /* $NetBSD: timekeeper.c,v 1.1 2000/01/05 08:48:56 nisimura Exp $ */
 
 /*-
@@ -41,6 +41,7 @@
 #include <sys/systm.h>
 #include <sys/device.h>
 #include <sys/kernel.h>
+#include <sys/evcount.h>
 
 #include <machine/autoconf.h>
 #include <machine/board.h>	/* machtype value */
@@ -58,6 +59,7 @@ struct timekeeper_softc {
 	struct device sc_dev;
 	void *sc_clock, *sc_nvram;
 	int sc_nvramsize;
+	struct evcount sc_count;
 };
 
 /*
@@ -130,7 +132,10 @@ clock_attach(parent, self, aux)
 		printf(": DS1397\n");
 		break;
 	}
-	clockattach(&sc->sc_dev, clockwork);
+
+	evcount_attach(&sc->sc_count, self->dv_xname, (void *)&ma->ma_ilvl, &evcount_intr);
+
+	clockattach(&sc->sc_dev, clockwork, &sc->sc_count);
 }
 
 /*
