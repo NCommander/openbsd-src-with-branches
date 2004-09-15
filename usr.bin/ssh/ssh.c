94e53239ef8b5205db00332a75072a5f9c11bb56
deraadt <deraadt>
2004-09-15 00:46:01 +0000

    /* fallthrough */ is something a programmer understands.  But /* FALLTHROUGH */ is also understood by lint, so that is better.

--- usr.bin/ssh/ssh.c
+++ usr.bin/ssh/ssh.c
@@ -40,7 +40,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: ssh.c,v 1.225 2004/08/23 14:26:38 dtucker Exp $");
+RCSID("$OpenBSD: ssh.c,v 1.226 2004/09/07 23:41:30 djm Exp $");
 
 #include <openssl/evp.h>
 #include <openssl/err.h>
@@ -311,7 +311,7 @@ again:
 					options.log_level++;
 				break;
 			}
-			/* fallthrough */
+			/* FALLTHROUGH */
 		case 'V':
 			fprintf(stderr, "%s, %s\n",
 			    SSH_VERSION, SSLeay_version(SSLEAY_VERSION));
