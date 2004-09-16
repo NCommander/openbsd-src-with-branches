6a7568166ccc8f6225a9ab13e0ad6c453b97fa9b
henning <henning>
2004-09-16 04:29:54 +0000

    minor KNF

--- usr.sbin/bgpd/rde_decide.c
+++ usr.sbin/bgpd/rde_decide.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rde_decide.c,v 1.37 2004/08/05 18:44:19 claudio Exp $ */
+/*	$OpenBSD: rde_decide.c,v 1.38 2004/08/06 12:04:08 claudio Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Claudio Jeker <claudio@openbsd.org>
@@ -97,7 +97,6 @@ int	prefix_cmp(struct prefix *, struct prefix *);
  *  - withdraw of a prefix (prefix_remove)
  *  - state change of the nexthop (nexthop-{in}validate)
  *  - state change of session (session down)
- *
  */
 
 /*
@@ -173,7 +172,7 @@ prefix_cmp(struct prefix *p1, struct prefix *p2)
 
 	fatalx("Uh, oh a politician in the decision process");
 	/* NOTREACHED */
-	return 0;
+	return (0);
 }
 
 /*
