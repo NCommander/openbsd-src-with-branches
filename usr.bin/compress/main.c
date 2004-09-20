8598f556a87fcd3921b740241ed5f539b7b36ea0
millert <millert>
2004-09-20 18:53:20 +0000

    Make "gunzip foo.bar" work when there is a foo.bar.gz file. OK otto@

--- usr.bin/compress/main.c
+++ usr.bin/compress/main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: main.c,v 1.54 2004/02/29 13:59:15 markus Exp $	*/
+/*	$OpenBSD: main.c,v 1.55 2004/07/29 04:07:44 millert Exp $	*/
 
 #ifndef SMALL
 static const char copyright[] =
@@ -36,7 +36,7 @@ static const char license[] =
 #endif /* SMALL */
 
 #ifndef SMALL
-static const char main_rcsid[] = "$OpenBSD: main.c,v 1.54 2004/02/29 13:59:15 markus Exp $";
+static const char main_rcsid[] = "$OpenBSD: main.c,v 1.55 2004/07/29 04:07:44 millert Exp $";
 #endif
 
 #include <sys/param.h>
@@ -349,7 +349,8 @@ main(int argc, char *argv[])
 			 */
 			/* XXX - is overwriting fts_statp legal? (millert) */
 			if (entry->fts_errno == ENOENT &&
-			    strchr(entry->fts_accpath, '.') == NULL &&
+			    ((p = strrchr(entry->fts_accpath, '.')) == NULL ||
+			    strcmp(p, suffix) != 0) &&
 			    snprintf(_infile, sizeof(_infile), "%s%s", infile,
 			    suffix) < sizeof(_infile) &&
 			    stat(_infile, entry->fts_statp) == 0 &&
