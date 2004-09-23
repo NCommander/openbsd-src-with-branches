25bddc88afe0c6cab2cb6b0bd3629df10c3aa96a
henning <henning>
2004-09-23 02:05:29 +0000

    cosmetics

--- usr.sbin/bgpctl/bgpctl.c
+++ usr.sbin/bgpctl/bgpctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bgpctl.c,v 1.63 2004/09/16 22:36:18 henning Exp $ */
+/*	$OpenBSD: bgpctl.c,v 1.64 2004/09/21 18:29:47 claudio Exp $ */
 
 /*
  * Copyright (c) 2003 Henning Brauer <henning@openbsd.org>
@@ -328,7 +328,7 @@ show_summary_msg(struct imsg *imsg)
 		if (p->state == STATE_ESTABLISHED) {
 			printf("%6u", p->stats.prefix_cnt);
 			if (p->conf.max_prefix != 0)
-				printf("/%u", p->conf.max_prefix);
+				printf("/%6u", p->conf.max_prefix);
 		} else
 			printf("%s", statenames[p->state]);
 		printf("\n");
