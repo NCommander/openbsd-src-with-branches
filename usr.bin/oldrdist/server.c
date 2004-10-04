700f3106a58c2f35f2ea26731780acbe3d3a958c
jsg <jsg>
2004-10-04 05:21:27 +0000

    hierarchy is spelt hierarchy, ok djm@

--- usr.bin/oldrdist/server.c
+++ usr.bin/oldrdist/server.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: server.c,v 1.25 2004/01/15 19:12:31 otto Exp $	*/
+/*	$OpenBSD: server.c,v 1.26 2004/01/16 01:12:35 espie Exp $	*/
 
 /*
  * Copyright (c) 1983, 1993
@@ -31,7 +31,7 @@
 
 #ifndef lint
 /* from: static char sccsid[] = "@(#)server.c	8.1 (Berkeley) 6/9/93"; */
-static char *rcsid = "$OpenBSD: server.c,v 1.25 2004/01/15 19:12:31 otto Exp $";
+static char *rcsid = "$OpenBSD: server.c,v 1.26 2004/01/16 01:12:35 espie Exp $";
 #endif /* not lint */
 
 #include <sys/wait.h>
@@ -272,7 +272,7 @@ install(src, dest, destdir, opts)
 		tp++;
 	/*
 	 * If we are renaming a directory and we want to preserve
-	 * the directory heirarchy (-w), we must strip off the leading
+	 * the directory hierarchy (-w), we must strip off the leading
 	 * directory name and preserve the rest.
 	 */
 	if (opts & WHOLE) {
