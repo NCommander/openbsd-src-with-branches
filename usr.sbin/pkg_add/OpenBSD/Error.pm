f99423d4ebf564fd66ac90c8d14dc4046bb59501
espie <espie>
2004-10-23 09:27:48 +0000

    store pkgname in shared items, so that delayed printing can be used (and use it).
    Optimisation: don't clone() directories

--- usr.sbin/pkg_add/OpenBSD/Error.pm
+++ usr.sbin/pkg_add/OpenBSD/Error.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: Error.pm,v 1.2 2004/10/04 12:12:30 espie Exp $
+# $OpenBSD: Error.pm,v 1.3 2004/10/20 11:38:57 espie Exp $
 #
 # Copyright (c) 2004 Marc Espie <espie@openbsd.org>
 #
@@ -107,7 +107,7 @@ sub delayed_output
 	for my $pkg (sort keys %{$self->{messages}}) {
 		my $msgs = $self->{messages}->{$pkg};
 		if (@$msgs > 0) {
-			print "$pkg -------------------\n";
+			print "--- $pkg -------------------\n";
 			print @$msgs;
 		}
 	}
