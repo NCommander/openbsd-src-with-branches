fe8cf8b7be25b79860f7dfd576d444a8ea5be330
brad <brad>
2004-11-13 23:56:37 +0000

    MFC: Fix by otto@
    Do not ignore SIGCHLD, repairing hangs after a subshell is executed
    via ~! or ~C. While there, fix description of fd's available to the
    command executed by ~C in man page and comments. This has been wrong
    since at least 4.4BSD... Spotted by Hugo Villeneuve. PR 3971.
    
    ok deraadt@ otto@

--- usr.bin/tip/tip.c
+++ usr.bin/tip/tip.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tip.c,v 1.22 2003/10/15 22:33:18 deraadt Exp $	*/
+/*	$OpenBSD: tip.c,v 1.23 2004/05/26 18:17:59 deraadt Exp $	*/
 /*	$NetBSD: tip.c,v 1.13 1997/04/20 00:03:05 mellon Exp $	*/
 
 /*
@@ -40,7 +40,7 @@ static const char copyright[] =
 #if 0
 static char sccsid[] = "@(#)tip.c	8.1 (Berkeley) 6/6/93";
 #endif
-static const char rcsid[] = "$OpenBSD: tip.c,v 1.22 2003/10/15 22:33:18 deraadt Exp $";
+static const char rcsid[] = "$OpenBSD: tip.c,v 1.23 2004/05/26 18:17:59 deraadt Exp $";
 #endif /* not lint */
 
 /*
@@ -138,7 +138,7 @@ notnumber:
 	(void)signal(SIGQUIT, cleanup);
 	(void)signal(SIGHUP, cleanup);
 	(void)signal(SIGTERM, cleanup);
-	(void)signal(SIGCHLD, SIG_IGN);
+	(void)signal(SIGCHLD, SIG_DFL);
 
 	if ((i = hunt(system)) == 0) {
 		printf("all ports busy\n");
