815e847a9cfbcc513733eb342ba5ab3208fe8770
brad <brad>
2004-11-16 00:31:02 +0000

    add PCI_ID_CODE
    From NetBSD

--- sys/dev/pci/pcireg.h
+++ sys/dev/pci/pcireg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pcireg.h,v 1.25 2003/11/16 20:30:06 avsm Exp $	*/
+/*	$OpenBSD: pcireg.h,v 1.26 2003/12/06 14:40:33 grange Exp $	*/
 /*	$NetBSD: pcireg.h,v 1.26 2000/05/10 16:58:42 thorpej Exp $	*/
 
 /*
@@ -57,6 +57,10 @@ typedef u_int16_t pci_product_id_t;
 #define	PCI_PRODUCT_MASK			0xffff
 #define	PCI_PRODUCT(id) \
 	    (((id) >> PCI_PRODUCT_SHIFT) & PCI_PRODUCT_MASK)
+
+#define PCI_ID_CODE(vid,pid) \
+	((((vid) & PCI_VENDOR_MASK) << PCI_VENDOR_SHIFT) | \
+	 (((pid) & PCI_PRODUCT_MASK) << PCI_PRODUCT_SHIFT))
 
 /*
  * Command and status register.
