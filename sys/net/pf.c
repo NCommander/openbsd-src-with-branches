34c29f5f8aab7fe3d522f1078b3562d38b0948b4
brad <brad>
2004-12-16 02:05:39 +0000

    MFC: Fix by mcbride@
    Initialise init_addr in pf_map_addr() in the PF_POOL_ROUNDROBIN,
    prevents a possible endless loop in pf_get_sport() with 'static-port'
    
    Reported by adm at celeritystorm dot com in FreeBSD PR74930, debugging
    by dhartmei@
    
    ok mcbride@ dhartmei@ deraadt@ henning@

--- sys/net/pf.c
+++ sys/net/pf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf.c,v 1.433.2.3 2004/11/13 23:24:37 brad Exp $ */
+/*	$OpenBSD: pf.c,v 1.433.2.4 2004/11/28 19:55:33 brad Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -1887,6 +1887,8 @@ pf_map_addr(sa_family_t af, struct pf_rule *r, struct pf_addr *saddr,
 
 	get_addr:
 		PF_ACPY(naddr, &rpool->counter, af);
+		if (init_addr != NULL && PF_AZERO(init_addr, af))
+			PF_ACPY(init_addr, naddr, af);
 		PF_AINC(&rpool->counter, af);
 		break;
 	}
