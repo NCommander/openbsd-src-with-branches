2e2623d57dd35a09db67c39493d4a67d8f098c55
deraadt <deraadt>
2004-12-23 19:40:21 +0000

    change FXPF_UCODE to mean "firmware load attempted".  if the first loadfirmware() fails because the file is missing, we do not want to try again when the first softclock -> fxp_stats_update -> fxp_init happens later. calling namei in that context is really bad; tested by mcbride

--- sys/dev/ic/fxp.c
+++ sys/dev/ic/fxp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fxp.c,v 1.61 2004/12/08 22:35:13 pascoe Exp $	*/
+/*	$OpenBSD: fxp.c,v 1.62 2004/12/22 12:02:47 grange Exp $	*/
 /*	$NetBSD: if_fxp.c,v 1.2 1997/06/05 02:01:55 thorpej Exp $	*/
 
 /*
@@ -1873,6 +1873,7 @@ fxp_load_ucode(struct fxp_softc *sc)
 	if (error) {
 		printf("%s: failed loadfirmware of file %s: errno %d\n",
 		    sc->sc_dev.dv_xname, uc->uname, error);
+		sc->sc_flags |= FXPF_UCODE;
 		return;
 	}
 
