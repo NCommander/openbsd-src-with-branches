4db98db08cdcd32e172d4fb20543cba306b4ee6b
drahn <drahn>
2004-12-31 14:57:13 +0000

    These are now zaurus headers, not cats ifdef protect appropriately.

--- sys/arch/zaurus/include/spinlock.h
+++ sys/arch/zaurus/include/spinlock.h
@@ -1,6 +1,6 @@
 
-/*	$OpenBSD: spinlock.h,v 1.1 2004/02/01 05:12:54 drahn Exp $	*/
-#ifndef _CATS_SPINLOCK_H_
-#define _CATS_SPINLOCK_H_
+/*	$OpenBSD: spinlock.h,v 1.1 2004/12/31 00:04:35 drahn Exp $	*/
+#ifndef _ZAURUS_SPINLOCK_H_
+#define _ZAURUS_SPINLOCK_H_
 #include <arm/spinlock.h>
-#endif _CATS_SPINLOCK_H_
+#endif _ZAURUS_SPINLOCK_H_
