e48cdda8e490ea4092a296906e3167227165fe39
espie <espie>
2005-01-13 00:27:39 +0000

    let collision reports give the name of the package they're trying to install. When you add 2000 packages, this can be *real* confusing otherwise.

--- usr.sbin/pkg_add/OpenBSD/Add.pm
+++ usr.sbin/pkg_add/OpenBSD/Add.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: Add.pm,v 1.28 2004/12/18 13:58:43 espie Exp $
+# $OpenBSD: Add.pm,v 1.29 2004/12/20 12:16:51 espie Exp $
 #
 # Copyright (c) 2003-2004 Marc Espie <espie@openbsd.org>
 #
@@ -103,7 +103,7 @@ sub validate_plist($$)
 
 		OpenBSD::CollisionReport::collision_report($colliding, $state);
 	}
-	Fatal "fatal issues" if $problems;
+	Fatal "fatal issues in installing $pkgname" if $problems;
 	return $totsize;
 }
 
