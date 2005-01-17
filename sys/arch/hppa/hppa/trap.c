c49474a7b19ad71d45bc67a303a98e8fb967e167
mickey <mickey>
2005-01-17 20:47:40 +0000

    proper fault address selection (list all possible pc-based traps)

--- sys/arch/hppa/hppa/trap.c
+++ sys/arch/hppa/hppa/trap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: trap.c,v 1.82 2004/12/06 20:12:24 miod Exp $	*/
+/*	$OpenBSD: trap.c,v 1.83 2005/01/17 19:01:00 mickey Exp $	*/
 
 /*
  * Copyright (c) 1998-2004 Michael Shalayeff
@@ -183,8 +183,9 @@ trap(type, frame)
 
 	trapnum = type & ~T_USER;
 	opcode = frame->tf_iir;
-	if (trapnum == T_ITLBMISS ||
-	    trapnum == T_EXCEPTION || trapnum == T_EMULATION) {
+	if (trapnum <= T_EXCEPTION || trapnum == T_HIGHERPL ||
+	    trapnum == T_LOWERPL || trapnum == T_TAKENBR ||
+	    trapnum == T_IDEBUG || trapnum == T_PERFMON) {
 		va = frame->tf_iioq_head;
 		space = frame->tf_iisq_head;
 		vftype = UVM_PROT_EXEC;
