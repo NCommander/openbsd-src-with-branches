7068efce206461e22e9fb94f576082fd0204560e
danh <danh>
2005-01-27 16:25:06 +0000

    fix some vi-mode bugs spotted by jmc@
    tested by jmc@, ok otto@

--- bin/ksh/vi.c
+++ bin/ksh/vi.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vi.c,v 1.17 2004/12/20 11:34:26 otto Exp $	*/
+/*	$OpenBSD: vi.c,v 1.18 2004/12/22 17:14:34 millert Exp $	*/
 
 /*
  *	vi command editing
@@ -855,7 +855,7 @@ vi_cmd(int argcnt, const char *cmd)
 
 		case 'g':
 			if (!argcnt)
-				argcnt = hlast + 1;
+				argcnt = hlast;
 			/* fall through */
 		case 'G':
 			if (!argcnt)
@@ -909,8 +909,15 @@ vi_cmd(int argcnt, const char *cmd)
 			modified = 1; hnum = hlast;
 			if (cmd[1] == 0)
 				vi_error();
-			else
-				es->cbuf[es->cursor] = cmd[1];
+			else {
+				int	n;
+
+				if (es->cursor + argcnt > es->linelen)
+					return -1;
+				for (n = 0; n < argcnt; ++n)
+					es->cbuf[es->cursor + n] = cmd[1];
+				es->cursor += n - 1;
+			}
 			break;
 
 		case 'R':
@@ -929,7 +936,7 @@ vi_cmd(int argcnt, const char *cmd)
 			break;
 
 		case 'v':
-			if (es->linelen == 0)
+			if (es->linelen == 0 && argcnt == 0)
 				return -1;
 			if (!argcnt) {
 				if (modified) {
