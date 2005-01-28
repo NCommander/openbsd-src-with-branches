619bf74316d61c21215ee64cda00833d25fa7ba0
norby <norby>
2005-01-28 17:26:05 +0000

    Change my email address to the OpenBSD one since Ericsson has nothing to do with this software, just happend to be my only valid email address at the time.

--- usr.sbin/ospfctl/ospfctl.c
+++ usr.sbin/ospfctl/ospfctl.c
@@ -1,8 +1,8 @@
-/*	$OpenBSD$ */
+/*	$OpenBSD: ospfctl.c,v 1.1 2005/01/28 14:18:46 claudio Exp $ */
 
 /*
  * Copyright (c) 2005 Claudio Jeker <claudio@openbsd.org>
- * Copyright (c) 2004 Esben Norby <esben.norby@ericsson.com>
+ * Copyright (c) 2004 Esben Norby <norby@openbsd.org>
  * Copyright (c) 2003 Henning Brauer <henning@openbsd.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
