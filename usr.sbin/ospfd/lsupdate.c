bc19c17f619ff97da95cf4e3a79d5650d595c5b2
claudio <claudio>
2005-02-09 15:41:33 +0000

    If the router self is the originator of the LS update flood it out no matter what. This solves an issue where self originated updates where not flooded because the router was the DR.

--- usr.sbin/ospfd/lsupdate.c
+++ usr.sbin/ospfd/lsupdate.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: lsupdate.c,v 1.2 2005/01/28 17:53:33 norby Exp $ */
+/*	$OpenBSD: lsupdate.c,v 1.3 2005/02/02 19:15:07 henning Exp $ */
 
 /*
  * Copyright (c) 2005 Claudio Jeker <claudio@openbsd.org>
@@ -86,7 +86,7 @@ lsa_flood(struct iface *iface, struct nbr *originator, struct lsa_hdr *lsa_hdr,
 	if (!queued)
 		return (0);
 
-	if (iface == originator->iface) {
+	if (iface == originator->iface && iface->self != originator) {
 		if (iface->dr == originator || iface->bdr == originator)
 			return (0);
 		if (iface->state & IF_STA_BACKUP)
