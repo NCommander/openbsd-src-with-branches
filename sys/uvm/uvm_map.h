877b6c9b55aa91526fdcd4369567d17616540ddd
henning <henning>
2005-02-19 17:58:03 +0000

    double default MAX_KMAPENT to 2000, theo ok everybody please update your trees and test this, we need to find out wether there is bad side-effects from the doubling. If this does not get enough testing by our user community we will play safe and revert this for the 3.7 release, so please test.

--- sys/uvm/uvm_map.h
+++ sys/uvm/uvm_map.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_map.h,v 1.28 2002/10/29 18:30:21 art Exp $	*/
+/*	$OpenBSD: uvm_map.h,v 1.29 2003/04/14 04:53:51 art Exp $	*/
 /*	$NetBSD: uvm_map.h,v 1.24 2001/02/18 21:19:08 chs Exp $	*/
 
 /* 
@@ -246,10 +246,10 @@ struct vm_map {
 /* XXX: number of kernel maps and entries to statically allocate */
 
 #if !defined(MAX_KMAPENT)
-#if (50 + (2 * NPROC) > 1000)
+#if (50 + (2 * NPROC) > 2000)
 #define MAX_KMAPENT (50 + (2 * NPROC))
 #else
-#define	MAX_KMAPENT	1000  /* XXXCDC: no crash */
+#define	MAX_KMAPENT	2000  /* XXXCDC: no crash */
 #endif
 #endif	/* !defined MAX_KMAPENT */
 
