df04e11259fdaf372e417d8a633d5ccbd4b37b0e
joris <joris>
2005-02-28 20:45:07 +0000

    do not forget to call cvs_connect() when we are using a remote cvs server.
    ok jfb@

--- usr.bin/cvs/history.c
+++ usr.bin/cvs/history.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: history.c,v 1.6 2005/01/31 21:46:43 jfb Exp $	*/
+/*	$OpenBSD: history.c,v 1.7 2005/01/31 22:04:58 jfb Exp $	*/
 /*
  * Copyright (c) 2004 Jean-Francois Brousseau <jfb@openbsd.org>
  * All rights reserved.
@@ -166,6 +166,9 @@ cvs_history(int argc, char **argv)
 		}
 		cvs_hist_close(hp);
 	} else {
+		if (cvs_connect(root) < 0)
+			return (EX_PROTOCOL);
+
 		if ((flags & CVS_HF_C) && (cvs_sendarg(root, "-c", 0) < 0))
 			return (EX_PROTOCOL);
 
