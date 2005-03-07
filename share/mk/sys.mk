86a50322d9ef1e50d64a09975a475686c48fca33
deraadt <deraadt>
2005-03-07 00:06:00 +0000

    move to 3.7-beta

--- share/mk/sys.mk
+++ share/mk/sys.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: sys.mk,v 1.43 2004/12/11 16:29:05 miod Exp $
+#	$OpenBSD: sys.mk,v 1.44 2004/12/28 19:35:02 jolan Exp $
 #	$NetBSD: sys.mk,v 1.27 1996/04/10 05:47:19 mycroft Exp $
 #	@(#)sys.mk	5.11 (Berkeley) 3/13/91
 
@@ -10,7 +10,7 @@
 
 unix=		We run OpenBSD.
 OSMAJOR=	3
-OSMINOR=	6
+OSMINOR=	7
 OSREV=		$(OSMAJOR).$(OSMINOR)
 OSrev=		$(OSMAJOR)$(OSMINOR)
 
--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: param.h,v 1.58 2004/08/11 15:28:23 deraadt Exp $	*/
+/*	$OpenBSD: param.h,v 1.59 2004/11/28 01:36:38 mickey Exp $	*/
 /*	$NetBSD: param.h,v 1.23 1996/03/17 01:02:29 thorpej Exp $	*/
 
 /*-
@@ -41,8 +41,8 @@
 #define BSD4_3	1
 #define BSD4_4	1
 
-#define OpenBSD	200411		/* OpenBSD version (year & month). */
-#define OpenBSD3_6 1		/* OpenBSD 3.6 */
+#define OpenBSD	200501		/* OpenBSD version (year & month). */
+#define OpenBSD3_7 1		/* OpenBSD 3.7 */
 
 #ifndef NULL
 #ifdef 	__GNUG__
