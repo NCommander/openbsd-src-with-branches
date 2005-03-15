4d7583e112d1042c3474363d8bfb69cac63a9807
claudio <claudio>
2005-03-15 22:09:43 +0000

    bgpctl parts for "show fib". OK norby@ deraadt@

--- usr.sbin/ospfctl/parser.h
+++ usr.sbin/ospfctl/parser.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: parser.h,v 1.3 2005/03/12 11:03:05 norby Exp $ */
+/*	$OpenBSD: parser.h,v 1.4 2005/03/14 18:21:29 norby Exp $ */
 
 /*
  * Copyright (c) 2004 Esben Norby <norby@openbsd.org>
@@ -35,6 +35,8 @@ enum actions {
 	SHOW_DBBYAREA,
 	SHOW_RIB,
 	SHOW_RIB_DTAIL,
+	SHOW_FIB,
+	SHOW_FIB_IFACE,
 	RELOAD
 };
 
