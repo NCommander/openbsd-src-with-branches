1d23ff20b01a482b12a711a60c9d2f8ae09e6361
dlg <dlg>
2005-03-30 14:12:35 +0000

    restore all of the registers on resume to cope with power loss during suspend which leaves the controller totally unconfigured.
    help from and ok pascoe@

--- sys/dev/usb/ohcivar.h
+++ sys/dev/usb/ohcivar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ohcivar.h,v 1.18 2003/07/08 13:19:09 nate Exp $ */
+/*	$OpenBSD: ohcivar.h,v 1.19 2005/03/30 14:02:03 dlg Exp $ */
 /*	$NetBSD: ohcivar.h,v 1.32 2003/02/22 05:24:17 tsutsui Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/ohcivar.h,v 1.13 1999/11/17 22:33:41 n_hibma Exp $	*/
 
@@ -131,6 +131,7 @@ typedef struct ohci_softc {
 #endif
 	u_int32_t sc_control;		/* Preserved during suspend/standby */
 	u_int32_t sc_intre;
+	u_int32_t sc_ival;
 
 	u_int sc_overrun_cnt;
 	struct timeval sc_overrun_ntc;
