e5811532075a5a06ddbd245660eb54b01f9ec73c
joris <joris>
2005-04-12 19:35:32 +0000

    don't show unneeded error messages.

--- usr.bin/cvs/cvs.c
+++ usr.bin/cvs/cvs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cvs.c,v 1.51 2005/04/12 14:58:40 joris Exp $	*/
+/*	$OpenBSD: cvs.c,v 1.52 2005/04/12 15:18:13 xsa Exp $	*/
 /*
  * Copyright (c) 2004 Jean-Francois Brousseau <jfb@openbsd.org>
  * All rights reserved.
@@ -438,7 +438,6 @@ main(int argc, char **argv)
 		fprintf(stderr, "an operation on a file or directory failed");
 		break;
 	default:
-		fprintf(stderr, "unknown error");
 		break;
 	}
 
