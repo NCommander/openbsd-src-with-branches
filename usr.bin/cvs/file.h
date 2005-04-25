8ba7a0f93bc34c0b25267614ea8c0bc8c0f14a98
joris <joris>
2005-04-25 21:58:32 +0000

    first part of file API changes:
    - fix our behaviour regarding directories or files in a different
      directory than we currently are as arguments to commands, this
      allows stuff like "cvs status sys/arch/i386/Makefile" to work.
    
    - when parsing a pathname only load the needed nodes of the path
      instead of loading *everything* in memory.
    
    the next step in this is to merge the collection and callback
    execution together.
    
    ok jfb@, xsa@

--- usr.bin/cvs/file.h
+++ usr.bin/cvs/file.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: file.h,v 1.15 2005/03/24 14:35:18 jfb Exp $	*/
+/*	$OpenBSD: file.h,v 1.16 2005/04/18 21:02:50 jfb Exp $	*/
 /*
  * Copyright (c) 2004 Jean-Francois Brousseau <jfb@openbsd.org>
  * All rights reserved.
@@ -115,6 +115,8 @@ typedef struct cvs_file {
 #define CVS_DIRF_STATIC    0x01
 #define CVS_DIRF_STICKY    0x02
 #define CVS_DIRF_BASE      0x04
+
+#define CVS_GDIR_IGNORE    0x08
 
 #define CVS_DIR_ROOT(f)  ((((f)->cf_type == DT_DIR) && \
 	((f)->cf_root != NULL)) ? (f)->cf_root : \
