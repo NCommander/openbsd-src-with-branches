0c94d7052d4edee636d4e019a774a4ce6ac23472
brad <brad>
2005-05-09 05:37:36 +0000

    Make sure that prefixes get purged.
    Fixes PR 3939.
    
    From NetBSD
    
    ok itojun@

--- sys/netinet6/nd6.c
+++ sys/netinet6/nd6.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: nd6.c,v 1.67 2004/06/21 23:50:37 tholo Exp $	*/
+/*	$OpenBSD: nd6.c,v 1.68 2004/10/28 20:34:57 henning Exp $	*/
 /*	$KAME: nd6.c,v 1.280 2002/06/08 19:52:07 itojun Exp $	*/
 
 /*
@@ -622,6 +622,13 @@ nd6_purge(ifp)
 	for (pr = nd_prefix.lh_first; pr; pr = npr) {
 		npr = pr->ndpr_next;
 		if (pr->ndpr_ifp == ifp) {
+			/*
+			 * Because if_detach() does *not* release prefixes
+			 * while purging addresses the reference count will
+			 * still be above zero. We therefore reset it to
+			 * make sure that the prefix really gets purged.
+			 */
+			pr->ndpr_refcnt = 0;
 			/*
 			 * Previously, pr->ndpr_addr is removed as well,
 			 * but I strongly believe we don't have to do it.
