dc04ffda60487c683a7cffd91f88b6cad7f4ae8e
henning <henning>
2005-06-06 17:13:56 +0000

    add support for a "family" token, allows to select address families

--- usr.sbin/bgpctl/parser.h
+++ usr.sbin/bgpctl/parser.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: parser.h,v 1.7 2004/12/23 17:26:51 henning Exp $ */
+/*	$OpenBSD: parser.h,v 1.8 2005/05/23 20:09:00 claudio Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -52,6 +52,7 @@ struct parse_result {
 	int			flags;
 	u_int8_t		prefixlen;
 	struct filter_set_head	set;
+	sa_family_t		af;
 };
 
 struct parse_result	*parse(int, char *[]);
