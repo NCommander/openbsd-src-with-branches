5b2e39fe29f20f2856b8e6024bd2fc6e458c650d
henning <henning>
2005-06-08 06:35:04 +0000

    no more netns handling for the various tunnel devices and loopback

--- sys/net/if_tun.c
+++ sys/net/if_tun.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_tun.c,v 1.65 2005/05/03 14:33:50 brad Exp $	*/
+/*	$OpenBSD: if_tun.c,v 1.66 2005/05/04 12:10:27 markus Exp $	*/
 /*	$NetBSD: if_tun.c,v 1.24 1996/05/07 02:40:48 thorpej Exp $	*/
 
 /*
@@ -69,11 +69,6 @@
 #include <netinet/in_var.h>
 #include <netinet/ip.h>
 #include <netinet/if_ether.h>
-#endif
-
-#ifdef NS
-#include <netns/ns.h>
-#include <netns/ns_if.h>
 #endif
 
 #ifdef IPX
