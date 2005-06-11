74e02852820f7a478827331efe31df4d106afa54
brad <brad>
2005-06-11 02:58:49 +0000

    MFC: Fixes by marco@, niklas@, mickey@ and beck@
    rev 1.87
    one last optimization, removing a jump, mostly by
    toby, committed so art will stop yelling at me to
    avoid conflicts...
    
    rev 1.86
    repair whichqs race by disabling ints in between check and halt-halt
    
    rev 1.85
    replace jnz FOO;jmp BAR;FOO: with jz BAR;FOO:, the idle loop will be even more efficient
    
    rev 1.84
    hlt hlt is not a good idea.  Rework APM to do what the spec actually says.
    Run actual BIOS calls whenever we are actually idle. Tovarisch!
    
    ok deraadt@ marco@ beck@

--- sys/arch/i386/i386/locore.s
+++ sys/arch/i386/i386/locore.s
@@ -1,4 +1,4 @@
-/*	$OpenBSD: locore.s,v 1.80 2004/07/20 20:16:44 art Exp $	*/
+/*	$OpenBSD: locore.s,v 1.80.2.1 2005/02/27 00:57:02 brad Exp $	*/
 /*	$NetBSD: locore.s,v 1.145 1996/05/03 19:41:19 christos Exp $	*/
 
 /*-
@@ -1597,38 +1597,31 @@ ENTRY(idle)
 	call	_C_LABEL(Xspllower)	# process pending interrupts
 
 ENTRY(idle_loop)
+	cli
 	cmpl	$0,_C_LABEL(whichqs)
 	jnz	_C_LABEL(idle_exit)
-#if NAPM > 0
-	call	_C_LABEL(apm_cpu_idle)
-	cmpl	$0,_C_LABEL(apm_dobusy)
-	je	1f
-	call	_C_LABEL(apm_cpu_busy)
-1:
-#endif
 #if NPCTR > 0 && NAPM == 0
 	addl	$1,_C_LABEL(pctr_idlcnt)
 	adcl	$0,_C_LABEL(pctr_idlcnt)+4
+#endif
+#if NAPM > 0
+	call	_C_LABEL(apm_cpu_idle)
 #else
+	sti
 	hlt
 #endif
 	jmp	_C_LABEL(idle_loop)
 
 ENTRY(idle_exit)
 	movl	$IPL_HIGH,CPL		# splhigh
+	sti
 #if defined(MULTIPROCESSOR) || defined(LOCKDEBUG)	
 	call	_C_LABEL(sched_lock_idle)
 #endif
-#if 0
-	GET_CPUINFO(%ebx)
-	leal	CPU_INFO_NAME(%ebx),%ebx
-	pushl	%ebx
-	pushl	$1f
-	call	_C_LABEL(printf)
-	addl	$8,%esp
+#if NAPM > 0
+	call	_C_LABEL(apm_cpu_busy)
 #endif
 	jmp	switch_search
-1:	.asciz	"%s: unidle\n"
 		
 #ifdef DIAGNOSTIC
 NENTRY(switch_error)
