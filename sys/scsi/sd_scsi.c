80f995ddb21f903daa15b905e50bf8fbbc186996
krw <krw>
2005-06-23 00:31:44 +0000

    Don't discard the blocksize information provided by the READ CAPACITY command. Return it in a new parameter to scsi_size.

--- sys/scsi/sd_scsi.c
+++ sys/scsi/sd_scsi.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sd_scsi.c,v 1.16 2005/06/15 03:06:47 krw Exp $	*/
+/*	$OpenBSD: sd_scsi.c,v 1.17 2005/06/22 02:49:36 krw Exp $	*/
 /*	$NetBSD: sd_scsi.c,v 1.8 1998/10/08 20:21:13 thorpej Exp $	*/
 
 /*-
@@ -142,7 +142,7 @@ sd_scsibus_get_parms(sd, dp, flags)
 	u_int16_t rpm = 0;
 	int page, error;
 
-	dp->disksize = scsi_size(sd->sc_link, flags);
+	dp->disksize = scsi_size(sd->sc_link, flags, &blksize);
 	dp->rot_rate = 3600;
 
 	/*
