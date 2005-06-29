4c462a241c91eba4013febbb4657b23abf26134a
claudio <claudio>
2005-06-29 09:43:26 +0000

    rtlabel support via filter sets. Just use "set rtlabel foobar" in filters network and neighbor statements and the routes are labeled accordingly. While doing that fix some mem-leaks by introducing filterset_free() and remove the free on send option of send_filterset(). This took a bit longer because we need to carefully track the rtlabel id refcnts or bad things may happen on reloads. henning@ looks fine

--- usr.sbin/bgpd/printconf.c
+++ usr.sbin/bgpd/printconf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: printconf.c,v 1.41 2005/04/28 13:54:45 claudio Exp $	*/
+/*	$OpenBSD: printconf.c,v 1.42 2005/06/09 15:32:03 claudio Exp $	*/
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -125,6 +125,13 @@ print_set(struct filter_set_head *set)
 			break;
 		case ACTION_PFTABLE:
 			printf("pftable %s ", s->action.pftable);
+			break;
+		case ACTION_RTLABEL:
+			printf("rtlabel %s ", s->action.rtlabel);
+			break;
+		case ACTION_RTLABEL_ID:
+			/* not possible */
+			printf("rtlabel id %d ", s->action.id);
 			break;
 		}
 	}
