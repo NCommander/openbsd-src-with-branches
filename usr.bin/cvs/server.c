826a2eabf910a4067530255570c15d2568772433
xsa <xsa>
2005-07-27 10:36:14 +0000

    rename cvs_remove_dir() to cvs_rmdir() because it might be confusing with the `remove' command local functions. ok joris@.

--- usr.bin/cvs/server.c
+++ usr.bin/cvs/server.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: server.c,v 1.18 2005/05/31 08:58:48 xsa Exp $	*/
+/*	$OpenBSD: server.c,v 1.19 2005/07/23 10:59:47 xsa Exp $	*/
 /*
  * Copyright (c) 2004 Jean-Francois Brousseau <jfb@openbsd.org>
  * All rights reserved.
@@ -126,7 +126,7 @@ cvs_server(int argc, char **argv)
 	}
 
 	/* cleanup the temporary tree */
-	ret = cvs_remove_dir(cvs_server_tmpdir);
+	ret = cvs_rmdir(cvs_server_tmpdir);
 
 	return (ret);
 }
