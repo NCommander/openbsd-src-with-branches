7ff9cfbba55f804dfe30db27c09eeb02771e8ae8
reyk <reyk>
2005-07-30 17:16:09 +0000

    flush print buffer on empty newlines

--- usr.sbin/hostapd/hostapd.c
+++ usr.sbin/hostapd/hostapd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: hostapd.c,v 1.14 2005/07/04 17:51:44 reyk Exp $	*/
+/*	$OpenBSD: hostapd.c,v 1.15 2005/07/04 18:48:05 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2004, 2005 Reyk Floeter <reyk@vantronix.net>
@@ -105,6 +105,9 @@ hostapd_printf(const char *fmt, ...)
 	if (vsnprintf(printbuf, sizeof(printbuf), newfmt, ap) == -1)
 		goto flush;
 	va_end(ap);
+
+	if (fmt[0] == '\n')
+		goto flush;
 }
 
 void
