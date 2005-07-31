094ed429a5df31e5de9050f6943f355c3462530e
pascoe <pascoe>
2005-07-31 03:52:19 +0000

    Introduce bpf_mtap_af and bpf_mtap_hdr to be used when passing a mbuf chain to bpf with either an address family or other header added.
    These helpers only allocate a much smaller struct m_hdr on the stack when
    needed, rather than leaving 256 byte struct mbufs on the stack in deep
    call paths.  Also removes a fair bit of duplicated code.
    
    commit now, tune after deraadt@

--- sys/netinet/ip_gre.c
+++ sys/netinet/ip_gre.c
@@ -1,4 +1,4 @@
-/*      $OpenBSD: ip_gre.c,v 1.25 2004/06/26 21:37:09 pb Exp $ */
+/*      $OpenBSD: ip_gre.c,v 1.26 2005/06/08 06:16:42 henning Exp $ */
 /*	$NetBSD: ip_gre.c,v 1.9 1999/10/25 19:18:11 drochner Exp $ */
 
 /*
@@ -192,23 +192,8 @@ gre_input2(m , hlen, proto)
 	m_adj(m, hlen);
 
 #if NBPFILTER > 0
-        if (sc->sc_if.if_bpf) {
-                /*
-                 * We need to prepend the address family as
-                 * a four byte field.  Cons up a fake header
-                 * to pacify bpf.  This is safe because bpf
-                 * will only read from the mbuf (i.e., it won't
-                 * try to free it or keep a pointer a to it).
-                 */
-                struct mbuf m0;
-
-		m0.m_flags = 0;
-                m0.m_next = m;
-                m0.m_len = 4;
-                m0.m_data = (char *) &af;
-
-                bpf_mtap(sc->sc_if.if_bpf, &m0);
-        }
+        if (sc->sc_if.if_bpf)
+		bpf_mtap_af(sc->sc_if.if_bpf, af, m);
 #endif
 
 	s = splimp();		/* possible */
@@ -333,24 +318,8 @@ gre_mobile_input(struct mbuf *m, ...)
 	ifq = &ipintrq;
 
 #if NBPFILTER > 0
-        if (sc->sc_if.if_bpf) {
-                /*
-                 * We need to prepend the address family as
-                 * a four byte field.  Cons up a fake header
-                 * to pacify bpf.  This is safe because bpf
-                 * will only read from the mbuf (i.e., it won't
-                 * try to free it or keep a pointer a to it).
-                 */
-                struct mbuf m0;
-		u_int af = AF_INET;
-
-		m0.m_flags = 0;
-                m0.m_next = m;
-                m0.m_len = 4;
-                m0.m_data = (char *) &af;
-
-                bpf_mtap(sc->sc_if.if_bpf, &m0);
-        }
+        if (sc->sc_if.if_bpf)
+		bpf_mtap_af(sc->sc_if.if_bpf, AF_INET, m);
 #endif
 
 	s = splimp();       /* possible */
