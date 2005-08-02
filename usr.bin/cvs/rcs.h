e2f6ad76664742574ec38870000cd85d5bdffdb0
joris <joris>
2005-08-02 11:48:56 +0000

    handle magic branch numbers.
    ok xsa@, jfb@

--- usr.bin/cvs/rcs.h
+++ usr.bin/cvs/rcs.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rcs.h,v 1.29 2005/05/26 01:45:54 jfb Exp $	*/
+/*	$OpenBSD: rcs.h,v 1.30 2005/07/25 11:28:59 xsa Exp $	*/
 /*
  * Copyright (c) 2004 Jean-Francois Brousseau <jfb@openbsd.org>
  * All rights reserved.
@@ -49,6 +49,7 @@
 #define RCS_SYM_INVALCHAR	"$,.:;@"
 
 
+#define RCS_MAGIC_BRANCH	".0."
 #define RCS_STATE_EXP		"Exp"
 #define RCS_STATE_DEAD		"dead"
 
