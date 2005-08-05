a870b492c56f06330495381055ccff31ea04028d
deraadt <deraadt>
2005-08-05 03:36:28 +0000

    typo

--- sys/arch/hp300/hp300/locore.s
+++ sys/arch/hp300/hp300/locore.s
@@ -1,4 +1,4 @@
-/*	$OpenBSD: locore.s,v 1.47 2005/01/14 22:39:27 miod Exp $	*/
+/*	$OpenBSD: locore.s,v 1.48 2005/01/18 13:32:56 miod Exp $	*/
 /*	$NetBSD: locore.s,v 1.91 1998/11/11 06:41:25 thorpej Exp $	*/
 
 /*
@@ -1926,7 +1926,7 @@ Lspldone:
 /*
  * _delay(u_int N)
  *
- * Delay for at least (N/256) microsecends.
+ * Delay for at least (N/256) microseconds.
  * This routine depends on the variable:  delay_divisor
  * which should be set based on the CPU clock rate.
  */
