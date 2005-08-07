a8cf10814ce2574e90985b4d718b4875ac4aee36
pascoe <pascoe>
2005-08-07 11:54:02 +0000

    Do not blindly reset the state count to zero after a clear, as we may not have purged all states in the case when an interface name was specified. pf_purge_expired_states should decrease the count as appropriate.
    ok dhartmei

--- sys/net/pf_ioctl.c
+++ sys/net/pf_ioctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf_ioctl.c,v 1.152 2005/08/05 09:03:19 dhartmei Exp $ */
+/*	$OpenBSD: pf_ioctl.c,v 1.153 2005/08/07 11:37:33 dhartmei Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -1696,7 +1696,6 @@ pfioctl(dev_t dev, u_long cmd, caddr_t addr, int flags, struct proc *p)
 			}
 		}
 		pf_purge_expired_states();
-		pf_status.states = 0;
 		psk->psk_af = killed;
 #if NPFSYNC
 		pfsync_clear_states(pf_status.hostid, psk->psk_ifname);
