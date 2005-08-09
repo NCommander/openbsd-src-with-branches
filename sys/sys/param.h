b347c932b97d942900f4ffc5a4270d2eed2cb7ab
deraadt <deraadt>
2005-08-09 00:46:15 +0000

    move to 3.8-beta

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: param.h,v 1.61 2005/03/11 22:07:32 deraadt Exp $	*/
+/*	$OpenBSD: param.h,v 1.62 2005/06/07 16:36:21 deraadt Exp $	*/
 /*	$NetBSD: param.h,v 1.23 1996/03/17 01:02:29 thorpej Exp $	*/
 
 /*-
@@ -41,8 +41,8 @@
 #define BSD4_3	1
 #define BSD4_4	1
 
-#define OpenBSD	200505		/* OpenBSD version (year & month). */
-#define OpenBSD3_7 1		/* OpenBSD 3.7 */
+#define OpenBSD	200511		/* OpenBSD version (year & month). */
+#define OpenBSD3_8 1		/* OpenBSD 3.8 */
 
 #ifndef NULL
 #ifdef 	__GNUG__
