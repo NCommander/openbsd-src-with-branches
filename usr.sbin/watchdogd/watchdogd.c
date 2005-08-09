49a6835e9a4ce9550912109293340ea6bb1d6987
mickey <mickey>
2005-08-09 15:09:06 +0000

    link static and mlockall() once daemonised to prevent sudden pageins and wdog misfire; discussed a lot at wtf; mbalmer@ art@ ok

--- usr.sbin/watchdogd/watchdogd.c
+++ usr.sbin/watchdogd/watchdogd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: watchdogd.c,v 1.2 2005/08/08 12:48:06 henning Exp $ */
+/*	$OpenBSD: watchdogd.c,v 1.3 2005/08/08 13:08:09 mbalmer Exp $ */
 
 /*
  * Copyright (c) 2005 Marc Balmer <marc@msys.ch>
@@ -132,6 +132,8 @@ main(int argc, char *argv[])
 		warn("can't daemonize, restoring original values");
 		goto restore;
 	}
+
+	(void)mlockall(MCL_CURRENT | MCL_FUTURE);
 
 	signal(SIGTERM, sighdlr);
 
