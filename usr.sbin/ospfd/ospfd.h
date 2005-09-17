4ffa3f7964b8f6e354fa74f28195d3d6a8ee873a
msf <msf>
2005-09-17 20:03:35 +0000

    - add get_rtr_id() based on bgpd's get_bgpid() - remove rtr_id from struct iface we don't need to keep a copy of this value   per interface - replace all references to iface->rtr_id.s_addr with calls to ospfe_rtr_id()
    ok claudio@ norby@

--- usr.sbin/ospfd/ospfd.h
+++ usr.sbin/ospfd/ospfd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ospfd.h,v 1.34 2005/05/27 02:07:13 norby Exp $ */
+/*	$OpenBSD: ospfd.h,v 1.35 2005/06/26 19:22:12 claudio Exp $ */
 
 /*
  * Copyright (c) 2004 Esben Norby <norby@openbsd.org>
@@ -302,7 +302,6 @@ struct iface {
 	struct in_addr		 addr;
 	struct in_addr		 dst;
 	struct in_addr		 mask;
-	struct in_addr		 rtr_id;
 	struct in_addr		 abr_id;
 	char			*auth_key;
 	struct nbr		*dr;	/* designated router */
