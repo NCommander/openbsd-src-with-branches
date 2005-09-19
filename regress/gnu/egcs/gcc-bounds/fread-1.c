9649177dd8ac80e68a78dcb1d3ff9e61558abe6e
cvs-fast-export <cvs-fast-export>
2005-09-19 03:34:18 +0000

    Synthetic commit for incomplete tag dlsym_test3

--- regress/gnu/egcs/gcc-bounds/fread-1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char **argv) {
-	char buf[100];
-	FILE *fp;
-	fread(buf, sizeof(char), sizeof(buf), fp);
-	return 1;
-}
--- sys/arch/cats/isa/isa_machdep.h
+++
@@ -1,3 +0,0 @@
-/*	$OpenBSD: isa_machdep.h,v 1.3 2004/01/29 20:06:24 drahn Exp $	*/
-/* stub until this file is moved */
-#include <machine/isa_machdep.h>
