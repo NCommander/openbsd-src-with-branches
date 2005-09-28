6d26e5e4e1d36e676236362280b0c26b6a875e27
drahn <drahn>
2005-09-28 15:41:06 +0000

    keep track of opencounts for dlopened objects, so that they get unloaded the right number of times on exit.

--- libexec/ld.so/dlfcn.c
+++ libexec/ld.so/dlfcn.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dlfcn.c,v 1.54 2005/09/22 22:33:40 drahn Exp $ */
+/*	$OpenBSD: dlfcn.c,v 1.55 2005/09/28 15:24:22 kurt Exp $ */
 
 /*
  * Copyright (c) 1998 Per Fogelstrom, Opsycon AB
@@ -145,6 +145,9 @@ dlopen(const char *libname, int flags)
 	}
 
 loaded:
+	if (failed == 0)
+		object->opencount++;
+
 	if (_dl_debug_map->r_brk) {
 		_dl_debug_map->r_state = RT_ADD;
 		(*((void (*)(void))_dl_debug_map->r_brk))();
@@ -279,10 +282,10 @@ _dl_real_close(void *handle)
 		return (1);
 	}
 
-
 	_dl_notify_unload_shlib(object);
 	_dl_run_all_dtors();
-	_dl_unlink_dlopen(object);
+	if(--object->opencount == 0)
+		_dl_unlink_dlopen(object);
 	_dl_unload_shlib(object);
 	return (0);
 }
--- libexec/ld.so/resolve.c
+++ libexec/ld.so/resolve.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: resolve.c,v 1.33 2005/09/22 01:33:07 drahn Exp $ */
+/*	$OpenBSD: resolve.c,v 1.34 2005/09/22 04:07:11 deraadt Exp $ */
 
 /*
  * Copyright (c) 1998 Per Fogelstrom, Opsycon AB
@@ -155,6 +155,7 @@ _dl_finalize_object(const char *objname, Elf_Dyn *dynp, const long *dl_data,
 	    _dl_loading_object->load_name ));
 	object->refcount = 1;
 	TAILQ_INIT(&object->child_list);
+	object->opencount = 0;	/* # dlopen() */
 	/* default dev, inode for dlopen-able objects. */
 	object->dev = 0;
 	object->inode = 0;
