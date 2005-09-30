cadadc5758b954c947b5ed88bc7a891c0a2f80d2
cvs-fast-export <cvs-fast-export>
2005-09-30 14:51:52 +0000

    Synthetic commit for incomplete tag dlclose_test2

--- sys/arch/cats/include/elf_abi.h
+++
@@ -1,3 +0,0 @@
-/*	$OpenBSD: elf_abi.h,v 1.1 2001/09/01 15:49:06 drahn Exp $	*/
-
-#include <arm/elf_abi.h>
--- sys/arch/zaurus/include/spinlock.h
+++
@@ -1,6 +0,0 @@
-
-/*	$OpenBSD: spinlock.h,v 1.1 2004/12/31 00:04:35 drahn Exp $	*/
-#ifndef _ZAURUS_SPINLOCK_H_
-#define _ZAURUS_SPINLOCK_H_
-#include <arm/spinlock.h>
-#endif _ZAURUS_SPINLOCK_H_
