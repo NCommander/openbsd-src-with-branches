41e62fa49f18f166d9910f0922eead943ea4367c
drahn <drahn>
2005-10-09 14:52:12 +0000

    Subtle changes to the powerpc fpu handling, basically fpuproc is handled in a non-raceable manner inside save_fpu and enable_fpu so that the eventual SMP case will not grab a half loaded fpu context.

--- sys/arch/macppc/macppc/machdep.c
+++ sys/arch/macppc/macppc/machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: machdep.c,v 1.75 2005/10/09 14:01:11 drahn Exp $	*/
+/*	$OpenBSD: machdep.c,v 1.76 2005/10/09 14:17:32 drahn Exp $	*/
 /*	$NetBSD: machdep.c,v 1.4 1996/10/16 19:33:11 ws Exp $	*/
 
 /*
@@ -93,7 +93,6 @@
  */
 struct pcb *curpcb;
 struct pmap *curpm;
-struct proc *fpuproc;
 
 extern struct user *proc0paddr;
 struct pool ppc_vecpl;
