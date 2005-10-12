7bff32d177c5db84a7dae2f45bd42d8e59edaff5
miod <miod>
2005-10-12 20:53:22 +0000

    Define IPL_SCHED and spslsched(), matching the statclock level.

--- sys/arch/luna88k/include/intr.h
+++ sys/arch/luna88k/include/intr.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: intr.h,v 1.2 2004/04/26 12:34:05 miod Exp $	*/
+/*	$OpenBSD: intr.h,v 1.3 2004/12/24 22:50:30 miod Exp $	*/
 /*
  * Copyright (C) 2000 Steve Murphree, Jr.
  * All rights reserved.
@@ -43,6 +43,7 @@
 #define IPL_VM		5
 #define IPL_CLOCK	6
 #define IPL_STATCLOCK	6
+#define	IPL_SCHED	6
 #define IPL_HIGH	7
 #define IPL_NMI		7
 #define IPL_ABORT	7
--- sys/arch/m88k/include/intr.h
+++ sys/arch/m88k/include/intr.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: intr.h,v 1.3 2005/05/01 09:55:49 miod Exp $	*/
+/*	$OpenBSD: intr.h,v 1.4 2005/09/06 19:28:22 miod Exp $	*/
 /*
  * Copyright (C) 2000 Steve Murphree, Jr.
  * All rights reserved.
@@ -55,7 +55,6 @@ void splassert_check(int, const char *);
 
 #endif /* _LOCORE */
 
-#define splnone			spl0
 #define splsoftclock()		raiseipl(IPL_SOFTCLOCK)
 #define splsoftnet()		raiseipl(IPL_SOFTNET)
 #define splbio()		raiseipl(IPL_BIO)
@@ -63,6 +62,7 @@ void splassert_check(int, const char *);
 #define spltty()		raiseipl(IPL_TTY)
 #define splclock()		raiseipl(IPL_CLOCK)
 #define splstatclock()		raiseipl(IPL_STATCLOCK)
+#define	splsched()		raiseipl(IPL_SCHED)
 #define splimp()		raiseipl(IPL_IMP)
 #define splvm()			raiseipl(IPL_VM)
 #define splhigh()		setipl(IPL_HIGH)
