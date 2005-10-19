1223d013399a24f25e0cf206a2e053c047bd74db
xsa <xsa>
2005-10-19 18:28:13 +0000

    sync usage();

--- usr.bin/rcs/rcsclean.c
+++ usr.bin/rcs/rcsclean.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rcsclean.c,v 1.8 2005/10/18 01:22:14 joris Exp $	*/
+/*	$OpenBSD: rcsclean.c,v 1.9 2005/10/19 16:15:36 xsa Exp $	*/
 /*
  * Copyright (c) 2005 Joris Vink <joris@openbsd.org>
  * All rights reserved.
@@ -109,7 +109,7 @@ void
 rcsclean_usage(void)
 {
 	fprintf(stderr,
-	    "usage: rcsclean [-qV] [-r rev] [file] ...\n");
+	    "usage: rcsclean [-nqV] [-kmode] [-rrev] [file] ...\n");
 }
 
 static int
