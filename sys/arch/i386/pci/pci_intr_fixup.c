71d4ee4dd5046c63cc60cfa1302c54c2a05c5ab7
mickey <mickey>
2005-10-20 15:09:31 +0000

    add 8237 support and repair 8231 that was confused very bad; lots of testing help by henning@

--- sys/arch/i386/pci/pci_intr_fixup.c
+++ sys/arch/i386/pci/pci_intr_fixup.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pci_intr_fixup.c,v 1.40 2005/10/15 11:42:56 brad Exp $	*/
+/*	$OpenBSD: pci_intr_fixup.c,v 1.41 2005/10/17 19:17:24 mickey Exp $	*/
 /*	$NetBSD: pci_intr_fixup.c,v 1.10 2000/08/10 21:18:27 soda Exp $	*/
 
 /*
@@ -181,6 +181,8 @@ const struct pciintr_icu_table {
 	  via82c586_init },
 
 	{ PCI_VENDOR_VIATECH,	PCI_PRODUCT_VIATECH_VT8231_ISA,
+	  via8231_init },
+	{ PCI_VENDOR_VIATECH,	PCI_PRODUCT_VIATECH_VT8237_ISA,
 	  via8231_init },
 
 	{ PCI_VENDOR_SIS,	PCI_PRODUCT_SIS_85C503,
