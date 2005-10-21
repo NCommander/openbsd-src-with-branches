a68531344e1078953acef149299f00ccfdbe004e
martin <martin>
2005-10-21 11:10:06 +0000

    remove two unused and deprecated Mach macros, alpha_round_page() and alpha_trunc_page()
    looks ok to Miod

--- sys/arch/alpha/include/param.h
+++ sys/arch/alpha/include/param.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: param.h,v 1.25 2004/08/06 22:31:30 mickey Exp $ */
+/* $OpenBSD: param.h,v 1.26 2005/07/31 15:31:08 miod Exp $ */
 /* $NetBSD: param.h,v 1.30 2000/06/09 16:03:04 thorpej Exp $ */
 
 /*
@@ -139,8 +139,6 @@
 /*
  * Mach derived conversion macros
  */
-#define	alpha_round_page(x)	((((unsigned long)(x)) + NBPG - 1) & ~(NBPG-1))
-#define	alpha_trunc_page(x)	((unsigned long)(x) & ~(NBPG-1))
 #define	alpha_btop(x)		((unsigned long)(x) >> PGSHIFT)
 #define	alpha_ptob(x)		((unsigned long)(x) << PGSHIFT)
 
