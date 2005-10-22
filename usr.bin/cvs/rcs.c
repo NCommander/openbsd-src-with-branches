a63da299a9b4662aa7517e7b9f5a95f8ed9e8136
joris <joris>
2005-10-22 17:23:21 +0000

    - don't check for admin files when running init; - don't pass the Entry for newly added files to cvs_date_parse() - in rcs_rev_add don't bother looking for the previous rev if we   are creating the RCS file.
    from PR 4575

--- usr.bin/cvs/rcs.c
+++ usr.bin/cvs/rcs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rcs.c,v 1.91 2005/10/16 17:44:15 joris Exp $	*/
+/*	$OpenBSD: rcs.c,v 1.92 2005/10/18 01:08:13 joris Exp $	*/
 /*
  * Copyright (c) 2004 Jean-Francois Brousseau <jfb@openbsd.org>
  * All rights reserved.
@@ -1457,11 +1457,13 @@ rcs_rev_add(RCSFILE *rf, RCSNUM *rev, const char *msg, time_t date,
 			return (-1);
 		}
 
-		ordp = NULL;
-		rcsnum_cpy(rev, old, 0);
-		while (ordp == NULL) {
-			old = rcsnum_dec(old);
-			ordp = rcs_findrev(rf, old);
+		if (!(rf->rf_flags & RCS_CREATE)) {
+			ordp = NULL;
+			rcsnum_cpy(rev, old, 0);
+			while (ordp == NULL) {
+				old = rcsnum_dec(old);
+				ordp = rcs_findrev(rf, old);
+			}
 		}
 	}
 
