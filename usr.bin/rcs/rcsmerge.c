5e66e9cc29b2ec70f61b49a7e528c038c94b82e5
niallo <niallo>
2005-10-23 11:42:43 +0000

    add extern int diff3_conflicts to unbreak. bad joris.

--- usr.bin/rcs/rcsmerge.c
+++ usr.bin/rcs/rcsmerge.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rcsmerge.c,v 1.3 2005/10/23 04:25:34 joris Exp $	*/
+/*	$OpenBSD: rcsmerge.c,v 1.4 2005/10/23 11:04:26 xsa Exp $	*/
 /*
  * Copyright (c) 2005 Xavier Santolaria <xsa@openbsd.org>
  * All rights reserved.
@@ -37,6 +37,7 @@
 #include "diff.h"
 #include "rcsprog.h"
 
+extern diff3_conflicts;
 
 static int kflag = RCS_KWEXP_ERR;
 
