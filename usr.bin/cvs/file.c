1c1f028f63c4e6e0d8e401e403e90c4f2932c14c
joris <joris>
2005-10-27 00:24:24 +0000

    if cvs_file_loadinfo fails, return failure to the caller instead of acting like everything went fine.
    this fixes eternal loops in the client which waits for data
    from the server while the server waits for data from the client.
    
    problem found and fixed by Nuno Morgadinho, thanks!

--- usr.bin/cvs/file.c
+++ usr.bin/cvs/file.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: file.c,v 1.124 2005/10/07 21:47:32 reyk Exp $	*/
+/*	$OpenBSD: file.c,v 1.125 2005/10/16 00:29:17 joris Exp $	*/
 /*
  * Copyright (c) 2004 Jean-Francois Brousseau <jfb@openbsd.org>
  * All rights reserved.
@@ -451,7 +451,7 @@ cvs_file_getspec(char **fspec, int fsn, int flags, int (*cb)(CVSFILE *, void *),
 		cf = cvs_file_loadinfo(pcopy, flags, cb, arg, freecf);
 		if (cf == NULL) {
 			if (cvs_error != CVS_EX_OK)
-				break;
+				return (-1);
 			continue;
 		}
 
