492a1cc30d40c51625aa20a99f597a868b6c43bc
mcbride <mcbride>
2005-10-27 12:34:40 +0000

    Basic support for attaching states from pfsync to the correct rules. Applies only to rules in the main ruleset (not anchors) if the ruleset checksum matches. Necessary to fix the following for pfsync'd states: 	- per-rule limits on number of states 	- altq 	- rule-based settings such as timeouts
    More work to do re: nat rules, src-nodes, etc.
    
    NOTE: This is modifies the pfsync header and version number.
    Tools which process pfsync packets must be recompiled, and firewalls with
    different versions will not sync.
    
    ok mpf@ henning@ dhartmei@

--- sys/net/pfvar.h
+++ sys/net/pfvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pfvar.h,v 1.230 2005/09/28 01:46:33 pascoe Exp $ */
+/*	$OpenBSD: pfvar.h,v 1.231 2005/10/17 08:43:35 henning Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -741,6 +741,8 @@ struct pf_ruleset {
 		struct pf_rulequeue	 queues[2];
 		struct {
 			struct pf_rulequeue	*ptr;
+			struct pf_rule		**ptr_array;
+			u_int32_t		 rcount;
 			u_int32_t		 ticket;
 			int			 open;
 		}			 active, inactive;
