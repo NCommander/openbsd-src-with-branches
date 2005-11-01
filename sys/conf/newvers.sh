c0ca4506ea15f1d7f6f68618839034bfbebbc139
brad <brad>
2005-11-01 01:07:32 +0000

    and here comes -stable

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.70 2005/08/09 00:46:15 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.71 2005/08/27 16:47:04 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -67,8 +67,9 @@ ost="OpenBSD"
 osr="3.8"
 
 cat >vers.c <<eof
-#define STATUS ""			/* release */
+#define STATUS "-stable"
 #if 0
+#define STATUS ""			/* release */
 #define STATUS "-beta"
 #define STATUS "-current"
 #endif
