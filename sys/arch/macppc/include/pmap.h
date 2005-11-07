642a16972f1aaeb91bda538d97fc5fbbcf41e90b
brad <brad>
2005-11-07 02:19:46 +0000

    remove vtophys() on *ppc.
    ok drahn@

--- sys/arch/macppc/include/pmap.h
+++ sys/arch/macppc/include/pmap.h
@@ -1,7 +1,3 @@
-/*	$OpenBSD: pmap.h,v 1.3 2001/09/10 17:48:09 drahn Exp $	*/
+/*	$OpenBSD: pmap.h,v 1.4 2002/03/14 01:26:36 millert Exp $	*/
 
 #include <powerpc/pmap.h>
-
-#ifndef	_LOCORE
-paddr_t vtophys(vaddr_t);
-#endif	/* _LOCORE */
