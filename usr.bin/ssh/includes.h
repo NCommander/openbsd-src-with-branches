c6c4d278660bff56c4e807d68a0d433a3b556562
millert <millert>
2005-11-15 11:59:54 +0000

    Include sys/queue.h explicitly instead of assuming some other header will pull it in.  At the moment it gets pulled in by sys/select.h (which ssh has no business including) via event.h.  OK markus@

--- usr.bin/ssh/includes.h
+++ usr.bin/ssh/includes.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: includes.h,v 1.18 2004/06/13 15:03:02 djm Exp $	*/
+/*	$OpenBSD: includes.h,v 1.19 2005/05/19 02:42:26 djm Exp $	*/
 
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
@@ -21,7 +21,7 @@ static /**/const char *const rcsid[] = { (const char *)rcsid, "\100(#)" msg }
 
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/select.h>
+#include <sys/queue.h>
 #include <sys/param.h>
 #include <sys/ioctl.h>
 #include <sys/endian.h>
