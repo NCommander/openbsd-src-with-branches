bea410a52cf3609e55062a6f73ad0937e98a2135
kjell <kjell>
2005-11-19 20:16:35 +0000

    Clean up some lint.

--- usr.bin/mg/tty.c
+++ usr.bin/mg/tty.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tty.c,v 1.21 2005/04/03 02:09:28 db Exp $	*/
+/*	$OpenBSD: tty.c,v 1.22 2005/06/14 18:14:40 kjell Exp $	*/
 
 /* This file is in the public domain. */
 
@@ -43,6 +43,7 @@ static char	*scroll_fwd;	/* How to scroll forward. */
 
 static void	 winchhandler(int);
 
+/* ARGSUSED */
 static void
 winchhandler(int sig)
 {
