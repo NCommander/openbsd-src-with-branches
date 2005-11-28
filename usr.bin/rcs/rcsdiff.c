78331c9bd7f637bec4b3342e1c7efd8a18203425
xsa <xsa>
2005-11-28 10:17:20 +0000

    -T is here for compatibility;

--- usr.bin/rcs/rcsdiff.c
+++ usr.bin/rcs/rcsdiff.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rcsdiff.c,v 1.19 2005/11/23 09:39:20 xsa Exp $	*/
+/*	$OpenBSD: rcsdiff.c,v 1.20 2005/11/25 15:56:10 xsa Exp $	*/
 /*
  * Copyright (c) 2005 Joris Vink <joris@openbsd.org>
  * All rights reserved.
@@ -97,7 +97,9 @@ rcsdiff_main(int argc, char **argv)
 			}
 			break;
 		case 'T':
-			flags |= PRESERVETIME;
+			/*
+			 * kept for compatibility
+			 */
 			break;
 		case 'V':
 			printf("%s\n", rcs_version);
