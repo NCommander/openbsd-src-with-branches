d589f98ca2dbd4a93544be0dd8ce4b06e15eda1a
deraadt <deraadt>
2005-12-01 22:09:59 +0000

    fix #endif crud

--- sys/arch/zaurus/include/spinlock.h
+++ sys/arch/zaurus/include/spinlock.h
@@ -1,6 +1,7 @@
 
-/*	$OpenBSD: spinlock.h,v 1.1 2004/12/31 00:04:35 drahn Exp $	*/
+/*	$OpenBSD: spinlock.h,v 1.2 2004/12/31 14:57:13 drahn Exp $	*/
 #ifndef _ZAURUS_SPINLOCK_H_
 #define _ZAURUS_SPINLOCK_H_
 #include <arm/spinlock.h>
-#endif _ZAURUS_SPINLOCK_H_
+#endif /* _ZAURUS_SPINLOCK_H_ */
+
