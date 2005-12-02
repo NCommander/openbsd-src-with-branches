a50b87995c42311fad18f0286b31d62d4c1689bc
claudio <claudio>
2005-12-02 12:21:31 +0000

    Fixed version of r1.35, readd candidate to list in both cases so that the list remains sorted. Tested and OK norby@

--- usr.sbin/ospfd/rde_spf.c
+++ usr.sbin/ospfd/rde_spf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rde_spf.c,v 1.36 2005/11/12 18:18:24 deraadt Exp $ */
+/*	$OpenBSD: rde_spf.c,v 1.37 2005/11/14 11:48:05 norby Exp $ */
 
 /*
  * Copyright (c) 2005 Esben Norby <norby@openbsd.org>
@@ -172,14 +172,19 @@ spf_calc(struct area *area)
 					w->cost = d;
 					w->prev = v;
 					calc_next_hop(w, v);
+					/*
+					 * need to readd to candidate list
+					 * because the list is sorted
+					 */
+					TAILQ_REMOVE(&cand_list, w, cand);
+					cand_list_add(w);
 				}
 			} else if (w->cost == LS_INFINITY && d < LS_INFINITY) {
 				w->cost = d;
-
-				cand_list_add(w);
 				w->prev = v;
 
 				calc_next_hop(w, v);
+				cand_list_add(w);
 			}
 		}
 
