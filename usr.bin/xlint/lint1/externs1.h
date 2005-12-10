178cead9d584dd2cf4999a507fe1eeee9020ba7f
cloder <cloder>
2005-12-10 17:41:03 +0000

    Back out all gcc attribute parsing changes until we can do this the right way some day.  The effect of __attribute__ on a LALR C grammar is just too ugly to fix quicklly.

--- usr.bin/xlint/lint1/externs1.h
+++ usr.bin/xlint/lint1/externs1.h
@@ -72,7 +72,6 @@ extern	pos_t	csrc_pos;
 extern	symt_t	symtyp;
 extern	FILE	*yyin;
 extern	u_quad_t qbmasks[], qlmasks[], qumasks[];
-attr_t	getkwattr(int, int);
 
 extern	void	initscan(void);
 extern	int	sign(quad_t, tspec_t, int);
@@ -259,17 +258,6 @@ extern	void	printflike(int);
 extern	void	scanflike(int);
 extern	void	protolib(int);
 extern	void	longlong(int);
-
-/*
- * attr.c
- */
-extern	attr_t		getattr(const char *);
-extern	attr_t		getqualattr(tqual_t);
-extern	attrnode_t	*newattrnode(attr_t);
-extern	void		appendattr(attrnode_t*, attr_t);
-extern	void		appendattrnode(attrnode_t*, attrnode_t*);
-extern	void		addattr(type_t *, attrnode_t*);
-extern	int		hasattr(type_t *, attr_t);
 
 /*
  * init.c
