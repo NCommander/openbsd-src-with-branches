875bee68e74c8e4a8000687e00599874a7f21a2a
reyk <reyk>
2005-12-10 13:42:37 +0000

    add an option to increase the multicast ttl which is currently limited to 1 hop. by using a higher multicast ttl, you could use inter-network multicast forwarding of hostapd messages (i.e. with mrouted(8) or multicast-capable routing switches); "set iapp mode multicast ttl 2".

--- usr.sbin/hostapd/hostapd.h
+++ usr.sbin/hostapd/hostapd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: hostapd.h,v 1.13 2005/12/01 01:28:19 reyk Exp $	*/
+/*	$OpenBSD: hostapd.h,v 1.14 2005/12/01 02:03:58 reyk Exp $	*/
 
 /*
  * Copyright (c) 2004, 2005 Reyk Floeter <reyk@vantronix.net>
@@ -263,6 +263,7 @@ struct hostapd_iapp {
 	struct sockaddr_in		i_addr;
 	struct sockaddr_in		i_broadcast;
 	struct sockaddr_in		i_multicast;
+	u_int8_t			i_ttl;
 	u_int8_t			i_flags;
 
 #define HOSTAPD_IAPP_F_ADD_NOTIFY	0x01
