8fc3e3e8164e9a66a4787f20ebcde064de8fb6ae
jsg <jsg>
2005-12-13 00:18:19 +0000

    Remove the advertising clause in the UCB license which Berkeley rescinded 22 July 1999. Checked by ian@, deraadt@ and millert@, arm portion checked by drahn@

--- sys/arch/amd64/amd64/machdep.c
+++ sys/arch/amd64/amd64/machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: machdep.c,v 1.39 2005/11/13 17:51:52 fgsch Exp $	*/
+/*	$OpenBSD: machdep.c,v 1.40 2005/11/13 21:44:06 fgsch Exp $	*/
 /*	$NetBSD: machdep.c,v 1.3 2003/05/07 22:58:18 fvdl Exp $	*/
 
 /*-
@@ -53,11 +53,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
