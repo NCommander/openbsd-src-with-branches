b88d16624dfc6bbad609e6b4649de629c19f2cfc
stevesk <stevesk>
2005-12-17 21:13:05 +0000

    spelling: fowarding, fowarded

--- usr.bin/ssh/session.c
+++ usr.bin/ssh/session.c
@@ -33,7 +33,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: session.c,v 1.188 2005/10/30 08:52:17 djm Exp $");
+RCSID("$OpenBSD: session.c,v 1.189 2005/12/12 13:46:18 markus Exp $");
 
 #include "ssh.h"
 #include "ssh1.h"
@@ -1505,7 +1505,7 @@ session_x11_req(Session *s)
 
 	if (s->auth_proto != NULL || s->auth_data != NULL) {
 		error("session_x11_req: session %d: "
-		    "x11 fowarding already active", s->self);
+		    "x11 forwarding already active", s->self);
 		return 0;
 	}
 	s->single_connection = packet_get_char();
