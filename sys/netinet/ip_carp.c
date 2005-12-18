87ef4414ed5a96617639b478035ed5dd4869da98
brad <brad>
2005-12-18 04:24:05 +0000

    MFC: Fix by mpf@
    Completly remove transition path INIT -> MASTER.
    A bug introduced in -r 1.4 led lower priotorized hosts
    switching to MASTER state for a short time at bootup,
    if preemption was enabled.
    
    ok deraadt@ mpf@

--- sys/netinet/ip_carp.c
+++ sys/netinet/ip_carp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_carp.c,v 1.103 2005/03/13 18:26:29 pat Exp $	*/
+/*	$OpenBSD: ip_carp.c,v 1.104 2005/03/15 15:51:27 mcbride Exp $	*/
 
 /*
  * Copyright (c) 2002 Michael Shalayeff. All rights reserved.
@@ -1320,19 +1320,9 @@ carp_setrun(struct carp_softc *sc, sa_family_t af)
 
 	switch (sc->sc_state) {
 	case INIT:
-		if (carp_opts[CARPCTL_PREEMPT] && !carp_suppress_preempt) {
-			carp_set_state(sc, MASTER);
-			carp_setroute(sc, RTM_ADD);
-			carp_send_ad(sc);
-			carp_send_arp(sc);
-#ifdef INET6
-			carp_send_na(sc);
-#endif /* INET6 */
-		} else {
-			carp_set_state(sc, BACKUP);
-			carp_setroute(sc, RTM_DELETE);
-			carp_setrun(sc, 0);
-		}
+		carp_set_state(sc, BACKUP);
+		carp_setroute(sc, RTM_DELETE);
+		carp_setrun(sc, 0);
 		break;
 	case BACKUP:
 		timeout_del(&sc->sc_ad_tmo);
