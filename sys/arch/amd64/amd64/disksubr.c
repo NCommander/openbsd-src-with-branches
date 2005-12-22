ef407bd3e37b19802f0805e02f01f6cf3a3c062f
krw <krw>
2005-12-22 03:02:48 +0000

    'spoofonly' does not mean what I thought it meant. The whole point is to read non-OpenBSD label information like MBRs and populate ('spoof') the OpenBSD disk label appropriately. So avoiding an I/O when doing 'spoofonly' is to miss the whole point.
    Backout all Sept. 26 and subsequent changes to disksubr.c files. The
    original CD problem with I/O errors on blank media will be addressed
    another way.
    
    Should fix the ext3 vs amd64 problems discussed on misc@. Thanks to
    steven@ for finally putting it terms that I understood.
    
    ok deraadt@

--- sys/arch/amd64/amd64/disksubr.c
+++ sys/arch/amd64/amd64/disksubr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: disksubr.c,v 1.4 2005/09/26 22:52:50 krw Exp $	*/
+/*	$OpenBSD: disksubr.c,v 1.5 2005/12/13 00:18:19 jsg Exp $	*/
 /*	$NetBSD: disksubr.c,v 1.21 1996/05/03 19:42:03 christos Exp $	*/
 
 /*
@@ -121,11 +121,6 @@ readdisklabel(dev, strat, lp, osdep, spoofonly)
 			wander = 0;
 			if (part_blkno < extoff)
 				part_blkno = extoff;
-
-			if (spoofonly) {
-				bzero(dp, NDOSPART * sizeof(*dp));
-				goto donot;
-			}
 
 			/* read boot record */
 			bp->b_blkno = part_blkno;
