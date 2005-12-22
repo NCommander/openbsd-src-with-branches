9bb9654396755afd29c28bd072115891e43aa920
brad <brad>
2005-12-22 04:20:17 +0000

    MFC: Fixes by mickey@
    rev 1.9
    
    make printf debug-only so some people would just shuddup
    
    rev 1.10
    
    only poll if scsi_poll was requested
    
    ok deraadt@ marco@ mickey@

--- sys/dev/ic/ciss.c
+++ sys/dev/ic/ciss.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ciss.c,v 1.5 2005/08/03 00:09:38 mickey Exp $	*/
+/*	$OpenBSD: ciss.c,v 1.6 2005/08/20 18:01:35 mickey Exp $	*/
 
 /*
  * Copyright (c) 2005 Michael Shalayeff
@@ -48,12 +48,14 @@
 #define	CISS_D_MISC	0x0004
 #define	CISS_D_DMA	0x0008
 #define	CISS_D_IOCTL	0x0010
+#define	CISS_D_ERR	0x0020
 int ciss_debug = 0
 	| CISS_D_CMD
 	| CISS_D_INTR
 	| CISS_D_MISC
 	| CISS_D_DMA
 	| CISS_D_IOCTL
+	| CISS_D_ERR
 	;
 #else
 #define	CISS_DPRINTF(m,a)	/* m, a */
@@ -469,7 +471,7 @@ ciss_cmd(struct ciss_ccb *ccb, int flags, int wait)
 	CISS_DPRINTF(CISS_D_CMD, ("submit=0x%x ", cmd->id));
 	bus_space_write_4(sc->iot, sc->ioh, CISS_INQ, ccb->ccb_cmdpa);
 
-	if (wait) {
+	if (wait && SCSI_POLL) {
 		struct timeval tv;
 		int etick;
 		CISS_DPRINTF(CISS_D_CMD, ("waiting "));
@@ -626,8 +628,9 @@ ciss_error(struct ciss_ccb *ccb)
 				break;
 
 			default:
-				printf("%s: cmd_stat %x scsi_stat 0x%x\n",
-				    sc->sc_dev.dv_xname, rv, err->scsi_stat);
+				CISS_DPRINTF(CISS_D_ERR, ("%s: "
+				    "cmd_stat %x scsi_stat 0x%x\n",
+				    sc->sc_dev.dv_xname, rv, err->scsi_stat));
 				xs->error = XS_DRIVER_STUFFUP;
 				break;
 			}
