0b92283d02dc2ecb6d256686fe754c679f1d03b0
miod <miod>
2005-12-27 18:35:34 +0000

    tprintf() is now only necessary for NFS, so do not compile it for kernels which like NFSSERVER and NFSCLIENT.

--- sys/kern/subr_prf.c
+++ sys/kern/subr_prf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: subr_prf.c,v 1.62 2005/09/27 12:26:13 grunk Exp $	*/
+/*	$OpenBSD: subr_prf.c,v 1.63 2005/12/09 09:09:52 jsg Exp $	*/
 /*	$NetBSD: subr_prf.c,v 1.45 1997/10/24 18:14:25 chuck Exp $	*/
 
 /*-
@@ -375,6 +375,8 @@ uprintf(const char *fmt, ...)
 	}
 }
 
+#if defined(NFSSERVER) || defined(NFSCLIENT)
+
 /*
  * tprintf functions: used to send messages to a specific process
  *
@@ -437,6 +439,8 @@ tprintf(tpr_t tpr, const char *fmt, ...)
 	va_end(ap);
 	logwakeup();
 }
+
+#endif	/* NFSSERVER || NFSCLIENT */
 
 
 /*
