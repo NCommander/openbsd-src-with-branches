74f3b399cd6e9ee55d286f9213e470318a0a90f8
aaron <aaron>
2006-01-17 00:56:04 +0000

    Fix malloc calculation to avoid memory bounds error.  deraadt@ ok

--- usr.bin/cdio/cddb.c
+++ usr.bin/cdio/cddb.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: cddb.c,v 1.8 2003/06/09 11:33:14 espie Exp $ */
+/* $OpenBSD: cddb.c,v 1.9 2005/11/12 15:26:23 deraadt Exp $ */
 /*
  * Copyright (c) 2002 Marc Espie.
  *
@@ -326,7 +326,7 @@ cddb(const char *host_port, int n, struct cd_toc_entry *e, char *arg)
 		}
 	} else if (strcmp(line, "200") != 0 || !further_query(cout, type))
 		goto end;
-	result = malloc(sizeof(char *) * n+1);
+	result = malloc(sizeof(char *) * (n + 1));
 	if (!result)
 		goto end;
 	for (i = 0; i <= n; i++)
