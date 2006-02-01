84d57f9c99ba75e097f0a374957556a544408728
otto <otto>
2006-02-01 19:10:12 +0000

    double semicolon; from Daniel Matic in PR 4929

--- sys/dev/pci/if_ixgb.c
+++ sys/dev/pci/if_ixgb.c
@@ -31,7 +31,7 @@ POSSIBILITY OF SUCH DAMAGE.
 
 ***************************************************************************/
 
-/* $OpenBSD: if_ixgb.c,v 1.2 2005/11/15 18:17:06 brad Exp $ */
+/* $OpenBSD: if_ixgb.c,v 1.3 2005/12/10 19:19:40 brad Exp $ */
 
 #include <dev/pci/if_ixgb.h>
 
@@ -454,7 +454,7 @@ ixgb_watchdog(struct ifnet * ifp)
 	ixgb_stop(sc);
 	ixgb_init(sc);
 
-	sc->watchdog_events++;;
+	sc->watchdog_events++;
 }
 
 /*********************************************************************
