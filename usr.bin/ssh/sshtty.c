75865b542640a6ae229d7517db2acde872c0b7b0
stevesk <stevesk>
2006-02-07 01:52:50 +0000

    "log.h" not needed

--- usr.bin/ssh/sshtty.c
+++ usr.bin/ssh/sshtty.c
@@ -35,12 +35,11 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: sshtty.c,v 1.6 2004/05/08 00:01:37 deraadt Exp $");
+RCSID("$OpenBSD: sshtty.c,v 1.7 2006/02/07 01:42:00 stevesk Exp $");
 
 #include <termios.h>
 
 #include "sshpty.h"
-#include "log.h"
 
 static struct termios _saved_tio;
 static int _in_raw_mode = 0;
