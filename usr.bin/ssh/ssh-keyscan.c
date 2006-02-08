da2720a92d0107ae29fcbdf62e28af71362ea82a
stevesk <stevesk>
2006-02-08 14:31:30 +0000

    move #include <sys/resource.h> out of includes.h; ok markus@

--- usr.bin/ssh/ssh-keyscan.c
+++ usr.bin/ssh/ssh-keyscan.c
@@ -7,9 +7,10 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: ssh-keyscan.c,v 1.57 2005/10/30 04:01:03 djm Exp $");
+RCSID("$OpenBSD: ssh-keyscan.c,v 1.58 2006/02/07 01:18:09 stevesk Exp $");
 
 #include <sys/queue.h>
+#include <sys/resource.h>
 
 #include <errno.h>
 #include <setjmp.h>
