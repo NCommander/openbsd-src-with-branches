8bb6dbf9ce58e17154fe5959a3bfce7ea5cde646
miod <miod>
2006-02-14 18:52:17 +0000

    Define a wsdisplay type for the Sun ZX/Leo frame buffer.

--- sbin/wsconsctl/util.c
+++ sbin/wsconsctl/util.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: util.c,v 1.30 2005/05/31 15:33:57 brad Exp $ */
+/*	$OpenBSD: util.c,v 1.31 2006/01/09 21:52:09 miod Exp $ */
 /*	$NetBSD: util.c,v 1.8 2000/03/14 08:11:53 sato Exp $ */
 
 /*-
@@ -140,7 +140,8 @@ static const struct nameint dpytype_tab[] = {
 	{ WSDISPLAY_TYPE_HYPERION,	"hyperion" },
 	{ WSDISPLAY_TYPE_TOPCAT,	"topcat" },
 	{ WSDISPLAY_TYPE_PXALCD,	"pxalcd" },
-	{ WSDISPLAY_TYPE_MAC68K,	"mac68k" }
+	{ WSDISPLAY_TYPE_MAC68K,	"mac68k" },
+	{ WSDISPLAY_TYPE_SUNLEO,	"sunleo" }
 };
 
 static const struct nameint kbdenc_tab[] = {
