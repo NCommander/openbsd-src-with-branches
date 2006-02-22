a6e7bd33c098aa20bf5dfdf7c7015618f5d2d0f1
otto <otto>
2006-02-22 07:16:32 +0000

    Avouid a race in atexit() handling by introducing a lock. Problem originally reported by Gergely Kovacs; help from dhartmei@; ok tedu@ millert@

--- lib/libpthread/thread/thread_malloc_lock.c
+++ lib/libpthread/thread/thread_malloc_lock.c
@@ -1,10 +1,11 @@
-/* $OpenBSD$ */
+/* $OpenBSD: thread_malloc_lock.c,v 1.4 2002/11/05 22:19:55 marc Exp $ */
 /* Public Domain <marc@snafu.org> */
 
 #include <pthread.h>
 #include "pthread_private.h"
 
 static spinlock_t malloc_lock = _SPINLOCK_INITIALIZER;
+static spinlock_t atexit_lock = _SPINLOCK_INITIALIZER;
 
 void
 _thread_malloc_lock()
@@ -21,4 +22,16 @@ _thread_malloc_unlock()
 void
 _thread_malloc_init()
 {
+}
+
+void
+_thread_atexit_lock()
+{
+	_SPINLOCK(&atexit_lock);
+}
+
+void
+_thread_atexit_unlock()
+{
+	_SPINUNLOCK(&atexit_lock);
 }
