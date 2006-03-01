d32bed27727d92e3d3f7e9cb265ec41b524813ba
deraadt <deraadt>
2006-03-01 21:51:39 +0000

    from mickey, like on i386: prioritise <16m memory for isa bus_dma'ble memorble memory allocations why? this is now needed because the nvidia ethernet running with jumbo frames is allocating much more memory, and was robbing isadma of bounce buffers.

--- sys/arch/amd64/include/isa_machdep.h
+++ sys/arch/amd64/include/isa_machdep.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: isa_machdep.h,v 1.4 2005/12/13 00:18:19 jsg Exp $	*/
+/*	$OpenBSD: isa_machdep.h,v 1.5 2006/02/12 19:55:38 miod Exp $	*/
 /*	$NetBSD: isa_machdep.h,v 1.2 2003/05/09 23:51:28 fvdl Exp $	*/
 
 /*-
@@ -82,6 +82,11 @@
 
 #include <machine/bus.h>
 #include <dev/isa/isadmavar.h>
+
+/*
+ * ISA can only DMA to 0-16M.
+ */
+#define	ISA_DMA_BOUNCE_THRESHOLD	0x00ffffff
 
 /*
  * XXX THIS FILE IS A MESS.  copyright: berkeley's probably.
