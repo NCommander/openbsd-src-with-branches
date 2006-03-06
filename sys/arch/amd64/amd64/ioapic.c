788930440c74baf5e20a26b2269ceb9f039a455f
kettenis <kettenis>
2006-03-06 20:51:46 +0000

    Remove accidentally committed debug printf.

--- sys/arch/amd64/amd64/ioapic.c
+++ sys/arch/amd64/amd64/ioapic.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ioapic.c,v 1.6 2005/05/31 09:34:22 art Exp $	*/
+/*	$OpenBSD: ioapic.c,v 1.7 2006/03/06 19:10:06 kettenis Exp $	*/
 /* 	$NetBSD: ioapic.c,v 1.6 2003/05/15 13:30:31 fvdl Exp $	*/
 
 /*-
@@ -513,7 +513,6 @@ ioapic_addroute(struct pic *pic, struct cpu_info *ci, int pin,
 	struct ioapic_pin *pp;
 
 	if (ioapic_cold) {
-		printf("ioapic_addroute: pin %d\n", pin);
 		pp = &sc->sc_pins[pin];
 		pp->ip_type = type;
 		pp->ip_vector = idtvec;
