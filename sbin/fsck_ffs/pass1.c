bd5ed28cfbf8e2a45071b72db541a0ec2543451b
deraadt <deraadt>
2006-03-12 02:28:28 +0000

    use asprintf instead of a static buffer; dhill, tested by pedro

--- sbin/fsck_ffs/pass1.c
+++ sbin/fsck_ffs/pass1.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pass1.c,v 1.17 2005/04/16 18:15:41 millert Exp $	*/
+/*	$OpenBSD: pass1.c,v 1.18 2005/12/19 15:18:01 pedro Exp $	*/
 /*	$NetBSD: pass1.c,v 1.16 1996/09/27 22:45:15 christos Exp $	*/
 
 /*
@@ -34,7 +34,7 @@
 #if 0
 static char sccsid[] = "@(#)pass1.c	8.1 (Berkeley) 6/5/93";
 #else
-static const char rcsid[] = "$OpenBSD: pass1.c,v 1.17 2005/04/16 18:15:41 millert Exp $";
+static const char rcsid[] = "$OpenBSD: pass1.c,v 1.18 2005/12/19 15:18:01 pedro Exp $";
 #endif
 #endif /* not lint */
 
@@ -59,9 +59,9 @@ static void checkinode(ino_t, struct inodesc *);
 static ino_t info_inumber;
 
 static int
-pass1_info(char *buf, int buflen)
+pass1_info(char *buf)
 {
-	return (snprintf(buf, buflen, "phase 1, inode %d/%d",
+	return (asprintf(&buf, "phase 1, inode %d/%d",
 	    info_inumber, sblock.fs_ipg * sblock.fs_ncg) > 0);
 }
 
