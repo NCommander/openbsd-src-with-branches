7ec1f64bfc2b56fa494d11833d5df204bfb8bfff
brad <brad>
2006-03-12 01:38:34 +0000

    IPL_IMP -> IPL_VM

--- sys/dev/isa/i82365_isasubr.c
+++ sys/dev/isa/i82365_isasubr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: i82365_isasubr.c,v 1.18 2004/01/09 21:32:24 brad Exp $	*/
+/*	$OpenBSD: i82365_isasubr.c,v 1.20 2005/01/27 17:03:23 millert Exp $	*/
 /*	$NetBSD: i82365_isasubr.c,v 1.1 1998/06/07 18:28:31 sommerfe Exp $  */
 
 /*
@@ -312,7 +312,7 @@ pcic_intr_find(sc, ist)
 				 * change interrupt) we need to do
 				 * higher.
 				 */
-				ih = isa_intr_establish(ic, irq, ist, IPL_IMP,
+				ih = isa_intr_establish(ic, irq, ist, IPL_VM,
 				    pcic_intr_probe, 0, NULL);
 				if (ih == NULL)
 					continue;
