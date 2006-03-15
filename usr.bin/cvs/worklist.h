4d098685d3074cc87ec9c81f465cbc611fe56243
deraadt <deraadt>
2006-03-15 18:24:43 +0000

    no need for one of the volatile

--- usr.bin/cvs/worklist.h
+++ usr.bin/cvs/worklist.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: worklist.h,v 1.1 2006/03/08 20:18:41 joris Exp $	*/
+/*	$OpenBSD: worklist.h,v 1.2 2006/03/15 13:30:48 niallo Exp $	*/
 /*
  * Copyright (c) 2006 Joris Vink <joris@openbsd.org>
  * All rights reserved.
@@ -28,7 +28,7 @@
 #define WORKLIST_H
 
 struct cvs_worklist {
-	volatile char				wkl_path[MAXPATHLEN];
+	char					wkl_path[MAXPATHLEN];
 	volatile SLIST_ENTRY(cvs_worklist)	wkl_list;
 };
 
