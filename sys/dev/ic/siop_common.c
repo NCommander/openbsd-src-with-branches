a19acae5945fa6a226e5e5c695564c181e2b312f
miod <miod>
2006-03-16 22:11:24 +0000

    Comment out tekram_sync_table and only degine {,dt_}scf_period[] for siop_common.c; shaves half a KB.

--- sys/dev/ic/siop_common.c
+++ sys/dev/ic/siop_common.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: siop_common.c,v 1.27 2005/11/20 22:32:48 krw Exp $ */
+/*	$OpenBSD: siop_common.c,v 1.28 2005/11/25 16:33:19 krw Exp $ */
 /*	$NetBSD: siop_common.c,v 1.37 2005/02/27 00:27:02 perry Exp $	*/
 
 /*
@@ -48,6 +48,7 @@
 #include <scsi/scsi_message.h>
 #include <scsi/scsiconf.h>
 
+#define SIOP_NEEDS_PERIOD_TABLES
 #include <dev/ic/siopreg.h>
 #include <dev/ic/siopvar_common.h>
 #include <dev/ic/siopvar.h>
