52ba0f0a90ab18f50c1e3b155e42f8748bcf0db5
miod <miod>
2006-03-16 06:57:23 +0000

    ventical -> vertical

--- sys/arch/macppc/dev/tpms.c
+++ sys/arch/macppc/dev/tpms.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tpms.c,v 1.2 2005/12/21 22:19:41 miod Exp $	*/
+/*	$OpenBSD: tpms.c,v 1.3 2005/12/22 13:47:31 xsa Exp $	*/
 
 /*
  * Copyright (c) 2005, Johan Wallén
@@ -528,7 +528,7 @@ reorder_sample(signed char *to, signed char *from)
 		to[i + 16] = from[5 * i + 42];
 #if 0
 		/* 
-		 * XXX This seems to introduce random ventical jumps, so
+		 * XXX This seems to introduce random vertical jumps, so
 		 * we ignore these sensors until we figure out their meaning.
 		 */
 		if (i < 2)
