c0b3fe223a0c78fb1cce320e687bb9bd17e74325
otto <otto>
2006-03-18 20:44:43 +0000

    Interchange roles: bc becomes parent, dc child. This leads to saner tty handling (try bc | grep foo followed by ^C, as noted by deraadt@). To make it all work, bc now waits for dc to exit after giving it a quit command.  ok deraadt@ jaredy@

--- usr.bin/bc/extern.h
+++ usr.bin/bc/extern.h
@@ -1,4 +1,4 @@
-/*      $OpenBSD: extern.h,v 1.4 2003/12/02 09:00:07 otto Exp $	*/
+/*      $OpenBSD: extern.h,v 1.5 2004/10/19 07:36:51 otto Exp $	*/
 
 /*
  * Copyright (c) 2003, Otto Moerbeek <otto@drijf.net>
@@ -36,3 +36,4 @@ extern int	sargc;
 extern char	**sargv;
 extern char	*filename;
 extern char	*cmdexpr;
+bool		interactive;
