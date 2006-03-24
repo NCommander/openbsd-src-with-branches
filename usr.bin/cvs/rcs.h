8a2776ee8c4f1075646ad61c70344eea9905bc7c
ray <ray>
2006-03-24 03:22:03 +0000

    Fix RCS_KWEXP_INVAL() macro.
    Pointed out by Theo.
    
    Okay xsa@.

--- usr.bin/cvs/rcs.h
+++ usr.bin/cvs/rcs.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rcs.h,v 1.49 2006/03/09 10:56:33 xsa Exp $	*/
+/*	$OpenBSD: rcs.h,v 1.50 2006/03/23 02:21:33 ray Exp $	*/
 /*
  * Copyright (c) 2004 Jean-Francois Brousseau <jfb@openbsd.org>
  * All rights reserved.
@@ -86,7 +86,7 @@
 
 #define RCS_KWEXP_INVAL(k) \
 	((k & RCS_KWEXP_ERR) || \
-	((k & RCS_KWEXP_OLD) && (RCS_KWEXP_OLD & ~RCS_KWEXP_OLD)))
+	((k & RCS_KWEXP_OLD) && (k & ~RCS_KWEXP_OLD)))
 
 
 struct rcs_kw {
