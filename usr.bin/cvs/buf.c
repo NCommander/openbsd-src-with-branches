b4b6ba2243694eed1baf950ec6ecff0bf37065f7
ray <ray>
2006-03-28 02:13:44 +0000

    Today is Integer Overflow Prevention Day:  - Sync xmalloc.? with ssh versions.  - Change all xrealloc() calls to new API.
    ``I really like this.'' niallo@

--- usr.bin/cvs/buf.c
+++ usr.bin/cvs/buf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: buf.c,v 1.39 2006/03/23 08:50:41 xsa Exp $	*/
+/*	$OpenBSD: buf.c,v 1.40 2006/03/25 21:29:58 ray Exp $	*/
 /*
  * Copyright (c) 2003 Jean-Francois Brousseau <jfb@openbsd.org>
  * All rights reserved.
@@ -435,7 +435,7 @@ cvs_buf_grow(BUF *b, size_t len)
 	if (b->cb_size == 0)
 		tmp = xmalloc(len);
 	else
-		tmp = xrealloc(b->cb_buf, b->cb_size + len);
+		tmp = xrealloc(b->cb_buf, 1, b->cb_size + len);
 	b->cb_buf = (u_char *)tmp;
 	b->cb_size += len;
 
