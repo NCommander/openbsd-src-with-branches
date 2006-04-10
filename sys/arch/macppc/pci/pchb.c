f0f3350db38422e7ede3d95de79cd92aa0fd07d2
brad <brad>
2006-04-10 08:34:22 +0000

    Intrepid 2 host bridges.

--- sys/arch/macppc/pci/pchb.c
+++ sys/arch/macppc/pci/pchb.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pchb.c,v 1.7 2005/09/29 20:11:13 kettenis Exp $	*/
+/*	$OpenBSD: pchb.c,v 1.8 2005/10/02 22:09:40 brad Exp $	*/
 /*	$NetBSD: pchb.c,v 1.4 2000/01/25 07:19:11 tsubai Exp $	*/
 
 /*-
@@ -86,6 +86,9 @@ pchbmatch(struct device *parent, void *cf, void *aux)
 		case PCI_PRODUCT_APPLE_SHASTA_HT:
 		case PCI_PRODUCT_APPLE_K2:
 		case PCI_PRODUCT_APPLE_K2_AGP:
+		case PCI_PRODUCT_APPLE_INTREPID2_AGP:
+		case PCI_PRODUCT_APPLE_INTREPID2_PCI1:
+		case PCI_PRODUCT_APPLE_INTREPID2_PCI1:
 			return (1);
 		}
 		break;
