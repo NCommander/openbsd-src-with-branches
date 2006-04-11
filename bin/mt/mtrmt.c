94409ba79848f2be8b74c4d10a28b6bd00c32d82
ray <ray>
2006-04-11 15:06:48 +0000

    Check for strdup() failure.
    OK jaredy@

--- bin/mt/mtrmt.c
+++ bin/mt/mtrmt.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mtrmt.c,v 1.15 2004/09/16 08:35:49 deraadt Exp $	*/
+/*	$OpenBSD: mtrmt.c,v 1.16 2006/03/23 23:03:07 deraadt Exp $	*/
 /*	$NetBSD: mtrmt.c,v 1.2 1996/03/06 06:22:07 scottr Exp $	*/
 
 /*-
@@ -87,9 +87,8 @@ static	int rmtreply(char *);
 int
 rmthost(char *host)
 {
-	rmtpeer = strdup(host);
-	if (!rmtpeer)
-		rmtpeer = host;
+	if ((rmtpeer = strdup(host)) == NULL)
+		err(1, "strdup");
 	signal(SIGPIPE, sigrmtconnaborted);
 	rmtgetconn();
 	if (rmtape < 0)
