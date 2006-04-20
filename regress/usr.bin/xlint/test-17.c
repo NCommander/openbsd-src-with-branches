30fe5d7736cdbeb71e0b4366eaa71864fb618e84
cloder <cloder>
2006-04-20 03:39:14 +0000

    Test case for unreachable breaks

---
+++ regress/usr.bin/xlint/test-17.c
@@ -0,0 +1,44 @@
+ /*	$OpenBSD$	*/
+
+/*
+ * Placed in the public domain by Chad Loder <cloder@openbsd.org>.
+ *
+ * Test lint dealing with unreachable break statements.
+ */
+
+/* ARGSUSED */
+int
+main(int argc, char* argv[])
+{
+	int a = 0;
+
+	switch (argc)
+	{
+	case 1:
+		a = 1;
+		return 1;
+		break; /* should not warn */
+	case 2:
+		a = 2;
+		while (a < 5) {
+			a++;
+			break;
+			break; /* should warn */
+		}
+		break;
+	default:
+		break;
+	}
+
+	while (argc < 5) {
+		if (argc ) {
+			return 1;
+			break; /* should warn */
+		}
+
+		argc++;
+		break;
+	}
+
+	return a;
+}
