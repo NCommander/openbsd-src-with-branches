8a4e2964736093901bf8d2dbcf915420b651828c
deraadt <deraadt>
2006-04-21 07:38:16 +0000

    back out last (powerpc on bgpd build shows it is wrong)

--- usr.bin/yacc/reader.c
+++ usr.bin/yacc/reader.c
@@ -1250,7 +1250,7 @@ end_rule(void)
     if (!last_was_action && plhs[nrules]->tag)
     {
 	for (i = nitems - 1; pitem[i]; --i) continue;
-	if (pitem[i] == 0 || pitem[i]->tag != plhs[nrules]->tag)
+	if (pitem[i+1] == 0 || pitem[i+1]->tag != plhs[nrules]->tag)
 	    default_action_warning();
     }
 
