80265bb7acf75fc582bfde7f85fe3ea75a0e9ece
jmc <jmc>
2006-04-24 16:16:56 +0000

    - do not show file ... as optional - remove angle brackets - sync usage()
    ok xsa

--- usr.bin/rcs/ident.c
+++ usr.bin/rcs/ident.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ident.c,v 1.16 2006/04/19 06:53:41 xsa Exp $	*/
+/*	$OpenBSD: ident.c,v 1.17 2006/04/21 14:18:26 xsa Exp $	*/
 /*
  * Copyright (c) 2005 Xavier Santolaria <xsa@openbsd.org>
  * All rights reserved.
@@ -149,5 +149,5 @@ ident_line(FILE *fp)
 void
 ident_usage(void)
 {
-	fprintf(stderr, "usage: ident [-qV] [file ...]\n");
+	fprintf(stderr, "usage: ident [-qV] file ...\n");
 }
