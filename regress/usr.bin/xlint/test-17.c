281f486856c077d289e72f6c5efac134577a8181
cloder <cloder>
2006-04-25 01:31:46 +0000

    Reflect latest lint commit, which fixes many type conversion false positives and negatives.

--- regress/usr.bin/xlint/test-17.c
+++ regress/usr.bin/xlint/test-17.c
@@ -1,4 +1,4 @@
- /*	$OpenBSD: test-17.c,v 1.1 2006/04/20 03:39:14 cloder Exp $	*/
+ /*	$OpenBSD: test-17.c,v 1.2 2006/04/21 02:37:28 cloder Exp $	*/
 
 /*
  * Placed in the public domain by Chad Loder <cloder@openbsd.org>.
@@ -45,6 +45,5 @@ main(int argc, char* argv[])
 		break;
 	}
 
-	break;
 	return a;
 }
