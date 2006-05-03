497c80cbbbdc5648a5ab6505f74836b3e88b3558
brad <brad>
2006-05-03 03:57:53 +0000

    MFC: Fix by kurt@
    Fix a process datasize leak with MAP_FIXED. When zapping old mappings
    call uvm_unmap_p instead of uvm_unmap so that it has the process information
    and can adjust vm_dused.

--- sys/uvm/uvm_mmap.c
+++ sys/uvm/uvm_mmap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_mmap.c,v 1.56 2005/05/24 21:11:47 tedu Exp $	*/
+/*	$OpenBSD: uvm_mmap.c,v 1.57 2005/06/01 18:34:40 tedu Exp $	*/
 /*	$NetBSD: uvm_mmap.c,v 1.49 2001/02/18 21:19:08 chs Exp $	*/
 
 /*
@@ -1197,7 +1197,7 @@ uvm_mmap(map, addr, size, prot, maxprot, flags, handle, foff, locklimit, p)
 		if (*addr & PAGE_MASK)
 			return(EINVAL);
 		uvmflag |= UVM_FLAG_FIXED;
-		uvm_unmap(map, *addr, *addr + size);	/* zap! */
+		uvm_unmap_p(map, *addr, *addr + size, p);	/* zap! */
 	}
 
 	/*
