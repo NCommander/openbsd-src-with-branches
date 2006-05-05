108b47c872ceeb762b70af91acb07592fcfdcab0
otto <otto>
2006-05-05 06:48:20 +0000

    SEGVs have been fixed, so include proper expected output

--- regress/usr.bin/xlint/test-21.c
+++ regress/usr.bin/xlint/test-21.c
@@ -1,4 +1,4 @@
- /*	$OpenBSD$*/
+ /*	$OpenBSD: test-21.c,v 1.1 2006/05/03 18:45:25 cloder Exp $*/
 
 /*
  * Placed in the public domain by Chad Loder <cloder@openbsd.org>.
@@ -12,6 +12,7 @@ struct foo
 };
 
 int bar(int, unsigned int(int, const struct foo *, int));
+int baz(int *(void *));
 
 /* ARGSUSED */
 int
