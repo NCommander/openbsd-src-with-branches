d519efd6ceb29ef4ab160fafa6aa566ea2198f05
kettenis <kettenis>
2006-05-15 21:02:44 +0000

    Implement PT_{GET|SET}FPREGS. "looks good to me" miod@, ok drahn@

--- sys/arch/powerpc/include/ptrace.h
+++ sys/arch/powerpc/include/ptrace.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ptrace.h,v 1.1 1996/12/28 06:09:22 rahnds Exp $	*/
+/*	$OpenBSD: ptrace.h,v 1.2 1998/08/07 02:22:05 rahnds Exp $	*/
 /*	$NetBSD: ptrace.h,v 1.7 1995/01/26 19:47:10 mycroft Exp $	*/
 
 /*
@@ -39,10 +39,7 @@
 #define	PT_STEP		(PT_FIRSTMACH + 0)
 #define	PT_GETREGS	(PT_FIRSTMACH + 1)
 #define	PT_SETREGS	(PT_FIRSTMACH + 2)
-
-#if NOT_SUPPORTED
 #define	PT_GETFPREGS	(PT_FIRSTMACH + 3)
 #define	PT_SETFPREGS	(PT_FIRSTMACH + 4)
-#endif
 
 #endif /* !_POWERPC_PTRACE_H_ */
