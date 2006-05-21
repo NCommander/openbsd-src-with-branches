23ad0b724513c93c134fbbea981b648a52e7cdf0
dlg <dlg>
2006-05-21 18:28:24 +0000

    the maximum possible number of ccbs that a megaraid can use (126 of them) is preallocated in the softc, even if the driver is only ever going to use one of them when running. this allocates them separately during attach as a first step toward having the driver only allocate as many ccbs as the firmware can support.
    ok krw@

--- sys/dev/ic/amivar.h
+++ sys/dev/ic/amivar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: amivar.h,v 1.46 2006/04/20 04:39:11 dlg Exp $	*/
+/*	$OpenBSD: amivar.h,v 1.47 2006/04/20 13:02:26 dlg Exp $	*/
 
 /*
  * Copyright (c) 2001 Michael Shalayeff
@@ -110,7 +110,7 @@ struct ami_softc {
 	volatile struct ami_iocmd *sc_mbox;
 	paddr_t			sc_mbox_pa;
 
-	struct ami_ccb		sc_ccbs[AMI_MAXCMDS];
+	struct ami_ccb		*sc_ccbs;
 	struct ami_ccb_list	sc_ccb_freeq, sc_ccb_preq, sc_ccb_runq;
 
 	struct ami_mem		*sc_ccbmem_am;
