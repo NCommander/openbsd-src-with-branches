2f78a9eeb663c61abf2703ec8777a1aa1de183d8
ray <ray>
2006-05-25 03:20:32 +0000

    Remove xmktemp() and uses plain old mkstemp(3), avoiding race conditions.
    OK moritz@, otto@

--- usr.bin/sdiff/common.h
+++ usr.bin/sdiff/common.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: common.h,v 1.1 2006/02/20 08:38:18 otto Exp $	*/
 
 /*
  * Written by Raymond Lai <ray@cyth.net>.
@@ -6,4 +6,3 @@
  */
 
 __dead void cleanup(const char *);
-char *xmktemp(const char *);
--- usr.bin/sdiff/extern.h
+++ usr.bin/sdiff/extern.h
@@ -1,10 +1,11 @@
-/*	$OpenBSD: extern.h,v 1.2 2005/12/27 04:06:16 tedu Exp $ */
+/*	$OpenBSD: extern.h,v 1.3 2005/12/27 04:18:07 tedu Exp $ */
 
 /*
  * Written by Raymond Lai <ray@cyth.net>.
  * Public domain.
  */
 
-extern FILE	*outfile;	/* file to save changes to */
+extern FILE		*outfile;	/* file to save changes to */
+extern const char	*tmpdir;
 
 int eparse(const char *, const char *, const char *);
