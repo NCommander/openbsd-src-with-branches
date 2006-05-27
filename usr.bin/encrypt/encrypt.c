1146b6e42c0030c11b13af287d5db457ab4c3eba
moritz <moritz>
2006-05-27 23:42:08 +0000

    Handle crypt(3) returning NULL. Found by Gustavo C. Pereira.
    ok deraadt@

--- usr.bin/encrypt/encrypt.c
+++ usr.bin/encrypt/encrypt.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: encrypt.c,v 1.21 2004/07/13 21:09:48 millert Exp $	*/
+/*	$OpenBSD: encrypt.c,v 1.22 2006/04/02 04:13:07 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1996, Jason Downs.  All rights reserved.
@@ -83,7 +83,7 @@ trim(char *line)
 void
 print_passwd(char *string, int operation, void *extra)
 {
-	char msalt[3], *salt;
+	char msalt[3], *salt, *cryptstr;
 	login_cap_t *lc;
 	int pwd_gensalt(char *, int, login_cap_t *, char);
 	void to64(char *, u_int32_t, int n);
@@ -128,7 +128,9 @@ print_passwd(char *string, int operation, void *extra)
 		break;
 	}
 
-	(void)fputs(crypt(string, salt), stdout);
+	if ((cryptstr = crypt(string, salt)) == NULL)
+		errx(1, "crypt failed");
+	fputs(cryptstr, stdout);
 }
 
 int
