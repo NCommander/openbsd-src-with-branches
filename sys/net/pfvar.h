10ec35e67a8175e8044e43dbb1de7a9cc3b2013a
mcbride <mcbride>
2006-05-28 02:45:45 +0000

    Enable adaptive timeouts by default, with adaptive.start of 60% of the state limit and adaptive.end of 120% of the state limit. Explicitly setting the adaptive timeouts will override the default, and it can be disabled by setting both adaptive.start and adaptive.end to 0.
    ok henning@

--- sys/net/pfvar.h
+++ sys/net/pfvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pfvar.h,v 1.233 2005/11/04 08:24:15 mcbride Exp $ */
+/*	$OpenBSD: pfvar.h,v 1.234 2006/03/14 11:09:42 djm Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -611,6 +611,8 @@ struct pf_rule {
 #define PFRULE_IFBOUND		0x00010000	/* if-bound */
 
 #define PFSTATE_HIWAT		10000	/* default state table size */
+#define PFSTATE_ADAPT_START	6000	/* default adaptive timeout start */
+#define PFSTATE_ADAPT_END	12000	/* default adaptive timeout end */
 
 
 struct pf_threshold {
