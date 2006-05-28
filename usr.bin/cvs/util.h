4806d1398174444c11f63a6caeeff8d676fb5e5d
joris <joris>
2006-05-28 17:25:18 +0000

    several fixes to the file api: - default to CVS_FILE when something is totally unknown - cvs_get_repository_path() now returns the full repository path for   the given argument. - cvs_get_repository_name() returns the contents of CVS/Repository   to the caller. - allow command callbacks to specify if our recursion code needs   to skip the directory or not. - when checking for a admin directory, make sure it is in fact   a directory. if it is not we dont want to recurse inside.

--- usr.bin/cvs/util.h
+++ usr.bin/cvs/util.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: util.h,v 1.5 2006/05/27 03:30:31 joris Exp $	*/
+/*	$OpenBSD: util.h,v 1.6 2006/05/27 16:18:23 joris Exp $	*/
 /*
  * Copyright (c) 2006 Niall O'Higgins <niallo@openbsd.org>
  * All rights reserved.
@@ -27,7 +27,8 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-void	  cvs_get_repo(const char *, char *, size_t);
+void	  cvs_get_repository_path(const char *, char *, size_t);
+void	  cvs_get_repository_name(const char *, char *, size_t);
 void	  cvs_modetostr(mode_t, char *, size_t);
 void	  cvs_strtomode(const char *, mode_t *);
 void	  cvs_splitpath(const char *, char *, size_t, char **);
