d1830318fc70e9bc36d333f4936d674fc7e944ff
drahn <drahn>
2006-05-29 17:13:19 +0000

    Add a new port for ARM based products from a variety of vendors, typically using Redboot as a loader and using pci, that have a variety of fairly minor hardware differences between them.

---
+++ sys/arch/armish/include/_types.h
@@ -0,0 +1,10 @@
+/* 	$OpenBSD: types.h,v 1.2 2005/05/27 21:29:00 miod Exp $	*/
+/*	$NetBSD: types.h,v 1.4 2002/02/28 03:17:26 simonb Exp $	*/
+
+#ifndef _ARMISH__TYPES_H_
+#define	_ARMISH__TYPES_H_
+
+#include <arm/_types.h>
+#define __HAVE_GENERIC_SOFT_INTERRUPTS
+
+#endif /* _ARMISH__TYPES_H_ */
