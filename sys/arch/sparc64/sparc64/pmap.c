e2bff30b6c55fee581c2434133b7b2a4244526cf
kettenis <kettenis>
2006-06-02 00:26:08 +0000

    Remove unused local variable from debug code.

--- sys/arch/sparc64/sparc64/pmap.c
+++ sys/arch/sparc64/sparc64/pmap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pmap.c,v 1.26 2003/11/03 07:01:33 david Exp $	*/
+/*	$OpenBSD: pmap.c,v 1.27 2005/11/11 16:38:30 miod Exp $	*/
 /*	$NetBSD: pmap.c,v 1.107 2001/08/31 16:47:41 eeh Exp $	*/
 #undef	NO_VCACHE /* Don't forget the locked TLB in dostart */
 /*
@@ -410,7 +410,7 @@ void pv_check(void);
 void
 pv_check()
 {
-	int i, j, s;
+	int i, s;
 	
 	s = splhigh();
 	for (i = 0; i < physmem; i++) {
