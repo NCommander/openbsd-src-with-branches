847659a911566ef93d590a5720c5dac4713d6839
claudio <claudio>
2006-06-15 09:59:48 +0000

    Shuffle some vars to make the struct ordered by size.

--- usr.sbin/bgpctl/parser.h
+++ usr.sbin/bgpctl/parser.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: parser.h,v 1.11 2006/01/24 15:28:03 henning Exp $ */
+/*	$OpenBSD: parser.h,v 1.12 2006/05/23 12:14:34 henning Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -50,11 +50,11 @@ enum actions {
 struct parse_result {
 	struct bgpd_addr	addr;
 	struct filter_as	as;
-	char			peerdesc[PEER_DESCR_LEN];
-	enum actions		action;
-	int			flags;
-	u_int8_t		prefixlen;
 	struct filter_set_head	set;
+	char			peerdesc[PEER_DESCR_LEN];
+	int			flags;
+	enum actions		action;
+	u_int8_t		prefixlen;
 	sa_family_t		af;
 };
 
