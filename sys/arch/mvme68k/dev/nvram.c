554ec4504b12b58d3dd1bb31812292407764638a
deraadt <deraadt>
2006-06-21 14:49:33 +0000

    let this compile after time.h changes

--- sys/arch/mvme68k/dev/nvram.c
+++ sys/arch/mvme68k/dev/nvram.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: nvram.c,v 1.15 2005/10/27 16:04:08 martin Exp $ */
+/*	$OpenBSD: nvram.c,v 1.16 2005/11/24 22:43:16 miod Exp $ */
 
 /*
  * Copyright (c) 1995 Theo de Raadt
@@ -147,7 +147,6 @@ microtime(tvp)
 #define	FROMBCD(x)	(((x) >> 4) * 10 + ((x) & 0xf))
 #define	TOBCD(x)	(((x) / 10 * 16) + ((x) % 10))
 
-#define	SECDAY		(24 * 60 * 60)
 #define	SECYR		(SECDAY * 365)
 #define	LEAPYEAR(y)	(((y) & 3) == 0)
 
