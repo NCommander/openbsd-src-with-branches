e774414de88d74e1274836a900611aca07faf300
brad <brad>
2006-06-26 15:52:59 +0000

    if piixpm does not try to attach an interrupt then indicate that the chip is in polling mode.
    ok kettenis@

--- sys/dev/pci/piixpm.c
+++ sys/dev/pci/piixpm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: piixpm.c,v 1.20 2006/02/27 08:25:02 grange Exp $	*/
+/*	$OpenBSD: piixpm.c,v 1.21 2006/06/26 13:56:29 brad Exp $	*/
 
 /*
  * Copyright (c) 2005, 2006 Alexander Yurchenko <grange@openbsd.org>
@@ -150,7 +150,8 @@ piixpm_attach(struct device *parent, struct device *self, void *aux)
 		}
 		if (sc->sc_poll)
 			printf(": polling");
-	}
+	} else
+		printf(": polling");
 
 	printf("\n");
 
