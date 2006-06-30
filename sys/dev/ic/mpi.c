2108abc770e2a24c768e0a8aff14ac873314ea4c
dlg <dlg>
2006-06-30 12:32:53 +0000

    tabs, not spaces

--- sys/dev/ic/mpi.c
+++ sys/dev/ic/mpi.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mpi.c,v 1.50 2006/06/29 10:43:21 dlg Exp $ */
+/*	$OpenBSD: mpi.c,v 1.51 2006/06/30 08:29:42 dlg Exp $ */
 
 /*
  * Copyright (c) 2005, 2006 David Gwynne <dlg@openbsd.org>
@@ -562,8 +562,8 @@ mpi_inq(struct mpi_softc *sc, u_int16_t target, int physdisk)
 
 	DNPRINTF(MPI_D_PPR, "%s: mpi_inq\n", DEVNAME(sc));
 
-        bzero(&inq, sizeof(inq));
-        inq.opcode = INQUIRY;
+	bzero(&inq, sizeof(inq));
+	inq.opcode = INQUIRY;
 
 	ccb = mpi_get_ccb(sc);
 	if (ccb == NULL)
