5d933940d9f30ab6e49b165b639a1285ad96e843
pedro <pedro>
2006-07-11 16:24:09 +0000

    Make the mounting process pass a hint to the kernel of where to find the VAT

--- sys/isofs/udf/udf_extern.h
+++ sys/isofs/udf/udf_extern.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: udf_extern.h,v 1.5 2006/07/08 23:11:59 pedro Exp $	*/
+/*	$OpenBSD: udf_extern.h,v 1.6 2006/07/09 04:14:25 pedro Exp $	*/
 
 /*
  * Written by Pedro Martelletto <pedro@openbsd.org> in February 2005.
@@ -9,7 +9,7 @@
  * udf_subr.c
  */
 int udf_rawnametounicode(u_int len, char *, unicode_t *);
-int udf_vat_get(struct umount *);
+int udf_vat_get(struct umount *, uint32_t);
 int udf_vat_map(struct umount *, uint32_t *);
 
 /*
