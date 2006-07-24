0d31941b0a997600ade26e59187f1dc941651b62
miod <miod>
2006-07-24 22:19:54 +0000

    Driver for the VS4000/90 frame buffer, adapted from NetBSD (ragge) and Blaz Antonic's work, no acceleration yet, untested due to the lack of hardware, but enough sacrifices were made to the RAMDAC Gods.

--- sbin/wsconsctl/util.c
+++ sbin/wsconsctl/util.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: util.c,v 1.32 2006/02/14 18:52:15 miod Exp $ */
+/*	$OpenBSD: util.c,v 1.33 2006/04/14 21:05:43 miod Exp $ */
 /*	$NetBSD: util.c,v 1.8 2000/03/14 08:11:53 sato Exp $ */
 
 /*-
@@ -142,7 +142,8 @@ static const struct nameint dpytype_tab[] = {
 	{ WSDISPLAY_TYPE_PXALCD,	"pxalcd" },
 	{ WSDISPLAY_TYPE_MAC68K,	"mac68k" },
 	{ WSDISPLAY_TYPE_SUNLEO,	"sunleo" },
-	{ WSDISPLAY_TYPE_TVRX,		"tvrx" }
+	{ WSDISPLAY_TYPE_TVRX,		"tvrx" },
+	{ WSDISPLAY_TYPE_LCSPX,		"dec-lcspx" }
 };
 
 static const struct nameint kbdenc_tab[] = {
