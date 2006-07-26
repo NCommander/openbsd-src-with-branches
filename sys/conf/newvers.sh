ff5cad086838c7cb7e32328bb6b53bce0fdd3814
deraadt <deraadt>
2006-07-26 20:34:11 +0000

    crank to 4.0-beta

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.72 2005/09/05 20:43:25 miod Exp $
+#	$OpenBSD: newvers.sh,v 1.76 2006/03/04 11:40:22 grange Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -64,12 +64,12 @@ id=`basename ${d}`
 #	A month or so before release, select STATUS "-beta"
 
 ost="OpenBSD"
-osr="3.9"
+osr="4.0"
 
 cat >vers.c <<eof
-#define STATUS "-current"
-#if 0
 #define STATUS "-beta"
+#if 0
+#define STATUS "-current"
 #define STATUS ""			/* release */
 #endif
 
--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: param.h,v 1.66 2006/01/19 03:30:05 deraadt Exp $	*/
+/*	$OpenBSD: param.h,v 1.67 2006/06/19 19:52:04 steven Exp $	*/
 /*	$NetBSD: param.h,v 1.23 1996/03/17 01:02:29 thorpej Exp $	*/
 
 /*-
@@ -41,8 +41,8 @@
 #define BSD4_3	1
 #define BSD4_4	1
 
-#define OpenBSD	200605		/* OpenBSD version (year & month). */
-#define OpenBSD3_9 1		/* OpenBSD 3.9 */
+#define OpenBSD	200611		/* OpenBSD version (year & month). */
+#define OpenBSD4_0 1		/* OpenBSD 4.0 */
 
 #ifndef NULL
 #ifdef 	__GNUG__
