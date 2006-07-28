1abc075dd86c2209ed9423fceda72889f13e9513
marco <marco>
2006-07-28 20:46:12 +0000

    Only read 1 sensor at a time instead of all of them.  This should help systems that have a burst of activity every 10 seconds.
    Also lower the polling frequency from 10 to 5 seconds.
    
    ok jordan@

--- sys/dev/ipmivar.h
+++ sys/dev/ipmivar.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: ipmivar.h,v 1.14 2006/05/15 00:50:57 marco Exp $ */
+/* $OpenBSD: ipmivar.h,v 1.15 2006/05/21 20:55:26 alek Exp $ */
 
 /*
  * Copyright (c) 2005 Jordan Hargrave
@@ -101,6 +101,8 @@ struct ipmi_softc {
 	struct rwlock		sc_lock;
 
 	struct ipmi_bmc_args	*sc_iowait_args;
+
+	struct ipmi_sensor	*current_sensor;
 };
 
 struct ipmi_thread {
