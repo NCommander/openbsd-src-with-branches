423b536aa4b844f0b738aec84e7f8fc6c47181bd
gwk <gwk>
2006-07-30 05:30:45 +0000

    Donot pass a uninitialized pointer to aml_register_notify(), should fix PR5153 awaiting confirmation of this from the submitter, but its wrong regardless. ok marco@, canacar@

--- sys/dev/acpi/acpitz.c
+++ sys/dev/acpi/acpitz.c
@@ -1,4 +1,4 @@
-/* $OpenBSD$ */
+/* $OpenBSD: acpitz.c,v 1.1 2006/05/19 09:24:32 canacar Exp $ */
 /*
  * Copyright (c) 2006 Can Erkin Acar <canacar@openbsd.org>
  * Copyright (c) 2005 Marco Peereboom <marco@openbsd.org>
@@ -112,7 +112,7 @@ acpitz_attach(struct device *parent, struct device *self, void *aux)
 		printf(", critical temperature: %u degC",
 		    (unsigned)(sc->sc_crt - 2732) / 10);
 
-	aml_register_notify(sc->sc_devnode->parent, aa->aaa_dev,
+	aml_register_notify(sc->sc_devnode->parent, NULL,
 	    acpitz_notify, sc);
 	
 	memset(&sc->sc_sens, 0, sizeof(sc->sc_sens));
