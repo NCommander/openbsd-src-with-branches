5b84f5b378e796eaf3b0a2ad6cbad55a006e831e
beck <beck>
2006-08-04 21:35:51 +0000

    Add ENOMEDIUM and EMEDIUMTYPE to report medium errors to userland programs when using removable media devices, along with changes to scsi_base to detect such cases in tapes other devices. This makes tar, dd, and friends report a semi useful error message instead of nonsense when there is nothing in the device.
    Includes libc minor bump, and will require the corresponding sets change.
    
    ok krw@ deraadt@

--- sys/sys/errno.h
+++ sys/sys/errno.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: errno.h,v 1.15 2005/12/13 00:35:23 millert Exp $	*/
+/*	$OpenBSD: errno.h,v 1.16 2005/12/28 16:33:57 millert Exp $	*/
 /*	$NetBSD: errno.h,v 1.10 1996/01/20 01:33:53 jtc Exp $	*/
 
 /*
@@ -157,7 +157,9 @@
 #define	EIPSEC		82		/* IPsec processing failure */
 #define	ENOATTR		83		/* Attribute not found */
 #define	EILSEQ		84		/* Illegal byte sequence */
-#define	ELAST		84		/* Must be equal largest errno */
+#define	ENOMEDIUM	85		/* No medium found */
+#define	EMEDIUMTYPE	86		/* Wrong Medium Type */
+#define	ELAST		86		/* Must be equal largest errno */
 #endif /* __BSD_VISIBLE */
 
 #ifdef _KERNEL
