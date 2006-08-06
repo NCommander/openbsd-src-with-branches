e45cc37eb912e1a0cfdd8ed16c3d498b146cb82c
brad <brad>
2006-08-06 02:04:20 +0000

    add the Intel 6321ESB LPC

--- sys/arch/i386/pci/ichpcib.c
+++ sys/arch/i386/pci/ichpcib.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ichpcib.c,v 1.8 2005/07/25 05:25:03 jsg Exp $	*/
+/*	$OpenBSD: ichpcib.c,v 1.9 2005/12/11 03:42:19 brad Exp $	*/
 /*
  * Copyright (c) 2004 Alexander Yurchenko <grange@openbsd.org>
  *
@@ -84,6 +84,7 @@ extern int setperf_prio;
 
 const struct pci_matchid ichpcib_devices[] = {
 	{ PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_6300ESB_LPC },
+	{ PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_6321ESB_LPC },
 	{ PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_82801AA_LPC },
 	{ PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_82801AB_LPC },
 	{ PCI_VENDOR_INTEL, PCI_PRODUCT_INTEL_82801BA_LPC },
