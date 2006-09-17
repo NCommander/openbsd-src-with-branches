67dc2df39602df3830414383e07b1039d597343f
steven <steven>
2006-09-17 16:47:27 +0000

    4.0-current.  yes deraadt

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.78 2006/08/28 21:16:16 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.79 2006/09/17 16:25:30 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -64,7 +64,7 @@ id=`basename ${d}`
 #	A month or so before release, select STATUS "-beta"
 
 ost="OpenBSD"
-osr="4.1"
+osr="4.0"
 
 cat >vers.c <<eof
 #define STATUS "-current"
