ed4aa146cfb3660f22e1f96ed373668811e471ee
krw <krw>
2006-09-24 22:43:00 +0000

    Bring hp300, mac68k and mvme68k disklabel initialization code into line with all other archs by initializing partitions up to RAWPART the same way. Should be no functional change.
    ok martin@ miod@

--- sys/arch/mvme68k/mvme68k/disksubr.c
+++ sys/arch/mvme68k/mvme68k/disksubr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: disksubr.c,v 1.34 2006/03/15 20:20:40 miod Exp $	*/
+/*	$OpenBSD: disksubr.c,v 1.35 2006/08/17 10:34:14 krw Exp $	*/
 /*
  * Copyright (c) 1998 Steve Murphree, Jr.
  * Copyright (c) 1995 Dale Rahn.
@@ -82,6 +82,7 @@ readdisklabel(dev, strat, lp, clp, spoofonly)
 	}
 	if (lp->d_partitions[i].p_size == 0)
 		lp->d_partitions[i].p_size = lp->d_secperunit;
+	lp->d_partitions[i].p_offset = 0;
 
 	/* don't read the on-disk label if we are in spoofed-only mode */
 	if (spoofonly)
