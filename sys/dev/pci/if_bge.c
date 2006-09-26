fcac74f9ddd6b35417f261b1ff7529655539bb81
brad <brad>
2006-09-26 23:57:16 +0000

    update the comment for bge_chipinit().

--- sys/dev/pci/if_bge.c
+++ sys/dev/pci/if_bge.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_bge.c,v 1.180 2006/09/17 23:31:07 brad Exp $	*/
+/*	$OpenBSD: if_bge.c,v 1.181 2006/09/26 13:10:44 mickey Exp $	*/
 
 /*
  * Copyright (c) 2001 Wind River Systems
@@ -1073,8 +1073,7 @@ bge_setpromisc(struct bge_softc *sc)
 }
 
 /*
- * Do endian, PCI and DMA initialization. Also check the on-board ROM
- * self-test results.
+ * Do endian, PCI and DMA initialization.
  */
 void
 bge_chipinit(struct bge_softc *sc)
