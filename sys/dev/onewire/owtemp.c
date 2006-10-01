7bb19dcee63751658a6e7b0af773c271f8d280f5
grange <grange>
2006-10-01 08:09:04 +0000

    Protect sensor_task_unregister() with some locking.

--- sys/dev/onewire/owtemp.c
+++ sys/dev/onewire/owtemp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: owtemp.c,v 1.1 2006/03/04 16:27:03 grange Exp $	*/
+/*	$OpenBSD: owtemp.c,v 1.2 2006/06/23 06:27:11 miod Exp $	*/
 
 /*
  * Copyright (c) 2006 Alexander Yurchenko <grange@openbsd.org>
@@ -105,7 +105,10 @@ owtemp_detach(struct device *self, int flags)
 	struct owtemp_softc *sc = (struct owtemp_softc *)self;
 
 	sensor_del(&sc->sc_sensor);
+
+	onewire_lock(sc, 0);
 	sensor_task_unregister(sc);
+	onewire_unlock(sc);
 
 	return (0);
 }
