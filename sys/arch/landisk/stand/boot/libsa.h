0c8223fc1da6f2906711163ba5fb90b2122a9013
drahn <drahn>
2006-10-29 14:47:59 +0000

    Disable cache before jumping into the kernel, makes bootloader more reliable. ok miod@

--- sys/arch/landisk/stand/boot/libsa.h
+++ sys/arch/landisk/stand/boot/libsa.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: libsa.h,v 1.1 2006/10/06 21:48:50 mickey Exp $	*/
+/*	$OpenBSD: libsa.h,v 1.2 2006/10/11 23:06:46 drahn Exp $	*/
 
 /*
  * Copyright (c) 2006 Michael Shalayeff
@@ -33,3 +33,4 @@ void scif_init(unsigned int);
 int scif_getc(void);
 void scif_putc(int);
 void cache_flush(void);
+void cache_disable(void);
