e0c47d599340f2315a32d14f69fe7ace6844a3b1
otto <otto>
2006-11-07 09:09:43 +0000

    Timecounters for amd64. Diff by art@, tested by many. ok deraadt@

--- sys/arch/amd64/include/_types.h
+++ sys/arch/amd64/include/_types.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: _types.h,v 1.1 2006/01/06 18:50:08 millert Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993
@@ -118,5 +118,6 @@ typedef	void *			__wctype_t;
 #define __HAVE_GENERIC_SOFT_INTERRUPTS
 #define __HAVE_CPUINFO
 #define __HAVE_MUTEX
+#define __HAVE_TIMECOUNTER
 
 #endif	/* _AMD64__TYPES_H_ */
