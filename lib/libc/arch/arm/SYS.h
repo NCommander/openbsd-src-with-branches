3f4303dc3f14920e4a649eb59bec5050d1c3f0e6
drahn <drahn>
2006-11-07 21:30:36 +0000

    Load the syscall number into the scratch register (r12) so that we dont need the copyin.

--- lib/libc/arch/arm/SYS.h
+++ lib/libc/arch/arm/SYS.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: SYS.h,v 1.2 2004/02/01 05:40:52 drahn Exp $	*/
+/*	$OpenBSD: SYS.h,v 1.3 2004/03/01 13:41:23 drahn Exp $	*/
 /*	$NetBSD: SYS.h,v 1.8 2003/08/07 16:42:02 agc Exp $	*/
 
 /*-
@@ -53,9 +53,11 @@
 
 #ifdef __STDC__
 #define SYSTRAP(x) \
+			ldr	r12, =SYS_ ## x;		\
 			swi SWI_OS_NETBSD | SYS_ ## x
 #else
 #define SYSTRAP(x) \
+			ldr	r12, =SYS_/**/x;		\
 			swi SWI_OS_NETBSD | SYS_/**/x
 #endif
 
