1fcdae91cf492fa775597b551899fe11cfa6e651
miod <miod>
2006-11-21 23:00:16 +0000

    Fail wi_get_txpower() on USB wi(4), so that we don't CSR_READ on them. ok mickey@ jsg@

--- sys/dev/ic/if_wi.c
+++ sys/dev/ic/if_wi.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_wi.c,v 1.134 2006/08/17 20:36:35 deraadt Exp $	*/
+/*	$OpenBSD: if_wi.c,v 1.135 2006/11/06 10:49:01 jsg Exp $	*/
 
 /*
  * Copyright (c) 1997, 1998, 1999
@@ -127,7 +127,7 @@ u_int32_t	widebug = WIDEBUG;
 
 #if !defined(lint) && !defined(__OpenBSD__)
 static const char rcsid[] =
-	"$OpenBSD: if_wi.c,v 1.134 2006/08/17 20:36:35 deraadt Exp $";
+	"$OpenBSD: if_wi.c,v 1.135 2006/11/06 10:49:01 jsg Exp $";
 #endif	/* lint */
 
 #ifdef foo
@@ -3178,6 +3178,9 @@ wi_get_txpower(struct wi_softc *sc, struct ieee80211_txpower *txpower)
 	u_int16_t	power;
 	int8_t		tmp;
 	int		error;
+
+	if (sc->wi_flags & WI_FLAGS_BUS_USB)
+		return (EOPNOTSUPP);
 
 	/* Get manual TX power */
 	cmd = WI_CMD_READ_MIF;
