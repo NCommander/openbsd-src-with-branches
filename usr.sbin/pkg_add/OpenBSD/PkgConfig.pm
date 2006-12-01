0bd1890d9c6182e01f96324034abb8bfead3e050
espie <espie>
2006-12-01 16:36:37 +0000

    handles continuation lines and comments.

--- usr.sbin/pkg_add/OpenBSD/PkgConfig.pm
+++ usr.sbin/pkg_add/OpenBSD/PkgConfig.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: PkgConfig.pm,v 1.6 2006/11/30 13:18:22 espie Exp $
+# $OpenBSD: PkgConfig.pm,v 1.7 2006/12/01 16:17:03 espie Exp $
 #
 # Copyright (c) 2006 Marc Espie <espie@openbsd.org>
 #
@@ -90,8 +90,16 @@ sub read_fh
 	$name = '' if !defined $name;
 	while (<$fh>) {
 		chomp;
+		# continuation lines
+		while (m/(?<!\\)\\$/) {
+			s/\\$//;
+			$_.=<$fh>;
+			chomp;
+		}
 		next if m/^\s*$/;
 		next if m/^\#/;
+		# zap comments
+		s/(?<!\\)\#.*//;
 		if (m/^([\w.]*)\=\s*(.*)$/) {
 			$cfg->add_variable($1, $2);
 		} elsif (m/^([\w.]*)\:\s+(.*)$/) {
