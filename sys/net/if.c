a140ee1a53412cb2fc9d2399d47e6e40411ec817
reyk <reyk>
2006-12-03 13:41:19 +0000

    Add Rapid Spanning Tree Protocol support (802.1d-2004) based on work by Andrew Thompson (thompsa@freebsd.org). The local changes include adoption to our bridge code, reduced stack usage and many other bits. If stp is enabled, RSTP will now be used by default.
    Thanks for help from Andrew.
    
    This code has been in snaps for while now, commit encouraged by deraadt@

--- sys/net/if.c
+++ sys/net/if.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if.c,v 1.151 2006/11/16 13:09:27 henning Exp $	*/
+/*	$OpenBSD: if.c,v 1.152 2006/11/24 20:57:46 canacar Exp $	*/
 /*	$NetBSD: if.c,v 1.35 1996/05/07 05:26:04 thorpej Exp $	*/
 
 /*
@@ -1029,6 +1029,10 @@ if_down(struct ifnet *ifp)
 	if (ifp->if_carp)
 		carp_carpdev_state(ifp);
 #endif
+#if NBRIDGE > 0
+	if (ifp->if_bridge)
+		bstp_ifstate(ifp);
+#endif
 	rt_ifmsg(ifp);
 }
 
@@ -1057,6 +1061,10 @@ if_up(struct ifnet *ifp)
 #if NCARP > 0
 	if (ifp->if_carp)
 		carp_carpdev_state(ifp);
+#endif
+#if NBRIDGE > 0
+	if (ifp->if_bridge)
+		bstp_ifstate(ifp);
 #endif
 	rt_ifmsg(ifp);
 #ifdef INET6
