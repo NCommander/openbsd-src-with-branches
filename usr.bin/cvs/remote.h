d4feffb26bc1cb1461edcc5f535f39e2806e0691
xsa <xsa>
2006-12-15 08:02:53 +0000

    server-side Sticky bits.

--- usr.bin/cvs/remote.h
+++ usr.bin/cvs/remote.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: remote.h,v 1.8 2006/11/14 15:39:42 xsa Exp $	*/
+/*	$OpenBSD: remote.h,v 1.9 2006/11/27 16:04:10 xsa Exp $	*/
 /*
  * Copyright (c) 2006 Joris Vink <joris@openbsd.org>
  *
@@ -72,6 +72,7 @@ void	cvs_server_questionable(char *);
 void	cvs_server_argument(char *);
 void	cvs_server_argumentx(char *);
 void	cvs_server_set(char *);
+void	cvs_server_sticky(char *);
 
 void	cvs_server_add(char *);
 void	cvs_server_admin(char *);
