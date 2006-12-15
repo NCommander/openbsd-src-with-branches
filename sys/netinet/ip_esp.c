2a31653f1bdee4f9183d77ddcbd5c754f6fbb86b
otto <otto>
2006-12-15 09:32:30 +0000

    make enc(4) count; ok markus@ henning@ deraadt@

--- sys/netinet/ip_esp.c
+++ sys/netinet/ip_esp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_esp.c,v 1.98 2006/05/28 02:04:15 mcbride Exp $ */
+/*	$OpenBSD: ip_esp.c,v 1.99 2006/09/21 11:10:52 otto Exp $ */
 /*
  * The authors of this code are John Ioannidis (ji@tla.org),
  * Angelos D. Keromytis (kermit@csd.uch.gr) and
@@ -739,6 +739,9 @@ esp_output(struct mbuf *m, struct tdb *tdb, struct mbuf **mp, int skip,
 	struct cryptop *crp;
 #if NBPFILTER > 0
 	struct ifnet *ifn = &(encif[0].sc_if);
+
+	ifn->if_opackets++;
+	ifn->if_obytes += m->m_pkthdr.len;
 
 	if (ifn->if_bpf) {
 		struct enchdr hdr;
