0d53c0f92e9e8e1fc8248be7af91bcedc98909dd
niallo <niallo>
2006-12-27 07:43:24 +0000

    - no need to mention -e option twice in usage.
    pointed out by Igor Sobrado <igor at string1.ciencias.uniovi.es>

--- usr.bin/rcs/rcsprog.c
+++ usr.bin/rcs/rcsprog.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rcsprog.c,v 1.133 2006/10/12 17:20:12 niallo Exp $	*/
+/*	$OpenBSD: rcsprog.c,v 1.134 2006/11/13 11:22:57 xsa Exp $	*/
 /*
  * Copyright (c) 2005 Jean-Francois Brousseau <jfb@openbsd.org>
  * All rights reserved.
@@ -160,7 +160,7 @@ void
 rcs_usage(void)
 {
 	fprintf(stderr,
-	    "usage: rcs [-eIiLqTUV] [-Aoldfile] [-ausers] [-b[rev]]\n"
+	    "usage: rcs [-IiLqTUV] [-Aoldfile] [-ausers] [-b[rev]]\n"
 	    "           [-cstring] [-e[users]] [-kmode] [-l[rev]] [-mrev:msg]\n"
 	    "           [-orev] [-sstate[:rev]] [-tstr] [-u[rev]]\n"
 	    "           [-xsuffixes] file ...\n");
