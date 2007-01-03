18430ec606be680bbc3088b46eb36db9f537cbf9
miod <miod>
2007-01-03 21:09:41 +0000

    Errata #7:        Insufficient validation in vga(4) may allow an attacker to gain        root privileges if the kernel is compiled with option PCIAGP and        the actual device is not an AGP device. The PCIAGP option is        present by default on i386 kernels only.

--- sys/dev/pci/agp.c
+++ sys/dev/pci/agp.c
@@ -1,4 +1,4 @@
-/* $OpenBSD$ */
+/* $OpenBSD: agp.c,v 1.1 2006/03/16 21:32:34 matthieu Exp $ */
 /*-
  * Copyright (c) 2000 Doug Rabson
  * All rights reserved.
@@ -145,6 +145,9 @@ agp_ioctl(void *v, u_long cmd, caddr_t addr, int flag, struct proc *pb)
 	agp_unbind *unbind;
 	vsize_t size;
 	int error = 0;
+
+	if (sc->sc_methods == NULL || sc->sc_chipc == NULL)
+		return (ENXIO);
 
 	switch (cmd) {
 	case AGPIOC_INFO:
