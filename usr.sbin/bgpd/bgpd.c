c04b3a3876c4ecb5f08ea0a926229b61a9de394f
henning <henning>
2007-01-04 18:38:51 +0000

    ignore SIGPIPE, like the other 2 processes already do. we detect broken pipes without the signal just fine. ok claudio

--- usr.sbin/bgpd/bgpd.c
+++ usr.sbin/bgpd/bgpd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bgpd.c,v 1.140 2006/11/28 16:39:34 henning Exp $ */
+/*	$OpenBSD: bgpd.c,v 1.141 2007/01/04 12:43:36 claudio Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -228,6 +228,7 @@ main(int argc, char *argv[])
 	signal(SIGHUP, sighdlr);
 	signal(SIGALRM, sighdlr);
 	signal(SIGUSR1, sighdlr);
+	signal(SIGPIPE, SIG_IGN);
 
 	close(pipe_m2s[1]);
 	close(pipe_m2r[1]);
