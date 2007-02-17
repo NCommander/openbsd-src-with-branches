9ac29f05c91fa59dd60f85bd75669c7354cbc170
jmc <jmc>
2007-02-17 13:32:15 +0000

    no code for -p, so remove from man page and usage(); confirmed by ckuethe; spotted by Mark Lumsden

--- usr.sbin/dhcpd/dhcpd.c
+++ usr.sbin/dhcpd/dhcpd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dhcpd.c,v 1.29 2006/06/14 20:19:20 jmc Exp $ */
+/*	$OpenBSD: dhcpd.c,v 1.30 2006/11/22 19:10:39 stevesk Exp $ */
 
 /*
  * Copyright (c) 2004 Henning Brauer <henning@cvs.openbsd.org>
@@ -193,7 +193,7 @@ usage(void)
 	fprintf(stderr, " [-C changed_ip_table]\n");
 	fprintf(stderr, "\t[-c config-file] [-L leased_ip_table]");
 	fprintf(stderr, " [-l lease-file]\n");
-	fprintf(stderr, "\t[-p pf-device] [if0 [...ifN]]\n");
+	fprintf(stderr, "\t[if0 [...ifN]]\n");
 	exit(1);
 }
 
