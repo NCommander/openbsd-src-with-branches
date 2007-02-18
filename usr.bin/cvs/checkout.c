87a80f264c95df5feee9467e7ed097d5fe1a65ad
otto <otto>
2007-02-18 10:30:45 +0000

    an erroneous free() crept in due to a merge error

--- usr.bin/cvs/checkout.c
+++ usr.bin/cvs/checkout.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: checkout.c,v 1.89 2007/01/31 21:07:35 xsa Exp $	*/
+/*	$OpenBSD: checkout.c,v 1.90 2007/02/17 18:23:43 xsa Exp $	*/
 /*
  * Copyright (c) 2006 Joris Vink <joris@openbsd.org>
  *
@@ -332,7 +332,6 @@ cvs_checkout_file(struct cvs_file *cf, RCSNUM *rnum, int co_flags)
 				(void)unlink(template);
 				cvs_worklist_run(&temp_files,
 				    cvs_worklist_unlink);
-				xfree(template);
 			}
 		}
 
