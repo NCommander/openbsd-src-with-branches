8018aa798f2e155ccfe108f59ce91906516a959e
jmc <jmc>
2007-02-21 06:22:25 +0000

    simplify synopsis and usage(); from Igor Sobrado

--- sbin/quotacheck/quotacheck.c
+++ sbin/quotacheck/quotacheck.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: quotacheck.c,v 1.19 2003/08/25 23:28:15 tedu Exp $	*/
+/*	$OpenBSD: quotacheck.c,v 1.20 2005/04/05 00:13:49 deraadt Exp $	*/
 /*	$NetBSD: quotacheck.c,v 1.12 1996/03/30 22:34:25 mark Exp $	*/
 
 /*
@@ -43,7 +43,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)quotacheck.c	8.3 (Berkeley) 1/29/94";
 #else
-static char rcsid[] = "$OpenBSD: quotacheck.c,v 1.19 2003/08/25 23:28:15 tedu Exp $";
+static char rcsid[] = "$OpenBSD: quotacheck.c,v 1.20 2005/04/05 00:13:49 deraadt Exp $";
 #endif
 #endif /* not lint */
 
@@ -207,9 +207,9 @@ main(int argc, char *argv[])
 void
 usage(void)
 {
-	(void)fprintf(stderr, "usage:\t%s\n\t%s\n",
-	    "quotacheck -a [-dguv] [-l <maxparallel>]",
-	    "quotacheck [-dguv] filesys ...");
+	extern char *__progname;
+	(void)fprintf(stderr, "usage: %s [-adguv] [-l maxparallel] "
+	    "filesystem ...\n", __progname);
 	exit(1);
 }
 
