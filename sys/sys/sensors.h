1af29dd31fbaf4113ea1c7880670b60b169ccc8a
deraadt <deraadt>
2007-02-23 22:55:40 +0000

    add missing copyright; Constantine

--- sys/sys/sensors.h
+++ sys/sys/sensors.h
@@ -1,7 +1,8 @@
-/*	$OpenBSD: sensors.h,v 1.20 2006/12/23 17:40:39 deraadt Exp $	*/
+/*	$OpenBSD: sensors.h,v 1.21 2006/12/27 13:04:29 mbalmer Exp $	*/
 
 /*
  * Copyright (c) 2003, 2004 Alexander Yurchenko <grange@openbsd.org>
+ * Copyright (c) 2006 Constantine A. Murenin <cnst+openbsd@bugmail.mojo.ru>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
