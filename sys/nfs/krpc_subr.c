4bfb0ef71a9bb6b24fc73e427151687fdb3e759a
deraadt <deraadt>
2007-02-27 19:09:56 +0000

    clarify RPC timeout messages so regular people can understand; ok various

--- sys/nfs/krpc_subr.c
+++ sys/nfs/krpc_subr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: krpc_subr.c,v 1.12 2001/11/14 23:37:33 mickey Exp $	*/
+/*	$OpenBSD: krpc_subr.c,v 1.13 2004/11/18 15:09:07 markus Exp $	*/
 /*	$NetBSD: krpc_subr.c,v 1.12.4.1 1996/06/07 00:52:26 cgd Exp $	*/
 
 /*
@@ -349,8 +349,9 @@ krpc_call(sa, prog, vers, func, data, from_p, retries)
 		if (timo < MAX_RESEND_DELAY)
 			timo++;
 		else
-			printf("RPC timeout for server 0x%x\n",
-			       ntohl(sin->sin_addr.s_addr));
+			printf("RPC timeout for server %s (0x%x) prog %u\n",
+			    inet_ntoa(sin->sin_addr),
+			    ntohl(sin->sin_addr.s_addr), prog);
 
 		/*
 		 * Wait for up to timo seconds for a reply.
