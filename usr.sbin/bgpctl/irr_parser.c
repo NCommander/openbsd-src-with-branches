2c8b4b3b39daa10a1e82f8a28e576283cfb0e90d
henning <henning>
2007-03-05 17:28:59 +0000

    need to eat whitespace at both ends of val

--- usr.sbin/bgpctl/irr_parser.c
+++ usr.sbin/bgpctl/irr_parser.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: irr_parser.c,v 1.4 2007/03/04 12:37:07 henning Exp $ */
+/*	$OpenBSD: irr_parser.c,v 1.5 2007/03/04 18:13:13 henning Exp $ */
 
 /*
  * Copyright (c) 2007 Henning Brauer <henning@openbsd.org>
@@ -69,8 +69,7 @@ parse_response(FILE *f, enum qtype qtype)
 			warnx("no \":\" found!");
 			return (-1);
 		}
-		while (ISWS(*val))
-			val++;
+		EATWS(val);
 
 		switch (qtype) {
 		case QTYPE_OWNAS:
