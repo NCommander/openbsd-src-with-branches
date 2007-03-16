cf7c69f3e2fa384bb19d5e2c458cd095a20c855e
art <art>
2007-03-16 09:28:38 +0000

    Timecounters for sparc64. The cleanest and easiest timecounter implementation this far. Uses the %tick register (as microtime was using before).
    kettenis@ ok

--- sys/arch/sparc64/include/_types.h
+++ sys/arch/sparc64/include/_types.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: _types.h,v 1.1 2006/01/06 18:50:09 millert Exp $	*/
+/*	$OpenBSD: _types.h,v 1.2 2007/02/03 20:08:50 miod Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993
@@ -124,5 +124,6 @@ typedef	void *			__wctype_t;
 #define __HAVE_DEVICE_REGISTER
 #define __HAVE_GENERIC_SOFT_INTERRUPTS
 #define	__HAVE_MUTEX
+#define __HAVE_TIMECOUNTER
 
 #endif	/* _SPARC64__TYPES_H_ */
