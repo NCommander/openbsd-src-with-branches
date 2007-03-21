42cf7cee0824fea29ccfd42981fbeb557403e2ff
tedu <tedu>
2007-03-21 03:29:05 +0000

    remove extra *p test, from charles longeau ok deraadt millert

--- sys/arch/zaurus/stand/zboot/cmd.c
+++ sys/arch/zaurus/stand/zboot/cmd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cmd.c,v 1.1 2005/04/16 17:23:34 uwe Exp $	*/
+/*	$OpenBSD: cmd.c,v 1.2 2005/05/11 16:42:15 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1997-1999 Michael Shalayeff
@@ -397,7 +397,7 @@ Xstty(void)
 		    cnspeed(dev, -1));
 	else {
 		sp = 0;
-		for (cp = cmd.argv[2]; *cp && isdigit(*cp); cp++)
+		for (cp = cmd.argv[2]; isdigit(*cp); cp++)
 			sp = sp * 10 + (*cp - '0');
 		cnspeed(dev, sp);
 	}
