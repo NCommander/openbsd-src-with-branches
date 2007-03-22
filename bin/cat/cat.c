9ef92345868fe89f87b93986cd6b52900504153f
millert <millert>
2007-03-22 18:35:07 +0000

    Fix handling of multiple stdin arguments in cooked mode, closes PR 5418. From Johan Veenhuizen

--- bin/cat/cat.c
+++ bin/cat/cat.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cat.c,v 1.16 2004/05/21 02:11:33 jolan Exp $	*/
+/*	$OpenBSD: cat.c,v 1.17 2005/04/05 01:18:53 jaredy Exp $	*/
 /*	$NetBSD: cat.c,v 1.11 1995/09/07 06:12:54 jtc Exp $	*/
 
 /*
@@ -43,7 +43,7 @@ static char copyright[] =
 #if 0
 static char sccsid[] = "@(#)cat.c	8.2 (Berkeley) 4/27/95";
 #else
-static char rcsid[] = "$OpenBSD: cat.c,v 1.16 2004/05/21 02:11:33 jolan Exp $";
+static char rcsid[] = "$OpenBSD: cat.c,v 1.17 2005/04/05 01:18:53 jaredy Exp $";
 #endif
 #endif /* not lint */
 
@@ -139,7 +139,9 @@ cook_args(char **argv)
 			filename = *argv++;
 		}
 		cook_buf(fp);
-		if (fp != stdin)
+		if (fp == stdin)
+			clearerr(fp);
+		else
 			(void)fclose(fp);
 	} while (*argv);
 }
