38f59e55bfc9c26af559a5b2a83c8c1a27717ee8
art <art>
2007-03-26 08:43:34 +0000

    Rip out the KERN_ error codes. ok otto@

--- sys/uvm/uvm_param.h
+++ sys/uvm/uvm_param.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_param.h,v 1.10 2005/11/25 15:52:11 mickey Exp $	*/
+/*	$OpenBSD: uvm_param.h,v 1.11 2007/03/25 11:31:07 art Exp $	*/
 /*	$NetBSD: uvm_param.h,v 1.5 2001/03/09 01:02:12 chs Exp $	*/
 
 /* 
@@ -134,17 +134,6 @@ struct _ps_strings {
 };
 
 #define SWAPSKIPBYTES	8192	/* never use at the start of a swap space */
-
-/* 
- *	Return values from the VM routines.
- */
-#define	KERN_INVALID_ADDRESS	EFAULT
-#define	KERN_PROTECTION_FAILURE	EACCES
-#define	KERN_NO_SPACE		ENOMEM
-#define	KERN_INVALID_ARGUMENT	EINVAL
-#define	KERN_FAILURE		EFAULT
-#define	KERN_RESOURCE_SHORTAGE	ENOMEM
-#define	KERN_PAGES_LOCKED	9		/* XXX never returned */
 
 #ifndef ASSEMBLER
 /*
