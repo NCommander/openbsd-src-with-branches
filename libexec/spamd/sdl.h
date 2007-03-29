58d9df089142ee638656215ebb9ceeac81c3b0fe
kjell <kjell>
2007-03-29 17:39:53 +0000

    I relinquish any copyright claim I have on this file

--- libexec/spamd/sdl.h
+++ libexec/spamd/sdl.h
@@ -1,7 +1,7 @@
-/*	$OpenBSD: sdl.h,v 1.3 2005/11/30 20:44:07 deraadt Exp $ */
+/*	$OpenBSD: sdl.h,v 1.4 2007/03/26 16:55:45 beck Exp $ */
 
 /*
- * Copyright (c) 2003-2007 Bob Beck, Kjell Wooding.  All rights reserved.
+ * Copyright (c) 2003-2007 Bob Beck.  All rights reserved.
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
