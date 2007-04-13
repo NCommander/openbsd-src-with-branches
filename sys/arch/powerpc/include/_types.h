2362b05fd0e76e8c3cb7e3b75fb3eea7df5893f1
kettenis <kettenis>
2007-04-13 18:48:38 +0000

    Timecounters for macppc.  As clean and simple as sparc64.  Uses the %tb special register.
    ok drahn@

--- sys/arch/powerpc/include/_types.h
+++ sys/arch/powerpc/include/_types.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: _types.h,v 1.3 2007/03/20 20:59:53 kettenis Exp $	*/
+/*	$OpenBSD: _types.h,v 1.4 2007/03/22 19:26:28 kettenis Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993
@@ -118,5 +118,6 @@ typedef	void *			__wctype_t;
 /* Feature test macros */
 #define __HAVE_CPUINFO
 #define __HAVE_MUTEX
+#define __HAVE_TIMECOUNTER
 
 #endif	/* _POWERPC__TYPES_H_ */
