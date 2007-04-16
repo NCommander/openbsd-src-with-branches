bb87138a0e9d94d72e96fbb22181de72f0f0503c
dlg <dlg>
2007-04-16 12:00:25 +0000

    macppcs can boot off usb disks, so treat "/usb" controllers in the ofw tree as storage controllers.
    this lets ottos macppc root off the usb disk he booted off.

--- sys/arch/macppc/macppc/autoconf.c
+++ sys/arch/macppc/macppc/autoconf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: autoconf.c,v 1.22 2005/12/27 18:31:09 miod Exp $	*/
+/*	$OpenBSD: autoconf.c,v 1.24 2007/03/28 06:17:37 dlg Exp $	*/
 /*
  * Copyright (c) 1996, 1997 Per Fogelstrom
  * Copyright (c) 1995 Theo de Raadt
@@ -37,7 +37,7 @@
  * from: Utah Hdr: autoconf.c 1.31 91/01/21
  *
  *	from: @(#)autoconf.c	8.1 (Berkeley) 6/10/93
- *      $Id: autoconf.c,v 1.23 2007/03/27 07:23:33 deraadt Exp $
+ *      $Id: autoconf.c,v 1.24 2007/03/28 06:17:37 dlg Exp $
  */
 
 /*
@@ -509,6 +509,7 @@ findtype(char **s)
 		{ "/k2-sata",		"wd", T_IDE },
 		{ "/disk@",		"sd", T_DISK },
 		{ "/disk",		"wd", T_DISK },
+		{ "/usb@",		"sd", T_SCSI },
 		{ "/bcom5704@4",	"bge0", T_IFACE },
 		{ "/bcom5704@4,1",	"bge1", T_IFACE },
 		{ "/ethernet",		"gem0", T_IFACE },
