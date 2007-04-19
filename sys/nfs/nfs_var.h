19dab6870041b1f71ac0193ab16969fb12741099
thib <thib>
2007-04-19 14:46:44 +0000

    Replace the nfskevq_lock lockmgr lock with rwlock. Replace nfs_kqinit() wich just calls lockinit with RWLOCK_INITALIZER. Assorted cleanup.
    ok tedu@
    "reads good" art@

--- sys/nfs/nfs_var.h
+++ sys/nfs/nfs_var.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: nfs_var.h,v 1.24 2007/04/11 12:18:54 thib Exp $	*/
+/*	$OpenBSD: nfs_var.h,v 1.25 2007/04/13 17:09:22 thib Exp $	*/
 /*	$NetBSD: nfs_var.h,v 1.3 1996/02/18 11:53:54 fvdl Exp $	*/
 
 /*
@@ -286,5 +286,4 @@ int nfs_savenickauth(struct nfsmount *, struct ucred *, int, NFSKERBKEY_T,
 
 /* nfs_kq.c */
 int  nfs_kqfilter(void *);
-void nfs_kqinit(void);
 
