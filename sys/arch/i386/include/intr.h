4f1604ae92f25602a2c5d672fe172dfe2f7f41d4
gwk <gwk>
2007-04-21 21:06:15 +0000

    Introduce a smp aware hw.setperf mechanism, it will scale all CPUs or cores by the same amount, i.e. if you do hw.setperf=50 both cores will be scaled to the opearting state corresponing to 50%. Tested by many with est (mainly on core2duo machines like X60 thinkpads). Only enable est during GENERIC.MP build no one tested powernow.
    ok art@

--- sys/arch/i386/include/intr.h
+++ sys/arch/i386/include/intr.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: intr.h,v 1.28 2007/03/23 16:03:52 art Exp $	*/
+/*	$OpenBSD: intr.h,v 1.29 2007/04/03 10:14:47 art Exp $	*/
 /*	$NetBSD: intr.h,v 1.5 1996/05/13 06:11:28 mycroft Exp $	*/
 
 /*
@@ -145,6 +145,7 @@ void i386_intlock(int);
 void i386_intunlock(int);
 void i386_softintlock(void);
 void i386_softintunlock(void);
+void i386_setperf_ipi(struct cpu_info *);
 
 extern void (*ipifunc[I386_NIPI])(struct cpu_info *);
 #endif
