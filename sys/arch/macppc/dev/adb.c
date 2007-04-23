3e5ab63fce41afc7f858136e58208f1a338db44f
deraadt <deraadt>
2007-04-23 16:27:20 +0000

    rename drivers with numbers in them; ok gwk drahn

--- sys/arch/macppc/dev/adb.c
+++ sys/arch/macppc/dev/adb.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: adb.c,v 1.25 2007/04/10 17:47:54 miod Exp $	*/
+/*	$OpenBSD: adb.c,v 1.26 2007/04/22 22:31:14 deraadt Exp $	*/
 /*	$NetBSD: adb.c,v 1.6 1999/08/16 06:28:09 tsubai Exp $	*/
 /*	$NetBSD: adb_direct.c,v 1.14 2000/06/08 22:10:45 tsubai Exp $	*/
 
@@ -1709,7 +1709,7 @@ adbattach(struct device *parent, struct device *self, void *aux)
 		if (OF_getprop(node, "name", name, sizeof name) <= 0)
 			continue;
 		if (strcmp(name, "pmu-i2c") == 0) {
-			nca.ca_name = "pi2c";
+			nca.ca_name = "piic";
 			nca.ca_node = node;
 			config_found(self, &nca, NULL);
 		}
