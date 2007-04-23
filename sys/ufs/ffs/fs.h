3ef571495b3a605e524079cc78de9c531cc6d43c
pedro <pedro>
2007-04-23 10:18:30 +0000

    Remove 'cg_space' from 'struct cg'. Due to the alignment on 64-bit architectures, CGSIZE() was returning something sligthly over one block. The 'new' fsck would round this value up to a fragment boundary, and end up trying to access memory beyond allocated space. From mickey@, okay pedro@, millert@ and otto@.

--- sys/ufs/ffs/fs.h
+++ sys/ufs/ffs/fs.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fs.h,v 1.27 2007/03/19 13:27:47 pedro Exp $	*/
+/*	$OpenBSD: fs.h,v 1.28 2007/04/20 16:30:31 millert Exp $	*/
 /*	$NetBSD: fs.h,v 1.6 1995/04/12 21:21:02 mycroft Exp $	*/
 
 /*
@@ -397,7 +397,6 @@ struct cg {
 	int32_t	 cg_sparecon32[3];	/* reserved for future use */
 	int64_t	 cg_ffs2_time;		/* time last written */
 	int64_t	 cg_sparecon64[3];	/* reserved for future use */
-	u_int8_t cg_space[1];		/* space for cylinder group maps */
 /* actually longer */
 };
 
