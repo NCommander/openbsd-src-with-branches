c54607fc04f797232420efabd7cdf2b6a1a2583a
deraadt <deraadt>
2007-04-26 20:28:25 +0000

    enable splassert (1) by default; requested and ok many

--- sys/kern/subr_prf.c
+++ sys/kern/subr_prf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: subr_prf.c,v 1.68 2006/11/17 09:21:52 jmc Exp $	*/
+/*	$OpenBSD: subr_prf.c,v 1.69 2006/12/11 05:08:18 deraadt Exp $	*/
 /*	$NetBSD: subr_prf.c,v 1.45 1997/10/24 18:14:25 chuck Exp $	*/
 
 /*-
@@ -132,7 +132,7 @@ int	db_console = 0;
 /*
  * panic on spl assertion failure?
  */
-int splassert_ctl = 0;
+int splassert_ctl = 1;
 
 /*
  * v_putc: routine to putc on virtual console
