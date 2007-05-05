7708eb3a7d96ad512ae30e8ba281848ca3667a16
cloder <cloder>
2007-05-05 17:43:34 +0000

    Kill a log message which looks like an error message but is actually both meaningless and harmless. ("nat_t_check_vendor_payload: bad size") ok todd

--- sbin/isakmpd/nat_traversal.c
+++ sbin/isakmpd/nat_traversal.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: nat_traversal.c,v 1.18 2007/02/22 10:01:02 hshoexer Exp $	*/
+/*	$OpenBSD: nat_traversal.c,v 1.19 2007/04/16 13:01:39 moritz Exp $	*/
 
 /*
  * Copyright (c) 2004 Håkan Olsson.  All rights reserved.
@@ -193,9 +193,6 @@ nat_t_check_vendor_payload(struct message *msg, struct payload *p)
 
 	for (i = 0; i < NUMNATTCAP; i++) {
 		if (vlen != isakmp_nat_t_cap[i].hashsize) {
-			LOG_DBG((LOG_EXCHANGE, 50, "nat_t_check_vendor_payload: "
-			    "bad size %lu != %lu", (unsigned long)vlen,
-			    (unsigned long)isakmp_nat_t_cap[i].hashsize));
 			continue;
 		}
 		if (memcmp(isakmp_nat_t_cap[i].hash, pbuf + ISAKMP_GEN_SZ,
