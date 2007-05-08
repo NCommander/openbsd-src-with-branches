425dff9fdcaf262b4d69c07c479b14307521a06e
deraadt <deraadt>
2007-05-08 18:50:06 +0000

    places they get called are #ifdef notdef, so the code itself can be too

--- sys/dev/pcmcia/if_ne_pcmcia.c
+++ sys/dev/pcmcia/if_ne_pcmcia.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ne_pcmcia.c,v 1.88 2006/07/09 19:37:00 miod Exp $	*/
+/*	$OpenBSD: if_ne_pcmcia.c,v 1.89 2006/09/22 22:19:53 fgsch Exp $	*/
 /*	$NetBSD: if_ne_pcmcia.c,v 1.17 1998/08/15 19:00:04 thorpej Exp $	*/
 
 /*
@@ -879,6 +879,7 @@ ne_pcmcia_activate(dev, act)
 	return (0);
 }
 
+#ifdef notyet
 int
 ne_pcmcia_enable(dsc)
 	struct dp8390_softc *dsc;
@@ -906,6 +907,7 @@ ne_pcmcia_disable(dsc)
 	pcmcia_intr_disestablish(psc->sc_pf, psc->sc_ih);
 	pcmcia_function_disable(psc->sc_pf);
 }
+#endif
 
 u_int8_t *
 ne_pcmcia_get_enaddr(psc, maddr, myea)
