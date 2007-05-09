f95b7fbb2627cf6c13e0a444a27d755b9c416fea
deraadt <deraadt>
2007-05-09 17:04:22 +0000

    unused function; ok pedro

--- sys/ufs/ufs/ufs_extern.h
+++ sys/ufs/ufs/ufs_extern.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ufs_extern.h,v 1.26 2006/10/03 19:49:06 pedro Exp $	*/
+/*	$OpenBSD: ufs_extern.h,v 1.27 2007/01/16 17:52:18 thib Exp $	*/
 /*	$NetBSD: ufs_extern.h,v 1.5 1996/02/09 22:36:03 christos Exp $	*/
 
 /*-
@@ -78,7 +78,6 @@ int	 ufs_remove(void *);
 int	 ufs_rename(void *);
 #define  ufs_revoke  vop_generic_revoke
 int	 ufs_rmdir(void *);
-int	 ufs_seek(void *);
 int	 ufs_poll(void *);
 int	 ufs_kqfilter(void *);
 int	 ufs_setattr(void *);
