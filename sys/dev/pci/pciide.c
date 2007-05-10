98ef00dc097f99139fee98fc94983bd0a2738220
kettenis <kettenis>
2007-05-10 17:41:50 +0000

    Don't skip channels that don't have the WDCF_IRQ_WAIT flag set.  Apparently some chips generate interrupts we don't expect, but we need to ack those. Fixes the cdrom hangs on various sparc64 systems.
    tested by some, ok deraadt@

--- sys/dev/pci/pciide.c
+++ sys/dev/pci/pciide.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pciide.c,v 1.265 2007/03/27 15:40:18 deraadt Exp $	*/
+/*	$OpenBSD: pciide.c,v 1.266 2007/05/02 00:53:25 jsg Exp $	*/
 /*	$NetBSD: pciide.c,v 1.127 2001/08/03 01:31:08 tsutsui Exp $	*/
 
 /*
@@ -1493,9 +1493,6 @@ pciide_pci_intr(void *arg)
 
 		/* If a compat channel skip. */
 		if (cp->compat)
-			continue;
-		/* if this channel not waiting for intr, skip */
-		if ((wdc_cp->ch_flags & WDCF_IRQ_WAIT) == 0)
 			continue;
 
 		if (pciide_intr_flag(cp) == 0)
