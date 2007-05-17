a1155f03fe927cf5ab12eceb72f97807268db7e9
jolan <jolan>
2007-05-17 23:53:41 +0000

    djm owes me a vb and a tism cd for breaking ssh compilation

--- usr.bin/ssh/sshconnect2.c
+++ usr.bin/ssh/sshconnect2.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: sshconnect2.c,v 1.162 2006/08/30 00:06:51 dtucker Exp $ */
+/* $OpenBSD: sshconnect2.c,v 1.163 2007/05/17 20:48:13 djm Exp $ */
 /*
  * Copyright (c) 2000 Markus Friedl.  All rights reserved.
  *
@@ -30,6 +30,7 @@
 #include <sys/stat.h>
 
 #include <errno.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <string.h>
 #include <signal.h>
