dd3d13854f7cf126d000a8cb6fdc4211d5a8b097
miod <miod>
2007-05-20 17:22:51 +0000

    On AV-class machines, SCSI interrupts still come from VIA2, not PSC, so IPL_BIO should not be modified. Found the hard way by martin@

--- sys/arch/mac68k/mac68k/intr.c
+++ sys/arch/mac68k/mac68k/intr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: intr.c,v 1.6 2006/01/01 13:16:01 miod Exp $	*/
+/*	$OpenBSD: intr.c,v 1.7 2006/03/13 19:39:52 brad Exp $	*/
 /*	$NetBSD: intr.c,v 1.2 1998/08/25 04:03:56 scottr Exp $	*/
 
 /*-
@@ -99,7 +99,7 @@ intr_init()
 		mac68k_statclockipl = (PSL_S | PSL_IPL2);
 
 		if (current_mac_model->class == MACH_CLASSAV)
-			mac68k_bioipl = mac68k_netipl = (PSL_S | PSL_IPL4);
+			mac68k_netipl = (PSL_S | PSL_IPL4);
 	}
 	
 	intr_computeipl();
