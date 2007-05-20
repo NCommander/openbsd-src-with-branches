9a58eadad35a4a28859e296cc4034fccf9277151
miod <miod>
2007-05-20 20:12:32 +0000

    Since we no longer use 3 bits but the whole 7 to get the processor revision number, we should test for 10, not 2, as the revision for which the xxx.usr errata applies; also, going through the errata, revision 2/10 (1010x) _is_ affected.

--- sys/arch/m88k/m88k/m8820x_machdep.c
+++ sys/arch/m88k/m88k/m8820x_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: m8820x_machdep.c,v 1.24 2007/02/11 12:49:37 miod Exp $	*/
+/*	$OpenBSD: m8820x_machdep.c,v 1.25 2007/03/22 18:49:18 miod Exp $	*/
 /*
  * Copyright (c) 2004, Miodrag Vallat.
  *
@@ -336,7 +336,7 @@ m8820x_cpu_configuration_print(int main)
 	{
 		static int errata_warn = 0;
 
-		if (proctype == ARN_88100 && procvers < 2) {
+		if (proctype == ARN_88100 && procvers <= 10) {
 			if (!errata_warn++)
 				printf("WARNING: M88100 bug workaround code "
 				    "not enabled.\nPlease recompile the kernel "
