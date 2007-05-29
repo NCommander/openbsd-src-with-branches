5bab7766038e1d4457808043f4e6cbb25c2bab0b
tedu <tedu>
2007-05-29 07:35:55 +0000

    theo says degrees is spelled degrees

--- sys/arch/amd64/amd64/identcpu.c
+++ sys/arch/amd64/amd64/identcpu.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: identcpu.c,v 1.12 2007/05/06 03:37:08 gwk Exp $	*/
+/*	$OpenBSD: identcpu.c,v 1.13 2007/05/29 06:31:44 tedu Exp $	*/
 /*	$NetBSD: identcpu.c,v 1.1 2003/04/26 18:39:28 fvdl Exp $	*/
 
 /*
@@ -134,7 +134,7 @@ intelcore_update_sensor(void *args)
 	msr = rdmsr(MSR_THERM_STATUS);
 	if (msr & MSR_THERM_STATUS_VALID_BIT) {
 		ci->ci_sensor.value = max - MSR_THERM_STATUS_TEMP(msr);
-		/* micro degress */
+		/* micro degrees */
 		ci->ci_sensor.value *= 1000000;
 		/* kelvin */
 		ci->ci_sensor.value += 273150000;
