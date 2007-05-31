26a371c6deb09d8c5ac2fe82233429a35f0ca163
espie <espie>
2007-05-31 13:11:21 +0000

    oops, move realname up so that ::Sample sees it too.
    remove last traces of MODULEs.

--- usr.sbin/pkg_add/OpenBSD/PackingList.pm
+++ usr.sbin/pkg_add/OpenBSD/PackingList.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: PackingList.pm,v 1.71 2007/05/30 12:29:19 espie Exp $
+# $OpenBSD: PackingList.pm,v 1.72 2007/05/30 16:32:14 espie Exp $
 #
 # Copyright (c) 2003-2007 Marc Espie <espie@openbsd.org>
 #
@@ -366,7 +366,7 @@ our @unique_categories =
 
 our @list_categories =
     (qw(conflict pkgpath incompatibility updateset depend 
-    	wantlib module groups users items));
+    	wantlib groups users items));
 
 our @cache_categories =
     (qw(depend wantlib));
