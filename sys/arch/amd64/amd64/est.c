c80429a03b2a766b9fb110c3aa52333f06ade684
jsg <jsg>
2007-06-04 06:57:57 +0000

    Use 166.67 and 266.67 for bus_clock and not 166.66 and 266.66, as per recommended values from Intel documentation. ok tedu@

--- sys/arch/amd64/amd64/est.c
+++ sys/arch/amd64/amd64/est.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: est.c,v 1.2 2007/06/01 22:28:21 tedu Exp $ */
+/*	$OpenBSD: est.c,v 1.3 2007/06/03 04:30:31 jsg Exp $ */
 /*
  * Copyright (c) 2003 Michael Eriksson.
  * All rights reserved.
@@ -167,13 +167,13 @@ p3_get_bus_clock(struct cpu_info *ci)
 			bus_clock = 13333;
 			break;
 		case 3:
-			bus_clock = 16666;
+			bus_clock = 16667;
 			break;
 		case 2:
 			bus_clock = 20000;
 			break;
 		case 0:
-			bus_clock = 26666;
+			bus_clock = 26667;
 			break;
 		case 4:
 			bus_clock = 33333;
