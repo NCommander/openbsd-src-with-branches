898bfaba32105ef96141fa9adf2db507bffffb40
pvalchev <pvalchev>
2007-06-07 19:41:46 +0000

    test umac-64@openssh.com ok djm@

--- regress/usr.bin/ssh/cipher-speed.sh
+++ regress/usr.bin/ssh/cipher-speed.sh
@@ -1,4 +1,4 @@
-#	$OpenBSD: cipher-speed.sh,v 1.1 2003/11/25 22:11:49 markus Exp $
+#	$OpenBSD: cipher-speed.sh,v 1.2 2005/05/24 04:09:54 djm Exp $
 #	Placed in the Public Domain.
 
 tid="cipher speed"
@@ -12,7 +12,7 @@ tries="1 2"
 DATA=/bin/ls
 DATA=/bsd
 
-macs="hmac-sha1 hmac-md5 hmac-sha1-96 hmac-md5-96"
+macs="hmac-sha1 hmac-md5 umac-64@openssh.com hmac-sha1-96 hmac-md5-96"
 ciphers="aes128-cbc 3des-cbc blowfish-cbc cast128-cbc 
 	arcfour128 arcfour256 arcfour aes192-cbc aes256-cbc aes128-ctr"
 
