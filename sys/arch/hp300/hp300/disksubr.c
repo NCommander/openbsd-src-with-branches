a9365d0dbfe5e966552df8757b5c1cbb967d9881
deraadt <deraadt>
2007-06-14 03:29:34 +0000

    do not depend on previous loop variable, use a constant instead

--- sys/arch/hp300/hp300/disksubr.c
+++ sys/arch/hp300/hp300/disksubr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: disksubr.c,v 1.36 2007/06/09 23:06:46 krw Exp $	*/
+/*	$OpenBSD: disksubr.c,v 1.37 2007/06/12 20:57:42 deraadt Exp $	*/
 /*	$NetBSD: disksubr.c,v 1.9 1997/04/01 03:12:13 scottr Exp $	*/
 
 /*
@@ -74,9 +74,9 @@ readdisklabel(dev_t dev, void (*strat)(struct buf *),
 		DL_SETPSIZE(&lp->d_partitions[i], 0);
 		DL_SETPOFFSET(&lp->d_partitions[i], 0);
 	}
-	if (DL_GETPSIZE(&lp->d_partitions[i]) == 0)
-		DL_SETPSIZE(&lp->d_partitions[i], DL_GETDSIZE(lp));
-	DL_SETPOFFSET(&lp->d_partitions[i], 0);
+	if (DL_GETPSIZE(&lp->d_partitions[RAW_PART]) == 0)
+		DL_SETPSIZE(&lp->d_partitions[RAW_PART], DL_GETDSIZE(lp));
+	DL_SETPOFFSET(&lp->d_partitions[RAW_PART], 0);
 
 	/* don't read the on-disk label if we are in spoofed-only mode */
 	if (spoofonly)
