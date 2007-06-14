aee3a1f3e1205bb19bf102883967d34168d72bfc
deraadt <deraadt>
2007-06-14 19:39:35 +0000

    bzero the right thing

--- sys/arch/sparc64/dev/fd.c
+++ sys/arch/sparc64/dev/fd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fd.c,v 1.15 2007/06/06 21:56:14 deraadt Exp $	*/
+/*	$OpenBSD: fd.c,v 1.16 2007/06/08 05:27:58 deraadt Exp $	*/
 /*	$NetBSD: fd.c,v 1.112 2003/08/07 16:29:35 agc Exp $	*/
 
 /*-
@@ -2010,7 +2010,7 @@ fdgetdisklabel(dev)
 	char *errstring;
 
 	bzero(lp, sizeof(struct disklabel));
-	bzero(lp, sizeof(struct cpu_disklabel));
+	bzero(clp, sizeof(struct cpu_disklabel));
 
 	lp->d_type = DTYPE_FLOPPY;
 	lp->d_secsize = FD_BSIZE(fd);
