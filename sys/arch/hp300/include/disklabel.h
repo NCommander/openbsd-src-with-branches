948581ef15daa1d0e738f3a438f21bb03f7d30b1
deraadt <deraadt>
2007-06-18 17:11:52 +0000

    delete comment above cpu_disklabel that is not needed

--- sys/arch/hp300/include/disklabel.h
+++ sys/arch/hp300/include/disklabel.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: disklabel.h,v 1.5 2001/12/06 23:47:26 miod Exp $	*/
+/*	$OpenBSD: disklabel.h,v 1.6 2007/06/17 00:27:27 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1994 Christopher G. Demetriou
@@ -37,7 +37,6 @@
 #define	LABELOFFSET	0			/* offset of label in sector */
 #define	MAXPARTITIONS	16			/* number of partitions */
 
-/* Just a dummy */
 struct cpu_disklabel {
 };
 
