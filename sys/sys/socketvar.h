c4945a232f43639a9be0c13e64463604201f44f7
dim <dim>
2007-07-05 09:04:04 +0000

    From FreeBSD:
      Fix a bug in sblock() that has existed since revision 1.1 from BSD:
      correctly return an error if M_NOWAIT is passed to sblock() and the
      operation might block.  This remarkably subtle macro bug appears to
      be responsible for quite a few undiagnosed socket buffer corruption
      and mbuf-related kernel panics.
    
    "diff is correct" todd@, "should go in asap" markus@

--- sys/sys/socketvar.h
+++ sys/sys/socketvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: socketvar.h,v 1.38 2005/11/21 18:16:46 millert Exp $	*/
+/*	$OpenBSD: socketvar.h,v 1.39 2007/02/26 23:53:33 kurt Exp $	*/
 /*	$NetBSD: socketvar.h,v 1.18 1996/02/09 18:25:38 christos Exp $	*/
 
 /*-
@@ -204,7 +204,7 @@ do {									\
  */
 #define sblock(sb, wf) ((sb)->sb_flags & SB_LOCK ? \
 		(((wf) == M_WAITOK) ? sb_lock(sb) : EWOULDBLOCK) : \
-		((sb)->sb_flags |= SB_LOCK), 0)
+		((sb)->sb_flags |= SB_LOCK, 0))
 
 /* release lock on sockbuf sb */
 #define	sbunlock(sb) { \
