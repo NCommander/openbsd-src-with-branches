9cfe7c01face17427e00dfbd668479a1ab0efc3e
sobrado <sobrado>
2007-08-14 17:41:10 +0000

    the ellipsis is not an optional argument; document the "-a" flag in whereis(1)
    help and feedback by jmc@ and otto@
    
    ok deraadt@, jmc@ and otto@

--- usr.bin/which/which.c
+++ usr.bin/which/which.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: which.c,v 1.12 2004/09/23 17:44:47 millert Exp $	*/
+/*	$OpenBSD: which.c,v 1.13 2004/09/24 19:45:27 fgsch Exp $	*/
 
 /*
  * Copyright (c) 1997 Todd C. Miller <Todd.Miller@courtesan.com>
@@ -17,7 +17,7 @@
  */
 
 #ifndef lint                                                              
-static const char rcsid[] = "$OpenBSD: which.c,v 1.12 2004/09/23 17:44:47 millert Exp $";
+static const char rcsid[] = "$OpenBSD: which.c,v 1.13 2004/09/24 19:45:27 fgsch Exp $";
 #endif /* not lint */                                                        
 
 #include <sys/param.h>
@@ -170,6 +170,6 @@ findprog(char *prog, char *path, int progmode, int allmatches)
 __dead void
 usage(void)
 {
-	(void) fprintf(stderr, "Usage: %s [-a] name [...]\n", __progname);
+	(void)fprintf(stderr, "usage: %s [-a] name ...\n", __progname);
 	exit(1);
 }
