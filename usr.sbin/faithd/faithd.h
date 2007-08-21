5d6e148477cad99a9ac385ccf965c987252fea44
krw <krw>
2007-08-21 22:18:41 +0000

    No need to keep RLOGIN_PORT and RSH_PORT defines hanging around since their uses were eliminated five years ago. Spotted by Diego Casati.
    ok deraadt@ hshoexer@

--- usr.sbin/faithd/faithd.h
+++ usr.sbin/faithd/faithd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: faithd.h,v 1.7 2002/02/16 21:28:02 millert Exp $	*/
+/*	$OpenBSD: faithd.h,v 1.8 2002/05/09 14:12:17 itojun Exp $	*/
 /*	$KAME: faithd.h,v 1.9 2002/05/09 09:41:24 itojun Exp $	*/
 
 /*
@@ -48,8 +48,6 @@ extern void exit_failure(const char *, ...)
 #define DEFAULT_PATH	(DEFAULT_DIR "/" DEFAULT_NAME)
 
 #define FTP_PORT	21
-#define RLOGIN_PORT	513
-#define RSH_PORT	514
 
 #define RETURN_SUCCESS	0
 #define RETURN_FAILURE	1
