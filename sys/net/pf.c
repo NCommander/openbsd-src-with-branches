8353f162229df387d9ee774698da0ef291c2a4a6
henning <henning>
2007-08-28 16:09:12 +0000

    showing this diff is shameful...
            if (r != NULL && r->rtableid >= 0)
    -               m->m_pkthdr.pf.rtableid = m->m_pkthdr.pf.rtableid;
    +               m->m_pkthdr.pf.rtableid = r->rtableid;
    
    fortunately it is in pf_send_tcp and thus the effect is very limited, RSTs
    sent due to "block return(-rst)" could be routed using the main routing
    table instead of an alternate one specified on the block rule.
    spotted by Janjaap van Velthooven <janjaap@stack.nl>

--- sys/net/pf.c
+++ sys/net/pf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf.c,v 1.550 2007/07/10 15:58:37 kurt Exp $ */
+/*	$OpenBSD: pf.c,v 1.553 2007/08/23 11:15:49 dhartmei Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -1598,7 +1598,7 @@ pf_send_tcp(const struct pf_rule *r, sa_family_t af,
 	m->m_pkthdr.pf.tag = rtag;
 
 	if (r != NULL && r->rtableid >= 0)
-		m->m_pkthdr.pf.rtableid = m->m_pkthdr.pf.rtableid;
+		m->m_pkthdr.pf.rtableid = r->rtableid;
 
 #ifdef ALTQ
 	if (r != NULL && r->qid) {
