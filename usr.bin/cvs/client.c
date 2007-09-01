0909e03519413fc9cca45f54df0123fb130ae094
joris <joris>
2007-09-01 15:41:34 +0000

    s/free/xfree

--- usr.bin/cvs/client.c
+++ usr.bin/cvs/client.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: client.c,v 1.71 2007/07/17 20:29:58 xsa Exp $	*/
+/*	$OpenBSD: client.c,v 1.72 2007/08/30 11:07:18 joris Exp $	*/
 /*
  * Copyright (c) 2006 Joris Vink <joris@openbsd.org>
  *
@@ -394,7 +394,7 @@ cvs_client_send_logmsg(char *msg)
 		}
 	}
 
-	free(buf);
+	xfree(buf);
 }
 
 void
