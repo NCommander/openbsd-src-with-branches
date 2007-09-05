c87be04316d00ae92e0505073d4f81ed5980e260
jsg <jsg>
2007-09-05 11:09:08 +0000

    Only match on RTL8185 if RTW_DEBUG is set, support for the RTL8185 was never finished due to a lack of information on the radios. ok reyk@

--- sys/dev/cardbus/if_rtw_cardbus.c
+++ sys/dev/cardbus/if_rtw_cardbus.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_rtw_cardbus.c,v 1.10 2006/06/21 11:27:03 fkr Exp $	*/
+/*	$OpenBSD: if_rtw_cardbus.c,v 1.11 2006/10/12 16:35:52 grange Exp $	*/
 /* $NetBSD: if_rtw_cardbus.c,v 1.4 2004/12/20 21:05:34 dyoung Exp $ */
 
 /*-
@@ -161,8 +161,10 @@ void rtw_cardbus_disable(struct rtw_softc *);
 void rtw_cardbus_power(struct rtw_softc *, int);
 
 const struct cardbus_matchid rtw_cardbus_devices[] = {
-	{ PCI_VENDOR_REALTEK,	PCI_PRODUCT_REALTEK_RT8185 },
 	{ PCI_VENDOR_REALTEK,	PCI_PRODUCT_REALTEK_RT8180 },
+#ifdef RTW_DEBUG
+	{ PCI_VENDOR_REALTEK,	PCI_PRODUCT_REALTEK_RT8185 },
+#endif
 	{ PCI_VENDOR_BELKIN2,	PCI_PRODUCT_BELKIN2_F5D6020V3 },
 	{ PCI_VENDOR_DLINK,	PCI_PRODUCT_DLINK_DWL610 },
 };
