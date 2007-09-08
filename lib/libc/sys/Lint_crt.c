13d338c010b3e8759945b560895cbcfa1e1e63ad
otto <otto>
2007-09-08 19:42:36 +0000

    typo; ok deraadt@

--- lib/libc/sys/Lint_crt.c
+++ lib/libc/sys/Lint_crt.c
@@ -1,6 +1,6 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: Lint_crt.c,v 1.1 2005/11/29 21:29:15 deraadt Exp $	*/
 
-/* Publid domain, Theo de Raadt, 2005 */
+/* Public domain, Theo de Raadt, 2005 */
 
 #include <unistd.h>
 
