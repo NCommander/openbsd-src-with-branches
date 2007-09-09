38aea0716fed6e1ac3e922251a09ff4fb61ca0ee
kettenis <kettenis>
2007-09-09 10:17:33 +0000

    Remove some more goo left over from sparc.

--- sys/arch/sparc64/sparc64/emul.c
+++ sys/arch/sparc64/sparc64/emul.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: emul.c,v 1.11 2003/07/14 00:05:35 jason Exp $	*/
+/*	$OpenBSD: emul.c,v 1.12 2003/11/03 07:01:33 david Exp $	*/
 /*	$NetBSD: emul.c,v 1.8 2001/06/29 23:58:40 eeh Exp $	*/
 
 /*-
@@ -46,7 +46,6 @@
 #include <machine/instr.h>
 #include <machine/cpu.h>
 #include <machine/psl.h>
-#include <sparc64/sparc64/cpuvar.h>
 #include <uvm/uvm_extern.h>
 
 #ifdef DEBUG_EMUL
