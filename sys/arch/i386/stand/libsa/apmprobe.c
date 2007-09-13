0981f3c091cd97fa4c368b7bd87104a605ae5a8a
weingart <weingart>
2007-09-13 06:58:47 +0000

    Don't bother bitching if APM does not exist.  Pleanty enough systems today do not have apm.  Also, the existance of apm is given on the probe line, absence means it ain't there.

--- sys/arch/i386/stand/libsa/apmprobe.c
+++ sys/arch/i386/stand/libsa/apmprobe.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: apmprobe.c,v 1.13 2004/03/11 17:41:23 tom Exp $	*/
+/*	$OpenBSD: apmprobe.c,v 1.14 2005/11/14 23:50:26 martin Exp $	*/
 
 /*
  * Copyright (c) 1997-2000 Michael Shalayeff
@@ -171,7 +171,9 @@ apmprobe(void)
 		apm_disconnect();
 
 		if (apm_connect(&ai) != 0) {
+#ifdef DEBUG
 			printf("\napm: connect error\n");
+#endif
 			return;
 		}
 #ifdef DEBUG
