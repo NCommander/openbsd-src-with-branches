7f2ed0ae7e818ae65060cae6ae25ce0f52480699
kjell <kjell>
2007-09-16 20:34:18 +0000

    I can't think of any good reason to inline this. Makes pcc happier, to boot.

--- usr.bin/mg/undo.c
+++ usr.bin/mg/undo.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: undo.c,v 1.42 2006/11/17 08:45:31 kjell Exp $ */
+/* $OpenBSD: undo.c,v 1.43 2006/11/19 16:51:19 deraadt Exp $ */
 /*
  * Copyright (c) 2002 Vincent Labrecque <vincent@openbsd.org>
  * Copyright (c) 2005, 2006 Kjell Wooding <kjell@openbsd.org>
@@ -161,7 +161,7 @@ drop_oldest_undo_record(void)
 	return (0);
 }
 
-static __inline__ int
+static int
 lastrectype(void)
 {
 	struct undo_rec *rec;
