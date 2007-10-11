c3cb4b53883f2912de5491806548f2d87e6dafe3
claudio <claudio>
2007-10-11 08:21:29 +0000

    Do not overload nbr->options with the dd exchange bits. nbr->options is used by the hello protocol. Instead add a dd_more flag that is used together with (the renamed) dd_master flag. Tested and OK norby@

--- usr.sbin/ospfd/ospfe.h
+++ usr.sbin/ospfd/ospfe.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ospfe.h,v 1.35 2007/03/21 10:54:30 claudio Exp $ */
+/*	$OpenBSD: ospfe.h,v 1.36 2007/05/29 22:08:25 claudio Exp $ */
 
 /*
  * Copyright (c) 2004, 2005 Esben Norby <norby@openbsd.org>
@@ -90,7 +90,8 @@ struct nbr {
 	u_int8_t		 options;
 	u_int8_t		 last_rx_options;
 	u_int8_t		 last_rx_bits;
-	u_int8_t		 master;
+	u_int8_t		 dd_master;
+	u_int8_t		 dd_more;
 };
 
 /* auth.c */
