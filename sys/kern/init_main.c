9ad1609ef842084b47edf0a015b732d18d2a1926
fgsch <fgsch>
2007-10-16 23:41:42 +0000

    more remove unneeded declarations that shadows existing vars; ok by many.

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: init_main.c,v 1.144 2007/09/10 18:49:45 miod Exp $	*/
+/*	$OpenBSD: init_main.c,v 1.145 2007/10/10 15:53:53 art Exp $	*/
 /*	$NetBSD: init_main.c,v 1.84.4.1 1996/06/02 09:08:06 mrg Exp $	*/
 
 /*
@@ -395,7 +395,6 @@ main(void *framep)
 #if !defined(NO_PROPOLICE)
 	{
 		volatile long newguard[8];
-		int i;
 
 		arc4random_bytes((long *)newguard, sizeof(newguard));
 
