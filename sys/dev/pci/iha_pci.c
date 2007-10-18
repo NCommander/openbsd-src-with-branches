6332e6b138d0cd7e895238e7babf5f856ef1532f
otto <otto>
2007-10-18 21:10:58 +0000

    Initio INIC-941 works as well; ok miod@

--- sys/dev/pci/iha_pci.c
+++ sys/dev/pci/iha_pci.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: iha_pci.c,v 1.10 2006/11/28 23:59:45 dlg Exp $ */
+/*	$OpenBSD: iha_pci.c,v 1.11 2006/11/29 00:56:51 dlg Exp $ */
 /*-------------------------------------------------------------------------
  *
  * Device driver for the INI-9XXXU/UW or INIC-940/950  PCI SCSI Controller.
@@ -62,6 +62,7 @@ iha_pci_probe(parent, match, aux)
 	if (PCI_VENDOR(pa->pa_id) == PCI_VENDOR_INITIO)
 		switch (PCI_PRODUCT(pa->pa_id)) {
 		case PCI_PRODUCT_INITIO_INIC940:
+		case PCI_PRODUCT_INITIO_INIC941:
 		case PCI_PRODUCT_INITIO_INIC950:
 			return (1);
 		}
