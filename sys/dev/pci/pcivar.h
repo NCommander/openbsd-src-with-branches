c04ec06c07267f71825ff22ece9b1256cfbd1b04
fgsch <fgsch>
2007-10-22 03:08:53 +0000

    Add pci_set_powerstate() to change the pm state for a given device. ok by many.

--- sys/dev/pci/pcivar.h
+++ sys/dev/pci/pcivar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pcivar.h,v 1.53 2007/05/21 22:10:45 kettenis Exp $	*/
+/*	$OpenBSD: pcivar.h,v 1.54 2007/08/01 21:43:03 deraadt Exp $	*/
 /*	$NetBSD: pcivar.h,v 1.23 1997/06/06 23:48:05 thorpej Exp $	*/
 
 /*
@@ -223,6 +223,7 @@ struct pci_matchid {
 };
 
 int pci_matchbyid(struct pci_attach_args *, const struct pci_matchid *, int);
+int pci_set_powerstate(pci_chipset_tag_t, pcitag_t, int);
 
 /*
  * Helper functions for autoconfiguration.
