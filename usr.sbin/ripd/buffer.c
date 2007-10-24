b91cd708f7ff5d0766dbce7f041a67419524bfcd
claudio <claudio>
2007-10-24 20:18:07 +0000

    The index should be unsigned int -- another lint finding

--- usr.sbin/ripd/buffer.c
+++ usr.sbin/ripd/buffer.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$ */
+/*	$OpenBSD: buffer.c,v 1.1 2006/10/18 16:11:58 norby Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -157,7 +157,7 @@ msgbuf_write(struct msgbuf *msgbuf)
 {
 	struct iovec	 iov[IOV_MAX];
 	struct buf	*buf, *next;
-	int		 i = 0;
+	unsigned int	 i = 0;
 	ssize_t		 n;
 	struct msghdr	 msg;
 
