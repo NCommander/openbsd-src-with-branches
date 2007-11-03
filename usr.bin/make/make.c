4083a79d29f7d9b78ae705e7813a58c4ac0a198d
espie <espie>
2007-11-03 11:45:52 +0000

    zap dead code

--- usr.bin/make/make.c
+++ usr.bin/make/make.c
@@ -215,14 +215,10 @@ Make_Update(GNode *cgn)	/* the child node */
 
 	/* Set the .IMPSRC variables for all the implied parents
 	 * of this node.  */
-	{
-	char	*cpref = Varq_Value(PREFIX_INDEX, cgn);
-
 	for (ln = Lst_First(&cgn->iParents); ln != NULL; ln = Lst_Adv(ln)) {
 		pgn = (GNode *)Lst_Datum(ln);
 		if (pgn->make)
 			Varq_Set(IMPSRC_INDEX, cname, pgn);
-	}
 	}
 
 }
