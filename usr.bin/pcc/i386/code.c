22ff518836faf534698d0b31a6311d241545ab45
ragge <ragge>
2007-11-04 18:55:21 +0000

    Update from master repo: > Work in progress: PIC code for i386. > Still missing: ebx as argument to functions.

--- usr.bin/pcc/i386/code.c
+++ usr.bin/pcc/i386/code.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: code.c,v 1.1 2007/10/07 17:58:52 otto Exp $	*/
+/*	$OpenBSD: code.c,v 1.1 2007/10/22 13:39:37 otto Exp $	*/
 /*
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -67,9 +67,11 @@ defnam(struct symtab *p)
 void
 efcode()
 {
+	extern int gotnr;
 	NODE *p, *q;
 	int sz;
 
+	gotnr = 0;	/* new number for next fun */
 	if (cftnsp->stype != STRTY+FTN && cftnsp->stype != UNIONTY+FTN)
 		return;
 	/* address of return struct is in eax */
