619d1c09702e18f5a4abd632c1adf409599752bf
kettenis <kettenis>
2007-11-12 20:54:54 +0000

    Make this compile on macppc (OpenPROM support only) and build it there.
    ok deraadt@

--- usr.sbin/eeprom/ophandlers.c
+++ usr.sbin/eeprom/ophandlers.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ophandlers.c,v 1.9 2005/03/06 16:12:48 miod Exp $	*/
+/*	$OpenBSD: ophandlers.c,v 1.10 2007/09/04 23:28:26 fgsch Exp $	*/
 /*	$NetBSD: ophandlers.c,v 1.2 1996/02/28 01:13:30 thorpej Exp $	*/
 
 /*-
@@ -49,7 +49,6 @@
 #include <unistd.h>
 #include <vis.h>
 
-#include <machine/eeprom.h>
 #include <machine/openpromio.h>
 
 #include "defs.h"
