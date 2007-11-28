7b790c7d36ad7b63dc44937471a0160463ed59d4
tedu <tedu>
2007-11-28 13:54:07 +0000

    remove the DIRECTOK flag.  the caller semantics were too weird, and it's a bad idea to have functions that react unpredictably.

--- sys/kern/kern_workq.c
+++ sys/kern/kern_workq.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_workq.c,v 1.3 2007/06/11 22:15:11 thib Exp $ */
+/*	$OpenBSD: kern_workq.c,v 1.4 2007/11/25 15:56:17 tedu Exp $ */
 
 /*
  * Copyright (c) 2007 David Gwynne <dlg@openbsd.org>
@@ -128,10 +128,6 @@ workq_add_task(struct workq *wq, int flags, workq_fn func,
 	int			s;
 
 	if (wq == NULL) {
-		if (flags & WQ_DIRECTOK) {
-			func(a1, a2);
-			return 0;
-		}
 		wq = &workq_syswq;
 	}
 	
