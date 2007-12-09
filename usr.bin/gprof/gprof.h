4fcb46c581cf5cfd87daac9bb09d43b3d59fc11d
espie <espie>
2007-12-09 19:51:30 +0000

    remove makefile hacks to build links. Instead, change the files that gprof uses directly.
    okay deraadt@, miod@.
    Thanks for checking that gcc 2.95 is happy with
    #include MACRO

--- usr.bin/gprof/gprof.h
+++ usr.bin/gprof/gprof.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: gprof.h,v 1.11 2004/07/20 18:20:55 marc Exp $	*/
+/*	$OpenBSD: gprof.h,v 1.12 2006/03/25 19:06:36 espie Exp $	*/
 /*	$NetBSD: gprof.h,v 1.13 1996/04/01 21:54:06 mark Exp $	*/
 
 /*
@@ -41,7 +41,7 @@
 #include <stdlib.h>
 #include <err.h>
 
-#include "machine.h"
+#include MD_INCLUDE
 
     /*
      * booleans
