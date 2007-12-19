691fc67c28dbeaf1d587b73e28dc99febb014e71
otto <otto>
2007-12-19 20:19:54 +0000

    sync to main repo; requested by mickey

--- usr.bin/pcc/hppa/order.c
+++ usr.bin/pcc/hppa/order.c
@@ -108,18 +108,6 @@ shumul(NODE *p)
 }
 
 /*
- * Rewrite increment/decrement operation.
- */
-int
-setincr(NODE *p)
-{
-	if (x2debug)
-		printf("setincr(%p)\n", p);
-
-	return(0);
-}
-
-/*
  * Rewrite operations on binary operators (like +, -, etc...).
  * Called as a result of table lookup.
  */
@@ -201,3 +189,11 @@ livecall(NODE *p)
 	return &r[0];
 }
 
+/*
+ * Signal whether the instruction is acceptable for this target.
+ */
+int
+acceptable(struct optab *op)
+{
+	return 1;
+}
