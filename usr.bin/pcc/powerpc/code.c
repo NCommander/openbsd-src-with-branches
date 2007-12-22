8e89cecb56c9ed1e60a214c127832f2bfe8d731d
stefan <stefan>
2007-12-22 14:05:04 +0000

    Sync with main repo.

--- usr.bin/pcc/powerpc/code.c
+++ usr.bin/pcc/powerpc/code.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: code.c,v 1.4 2007/11/16 09:00:13 otto Exp $	*/
+/*	$OpenBSD: code.c,v 1.5 2007/11/22 15:06:43 stefan Exp $	*/
 /*
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -291,16 +291,6 @@ bycode(int t, int i)
 			putchar(t);
 		}
 	}
-}
-
-/*
- * n integer words of zeros
- */
-void
-zecode(int n)
-{
-	printf("	.zero %d\n", n * (SZINT/SZCHAR));
-//	inoff += n * SZINT;
 }
 
 /*
