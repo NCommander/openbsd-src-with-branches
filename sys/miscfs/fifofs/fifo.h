86c91b635bd2b17e43bfd2fca377a2ee70b872d2
thib <thib>
2007-12-27 13:59:12 +0000

    fifofs, deadfs and specfs all have the same "trivial lookup routine that always fails". Introduce vop_generic_lookup(), which is a trivial lookup routine that always fails and use that instead, zap the redundant copies.
    ok toby@, tedu@, art@

--- sys/miscfs/fifofs/fifo.h
+++ sys/miscfs/fifofs/fifo.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fifo.h,v 1.15 2004/03/02 04:42:52 tedu Exp $	*/
+/*	$OpenBSD: fifo.h,v 1.16 2007/01/16 17:52:18 thib Exp $	*/
 /*	$NetBSD: fifo.h,v 1.10 1996/02/09 22:40:15 christos Exp $	*/
 
 /*
@@ -39,7 +39,6 @@
 int	fifo_badop(void *);
 int	fifo_ebadf(void *);
 
-int	fifo_lookup(void *);
 #define fifo_create	fifo_badop
 #define fifo_mknod	fifo_badop
 int	fifo_open(void *);
