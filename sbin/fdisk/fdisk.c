9b1f2956d1e2fbe7521a36026c7b8d02654b48dd
sobrado <sobrado>
2007-12-30 13:20:13 +0000

    sort flags in both synopsis and usage
    ok jmc@

--- sbin/fdisk/fdisk.c
+++ sbin/fdisk/fdisk.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fdisk.c,v 1.46 2006/11/09 00:01:10 deraadt Exp $	*/
+/*	$OpenBSD: fdisk.c,v 1.47 2007/04/26 23:39:01 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1997 Tobias Weingartner
@@ -50,7 +50,7 @@ usage(void)
 	extern char * __progname;
 
 	fprintf(stderr, "usage: %s "
-	    "[-ieuy] [-c cylinders -h heads -s sectors] [-f mbrfile] device\n"
+	    "[-eiuy] [-c cylinders -h heads -s sectors] [-f mbrfile] device\n"
 	    "\t-i: initialize disk with virgin MBR\n"
 	    "\t-u: update MBR code, preserve partition table\n"
 	    "\t-e: edit MBRs on disk interactively\n"
