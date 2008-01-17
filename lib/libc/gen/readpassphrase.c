689c85a4a09151961081ecf42c6b05f1572cbfeb
millert <millert>
2008-01-17 16:27:07 +0000

    Use TCSAFLUSH instead of TCSANOW when turning echo back on so that there is no chance of output that was written but still pending to be displayed.  This is what the original getpass(3) did.

--- lib/libc/gen/readpassphrase.c
+++ lib/libc/gen/readpassphrase.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: readpassphrase.c,v 1.19 2006/03/31 05:33:59 deraadt Exp $	*/
+/*	$OpenBSD: readpassphrase.c,v 1.20 2007/10/30 12:03:48 millert Exp $	*/
 
 /*
  * Copyright (c) 2000-2002, 2007 Todd C. Miller <Todd.Miller@courtesan.com>
@@ -129,7 +129,7 @@ restart:
 
 	/* Restore old terminal settings and signals. */
 	if (memcmp(&term, &oterm, sizeof(term)) != 0) {
-		while (tcsetattr(input, TCSANOW|TCSASOFT, &oterm) == -1 &&
+		while (tcsetattr(input, TCSAFLUSH|TCSASOFT, &oterm) == -1 &&
 		    errno == EINTR)
 			continue;
 	}
