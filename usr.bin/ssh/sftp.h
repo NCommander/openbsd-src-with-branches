2b20fe120e10030b3f3b9773a9df77e05d73e3c7
markus <markus>
2008-02-04 21:53:00 +0000

    link sftp-server into sshd; feedback and ok djm@

--- usr.bin/ssh/sftp.h
+++ usr.bin/ssh/sftp.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: sftp.h,v 1.4 2002/02/13 00:59:23 djm Exp $ */
+/* $OpenBSD: sftp.h,v 1.5 2006/03/25 22:22:43 djm Exp $ */
 
 /*
  * Copyright (c) 2001 Markus Friedl.  All rights reserved.
@@ -90,3 +90,6 @@
 #define SSH2_FX_CONNECTION_LOST		7
 #define SSH2_FX_OP_UNSUPPORTED		8
 #define SSH2_FX_MAX			8
+
+int	sftp_server_main(int, char **);
+void	sftp_server_cleanup_exit(int) __dead;
