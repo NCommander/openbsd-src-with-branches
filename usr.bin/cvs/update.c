065bd36a9f7f19e293f3f8c56c3650542f1059e9
joris <joris>
2008-02-09 17:52:32 +0000

    in cvs_update_enterdir() write the CVS/Tag file if the cvs_specified_date is != 0 as well;

--- usr.bin/cvs/update.c
+++ usr.bin/cvs/update.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: update.c,v 1.126 2008/02/09 13:09:33 joris Exp $	*/
+/*	$OpenBSD: update.c,v 1.127 2008/02/09 17:01:43 tobias Exp $	*/
 /*
  * Copyright (c) 2006 Joris Vink <joris@openbsd.org>
  *
@@ -204,7 +204,7 @@ cvs_update_enterdir(struct cvs_file *cf)
 		    cf->file_path, CVS_PATH_TAG);
 		(void)unlink(fpath);
 	} else {
-		if (cvs_specified_tag != NULL)
+		if (cvs_specified_tag != NULL || cvs_specified_date != 0)
 			cvs_write_tagfile(cf->file_path,
 				    cvs_specified_tag, NULL);
 	}
