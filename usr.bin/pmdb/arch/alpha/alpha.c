4b2763921425df34756fae489d37790d48b4a9b9
martin <martin>
2008-02-09 20:59:09 +0000

    missing newline at EOF

--- usr.bin/pmdb/arch/alpha/alpha.c
+++ usr.bin/pmdb/arch/alpha/alpha.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: alpha.c,v 1.2 2002/03/15 16:41:06 jason Exp $	*/
+/*	$OpenBSD: alpha.c,v 1.3 2002/07/22 02:54:23 art Exp $	*/
 /*
  * Copyright (c) 2002 Artur Grabowski <art@openbsd.org>
  * All rights reserved. 
