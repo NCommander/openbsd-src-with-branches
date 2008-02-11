81b4912d229ae1218c20819192c863848276d5dd
miod <miod>
2008-02-11 20:44:43 +0000

    Remove long dead CLKF_BASEPRI which crept in by mistake.

--- sys/arch/sh/include/cpu.h
+++ sys/arch/sh/include/cpu.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cpu.h,v 1.10 2007/06/06 17:15:12 deraadt Exp $	*/
+/*	$OpenBSD: cpu.h,v 1.11 2007/10/10 15:53:52 art Exp $	*/
 /*	$NetBSD: cpu.h,v 1.41 2006/01/21 04:24:12 uwe Exp $	*/
 
 /*-
@@ -81,7 +81,6 @@ struct clockframe {
 };
 
 #define	CLKF_USERMODE(cf)	(!KERNELMODE((cf)->ssr))
-#define	CLKF_BASEPRI(cf)	(((cf)->ssr & 0xf0) == 0)
 #define	CLKF_PC(cf)		((cf)->spc)
 #define	CLKF_INTR(cf)		0	/* XXX */
 
