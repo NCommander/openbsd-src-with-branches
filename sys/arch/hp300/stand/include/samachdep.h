10660d051c375ecb722e2436eacf9c7a986798b2
miod <miod>
2008-02-13 21:20:32 +0000

    #undef IIOV before redefining for 1:1 mappings, to silence a warning.

--- sys/arch/hp300/stand/include/samachdep.h
+++ sys/arch/hp300/stand/include/samachdep.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: samachdep.h,v 1.4 2003/06/02 23:27:46 millert Exp $	*/
+/*	$OpenBSD: samachdep.h,v 1.5 2006/08/17 06:31:10 miod Exp $	*/
 /*	$NetBSD: samachdep.h,v 1.3 1997/05/12 07:54:45 thorpej Exp $	*/
 
 /*
@@ -45,6 +45,7 @@
 
 /* from cpu.h */
 #define	INTIOBASE	(0x00400000)
+#undef	IIOV
 #define IIOV(x)		(x)
 #define DIOBASE		(0x600000)
 #define	DIOCSIZE	(0x10000)
