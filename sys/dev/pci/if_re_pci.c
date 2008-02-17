581ec148f17437d0a763518e81eae3f2aa4df0f0
brad <brad>
2008-02-17 05:29:25 +0000

    Simplify the hw revision handling by removing the use of the rl_type softc field and only relying on the sc_hwrev softc field thoughout the driver.
    Tested by a number of users from tech@
    
    ok dlg@

--- sys/dev/pci/if_re_pci.c
+++ sys/dev/pci/if_re_pci.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_re_pci.c,v 1.18 2006/11/06 20:10:58 deraadt Exp $	*/
+/*	$OpenBSD: if_re_pci.c,v 1.19 2006/11/28 20:04:02 brad Exp $	*/
 
 /*
  * Copyright (c) 2005 Peter Valchev <pvalchev@openbsd.org>
@@ -186,18 +186,8 @@ re_pci_attach(struct device *parent, struct device *self, void *aux)
 	}
 
 	sc->sc_dmat = pa->pa_dmat;
-	sc->sc_flags |= RL_ENABLED;
 
-	if (PCI_VENDOR(pa->pa_id) == PCI_VENDOR_REALTEK) {
-		if (PCI_PRODUCT(pa->pa_id) == PCI_PRODUCT_REALTEK_RT8139)
-			sc->rl_type = RL_8139CPLUS;
-		else
-			sc->rl_type = RL_8169;
-	} else if (PCI_VENDOR(pa->pa_id) == PCI_VENDOR_TTTECH &&
-		   PCI_PRODUCT(pa->pa_id) == PCI_PRODUCT_TTTECH_MC322)
-		sc->rl_type = RL_8139CPLUS;
-	else
-		sc->rl_type = RL_8169;
+	sc->sc_flags |= RL_ENABLED;
 
 	/* Call bus-independent attach routine */
 	re_attach(sc, intrstr);
