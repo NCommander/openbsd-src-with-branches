0110861bb90eccd74feb6b476eecad40d0e05a9f
krw <krw>
2008-02-18 16:24:14 +0000

    'like\nlike' -> 'like' in a couple of comments.
    From Giovanni Bechis.

--- sys/dev/isa/if_ep_isa.c
+++ sys/dev/isa/if_ep_isa.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ep_isa.c,v 1.23 2005/11/21 18:16:40 millert Exp $	*/
+/*	$OpenBSD: if_ep_isa.c,v 1.24 2006/06/17 17:57:00 brad Exp $	*/
 /*	$NetBSD: if_ep_isa.c,v 1.5 1996/05/12 23:52:36 mycroft Exp $	*/
 
 /*
@@ -135,7 +135,7 @@ epaddcard(bus, iobase, irq, model)
 /*
  * 3c509 cards on the ISA bus are probed in ethernet address order.
  * The probe sequence requires careful orchestration, and we'd like
- * like to allow the irq and base address to be wildcarded. So, we
+ * to allow the irq and base address to be wildcarded. So, we
  * probe all the cards the first time epprobe() is called. On subsequent
  * calls we look for matching cards.
  */
