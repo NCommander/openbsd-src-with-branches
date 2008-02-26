879bc286bbbe52b1f453867b13439f19f97e66a7
joris <joris>
2008-02-26 21:23:00 +0000

    spacing

--- usr.bin/cvs/cvs.c
+++ usr.bin/cvs/cvs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cvs.c,v 1.141 2008/01/28 21:32:00 tobias Exp $	*/
+/*	$OpenBSD: cvs.c,v 1.142 2008/02/02 19:32:28 joris Exp $	*/
 /*
  * Copyright (c) 2006, 2007 Joris Vink <joris@openbsd.org>
  * Copyright (c) 2004 Jean-Francois Brousseau <jfb@openbsd.org>
@@ -158,7 +158,6 @@ main(int argc, char **argv)
 	if ((cvs_homedir = getenv("HOME")) == NULL) {
 		if ((pw = getpwuid(getuid())) != NULL)
 			cvs_homedir = pw->pw_dir;
-
 	}
 
 	if ((envstr = getenv("TMPDIR")) != NULL)
