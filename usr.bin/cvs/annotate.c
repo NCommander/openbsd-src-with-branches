374dc8ef3f5efaae625954ade4efea4e9657240d
tobias <tobias>
2008-03-02 19:14:10 +0000

    rannotate needs at least one module as argument.
    ok joris

--- usr.bin/cvs/annotate.c
+++ usr.bin/cvs/annotate.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: annotate.c,v 1.55 2008/02/10 10:21:42 joris Exp $	*/
+/*	$OpenBSD: annotate.c,v 1.56 2008/03/02 19:05:34 tobias Exp $	*/
 /*
  * Copyright (c) 2007 Tobias Stoeckmann <tobias@openbsd.org>
  * Copyright (c) 2006 Xavier Santolaria <xsa@openbsd.org>
@@ -93,8 +93,12 @@ cvs_annotate(int argc, char **argv)
 	argc -= optind;
 	argv += optind;
 
-	if (cvs_cmdop == CVS_OP_RANNOTATE)
+	if (cvs_cmdop == CVS_OP_RANNOTATE) {
+		if (argc == 0)
+			fatal("%s", cvs_cmd_rannotate.cmd_synopsis);
+
 		flags |= CR_REPO;
+	}
 
 	cr.enterdir = NULL;
 	cr.leavedir = NULL;
