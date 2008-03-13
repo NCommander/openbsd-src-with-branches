356fee4d5a2dcf245f18d33f926175fac45122c6
joris <joris>
2008-03-13 17:01:25 +0000

    properly initialize variables

--- usr.bin/cvs/rcs.c
+++ usr.bin/cvs/rcs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rcs.c,v 1.260 2008/03/08 22:33:03 joris Exp $	*/
+/*	$OpenBSD: rcs.c,v 1.261 2008/03/09 19:41:13 tobias Exp $	*/
 /*
  * Copyright (c) 2004 Jean-Francois Brousseau <jfb@openbsd.org>
  * All rights reserved.
@@ -1276,6 +1276,7 @@ rcs_rev_remove(RCSFILE *rf, RCSNUM *rev)
 
 	newdeltatext = NULL;
 	prevbuf = NULL;
+	path_tmp1 = path_tmp2 = NULL;
 
 	if (prevrdp != NULL && nextrdp != NULL) {
 		newdiff = cvs_buf_alloc(64);
