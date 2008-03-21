104d5242e45ef8eaaba69fd5039808ef51390a98
brad <brad>
2008-03-21 20:39:45 +0000

    fix use-after-free: pfxlist_onlink_check() might free rt_llinfo for the current route, so make sure RTF_LLINFO is still set; fixes pr 5711;
    ok deraadt@

--- sys/netinet6/nd6_nbr.c
+++ sys/netinet6/nd6_nbr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: nd6_nbr.c,v 1.44 2007/06/01 00:52:38 henning Exp $	*/
+/*	$OpenBSD: nd6_nbr.c,v 1.45 2007/06/08 09:31:38 henning Exp $	*/
 /*	$KAME: nd6_nbr.c,v 1.61 2001/02/10 16:06:14 jinmei Exp $	*/
 
 /*
@@ -682,6 +682,8 @@ nd6_na_input(m, off, icmp6len)
 			 * affect the status of associated prefixes..
 			 */
 			pfxlist_onlink_check();
+			if ((rt->rt_flags & RTF_LLINFO) == 0)
+				goto freeit;	/* ln is gone */
 		}
 	} else {
 		int llchange;
