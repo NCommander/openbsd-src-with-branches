63d0e31e0c9d7cb9adc6871ee3cf8f365bf8f2a0
brad <brad>
2008-03-21 20:33:12 +0000

    Set the RX FIFO threshold to no RX threshold for re(4) adapters. This resolves a number of various bad symptoms experienced by numerous users especially with the adapters at Gig speed.
    From FreeBSD/NetBSD
    
    ok deraadt@

--- sys/dev/ic/re.c
+++ sys/dev/ic/re.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: re.c,v 1.68 2007/02/25 08:00:06 deraadt Exp $	*/
+/*	$OpenBSD: re.c,v 1.69 2007/03/02 17:49:51 krw Exp $	*/
 /*	$FreeBSD: if_re.c,v 1.31 2004/09/04 07:54:05 ru Exp $	*/
 /*
  * Copyright (c) 1997, 1998-2003
@@ -1859,7 +1859,8 @@ re_init(struct ifnet *ifp)
 
 	CSR_WRITE_1(sc, RL_EARLY_TX_THRESH, 16);
 
-	CSR_WRITE_4(sc, RL_RXCFG, RL_RXCFG_CONFIG);
+	CSR_WRITE_4(sc, RL_RXCFG,
+	    RL_RXFIFO_NOTHRESH|RL_RX_MAXDMA|RL_RX_BUF_SZ);
 
 	/* Set the individual bit to receive frames for this host only. */
 	rxcfg = CSR_READ_4(sc, RL_RXCFG);
