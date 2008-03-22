e77c39075f8d16f7afdb89e78a7a196b140fd126
millert <millert>
2008-03-22 21:15:54 +0000

    Fix the freeing of two uninitialized strings.  OK niallo@

--- usr.bin/rcs/ci.c
+++ usr.bin/rcs/ci.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ci.c,v 1.202 2007/07/03 00:56:23 ray Exp $	*/
+/*	$OpenBSD: ci.c,v 1.203 2007/09/06 19:38:47 niallo Exp $	*/
 /*
  * Copyright (c) 2005, 2006 Niall O'Higgins <niallo@openbsd.org>
  * All rights reserved.
@@ -347,6 +347,7 @@ checkin_diff_file(struct checkin_params *pb)
 	char rbuf[RCS_REV_BUFSZ];
 
 	b1 = b2 = b3 = NULL;
+	path1 = path2 = NULL;
 	rcsnum_tostr(pb->frev, rbuf, sizeof(rbuf));
 
 	if ((b1 = rcs_buf_load(pb->filename, BUF_AUTOEXT)) == NULL) {
