07bc80a98b1081bbcaa42a623bb9714a710f5e0e
drahn <drahn>
2008-03-28 17:10:57 +0000

    Enable shared libraries on landisk. The sh architecture required some different linkage than most platforms, This fixes those other requirements.

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.own.mk,v 1.97 2006/10/06 20:58:17 miod Exp $
+#	$OpenBSD: bsd.own.mk,v 1.99 2008/03/28 16:56:30 drahn Exp $
 #	$NetBSD: bsd.own.mk,v 1.24 1996/04/13 02:08:09 thorpej Exp $
 
 # Host-specific overrides
@@ -116,8 +116,7 @@ STATIC?=	-static
 
 # don't try to generate PIC versions of libraries on machines
 # which don't support PIC.
-.if ${MACHINE_ARCH} == "m88k" || ${MACHINE_ARCH} == "vax" || \
-    ${MACHINE_ARCH} == "sh"
+.if ${MACHINE_ARCH} == "m88k" || ${MACHINE_ARCH} == "vax"
 NOPIC=
 .endif
 
