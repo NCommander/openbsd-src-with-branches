baacdb2f565cefa6dbbe868639664d938b272a5a
miod <miod>
2008-03-31 22:14:43 +0000

    Switch bootloaders to mi loadfile().

--- sys/arch/mvme88k/stand/bootst/version.c
+++ sys/arch/mvme88k/stand/bootst/version.c
@@ -1,3 +1,7 @@
-/*	$OpenBSD: version.c,v 1.2 1998/12/15 06:09:51 smurph Exp $ */
+/*	$OpenBSD: version.c,v 1.3 2006/05/16 22:52:55 miod Exp $ */
 
-char *version = "1.3";
+/*
+ *	1.4	kernel loaded with loadfile, a.out and ELF formats
+ *	1.3	rewritten startup code and general cleanup
+ */
+char *version = "1.4";
