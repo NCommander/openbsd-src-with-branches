535b1c793fa00c4eafdd8090d8b038fa184648be
deraadt <deraadt>
2008-04-09 22:04:11 +0000

    Recognize and support the Microchip MCP9805 JC-42.2 sensor (we hope; I am simply going through datasheets found on the net...)

--- sys/dev/i2c/i2c_scan.c
+++ sys/dev/i2c/i2c_scan.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: i2c_scan.c,v 1.118 2008/04/07 00:31:07 deraadt Exp $	*/
+/*	$OpenBSD: i2c_scan.c,v 1.119 2008/04/09 21:56:40 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2005 Theo de Raadt <deraadt@openbsd.org>
@@ -855,6 +855,10 @@ iic_probe_sensor(struct device *self, u_int8_t addr)
 	    iicprobew(0x07) == 0xa200 &&
 	    (iicprobew(0x00) & 0xfff0) == 0x0010) {
 		name = "se97";
+	} else if ((addr & 0x18) == 0x18 && iicprobew(0x06) == 0x0054 &&
+	    iicprobew(0x07) == 0x0000 &&
+	    (iicprobew(0x00) & 0xffe0) == 0x0000) {
+		name = "mcp9805";
 	} else if ((addr & 0x18) == 0x18 && iicprobew(0x06) == 0x11d4 &&
 	    (iicprobew(0x07) & 0xfff0) == 0x0800 &&
 	    iicprobew(0x00) == 0x001d) {
