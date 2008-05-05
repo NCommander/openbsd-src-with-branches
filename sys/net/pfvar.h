a101c295f9a2f94746728efec7cbdfc6a737ae4e
henning <henning>
2008-05-05 13:00:43 +0000

    remove a useless refcnt in pf_state_key. when it is in fact only used to delete the state key when the number of attached states (in a tailq) drops to zero, we can as well test for the queue beeing empty. this is a leftover from some early version that did things differently. ok ryan

--- sys/net/pfvar.h
+++ sys/net/pfvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pfvar.h,v 1.258 2007/09/27 22:24:05 mpf Exp $ */
+/*	$OpenBSD: pfvar.h,v 1.259 2007/12/02 12:08:04 pascoe Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -713,7 +713,6 @@ struct pf_state_key {
 	RB_ENTRY(pf_state_key)	 entry_lan_ext;
 	RB_ENTRY(pf_state_key)	 entry_ext_gwy;
 	struct pf_statelist	 states;
-	u_short		 refcnt;	/* same size as if_index */	 
 };
 
 
