80e3c93bb79f7f10bb5eb2fc5ff40023369990d9
krw <krw>
2008-05-06 07:18:09 +0000

    M_PREPEND can set its first parameter to NULL, and all other code using M_PREPEND checks for NULL after the invocation. So check here too and return ENOBUFS if NULL is detected.
    ok henning@

--- sys/net/if_tun.c
+++ sys/net/if_tun.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_tun.c,v 1.90 2007/09/15 16:43:51 henning Exp $	*/
+/*	$OpenBSD: if_tun.c,v 1.91 2007/12/20 02:53:02 brad Exp $	*/
 /*	$NetBSD: if_tun.c,v 1.24 1996/05/07 02:40:48 thorpej Exp $	*/
 
 /*
@@ -551,6 +551,8 @@ tun_output(struct ifnet *ifp, struct mbuf *m0, struct sockaddr *dst,
 		return (ether_output(ifp, m0, dst, rt));
 
 	M_PREPEND(m0, sizeof(*af), M_DONTWAIT);
+	if (m0 == NULL)
+		return (ENOBUFS);
 	af = mtod(m0, u_int32_t *);
 	*af = htonl(dst->sa_family);
 
