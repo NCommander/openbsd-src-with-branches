1a38c351b31796c8fa2422dbb7796796dba60676
tedu <tedu>
2008-05-11 23:54:40 +0000

    set p_flag to 0 sooner, so we don't overwrite the thread flag.  and correctly free things when exiting a threaded proc.  from philip guenther

--- sys/kern/kern_exit.c
+++ sys/kern/kern_exit.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_exit.c,v 1.71 2007/04/12 22:14:15 tedu Exp $	*/
+/*	$OpenBSD: kern_exit.c,v 1.72 2007/10/10 15:53:53 art Exp $	*/
 /*	$NetBSD: kern_exit.c,v 1.39 1996/04/22 01:38:25 christos Exp $	*/
 
 /*
@@ -590,10 +590,8 @@ proc_zap(struct proc *p)
 	 * Remove us from our process list, possibly killing the process
 	 * in the process (pun intended).
 	 */
-#if 0
-	TAILQ_REMOVE(&p->p_p->ps_threads, p, p_thr_link);
-#endif
-	if (TAILQ_EMPTY(&p->p_p->ps_threads)) {
+	if ((p->p_flag & P_THREAD) == 0) {
+		KASSERT(TAILQ_EMPTY(&p->p_p->ps_threads));
 		limfree(p->p_p->ps_limit);
 		if (--p->p_p->ps_cred->p_refcnt == 0) {
 			crfree(p->p_p->ps_cred->pc_ucred);
