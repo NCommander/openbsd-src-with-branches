5bd762617540a48fbfe967937f0d09470dbf3a39
millert <millert>
2008-05-13 18:52:25 +0000

    Don't forget to va_end() the va_list we get from va_copy().  From FreeBSD.

--- lib/libc/stdio/vfprintf.c
+++ lib/libc/stdio/vfprintf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vfprintf.c,v 1.43 2007/11/28 19:06:19 deraadt Exp $	*/
+/*	$OpenBSD: vfprintf.c,v 1.44 2008/05/05 21:50:45 chl Exp $	*/
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
  * All rights reserved.
@@ -813,6 +813,7 @@ number:			if ((dprec = prec) >= 0)
 done:
 	FLUSH();
 error:
+	va_end(orgap);
 	if (__sferror(fp))
 		ret = -1;
 	goto finish;
