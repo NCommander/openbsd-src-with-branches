15630e62e1fc468b4bd319665444d19d19efd134
oga <oga>
2008-06-03 17:21:22 +0000

    Make *drm(4) use D_CLONE so that the per-open data actually works. Since i'm modifying this code anyway, prepare for privsep by making it so that "master" openers must be root, and remove some spurious suser() checks. For example, every DRM_ROOT_ONLY ioctl is also DRM_MASTER. Without this change, privsep wouldn't work since the fd is no longer root owned.
    With this, X privsep should work as soon as the userland bits are done
    (currently unwritten).
    
    Looked over by kettenis@, ok thib@.

--- sys/sys/conf.h
+++ sys/sys/conf.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: conf.h,v 1.87 2008/04/09 19:49:55 robert Exp $	*/
+/*	$OpenBSD: conf.h,v 1.88 2008/05/24 19:37:34 mglocker Exp $	*/
 /*	$NetBSD: conf.h,v 1.33 1996/05/03 20:03:32 christos Exp $	*/
 
 /*-
@@ -516,7 +516,7 @@ void	randomattach(void);
 	dev_init(c,n,open), dev_init(c,n,close), dev_init(c,n,read), \
 	(dev_type_write((*))) enodev, dev_init(c,n,ioctl), \
 	(dev_type_stop((*))) enodev, 0, dev_init(c,n,poll), \
-	dev_init(c,n,mmap), D_TTY }
+	dev_init(c,n,mmap), 0, D_CLONE }
 
 #endif
 
