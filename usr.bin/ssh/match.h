b21c4c5074def2ce3e78eb6735421c2a45927c3f
djm <djm>
2008-06-10 03:57:27 +0000

    support CIDR address matching in sshd_config "Match address" blocks, with full support for negation and fall-back to classic wildcard matching. For example:
    Match address 192.0.2.0/24,3ffe:ffff::/32,!10.*
        PasswordAuthentication yes
    
    addrmatch.c code mostly lifted from flowd's addr.c
    
    feedback and ok dtucker@

--- usr.bin/ssh/match.h
+++ usr.bin/ssh/match.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: match.h,v 1.12 2002/03/01 13:12:10 markus Exp $ */
+/* $OpenBSD: match.h,v 1.13 2006/03/25 22:22:43 djm Exp $ */
 
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
@@ -20,5 +20,8 @@ int	 match_hostname(const char *, const char *, u_int);
 int	 match_host_and_ip(const char *, const char *, const char *);
 int	 match_user(const char *, const char *, const char *, const char *);
 char	*match_list(const char *, const char *, u_int *);
+
+/* addrmatch.c */
+int	 addr_match_list(const char *, const char *);
 
 #endif
