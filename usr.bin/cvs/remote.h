2ec2c1a850946959e7780a3ab56eb3d1538c84e7
joris <joris>
2008-06-12 07:16:14 +0000

    completely kill the need for TMP_DIR when running checkout, which gives us better performance among other things.
    this is however disabled in a few cases:
    1) whenever you run checkout over an existing tree (which acts as update)
    2) whenever you specify -d or a module alias
    
    we're planning on enabling this behaviour for case 2 in the future.
    
    anoncvs sysadmins now love us even more.
    
    ok tobias@

--- usr.bin/cvs/remote.h
+++ usr.bin/cvs/remote.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: remote.h,v 1.31 2008/02/10 14:00:41 joris Exp $	*/
+/*	$OpenBSD: remote.h,v 1.32 2008/02/27 22:34:04 joris Exp $	*/
 /*
  * Copyright (c) 2006 Joris Vink <joris@openbsd.org>
  *
@@ -120,6 +120,7 @@ void	cvs_remote_output(const char *);
 char	*cvs_remote_input(void);
 void	cvs_remote_receive_file(int, size_t);
 void	cvs_remote_send_file(const char *, int);
+void	cvs_remote_send_file_buf(char *, BUF *, mode_t);
 
 extern int cvs_client_inlog_fd;
 extern int cvs_client_outlog_fd;
