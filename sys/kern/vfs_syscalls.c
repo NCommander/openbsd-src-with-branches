13d8ec0784008499c2a0dcaef23a8fe89d5c7472
rainer <rainer>
2008-06-13 01:59:45 +0000

    Delete vfs_bufstats() leftovers and unbreak compile with -DDEBUG
    Ok reyk@, "kill kill kill" thib@

--- sys/kern/vfs_syscalls.c
+++ sys/kern/vfs_syscalls.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vfs_syscalls.c,v 1.145 2008/01/22 14:33:40 millert Exp $	*/
+/*	$OpenBSD: vfs_syscalls.c,v 1.146 2008/05/22 17:04:59 thib Exp $	*/
 /*	$NetBSD: vfs_syscalls.c,v 1.71 1996/04/23 10:29:02 mycroft Exp $	*/
 
 /*
@@ -489,10 +489,6 @@ sys_sync(struct proc *p, void *v, register_t *retval)
 		vfs_unbusy(mp);
 	}
 
-#ifdef DEBUG
-	if (syncprt)
-		vfs_bufstats();
-#endif /* DEBUG */
 	return (0);
 }
 
