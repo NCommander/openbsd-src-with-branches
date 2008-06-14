e1e930e4622a0674c448797f3d016c640d6c863c
kjell <kjell>
2008-06-14 08:46:30 +0000

    We do not need a whole MODE for blinking one character.
    both emacsen automatically blink, so mg should too. This can be
    overridden in your ~/.mg by adding, e.g.
    
    global-set-key ")" self-insert-command
    
    So, I've eliminated blink mode, and renamed "blink-matching-paren-hack"
    to "blink-and-insert". It's not just for parens anyway.
    
    While I'm here, set up an empty (rescan) target for backtab, so I can
    bind something convenient to it; e.g.
    
    global-set-key "\e[Z" backward-char
    
    Finally, remove all references to Scribd.
    Theo doesn't hate this, though I would hesitate to call it an ok.

--- usr.bin/mg/modes.c
+++ usr.bin/mg/modes.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: modes.c,v 1.16 2005/12/13 07:20:13 kjell Exp $	*/
+/*	$OpenBSD: modes.c,v 1.17 2008/06/11 23:23:55 kjell Exp $	*/
 
 /* This file is in the public domain. */
 
@@ -69,15 +69,6 @@ int
 fillmode(int f, int n)
 {
 	return (changemode(f, n, "fill"));
-}
-
-/*
- * Fake the GNU "blink-matching-paren" variable.
- */
-int
-blinkparen(int f, int n)
-{
-	return (changemode(f, n, "blink"));
 }
 
 #ifdef NOTAB
