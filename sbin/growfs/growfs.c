83bceb4c3cc69358271eac6402480c23b5f906d0
sobrado <sobrado>
2008-06-24 08:33:04 +0000

    Index: src/sbin/growfs/growfs.c =================================================================== RCS file: /cvs/src/sbin/growfs/growfs.c,v retrieving revision 1.23 diff -u -p -r1.23 growfs.c --- src/sbin/growfs/growfs.c	5 Oct 2007 13:56:14 -0000	1.23 +++ src/sbin/growfs/growfs.c	4 Jun 2008 10:10:32 -0000 @@ -2227,7 +2227,7 @@ usage(void)
     	DBG_ENTER;
    
    -	fprintf(stderr, "usage: growfs [-Ny] [-s size] special\n");
    +	fprintf(stderr, "usage: growfs [-Nqy] [-s size] special\n");
    
     	DBG_LEAVE;
     	exit(1);

--- sbin/growfs/growfs.c
+++ sbin/growfs/growfs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: growfs.c,v 1.22 2007/09/02 23:50:03 deraadt Exp $	*/
+/*	$OpenBSD: growfs.c,v 1.23 2007/10/05 13:56:14 chl Exp $	*/
 /*
  * Copyright (c) 2000 Christoph Herrmann, Thomas-Henning von Kamptz
  * Copyright (c) 1980, 1989, 1993 The Regents of the University of California.
@@ -2227,7 +2227,7 @@ usage(void)
 
 	DBG_ENTER;
 
-	fprintf(stderr, "usage: growfs [-Ny] [-s size] special\n");
+	fprintf(stderr, "usage: growfs [-Nqy] [-s size] special\n");
 
 	DBG_LEAVE;
 	exit(1);
