8ab2a03a1dd8b23fe93676fd04607b495149c4c6
guenther <guenther>
2008-07-07 04:59:22 +0000

    Application-specified thread stacks didn't work with libpthread because it treated the supplied memory as holding an internal data structure instead of as the stack space itself
    ok kurt@, "looks ok" otto@, tested on hppa by kettenis@

--- lib/libpthread/uthread/uthread_create.c
+++ lib/libpthread/uthread/uthread_create.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uthread_create.c,v 1.20 2004/02/16 22:51:11 brad Exp $	*/
+/*	$OpenBSD: uthread_create.c,v 1.21 2005/01/23 19:23:47 kettenis Exp $	*/
 /*
  * Copyright (c) 1995-1998 John Birrell <jb@cimlogic.com.au>
  * All rights reserved.
@@ -84,17 +84,13 @@ pthread_create(pthread_t *thread, const pthread_attr_t *attr,
 		} else {
 			pattr = *attr;
 		}
-		/* Check if a stack was specified in the thread attributes: */
-		if ((stack = pattr->stackaddr_attr) != NULL) {
-		}
+
 		/* Allocate a stack: */
-		else {
-			stack = _thread_stack_alloc(pattr->stackaddr_attr,
-			    pattr->stacksize_attr);
-			if (stack == NULL) {
-				ret = EAGAIN;
-				free(new_thread);
-			}
+		stack = _thread_stack_alloc(pattr->stackaddr_attr,
+		    pattr->stacksize_attr);
+		if (stack == NULL) {
+			ret = EAGAIN;
+			free(new_thread);
 		}
 
 		/* Check for errors: */
