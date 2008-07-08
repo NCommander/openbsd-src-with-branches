f29825910870192b97d14bee3dd6ecccf754d4f3
martynas <martynas>
2008-07-08 21:07:57 +0000

    - add support for recursive transfers (but not for floppies), e.g. 'mget -cr 4.*' would recursively fetch (-r), and resume the previous transfers (-c) of 4.X release directories uses local matching (fnmatch), but only for recursive transfers. current behavior is not changed in any way. - while here, ifndef SMALL debugging stuff, this saves some space, for floppies - some debugging code was enabled for non-debugging mode, checks assume debug is set to zero, but it's not initially set - all "Confirm with" prompts are forced, remove redundant argument - fix usage: -C and -c are not available for SMALL
    discussed a year ago w/ pyr@
    looks good to millert@
    previous version looked good to pyr@
    man page tweaks & ok jmc@

--- usr.bin/ftp/complete.c
+++ usr.bin/ftp/complete.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: complete.c,v 1.19 2006/06/23 20:35:25 steven Exp $	*/
+/*	$OpenBSD: complete.c,v 1.20 2008/06/26 05:42:20 ray Exp $	*/
 /*	$NetBSD: complete.c,v 1.10 1997/08/18 10:20:18 lukem Exp $	*/
 
 /*-
@@ -32,7 +32,7 @@
 
 #ifndef SMALL
 #ifndef lint
-static const char rcsid[] = "$OpenBSD: complete.c,v 1.19 2006/06/23 20:35:25 steven Exp $";
+static const char rcsid[] = "$OpenBSD: complete.c,v 1.20 2008/06/26 05:42:20 ray Exp $";
 #endif /* not lint */
 
 /*
@@ -234,8 +234,10 @@ complete_remote(char *word, int list)
 
 		mflag = 1;
 		emesg = NULL;
+#ifndef SMALL
 		if (debug)
 			(void)putc('\n', ttyout);
+#endif /* !SMALL */
 		while ((cp = remglob(dummyargv, 0, &emesg)) != NULL) {
 			char *tcp;
 
