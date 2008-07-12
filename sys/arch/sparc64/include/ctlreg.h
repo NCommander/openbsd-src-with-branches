16da50e2ddbd4576bd0cbc1e7e566c78b47c6eab
kettenis <kettenis>
2008-07-12 07:37:25 +0000

    Add macros to get the appropriate interrupt target ID for non-UPA CPUs and use them in cpu_myid().

--- sys/arch/sparc64/include/ctlreg.h
+++ sys/arch/sparc64/include/ctlreg.h
@@ -246,10 +246,17 @@
  * The following are 4u control registers
  */
 
+/* Get the CPU's UPA port ID */
+#define	UPA_CR_MID(x)		(((x) >> 17) & 0x1f)
+#define	CPU_UPAID		UPA_CR_MID(ldxa(0, ASI_MID_REG))
 
-/* Get the CPU's UPAID */
-#define	UPA_CR_MID(x)	(((x)>>17)&0x1f)	
-#define	CPU_UPAID	UPA_CR_MID(ldxa(0, ASI_MID_REG))
+/* Get the CPU's Fireplane agent ID */
+#define FIREPLANE_CR_AID(x)	(((x) >> 17) & 0x3ff)
+#define CPU_FIREPLANEID		FIREPLANE_CR_AID(ldxa(0, ASI_MID_REG))
+
+/* Get the CPU's Jupiter Bus interrupt target ID */
+#define JUPITER_CR_ITID(x)	((x) & 0x3ff)
+#define CPU_JUPITERID		JUPITER_CR_ITID(ldxa(0, ASI_MID_REG))
 
 /*
  * [4u] MMU and Cache Control Register (MCCR)
