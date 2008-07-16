b78fa3c9338d70865de710f95c101cfd769fe118
martynas <martynas>
2008-07-16 15:11:16 +0000

    add -E flag and 'skipempty' option to skip sending messages with empty bodies.  useful for sending mails from crontabs. from rivo nurges <rix at estpak dot ee>, with missing 'skipempty' documentation and usage update from me ok millert@

--- usr.bin/mail/send.c
+++ usr.bin/mail/send.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: send.c,v 1.18 2007/03/20 21:01:08 millert Exp $	*/
+/*	$OpenBSD: send.c,v 1.19 2008/07/16 14:53:41 martynas Exp $	*/
 /*	$NetBSD: send.c,v 1.6 1996/06/08 19:48:39 christos Exp $	*/
 
 /*
@@ -34,7 +34,7 @@
 #if 0
 static const char sccsid[] = "@(#)send.c	8.1 (Berkeley) 6/6/93";
 #else
-static const char rcsid[] = "$OpenBSD: send.c,v 1.18 2007/03/20 21:01:08 millert Exp $";
+static const char rcsid[] = "$OpenBSD: send.c,v 1.19 2008/07/16 14:53:41 martynas Exp $";
 #endif
 #endif /* not lint */
 
@@ -339,6 +339,8 @@ mail1(struct header *hp, int printheaders)
 	if ((mtf = collect(hp, printheaders)) == NULL)
 		return;
 	if (fsize(mtf) == 0) {
+		if (value("skipempty") != NULL)
+			goto out;
 		if (hp->h_subject == NULL)
 			puts("No message, no subject; hope that's ok");
 		else
