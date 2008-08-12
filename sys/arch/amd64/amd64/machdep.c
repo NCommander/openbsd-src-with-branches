68c0ac03d7c1d198c25a2b0b14aec1c2481b8f9a
weingart <weingart>
2008-08-12 18:27:22 +0000

    Nuke bigmem for release, still has issues.
    ok deraadt@

--- sys/arch/amd64/amd64/machdep.c
+++ sys/arch/amd64/amd64/machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: machdep.c,v 1.80 2008/06/26 05:42:09 ray Exp $	*/
+/*	$OpenBSD: machdep.c,v 1.81 2008/06/27 17:22:14 miod Exp $	*/
 /*	$NetBSD: machdep.c,v 1.3 2003/05/07 22:58:18 fvdl Exp $	*/
 
 /*-
@@ -1198,7 +1198,7 @@ extern vector IDTVEC(oosyscall);
 extern vector *IDTVEC(exceptions)[];
 
 /* Tweakable by config(8) */
-int bigmem = 1;
+int bigmem = 0;
 
 void
 init_x86_64(paddr_t first_avail)
