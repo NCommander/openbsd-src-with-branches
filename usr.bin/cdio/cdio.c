e2b419fb082787652ae5648868a8f038de32c018
av <av>
2008-08-13 12:21:19 +0000

    Old drives (reported Giovanni Bechis <bigionews@snb.it>) don't report CD-RW write feature. Add additional check for media type before blanking. The patch fixes problem for Giovanni. ok fgsch

--- usr.bin/cdio/cdio.c
+++ usr.bin/cdio/cdio.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cdio.c,v 1.64 2008/07/23 21:33:32 av Exp $	*/
+/*	$OpenBSD: cdio.c,v 1.65 2008/08/08 07:26:40 fgsch Exp $	*/
 
 /*  Copyright (c) 1995 Serge V. Vakulenko
  * All rights reserved.
@@ -517,7 +517,8 @@ run(int cmd, char *arg)
 			warnx("Can't determine media type");
 			return (0);
 		}
-		if ((mediacap & MEDIACAP_CDRW_WRITE) == 0) {
+		if ((mediacap & MEDIACAP_CDRW_WRITE) == 0 &&
+		    get_media_type() != MEDIATYPE_CDRW) {
 			warnx("The media doesn't support blanking");
 			return (0);
 		}
