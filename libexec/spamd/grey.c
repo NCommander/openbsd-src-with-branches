f9a7291e0ab3a02b66ce88905727eb438971b60a
jsg <jsg>
2008-08-26 22:49:09 +0000

    Fix an if (); typo. ok beck@

--- libexec/spamd/grey.c
+++ libexec/spamd/grey.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: grey.c,v 1.42 2008/07/11 01:40:50 phessler Exp $	*/
+/*	$OpenBSD: grey.c,v 1.43 2008/07/11 14:53:32 reyk Exp $	*/
 
 /*
  * Copyright (c) 2004-2006 Bob Beck.  All rights reserved.
@@ -293,7 +293,7 @@ dequotetolower(const char *addr)
 	static char buf[MAX_MAIL];
 	char *cp;
 
-	if (*addr == '<');
+	if (*addr == '<')
 		addr++;
 	(void) strlcpy(buf, addr, sizeof(buf));
 	cp = strrchr(buf, '>');
