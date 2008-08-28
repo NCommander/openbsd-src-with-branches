3d3d51014e5136fc8c1e4f06a7a62e58e59987b9
brad <brad>
2008-08-28 13:10:54 +0000

    Fix a typo in the naming of the PPPoE input queues, ppoe -> pppoe.
    ok canacar@

--- sys/net/if_pppoe.h
+++ sys/net/if_pppoe.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_pppoe.h,v 1.3 2004/11/30 11:44:18 markus Exp $ */
+/*	$OpenBSD: if_pppoe.h,v 1.4 2008/06/26 05:42:20 ray Exp $ */
 /*	$NetBSD: if_pppoe.h,v 1.5 2003/11/28 08:56:48 keihan Exp $ */
 
 /*
@@ -66,8 +66,8 @@ struct pppoeconnectionstate {
 
 #ifdef _KERNEL
 
-extern struct ifqueue ppoediscinq;
-extern struct ifqueue ppoeinq;
+extern struct ifqueue pppoediscinq;
+extern struct ifqueue pppoeinq;
 
 void pppoeintr(void);
 
