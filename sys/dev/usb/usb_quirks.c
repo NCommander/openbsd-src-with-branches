39629d868d6685e69ee9dba4bc4e5c63d4639c5c
martynas <martynas>
2008-09-08 16:58:32 +0000

    prevent mustek from attaching as uhid.  ok theo, sthen@

--- sys/dev/usb/usb_quirks.c
+++ sys/dev/usb/usb_quirks.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: usb_quirks.c,v 1.46 2008/08/26 22:25:20 deraadt Exp $ */
+/*	$OpenBSD: usb_quirks.c,v 1.47 2008/08/27 17:33:32 deraadt Exp $ */
 /*	$NetBSD: usb_quirks.c,v 1.45 2003/05/10 17:47:14 hamajima Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/usb_quirks.c,v 1.30 2003/01/02 04:15:55 imp Exp $	*/
 
@@ -137,6 +137,7 @@ const struct usbd_quirk_entry {
  { USB_VENDOR_MGE, USB_PRODUCT_MGE_UPS1,	    ANY,   { UQ_BAD_HID }},
  { USB_VENDOR_MGE, USB_PRODUCT_MGE_UPS2,	    ANY,   { UQ_BAD_HID }},
  { USB_VENDOR_CYBERPOWER, USB_PRODUCT_CYBERPOWER_1500,	ANY,	{ UQ_BAD_HID }},
+ { USB_VENDOR_MUSTEK2, USB_PRODUCT_MUSTEK2_PM800,	ANY,	{ UQ_BAD_HID }},
 
   /* MS keyboards do weird things */
   { USB_VENDOR_MICROSOFT, USB_PRODUCT_MICROSOFT_WLNOTEBOOK,
