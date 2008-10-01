dd67808d36630b1b683bada79fc01ef8d1af5700
drahn <drahn>
2008-10-01 14:59:18 +0000

    Assert my copyright on files I gave to d@ back in 1998.

--- lib/libpthread/arch/powerpc/_atomic_lock.c
+++ lib/libpthread/arch/powerpc/_atomic_lock.c
@@ -1,4 +1,20 @@
-/*	$OpenBSD: _atomic_lock.c,v 1.6 2005/12/14 21:29:48 drahn Exp $	*/
+/*	$OpenBSD: _atomic_lock.c,v 1.7 2006/02/06 17:03:17 jmc Exp $	*/
+/*
+ * Copyright (c) 1998 Dale Rahn <drahn@openbsd.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 /*
  * Atomic lock for powerpc
  */
