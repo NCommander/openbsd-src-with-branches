b199621f9a8557177b12dadee5146d1c7789cba2
miod <miod>
2008-10-04 19:21:00 +0000

    Remove bogus code from the error path in sr_raid0_rw(); ok marco@

--- sys/dev/softraid_raid0.c
+++ sys/dev/softraid_raid0.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: softraid_raid0.c,v 1.8 2008/02/05 16:49:25 marco Exp $ */
+/* $OpenBSD: softraid_raid0.c,v 1.9 2008/07/19 22:41:58 marco Exp $ */
 /*
  * Copyright (c) 2008 Marco Peereboom <marco@peereboom.us>
  *
@@ -235,7 +235,6 @@ sr_raid0_rw(struct sr_workunit *wu)
 		/* make sure chunk is online */
 		scp = sd->sd_vol.sv_chunks[chunk];
 		if (scp->src_meta.scm_status != BIOC_SDONLINE) {
-			sr_ccb_put(ccb);
 			goto bad;
 		}
 
