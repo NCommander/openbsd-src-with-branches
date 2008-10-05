538f8ebee4718b5102df96fc8146b6b7cf87ed9f
kettenis <kettenis>
2008-10-05 16:57:36 +0000

    Remove IOAPIC ID remapping code.  It should not be necessary to avoid reusing LAPIC IDs and if there are MPBIOSes we handle them with ACPI now.
    ok deraadt@

--- sys/arch/i386/i386/mpbios.c
+++ sys/arch/i386/i386/mpbios.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mpbios.c,v 1.24 2008/06/26 05:42:10 ray Exp $	*/
+/*	$OpenBSD: mpbios.c,v 1.25 2008/09/16 20:02:47 brad Exp $	*/
 /*	$NetBSD: mpbios.c,v 1.2 2002/10/01 12:56:57 fvdl Exp $	*/
 
 /*-
@@ -1035,7 +1035,7 @@ mpbios_int(const u_int8_t *ent, struct mp_intr_map *mpi)
 	struct mp_intr_map *altmpi;
 	struct mp_bus *mpb;
 
-	u_int32_t id = IOAPIC_REMAPPED_ID(entry->dst_apic_id);
+	u_int32_t id = entry->dst_apic_id;
 	u_int32_t pin = entry->dst_apic_int;
 	u_int32_t bus = entry->src_bus_id;
 	u_int32_t dev = entry->src_bus_irq;
