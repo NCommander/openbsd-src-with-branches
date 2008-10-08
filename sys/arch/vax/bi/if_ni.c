27d41c3b8fc68d2ab26c63962981ddae120728ff
brad <brad>
2008-10-08 23:53:08 +0000

    cosmetic change for ioctl funtions.. move splnet out from variable declaration.

--- sys/arch/vax/bi/if_ni.c
+++ sys/arch/vax/bi/if_ni.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ni.c,v 1.10 2007/05/11 10:06:55 pedro Exp $ */
+/*	$OpenBSD: if_ni.c,v 1.11 2008/10/02 20:21:13 brad Exp $ */
 /*	$NetBSD: if_ni.c,v 1.15 2002/05/22 16:03:14 wiz Exp $ */
 /*
  * Copyright (c) 2000 Ludd, University of Lule}, Sweden. All rights reserved.
@@ -697,7 +697,9 @@ niioctl(ifp, cmd, data)
 	struct ni_softc *sc = ifp->if_softc;
 	struct ifreq *ifr = (struct ifreq *)data;
 	struct ifaddr *ifa = (struct ifaddr *)data;
-	int s = splnet(), error = 0;
+	int s, error = 0;
+
+	s = splnet();
 
 	switch (cmd) {
 	case SIOCSIFADDR:
