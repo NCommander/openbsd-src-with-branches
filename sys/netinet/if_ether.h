6589b4e241f1a31a26eef4a33fea0861a0649139
claudio <claudio>
2008-10-16 18:23:53 +0000

    Kill M_HASFCS, it is not used in OpenBSD. If a ethernet chip returns the checksum in the packet it should be trimmed away by the driver and not by driver independent code. OK brad@

--- sys/netinet/if_ether.h
+++ sys/netinet/if_ether.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ether.h,v 1.39 2007/05/16 09:24:07 dlg Exp $	*/
+/*	$OpenBSD: if_ether.h,v 1.40 2008/04/18 09:16:14 djm Exp $	*/
 /*	$NetBSD: if_ether.h,v 1.22 1996/05/11 13:00:00 mycroft Exp $	*/
 
 /*
@@ -87,11 +87,6 @@ struct	ether_header {
  */
 #define	ETHER_CRC_POLY_LE	0xedb88320
 #define	ETHER_CRC_POLY_BE	0x04c11db6
-
-/*
- * Ethernet-specific mbuf flags.
- */
-#define M_HASFCS	M_LINK0	/* FCS included at end of frame */
 
 #ifdef _KERNEL
 /*
