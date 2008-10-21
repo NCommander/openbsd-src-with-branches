b634ecc7753ad9d20fe707e7f3fd1366069abdff
marco <marco>
2008-10-21 03:16:21 +0000

    Stray space

--- sys/dev/ic/amireg.h
+++ sys/dev/ic/amireg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: amireg.h,v 1.26 2006/04/26 07:53:22 dlg Exp $	*/
+/*	$OpenBSD: amireg.h,v 1.27 2006/06/09 04:27:10 marco Exp $	*/
 
 /*
  * Copyright (c) 2000 Michael Shalayeff
@@ -515,7 +515,7 @@ struct ami_fc_einquiry {
 
 	u_int8_t	ain_pdstat[AMI_BIG_MAX_PDRIVES];
 	u_int16_t	ain_pdfmtinp[AMI_BIG_MAX_PDRIVES];
-	u_int8_t	ain_pdrates [80];	/* pdrv xfer rates */
+	u_int8_t	ain_pdrates[80];	/* pdrv xfer rates */
 	u_int8_t	ain_pad[263];		/* pad to 1k */
 } __packed;
 
