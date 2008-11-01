98ef3ef398b2a7d3fb10b7c2dd9013c959b48a56
deraadt <deraadt>
2008-11-01 20:34:10 +0000

    change vrele() to return an int.  if it returns 0, it can gaurantee that it did not sleep.  this is used to avoid checkdirs() to avoid having to restart the allproc walk every time through idea from tedu, ok thib pedro

--- sys/sys/vnode.h
+++ sys/sys/vnode.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vnode.h,v 1.94 2008/05/08 17:45:45 thib Exp $	*/
+/*	$OpenBSD: vnode.h,v 1.95 2008/06/09 23:38:37 millert Exp $	*/
 /*	$NetBSD: vnode.h,v 1.38 1996/02/29 20:59:05 cgd Exp $	*/
 
 /*
@@ -365,7 +365,7 @@ int	vwaitforio(struct vnode *, int, char *, int);
 void	vwakeup(struct vnode *);
 void	vput(struct vnode *);
 int	vrecycle(struct vnode *, struct proc *);
-void	vrele(struct vnode *);
+int	vrele(struct vnode *);
 void	vref(struct vnode *);
 void	vprint(char *, struct vnode *);
 void	copy_statfs_info(struct statfs *, const struct mount *);
