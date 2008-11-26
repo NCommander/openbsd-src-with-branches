c236ca017a73db6e89e88c32c7c1d2a1c3a44a04
drahn <drahn>
2008-11-26 14:47:50 +0000

    Start at the port of OpenBSD to the OpenMoko hardware. Work in progress.

---
+++ sys/arch/moko/include/_types.h
@@ -0,0 +1,11 @@
+/* 	$OpenBSD: _types.h,v 1.4 2008/01/03 17:59:32 kettenis Exp $	*/
+
+#ifndef _MOKO_TYPES_H_
+#define	_MOKO_TYPES_H_
+
+#include <arm/_types.h>
+
+#define __HAVE_GENERIC_SOFT_INTERRUPTS
+#define __HAVE_TIMECOUNTER
+
+#endif
