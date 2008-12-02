e57a69a6550c81694d3189d1e6b22fd26338aba6
deraadt <deraadt>
2008-12-02 00:51:40 +0000

    no need to close() before exit()

--- distrib/common/rdsetroot.c
+++ distrib/common/rdsetroot.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rdsetroot.c,v 1.13 2008/12/02 00:42:41 weerd Exp $	*/
+/*	$OpenBSD: rdsetroot.c,v 1.14 2008/12/02 00:48:19 deraadt Exp $	*/
 /*	$NetBSD: rdsetroot.c,v 1.2 1995/10/13 16:38:39 gwr Exp $	*/
 
 /*
@@ -205,7 +205,6 @@ main(int argc, char *argv[])
 #ifdef	DEBUG
 	fprintf(stderr, "copied %d bytes\n", n);
 #endif
-	close(fd);
 	exit(0);
 }
 
