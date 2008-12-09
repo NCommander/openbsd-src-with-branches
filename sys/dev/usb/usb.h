15fc01122f24019c10abb14316fbda4338810dc1
yuo <yuo>
2008-12-09 03:08:07 +0000

    assign ehcidebug to USB_DEBUG ioctl argument.
    add priv check to the ioctl.
    only root priv should access these debug flags.
    
    ok deraadt@

--- sys/dev/usb/usb.h
+++ sys/dev/usb/usb.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: usb.h,v 1.32 2008/06/29 10:04:15 yuo Exp $ */
+/*	$OpenBSD: usb.h,v 1.33 2008/08/09 22:59:20 mglocker Exp $ */
 /*	$NetBSD: usb.h,v 1.69 2002/09/22 23:20:50 augustss Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/usb.h,v 1.14 1999/11/17 22:33:46 n_hibma Exp $	*/
 
@@ -653,7 +653,7 @@ struct usb_event {
 
 /* USB controller */
 #define USB_REQUEST		_IOWR('U', 1, struct usb_ctl_request)
-#define USB_SETDEBUG		_IOW ('U', 2, int)
+#define USB_SETDEBUG		_IOW ('U', 2, unsigned int)
 #define USB_DISCOVER		_IO  ('U', 3)
 #define USB_DEVICEINFO		_IOWR('U', 4, struct usb_device_info)
 #define USB_DEVICESTATS		_IOR ('U', 5, struct usb_device_stats)
