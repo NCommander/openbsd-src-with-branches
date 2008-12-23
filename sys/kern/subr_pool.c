710bd99a13eb1a65ab0201f11c675f031f78549c
dlg <dlg>
2008-12-23 06:50:48 +0000

    record the offset into each pool page that item allocations actually begin on, aka, its coloring.
    ok tedu@

--- sys/kern/subr_pool.c
+++ sys/kern/subr_pool.c
@@ -72,6 +72,7 @@ struct pool_item_header {
 				ph_node;	/* Off-page page headers */
 	int			ph_nmissing;	/* # of chunks in use */
 	caddr_t			ph_page;	/* this page's address */
+	caddr_t			ph_colored;	/* page's colored address */
 	int			ph_pagesize;
 };
 
@@ -845,6 +846,7 @@ pool_prime_page(struct pool *pp, caddr_t storage, struct pool_item_header *ph)
 	 */
 	if (ioff != 0)
 		cp = (caddr_t)(cp + (align - ioff));
+	ph->ph_colored = cp;
 
 	/*
 	 * Insert remaining chunks on the bucket list.
