b56a8f0f00746cef3187ad15a4361a7b004e2a21
miod <miod>
2009-01-11 15:27:32 +0000

    Some frame buffer fcode do not fill window-top and window-left put nevertheless provide them; cope with zero values.

--- sys/arch/sparc/dev/fb.c
+++ sys/arch/sparc/dev/fb.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fb.c,v 1.44 2008/12/26 22:30:21 miod Exp $	*/
+/*	$OpenBSD: fb.c,v 1.45 2009/01/04 20:32:55 miod Exp $	*/
 /*	$NetBSD: fb.c,v 1.23 1997/07/07 23:30:22 pk Exp $ */
 
 /*
@@ -369,6 +369,9 @@ fbwscons_init(struct sunfb *sf, int isconsole)
 			 * 80x34 console window.
 			 */
 			fw = 12; fh = 22;
+			wt = 0; wl = 0;
+		}
+		if (wt == 0 || wl == 0) {
 			ri->ri_flg |= RI_CENTER;
 
 			/*
