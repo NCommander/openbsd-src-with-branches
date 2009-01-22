0d41243ebdf88563c529181c062f2490eebd1788
kettenis <kettenis>
2009-01-22 22:41:42 +0000

    Make bus_mem_add_mapping() return ENOMEM if we fail to allocate kva instead of panicing.
    ok drahn@

--- sys/arch/macppc/macppc/machdep.c
+++ sys/arch/macppc/macppc/machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: machdep.c,v 1.102 2008/11/21 17:35:52 deraadt Exp $	*/
+/*	$OpenBSD: machdep.c,v 1.103 2008/11/22 14:42:29 art Exp $	*/
 /*	$NetBSD: machdep.c,v 1.4 1996/10/16 19:33:11 ws Exp $	*/
 
 /*
@@ -1229,8 +1229,7 @@ bus_mem_add_mapping(bus_addr_t bpa, bus_size_t size, int cacheable,
 		vaddr = uvm_km_kmemalloc(phys_map, NULL, len,
 		    UVM_KMF_NOWAIT|UVM_KMF_VALLOC);
 		if (vaddr == 0)
-			panic("bus_mem_add_mapping: kvm alloc of 0x%x failed",
-			    len);
+			return (ENOMEM);
 	}
 	*bshp = vaddr + off;
 #ifdef DEBUG_BUS_MEM_ADD_MAPPING
