b7f62a46780aad4365eb89bd355cd814d1a1168a
espie <espie>
2009-03-05 10:43:00 +0000

    more stuff to check digests during extraction, if wanted. Fix read/write of digitalsignature stuff. don't integrate manual-installs, since it's highly variable. create correct category for digitalsignature, so it gets written out.

--- usr.sbin/pkg_add/OpenBSD/PackingList.pm
+++ usr.sbin/pkg_add/OpenBSD/PackingList.pm
@@ -381,7 +381,7 @@ sub is_signed
 }
 
 our @unique_categories =
-    (qw(name no-default-conflict manual-installation extrainfo localbase arch));
+    (qw(name digital-signature no-default-conflict manual-installation extrainfo localbase arch));
 
 our @list_categories =
     (qw(conflict pkgpath incompatibility updateset depend 
