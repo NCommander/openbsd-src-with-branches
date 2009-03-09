18ec2d142a5a4f2051d83aa3433619cfc2832ce1
miod <miod>
2009-03-09 19:51:18 +0000

    Switch mvme88k to timecounters.

--- sys/arch/mvme88k/include/_types.h
+++ sys/arch/mvme88k/include/_types.h
@@ -1,4 +1,7 @@
-/*	$OpenBSD: _types.h,v 1.2 2006/01/11 07:22:01 miod Exp $	*/
+/*	$OpenBSD: _types.h,v 1.3 2007/05/15 01:56:47 deraadt Exp $	*/
 
 /* public domain */
 #include <m88k/_types.h>
+
+/* Feature test macros */
+#define	__HAVE_TIMECOUNTER
