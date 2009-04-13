78f2d4a60332780b62cc04b3e941837936c75231
miod <miod>
2009-04-13 21:17:54 +0000

    The start of Origin 200 support. Based on some code contributed by pefo@ some years ago for KL enumeration, building on the existing XBow support to limit ourselves to a single node for now.
    This is a work-in-progress; it currently lacks complete interrupt code,
    as well as PCI resource management. And there are likely bugs creeping
    inside.

--- sys/arch/sgi/dev/dsrtc.c
+++ sys/arch/sgi/dev/dsrtc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: clock_md.c,v 1.11 2008/02/20 18:46:20 miod Exp $ */
+/*	$OpenBSD: dsrtc.c,v 1.1 2008/04/07 22:36:26 miod Exp $ */
 
 /*
  * Copyright (c) 2001-2004 Opsycon AB  (www.opsycon.se / www.opsycon.com)
@@ -95,10 +95,13 @@ tobcd(int x)
 int
 dsrtc_match_ioc(struct device *parent, void *match, void *aux)
 {
-	if (sys_config.system_type != SGI_OCTANE)
+	switch (sys_config.system_type) {
+	case SGI_OCTANE:
+	case SGI_O200:
+		return 1;
+	default:
 		return 0;
-
-	return 1;
+	}
 }
 
 void
