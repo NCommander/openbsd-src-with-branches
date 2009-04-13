cb79a577999ea0201646a8938f5f196fd2a0aad8
deraadt <deraadt>
2009-04-13 01:47:04 +0000

    add a "-T title" option to ftp; this is is used in progress bar mode to show which filename is being used. ok krw, mdoc repair from jmc

--- usr.bin/ftp/extern.h
+++ usr.bin/ftp/extern.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: extern.h,v 1.34 2008/06/25 21:15:19 martynas Exp $	*/
+/*	$OpenBSD: extern.h,v 1.35 2008/07/08 21:07:57 martynas Exp $	*/
 /*	$NetBSD: extern.h,v 1.17 1997/08/18 10:20:19 lukem Exp $	*/
 
 /*
@@ -210,6 +210,7 @@ extern struct	cmd cmdtab[];
 extern FILE    *cout;
 extern int	data;
 extern char    *home;
+extern char    *title;
 extern jmp_buf	jabort;
 extern int	family;
 extern int	proxy;
