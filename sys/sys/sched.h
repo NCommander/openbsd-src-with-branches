8623d99e35bca4de072c90aa7206f2c35918b685
art <art>
2009-04-14 09:13:25 +0000

    Some tweaks to the cpu affinity code.  - Split up choosing of cpu between fork and "normal" cases. Fork is    very different and should be treated as such.  - Instead of implicitly choosing a cpu in setrunqueue, do it outside    where it actually makes sense.  - Just because a cpu is marked as idle doesn't mean it will be soon.    There could be a thundering herd effect if we call wakeup from an    interrupt handler, so subtract cpus with queued processes when    deciding which cpu is actually idle.  - some simplifications allowed by the above.
    kettenis@ ok (except one bugfix that was not in the intial diff)

--- sys/sys/sched.h
+++ sys/sys/sched.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sched.h,v 1.20 2009/03/23 13:25:11 art Exp $	*/
+/*	$OpenBSD: sched.h,v 1.21 2009/04/03 09:29:15 art Exp $	*/
 /* $NetBSD: sched.h,v 1.2 1999/02/28 18:14:58 ross Exp $ */
 
 /*-
@@ -140,7 +140,8 @@ void sched_exit(struct proc *);
 void mi_switch(void);
 void cpu_switchto(struct proc *, struct proc *);
 struct proc *sched_chooseproc(void);
-void sched_choosecpu(struct proc *);
+struct cpu_info *sched_choosecpu(struct proc *);
+struct cpu_info *sched_choosecpu_fork(struct proc *parent, int);
 void cpu_idle_enter(void);
 void cpu_idle_cycle(void);
 void cpu_idle_leave(void);
