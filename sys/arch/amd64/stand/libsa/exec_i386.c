8a39e3e72b8b3afe305f236e53ef11fb4563ef9e
dlg <dlg>
2009-04-30 01:16:56 +0000

    pass db_console from the bootloader to the kernel, and then use it to set ddb.console real early.
    requested by art@
    no problem! deraadt@

--- sys/arch/amd64/stand/libsa/exec_i386.c
+++ sys/arch/amd64/stand/libsa/exec_i386.c
@@ -54,6 +54,8 @@ run_loadfile(u_long *marks, int howto)
 	caddr_t av = (caddr_t)BOOTARG_OFF;
 	bios_consdev_t cd;
 	extern int com_speed; /* from bioscons.c */
+	bios_ddb_t ddb;
+	extern int db_console;
 
 	if (sa_cleanup != NULL)
 		(*sa_cleanup)();
@@ -64,6 +66,11 @@ run_loadfile(u_long *marks, int howto)
 
 	if (bootmac != NULL)
 		addbootarg(BOOTARG_BOOTMAC, sizeof(bios_bootmac_t), bootmac);
+
+	if (db_console != -1) {
+		ddb.db_console = db_console;
+		addbootarg(BOOTARG_DDB, sizeof(ddb), &ddb);
+	}
 
 	/* Pass memory map to the kernel */
 	mem_pass();
