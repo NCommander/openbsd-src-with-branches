5f7d3953d9d6baa87e4891c5a81f4ab80fd5f19e
otto <otto>
2009-05-05 12:24:19 +0000

    use logical or when it's intended to be a logical or; ok krw@

--- sbin/disklabel/disklabel.c
+++ sbin/disklabel/disklabel.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: disklabel.c,v 1.151 2009/04/12 01:01:24 krw Exp $	*/
+/*	$OpenBSD: disklabel.c,v 1.152 2009/04/30 00:45:00 krw Exp $	*/
 
 /*
  * Copyright (c) 1987, 1993
@@ -39,7 +39,7 @@ static const char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static const char rcsid[] = "$OpenBSD: disklabel.c,v 1.151 2009/04/12 01:01:24 krw Exp $";
+static const char rcsid[] = "$OpenBSD: disklabel.c,v 1.152 2009/04/30 00:45:00 krw Exp $";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -235,7 +235,7 @@ main(int argc, char *argv[])
 		op = READ;
 #endif
 
-	if (argc < 1 || (fstabfile && !(op == EDITOR | aflag)))
+	if (argc < 1 || (fstabfile && !(op == EDITOR || aflag)))
 		usage();
 
 	dkname = argv[0];
