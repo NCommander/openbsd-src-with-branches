0ac6c84d9cf75eaf512c5e6f156f98f551be80fa
deraadt <deraadt>
2009-05-18 20:20:01 +0000

    make _dl_wrstderr() not call write per-character; ok drahn

--- libexec/ld.so/util.h
+++ libexec/ld.so/util.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: util.h,v 1.19 2004/10/17 03:56:49 drahn Exp $	*/
+/*	$OpenBSD: util.h,v 1.20 2007/02/09 14:51:13 drahn Exp $	*/
 
 /*
  * Copyright (c) 1998 Todd C. Miller <Todd.Miller@courtesan.com>
@@ -54,10 +54,12 @@ long _dl_strtol(const char *nptr, char **endptr, int base);
 static inline void
 _dl_wrstderr(const char *s)
 {
-	while (*s) {
-		_dl_write(2, s, 1);
-		s++;
-	}
+	const char *p = s;
+	size_t n = 0;
+
+	while (*p++)
+		n++;
+	_dl_write(2, s, n);
 }
 
 static inline void *
