5c5dcf49b5b38cd3dd938f3d0444c74b3c6e5d24
sobrado <sobrado>
2009-06-02 16:53:20 +0000

    remove unused flag; while here, make usage's output fit on 80-column displays.

--- usr.bin/tmux/tmux.c
+++ usr.bin/tmux/tmux.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: tmux.c,v 1.2 2009/06/02 10:49:40 ray Exp $ */
+/* $OpenBSD: tmux.c,v 1.3 2009/06/02 15:55:32 pyr Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -60,8 +60,9 @@ char 		*makesockpath(const char *);
 __dead void
 usage(void)
 {
-	fprintf(stderr, "usage: %s [-28dqUuVv] [-f file] "
-	    "[-L socket-name] [-S socket-path] [command [flags]]\n",
+	fprintf(stderr,
+	    "usage: %s [-28dqUuv] [-f file] [-L socket-name] [-S socket-path]\n"
+	    "            [command [flags]]\n",
 	    __progname);
 	exit(1);
 }
