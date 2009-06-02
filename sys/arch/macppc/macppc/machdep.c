25b9b8b78a96c6795f56214be5ee3415349ae187
drahn <drahn>
2009-06-02 21:38:10 +0000

    Reintroduce the macppc interrupt subsystem rewrite. Several bugs have been found and corrected.

--- sys/arch/macppc/macppc/machdep.c
+++ sys/arch/macppc/macppc/machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: machdep.c,v 1.103 2008/11/22 14:42:29 art Exp $	*/
+/*	$OpenBSD: machdep.c,v 1.101 2008/09/18 03:56:25 drahn Exp $	*/
 /*	$NetBSD: machdep.c,v 1.4 1996/10/16 19:33:11 ws Exp $	*/
 
 /*
@@ -876,8 +876,6 @@ dumpsys()
 	delay(5000000);         /* 5 seconds */
 
 }
-
-int imask[IPL_NUM];
 
 /*
  * this is a hack interface to allow zs to work better until
