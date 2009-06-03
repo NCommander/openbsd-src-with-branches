7e2819d6f3d612caf4c360213760271aca828c12
art <art>
2009-06-03 00:49:12 +0000

    Just like on amd64. Instead of keeping a bitmap of which cpus a pmap is active on, save a curpmap pointer in cpu_info. This lets us simplify a few things and do lazy context switching from a user process to a kernel thread. There's a new IPI introduced for forcing a cr3 reload when we're tearing down a dead pmap.
    kettenis@ ok (after I polished a few minor things)

--- sys/arch/i386/include/i82489var.h
+++ sys/arch/i386/include/i82489var.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: i82489var.h,v 1.8 2008/04/26 14:33:27 kettenis Exp $	*/
+/*	$OpenBSD: i82489var.h,v 1.9 2008/06/26 05:42:10 ray Exp $	*/
 /*	$NetBSD: i82489var.h,v 1.1.2.2 2000/02/21 18:46:14 sommerfeld Exp $	*/
 
 /*-
@@ -103,10 +103,12 @@ extern void Xintrltimer(void);
 #define LAPIC_IPI_INVLTLB		(LAPIC_IPI_OFFSET + 0)
 #define LAPIC_IPI_INVLPG		(LAPIC_IPI_OFFSET + 1)
 #define LAPIC_IPI_INVLRANGE		(LAPIC_IPI_OFFSET + 2)
+#define LAPIC_IPI_RELOADCR3		(LAPIC_IPI_OFFSET + 3)
 
 extern void Xintripi_invltlb(void);
 extern void Xintripi_invlpg(void);
 extern void Xintripi_invlrange(void);
+extern void Xintripi_reloadcr3(void);
 
 extern void Xintrsoftclock(void);
 extern void Xintrsoftnet(void);
