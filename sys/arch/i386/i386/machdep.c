eaefb539cbc543873feb3aa37786be242c0ea0d9
weingart <weingart>
2009-06-03 00:41:48 +0000

    Nuke CYRIX_CACHE (and related defines).  They've not been used in a long time.  Beginning of refactoring cpu identification.
    ok kettenis@

--- sys/arch/i386/i386/machdep.c
+++ sys/arch/i386/i386/machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: machdep.c,v 1.449 2009/05/19 01:31:15 weingart Exp $	*/
+/*	$OpenBSD: machdep.c,v 1.450 2009/06/01 20:11:38 weingart Exp $	*/
 /*	$NetBSD: machdep.c,v 1.214 1996/11/10 03:16:17 thorpej Exp $	*/
 
 /*-
@@ -1841,17 +1841,8 @@ identifycpu(struct cpu_info *ci)
 
 	ci->cpu_class = class;
 
-	if (cpu == CPU_486DLC) {
-#ifndef CYRIX_CACHE_WORKS
+	if (cpu == CPU_486DLC)
 		printf("WARNING: CYRIX 486DLC CACHE UNCHANGED.\n");
-#else
-#ifndef CYRIX_CACHE_REALLY_WORKS
-		printf("WARNING: CYRIX 486DLC CACHE ENABLED IN HOLD-FLUSH MODE.\n");
-#else
-		printf("WARNING: CYRIX 486DLC CACHE ENABLED.\n");
-#endif
-#endif
-	}
 
 	/*
 	 * Enable ring 0 write protection (486 or above, but 386
