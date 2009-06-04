f5041aaa3237caee24d354cacb3dd8bdb57fb079
jsg <jsg>
2009-06-04 22:27:31 +0000

    switch to a bnx style dynamic pool backed tx so we don't have to allocate everything up front.
    Requested by reyk@, 'I'm fine with it' dlg@, 'commit it' deraadt@

--- sys/dev/pci/ixgbe.h
+++ sys/dev/pci/ixgbe.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ixgbe.h,v 1.2 2008/06/08 20:33:51 reyk Exp $	*/
+/*	$OpenBSD: ixgbe.h,v 1.3 2008/06/08 21:15:34 reyk Exp $	*/
 
 /******************************************************************************
 
@@ -49,6 +49,8 @@
 #include <sys/device.h>
 #include <sys/socket.h>
 #include <sys/timeout.h>
+#include <sys/pool.h>
+#include <sys/rwlock.h>
 #include <sys/workq.h>
 
 #include <net/if.h>
