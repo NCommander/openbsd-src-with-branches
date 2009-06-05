5629beeded09765dbccdafbf03a89d40a3f30739
krw <krw>
2009-06-05 04:35:25 +0000

    Update to Reinoud Zandijk's much more current version of ecma167-udf.h and make textual tweaks to .c files to keep them compiling. From NetBSD.
    No changes to .o's at this point.

--- sys/isofs/udf/udf_vnops.c
+++ sys/isofs/udf/udf_vnops.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: udf_vnops.c,v 1.33 2008/06/14 10:55:21 mk Exp $	*/
+/*	$OpenBSD: udf_vnops.c,v 1.34 2008/11/08 01:27:51 chl Exp $	*/
 
 /*
  * Copyright (c) 2001, 2002 Scott Long <scottl@freebsd.org>
@@ -1260,7 +1260,7 @@ udf_bmap_internal(struct unode *up, off_t offset, daddr64_t *sector,
 		} while(offset >= icblen);
 
 		lsector = (offset  >> ump->um_bshift) +
-		    letoh32(((struct short_ad *)(icb))->pos);
+		    letoh32(((struct short_ad *)(icb))->lb_num);
 
 		*max_size = GETICBLEN(short_ad, icb);
 
