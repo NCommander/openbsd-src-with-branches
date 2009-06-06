aa8ba2719747341af8ef142257905fc7e265649d
pyr <pyr>
2009-06-06 05:04:12 +0000

    remove unnecessary and scary pid check in imsg_init. ok eric@, claudio@

--- usr.sbin/relayd/imsg.c
+++ usr.sbin/relayd/imsg.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: imsg.c,v 1.22 2009/06/05 00:04:01 pyr Exp $	*/
+/*	$OpenBSD: imsg.c,v 1.23 2009/06/05 23:39:51 pyr Exp $	*/
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -31,14 +31,12 @@
 void
 imsg_init(struct imsgbuf *ibuf, int fd)
 {
-	if (!ibuf->pid) {
-		msgbuf_init(&ibuf->w);
-		bzero(&ibuf->r, sizeof(ibuf->r));
-		ibuf->pid = getpid();
-		TAILQ_INIT(&ibuf->fds);
-	}
+	msgbuf_init(&ibuf->w);
+	bzero(&ibuf->r, sizeof(ibuf->r));
 	ibuf->fd = fd;
 	ibuf->w.fd = fd;
+	ibuf->pid = getpid();
+	TAILQ_INIT(&ibuf->fds);
 }
 
 ssize_t
