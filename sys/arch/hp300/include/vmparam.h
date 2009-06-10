d3f90aaf50074fef35479917f31b4d3918c5bd23
miod <miod>
2009-06-10 15:58:51 +0000

    Allow /dev/kmem reads in the direct mapping region where there is corresponding physical memory; makes procmap(8) happy on hp300.

--- sys/arch/hp300/include/vmparam.h
+++ sys/arch/hp300/include/vmparam.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vmparam.h,v 1.16 2006/06/04 17:20:38 miod Exp $	*/
+/*	$OpenBSD: vmparam.h,v 1.17 2006/07/09 19:41:23 miod Exp $	*/
 /*	$NetBSD: vmparam.h,v 1.16 1998/08/20 08:33:48 kleink Exp $	*/
 
 /*
@@ -46,10 +46,13 @@
  * Machine dependent constants for HP300
  */
 
+/* address where direct translation starts */
+#define	HP300_DIRECT_BASE	0xc0000000
+
 #include <m68k/vmparam.h>
 
 #undef	VM_MAX_KERNEL_ADDRESS
-#define	VM_MAX_KERNEL_ADDRESS	(0xc0000000 - PAGE_SIZE)
+#define	VM_MAX_KERNEL_ADDRESS	(HP300_DIRECT_BASE - PAGE_SIZE)
 
 /*
  * Constants which control the way the VM system deals with memory segments.
