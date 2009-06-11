b5ff314b2d5cb71144519917340ced6937663657
chl <chl>
2009-06-11 21:09:46 +0000

    remove unused variables
    ok millert@

--- usr.bin/fstat/fstat.c
+++ usr.bin/fstat/fstat.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fstat.c,v 1.64 2009/06/03 14:45:55 jj Exp $	*/
+/*	$OpenBSD: fstat.c,v 1.65 2009/06/07 03:10:09 millert Exp $	*/
 
 /*
  * Copyright (c) 2009 Todd C. Miller <Todd.Miller@courtesan.com>
@@ -53,7 +53,7 @@ static char copyright[] =
 
 #ifndef lint
 /*static char sccsid[] = "from: @(#)fstat.c	8.1 (Berkeley) 6/6/93";*/
-static char *rcsid = "$OpenBSD: fstat.c,v 1.64 2009/06/03 14:45:55 jj Exp $";
+static char *rcsid = "$OpenBSD: fstat.c,v 1.65 2009/06/07 03:10:09 millert Exp $";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -498,8 +498,7 @@ socktrans(struct kinfo_file2 *kf)
 		"seqpak"	/* 5 */
 	};
 #define	STYPEMAX 5
-	int len;
-	char *dname, *stype, stypebuf[24];
+	char *stype, stypebuf[24];
 	struct in_addr laddr, faddr;
 #ifdef INET6
 	char xaddrbuf[NI_MAXHOST + 2];
