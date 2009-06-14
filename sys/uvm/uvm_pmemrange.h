80e2450ffe596321cb1a0914a650d6c75d310e21
deraadt <deraadt>
2009-06-14 02:20:23 +0000

    back out the free vs dirty fix, because pmemrange will go soon too

--- sys/uvm/uvm_pmemrange.h
+++ sys/uvm/uvm_pmemrange.h
@@ -34,7 +34,7 @@ RB_HEAD(uvm_pmr_size, vm_page);
  * - DIRTY: this page may contain random data.
  * - ZERO: this page has been zeroed.
  */
-#define UVM_PMR_MEMTYPE_DIRTY	0
+#define UVM_PMR_MEMTYPE_DIRTY	1
 #define UVM_PMR_MEMTYPE_ZERO	1
 #define UVM_PMR_MEMTYPE_MAX	2
 
