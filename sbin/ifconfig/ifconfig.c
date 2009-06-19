af5c2998e6da0603b09c9336fc549530f6616bb0
henning <henning>
2009-06-19 14:05:32 +0000

    after long discussion with many... ifconfig <if> inet6 used to print all inet6 addresses, and last not least the installer relies on that behaviour. so don't. to turn inet6 on again you have to assign any inet6 address or run rtsol. nobody happy about this asymmetry, but that is the best we could come up with for now.

--- sbin/ifconfig/ifconfig.c
+++ sbin/ifconfig/ifconfig.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ifconfig.c,v 1.218 2009/06/11 20:15:28 chl Exp $	*/
+/*	$OpenBSD: ifconfig.c,v 1.219 2009/06/14 00:16:50 dlg Exp $	*/
 /*	$NetBSD: ifconfig.c,v 1.40 1997/10/01 02:19:43 enami Exp $	*/
 
 /*
@@ -599,9 +599,7 @@ main(int argc, char *argv[])
 		(void)getinfo(&ifr, create);
 	}
 #ifdef INET6
-	if (argc == 0 && af == AF_INET6)
-		noprint = 1;	/* handles "ifconfig <if> inet6" */
-	if (af == AF_INET6)
+	if (argc != 0 && af == AF_INET6)
 		setifxflags("inet6", -IFXF_NOINET6);
 #endif
 	while (argc > 0) {
