b28dac21d64994a6e860728436f0625a2d344688
jsg <jsg>
2009-07-14 16:37:31 +0000

    Don't specify mode(SI) attribute for __EH_FRAME_END__ as we have 32 bit ints on OpenBSD.
    ok kettenis@

--- lib/csu/common_elf/crtend.c
+++ lib/csu/common_elf/crtend.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: crtend.c,v 1.6 2004/10/10 18:29:15 kettenis Exp $	*/
+/*	$OpenBSD: crtend.c,v 1.7 2009/04/13 20:15:24 kurt Exp $	*/
 /*	$NetBSD: crtend.c,v 1.1 1996/09/12 16:59:04 cgd Exp $	*/
 
 #include <sys/cdefs.h>
@@ -11,7 +11,7 @@ static init_f __DTOR_LIST__[1]
     __attribute__((section(".dtors"))) = { (void *)0 };		/* XXX */
 
 static const int __EH_FRAME_END__[]
-__attribute__((unused, mode(SI), section(".eh_frame"), aligned(4))) = { 0 };
+__attribute__((unused, section(".eh_frame"), aligned(4))) = { 0 };
 
 #if (__GNUC__ > 2)
 static void * __JCR_END__[]
