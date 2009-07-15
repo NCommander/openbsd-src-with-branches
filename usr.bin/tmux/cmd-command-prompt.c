7473bffbbfd5467b94c0b5a936d8e2ec711c88c3
nicm <nicm>
2009-07-15 17:39:00 +0000

    Make status_message_set a variadic printf-like function. No functional change - helpful for a couple of things coming soon.

--- usr.bin/tmux/cmd-command-prompt.c
+++ usr.bin/tmux/cmd-command-prompt.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: cmd-command-prompt.c,v 1.1 2009/06/01 22:58:49 nicm Exp $ */
+/* $OpenBSD: cmd-command-prompt.c,v 1.2 2009/07/13 23:11:35 nicm Exp $ */
 
 /*
  * Copyright (c) 2008 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -150,7 +150,7 @@ cmd_command_prompt_callback(void *data, const char *s)
 		if (cause == NULL)
 			return (0);
 		*cause = toupper((u_char) *cause);
-		status_message_set(c, cause);
+		status_message_set(c, "%s", cause);
 		xfree(cause);
 		cmdlist = NULL;
 	}
