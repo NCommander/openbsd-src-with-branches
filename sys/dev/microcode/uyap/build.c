e74260cc348fa80470c6ee8b493aa177e782a173
jsg <jsg>
2009-07-18 09:58:06 +0000

    #include stdio.h for printf here also.

--- sys/dev/microcode/uyap/build.c
+++ sys/dev/microcode/uyap/build.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: build.c,v 1.2 2005/04/14 19:01:08 damien Exp $	*/
+/*	$OpenBSD: build.c,v 1.3 2005/05/17 18:48:52 jason Exp $	*/
 
 /*
  * Copyright (c) 2004 Theo de Raadt <deraadt@openbsd.org>
@@ -19,6 +19,7 @@
 #include <sys/uio.h>
 #include <fcntl.h>
 #include <sys/param.h>
+#include <stdio.h>
 
 #include <dev/usb/ezload.h>
 #include "uyap_firmware.h"
