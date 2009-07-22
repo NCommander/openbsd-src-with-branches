6b8f3214d3e379b5e155665ee4b93c98543b37c0
miod <miod>
2009-07-22 21:15:42 +0000

    When disassembling, if our pc matches the beginning of a routine, then it's at routine+0; do not search for the previous routine in the symbol table. This fixes tracebacks when the fault occurs on the first instruction of a routine.

--- sys/arch/mips64/mips64/db_machdep.c
+++ sys/arch/mips64/mips64/db_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: db_machdep.c,v 1.16 2008/02/29 19:00:39 miod Exp $ */
+/*	$OpenBSD: db_machdep.c,v 1.17 2008/06/22 21:02:10 miod Exp $ */
 
 /*
  * Copyright (c) 1998-2003 Opsycon AB (www.opsycon.se)
@@ -284,8 +284,6 @@ loop:
 	 * Watch out for function tail optimizations.
 	 */
 	sym = db_search_symbol(pc, DB_STGY_ANY, &diff);
-	if (sym != DB_SYM_NULL && diff == 0)
-		sym = db_search_symbol(pc - 4, DB_STGY_ANY, &diff);
 	db_symbol_values(sym, &symname, 0);
 	if (sym != DB_SYM_NULL) {
 		subr = pc - diff;
