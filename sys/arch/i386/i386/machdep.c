3f9aeec926487aff3553978d7eb692a330efb9e6
dms <dms>
2009-07-27 11:28:55 +0000

    add bus speed recognition on Nehalem based cpus, this makes Enhanced SpeedStep work on new machines, but requires acpimadt0 to be enabled.
    ok by jsg@ and claudio@

--- sys/arch/i386/i386/machdep.c
+++ sys/arch/i386/i386/machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: machdep.c,v 1.452 2009/06/03 21:30:19 beck Exp $	*/
+/*	$OpenBSD: machdep.c,v 1.453 2009/06/15 17:01:26 beck Exp $	*/
 /*	$NetBSD: machdep.c,v 1.214 1996/11/10 03:16:17 thorpej Exp $	*/
 
 /*-
@@ -2021,6 +2021,9 @@ p3_get_bus_clock(struct cpu_info *ci)
 			goto print_msr;
 		}
 		break;
+       case 0x1a: /* Nehalem based Core i7 and Xeon */
+               bus_clock = BUS133;
+               break;
 	case 0x1c: /* Atom */
 		msr = rdmsr(MSR_FSB_FREQ);
 		bus = (msr >> 0) & 0x7;
