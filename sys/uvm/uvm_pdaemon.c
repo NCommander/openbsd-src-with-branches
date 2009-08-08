de11743a63c12f0f76aeb35b94960999fa9c273a
beck <beck>
2009-08-08 13:43:59 +0000

    fix the page daemon to back off the buffer cache correctly even in the case where we are below the inactive page target. This fixes a problem with a large buffer cache on low memory machines where the the page daemon would woken up, however the buffer cache would never be backed off because we were below the inactive page target, which could result in constant paging and basically a livelock condition.
    ok oga@ art@

--- sys/uvm/uvm_pdaemon.c
+++ sys/uvm/uvm_pdaemon.c
@@ -212,6 +212,7 @@ uvm_pageout(void *arg)
 	 */
 
 	for (;;) {
+		int scanned = 0;
 		uvm_lock_fpageq();
 		UVMHIST_LOG(pdhist,"  <<SLEEPING>>",0,0,0,0);
 		msleep(&uvm.pagedaemon, &uvm.fpageqlock, PVM | PNORELOCK,
@@ -241,12 +242,17 @@ uvm_pageout(void *arg)
 		/*
 		 * get pages from the buffer cache, or scan if needed
 		 */
-		if (uvmexp.inactive < uvmexp.inactarg)
-			uvmpd_scan();
-		else if ((uvmexp.free - BUFPAGES_DEFICIT) < uvmexp.freetarg) {
-			if (bufbackoff() == -1)
+		if ((uvmexp.free - BUFPAGES_DEFICIT) < uvmexp.freetarg) {
+			if (bufbackoff() == -1) {
 				uvmpd_scan();
+				scanned = 1;
+			} else
+				scanned = 0;
 		}
+
+		if (!scanned && (uvmexp.inactive < uvmexp.inactarg))
+			uvmpd_scan();
+
 
 		/*
 		 * if there's any free memory to be had,
