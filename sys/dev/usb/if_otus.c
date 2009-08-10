bca7464a7f1698999abbc55d66b602764df6cf1d
damien <damien>
2009-08-10 17:23:55 +0000

    undef OTUS_DEBUG. can't believe this made it into 4.6 :-(

--- sys/dev/usb/if_otus.c
+++ sys/dev/usb/if_otus.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_otus.c,v 1.9 2009/05/11 20:11:54 damien Exp $	*/
+/*	$OpenBSD: if_otus.c,v 1.10 2009/05/23 18:03:41 jsg Exp $	*/
 
 /*-
  * Copyright (c) 2009 Damien Bergamini <damien.bergamini@free.fr>
@@ -67,8 +67,6 @@
 #ifdef USB_DEBUG
 #define OTUS_DEBUG
 #endif
-
-#define OTUS_DEBUG
 
 #ifdef OTUS_DEBUG
 #define DPRINTF(x)	do { if (otus_debug) printf x; } while (0)
