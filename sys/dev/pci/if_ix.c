3ba2d88e475d0f1cb8edd1abfccd2ba63b973a43
dlg <dlg>
2009-08-12 14:39:05 +0000

    if we get dangerously low on clusters during interrupts, we need to free some for use on the rx rings on network cards.
    this modifies m_cluncount to advise callers when we're in such a
    situation, and makes them responsible for freeing up the cluster
    for allocation by MCLGETI later.
    
    fixes an awesome lockup with sis(4) henning has been experiencing.
    this is not the best fix, but it is better than the current situation.
    
    yep deraadt@ tested by henning@

--- sys/dev/pci/if_ix.c
+++ sys/dev/pci/if_ix.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ix.c,v 1.25 2009/08/09 11:40:56 deraadt Exp $	*/
+/*	$OpenBSD: if_ix.c,v 1.26 2009/08/10 19:41:05 deraadt Exp $	*/
 
 /******************************************************************************
 
@@ -2631,10 +2631,12 @@ ixgbe_rxeof(struct rx_ring *rxr, int count)
 			    rxr->last_rx_desc_filled);
 		}
 
-		m_cluncount(m, 1);
 		rxr->rx_ndescs--;
+		if (m_cluncount(m) == 0)
+			accept_frame = 1;
+		else
+			accept_frame = 0;
 
-		accept_frame = 1;
 		prev_len_adj = 0;
 		desc_len = letoh16(rxdesc->wb.upper.length);
 
