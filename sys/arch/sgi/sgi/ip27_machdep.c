ca4ed28acf5f15f6f62e09009d138f40191de48d
miod <miod>
2009-08-18 19:31:59 +0000

    Blind support for SGI PIC PCI-X controller found on Origin 350 and Tezro systems. PIC was supposed to be mostly XBridge compatible, but a silicon bug prevents it from working correctly if 32 bit register writes are used, so the xbridge(4) code now needs to issue 64 bit writes.
    In order to make this a bit more transparent, rebase all widget registers to
    a 8 byte boundary, and provide a few inline accessors which will do the right
    thing if you want 32 bit writes.
    
    Tested to not cause regressions on Octane, Origin 200 and Fuel; covering
    Bridge < 4, Bridge >= 4 and XBridge flavours.

--- sys/arch/sgi/sgi/ip27_machdep.c
+++ sys/arch/sgi/sgi/ip27_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip27_machdep.c,v 1.17 2009/07/11 19:56:04 miod Exp $	*/
+/*	$OpenBSD: ip27_machdep.c,v 1.18 2009/07/25 19:17:25 miod Exp $	*/
 
 /*
  * Copyright (c) 2008, 2009 Miodrag Vallat.
@@ -381,7 +381,7 @@ ip27_widget_id(int16_t nasid, u_int widget, uint32_t *wid)
 	}
 
 	wpa = ip27_widget_short(nasid, widget);
-	if (guarded_read_4(wpa + WIDGET_ID, &id) != 0)
+	if (guarded_read_4(wpa + (WIDGET_ID | 4), &id) != 0)
 		return ENXIO;
 
 	if (wid != NULL)
