e08c366b3182f7bff9b2f32ff92d44b82827e103
krw <krw>
2009-08-25 10:42:27 +0000

    Fix order of memcpy() arguments so data is copied from src to dest, not the other way. At least partial fix for interfaces with >64 aliases. From Max Laier via Marc Balmer.
    'seems right' sthen@ ok markus@

--- sys/net/pf_if.c
+++ sys/net/pf_if.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf_if.c,v 1.55 2008/11/24 13:22:09 mikeb Exp $ */
+/*	$OpenBSD: pf_if.c,v 1.56 2009/03/09 13:53:10 mcbride Exp $ */
 
 /*
  * Copyright 2005 Henning Brauer <henning@openbsd.org>
@@ -529,7 +529,7 @@ pfi_address_add(struct sockaddr *sa, int af, int net)
 			    "(%d/%d)\n", pfi_buffer_cnt, PFI_BUFFER_MAX);
 			return;
 		}
-		memcpy(pfi_buffer, p, pfi_buffer_cnt * sizeof(*pfi_buffer));
+		memcpy(p, pfi_buffer, pfi_buffer_cnt * sizeof(*pfi_buffer));
 		/* no need to zero buffer */
 		free(pfi_buffer, PFI_MTYPE);
 		pfi_buffer = p;
