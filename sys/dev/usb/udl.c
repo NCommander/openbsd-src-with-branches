70a96232ec59fb38e1ee0843445e6866ba39c6f0
mglocker <mglocker>
2009-08-26 19:30:46 +0000

    Remove obsolete comment.

--- sys/dev/usb/udl.c
+++ sys/dev/usb/udl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: udl.c,v 1.24 2009/08/26 12:23:39 mglocker Exp $ */
+/*	$OpenBSD: udl.c,v 1.25 2009/08/26 19:07:45 mglocker Exp $ */
 
 /*
  * Copyright (c) 2009 Marcus Glocker <mglocker@openbsd.org>
@@ -27,8 +27,7 @@
  * to deal with similar challenges, like no direct access to the video
  * memory.
  *
- * TODO: - Cleanup the endianess mess.
- *	 - Reduce padding overhead in compressed blocks.
+ * TODO: - Reduce padding overhead in compressed blocks.
  */
 
 #include <sys/param.h>
