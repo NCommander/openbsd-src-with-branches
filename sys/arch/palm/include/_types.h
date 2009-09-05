2c99b5ba80a9d8cf02e1c733debabb5513db85ee
marex <marex>
2009-09-05 01:22:11 +0000

    Palm: initial commit of sys/arch/palm
    OK deraadt@

---
+++ sys/arch/palm/include/_types.h
@@ -0,0 +1,11 @@
+/* 	$OpenBSD: _types.h,v 1.4 2008/01/03 17:59:32 kettenis Exp $	*/
+
+#ifndef _PALM__TYPES_H_
+#define	_PALM__TYPES_H_
+
+#include <arm/_types.h>
+
+#define __HAVE_GENERIC_SOFT_INTERRUPTS
+#define __HAVE_TIMECOUNTER
+
+#endif
---
+++ sys/arch/palm/include/atomic.h
@@ -0,0 +1,10 @@
+/*	$OpenBSD: atomic.h,v 1.4 2007/02/19 17:18:43 deraadt Exp $	*/
+
+/* Public Domain */
+
+#ifndef __PALM_ATOMIC_H__
+#define __PALM_ATOMIC_H__
+
+#include <arm/atomic.h>
+
+#endif /* __PALM_ATOMIC_H__ */
