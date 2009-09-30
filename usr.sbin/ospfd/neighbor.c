c0ede1dd6eb989360f4d1c955c9c4c26d125bec3
claudio <claudio>
2009-09-30 14:37:11 +0000

    Announce a stub network LSA for backup carp interfaces. This should help when fail-over happens, since removing the better route will not result in a blackhole until the update from the new master is processed. Tested, OK and input sthen@, phessler@

--- usr.sbin/ospfd/neighbor.c
+++ usr.sbin/ospfd/neighbor.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: neighbor.c,v 1.36 2007/10/11 08:21:29 claudio Exp $ */
+/*	$OpenBSD: neighbor.c,v 1.37 2008/02/11 12:37:37 norby Exp $ */
 
 /*
  * Copyright (c) 2005 Claudio Jeker <claudio@openbsd.org>
@@ -211,6 +211,11 @@ nbr_fsm(struct nbr *nbr, enum nbr_event event)
 
 			gettimeofday(&now, NULL);
 			nbr->uptime = now.tv_sec;
+
+			/* demote P2P links if the neighbor resets */
+			if (nbr->iface->type == IF_TYPE_POINTOPOINT)
+				ospfe_demote_iface(nbr->iface,
+				    !(old_state & NBR_STA_FULL));
 		}
 
 		/* bidirectional communication lost */
