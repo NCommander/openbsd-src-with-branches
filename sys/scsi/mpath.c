480b12c5635387e90985ef042595c14bb2af4b41
dlg <dlg>
2009-10-23 01:02:29 +0000

    if you're attempting to detach multiple devices (eg, many targets, many luns, or the entire bus), dont report ENXIO as an error to the caller. this broke autoconf when it tried to forcefully remove a bus such as umass and it thought there was a failure.
    this introduces a way for scsi hbas to call activate/deactivate on
    a device based on its target/lun address via a call to scsi_activate().
    they can then schedule the actual detach/attach in a thread later via
    scsi_req_probe/detach.
    
    the mpi changes tweak the sas event handling code to use these apis
    to properly handle attaches and detaches of disks. event handling
    is still disabled till i can make it less chatty.
    
    umass breakage reported by form@

--- sys/scsi/mpath.c
+++ sys/scsi/mpath.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mpath.c,v 1.5 2009/10/14 01:33:22 dlg Exp $ */
+/*	$OpenBSD: mpath.c,v 1.6 2009/10/22 11:56:32 dlg Exp $ */
 
 /*
  * Copyright (c) 2009 David Gwynne <dlg@openbsd.org>
@@ -308,15 +308,15 @@ mpath_path_detach(struct scsi_link *link, int flags)
 	panic("mpath: unable to locate path for detach");
 }
 
-int
+void
 mpath_path_activate(struct scsi_link *link)
 {
-	return (0);
+
 }
 
-int
+void
 mpath_path_deactivate(struct scsi_link *link)
 {
-	return (0);
+
 }
 
