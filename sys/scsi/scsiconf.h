bae0a396455f9b79f816e9b37bb442bb9f10cf73
dlg <dlg>
2009-11-10 10:18:59 +0000

    fix the NO_CCB handling i broke when working on simplifying the midlayer.
    marco@ discovered my rewrite retried commands rejected by an adapter
    from a timeout, which was trivially starved by normal io going to
    disks. this diff allows an xs to be marked as XS_NO_CCB, which will
    cause it to be returned to the device driver to be retried as part
    of the normal io queue.
    
    tested by krw@ marco@ johan@
    ok krw@ marco@

--- sys/scsi/scsiconf.h
+++ sys/scsi/scsiconf.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: scsiconf.h,v 1.104 2009/10/22 11:56:32 dlg Exp $	*/
+/*	$OpenBSD: scsiconf.h,v 1.105 2009/10/23 01:02:29 dlg Exp $	*/
 /*	$NetBSD: scsiconf.h,v 1.35 1997/04/02 02:29:38 mycroft Exp $	*/
 
 /*
@@ -500,6 +500,7 @@ struct scsi_xfer {
 #define XS_BUSY		5	/* The device busy, try again later?	  */
 #define XS_SHORTSENSE   6	/* Check the ATAPI sense for the error */
 #define XS_RESET	8	/* bus was reset; possible retry command  */
+#define XS_NO_CCB	9	/* device should requeue io and retry */
 
 /*
  * Possible retries for scsi_test_unit_ready()
