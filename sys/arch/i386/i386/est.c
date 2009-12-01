d04eec205523bc53d5770b57aaf8db90e1383a1c
jsg <jsg>
2009-12-01 18:31:36 +0000

    The MD ACPI PSS paths are using MSRs they shouldn't be so don't try to do EST on machines we don't know the bus_clock on for now.
    ok deraadt@

--- sys/arch/i386/i386/est.c
+++ sys/arch/i386/i386/est.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: est.c,v 1.31 2009/06/06 23:21:43 gwk Exp $ */
+/*	$OpenBSD: est.c,v 1.32 2009/06/08 23:18:05 gwk Exp $ */
 /*
  * Copyright (c) 2003 Michael Eriksson.
  * All rights reserved.
@@ -1060,6 +1060,12 @@ est_init(const char *cpu_device, int vendor)
 
 	if ((cpu_ecxfeature & CPUIDECX_EST) == 0)
 		return;
+
+	if (bus_clock == 0) {
+		printf("%s: EST: PSS not yet available for this processor\n",
+		    cpu_device);
+		return;
+	}
 
 	msr = rdmsr(MSR_PERF_STATUS);
 	idhi = (msr >> 32) & 0xffff;
