69ba9937b8035e8b231b24cb016b0d17d23b92ba
marco <marco>
2009-12-01 04:29:21 +0000

    Fix dmesg printing of error.

--- sys/dev/acpi/acpiutil.c
+++ sys/dev/acpi/acpiutil.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: acpiutil.c,v 1.3 2006/12/19 23:01:47 deraadt Exp $ */
+/* $OpenBSD: acpiutil.c,v 1.4 2008/06/11 04:42:09 marco Exp $ */
 /*
  * Copyright (c) 2005 Thorsten Lockert <tholo@sigmasoft.com>
  *
@@ -35,6 +35,7 @@ acpi_checksum(const void *v, size_t len)
 		s += p[i];
 
 	if (s)
-		printf("acpi: bad checksum at %p\n", v);
+		printf("\nacpi0: bad checksum at %p", v);
+
 	return (s);
 }
