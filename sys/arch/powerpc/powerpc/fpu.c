63a3ab83bb1717293b31ad0f54ed6be12d7ed49f
deraadt <deraadt>
2009-12-10 16:46:09 +0000

    ansi

--- sys/arch/powerpc/powerpc/fpu.c
+++ sys/arch/powerpc/powerpc/fpu.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fpu.c,v 1.10 2007/03/20 20:59:53 kettenis Exp $	*/
+/*	$OpenBSD: fpu.c,v 1.11 2008/04/27 16:01:47 drahn Exp $	*/
 /*	$NetBSD: fpu.c,v 1.1 1996/09/30 16:34:44 ws Exp $	*/
 
 /*
@@ -100,7 +100,7 @@ enable_fpu(struct proc *p)
 }
 
 void
-save_fpu()
+save_fpu(void)
 {
 	struct cpu_info *ci = curcpu();
 	struct pcb *pcb;
