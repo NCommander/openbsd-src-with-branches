6995f95436f238a8392e508326d4f0894703320c
nicm <nicm>
2009-12-14 10:43:41 +0000

    New server option, escape-time, to set the timeout used to detect if escapes are alone or part of a function key or meta sequence.

--- usr.bin/tmux/tmux.c
+++ usr.bin/tmux/tmux.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: tmux.c,v 1.63 2009/12/10 09:16:52 nicm Exp $ */
+/* $OpenBSD: tmux.c,v 1.64 2009/12/11 13:58:48 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -317,6 +317,7 @@ main(int argc, char **argv)
 	options_init(&global_options, NULL);
 	oo = &global_options;
 	options_set_number(oo, "quiet", quiet);
+	options_set_number(oo, "escape-time", 500);
 
 	options_init(&global_s_options, NULL);
 	so = &global_s_options;
