bb436655f1e869e7790f165836687693503635ee
jakemsr <jakemsr>
2009-12-15 22:14:28 +0000

    don't make mixer items for vendor widgets.  we don't know what they do.

--- sys/dev/pci/azalia.c
+++ sys/dev/pci/azalia.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: azalia.c,v 1.162 2009/11/24 10:00:39 jakemsr Exp $	*/
+/*	$OpenBSD: azalia.c,v 1.163 2009/12/15 21:22:04 jakemsr Exp $	*/
 /*	$NetBSD: azalia.c,v 1.20 2006/05/07 08:31:44 kent Exp $	*/
 
 /*-
@@ -2912,6 +2912,8 @@ azalia_widget_init(widget_t *this, const codec_t *codec, nid_t nid)
 		this->d.volume.cap = result;
 		break;
 	case COP_AWTYPE_POWER:
+		/* FALLTHROUGH */
+	case COP_AWTYPE_VENDOR_DEFINED:
 		this->enable = 0;
 		break;
 	}
