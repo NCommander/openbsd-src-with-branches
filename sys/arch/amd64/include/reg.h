660bcd4f53452a2b6d531c6a00d148317d856191
miod <miod>
2009-12-25 20:54:48 +0000

    Do not #include unused <machine/mcontext.h>

--- sys/arch/amd64/include/reg.h
+++ sys/arch/amd64/include/reg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: reg.h,v 1.3 2004/01/29 13:21:10 mickey Exp $	*/
+/*	$OpenBSD: reg.h,v 1.4 2005/12/13 00:18:19 jsg Exp $	*/
 /*	$NetBSD: reg.h,v 1.1 2003/04/26 18:39:47 fvdl Exp $	*/
 
 /*-
@@ -38,7 +38,6 @@
 #ifndef _AMD64_REG_H_
 #define _AMD64_REG_H_
 
-#include <machine/mcontext.h>
 #include <machine/fpu.h>
 
 /*
