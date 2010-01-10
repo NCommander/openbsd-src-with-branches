1a7b2e13fc9111953967576ad9d03d5e0e40bb15
espie <espie>
2010-01-10 11:32:41 +0000

    bump copyright

--- usr.sbin/pkg_add/OpenBSD/PackageRepository/Installed.pm
+++ usr.sbin/pkg_add/OpenBSD/PackageRepository/Installed.pm
@@ -1,7 +1,7 @@
 # ex:ts=8 sw=4:
 # $OpenBSD$
 #
-# Copyright (c) 2007 Marc Espie <espie@openbsd.org>
+# Copyright (c) 2007-2010 Marc Espie <espie@openbsd.org>
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
