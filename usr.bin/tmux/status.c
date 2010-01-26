c16f0c54fcdbe434d2d612b6289f45720fbd5bc0
nicm <nicm>
2010-01-26 21:36:53 +0000

    Actually use the copy made when no newline is found, from martynas@.

--- usr.bin/tmux/status.c
+++ usr.bin/tmux/status.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: status.c,v 1.55 2009/12/03 22:50:10 nicm Exp $ */
+/* $OpenBSD: status.c,v 1.56 2009/12/14 10:47:11 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -550,10 +550,11 @@ status_job_callback(struct job *job)
 		xfree(job->data);
 	else
 		server_redraw_client(job->client);
-	job->data = xstrdup(line);
 
-	if (buf != NULL)
-		xfree(buf);
+	if (line == NULL)
+		job->data = buf;
+	else
+		job->data = xstrdup(line);
 }
 
 /* Calculate winlink status line entry width. */
