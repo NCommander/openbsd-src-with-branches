850b9e1233698ff9b37581ee92f76221c967f493
jsg <jsg>
2010-02-18 14:02:16 +0000

    Do an event_del before event_set/event_add in case we double add something here as well.
    ok claudio@ phessler@

--- usr.sbin/relayd/check_tcp.c
+++ usr.sbin/relayd/check_tcp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: check_tcp.c,v 1.34 2009/06/04 14:03:22 reyk Exp $	*/
+/*	$OpenBSD: check_tcp.c,v 1.35 2009/08/07 11:10:23 reyk Exp $	*/
 
 /*
  * Copyright (c) 2006 Pierre-Yves Ritschard <pyr@openbsd.org>
@@ -99,6 +99,7 @@ check_tcp(struct ctl_tcp_event *cte)
 
 	cte->buf = NULL;
 	cte->host->up = HOST_UP;
+	event_del(&cte->ev);
 	event_set(&cte->ev, s, EV_TIMEOUT|EV_WRITE, tcp_write, cte);
 	event_add(&cte->ev, &tv);
 	return;
