f9193fff7eba9d5f613b21fb8944ecb9fb95011f
jmc <jmc>
2010-02-24 14:09:45 +0000

    match code and doc; ok claudio

--- usr.sbin/snmpd/ber.c
+++ usr.sbin/snmpd/ber.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ber.c,v 1.18 2009/11/12 15:28:32 deraadt Exp $ */
+/*	$OpenBSD: ber.c,v 1.19 2009/12/16 22:17:53 deraadt Exp $ */
 
 /*
  * Copyright (c) 2007 Reyk Floeter <reyk@vantronix.net>
@@ -738,10 +738,7 @@ ber_scanf_elements(struct ber_element *ber, char *fmt, ...)
  *	root	fully populated element tree
  *
  * returns:
- *	0	on success
- *
- * returns:
- *	0	on success
+ *      >=0     number of bytes written
  *	-1	on failure and sets errno
  */
 int
