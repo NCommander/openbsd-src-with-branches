91231e572584e9094fa7bdfd248ed0ac14a4891b
miod <miod>
2010-03-05 10:59:35 +0000

    head towards release, correctly. tsk tsk tsk.

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.101 2010/01/26 23:04:28 miod Exp $
+#	$OpenBSD: newvers.sh,v 1.102 2010/03/05 08:54:01 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -65,11 +65,11 @@ ost="OpenBSD"
 osr="4.7"
 
 cat >vers.c <<eof
-#if 0
 #define STATUS ""			/* release */
-#endif
+#if 0
 #define STATUS "-current"		/* just after a release */
 #define STATUS "-beta"			/* just before a release */
+#endif
 
 const char ostype[] = "${ost}";
 const char osrelease[] = "${osr}";
