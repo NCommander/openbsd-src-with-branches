bb5c213ee969e4ccbe1838b0125c909b52b17589
henning <henning>
2010-03-08 20:52:52 +0000

    aye, broadcast addr too. spotted by kettenis

--- sys/net/if.c
+++ sys/net/if.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if.c,v 1.208 2010/03/05 13:52:23 henning Exp $	*/
+/*	$OpenBSD: if.c,v 1.209 2010/03/08 20:29:41 henning Exp $	*/
 /*	$NetBSD: if.c,v 1.35 1996/05/07 05:26:04 thorpej Exp $	*/
 
 /*
@@ -2169,8 +2169,6 @@ ifa_add(struct ifnet *ifp, struct ifaddr *ifa)
 		TAILQ_INSERT_HEAD(&ifp->if_addrlist, ifa, ifa_list);
 	else
 		TAILQ_INSERT_TAIL(&ifp->if_addrlist, ifa, ifa_list);
-	if (ifp->if_flags & IFF_BROADCAST && ifa->ifa_broadaddr)
-		ifa_item_insert(ifa->ifa_broadaddr, ifa, ifp);
 }
 
 void
