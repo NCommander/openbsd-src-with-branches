ea07d273f25ccc754717fe785eb0f5f2e0efde2e
schwarze <schwarze>
2010-03-22 19:33:19 +0000

    Even though this cannot happen in the diff -r case, even though diff(1) will exit(3) soon after this mkstemp(3) failure, even though this cannot really leak anything, close the file descriptor as soon as it is not used any more to make correctness of the code more obvious.
    patch from Igor Zinovik <zinovik dot igor at gmail dot com> on tech@
    "looks nice" deraadt@

--- usr.bin/diff/diffreg.c
+++ usr.bin/diff/diffreg.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: diffreg.c,v 1.72 2009/06/07 08:39:13 ray Exp $	*/
+/*	$OpenBSD: diffreg.c,v 1.73 2009/10/27 23:59:37 deraadt Exp $	*/
 
 /*
  * Copyright (C) Caldera International Inc.  2001-2002.
@@ -514,8 +514,10 @@ opentemp(const char *file)
 		return (NULL);
 	}
 
-	if ((ofd = mkstemp(tempfile)) < 0)
+	if ((ofd = mkstemp(tempfile)) < 0) {
+		close(ifd);
 		return (NULL);
+	}
 	unlink(tempfile);
 	while ((nread = read(ifd, buf, BUFSIZ)) > 0) {
 		if (write(ofd, buf, nread) != nread) {
