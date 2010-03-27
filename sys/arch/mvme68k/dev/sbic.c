fbb75ec8948c606a70de9d036f83ac5c9ae0e80e
miod <miod>
2010-03-27 16:04:24 +0000

    Fix fallout of the scsi_cmd() interface change.

--- sys/arch/mvme68k/dev/sbic.c
+++ sys/arch/mvme68k/dev/sbic.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sbic.c,v 1.24 2010/01/09 23:15:06 krw Exp $ */
+/*	$OpenBSD: sbic.c,v 1.25 2010/03/23 01:57:19 krw Exp $ */
 /*	$NetBSD: sbic.c,v 1.2 1996/04/23 16:32:54 chuck Exp $	*/
 
 /*
@@ -339,7 +339,7 @@ sbic_load_ptrs(dev)
  * so I will too.  I could plug it in, however so could they
  * in scsi_scsi_cmd().
  */
-int
+void
 sbic_scsicmd(xs)
     struct scsi_xfer *xs;
 {
