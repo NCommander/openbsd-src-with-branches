c529fff74d227f744c81dea408e05aa285b14b1d
oga <oga>
2010-03-30 17:40:55 +0000

    Prevent the apmd/x races for good.
    When we hit suspend time, go through all wsdisplays on the system. if
    they are in mode MAPPED, but not MODE_DUMBFB then if possible do a full
    vt switch to a !mapped vt, and prevent switching back until resume time.
    This has to be called from MD code because this involves userland
    running so that X can run the vt switch signal handler. This way, any
    case where we are using the "poke registers from userland" model, we
    will not be on the hardware when we go down, so the kernel can actually
    handle thing properly.
    
    Tested on several acpi laptops (by kettenis@ and ian@), x40 (me and
    beck@ at LEAST) and zaurus (me). Maybe others, but if so I forgot who at
    this time..
    
    Idea from deraadt somewhere over the Faroe Islands (I thought of a
    similar thing myself a while ago). Much prompting from him. Ok and
    comments miod@

--- sys/dev/wscons/wsdisplayvar.h
+++ sys/dev/wscons/wsdisplayvar.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: wsdisplayvar.h,v 1.22 2006/12/02 11:25:09 miod Exp $ */
+/* $OpenBSD: wsdisplayvar.h,v 1.23 2009/09/05 14:09:35 miod Exp $ */
 /* $NetBSD: wsdisplayvar.h,v 1.30 2005/02/04 02:10:49 perry Exp $ */
 
 /*
@@ -209,6 +209,8 @@ int wsdisplay_cfg_ioctl(struct wsdisplay_softc *sc,
  */
 #define WSDISPLAY_NULLSCREEN	-1
 void wsdisplay_switchtoconsole(void);
+void wsdisplay_suspend(void);
+void wsdisplay_resume(void);
 const struct wsscreen_descr *
     wsdisplay_screentype_pick(const struct wsscreen_list *, const char *);
 
