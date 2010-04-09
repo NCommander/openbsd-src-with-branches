2ba7975eb013ea9a013bb9a3a4e2b5f7816404d8
oga <oga>
2010-04-09 20:59:07 +0000

    encif is a global variable and thus pre-zeroed, don't bother bzero()ing it after the fact.
    ok henning@, claudio@

--- sys/net/if_enc.c
+++ sys/net/if_enc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_enc.c,v 1.46 2007/05/26 17:13:30 jason Exp $	*/
+/*	$OpenBSD: if_enc.c,v 1.47 2007/12/20 02:53:02 brad Exp $	*/
 /*
  * The authors of this code are John Ioannidis (ji@tla.org),
  * Angelos D. Keromytis (kermit@csd.uch.gr) and 
@@ -84,8 +84,6 @@ encattach(int nenc)
 {
 	struct ifnet *ifp;
 	int i;
-
-	bzero(encif, sizeof(encif));
 
 	for (i = 0; i < NENC; i++) {
 		ifp = &encif[i].sc_if;
