2cb4c6955ee51b506de93220f41c065185095fc0
miod <miod>
2010-04-15 20:38:11 +0000

    Turn DIO_INHOLE() into a function, returning the next select code to probe at; and skip function codes 132 to 135, inclusive, on models 362 and 382 - this is a fake DIO device probably intended to let non-SGC aware (i.e. legacy) HP-UX lowlevel code be able to ``detect'' a frame buffer.
    This change (as well as letting sgc attach) is untested due to the lack of any
    3x2 hardware, but shouldn't make things worse.

--- sys/arch/hp300/hp300/autoconf.c
+++ sys/arch/hp300/hp300/autoconf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: autoconf.c,v 1.46 2009/03/15 20:40:23 miod Exp $	*/
+/*	$OpenBSD: autoconf.c,v 1.47 2009/10/26 20:04:06 miod Exp $	*/
 /*	$NetBSD: autoconf.c,v 1.45 1999/04/10 17:31:02 kleink Exp $	*/
 
 /*
@@ -695,7 +695,7 @@ console_scan(func, arg)
 	int (*func)(int, caddr_t, void *);
 	void *arg;
 {
-	int size, scode, sctop;
+	int size, scode, sctop, sctmp;
 	caddr_t pa, va;
 
 	/*
@@ -703,13 +703,16 @@ console_scan(func, arg)
 	 * hardware.  If there's something there, call (*func)().
 	 */
 	sctop = DIO_SCMAX(machineid);
-	for (scode = 0; scode < sctop; ++scode) {
+	for (scode = 0; scode < sctop; scode++) {
 		/*
 		 * Skip over the select code hole and
 		 * the internal HP-IB controller.
 		 */
-		if (DIO_INHOLE(scode) ||
-		    ((scode == 7) && internalhpib))
+		if ((sctmp = dio_inhole(scode)) != 0) {
+			scode = sctmp - 1;
+			continue;
+		}
+		if (scode == 7 && internalhpib)
 			continue;
 
 		/* Map current PA. */
