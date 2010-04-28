47fb1cd2e7be24fa537a870d73ea559c5777c62e
kettenis <kettenis>
2010-04-28 21:23:18 +0000

    s/i386/__i386__/, no binary change.

--- sys/dev/isa/aha.c
+++ sys/dev/isa/aha.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: aha.c,v 1.66 2010/01/10 00:40:25 krw Exp $	*/
+/*	$OpenBSD: aha.c,v 1.67 2010/03/23 01:57:20 krw Exp $	*/
 /*	$NetBSD: aha.c,v 1.11 1996/05/12 23:51:23 mycroft Exp $	*/
 
 #undef AHADIAG
@@ -77,7 +77,7 @@
  * on i386 at least, xfers to/from user memory
  * cannot be serviced at interrupt time.
  */
-#ifdef i386
+#ifdef __i386__
 #define VOLATILE_XS(xs) \
 	((xs)->datalen > 0 && (xs)->bp == NULL && \
 	((xs)->flags & SCSI_POLL) == 0)
