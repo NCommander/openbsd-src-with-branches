b773bad92f5c6890bb32bc81b6f700fc6c15c5d1
thib <thib>
2010-05-20 16:31:03 +0000

    remove unused variable. fallout from previous. ok phessler@

--- sys/arch/sparc64/dev/vdsk.c
+++ sys/arch/sparc64/dev/vdsk.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vdsk.c,v 1.19 2010/03/24 06:55:28 dlg Exp $	*/
+/*	$OpenBSD: vdsk.c,v 1.20 2010/05/20 00:55:17 krw Exp $	*/
 /*
  * Copyright (c) 2009 Mark Kettenis
  *
@@ -1117,7 +1117,6 @@ vdsk_scsi_capacity(struct scsi_xfer *xs)
 void
 vdsk_scsi_done(struct scsi_xfer *xs, int error)
 {
-	int s;
 
 	xs->error = error;
 
