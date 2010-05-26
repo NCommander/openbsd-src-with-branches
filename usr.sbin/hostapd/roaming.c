e0b24e6d8cc742e077517525a00e22deaa78ae1d
millert <millert>
2010-05-26 19:18:10 +0000

    No need to include <sys/sysctl.h> here and it pulls in <sys/proc.h> which causes conflicts with the libevent macros.  OK deraadt@

--- usr.sbin/hostapd/roaming.c
+++ usr.sbin/hostapd/roaming.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: roaming.c,v 1.3 2007/02/08 11:15:55 reyk Exp $	*/
+/*	$OpenBSD: roaming.c,v 1.4 2009/06/05 22:44:51 chris Exp $	*/
 
 /*
  * Copyright (c) 2005, 2006 Reyk Floeter <reyk@openbsd.org>
@@ -19,7 +19,6 @@
 #include <sys/param.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/sysctl.h>
 #include <sys/tree.h>
 #include <sys/ioctl.h>
 
