87d7aefc9cd5e34910d721c6ad2a37b862b407da
kettenis <kettenis>
2010-05-28 08:32:41 +0000

    Make sure the temporary buffer used to generate tcp options is properly aligned, otherwise we lose on strict alignment architecture.  Should fix problems with gcc4 compiled bsd.rd's that people see on sparc64.
    ok millert@, beck@, jsing@

--- sys/netinet/tcp_output.c
+++ sys/netinet/tcp_output.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tcp_output.c,v 1.86 2008/09/03 12:51:39 henning Exp $	*/
+/*	$OpenBSD: tcp_output.c,v 1.87 2009/06/05 00:05:22 claudio Exp $	*/
 /*	$NetBSD: tcp_output.c,v 1.16 1997/06/03 16:17:09 kml Exp $	*/
 
 /*
@@ -212,7 +212,8 @@ tcp_output(struct tcpcb *tp)
 	int off, flags, error;
 	struct mbuf *m;
 	struct tcphdr *th;
-	u_char opt[MAX_TCPOPTLEN];
+	u_int32_t optbuf[howmany(MAX_TCPOPTLEN, sizeof(u_int32_t))];
+	u_char *opt = (u_char *)optbuf;
 	unsigned int optlen, hdrlen, packetlen;
 	int idle, sendalot = 0;
 #ifdef TCP_SACK
