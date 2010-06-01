f4a36a0525a2eee48688524f129d52ff246f0942
bluhm <bluhm>
2010-06-01 11:22:28 +0000

    Fix error message. ok claudio@ stsp@

--- usr.sbin/ospf6d/rde.c
+++ usr.sbin/ospf6d/rde.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rde.c,v 1.37 2010/03/01 08:58:48 claudio Exp $ */
+/*	$OpenBSD: rde.c,v 1.38 2010/04/16 11:36:10 claudio Exp $ */
 
 /*
  * Copyright (c) 2004, 2005 Claudio Jeker <claudio@openbsd.org>
@@ -1416,7 +1416,7 @@ orig_intra_lsa_rtr(struct area *area, struct vertex *old)
 
 	len = sizeof(struct lsa_hdr) + sizeof(struct lsa_intra_prefix);
 	if ((lsa = calloc(1, len)) == NULL)
-		fatal("orig_intra_lsa_net");
+		fatal("orig_intra_lsa_rtr");
 
 	lsa->data.pref_intra.ref_type = htons(LSA_TYPE_ROUTER);
 	lsa->data.pref_intra.ref_ls_id = 0;
