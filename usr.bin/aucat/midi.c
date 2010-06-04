ec0c2bc6d3a5069a626a8d7dadba1ce5e9d44d23
ratchov <ratchov>
2010-06-04 07:02:59 +0000

    midi control aproc structure is can be destroyed before any stream referencing it (for instance while a recording stream drains buffers of a closed device). So, don't abort() if this happen.

--- usr.bin/aucat/midi.c
+++ usr.bin/aucat/midi.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: midi.c,v 1.24 2010/05/08 15:35:45 ratchov Exp $	*/
+/*	$OpenBSD: midi.c,v 1.25 2010/06/04 06:15:28 ratchov Exp $	*/
 /*
  * Copyright (c) 2008 Alexandre Ratchov <alex@caoua.org>
  *
@@ -1161,13 +1161,6 @@ ctl_done(struct aproc *p)
 	for (i = 0, s = p->u.ctl.slot; i < CTL_NSLOT; i++, s++) {
 		if (s->ops != NULL)
 			s->ops->quit(s->arg);
-#ifdef DEBUG
-		if (s->ops != NULL) {
-			ctl_slotdbg(p, i);
-			dbg_puts(": still in use\n");
-			dbg_panic();
-		}
-#endif
 	}
 }
 
