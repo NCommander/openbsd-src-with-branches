e67272ea54ce5be1301c8937187c9b7147170a61
william <william>
2010-06-11 01:32:25 +0000

    4.7-stable; ok deraadt@

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.102 2010/03/05 08:54:01 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.103 2010/03/05 10:59:35 miod Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -65,7 +65,7 @@ ost="OpenBSD"
 osr="4.7"
 
 cat >vers.c <<eof
-#define STATUS ""			/* release */
+#define STATUS "-stable"			/* release */
 #if 0
 #define STATUS "-current"		/* just after a release */
 #define STATUS "-beta"			/* just before a release */
