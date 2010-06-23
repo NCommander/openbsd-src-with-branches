bc300121a72af35d8f91caa9cf2d214ce27793a7
jmc <jmc>
2010-06-23 16:22:29 +0000

    sort the options list, and add some new options to usage();

--- usr.sbin/ldapctl/ldapctl.c
+++ usr.sbin/ldapctl/ldapctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ldapctl.c,v 1.1 2010/05/31 17:36:31 martinh Exp $	*/
+/*	$OpenBSD: ldapctl.c,v 1.2 2010/06/23 13:31:30 martinh Exp $	*/
 
 /*
  * Copyright (c) 2009, 2010 Martin Hedenfalk <martin@bzero.se>
@@ -66,7 +66,8 @@ usage(void)
 {
 	extern char *__progname;
 
-	fprintf(stderr, "usage: %s [-s socket] command [arg ...]\n",
+	fprintf(stderr,
+	    "usage: %s [-v] [-f file] [-s socket] command [argument ...]\n",
 	    __progname);
 	exit(1);
 }
