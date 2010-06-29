0d2e50db0a2f43853e79ce4adcc5b8730ec88499
thib <thib>
2010-06-29 21:13:43 +0000

    fpu_kernel_{enter,exit}; Functions to allow the use of the FPU in the kernel.
    From Mike Belopuhov; Little bits by myself.
    
    Comments/OK kettenis@

--- sys/arch/amd64/include/fpu.h
+++ sys/arch/amd64/include/fpu.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fpu.h,v 1.2 2004/02/28 22:26:05 deraadt Exp $	*/
+/*	$OpenBSD: fpu.h,v 1.3 2006/10/01 10:52:10 kettenis Exp $	*/
 /*	$NetBSD: fpu.h,v 1.1 2003/04/26 18:39:40 fvdl Exp $	*/
 
 #ifndef	_AMD64_FPU_H_
@@ -51,11 +51,12 @@ struct cpu_info;
 
 void fpuinit(struct cpu_info *);
 void fpudrop(void);
-void fpusave(struct proc *);
 void fpudiscard(struct proc *);
 void fputrap(struct trapframe *);
 void fpusave_proc(struct proc *, int);
 void fpusave_cpu(struct cpu_info *, int);
+void fpu_kernel_enter(void);
+void fpu_kernel_exit(void);
 
 #endif
 
