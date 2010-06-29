deb99f21cc9c5d2d2cb357ec7ecba7e26529b1c4
miod <miod>
2010-06-29 21:33:54 +0000

    In the boot blocks, stop assuming we have a 1:1 mapping of low physical memory, but instead gather memory layout information and work with the MMU (or the PROM) to make sure we can actually load the kernel image in a proper contiguous physical memory area.
    In order to do this, we look at the kernel image twice; during the first pass,
    the kernel footprint is computed, and then after making sure it can be
    loaded, the second pass loads the actual image.
    
    Since such a logic doesn't work on media which can not seek backwards, such
    as tapes, we check for the boot device being a tape and, in that case, assume
    a fixed (generous) image size and don't load the kernel symbol table (to
    avoid seeking backwards); since tape boot is supposed to be only used to
    boot bsd.rd, this is something we can live with.
    
    While there, lower the address the boot blocks are loaded in memory, because
    the last crank did not work with some early sun4c OpenPROM, which only
    map about 3.5MB of memory.
    
    Memory games logic from NetBSD, tape handling by me.

--- sys/arch/sparc/stand/common/version.c
+++ sys/arch/sparc/stand/common/version.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: version.c,v 1.3 2002/08/12 00:25:26 art Exp $	*/
+/*	$OpenBSD: version.c,v 1.4 2009/07/01 20:13:39 miod Exp $	*/
 /*	$NetBSD: version.c,v 1.4 1995/09/16 23:20:39 pk Exp $ */
 
 /*
@@ -43,6 +43,7 @@
  *	2.1	Bumped RELOC
  *	2.2	ELF support added.
  *	2.3	Bumped RELOC
+ *	2.4	Support for larger kernels and fragmented memory layouts.
  */
 
-char *version = "2.3";
+char *version = "2.4";
