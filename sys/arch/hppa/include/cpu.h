797c6573a2f1b729bb393decf8e3934a7a90b334
jsing <jsing>
2010-06-29 00:50:40 +0000

    Store pointer to process FPU state in struct cpu_info.
    ok kettenis@

--- sys/arch/hppa/include/cpu.h
+++ sys/arch/hppa/include/cpu.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cpu.h,v 1.72 2010/05/21 15:24:29 jsing Exp $	*/
+/*	$OpenBSD: cpu.h,v 1.74 2010/06/03 15:48:58 jsing Exp $	*/
 
 /*
  * Copyright (c) 2000-2004 Michael Shalayeff
@@ -84,6 +84,7 @@ struct cpu_info {
 	volatile int	ci_flags;
 
 	struct proc	*ci_curproc;
+	paddr_t		ci_fpu_state;		/* Process FPU state. */
 	paddr_t		ci_stack;
 
 	register_t	ci_psw;			/* Processor Status Word. */
