01e51ead2deced0d3c6ff3b0c4e792575d86fdf3
art <art>
2010-07-01 21:27:39 +0000

    Implement vs{,un}lock_device and use it for physio.
    Just like normal vs{,un}lock, but in case the pages we get are not dma
    accessible, we bounce them, if they are dma acessible, the functions behave
    exactly like normal vslock. The plan for the future is to have fault_wire
    allocate dma acessible pages so that we don't need to bounce (especially
    in cases where the same buffer is reused for physio over and over again),
    but for now, keep it as simple as possible.

--- sys/uvm/uvm_extern.h
+++ sys/uvm/uvm_extern.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_extern.h,v 1.85 2010/04/22 19:02:55 oga Exp $	*/
+/*	$OpenBSD: uvm_extern.h,v 1.87 2010/06/27 03:03:49 thib Exp $	*/
 /*	$NetBSD: uvm_extern.h,v 1.57 2001/03/09 01:02:12 chs Exp $	*/
 
 /*
@@ -502,6 +502,11 @@ boolean_t		uvm_kernacc(caddr_t, size_t, int);
 int			uvm_vslock(struct proc *, caddr_t, size_t,
 			    vm_prot_t);
 void			uvm_vsunlock(struct proc *, caddr_t, size_t);
+
+int			uvm_vslock_device(struct proc *, void *, size_t,
+			    vm_prot_t, void **);
+void			uvm_vsunlock_device(struct proc *, void *, size_t,
+			    void *);
 
 
 /* uvm_init.c */
