baf419450b9e52529267826d2d883afc02cfe2ec
jsing <jsing>
2010-07-01 04:33:59 +0000

    Fix incorrect function prototype.

--- sys/arch/hppa64/hppa64/trap.c
+++ sys/arch/hppa64/hppa64/trap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: trap.c,v 1.8 2007/09/15 14:55:30 krw Exp $	*/
+/*	$OpenBSD: trap.c,v 1.9 2010/05/24 15:06:05 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2005 Michael Shalayeff
@@ -115,7 +115,7 @@ u_char hppa64_regmap[32] = {
 	offsetof(struct trapframe, tf_r31) / 8,
 };
 
-void	userret(struct proc *p);
+void	userret(struct proc *p, register_t pc, u_quad_t oticks);
 
 void
 userret(struct proc *p, register_t pc, u_quad_t oticks)
