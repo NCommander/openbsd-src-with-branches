eb6822d6b1a8e9991e89ca5179cd85835d29ec4e
deraadt <deraadt>
2010-07-02 22:01:10 +0000

    #ifndef SMALL a few more things to save space on the ramdisks

--- usr.bin/ftp/ftp.c
+++ usr.bin/ftp/ftp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ftp.c,v 1.78 2009/04/27 23:20:48 martynas Exp $	*/
+/*	$OpenBSD: ftp.c,v 1.79 2009/06/06 23:14:44 martynas Exp $	*/
 /*	$NetBSD: ftp.c,v 1.27 1997/08/18 10:20:23 lukem Exp $	*/
 
 /*
@@ -1907,6 +1907,7 @@ abort:
 	(void)signal(SIGINT, oldintr);
 }
 
+#ifndef SMALL
 /* ARGSUSED */
 void
 reset(int argc, char *argv[])
@@ -1926,6 +1927,7 @@ reset(int argc, char *argv[])
 		}
 	}
 }
+#endif
 
 char *
 gunique(const char *local)
