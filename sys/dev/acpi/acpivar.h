5b6269f4f28ab03f066559c2f1601aced683178a
deraadt <deraadt>
2010-07-06 20:14:17 +0000

    Don't use a workq to do the suspend, because it races aginst the acpi thread.  Instead, just tell the acpi thread to do the suspend for us. This makes apmd & zzz work correctly.  While here, have acpithinkpad attempt to post the event to apm before prompting the suspend itself. ok kettenis marco mlarkin

--- sys/dev/acpi/acpivar.h
+++ sys/dev/acpi/acpivar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: acpivar.h,v 1.56 2010/03/31 19:21:19 kettenis Exp $	*/
+/*	$OpenBSD: acpivar.h,v 1.57 2010/04/07 06:33:06 kettenis Exp $	*/
 /*
  * Copyright (c) 2005 Thorsten Lockert <tholo@sigmasoft.com>
  *
@@ -207,6 +207,8 @@ struct acpi_softc {
 	int			sc_powerbtn;
 	int			sc_sleepbtn;
 
+	int			sc_sleepmode;
+
 	struct {
 		int slp_typa;
 		int slp_typb;
@@ -302,6 +304,8 @@ void	 acpi_sleep_walk(struct acpi_softc *, int);
 #define ACPI_IOWRITE 1
 
 void acpi_delay(struct acpi_softc *, int64_t);
+void acpi_wakeup(void *);
+
 int acpi_gasio(struct acpi_softc *, int, int, uint64_t, int, int, void *);
 
 int	acpi_set_gpehandler(struct acpi_softc *, int,
