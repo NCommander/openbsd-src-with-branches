25d2c5d724b3f951b8b45985b5a667560960ff52
espie <espie>
2010-07-09 12:42:43 +0000

    fix pkg_mklocatedb

--- usr.sbin/pkg_add/OpenBSD/AddCreateDelete.pm
+++ usr.sbin/pkg_add/OpenBSD/AddCreateDelete.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD$
+# $OpenBSD: AddCreateDelete.pm,v 1.10 2010/06/30 10:51:04 espie Exp $
 #
 # Copyright (c) 2007-2010 Marc Espie <espie@openbsd.org>
 #
@@ -34,6 +34,7 @@ sub init
 	$self->{progressmeter} = OpenBSD::ProgressMeter->new;
 	$self->{bad} = 0;
 	$self->SUPER::init;
+	$self->{export_level}++;
 }
 
 sub progress
