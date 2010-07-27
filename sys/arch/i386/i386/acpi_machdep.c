9cc49494a08694df3ac3af6440866769102ac90c
deraadt <deraadt>
2010-07-27 16:53:11 +0000

    delete a #if 0 chunk which was used for early development and will never be reused ok jordan

--- sys/arch/i386/i386/acpi_machdep.c
+++ sys/arch/i386/i386/acpi_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: acpi_machdep.c,v 1.34 2010/07/06 06:25:55 deraadt Exp $	*/
+/*	$OpenBSD: acpi_machdep.c,v 1.35 2010/07/25 21:43:35 deraadt Exp $	*/
 /*
  * Copyright (c) 2005 Thorsten Lockert <tholo@sigmasoft.com>
  *
@@ -260,11 +260,6 @@ acpi_sleep_machdep(struct acpi_softc *sc, int state)
 	}
 
 	/* Resume path continues here */
-#if 0
-        /* Temporarily disabled for debugging purposes */
-        /* Reset the wakeup vector to avoid resuming on reboot */
-        sc->sc_facs->wakeup_vector = 0;
-#endif
 
 #if NISA > 0
 	isa_defaultirq();
