d238a311f65be059b64863b7393cbd52ffb62a56
kettenis <kettenis>
2010-08-19 19:31:53 +0000

    Add "memory" clobber to lidt inline asm, to prevent the GCC optimizer from getting stupid ideas like optimizing away stores to the descriptor that we're setting.  This may be overkill, but this code is far from performance critical and it may prevent future surprises.  Fixes instant reboots with bsd.rd on Pentiums with the F00F bug.
    Thanks to espie@, for narrowing the issue down enough for me to find the
    problem.  ok deraadt@

--- sys/arch/i386/include/cpufunc.h
+++ sys/arch/i386/include/cpufunc.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cpufunc.h,v 1.15 2009/08/13 13:24:48 kettenis Exp $	*/
+/*	$OpenBSD: cpufunc.h,v 1.16 2009/12/09 14:28:46 oga Exp $	*/
 /*	$NetBSD: cpufunc.h,v 1.8 1994/10/27 04:15:59 cgd Exp $	*/
 
 /*
@@ -78,7 +78,7 @@ invlpg(u_int addr)
 static __inline void
 lidt(void *p)
 {
-	__asm __volatile("lidt (%0)" : : "r" (p));
+	__asm __volatile("lidt (%0)" : : "r" (p) : "memory");
 }
 
 static __inline void
