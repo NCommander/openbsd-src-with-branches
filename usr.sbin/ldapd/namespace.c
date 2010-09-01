175d74ba2565b12c339d1ce2e73b05e4a0564bec
martinh <martinh>
2010-09-01 17:34:15 +0000

    Move generic imsg/libevent glue to a separate file.
    with eric@ at c2k10

--- usr.sbin/ldapd/namespace.c
+++ usr.sbin/ldapd/namespace.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: namespace.c,v 1.9 2010/06/29 21:54:38 martinh Exp $ */
+/*	$OpenBSD: namespace.c,v 1.10 2010/07/10 14:27:15 martinh Exp $ */
 
 /*
  * Copyright (c) 2009, 2010 Martin Hedenfalk <martin@bzero.se>
@@ -151,7 +151,7 @@ namespace_reopen(const char *path)
 		return -1;
 	}
 
-	return imsg_compose_event(iev_ldapd, IMSG_LDAPD_OPEN, 0, 0, -1, &req,
+	return imsgev_compose(iev_ldapd, IMSG_LDAPD_OPEN, 0, 0, -1, &req,
 	    sizeof(req));
 }
 
