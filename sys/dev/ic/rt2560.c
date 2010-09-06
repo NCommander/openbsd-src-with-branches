ecabb759dc1e3a354003d24ac24058d2c5cf3d0b
kettenis <kettenis>
2010-09-06 18:22:02 +0000

    Make the interrupt handler bail out early if the device wen away.  Fixes freezes that occur when removing CardBus cards when the interface is up.
    ok deraadt@

--- sys/dev/ic/rt2560.c
+++ sys/dev/ic/rt2560.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rt2560.c,v 1.53 2010/08/27 17:08:00 jsg Exp $  */
+/*	$OpenBSD: rt2560.c,v 1.54 2010/08/28 18:08:07 deraadt Exp $  */
 
 /*-
  * Copyright (c) 2005, 2006
@@ -1337,7 +1337,10 @@ rt2560_intr(void *arg)
 	struct ifnet *ifp = &sc->sc_ic.ic_if;
 	uint32_t r;
 
-	if ((r = RAL_READ(sc, RT2560_CSR7)) == 0)
+	r = RAL_READ(sc, RT2560_CSR7);
+	if (__predict_false(r == 0xffffffff))
+		return 0;	/* device likely went away */
+	if (r == 0)
 		return 0;	/* not for us */
 
 	/* disable interrupts */
