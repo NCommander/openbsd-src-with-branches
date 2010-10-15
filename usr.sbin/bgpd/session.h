e0dc438807a3b4f2499ce90d166c4e69a06f77fc
claudio <claudio>
2010-10-15 07:45:32 +0000

    Add the FSM suberrors specified in draft-ietf-idr-fsm-subcode. This should help identifying FSM errors since the state is know known. OK henning@ sthen@

--- usr.sbin/bgpd/session.h
+++ usr.sbin/bgpd/session.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: session.h,v 1.107 2010/05/26 13:56:07 nicm Exp $ */
+/*	$OpenBSD: session.h,v 1.108 2010/06/27 19:53:34 claudio Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -92,6 +92,13 @@ enum suberr_open {
 	ERR_OPEN_AUTH,
 	ERR_OPEN_HOLDTIME,
 	ERR_OPEN_CAPA
+};
+
+enum suberr_fsm {
+	ERR_FSM_UNSPECIFIC = 0,
+	ERR_FSM_UNEX_OPENSENT,
+	ERR_FSM_UNEX_OPENCONFIRM,
+	ERR_FSM_UNEX_ESTABLISHED
 };
 
 enum opt_params {
