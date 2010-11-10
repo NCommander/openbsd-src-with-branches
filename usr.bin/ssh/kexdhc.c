6db1ad5adc8d48a98ef6e56975ccdce7aa6c9aeb
djm <djm>
2010-11-10 01:33:07 +0000

    use only libcrypto APIs that are retained with OPENSSL_NO_DEPRECATED. these have been around for years by this time. ok markus

--- usr.bin/ssh/kexdhc.c
+++ usr.bin/ssh/kexdhc.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: kexdhc.c,v 1.10 2006/10/31 16:33:12 markus Exp $ */
+/* $OpenBSD: kexdhc.c,v 1.11 2006/11/06 21:25:28 markus Exp $ */
 /*
  * Copyright (c) 2001 Markus Friedl.  All rights reserved.
  *
@@ -24,6 +24,8 @@
  */
 
 #include <sys/types.h>
+
+#include <openssl/dh.h>
 
 #include <stdio.h>
 #include <string.h>
--- usr.bin/ssh/kexgexc.c
+++ usr.bin/ssh/kexgexc.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: kexgexc.c,v 1.10 2006/10/31 16:33:12 markus Exp $ */
+/* $OpenBSD: kexgexc.c,v 1.11 2006/11/06 21:25:28 markus Exp $ */
 /*
  * Copyright (c) 2000 Niels Provos.  All rights reserved.
  * Copyright (c) 2001 Markus Friedl.  All rights reserved.
@@ -25,6 +25,8 @@
  */
 
 #include <sys/types.h>
+
+#include <openssl/dh.h>
 
 #include <stdio.h>
 #include <string.h>
