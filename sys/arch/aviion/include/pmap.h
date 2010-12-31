0b48fd9e01efed17d7c7838ef439f20b2e74379c
miod <miod>
2010-12-31 21:38:08 +0000

    Massive overhauling of the m88k pmap, though I can't pretend it's a new pmap since a large part of the structures and logic remains.
    Since m88k has separate supervisor/user spaces, we can map physical memory 1:1
    in supervisor space, and have the kernel virtual address space start from the
    end of physical memory.
    
    This allows us to switch to __HAVE_PMAP_DIRECT. And to get rid of the double
    mapped sdt, since now their virtual and physical addresses will always match.
    
    The upper bound of the kernel virtual memory space is now platform dependent,
    until the code which relies upon some hardware devices being mapped 1:1 in
    supervisor mode is updated to no longer require this (this is mainly a PITA on
    luna88k, where onboard devices start at 0x40000000, leaving only 1GB of KVA at
    the moment - still much better than the previous 512MB).
    
    Tested on mvme88k only (187, 188, 197LE, 197DP). Other platforms ought to
    work, aviion will be checked shortly and fixed if necessary. No known
    OpenBSD/luna88k system in working condition at the moment.

--- sys/arch/aviion/include/pmap.h
+++ sys/arch/aviion/include/pmap.h
@@ -1,13 +1,3 @@
-/* $OpenBSD: pmap.h,v 1.1.1.1 2006/05/09 18:32:33 miod Exp $ */
+/* $OpenBSD: pmap.h,v 1.2 2007/12/19 21:51:29 miod Exp $ */
 /* public domain */
-
-#ifndef	_AVIION_PMAP_H_
-#define	_AVIION_PMAP_H_
-
 #include <m88k/pmap.h>
-
-#ifdef	_KERNEL
-#define	pmap_bootstrap_md(va)		(va)
-#endif
-
-#endif	_AVIION_PMAP_H_
--- sys/arch/luna88k/include/pmap.h
+++ sys/arch/luna88k/include/pmap.h
@@ -1,11 +1,3 @@
-/* $OpenBSD: pmap.h,v 1.3 2004/06/02 13:49:41 miod Exp $ */
+/* $OpenBSD: pmap.h,v 1.4 2004/07/25 11:06:41 miod Exp $ */
 /* public domain */
-
-#ifndef	_LUNA88K_PMAP_H_
-#define	_LUNA88K_PMAP_H_
-
 #include <m88k/pmap.h>
-
-#define	pmap_bootstrap_md(va)	(va)
-
-#endif	_LUNA88K_PMAP_H_
