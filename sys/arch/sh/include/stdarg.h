af8fadc2d74357d805b9ff60c1414bca7f3b5cf5
matthew <matthew>
2011-03-02 18:31:58 +0000

    Change va_arg and va_end definitions from object-like macros to function-like macros.  Allows (questionable but legal) use of "va_arg" as a local variable name in code that includes <stdarg.h>.
    "seems right" deraadt@

--- sys/arch/sh/include/stdarg.h
+++ sys/arch/sh/include/stdarg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: stdarg.h,v 1.1.1.1 2006/10/06 21:02:55 miod Exp $	*/
+/*	$OpenBSD: stdarg.h,v 1.2 2008/10/23 21:25:07 kettenis Exp $	*/
 /*	$NetBSD: stdarg.h,v 1.9 2006/05/21 22:39:04 uwe Exp $	*/
 
 /*
@@ -49,8 +49,8 @@ typedef __va_list	va_list;
 #endif
 
 #define	va_start(ap, last)	__builtin_stdarg_start((ap), (last))
-#define	va_arg			__builtin_va_arg
-#define	va_end			__builtin_va_end
+#define	va_arg(ap, type)	__builtin_va_arg((ap), type)
+#define	va_end(ap)		__builtin_va_end((ap))
 #define	__va_copy(dest, src)	__builtin_va_copy((dest), (src))
 
 #endif /* !_SH_STDARG_H_ */
