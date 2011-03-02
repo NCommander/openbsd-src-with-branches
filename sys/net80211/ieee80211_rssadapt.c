6fe115ce86cf1883d14f5166a963904ea476fc96
fgsch <fgsch>
2011-03-02 08:48:59 +0000

    Allow the kernel to compile with IEEE80211_DEBUG. deraadt@ ok.

--- sys/net80211/ieee80211_rssadapt.c
+++ sys/net80211/ieee80211_rssadapt.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ieee80211_rssadapt.c,v 1.8 2010/04/20 22:05:43 tedu Exp $	*/
+/*	$OpenBSD: ieee80211_rssadapt.c,v 1.9 2010/07/17 16:25:09 damien Exp $	*/
 /*	$NetBSD: ieee80211_rssadapt.c,v 1.7 2004/05/25 04:33:59 dyoung Exp $	*/
 
 /*-
@@ -29,6 +29,7 @@
  */
 
 #include <sys/param.h>
+#include <sys/systm.h>
 #include <sys/kernel.h>
 #include <sys/socket.h>
 
