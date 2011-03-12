f10f7d537704dcf08cda0a7a9d16f477d7d1b70c
deraadt <deraadt>
2011-03-12 19:40:34 +0000

    incorrect parameters to warn()

--- sys/arch/sparc/stand/installboot/installboot.c
+++ sys/arch/sparc/stand/installboot/installboot.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: installboot.c,v 1.4 2003/08/25 23:36:46 tedu Exp $	*/
+/*	$OpenBSD: installboot.c,v 1.5 2010/11/19 18:11:21 deraadt Exp $	*/
 /*	$NetBSD: installboot.c,v 1.1 1997/06/01 03:39:45 mrg Exp $	*/
 
 /*
@@ -239,7 +239,7 @@ loadprotoblocks(fname, size)
 		return NULL;
 	}
 	if (statbuf.st_size == 0) {
-		warn(1, "%s is empty", fname);
+		warn("%s is empty", fname);
 		close(fd);
 		return NULL;
 	}
