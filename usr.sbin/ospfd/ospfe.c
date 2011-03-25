9f1358e77cdf6093910d3f8d22fddcc86c9dc7c6
claudio <claudio>
2011-03-25 08:52:21 +0000

    Rewrite the lsack generation. Make it more like the lsupdate code. Removes some of the rather strange packet handling. Tested by a few especially sthen@

--- usr.sbin/ospfd/ospfe.c
+++ usr.sbin/ospfd/ospfe.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ospfe.c,v 1.78 2011/03/08 10:56:02 claudio Exp $ */
+/*	$OpenBSD: ospfe.c,v 1.79 2011/03/24 08:35:59 claudio Exp $ */
 
 /*
  * Copyright (c) 2005 Claudio Jeker <claudio@openbsd.org>
@@ -643,7 +643,7 @@ ospfe_dispatch_rde(int fd, short event, void *bula)
 				break;
 
 			/* send a direct acknowledgement */
-			send_ls_ack(nbr->iface, nbr->addr, imsg.data,
+			send_direct_ack(nbr->iface, nbr->addr, imsg.data,
 			    imsg.hdr.len - IMSG_HEADER_SIZE);
 
 			break;
