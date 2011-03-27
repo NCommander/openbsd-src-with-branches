8d8b722e8adb083b427290e4750d60fb224b7094
nicm <nicm>
2011-03-27 20:31:25 +0000

    Don't include meta twice when working out the flags to output for xterm-style keys - bit 3 is accepted on input but not on output. Also a style nit in the header.

--- usr.bin/tmux/xterm-keys.c
+++ usr.bin/tmux/xterm-keys.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: xterm-keys.c,v 1.7 2010/10/14 00:37:51 nicm Exp $ */
+/* $OpenBSD: xterm-keys.c,v 1.8 2011/01/01 03:43:20 nicm Exp $ */
 
 /*
  * Copyright (c) 2009 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -174,8 +174,6 @@ xterm_keys_lookup(int key)
 		modifiers += 2;
 	if (key & KEYC_CTRL)
 		modifiers += 4;
-	if (key & KEYC_ESCAPE)
-		modifiers += 8;
 
 	/*
 	 * If the key has no modifiers, return NULL and let it fall through to
