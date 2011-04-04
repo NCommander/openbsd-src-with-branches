e1529b0273fdcb6e907f556dde22f79f2794098f
pirofti <pirofti>
2011-04-04 21:53:50 +0000

    Regenerate -- linux_sys_[g|s]et_thread_area().

--- sys/compat/linux/linux_syscall.h
+++ sys/compat/linux/linux_syscall.h
@@ -718,6 +718,12 @@
 /* syscall: "fremovexattr" ret: "int" args: */
 #define	LINUX_SYS_fremovexattr	237
 
+/* syscall: "set_thread_area" ret: "int" args: "struct l_segment_descriptor *" */
+#define	LINUX_SYS_set_thread_area	243
+
+/* syscall: "get_thread_area" ret: "int" args: "struct l_segment_descriptor *" */
+#define	LINUX_SYS_get_thread_area	244
+
 /* syscall: "fadvise64" ret: "int" args: */
 #define	LINUX_SYS_fadvise64	250
 
