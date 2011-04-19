147174e8b7b1ac5f23aa7912dc27cc4636d37cee
krw <krw>
2011-04-19 23:59:11 +0000

    Iopoolification. Much simpler version than the one revived at k2k11. This one works. For me at least.
    Botch spotted by matthew@.
    
    ok matthew@ dlg@

--- sys/dev/ic/gdtvar.h
+++ sys/dev/ic/gdtvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: gdtvar.h,v 1.17 2009/08/12 17:51:33 jsg Exp $	*/
+/*	$OpenBSD: gdtvar.h,v 1.18 2011/04/19 21:17:07 krw Exp $	*/
 
 /*
  * Copyright (c) 1999, 2000 Niklas Hallqvist.  All rights reserved.
@@ -296,6 +296,9 @@ struct gdt_softc {
 	TAILQ_HEAD(, gdt_ucmd) sc_ucmdq;
 	LIST_HEAD(, scsi_xfer) sc_queue;
 	struct scsi_xfer *sc_queuelast;
+
+	struct mutex		sc_ccb_mtx;
+	struct scsi_iopool	sc_iopool;
 
 	int	sc_ndevs;
 
