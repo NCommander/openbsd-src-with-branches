5319fbcaff76cc28e47a0d183f24ac201ce7daad
krw <krw>
2011-04-21 23:10:08 +0000

    A plausible iopoolification. Utter dearth of interested parties after more than a year of grovelling emails shows further effort is pointless.
    ok matthew@ dlg@

--- sys/dev/ic/cacvar.h
+++ sys/dev/ic/cacvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cacvar.h,v 1.4 2008/06/26 05:42:15 ray Exp $	*/
+/*	$OpenBSD: cacvar.h,v 1.5 2008/10/29 21:17:15 brad Exp $	*/
 /*	$NetBSD: cacvar.h,v 1.7 2000/10/19 14:28:47 ad Exp $	*/
 
 /*-
@@ -110,6 +110,9 @@ struct cac_softc {
 	int			(*sc_ioctl)(struct device *, u_long, caddr_t);
 	struct ksensor		*sc_sensors;
 	struct ksensordev	sc_sensordev;
+
+	struct mutex		sc_ccb_mtx; /* ccb queue protection */
+	struct scsi_iopool	sc_iopool;
 };
 
 /* XXX These have to become spinlocks in case of fine SMP */
