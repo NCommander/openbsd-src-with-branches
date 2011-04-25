87b66b9e5ca142b140531bd7bddca33867b898dc
deraadt <deraadt>
2011-04-25 19:29:11 +0000

    Remove backwards compatibility for svnd.  All new operations must use the vnd devices nodes, which impliment svnd functionality -- that being the only thing which vnd now supports.  Confused?  Don't worry, it is documented. ok krw thib

--- sys/dev/vnd.c
+++ sys/dev/vnd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vnd.c,v 1.112 2011/04/23 17:01:04 jsing Exp $	*/
+/*	$OpenBSD: vnd.c,v 1.113 2011/04/23 22:00:09 miod Exp $	*/
 /*	$NetBSD: vnd.c,v 1.26 1996/03/30 23:06:11 christos Exp $	*/
 
 /*
@@ -83,11 +83,7 @@ int vnddebug = 0x00;
 #define	DNPRINTF(f, p...)	/* nothing */
 #endif	/* VNDDEBUG */
 
-/*
- * vndunit is a bit weird.  have to reconstitute the dev_t for
- * DISKUNIT(), but with the minor masked off.
- */
-#define	vndunit(x)	DISKUNIT(makedev(major(x), minor(x) & 0x7ff))
+#define	vndunit(x)	DISKUNIT(makedev(major(x), minor(x)))
 #define	VNDLABELDEV(dev)	\
 	makedev(major(dev), DISKMINOR(vndunit(dev), RAW_PART))
 
