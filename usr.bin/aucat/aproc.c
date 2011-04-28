0af56f5b055c66dd0e361e61f7dff854d9d8a148
ratchov <ratchov>
2011-04-28 07:20:03 +0000

    remove deadcode, use err(1, "%s", str) instead of err(1, str) from Michael W. Bombardieri <mb at ii.net>, thanks!

--- usr.bin/aucat/aproc.c
+++ usr.bin/aucat/aproc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: aproc.c,v 1.62 2010/10/21 21:42:46 ratchov Exp $	*/
+/*	$OpenBSD: aproc.c,v 1.63 2010/11/04 17:55:28 ratchov Exp $	*/
 /*
  * Copyright (c) 2008 Alexandre Ratchov <alex@caoua.org>
  *
@@ -158,7 +158,7 @@ aproc_new(struct aproc_ops *ops, char *name)
 
 	p = malloc(sizeof(struct aproc));
 	if (p == NULL)
-		err(1, name);
+		err(1, "%s", name);
 	LIST_INIT(&p->ins);
 	LIST_INIT(&p->outs);
 	p->name = name;
