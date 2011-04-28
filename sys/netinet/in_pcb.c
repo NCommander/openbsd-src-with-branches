c04f280652620f7ede9cd011d78f150d52fc130f
claudio <claudio>
2011-04-28 07:10:03 +0000

    Allow SOCK_DGRAM sockets to be bound to the local network broadcast addr. These sockets will only see broadcast sent to that particular address. Some applications like samba use this feature. Problem found and initial version by sthen@, OK sthen@, henning@

--- sys/netinet/in_pcb.c
+++ sys/netinet/in_pcb.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: in_pcb.c,v 1.116 2011/04/19 03:47:29 dlg Exp $	*/
+/*	$OpenBSD: in_pcb.c,v 1.117 2011/04/24 19:36:54 bluhm Exp $	*/
 /*	$NetBSD: in_pcb.c,v 1.25 1996/02/13 23:41:53 christos Exp $	*/
 
 /*
@@ -278,7 +278,10 @@ in_pcbbind(v, nam, p)
 			if (!(so->so_options & SO_BINDANY) &&
 			    in_iawithaddr(sin->sin_addr,
 			    inp->inp_rtableid) == NULL)
-				return (EADDRNOTAVAIL);
+				/* SOCK_RAW does not use in_pcbbind() */
+				if (!(so->so_type == SOCK_DGRAM &&
+				    in_broadcast(sin->sin_addr, NULL)))
+					return (EADDRNOTAVAIL);
 		}
 		if (lport) {
 			struct inpcb *t;
