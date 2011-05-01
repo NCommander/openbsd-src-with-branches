e5ab05a463aee2e919a1243bd8dab72d6125ff0e
miod <miod>
2011-05-01 07:01:37 +0000

    Be sure to cast physmem to a 64-bit type before passing it to ptoa().

--- sys/arch/mips64/mips64/mem.c
+++ sys/arch/mips64/mips64/mem.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mem.c,v 1.15 2010/01/22 21:45:25 miod Exp $	*/
+/*	$OpenBSD: mem.c,v 1.16 2010/06/26 23:24:43 guenther Exp $	*/
 /*	$NetBSD: mem.c,v 1.6 1995/04/10 11:55:03 mycroft Exp $	*/
 
 /*
@@ -115,7 +115,7 @@ mmrw(dev_t dev, struct uio *uio, int flags)
 		case 0:
 			v = uio->uio_offset;
 			c = iov->iov_len;
-			if (v + c > ptoa(physmem))
+			if (v + c > ptoa((psize_t)physmem))
 				return (EFAULT);
 			v = (vaddr_t)PHYS_TO_XKPHYS(v, CCA_NONCOHERENT);
 			error = uiomove((caddr_t)v, c, uio);
