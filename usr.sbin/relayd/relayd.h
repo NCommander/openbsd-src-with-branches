2fcd4b3ec7267624b3bf321d889d0f8ee8eaf4d2
phessler <phessler>
2011-05-05 10:20:24 +0000

    Allow a user to specify the route priority
    OK reyk@ claudio@ sthen@

--- usr.sbin/relayd/relayd.h
+++ usr.sbin/relayd/relayd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: relayd.h,v 1.143 2011/04/12 12:37:22 reyk Exp $	*/
+/*	$OpenBSD: relayd.h,v 1.144 2011/04/24 10:07:43 bluhm Exp $	*/
 
 /*
  * Copyright (c) 2006, 2007 Pierre-Yves Ritschard <pyr@openbsd.org>
@@ -273,6 +273,7 @@ struct host_config {
 	char			 name[MAXHOSTNAMELEN];
 	struct sockaddr_storage	 ss;
 	int			 ttl;
+	int			 priority;
 };
 
 struct host {
