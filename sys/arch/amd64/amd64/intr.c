d11e74c046948e94dc77d5806883478f4a65aa10
kettenis <kettenis>
2011-05-28 21:10:40 +0000

    Remove out of data XXX comment.

--- sys/arch/amd64/amd64/intr.c
+++ sys/arch/amd64/amd64/intr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: intr.c,v 1.26 2010/12/27 20:22:23 guenther Exp $	*/
+/*	$OpenBSD: intr.c,v 1.27 2011/04/16 00:40:56 deraadt Exp $	*/
 /*	$NetBSD: intr.c,v 1.3 2003/03/03 22:16:20 fvdl Exp $	*/
 
 /*
@@ -171,10 +171,6 @@ intr_calculatemasks(struct cpu_info *ci)
 		ci->ci_iunmask[level] = ~ci->ci_imask[level];
 }
 
-
-/*
- * XXX if defined(MULTIPROCESSOR) && .. ?
- */
 #if NIOAPIC > 0
 int
 intr_find_mpmapping(int bus, int pin, int *handle)
