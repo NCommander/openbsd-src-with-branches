9c02ead5fd13460a3789690f5526592d3f78c1d1
kevlo <kevlo>
2011-05-28 08:31:51 +0000

    Sync MCR0_BROADCAST register naming from FreeBSD now that its known the register is to disable broadcast instead of enabling broadcast packets.
    From Brad

--- sys/dev/pci/if_vte.c
+++ sys/dev/pci/if_vte.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_vte.c,v 1.3 2011/01/21 06:34:04 kevlo Exp $	*/
+/*	$OpenBSD: if_vte.c,v 1.4 2011/03/14 03:01:19 kevlo Exp $	*/
 /*-
  * Copyright (c) 2010, Pyun YongHyeon <yongari@FreeBSD.org>
  * All rights reserved.
@@ -1457,8 +1457,7 @@ vte_iff(struct vte_softc *sc)
 	}
 
 	mcr = CSR_READ_2(sc, VTE_MCR0);
-	/* Always accept broadcast frames so MCR0_BROADCAST bit must be 0 */
-	mcr &= ~(MCR0_PROMISC | MCR0_BROADCAST | MCR0_MULTICAST);
+	mcr &= ~(MCR0_PROMISC | MCR0_BROADCAST_DIS | MCR0_MULTICAST);
 	ifp->if_flags &= ~IFF_ALLMULTI;
 
 	if (ifp->if_flags & IFF_PROMISC || ac->ac_multirangecnt > 0) {
