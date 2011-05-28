2746fe87ba262f509e302db81009ee3cc3e7f669
weerd <weerd>
2011-05-28 12:51:40 +0000

    Do not allow traffic to be sent with a destination address in 0/8; this is not allowed according to Stevens and RFCs 5735 and 1122.
    Suggestion to use ENETUNREACH from claudio.
    
    OK phessler@, claudio@

--- sys/netinet/ip_output.c
+++ sys/netinet/ip_output.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_output.c,v 1.219 2011/04/28 09:56:27 claudio Exp $	*/
+/*	$OpenBSD: ip_output.c,v 1.220 2011/05/02 13:48:38 mikeb Exp $	*/
 /*	$NetBSD: ip_output.c,v 1.28 1996/02/13 23:43:07 christos Exp $	*/
 
 /*
@@ -160,6 +160,15 @@ ip_output(struct mbuf *m0, ...)
 		ipstat.ips_localout++;
 	} else {
 		hlen = ip->ip_hl << 2;
+	}
+
+	/*
+	 * We should not send traffic to 0/8 say both Stevens and RFCs
+	 * 5735 section 3 and 1122 sections 3.2.1.3 and 3.3.6.
+	 */
+	if ((ntohl(ip->ip_dst.s_addr) >> IN_CLASSA_NSHIFT) == 0) {
+		error = ENETUNREACH;
+		goto bad;
 	}
 
 	/*
