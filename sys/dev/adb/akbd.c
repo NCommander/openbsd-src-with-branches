5ac70ed3bb81ad4d49b1e766e9046bf4e3fdb23c
shadchin <shadchin>
2011-06-13 16:29:11 +0000

    Not need define KEYBOARD_ARRAY (keyboard array used only in dev/adb/akbd.c if defined WSDISPLAY_COMPAT_RAWKBD). Move keyboard array from dev/adb/keyboard.h to dev/adb/akbdmap.h.
    ok miod@

--- sys/dev/adb/akbd.c
+++ sys/dev/adb/akbd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: akbd.c,v 1.7 2009/01/21 21:53:59 grange Exp $	*/
+/*	$OpenBSD: akbd.c,v 1.8 2010/05/23 15:09:38 deraadt Exp $	*/
 /*	$NetBSD: akbd.c,v 1.17 2005/01/15 16:00:59 chs Exp $	*/
 
 /*
@@ -48,10 +48,6 @@
 #include <dev/adb/adb.h>
 #include <dev/adb/akbdmap.h>
 #include <dev/adb/akbdvar.h>
-
-#ifdef WSDISPLAY_COMPAT_RAWKBD
-#define KEYBOARD_ARRAY
-#endif
 #include <dev/adb/keyboard.h>
 
 /*
