546947de91287ee5ef060b05c29cb1e16941c8e9
claudio <claudio>
2011-06-18 21:19:44 +0000

    Use MSI interrupts when available. Tested by deraadt@ and jsg@. Removing a leftover M from my tree makes also my machine happy.

--- sys/dev/pci/if_ix.c
+++ sys/dev/pci/if_ix.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ix.c,v 1.52 2011/06/10 12:46:35 claudio Exp $	*/
+/*	$OpenBSD: if_ix.c,v 1.53 2011/06/15 00:03:00 dlg Exp $	*/
 
 /******************************************************************************
 
@@ -1532,7 +1532,7 @@ ixgbe_allocate_legacy(struct ix_softc *sc)
 	pci_intr_handle_t	ih;
 
 	/* We allocate a single interrupt resource */
-	if (/* pci_intr_map_msi(pa, &ih) != 0 && */
+	if (pci_intr_map_msi(pa, &ih) != 0 &&
 	    pci_intr_map(pa, &ih) != 0) {
 		printf(": couldn't map interrupt\n");
 		return (ENXIO);
