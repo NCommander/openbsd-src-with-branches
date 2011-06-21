ed2cc80986e4611f3d85e6ecc0f5f8aab1f2b6e9
jasper <jasper>
2011-06-21 07:18:35 +0000

    - adjust add_property() to allow more liberal whitespace usage.
    from simon@, tested in a bulk by landry@
    ok espie@

--- usr.sbin/pkg_add/OpenBSD/PkgConfig.pm
+++ usr.sbin/pkg_add/OpenBSD/PkgConfig.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD$
+# $OpenBSD: PkgConfig.pm,v 1.18 2010/12/24 09:04:14 espie Exp $
 #
 # Copyright (c) 2006 Marc Espie <espie@openbsd.org>
 #
@@ -73,7 +73,7 @@ sub add_property
 		if (defined $parse->{$name}) {
 			$v = $parse->{$name}($value);
 		} else {
-			$v = [split /\s+/o, $value] ;
+			$v = [split /(?<!\\)\s+/o, $value];
 		}
 	} else {
 		$v = [];
