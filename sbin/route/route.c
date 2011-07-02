2a4f04a1031df1ba4eeddd214a662e27942dacf3
claudio <claudio>
2011-07-02 22:00:54 +0000

    -interface is an alias of -iface so make sure route(8) accepts both flags in the monitor case. Brought up on the mailing list some long time ago.

--- sbin/route/route.c
+++ sbin/route/route.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: route.c,v 1.152 2010/10/25 19:39:55 deraadt Exp $	*/
+/*	$OpenBSD: route.c,v 1.153 2011/04/25 11:14:37 sthen Exp $	*/
 /*	$NetBSD: route.c,v 1.16 1996/04/15 18:27:05 cgd Exp $	*/
 
 /*
@@ -1035,6 +1035,7 @@ monitor(int argc, char *argv[])
 				af = AF_INET6;
 				break;
 			case K_IFACE:
+			case K_INTERFACE:
 				filter = ROUTE_FILTER(RTM_IFINFO) |
 				    ROUTE_FILTER(RTM_IFANNOUNCE);
 				break;
