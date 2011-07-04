bb43e31316ff2f32a13a61cece1096cd2c251230
nicm <nicm>
2011-07-04 21:34:54 +0000

    Nuke a load of trailing whitespace; no binary change.

--- usr.bin/cap_mkdb/cap_mkdb.c
+++ usr.bin/cap_mkdb/cap_mkdb.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cap_mkdb.c,v 1.16 2009/10/27 23:59:36 deraadt Exp $	*/
+/*	$OpenBSD: cap_mkdb.c,v 1.17 2009/11/01 23:16:39 nicm Exp $	*/
 /*	$NetBSD: cap_mkdb.c,v 1.5 1995/09/02 05:47:12 jtc Exp $	*/
 
 /*-
@@ -289,10 +289,10 @@ db_build(char **ifiles)
 				 * spaces, it is a description rather than an
 				 * alias, so skip it and break.
 				 */
-				if (*p != '|' && 
+				if (*p != '|' &&
 				    memchr(key.data, ' ', key.size) != NULL)
 					break;
-				
+
 				switch(capdbp->put(capdbp,
 				    &key, &data, R_NOOVERWRITE)) {
 				case -1:
