a4da7faa69cb62274a81e43a9a3e65d48f5f7334
henning <henning>
2011-07-05 20:03:23 +0000

    IFQ_IS_EMPTY instead of fiddling with ifqueue internals isn't it awesome that 5 out of 6 cases of this crap were in the various ppp implementations? ok claudio ryan the 6th to be fixed when we return from MEC

--- sys/net/ppp_tty.c
+++ sys/net/ppp_tty.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ppp_tty.c,v 1.19 2003/09/23 16:51:13 millert Exp $	*/
+/*	$OpenBSD: ppp_tty.c,v 1.20 2003/12/10 07:22:42 itojun Exp $	*/
 /*	$NetBSD: ppp_tty.c,v 1.12 1997/03/24 21:23:10 christos Exp $	*/
 
 /*
@@ -311,7 +311,7 @@ pppread(tp, uio, flag)
 	    splx(s);
 	    return 0;
 	}
-	if (sc->sc_inq.ifq_head != NULL)
+	if (!IFQ_IS_EMPTY(sc->sc_inq))
 	    break;
 	if ((tp->t_state & TS_CARR_ON) == 0 && (tp->t_cflag & CLOCAL) == 0
 	    && (tp->t_state & TS_ISOPEN)) {
