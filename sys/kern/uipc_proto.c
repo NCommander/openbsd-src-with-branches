fae22ab653ccf353cb14426a4db6963ab8d5d966
matthew <matthew>
2011-07-06 06:31:38 +0000

    Implement SOCK_SEQPACKET for UNIX sockets.
    ok claudio@

--- sys/kern/uipc_proto.c
+++ sys/kern/uipc_proto.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uipc_proto.c,v 1.4 2003/06/02 23:28:07 millert Exp $	*/
+/*	$OpenBSD: uipc_proto.c,v 1.5 2011/04/05 19:54:36 jasper Exp $	*/
 /*	$NetBSD: uipc_proto.c,v 1.8 1996/02/13 21:10:47 christos Exp $	*/
 
 /*-
@@ -51,6 +51,11 @@ extern	struct domain unixdomain;		/* or at least forward */
 
 struct protosw unixsw[] = {
 { SOCK_STREAM,	&unixdomain,	PF_LOCAL,	PR_CONNREQUIRED|PR_WANTRCVD|PR_RIGHTS,
+  0,		0,		0,		0,
+  uipc_usrreq,
+  0,		0,		0,		0,
+},
+{ SOCK_SEQPACKET,&unixdomain,	PF_LOCAL,	PR_ATOMIC|PR_CONNREQUIRED|PR_WANTRCVD|PR_RIGHTS,
   0,		0,		0,		0,
   uipc_usrreq,
   0,		0,		0,		0,
