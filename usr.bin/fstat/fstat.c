1caadb6984445b285c536bb5894fbec7e47b758c
henning <henning>
2011-07-09 00:45:40 +0000

    rmove rotten netatalk bits

--- usr.bin/fstat/fstat.c
+++ usr.bin/fstat/fstat.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fstat.c,v 1.69 2009/07/12 21:33:30 nicm Exp $	*/
+/*	$OpenBSD: fstat.c,v 1.70 2009/10/27 23:59:38 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2009 Todd C. Miller <Todd.Miller@courtesan.com>
@@ -693,12 +693,6 @@ socktrans(struct kinfo_file2 *kf)
 	case AF_NATM:
 		/* print protocol number and socket address */
 		printf("* natm %s", stype);
-		printf(" %d %p", kf->so_protocol,
-		    (void *)(uintptr_t)kf->f_data);
-		break;
-	case AF_APPLETALK:
-		/* print protocol number and socket address */
-		printf("* appletalk %s", stype);
 		printf(" %d %p", kf->so_protocol,
 		    (void *)(uintptr_t)kf->f_data);
 		break;
