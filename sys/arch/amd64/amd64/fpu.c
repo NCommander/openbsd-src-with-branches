1031ad7b753aa96d16ae8f149b737bd998de40a5
deraadt <deraadt>
2011-07-10 18:09:27 +0000

    remove irrelevant old comment

--- sys/arch/amd64/amd64/fpu.c
+++ sys/arch/amd64/amd64/fpu.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fpu.c,v 1.23 2011/07/06 21:41:37 art Exp $	*/
+/*	$OpenBSD: fpu.c,v 1.24 2011/07/07 18:11:23 art Exp $	*/
 /*	$NetBSD: fpu.c,v 1.1 2003/04/26 18:39:28 fvdl Exp $	*/
 
 /*-
@@ -30,12 +30,6 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- *	@(#)npx.c	7.2 (Berkeley) 5/12/91
- */
-
-/*
- * XXXfvdl update copyright notice. this started out as a stripped isa/npx.c
  */
 
 #include <sys/param.h>
