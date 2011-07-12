a2ea086483815ce96f10976233bcb5de57e999a6
espie <espie>
2011-07-12 10:30:29 +0000

    new pkg_delete, very similar to pkg_add, support for -a

--- usr.sbin/pkg_add/OpenBSD/Delete.pm
+++ usr.sbin/pkg_add/OpenBSD/Delete.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: Delete.pm,v 1.113 2011/01/23 06:56:53 espie Exp $
+# $OpenBSD$
 #
 # Copyright (c) 2003-2007 Marc Espie <espie@openbsd.org>
 #
@@ -119,8 +119,6 @@ sub delete_package
 	$state->vstat->synchronize;
 
 	delete_plist($plist, $state);
-	$state->{done}++;
-	$state->progress->next($state->ntogo);
 }
 
 sub unregister_dependencies
