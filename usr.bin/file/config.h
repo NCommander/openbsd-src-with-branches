47bcd1e44e334b85475188a138fd6bfdb63f11bb
martynas <martynas>
2011-07-25 16:21:22 +0000

    - Use strtof instead of casting strtod back to float (apprentice.c), - Don't reimplement strndup (softmagic.c). OK millert@.

--- usr.bin/file/config.h
+++ usr.bin/file/config.h
@@ -4,7 +4,7 @@
  * but can still carefully import stuff from Christos' version.
  *
  * This file is in the public domain. Original Author Ian F. Darwin.
- * $OpenBSD: config.h,v 1.5 2009/06/13 11:14:19 miod Exp $
+ * $OpenBSD: config.h,v 1.6 2011/01/10 20:59:42 deraadt Exp $
  */
 
 /* header file issues. */
@@ -23,6 +23,8 @@
 #define HAVE_STRERROR
 #define HAVE_VSNPRINTF
 #define HAVE_SNPRINTF
+#define HAVE_STRNDUP
+#define HAVE_STRTOF
 
 /* Compiler issues */
 #define SIZEOF_LONG_LONG 8
