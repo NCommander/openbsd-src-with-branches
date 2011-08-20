edb27afe7bd2fc384b4346a1e80c41a810e62c77
sthen <sthen>
2011-08-20 11:16:09 +0000

    Decouple log_verbose() from log_init() so the verbose flag stays set with "ospfd -v" (previously only "-vd" worked). Feedback on earlier implementation from henning@ blambert@ claudio@, ok claudio@

--- usr.sbin/ospfd/log.c
+++ usr.sbin/ospfd/log.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: log.c,v 1.5 2006/03/09 16:58:40 claudio Exp $ */
+/*	$OpenBSD: log.c,v 1.6 2009/11/02 20:20:54 claudio Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -44,7 +44,6 @@ log_init(int n_debug)
 	extern char	*__progname;
 
 	debug = n_debug;
-	verbose = n_debug;
 
 	if (!debug)
 		openlog(__progname, LOG_PID | LOG_NDELAY, LOG_DAEMON);
