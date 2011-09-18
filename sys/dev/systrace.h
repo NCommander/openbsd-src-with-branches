9adc78292f91481c0009caab221a046e8ea2569a
matthew <matthew>
2011-09-18 23:24:14 +0000

    Add support for *at(2) system calls to systrace(1).
    ok deraadt@, sthen@, jasper@

--- sys/dev/systrace.h
+++ sys/dev/systrace.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: systrace.h,v 1.21 2008/11/09 05:13:53 deraadt Exp $	*/
+/*	$OpenBSD: systrace.h,v 1.22 2011/06/22 01:32:16 djm Exp $	*/
 /*
  * Copyright 2002 Niels Provos <provos@citi.umich.edu>
  * All rights reserved.
@@ -167,6 +167,11 @@ struct systrace_inject {
 	pid_t   stri_pid;
 };
 
+struct systrace_getcwd {
+	pid_t	strgd_pid;
+	int	strgd_atfd;
+};
+
 #define STRIOCCLONE		_IOR('s', 100, int)
 #define SYSTR_CLONE		STRIOCCLONE
 #define STRIOCATTACH		_IOW('s', 101, pid_t)
@@ -174,7 +179,7 @@ struct systrace_inject {
 #define STRIOCANSWER		_IOW('s', 103, struct systrace_answer)
 #define STRIOCIO		_IOWR('s', 104, struct systrace_io)
 #define STRIOCPOLICY		_IOWR('s', 105, struct systrace_policy)
-#define STRIOCGETCWD		_IOW('s', 106, pid_t)
+#define STRIOCGETCWD		_IOW('s', 106, struct systrace_getcwd)
 #define STRIOCRESCWD		_IO('s', 107)
 #define STRIOCREPORT		_IOW('s', 108, pid_t)
 #define STRIOCREPLACE		_IOW('s', 109, struct systrace_replace)
