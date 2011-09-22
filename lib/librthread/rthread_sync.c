286ef55527ae455aa016c1a3c381bd9c8e946e18
guenther <guenther>
2011-09-22 04:54:38 +0000

    Return the correct errno (EBUSY) when pthread_mutex_trylock() is called on an ERRORCHECK mutex that is locked by this thread.  Problem observed by uwe@

--- lib/librthread/rthread_sync.c
+++ lib/librthread/rthread_sync.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rthread_sync.c,v 1.22 2009/11/27 19:45:54 guenther Exp $ */
+/*	$OpenBSD: rthread_sync.c,v 1.23 2010/04/12 03:34:31 guenther Exp $ */
 /*
  * Copyright (c) 2004,2005 Ted Unangst <tedu@openbsd.org>
  * All Rights Reserved.
@@ -277,7 +277,7 @@ _rthread_mutex_lock(pthread_mutex_t *mutexp, int trywait)
 			return (0);
 		}
 		if (mutex->type == PTHREAD_MUTEX_ERRORCHECK)
-			return (EDEADLK);
+			return (trywait ? EBUSY : EDEADLK);
 	}
 	if (!_sem_wait((void *)&mutex->sem, trywait))
 		return (EBUSY);
