08621b063a6ba57088d975ff20c38c483ac00f3b
nicm <nicm>
2011-10-18 09:37:35 +0000

    Support K and M as well as k and m for sizes. From Thomas Pfaff.
    ok millert jmc

--- bin/dd/args.c
+++ bin/dd/args.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: args.c,v 1.17 2006/11/01 05:46:20 ray Exp $	*/
+/*	$OpenBSD: args.c,v 1.18 2009/10/27 23:59:21 deraadt Exp $	*/
 /*	$NetBSD: args.c,v 1.7 1996/03/01 01:18:58 jtc Exp $	*/
 
 /*-
@@ -341,6 +341,7 @@ get_bsz(char *val)
 		++expr;
 		break;
 	case 'k':
+	case 'K':
 		t = num;
 		num *= 1024;
 		if (t > num)
@@ -348,6 +349,7 @@ get_bsz(char *val)
 		++expr;
 		break;
 	case 'm':
+	case 'M':
 		t = num;
 		num *= 1048576;
 		if (t > num)
@@ -411,6 +413,7 @@ get_off(char *val)
 		++expr;
 		break;
 	case 'k':
+	case 'K':
 		t = num;
 		num *= 1024;
 		if (t > num)
@@ -418,6 +421,7 @@ get_off(char *val)
 		++expr;
 		break;
 	case 'm':
+	case 'M':
 		t = num;
 		num *= 1048576;
 		if (t > num)
