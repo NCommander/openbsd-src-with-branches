5a3384b923bcf5c1654faf86386227ac0a9ff921
deraadt <deraadt>
2011-11-10 23:14:13 +0000

    setting COM_FREQ here is very wrong

--- sys/arch/hppa64/include/param.h
+++ sys/arch/hppa64/include/param.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: param.h,v 1.14 2011/05/03 21:00:23 kettenis Exp $	*/
+/*	$OpenBSD: param.h,v 1.15 2011/09/08 03:40:32 guenther Exp $	*/
 
 /* 
  * Copyright (c) 1988-1994, The University of Utah and
@@ -82,9 +82,5 @@
 
 #define	btodb(x)	((x) >> DEV_BSHIFT)
 #define	dbtob(x)	((x) << DEV_BSHIFT)
-
-#ifndef _LOCORE
-#define	COM_FREQ	7372800
-#endif
 
 #define	__SWAP_BROKEN
