d76eaaca193678be0e584f57fa8f7016ec276ffe
jsing <jsing>
2011-12-28 16:19:52 +0000

    Cleanup the metadata initialisation process - have sr_meta_init() initialise both the volume and chunk metadata before the discipline specific sd_create() function is called. The sr_meta_init_complete() function is then called to complete the initialisation based on values provided by sd_create().

--- sys/dev/softraidvar.h
+++ sys/dev/softraidvar.h
@@ -465,6 +465,8 @@ struct sr_volume {
 	/* runtime data */
 	struct sr_chunk_head	sv_chunk_list;	/* linked list of all chunks */
 	struct sr_chunk		**sv_chunks;	/* array to same chunks */
+	int64_t			sv_chunk_minsz; /* Size of smallest chunk. */
+	int64_t			sv_chunk_maxsz; /* Size of largest chunk. */
 
 	/* sensors */
 	struct ksensor		sv_sensor;
