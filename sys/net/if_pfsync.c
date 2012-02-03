4e639fafe8c273a5a6b96ac2cb5b1ec46c953472
bluhm <bluhm>
2012-02-03 01:57:51 +0000

    The kernel did not compile without INET6.  Put some #ifdefs into pf to fix that. - add #ifdef INET6 in obvious places - af translation is only possible with both INET and INET6 - interleave #endif /* INET6 */ and closing brace correctly - it is not necessary to #ifdef function prototypes - do not compile af translate functions at all instead of empty stub,   then the linker will report inconsistencies - pf_poolmask() actually takes an sa_family_t not an u_int8_t argument No binary change for GENERIC compiled with -O2 and -UDIAGNOSTIC. reported by Olivier Cochard-Labbe; ok mikeb@ henning@

--- sys/net/if_pfsync.c
+++ sys/net/if_pfsync.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_pfsync.c,v 1.179 2011/12/01 20:43:03 mcbride Exp $	*/
+/*	$OpenBSD: if_pfsync.c,v 1.180 2012/01/16 10:28:02 mikeb Exp $	*/
 
 /*
  * Copyright (c) 2002 Michael Shalayeff
@@ -1852,8 +1852,8 @@ pfsync_undefer(struct pfsync_deferral *pd, int drop)
 		                ip6_output(pd->pd_m, NULL, NULL, 0,
 				    NULL, NULL, NULL);
 				break;
-			}
 #endif /* INET6 */
+			}
                 }
 	}
 
