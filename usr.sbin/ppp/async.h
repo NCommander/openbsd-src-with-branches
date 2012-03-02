fda4966c2ccbe71e648f263f84eb928cd18bc414
cvs-fast-export <cvs-fast-export>
2012-03-02 08:38:57 +0000

    Synthetic commit for incomplete tag NSD_3_2_10

--- usr.sbin/ppp/async.h
+++
@@ -1,8 +0,0 @@
-/*
- * $Id: async.h,v 1.1 1997/10/26 01:02:06 brian Exp $
- */
-
-extern void AsyncInit(void);
-extern void SetLinkParams(struct lcpstate *);
-extern void AsyncOutput(int, struct mbuf *, int);
-extern void AsyncInput(u_char *, int);
