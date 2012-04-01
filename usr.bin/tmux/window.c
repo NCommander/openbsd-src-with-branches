93653b414139646e200f516b3ccb7dd7c0cc8a4f
nicm <nicm>
2012-04-01 13:18:38 +0000

    Add a layout history which can be stepped through with select-layout -u and -U commands (bound to 'u' and 'U' by default).

--- usr.bin/tmux/window.c
+++ usr.bin/tmux/window.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: window.c,v 1.75 2012/03/20 11:01:00 nicm Exp $ */
+/* $OpenBSD: window.c,v 1.76 2012/03/20 14:06:44 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -292,6 +292,7 @@ window_create1(u_int sx, u_int sy)
 
 	w->lastlayout = -1;
 	w->layout_root = NULL;
+	TAILQ_INIT(&w->layout_list);
 
 	w->sx = sx;
 	w->sy = sy;
