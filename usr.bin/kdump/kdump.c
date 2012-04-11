596fd8e5ff4747a4a4519b0335c7dc7d382a4233
mikeb <mikeb>
2012-04-11 13:55:26 +0000

    guenther and kettenis say THREAD_PID_OFFSET shouldn't be subtracted

--- usr.bin/kdump/kdump.c
+++ usr.bin/kdump/kdump.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kdump.c,v 1.67 2012/04/10 20:39:37 mikeb Exp $	*/
+/*	$OpenBSD: kdump.c,v 1.68 2012/04/11 12:44:02 jmc Exp $	*/
 
 /*-
  * Copyright (c) 1988, 1993
@@ -373,7 +373,7 @@ dumpheader(struct ktr_header *kth)
 
 	(void)printf("%6ld", (long)kth->ktr_pid);
 	if (needtid)
-		(void)printf("/%-5ld", (long)kth->ktr_tid - THREAD_PID_OFFSET);
+		(void)printf("/%-7ld", (long)kth->ktr_tid);
 	(void)printf(" %-8.*s ", MAXCOMLEN, kth->ktr_comm);
 	if (timestamp) {
 		if (timestamp == 2) {
