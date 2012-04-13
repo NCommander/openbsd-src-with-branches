4a7c53a1011c177aa9ed5c57994e7b59ee11349a
deraadt <deraadt>
2012-04-13 19:16:11 +0000

    oops, wrong version of diff in previous

--- sys/kern/uipc_usrreq.c
+++ sys/kern/uipc_usrreq.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uipc_usrreq.c,v 1.57 2012/04/13 09:38:32 deraadt Exp $	*/
+/*	$OpenBSD: uipc_usrreq.c,v 1.58 2012/04/13 19:12:44 deraadt Exp $	*/
 /*	$NetBSD: uipc_usrreq.c,v 1.18 1996/02/09 19:00:50 christos Exp $	*/
 
 /*
@@ -649,8 +649,10 @@ unp_externalize(struct mbuf *rights, socklen_t controllen)
 		controllen = 0;
 	else
 		controllen -= CMSG_ALIGN(sizeof(struct cmsghdr));
-	if (nfds > controllen / sizeof(int))
-		return EMSGSIZE;
+	if (nfds > controllen / sizeof(int)) {
+		error = EMSGSIZE;
+		goto restart;
+	}
 
 	rp = (struct file **)CMSG_DATA(cm);
 
