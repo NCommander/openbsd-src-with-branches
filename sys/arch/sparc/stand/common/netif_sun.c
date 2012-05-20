0b89ef489e7cb6bbe60760da0c8eda1354b8f18c
deraadt <deraadt>
2012-05-20 14:57:07 +0000

    do not need to pull in <time.h>

--- sys/arch/sparc/stand/common/netif_sun.c
+++ sys/arch/sparc/stand/common/netif_sun.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: netif_sun.c,v 1.1 1997/09/17 10:46:18 downsj Exp $	*/
+/*	$OpenBSD: netif_sun.c,v 1.2 2011/04/14 18:27:49 miod Exp $	*/
 /*	$NetBSD: netif_sun.c,v 1.2 1995/09/18 21:31:48 pk Exp $	*/
 
 /*
@@ -40,7 +40,6 @@
 #include <sys/param.h>
 #include <sys/socket.h>
 #include <string.h>
-#include <time.h>
 
 #include <net/if.h>
 
