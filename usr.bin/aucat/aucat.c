b4ef6d6bf6685f08b6da827f1f1785d6ff5801d3
ratchov <ratchov>
2012-05-23 19:25:11 +0000

    Make the "default" string a valid device name that has the same effect as NULL has. This will (hopefully) simplify ports where the user passes the device string.

--- usr.bin/aucat/aucat.c
+++ usr.bin/aucat/aucat.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: aucat.c,v 1.133 2012/02/09 18:33:36 ratchov Exp $	*/
+/*	$OpenBSD: aucat.c,v 1.134 2012/04/11 06:05:43 ratchov Exp $	*/
 /*
  * Copyright (c) 2008 Alexandre Ratchov <alex@caoua.org>
  *
@@ -26,6 +26,7 @@
 #include <limits.h>
 #include <pwd.h>
 #include <signal.h>
+#include <sndio.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -349,7 +350,7 @@ mkdev(char *path, int mode, int bufsz, int round, int hold, int autovol)
 	} else {
 		if (dev_list)
 			return dev_list;
-		path = "default";
+		path = SIO_DEVANY;
 	}
 	if (!bufsz && !round) {
 		round = DEFAULT_ROUND;
--- usr.bin/aucat/siofile.c
+++ usr.bin/aucat/siofile.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: siofile.c,v 1.9 2011/11/20 22:54:51 ratchov Exp $	*/
+/*	$OpenBSD: siofile.c,v 1.10 2012/04/11 06:05:43 ratchov Exp $	*/
 /*
  * Copyright (c) 2008 Alexandre Ratchov <alex@caoua.org>
  *
@@ -209,22 +209,20 @@ siofile_new(struct fileops *ops, char *path, unsigned int *rmode,
     struct aparams *ipar, struct aparams *opar,
     unsigned int *bufsz, unsigned int *round)
 {
-	char *siopath;
 	struct sio_par par;
 	struct sio_hdl *hdl;
 	struct siofile *f;
 	unsigned int mode = *rmode;
 
-	siopath = (strcmp(path, "default") == 0) ? NULL : path;
-	hdl = sio_open(siopath, mode, 1);
+	hdl = sio_open(path, mode, 1);
 	if (hdl == NULL) {
 		if (mode != (SIO_PLAY | SIO_REC))
 			return NULL;
-		hdl = sio_open(siopath, SIO_PLAY, 1);
+		hdl = sio_open(path, SIO_PLAY, 1);
 		if (hdl != NULL)
 			mode = SIO_PLAY;
 		else {
-			hdl = sio_open(siopath, SIO_REC, 1);
+			hdl = sio_open(path, SIO_REC, 1);
 			if (hdl != NULL)
 				mode = SIO_REC;
 			else
