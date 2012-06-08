b97de7a3288bb14a770c7d8417341cf49bc54b2a
lum <lum>
2012-06-08 13:41:16 +0000

    Add checks for command line input (or the TOP environment var) ommitted from 1.78.  Now a $ top -U -username -U username command will behave the same as interactive mode. ok brynet@

--- usr.bin/top/top.c
+++ usr.bin/top/top.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: top.c,v 1.77 2012/04/20 16:36:11 pirofti Exp $	*/
+/*	$OpenBSD: top.c,v 1.78 2012/06/05 18:52:53 brynet Exp $	*/
 
 /*
  *  Top users/processes display for Unix
@@ -153,9 +153,13 @@ parseargs(int ac, char **av)
 				if ((ps.huid = userid(optarg+1)) == (uid_t)-1)
 					new_message(MT_delayed, "%s: unknown user",
 					    optarg);
+				else
+					ps.uid = (uid_t)-1;
 			} else if ((ps.uid = userid(optarg)) == (uid_t)-1)
 				new_message(MT_delayed, "%s: unknown user",
 				    optarg);
+			else
+				ps.huid = (uid_t)-1;
 			break;
 
 		case 'p': {	/* display only process id */
