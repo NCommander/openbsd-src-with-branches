3c0c347ccd6d6bbed74df72a7eb43038df912786
deraadt <deraadt>
2012-06-20 13:47:20 +0000

    With MP rthread setups, we cannot remove the X permission from the GOT entry momentarily, because another thread might attempt lazy resolve. ok matthew guenther

--- libexec/ld.so/hppa/rtld_machine.c
+++ libexec/ld.so/hppa/rtld_machine.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rtld_machine.c,v 1.22 2010/06/05 21:40:58 miod Exp $	*/
+/*	$OpenBSD: rtld_machine.c,v 1.23 2011/04/06 11:36:25 miod Exp $	*/
 
 /*
  * Copyright (c) 2004 Michael Shalayeff
@@ -460,7 +460,7 @@ _dl_bind(elf_object_t *object, int reloff)
 		_dl_thread_bind_lock(0, &savedmask);
 		/* mprotect the actual modified region, not the whole plt */
 		_dl_mprotect((void*)addr, sizeof (Elf_Addr) * 2,
-		    PROT_READ|PROT_WRITE);
+		    PROT_READ|PROT_WRITE|PROT_EXEC);
 	}
 
 	addr[0] = value;
