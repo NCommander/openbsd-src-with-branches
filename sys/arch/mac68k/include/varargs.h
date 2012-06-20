83c26637792fdeca1b7d055e3074336dde891ce3
matthew <matthew>
2012-06-20 18:23:52 +0000

    RIP mac68k.  No one loves you anymore.

--- sys/arch/mac68k/include/varargs.h
+++
@@ -1,9 +0,0 @@
-/*	$OpenBSD: varargs.h,v 1.3 1997/11/30 06:10:36 gene Exp $	*/
-/*	$NetBSD: varargs.h,v 1.2 1994/10/26 08:46:51 cgd Exp $	*/
-
-#ifndef _MACHINE_VARARGS_H_
-#define _MACHINE_VARARGS_H_
-
-#include <m68k/varargs.h>
-
-#endif
