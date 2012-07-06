912261c632a7b42daf576ff9c7f6b053f782d1fa
espie <espie>
2012-07-06 11:30:41 +0000

    add my copyright, since I'm going to do yet MORE changes...

--- usr.bin/libtool/LT/Exec.pm
+++ usr.bin/libtool/LT/Exec.pm
@@ -1,6 +1,7 @@
-# $OpenBSD: Exec.pm,v 1.1 2012/06/19 09:30:44 espie Exp $
+# $OpenBSD: Exec.pm,v 1.2 2012/07/04 12:39:34 espie Exp $
 
 # Copyright (c) 2007-2010 Steven Mestdagh <steven@openbsd.org>
+# Copyright (c) 2012 Marc Espie <espie@openbsd.org>
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
