fc4b133caa2c37c4613634b2f21b8073e1cfc6c4
nicm <nicm>
2012-08-27 21:35:11 +0000

    Change a log to fprintf that was missed last time around, from Tiago Cunha.

--- usr.bin/tmux/client.c
+++ usr.bin/tmux/client.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: client.c,v 1.57 2012/06/18 13:34:56 nicm Exp $ */
+/* $OpenBSD: client.c,v 1.58 2012/07/10 11:53:01 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -189,7 +189,7 @@ client_main(int argc, char **argv, int flags)
 		 * flag.
 		 */
 		if ((cmdlist = cmd_list_parse(argc, argv, &cause)) == NULL) {
-			log_warnx("%s", cause);
+			fprintf(stderr, "%s\n", cause);
 			return (1);
 		}
 		cmdflags &= ~CMD_STARTSERVER;
