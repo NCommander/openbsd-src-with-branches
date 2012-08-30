6ca8652bccd2da2677e6b34434db0f81ce941848
lum <lum>
2012-08-30 06:09:12 +0000

    This diff allows the user to decide what to do with buffers that experience write errors during C-x C-c (exiting mg). Emacs stops the exiting process when it encounters problem buffers and lets the user decide what to do, currently mg continues exiting and the contents of these buffers are lost. This diff bring mg more into line with emacs.
    Review and observations from Sunil Nimmagadda.

--- usr.bin/mg/main.c
+++ usr.bin/mg/main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: main.c,v 1.66 2012/05/29 05:52:46 lum Exp $	*/
+/*	$OpenBSD: main.c,v 1.67 2012/05/29 06:08:48 lum Exp $	*/
 
 /* This file is in the public domain. */
 
@@ -224,6 +224,8 @@ quit(int f, int n)
 
 	if ((s = anycb(FALSE)) == ABORT)
 		return (ABORT);
+	if (s == FIOERR)
+		return (FALSE);
 	if (s == FALSE
 	    || eyesno("Modified buffers exist; really exit") == TRUE) {
 		vttidy();
