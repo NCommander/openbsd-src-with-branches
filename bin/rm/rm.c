4eb7ff39e30e592c559d47f3eb43d9fb21465751
naddy <naddy>
2012-09-05 19:49:08 +0000

    overwrite with random stream; a repeating chunk of random data isn't random at all ok tedu@

--- bin/rm/rm.c
+++ bin/rm/rm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rm.c,v 1.25 2012/06/18 01:03:05 guenther Exp $	*/
+/*	$OpenBSD: rm.c,v 1.26 2012/09/04 22:22:50 tedu Exp $	*/
 /*	$NetBSD: rm.c,v 1.19 1995/09/07 06:48:50 jtc Exp $	*/
 
 /*-
@@ -329,9 +329,9 @@ pass(int fd, off_t len, char *buf, size_t bsize)
 {
 	size_t wlen;
 
-	arc4random_buf(buf, bsize);
 	for (; len > 0; len -= wlen) {
 		wlen = len < bsize ? len : bsize;
+		arc4random_buf(buf, wlen);
 		if (write(fd, buf, wlen) != wlen)
 			return (0);
 	}
