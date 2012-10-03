6d21017ce64a3f2e778c4241a3d77b703b5586c8
reyk <reyk>
2012-10-03 08:33:31 +0000

    Support more than one relay backup table.  Instead of duplicating the code for main and backup table all over the place, turn the relay tables into a list attached to the relay.  This improves the code and allows some other tricks with multiple tables later.

--- usr.sbin/relayd/relayd.c
+++ usr.sbin/relayd/relayd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: relayd.c,v 1.108 2012/05/08 15:10:15 benno Exp $	*/
+/*	$OpenBSD: relayd.c,v 1.109 2012/09/21 09:56:27 benno Exp $	*/
 
 /*
  * Copyright (c) 2007, 2008 Reyk Floeter <reyk@openbsd.org>
@@ -564,6 +564,7 @@ void
 purge_relay(struct relayd *env, struct relay *rlay)
 {
 	struct rsession		*con;
+	struct relay_table	*rlt;
 
 	/* shutdown and remove relay */
 	if (event_initialized(&rlay->rl_ev))
@@ -590,6 +591,11 @@ purge_relay(struct relayd *env, struct relay *rlay)
 		free(rlay->rl_ssl_key);
 	if (rlay->rl_ssl_ca != NULL)
 		free(rlay->rl_ssl_ca);
+
+	while ((rlt = TAILQ_FIRST(&rlay->rl_tables))) {
+		TAILQ_REMOVE(&rlay->rl_tables, rlt, rlt_entry);
+		free(rlt);
+	}
 
 	free(rlay);
 }
