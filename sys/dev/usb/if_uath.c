54fa6b3a87815d2e8a0928d3815f6215ec0cf16a
sthen <sthen>
2012-10-03 08:05:26 +0000

    attach to D-Link WUA-2340, from Aaron Wirtz on misc@

--- sys/dev/usb/if_uath.c
+++ sys/dev/usb/if_uath.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_uath.c,v 1.50 2011/04/07 15:30:16 miod Exp $	*/
+/*	$OpenBSD: if_uath.c,v 1.51 2011/07/03 15:47:17 matthew Exp $	*/
 
 /*-
  * Copyright (c) 2006
@@ -111,6 +111,7 @@ static const struct uath_type {
 	UATH_DEV_UX(DLINK,		DWLAG122),
 	UATH_DEV_UX(DLINK,		DWLAG132),	
 	UATH_DEV_UG(DLINK,		DWLG132),
+	UATH_DEV_UG(DLINK2,		WUA2340),
 	UATH_DEV_UG(GIGASET,		AR5523),
 	UATH_DEV_UG(GIGASET,		SMCWUSBTG),
 	UATH_DEV_UG(GLOBALSUN,		AR5523_1),
