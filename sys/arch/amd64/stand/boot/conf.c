18ba0516a8ff46b9da23df949311b1214768b26c
jsing <jsing>
2012-10-27 15:43:42 +0000

    Move the code for softraid boot into separate files, leaving only minimal hooks that are called from the normal code path. This allows softraid boot support to be completely enabled or disabled at compile time. For boot(8) all softraid boot support is now enabled by default, including support for booting from crypto volumes. Disable softraid boot support for fdboot, which further shrinks the binary.

--- sys/arch/amd64/stand/boot/conf.c
+++ sys/arch/amd64/stand/boot/conf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: conf.c,v 1.23 2012/06/03 13:18:33 kettenis Exp $	*/
+/*	$OpenBSD: conf.c,v 1.25 2012/10/12 15:00:33 jsing Exp $	*/
 
 /*
  * Copyright (c) 1996 Michael Shalayeff
@@ -42,7 +42,7 @@
 #include <biosdev.h>
 #include <dev/cons.h>
 
-const char version[] = "3.22";
+const char version[] = "3.23";
 int	debug = 1;
 
 
