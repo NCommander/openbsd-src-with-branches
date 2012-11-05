a67a901199e6bf95f52aa091493c64e0a82dfe27
miod <miod>
2012-11-05 13:20:16 +0000

    Switch sparc to timecounters. Heavily based on NetBSD.

--- sys/arch/sparc/include/_types.h
+++ sys/arch/sparc/include/_types.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: _types.h,v 1.16 2011/11/15 17:12:14 deraadt Exp $	*/
+/*	$OpenBSD: _types.h,v 1.17 2012/04/28 17:24:50 miod Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993
@@ -142,5 +142,8 @@ typedef int			__wint_t;
 typedef	int			__rune_t;
 typedef	void *			__wctrans_t;
 typedef	void *			__wctype_t;
+
+/* Feature test macros */
+#define __HAVE_TIMECOUNTER
 
 #endif	/* _MACHINE__TYPES_H_ */
--- sys/arch/sparc/sparc/machdep.c
+++ sys/arch/sparc/sparc/machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: machdep.c,v 1.141 2012/10/08 21:47:50 deraadt Exp $	*/
+/*	$OpenBSD: machdep.c,v 1.142 2012/10/22 17:28:27 kettenis Exp $	*/
 /*	$NetBSD: machdep.c,v 1.85 1997/09/12 08:55:02 pk Exp $ */
 
 /*
@@ -208,9 +208,6 @@ cpu_startup()
 	 * Set up buffers, so they can be used to read disk labels.
 	 */
 	bufinit();
-
-	/* Early interrupt handlers initialization */
-	intr_init();
 }
 
 /*
