25cd732052b2b3996088e1e7c45632df13cce2ce
eric <eric>
2012-11-12 14:58:53 +0000

    Cleanups and improvements:
    * Log more events (especially client session) and use a better scheme
      for that: each messages is prefixed with a token to easily identify
      its class:
        - info/warn/debug: general server messages
        - smtp-in: smtp client connections
        - relay: status update for relayed messages
        - delivery: status update for local deliveries
    
    * Implement "smtpctl monitor" to display updates of selected internal
      counters.
    
    * When reloading the on-disk queue at startup do not commit a message
      if no envelope was submitted for that message.
    
    * Remove unused stuff in the config parser.
    
    ok gilles@

--- usr.sbin/smtpd/queue_backend.c
+++ usr.sbin/smtpd/queue_backend.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: queue_backend.c,v 1.38 2012/09/01 16:09:14 gilles Exp $	*/
+/*	$OpenBSD: queue_backend.c,v 1.39 2012/10/09 13:39:00 eric Exp $	*/
 
 /*
  * Copyright (c) 2011 Gilles Chehade <gilles@openbsd.org>
@@ -287,7 +287,7 @@ queue_envelope_load(uint64_t evpid, struct envelope *ep)
 			ep->id = evpid;
 			return (1);
 		}
-		log_debug("invalid envelope %016" PRIx64 ": %s", ep->id, e);
+		log_debug("debug: invalid envelope %016" PRIx64 ": %s", ep->id, e);
 	}
 	return (0);
 }
--- usr.sbin/smtpd/ruleset.c
+++ usr.sbin/smtpd/ruleset.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ruleset.c,v 1.24 2012/10/11 21:14:32 gilles Exp $ */
+/*	$OpenBSD: ruleset.c,v 1.25 2012/10/13 08:01:47 eric Exp $ */
 
 /*
  * Copyright (c) 2009 Gilles Chehade <gilles@openbsd.org>
@@ -78,8 +78,7 @@ ruleset_match(const struct envelope *evp)
 
 			if (map->m_src == S_NONE) {
 				TAILQ_FOREACH(me, &map->m_contents, me_entry) {
-					if (hostname_match(maddr->domain,
-					    me->me_key.med_string))
+					if (hostname_match(maddr->domain, me->me_key))
 						return r;
 				}
 			}
@@ -143,12 +142,11 @@ ruleset_check_source(struct map *map, const struct sockaddr_storage *ss)
 	if (map->m_src == S_NONE) {
 		TAILQ_FOREACH(me, &map->m_contents, me_entry) {
 			if (ss->ss_family == AF_LOCAL) {
-				if (!strcmp(me->me_key.med_string, "local"))
+				if (!strcmp(me->me_key, "local"))
 					return 1;
 				continue;
 			}
-			if (ruleset_cmp_source(ss_to_text(ss),
-				me->me_key.med_string))
+			if (ruleset_cmp_source(ss_to_text(ss), me->me_key))
 				return 1;
 		}
 	}
