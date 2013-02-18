542905d1e6bfeed79a7fc7e8cc68fb8be0cba208
eric <eric>
2013-02-18 13:37:14 +0000

    missing comma in log message
    ok gilles@

--- usr.sbin/smtpd/mta.c
+++ usr.sbin/smtpd/mta.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mta.c,v 1.153 2013/02/05 11:45:18 gilles Exp $	*/
+/*	$OpenBSD: mta.c,v 1.154 2013/02/08 13:29:09 eric Exp $	*/
 
 /*
  * Copyright (c) 2008 Pierre-Yves Ritschard <pyr@openbsd.org>
@@ -1146,7 +1146,7 @@ mta_log(const struct mta_envelope *evp, const char *prefix, const char *relay,
 		snprintf(rcpt, sizeof rcpt, "rcpt=<%s>, ", evp->rcpt);
 
 	log_info("relay: %s for %016" PRIx64 ": from=<%s>, to=<%s>, "
-	    "%srelay=%s delay=%s, stat=%s",
+	    "%srelay=%s, delay=%s, stat=%s",
 	    prefix,
 	    evp->id,
 	    evp->task->sender,
