5fb9e0e3946315c6e66ee1f3d4609b105001a201
deraadt <deraadt>
2013-03-23 15:50:23 +0000

    avoid machine/param.h use

--- sys/arch/loongson/stand/boot/dev.c
+++ sys/arch/loongson/stand/boot/dev.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dev.c,v 1.2 2010/02/16 21:28:39 miod Exp $	*/
+/*	$OpenBSD: dev.c,v 1.3 2011/03/13 00:13:53 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2010 Miodrag Vallat.
@@ -44,10 +44,9 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <sys/types.h>
+#include <sys/param.h>
 #include "libsa.h"
 #include <sys/disklabel.h>
-#include <machine/param.h>
 #include <machine/cpu.h>
 #include <machine/pmon.h>
 
