d3ee3fc783cf21d5c90685dc2d78081e9d3e643d
martynas <martynas>
2013-03-28 17:30:45 +0000

    Add __strong_alias macros for all architectures to be able to alias another symbol without weak attribute.  To be used in libc and libm soon.  Agreed by kettenis@, guenther@, matthew@.

--- sys/arch/octeon/include/cdefs.h
+++ sys/arch/octeon/include/cdefs.h
@@ -1,8 +1,3 @@
-/* $OpenBSD: cdefs.h,v 1.1.1.1 2009/07/31 09:28:41 miod Exp $ */
+/* $OpenBSD: cdefs.h,v 1.1 2010/09/20 06:32:30 syuu Exp $ */
 /* public domain */
 #include <mips64/cdefs.h>
-
-#if defined(lint) && !defined(__MIPSEB__)
-#define __MIPSEB__
-#undef __MIPSEL__
-#endif
