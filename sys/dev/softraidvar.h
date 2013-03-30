ea30ad13034ce1414eedcce897a385c8be162df9
jsing <jsing>
2013-03-30 02:02:14 +0000

    Provide a default discipline interrupt handling function and migrate all of the disciplines that now have the same interrupt code.
    ok krw@

--- sys/dev/softraidvar.h
+++ sys/dev/softraidvar.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: softraidvar.h,v 1.129 2013/03/25 16:01:49 jsing Exp $ */
+/* $OpenBSD: softraidvar.h,v 1.130 2013/03/29 12:00:59 jsing Exp $ */
 /*
  * Copyright (c) 2006 Marco Peereboom <marco@peereboom.us>
  * Copyright (c) 2008 Chris Kuethe <ckuethe@openbsd.org>
@@ -697,6 +697,7 @@ int			sr_raid_tur(struct sr_workunit *);
 int			sr_raid_request_sense( struct sr_workunit *);
 int			sr_raid_start_stop(struct sr_workunit *);
 int			sr_raid_sync(struct sr_workunit *);
+void			sr_raid_intr(struct buf *);
 void			sr_raid_startwu(struct sr_workunit *);
 void			sr_raid_recreate_wu(struct sr_workunit *);
 
