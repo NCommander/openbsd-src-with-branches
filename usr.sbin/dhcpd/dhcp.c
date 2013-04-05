393157c046f83d1e3137879189c9472e4ebe464f
krw <krw>
2013-04-05 19:31:36 +0000

    send_packet() and writev() return ssize_t, not int. Use correct type to store the returned value. From dhill.

--- usr.sbin/dhcpd/dhcp.c
+++ usr.sbin/dhcpd/dhcp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dhcp.c,v 1.34 2013/03/11 15:43:38 krw Exp $ */
+/*	$OpenBSD: dhcp.c,v 1.35 2013/03/22 23:45:52 krw Exp $ */
 
 /*
  * Copyright (c) 1995, 1996, 1997, 1998, 1999
@@ -557,7 +557,8 @@ nak_lease(struct packet *packet, struct iaddr *cip)
 {
 	struct sockaddr_in to;
 	struct in_addr from;
-	int result, i;
+	ssize_t result;
+	int i;
 	struct dhcp_packet raw;
 	unsigned char nak = DHCPNAK;
 	struct packet outgoing;
