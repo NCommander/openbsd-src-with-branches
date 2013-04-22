593b86f481ea488072abc19af62bfeb35e624d06
espie <espie>
2013-04-22 07:21:52 +0000

    unifdef non full TIMESPEC code

--- usr.bin/make/timestamp.c
+++ usr.bin/make/timestamp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: timestamp.c,v 1.6 2010/07/19 19:46:44 espie Exp $ */
+/*	$OpenBSD: timestamp.c,v 1.7 2011/12/10 04:12:36 guenther Exp $ */
 
 /*
  * Copyright (c) 2001 Marc Espie.
@@ -28,25 +28,13 @@
 #include "defines.h"
 #include "timestamp.h"
 
-#ifndef USE_TIMESPEC
-#include <sys/types.h>
-#include <utime.h>
-#endif
 
 TIMESTAMP now;		/* The time at the start of this whole process */
 
 int
 set_times(const char *f)
 {
-#ifdef USE_TIMESPEC
     return utimes(f, NULL);
-#else
-    struct utimbuf times;
-
-    time(&times.actime);
-    times.modtime = times.actime;
-    return utime(f, &times);
-#endif
 }
 
 char *
