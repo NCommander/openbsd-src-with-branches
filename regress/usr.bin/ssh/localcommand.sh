19baae8225f1f55111b4ae24ddaed51985564923
dtucker <dtucker>
2013-05-17 10:24:48 +0000

    use backticks for portability

--- regress/usr.bin/ssh/localcommand.sh
+++ regress/usr.bin/ssh/localcommand.sh
@@ -1,4 +1,4 @@
-#	$OpenBSD: try-ciphers.sh,v 1.11 2007/06/07 19:41:46 pvalchev Exp $
+#	$OpenBSD: localcommand.sh,v 1.1 2007/10/29 06:57:13 dtucker Exp $
 #	Placed in the Public Domain.
 
 tid="localcommand"
@@ -8,7 +8,7 @@ echo 'LocalCommand echo foo' >> $OBJ/ssh_proxy
 
 for p in 1 2; do
 	verbose "test $tid: proto $p localcommand"
-	a=$(${SSH} -F $OBJ/ssh_proxy -$p somehost true)
+	a=`${SSH} -F $OBJ/ssh_proxy -$p somehost true`
 	if [ "$a" != "foo" ] ; then
 		fail "$tid proto $p"
 	fi
