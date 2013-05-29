5e1f558e60fc54dbd91f35f3c944c6666b7015b2
tedu <tedu>
2013-05-29 22:23:02 +0000

    uvm_loan has not (ever) been compiled or used.

--- sys/uvm/uvm.h
+++ sys/uvm/uvm.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm.h,v 1.46 2011/07/06 19:50:38 beck Exp $	*/
+/*	$OpenBSD: uvm.h,v 1.47 2012/03/09 13:01:29 ariane Exp $	*/
 /*	$NetBSD: uvm.h,v 1.24 2000/11/27 08:40:02 chs Exp $	*/
 
 /*
@@ -49,7 +49,6 @@
 #include <uvm/uvm_fault.h>
 #include <uvm/uvm_glue.h>
 #include <uvm/uvm_km.h>
-#include <uvm/uvm_loan.h>
 #include <uvm/uvm_map.h>
 #include <uvm/uvm_object.h>
 #include <uvm/uvm_page.h>
