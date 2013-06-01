7cee7509df1ed2c500067c23ec0f0f48096035e0
claudio <claudio>
2013-06-01 19:28:55 +0000

    The keepalive timeout should be configured on a global basis and not per interface. Remove the iface pointer from the 'nbr' structure because it's not needed anymore. Diff from Renato Westphal

--- usr.sbin/ldpd/printconf.c
+++ usr.sbin/ldpd/printconf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: printconf.c,v 1.3 2010/05/25 13:29:45 claudio Exp $ */
+/*	$OpenBSD: printconf.c,v 1.4 2013/06/01 18:35:02 claudio Exp $ */
 
 /*
  * Copyright (c) 2004, 2005, 2008 Esben Norby <norby@openbsd.org>
@@ -50,6 +50,8 @@ print_mainconf(struct ldpd_conf *conf)
 		printf("advertisement ondemand\n");
 	else
 		printf("advertisement unsolicited\n");
+
+	printf("keepalive %u\n", conf->keepalive);
 }
 
 void
