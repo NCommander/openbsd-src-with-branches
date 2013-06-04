ac97116287075af84b97b4dc31113e07891c16e0
claudio <claudio>
2013-06-04 02:34:48 +0000

    Always accept TCP connection requests and identify to which neighbor it belongs only _after_ receiving an Initialization message containing the information we need. Before an Initialization message is received, the TCP connection shouldn't be associated with any neighbor/adjacency. Therefor refactor that part into a own module. From Renato Westphal

--- usr.sbin/ldpd/keepalive.c
+++ usr.sbin/ldpd/keepalive.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: keepalive.c,v 1.8 2013/06/01 18:35:02 claudio Exp $ */
+/*	$OpenBSD: keepalive.c,v 1.9 2013/06/01 18:47:07 claudio Exp $ */
 
 /*
  * Copyright (c) 2009 Michele Marchetto <michele@openbsd.org>
@@ -54,7 +54,7 @@ send_keepalive(struct nbr *nbr)
 
 	gen_msg_tlv(buf, MSG_TYPE_KEEPALIVE, size);
 
-	evbuf_enqueue(&nbr->wbuf, buf);
+	evbuf_enqueue(&nbr->tcp->wbuf, buf);
 }
 
 int
