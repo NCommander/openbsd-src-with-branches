7940923f12a562da98415f36b1ac0cb13e7d9c88
sthen <sthen>
2013-06-11 19:18:02 +0000

    revert r1.156 "Add support for focus notifications when tmux pane changes" beck@ found annoying beeps if a machine was shutdown while tmux is running and you then focus in/out of an xterm; kettenis tracked it down to 1.156.

--- usr.bin/tmux/tty.c
+++ usr.bin/tmux/tty.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: tty.c,v 1.159 2013/04/11 07:27:27 nicm Exp $ */
+/* $OpenBSD: tty.c,v 1.156 2013/03/24 09:18:16 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -220,7 +220,7 @@ tty_start_tty(struct tty *tty)
 		tty_puts(tty, "\033[?1000l\033[?1006l\033[?1005l");
 
 	if (tty_term_has(tty->term, TTYC_XT))
-		tty_puts(tty, "\033[c\033[>4;1m\033[?1004h\033[m");
+		tty_puts(tty, "\033[c\033[>4;1m\033[m");
 
 	tty->cx = UINT_MAX;
 	tty->cy = UINT_MAX;
@@ -283,7 +283,7 @@ tty_stop_tty(struct tty *tty)
 		tty_raw(tty, "\033[?1000l\033[?1006l\033[?1005l");
 
 	if (tty_term_has(tty->term, TTYC_XT))
-		tty_raw(tty, "\033[>4m\033[?1004l\033[m");
+		tty_raw(tty, "\033[>4m\033[m");
 
 	tty_raw(tty, tty_term_string(tty->term, TTYC_RMCUP));
 
