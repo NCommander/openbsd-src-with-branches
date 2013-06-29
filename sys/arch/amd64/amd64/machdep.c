7a95b0c876bae71a74ae6669c4d71725799783ab
brad <brad>
2013-06-29 21:06:15 +0000

    Remove unsigned comparison >= 0.
    Pointed out by LLVM.
    
    ok matthew@ kettenis@

--- sys/arch/amd64/amd64/machdep.c
+++ sys/arch/amd64/amd64/machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: machdep.c,v 1.163 2013/06/02 16:38:05 guenther Exp $	*/
+/*	$OpenBSD: machdep.c,v 1.164 2013/06/11 16:42:06 deraadt Exp $	*/
 /*	$NetBSD: machdep.c,v 1.3 2003/05/07 22:58:18 fvdl Exp $	*/
 
 /*-
@@ -1675,7 +1675,7 @@ amd64_pa_used(paddr_t addr)
 		return 1;
 
 	/* Low memory used for various bootstrap things */
-	if (addr >= 0 && addr < avail_start)
+	if (addr < avail_start)
 		return 1;
 
 	/*
