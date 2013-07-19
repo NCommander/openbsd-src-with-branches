91cfb3c9f7e647fbdcb4a9a94ba7b5107440777e
eric <eric>
2013-07-19 21:14:52 +0000

    Many MTA improvements:
    - Better transient error handling logic: failing destinations are
      automatically disabled for a while.  When a destination is active
      again, ask the scheduler to retry previous envelopes immediatly.
    - More informative error report when all routes fail for a mail.
    - Implement a "smtpctl show hoststats" command to get the latest stat
      message per MX domain.
    - Implement a "smtpctl show routes" command to show the state the
      currently known routes to remote MXs.
    - Implement a "smtpctl resume route" command to re-enable a route that
      has been disabled.
    - Do not hardcode limits
    - Minor code improvements

--- usr.sbin/smtpd/control.c
+++ usr.sbin/smtpd/control.c
@@ -116,6 +116,22 @@ control_imsg(struct mproc *p, struct imsg *imsg)
 			return;
 		}
 	}
+	if (p->proc == PROC_MTA) {
+		switch (imsg->hdr.type) {
+		case IMSG_CTL_MTA_SHOW_ROUTES:
+			c = tree_get(&ctl_conns, imsg->hdr.peerid);
+			if (c == NULL)
+				return;
+			m_forward(&c->mproc, imsg);
+			return;
+		case IMSG_CTL_MTA_SHOW_HOSTSTATS:
+			c = tree_get(&ctl_conns, imsg->hdr.peerid);
+			if (c == NULL)
+				return;
+			m_forward(&c->mproc, imsg);
+			return;
+		}
+	}
 
 	switch (imsg->hdr.type) {
 	case IMSG_STAT_INCREMENT:
@@ -659,6 +675,15 @@ control_dispatch_ext(struct mproc *p, struct imsg *imsg)
 		m_compose(p, IMSG_CTL_OK, 0, 0, -1, NULL, 0);
 		return;
 
+	case IMSG_CTL_RESUME_ROUTE:
+		if (c->euid)
+			goto badcred;
+
+		log_info("info: route resumed");
+		m_forward(p_mta, imsg);
+		m_compose(p, IMSG_CTL_OK, 0, 0, -1, NULL, 0);
+		return;
+
 	case IMSG_CTL_LIST_MESSAGES:
 		if (c->euid)
 			goto badcred;
@@ -670,6 +695,20 @@ control_dispatch_ext(struct mproc *p, struct imsg *imsg)
 		if (c->euid)
 			goto badcred;
 		m_compose(p_scheduler, IMSG_CTL_LIST_ENVELOPES, c->id, 0, -1,
+		    imsg->data, imsg->hdr.len - sizeof(imsg->hdr));
+		return;
+
+	case IMSG_CTL_MTA_SHOW_ROUTES:
+		if (c->euid)
+			goto badcred;
+		m_compose(p_mta, IMSG_CTL_MTA_SHOW_ROUTES, c->id, 0, -1,
+		    imsg->data, imsg->hdr.len - sizeof(imsg->hdr));
+		return;
+
+	case IMSG_CTL_MTA_SHOW_HOSTSTATS:
+		if (c->euid)
+			goto badcred;
+		m_compose(p_mta, IMSG_CTL_MTA_SHOW_HOSTSTATS, c->id, 0, -1,
 		    imsg->data, imsg->hdr.len - sizeof(imsg->hdr));
 		return;
 
