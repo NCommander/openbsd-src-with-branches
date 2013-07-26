9161cc040393ff5b9d6ef66cda37cae1f3613be2
guenther <guenther>
2013-07-26 18:13:02 +0000

    Make sure tmpline[] is always NUL terminated, to avoid possible read-beyond-end in get_line().
    Analysis and patch by Maxime Villard (rustyBSD (at) gmx.fr)
    ok millert@ deraadt@

--- libexec/ftpd/ftpd.c
+++ libexec/ftpd/ftpd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ftpd.c,v 1.195 2012/03/04 04:05:15 fgsch Exp $	*/
+/*	$OpenBSD: ftpd.c,v 1.196 2012/12/04 02:24:47 deraadt Exp $	*/
 /*	$NetBSD: ftpd.c,v 1.15 1995/06/03 22:46:47 mycroft Exp $	*/
 
 /*
@@ -2225,7 +2225,7 @@ myoob(void)
 	if (!transflag)
 		return;
 	cp = tmpline;
-	ret = get_line(cp, 7, stdin);
+	ret = get_line(cp, sizeof(tmpline)-1, stdin);
 	if (ret == -1) {
 		reply(221, "You could at least say goodbye.");
 		dologout(0);
