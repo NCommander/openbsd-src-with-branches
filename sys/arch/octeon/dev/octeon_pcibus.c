d5b988d35e947b3482bf0182b800367e6341917b
pirofti <pirofti>
2013-08-29 07:53:07 +0000

    Fix attach messages format for the pcibus.
    Before:
    pcibus0 at iobus0 irq 0pci0 at pcibus0 bus 0
    
    After:
    pcibus0 at iobus0 irq 0
    pci0 at pcibus0 bus 0
    
    Found on my DSR-500. Okay miod@

--- sys/arch/octeon/dev/octeon_pcibus.c
+++ sys/arch/octeon/dev/octeon_pcibus.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: octeon_pcibus.c,v 1.9 2013/06/01 01:16:20 jasper Exp $	*/
+/*	$OpenBSD: octeon_pcibus.c,v 1.10 2013/06/02 19:47:01 jasper Exp $	*/
 /*	$NetBSD: bonito_mainbus.c,v 1.11 2008/04/28 20:23:10 martin Exp $	*/
 /*	$NetBSD: bonito_pci.c,v 1.5 2008/04/28 20:23:28 martin Exp $	*/
 
@@ -211,6 +211,8 @@ octeon_pcibus_attach(struct device *parent, struct device *self, void *aux)
 	sc = (struct octeon_pcibus_softc *)self;
 	aa = aux;
 	sc->sc_aa = aa;
+
+	printf("\n");
 
 	/*
 	 * Attach PCI bus.
