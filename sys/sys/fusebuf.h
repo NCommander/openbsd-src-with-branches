e39d23c2173bff7243ee25d1a1d98f2f9a0511db
syl <syl>
2013-10-07 18:15:22 +0000

    Introduce fb_delete() helper and use it in FUSE code.

--- sys/sys/fusebuf.h
+++ sys/sys/fusebuf.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: fusebuf.h,v 1.3 2013/06/04 18:25:09 tedu Exp $ */
+/* $OpenBSD: fusebuf.h,v 1.4 2013/10/07 18:04:53 syl Exp $ */
 /*
  * Copyright (c) 2013 Sylvestre Gallon
  * Copyright (c) 2013 Martin Pieuchot
@@ -136,6 +136,7 @@ struct fusebuf {
 /* fusebuf(9) prototypes */
 struct	fusebuf *fb_setup(size_t, ino_t, int, struct proc *);
 int	fb_queue(dev_t, struct fusebuf *);
+void	fb_delete(struct fusebuf *);
 
 #endif /* _KERNEL */
 #endif /* _SYS_FUSEBUF_H_ */
