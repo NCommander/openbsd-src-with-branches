7eb4d458929c1b07db85a1ada82ff0178483a708
krw <krw>
2013-10-07 10:05:24 +0000

    Oops. Too mechanical -- can't use DL_SECTOBLK() to set fssize, since command line sector size overrides disk label sector size. Add a comment to make sure I don't do it again.

--- sbin/newfs/newfs.c
+++ sbin/newfs/newfs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: newfs.c,v 1.91 2013/06/11 16:42:05 deraadt Exp $	*/
+/*	$OpenBSD: newfs.c,v 1.92 2013/10/06 22:13:26 krw Exp $	*/
 /*	$NetBSD: newfs.c,v 1.20 1996/05/16 07:13:03 thorpej Exp $	*/
 
 /*
@@ -441,7 +441,8 @@ havelabel:
 	       fatal("%s: maximum file system size on the `%c' partition is "
 		   "%llu sectors", argv[0], *cp, DL_GETPSIZE(pp));
 
-	fssize = DL_SECTOBLK(lp, nsecs);
+	/* Can't use DL_SECTOBLK() because sectorsize may not be from label! */
+	fssize = nsecs * (sectorsize / DEV_BSIZE);
 	if (oflagset == 0 && fssize >= INT_MAX)
 		Oflag = 2;	/* FFS2 */
 	if (fsize == 0) {
