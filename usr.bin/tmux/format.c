d8e4d2fb5019a4eda5b3062bcf31a5b57882a2ce
nicm <nicm>
2013-10-10 11:47:11 +0000

    Don't add client formats when they are NULL.

--- usr.bin/tmux/format.c
+++ usr.bin/tmux/format.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: format.c,v 1.25 2013/05/31 19:46:42 nicm Exp $ */
+/* $OpenBSD: format.c,v 1.26 2013/07/05 15:27:14 nicm Exp $ */
 
 /*
  * Copyright (c) 2011 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -346,8 +346,10 @@ format_client(struct format_tree *ft, struct client *c)
 	format_add(ft, "client_cwd", "%s", c->cwd);
 	format_add(ft, "client_height", "%u", c->tty.sy);
 	format_add(ft, "client_width", "%u", c->tty.sx);
-	format_add(ft, "client_tty", "%s", c->tty.path);
-	format_add(ft, "client_termname", "%s", c->tty.termname);
+	if (c->tty.path != NULL)
+		format_add(ft, "client_tty", "%s", c->tty.path);
+	if (c->tty.termname != NULL)
+		format_add(ft, "client_termname", "%s", c->tty.termname);
 
 	t = c->creation_time.tv_sec;
 	format_add(ft, "client_created", "%lld", (long long) t);
