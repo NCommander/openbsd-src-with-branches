c6c3acde4ed6d5a992b2ef7fa9f861522341310a
florian <florian>
2013-10-21 18:16:28 +0000

    No need for volatile here. pointed out by deraadt@

--- usr.sbin/slowcgi/slowcgi.c
+++ usr.sbin/slowcgi/slowcgi.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: slowcgi.c,v 1.19 2013/10/20 09:36:29 benno Exp $ */
+/*	$OpenBSD: slowcgi.c,v 1.20 2013/10/20 16:47:24 deraadt Exp $ */
 /*
  * Copyright (c) 2013 David Gwynne <dlg@openbsd.org>
  * Copyright (c) 2013 Florian Obser <florian@openbsd.org>
@@ -70,7 +70,7 @@
 
 #define FD_RESERVE		5
 #define FD_NEEDED		6
-volatile int cgi_inflight = 0;
+int cgi_inflight = 0;
 
 struct listener {
 	struct event	ev, pause;
