5c6069bf080078194b7c9d68248e7e40ba0724a6
guenther <guenther>
2013-10-25 04:42:48 +0000

    Move the declarations for dogetrusage(), itimerround(), and dowait4() to sys/*.h headers so that the compat/linux code can use them. Change dowait4() to not copyout() the status value, but rather leave that for its caller, as compat/linux has to translate it, with the side benefit of simplifying the native code.
    Originally written months ago as part of the time_t work; long
    memory, prodding, and ok from pirofti@

--- sys/kern/kern_resource.c
+++ sys/kern/kern_resource.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_resource.c,v 1.44 2013/09/14 01:35:00 guenther Exp $	*/
+/*	$OpenBSD: kern_resource.c,v 1.45 2013/09/14 03:06:41 guenther Exp $	*/
 /*	$NetBSD: kern_resource.c,v 1.38 1996/10/23 07:19:38 matthias Exp $	*/
 
 /*-
@@ -420,8 +420,6 @@ calcru(struct tusage *tup, struct timeval *up, struct timeval *sp,
 	if (ip != NULL)
 		TIMESPEC_TO_TIMEVAL(ip, &i);
 }
-
-int	dogetrusage(struct proc *, int, struct rusage *);
 
 /* ARGSUSED */
 int
