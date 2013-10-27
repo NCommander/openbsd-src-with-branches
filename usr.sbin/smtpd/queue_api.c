baf06c811a0f59bc3db3558a038ee99ccea50bb3
eric <eric>
2013-10-27 18:21:07 +0000

    obvious fixes in the external queue code. work-in-progress: not supported yet.

--- usr.sbin/smtpd/queue_api.c
+++ usr.sbin/smtpd/queue_api.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: queue_api.c,v 1.1 2013/07/19 20:37:07 eric Exp $	*/
+/*	$OpenBSD$	*/
 
 /*
  * Copyright (c) 2013 Eric Faurot <eric@openbsd.org>
@@ -149,6 +149,7 @@ queue_msg_dispatch(void)
 
 		/* XXX needs more love */
 		r = -1;
+		snprintf(path, sizeof path, "/tmp/message.XXXXXXXXXX");
 		fd = mkstemp(path);
 		if (fd == -1) {
 			log_warn("warn: queue-api: mkstemp");
