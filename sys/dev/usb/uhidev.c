1e8c11a29ccf6fc579a16d4126a32db92297bfb2
pirofti <pirofti>
2013-11-11 09:16:03 +0000

    Clean-up the activate routines from the uoak*(4) drivers.
    Leave the deactivate bits to the parent.
    Simplify by making a single dying check in uhidev(4) interrupt routine
    instead of having each child doing it.
    
    Okay mpi@

--- sys/dev/usb/uhidev.c
+++ sys/dev/usb/uhidev.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uhidev.c,v 1.48 2013/11/01 12:05:26 mpi Exp $	*/
+/*	$OpenBSD: uhidev.c,v 1.49 2013/11/01 17:42:59 mpi Exp $	*/
 /*	$NetBSD: uhidev.c,v 1.14 2003/03/11 16:44:00 augustss Exp $	*/
 
 /*
@@ -354,6 +354,7 @@ uhidev_activate(struct device *self, int act)
 				if (r && r != EOPNOTSUPP)
 					rv = r;
 			}
+		usbd_deactivate(sc->sc_udev);
 		break;
 	}
 	return (rv);
@@ -392,6 +393,9 @@ uhidev_intr(struct usbd_xfer *xfer, void *addr, usbd_status status)
 	u_char *p;
 	u_int rep;
 	u_int32_t cc;
+
+	if (usbd_is_dying(sc->sc_udev))
+		return;
 
 	usbd_get_xfer_status(xfer, NULL, NULL, &cc, NULL);
 
