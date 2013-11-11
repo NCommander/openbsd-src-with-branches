febf18d9f97d455de39ccee071b438b6338be66d
krw <krw>
2013-11-11 15:39:20 +0000

    RFC 2131 and friends state that when renewing a lease the client will begin by using unicast communications with the server, and fall back to using broadcast packets if unicast fails.
    We were not failing back, but instead doggedly sticking with unicast
    attempts until the lease expired.
    
    Instead, try unicast requests for the reboot timeout interval and
    then switch to using broadcast requests.
    
    Problem reported by Marc Peters and Dennis den Brok via misc@.
    
    ok dlg@ millert@

--- sbin/dhclient/dhclient.c
+++ sbin/dhclient/dhclient.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dhclient.c,v 1.262 2013/09/09 20:30:05 krw Exp $	*/
+/*	$OpenBSD: dhclient.c,v 1.263 2013/10/22 18:15:58 krw Exp $	*/
 
 /*
  * Copyright 2004 Henning Brauer <henning@openbsd.org>
@@ -1359,7 +1359,8 @@ send_request(void)
 	destination.sin_family = AF_INET;
 	destination.sin_len = sizeof(destination);
 
-	if (client->state != S_REQUESTING)
+	if ((client->state != S_REQUESTING) &&
+	    (interval <= config->reboot_timeout))
 		from.s_addr = client->active->address.s_addr;
 	else
 		from.s_addr = INADDR_ANY;
