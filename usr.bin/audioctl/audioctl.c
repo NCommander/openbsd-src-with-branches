20fc065fa2e89714e3165a0795c130861662b360
deraadt <deraadt>
2013-11-13 18:50:05 +0000

    handle missing proto

--- usr.bin/audioctl/audioctl.c
+++ usr.bin/audioctl/audioctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: audioctl.c,v 1.21 2010/07/15 03:43:12 jakemsr Exp $	*/
+/*	$OpenBSD: audioctl.c,v 1.22 2012/07/27 00:21:17 krw Exp $	*/
 /*	$NetBSD: audioctl.c,v 1.14 1998/04/27 16:55:23 augustss Exp $	*/
 
 /*
@@ -186,7 +186,7 @@ findfield(char *name)
 	return (0);
 }
 
-void
+static void
 prval(u_int format, void *valp)
 {
 	u_int v;
