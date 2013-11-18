8edc13e8b5beee562c0e766e754013ed36eba1a0
tedu <tedu>
2013-11-18 23:10:48 +0000

    interprocess semaphores ala sem_open. mostly following in the pattern of shm_open. with some additions and fixes from zhuk.

--- lib/librthread/rthread.h
+++ lib/librthread/rthread.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rthread.h,v 1.44 2013/06/03 04:33:44 tedu Exp $ */
+/*	$OpenBSD: rthread.h,v 1.45 2013/06/21 06:08:50 guenther Exp $ */
 /*
  * Copyright (c) 2004,2005 Ted Unangst <tedu@openbsd.org>
  * All Rights Reserved.
@@ -64,7 +64,7 @@ struct __sem {
 	struct _spinlock lock;
 	volatile int waitcount;
 	volatile int value;
-	int __pad;
+	struct __sem *shared;
 };
 
 TAILQ_HEAD(pthread_queue, pthread);
