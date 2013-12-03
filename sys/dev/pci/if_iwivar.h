cff6e514fffadcedfb9eed9745240cd0dd9bb462
kettenis <kettenis>
2013-12-03 22:37:24 +0000

    Seems we simply have to live with fatal firmware errors.  Even Linux, with its driver written by Intel engineers occasionally gets them.  So when we get one, simply reset the chip, reload the firware and bring the interface up again.
    Tested by naddy@, who unfortunately is experiencing the quantum zeno effect
    making his iwi(4) not crap out.
    ok stsp@

--- sys/dev/pci/if_iwivar.h
+++ sys/dev/pci/if_iwivar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_iwivar.h,v 1.23 2010/09/07 16:21:45 deraadt Exp $	*/
+/*	$OpenBSD: if_iwivar.h,v 1.24 2013/11/14 12:39:14 dlg Exp $	*/
 
 /*-
  * Copyright (c) 2004-2006
@@ -112,9 +112,9 @@ struct iwi_softc {
 	pcitag_t		sc_pcitag;
 	bus_size_t		sc_sz;
 
-	int			sc_tx_timer;
+	struct task		init_task;
 
-	struct task		sc_resume_t;
+	int			sc_tx_timer;
 
 #if NBPFILTER > 0
 	caddr_t			sc_drvbpf;
