695f0e6872fd37a24737c328179f2d6ee07da8b9
eric <eric>
2013-12-06 14:12:34 +0000

    fix smtpctl resume route

--- usr.sbin/smtpd/parser.c
+++ usr.sbin/smtpd/parser.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: parser.c,v 1.36 2013/10/25 18:58:10 eric Exp $	*/
+/*	$OpenBSD$	*/
 
 /*
  * Copyright (c) 2013 Eric Faurot	<eric@openbsd.org>
@@ -159,7 +159,7 @@ cmd_check(const char *str, struct node *node, struct parameter *res)
 		return (1);
 
 	case P_ROUTEID:
-		res->u.u_int = strtonum(str, 1, LLONG_MAX, &e);
+		res->u.u_routeid = strtonum(str, 1, LLONG_MAX, &e);
 		if (e)
 			return (0);
 		return (1);
