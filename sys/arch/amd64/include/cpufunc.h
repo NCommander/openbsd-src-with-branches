6202a91938761e4276cd02477990ca7d7fff2355
kettenis <kettenis>
2013-12-06 22:56:20 +0000

    Make clflush() flush the cache line specified by the address we pass it instead of the cache line containing the local variable used to specify the address.  Fixes the gnome corruption and hangs people have been experiencing for the last couple of months or so.
    ok deraadt@, mlarkin@

--- sys/arch/amd64/include/cpufunc.h
+++ sys/arch/amd64/include/cpufunc.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cpufunc.h,v 1.8 2012/12/05 23:20:10 deraadt Exp $	*/
+/*	$OpenBSD: cpufunc.h,v 1.9 2013/10/05 16:58:30 guenther Exp $	*/
 /*	$NetBSD: cpufunc.h,v 1.3 2003/05/08 10:27:43 fvdl Exp $	*/
 
 /*-
@@ -267,7 +267,7 @@ wbinvd(void)
 static __inline void
 clflush(u_int64_t addr)
 {
-	__asm __volatile("clflush %0" : "+m" (addr));
+	__asm __volatile("clflush %0" : "+m" (*(volatile char *)addr));
 }
 
 static __inline void
