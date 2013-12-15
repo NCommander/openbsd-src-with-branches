b4139d158dae4bd578961990d227cd661d67d677
kettenis <kettenis>
2013-12-15 22:09:37 +0000

    Fix error handling of ->fb_create() call.  Prevents kernel panic if an unsupported frame buffer configuration is requested.

--- sys/dev/pci/drm/drmP.h
+++ sys/dev/pci/drm/drmP.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: drmP.h,v 1.155 2013/12/01 09:13:46 kettenis Exp $ */
+/* $OpenBSD: drmP.h,v 1.156 2013/12/05 13:29:56 kettenis Exp $ */
 /* drmP.h -- Private header for Direct Rendering Manager -*- linux-c -*-
  * Created: Mon Jan  4 10:05:05 1999 by faith@precisioninsight.com
  */
@@ -196,6 +196,12 @@ static inline void *
 ERR_PTR(long error)
 {
 	return (void *) error;
+}
+
+static inline long
+PTR_ERR(const void *ptr)
+{
+	return (long) ptr;
 }
 
 static inline long
