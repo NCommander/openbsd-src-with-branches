5fef3cc4f41636703f4fb0f1c1d0b55976cdf06c
tedu <tedu>
2014-01-14 17:25:06 +0000

    this can be static

--- usr.bin/signify/signify.c
+++ usr.bin/signify/signify.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: signify.c,v 1.33 2014/01/13 09:42:53 espie Exp $ */
+/* $OpenBSD: signify.c,v 1.34 2014/01/14 17:15:12 tedu Exp $ */
 /*
  * Copyright (c) 2013 Ted Unangst <tedu@openbsd.org>
  *
@@ -172,7 +172,7 @@ readb64file(const char *filename, void *buf, size_t len, char *comment)
 	close(fd);
 }
 
-uint8_t *
+static uint8_t *
 readmsg(const char *filename, unsigned long long *msglenp)
 {
 	unsigned long long msglen;
