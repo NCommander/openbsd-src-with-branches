1f78ac158de5c8a23e1ae7b630778c66f7ed9c37
dtucker <dtucker>
2014-01-19 23:43:02 +0000

    Don't use -q on sftp as it suppresses logging, instead redirect the output to the regress logfile.

--- regress/usr.bin/ssh/sftp-chroot.sh
+++ regress/usr.bin/ssh/sftp-chroot.sh
@@ -1,4 +1,4 @@
-#	$OpenBSD: sftp-chroot.sh,v 1.1 2013/04/18 02:46:12 djm Exp $
+#	$OpenBSD: sftp-chroot.sh,v 1.2 2013/05/17 04:29:14 dtucker Exp $
 #	Placed in the Public Domain.
 
 tid="sftp in chroot"
@@ -17,7 +17,8 @@ $SUDO sh -c "echo mekmitastdigoat > $PRIVDATA" || \
 start_sshd -oChrootDirectory=$CHROOT -oForceCommand="internal-sftp -d /"
 
 verbose "test $tid: get"
-${SFTP} -qS "$SSH" -F $OBJ/ssh_config host:/${FILENAME} $COPY || \
+${SFTP} -S "$SSH" -F $OBJ/ssh_config host:/${FILENAME} $COPY \
+    >$TEST_REGRESS_LOGFILE 2>&1 || \
 	fatal "Fetch ${FILENAME} failed"
 cmp $PRIVDATA $COPY || fail "$PRIVDATA $COPY differ"
 
