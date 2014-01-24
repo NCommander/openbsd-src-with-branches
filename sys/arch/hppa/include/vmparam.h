cadaa1a83cf4509d64b7e543acbb8a9ec6a7cdf1
miod <miod>
2014-01-24 05:21:19 +0000

    Do not protect struct vm_page_md with defined(_KERNEL), for userland uvm grovellers need to know it to be able to get the right size for struct vm_page.

--- sys/arch/hppa/include/vmparam.h
+++ sys/arch/hppa/include/vmparam.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vmparam.h,v 1.39 2011/11/10 22:48:13 deraadt Exp $	*/
+/*	$OpenBSD: vmparam.h,v 1.40 2014/01/23 22:06:29 miod Exp $	*/
 
 /* 
  * Copyright (c) 1988-1994, The University of Utah and
@@ -92,7 +92,7 @@
 
 #define	VM_PHYSSEG_NOADD	/* XXX until uvm code is fixed */
 
-#if defined(_KERNEL) && !defined(_LOCORE)
+#if !defined(_LOCORE)
 
 #include <sys/lock.h>
 
