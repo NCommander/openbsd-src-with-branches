01bdfc7cddf6517deb0c12bcb0e491787f1aae9e
miod <miod>
2014-01-24 22:29:21 +0000

    Don't include <machine/vmparam.h> directly. Instead, get it with its necessary dependencies via <sys/sysctl.h>. Unbreaks ppc userland build.

--- sbin/newfs/mkfs.c
+++ sbin/newfs/mkfs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mkfs.c,v 1.83 2013/11/03 02:22:07 krw Exp $	*/
+/*	$OpenBSD: mkfs.c,v 1.84 2013/11/12 04:59:02 deraadt Exp $	*/
 /*	$NetBSD: mkfs.c,v 1.25 1995/06/18 21:35:38 cgd Exp $	*/
 
 /*
@@ -40,7 +40,6 @@
  */
 
 #include <sys/param.h>
-#include <machine/vmparam.h>
 #include <sys/time.h>
 #include <sys/disklabel.h>
 #include <sys/ioctl.h>
