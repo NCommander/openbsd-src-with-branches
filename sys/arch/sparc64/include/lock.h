94307d0d711e964f0ac204ca16068427c0a22940
dlg <dlg>
2014-01-30 00:51:13 +0000

    move sparc64 behind the MI atomic api.
    this basically replaces sparc64_cas and sparc64_casx with atomic_cas_uint
    and atomic_cas_ulong respectively. it then builds atomic_add and
    atomic_sub out of those. this avoids the gcc atomic builtins that
    the MI atomic_foo api uses by default, so we dont get the extra
    membars that the builtins do but the atomic_foo api doesnt promise.
    
    it also fixes up the code that used to use sparc64_{cas,casx} to
    use the atomic_cas api instead.
    
    use of the sparc64 membar() macros are left untouched for now.
    
    ok kettenis@

--- sys/arch/sparc64/include/lock.h
+++ sys/arch/sparc64/include/lock.h
@@ -1,13 +1,12 @@
-/*	$OpenBSD: lock.h,v 1.7 2011/07/02 22:19:16 guenther Exp $	*/
+/*	$OpenBSD: lock.h,v 1.8 2013/05/21 20:05:30 tedu Exp $	*/
 
 /* public domain */
 
 #ifndef	_MACHINE_LOCK_H_
 #define	_MACHINE_LOCK_H_
 
-#include <machine/atomic.h>
-#include <machine/ctlreg.h>
+#include <sys/atomic.h>
 
-#define	rw_cas(p, o, n)		(sparc64_casx(p, o, n) != o)
+#define	rw_cas(p, o, n)		(atomic_cas_ulong(p, o, n) != o)
 
 #endif	/* _MACHINE_LOCK_H_ */
