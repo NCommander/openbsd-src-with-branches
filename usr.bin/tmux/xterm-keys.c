3943d9f2a730609fc6ab954c522b7dabbc1a8aaf
nicm <nicm>
2014-02-10 11:20:41 +0000

    The last fix to xterm keys meant that some keys such as \033OA were being wrongly treated as partial matches. So both check xterm keys after standard keys and only wildcard the minimum required ('1' to '8'). Problems reported by Ralf Horstmann and Tim van der Molen.

--- usr.bin/tmux/xterm-keys.c
+++ usr.bin/tmux/xterm-keys.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: xterm-keys.c,v 1.11 2013/03/22 10:30:04 nicm Exp $ */
+/* $OpenBSD: xterm-keys.c,v 1.12 2014/01/31 11:20:28 nicm Exp $ */
 
 /*
  * Copyright (c) 2009 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -131,7 +131,9 @@ xterm_keys_match(const char *template, const char *buf, size_t len)
 
 	pos = 0;
 	do {
-		if (*template != '_' && buf[pos] != *template)
+		if (*template == '_' && buf[pos] >= '1' && buf[pos] <= '8')
+			continue;
+		if (buf[pos] != *template)
 			return (-1);
 	} while (*++template != '\0' && ++pos != len);
 
