369bed45b7cf891bad20ae2d748cf167da31d445
mpi <mpi>
2014-02-25 15:49:10 +0000

    Abort and close all the interrupt pipes when detaching HID devices since drivers attaching to uhidev(4) do not always do it.  Seems to fix one of the transfer aborting races triggering a lot of "ehci_idone: ..." vomit upon resume.
    Reported by and fix tested by Ville Valkonen, ok deraadt@

--- sys/dev/usb/uhidev.c
+++ sys/dev/usb/uhidev.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uhidev.c,v 1.50 2013/11/11 09:16:03 pirofti Exp $	*/
+/*	$OpenBSD: uhidev.c,v 1.51 2013/11/19 14:04:07 pirofti Exp $	*/
 /*	$NetBSD: uhidev.c,v 1.14 2003/03/11 16:44:00 augustss Exp $	*/
 
 /*
@@ -362,8 +362,17 @@ uhidev_detach(struct device *self, int flags)
 
 	DPRINTF(("uhidev_detach: sc=%p flags=%d\n", sc, flags));
 
-	if (sc->sc_ipipe != NULL)
+	if (sc->sc_opipe != NULL) {
+		usbd_abort_pipe(sc->sc_opipe);
+		usbd_close_pipe(sc->sc_opipe);
+		sc->sc_opipe = NULL;
+	}
+
+	if (sc->sc_ipipe != NULL) {
 		usbd_abort_pipe(sc->sc_ipipe);
+		usbd_close_pipe(sc->sc_ipipe);
+		sc->sc_ipipe = NULL;
+	}
 
 	if (sc->sc_repdesc != NULL)
 		free(sc->sc_repdesc, M_USBDEV);
