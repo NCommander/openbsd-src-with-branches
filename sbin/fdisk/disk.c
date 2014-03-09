32e0b34ce9dc7831986f1cfcd97bf40ff7121745
krw <krw>
2014-03-09 22:25:06 +0000

    Instead of passing around 'char buf[DEV_BSIZE]' buffers, pass around 'struct dos_mbr' variables, since that is what the buffers were used for. Removes need to know about DEV_BSIZE and thus include param.h from all files but one so move the param.h #include to that file (mbr.c).
    Nuke a bunch of local MBR #defines in favour of the disklabel.h ones.
    
    Remove a bunch of unneeded #includes, replace the odd malloc/bzero
    with calloc, replace equally odd bcopy's with memcpy, remove a stray
    duplicate MBR parsing in MBR_pcopy().
    
    No intentional functional change.

--- sbin/fdisk/disk.c
+++ sbin/fdisk/disk.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: disk.c,v 1.34 2013/03/21 18:45:58 deraadt Exp $	*/
+/*	$OpenBSD: disk.c,v 1.35 2014/03/07 21:56:13 krw Exp $	*/
 
 /*
  * Copyright (c) 1997, 2001 Tobias Weingartner
@@ -27,7 +27,6 @@
 
 #include <sys/types.h>
 #include <sys/fcntl.h>
-#include <sys/ioctl.h>
 #include <sys/dkio.h>
 #include <sys/stdint.h>
 #include <sys/stat.h>
@@ -36,7 +35,7 @@
 #include <util.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+
 #include "disk.h"
 #include "misc.h"
 
@@ -154,7 +153,7 @@ DISK_printmetrics(struct disk *disk, char *units)
 		printf("geometry: %d/%d/%d [%.0f ",
 		    disk->real->cylinders, disk->real->heads,
 		    disk->real->sectors, size);
-		if (i == SECTORS && secsize != DEV_BSIZE)
+		if (i == SECTORS && secsize != sizeof(struct dos_mbr))
 			printf("%d-byte ", secsize);
 		printf("%s]\n", unit_types[i].lname);
 	} else
