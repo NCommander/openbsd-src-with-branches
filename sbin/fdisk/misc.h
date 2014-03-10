9e5badf2725fd8e1925d36ba5fa48b3693ce1b54
krw <krw>
2014-03-10 21:40:58 +0000

    No need to pass the name of the command to the function that is implementing that command, a pointer to the command arguments is the only thing needed. No need to have a pointer to the command table in every command, since there is only one table. No need for shiney new stack space for every command name and arguments parsed, since we don't recurse we can just use static memory.
    No intentional functional change.

--- sbin/fdisk/misc.h
+++ sbin/fdisk/misc.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: misc.h,v 1.18 2013/03/21 18:45:58 deraadt Exp $	*/
+/*	$OpenBSD: misc.h,v 1.19 2014/03/07 21:56:13 krw Exp $	*/
 
 /*
  * Copyright (c) 1997 Tobias Weingartner
@@ -47,7 +47,7 @@ extern struct unit_type unit_types[];
 
 /* Prototypes */
 int unit_lookup(char *);
-int ask_cmd(struct cmd *);
+int ask_cmd(char **, char **);
 int ask_num(const char *, int, int, int);
 int ask_pid(int);
 int ask_yn(const char *);
