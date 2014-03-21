04c856c18750257cff82f3e54ecaaade22811b90
andre <andre>
2014-03-21 21:54:14 +0000

    Fix missing brackets: longitude/latitude status set to critical every time nmea_timeout() is triggered introduced in rev 1.37.
    ok sthen@

--- sys/kern/tty_nmea.c
+++ sys/kern/tty_nmea.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tty_nmea.c,v 1.39 2010/05/27 17:18:23 sthen Exp $ */
+/*	$OpenBSD: tty_nmea.c,v 1.40 2014/03/21 21:46:47 andre Exp $ */
 
 /*
  * Copyright (c) 2006, 2007, 2008 Marc Balmer <mbalmer@openbsd.org>
@@ -554,8 +554,9 @@ nmea_timeout(void *xnp)
 		 * sentences are received.
 		 */
 		timeout_add_sec(&np->nmea_tout, TRUSTTIME);
-	} else
+	} else {
 		np->time.status = SENSOR_S_CRIT;
 		np->latitude.status = SENSOR_S_CRIT;
 		np->longitude.status = SENSOR_S_CRIT;
+	}
 }
