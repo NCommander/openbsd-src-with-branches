72f068447449aaf4e927517509f2fa5711607879
krw <krw>
2014-03-31 19:50:52 +0000

    Nuke [get|put][short|long] and just use letoh[16|32] and htole[16|32]. This is made possible by a) copying unaligned data (struct dos_partition's) into properly aligned structs and accessing the fields of the same; and b) realizing that gcc is smart enough to access fields in packed struct's *if* it knows it is accessing a packed struct. Even sparc64 finally gave a thumbs up.
    After several attempts and feedback from deraadt@ and dlg@, it looks
    good to deraadt@.

--- sbin/fdisk/misc.h
+++ sbin/fdisk/misc.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: misc.h,v 1.20 2014/03/10 21:40:58 krw Exp $	*/
+/*	$OpenBSD: misc.h,v 1.21 2014/03/17 16:40:00 krw Exp $	*/
 
 /*
  * Copyright (c) 1997 Tobias Weingartner
@@ -42,10 +42,6 @@ int ask_cmd(char **, char **);
 int ask_num(const char *, int, int, int);
 int ask_pid(int);
 int ask_yn(const char *);
-u_int16_t getshort(void *);
-u_int32_t getlong(void *);
-void putshort(void *, u_int16_t);
-void putlong(void *, u_int32_t);
 u_int32_t getuint(struct disk *, char *, u_int32_t, u_int32_t);
 
 #endif /* _MISC_H */
