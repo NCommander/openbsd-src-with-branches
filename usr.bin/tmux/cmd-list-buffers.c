350829bab98c19e5a09c95f3567fce9e8eee922a
nicm <nicm>
2014-04-02 18:12:18 +0000

    Support UTF-8 with choose-buffer, from Kosuke ASAMI. Also make buffer_sample bigger to let it trim at window right edge.

--- usr.bin/tmux/cmd-list-buffers.c
+++ usr.bin/tmux/cmd-list-buffers.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: cmd-list-buffers.c,v 1.17 2013/03/24 09:54:10 nicm Exp $ */
+/* $OpenBSD: cmd-list-buffers.c,v 1.18 2013/10/10 12:00:20 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -55,7 +55,7 @@ cmd_list_buffers_exec(unused struct cmd *self, struct cmd_q *cmdq)
 	while ((pb = paste_walk_stack(&global_buffers, &idx)) != NULL) {
 		ft = format_create();
 		format_add(ft, "line", "%u", idx - 1);
-		format_paste_buffer(ft, pb);
+		format_paste_buffer(ft, pb, 0);
 
 		line = format_expand(ft, template);
 		cmdq_print(cmdq, "%s", line);
