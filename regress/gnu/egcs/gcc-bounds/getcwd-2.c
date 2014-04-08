ef21ffea92128dea99c479c96dfb6934608ba1e7
djm <djm>
2014-04-08 00:55:23 +0000

    MFC: cherrypick fix for CVE-2014-0160 "heartbleed" vulnerability from OpenSSL git; ok sthen@

--- regress/gnu/egcs/gcc-bounds/getcwd-2.c
+++
@@ -1,7 +0,0 @@
-#include <unistd.h>
-
-int main(int argc, char **argv) {
-	char buf[10];
-	getcwd(buf, sizeof(buf) - 1000);
-	return 1;
-}
--- sys/arch/mvme68k/stand/bootsd/version.c
+++
@@ -1,7 +0,0 @@
-/*	$OpenBSD: version.c,v 1.8 1998/08/18 04:17:08 smurph Exp $ */
-
-/*
- *	1.9	kernel loaded with loadfile, a.out and ELF formats
- */
-
-char *version = "1.9";
