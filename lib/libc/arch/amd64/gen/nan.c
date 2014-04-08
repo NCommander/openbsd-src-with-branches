d26e014858914bac03d04940c5c18326e241a4ff
djm <djm>
2014-04-08 00:55:39 +0000

    MFC: cherrypick fix for CVE-2014-0160 "heartbleed" vulnerability from OpenSSL git; ok sthen@

--- lib/libc/arch/amd64/gen/nan.c
+++
@@ -1,9 +0,0 @@
-/*	$OpenBSD$	*/
-
-/* Written by Martynas Venckus.  Public Domain. */
-
-#include <math.h>
-
-/* bytes for qNaN on an amd64 (IEEE single format) */
-char __nan[] __attribute__((__aligned__(sizeof(float)))) =
-					{ 0, 0, 0xc0, 0x7f };
--- sys/arch/landisk/include/limits.h
+++
@@ -1,3 +0,0 @@
-/*	$OpenBSD$	*/
-
-#include <sh/limits.h>
