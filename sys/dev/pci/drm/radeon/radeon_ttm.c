236fac4495effc236a7194191f1450c1b79be167
jsg <jsg>
2014-04-12 06:03:30 +0000

    drm/radeon: TTM must be init with cpu-visible VRAM, v2
    From Lauri Kasanen
    7e88b3ea565f869721b8ac011ee73b95b773e938 in ubuntu 3.8
    14eedc32a3c0ec9dd70448a73763ee21feae3111 in mainline linux

--- sys/dev/pci/drm/radeon/radeon_ttm.c
+++ sys/dev/pci/drm/radeon/radeon_ttm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: radeon_ttm.c,v 1.3 2014/02/09 11:03:31 jsg Exp $	*/
+/*	$OpenBSD: radeon_ttm.c,v 1.4 2014/03/24 17:06:49 kettenis Exp $	*/
 /*
  * Copyright 2009 Jerome Glisse.
  * All Rights Reserved.
@@ -791,6 +791,9 @@ int radeon_ttm_init(struct radeon_device *rdev)
 		DRM_ERROR("Failed initializing VRAM heap.\n");
 		return r;
 	}
+	/* Change the size here instead of the init above so only lpfn is affected */
+	radeon_ttm_set_active_vram_size(rdev, rdev->mc.visible_vram_size);
+
 #ifdef __sparc64__
 	r = radeon_bo_create(rdev, rdev->fb_offset, PAGE_SIZE, true,
 			     RADEON_GEM_DOMAIN_VRAM,
