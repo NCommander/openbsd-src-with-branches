05c7af2bfbc89e1faed1f4e7918b7e3c658b9473
cvs-fast-export <cvs-fast-export>
2014-04-15 18:01:18 +0000

    Synthetic commit for incomplete tag openssl_1_0_1g

--- lib/libc/arch/sh/gen/infinity.c
+++
@@ -1,9 +0,0 @@
-/*	$OpenBSD$	*/
-
-/* infinity.c */
-
-#include <math.h>
-
-/* bytes for +Infinity on a SH4 FPU (double precision) */
-char __infinity[] __attribute__((__aligned__(sizeof(double)))) =
-    { 0x7f, (char)0xf0, 0, 0, 0, 0, 0, 0 };
