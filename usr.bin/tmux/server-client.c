d51b31b8389a85c1e3064d19ed90d3c962190fae
nicm <nicm>
2014-04-16 08:02:31 +0000

    Because we pass the file descriptor from client to server, tmux can't usefully work if stdin is /dev/tty. Complain about it more clearly.

--- usr.bin/tmux/server-client.c
+++ usr.bin/tmux/server-client.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: server-client.c,v 1.118 2014/02/17 22:42:20 nicm Exp $ */
+/* $OpenBSD: server-client.c,v 1.119 2014/02/23 00:53:06 nicm Exp $ */
 
 /*
  * Copyright (c) 2009 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -118,6 +118,11 @@ server_client_open(struct client *c, char **cause)
 {
 	if (c->flags & CLIENT_CONTROL)
 		return (0);
+
+	if (strcmp(c->ttyname, "/dev/tty") == 0) {
+		*cause = xstrdup("can't use /dev/tty");
+		return (-1);
+	}
 
 	if (!(c->flags & CLIENT_TERMINAL)) {
 		*cause = xstrdup("not a terminal");
