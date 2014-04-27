a33e714c82c848b57b9bb06e39904ff1adc05fa2
mpi <mpi>
2014-04-27 14:48:10 +0000

    Start de-obfuscating the HC drivers.
    Since pipe methods have an xfer argument, always use it to get acces to
    various data structure (pipe, bus, device) instead of having a different
    way to get a pointer to these descriptors in every function.
    
    Also kill the {E,U}XFER() macro and use a consistent name accross all the
    methods for {e,u}hci_xfer.

--- sys/dev/usb/uhcivar.h
+++ sys/dev/usb/uhcivar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uhcivar.h,v 1.28 2013/11/01 17:29:02 mpi Exp $ */
+/*	$OpenBSD: uhcivar.h,v 1.29 2014/03/25 20:27:37 mpi Exp $ */
 /*	$NetBSD: uhcivar.h,v 1.36 2002/12/31 00:39:11 augustss Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/uhcivar.h,v 1.14 1999/11/17 22:33:42 n_hibma Exp $	*/
 
@@ -73,8 +73,6 @@ struct uhci_xfer {
 	int isdone;
 #endif
 };
-
-#define UXFER(xfer) ((struct uhci_xfer *)(xfer))
 
 /*
  * Extra information that we need for a TD.
