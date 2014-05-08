f844ef0e5cbb2c16298e83f7f1d44062c961d031
blambert <blambert>
2014-05-08 13:08:48 +0000

    match relayd proc.c infrastructure with snmpd
    okay reyk@

--- usr.sbin/relayd/relayd.c
+++ usr.sbin/relayd/relayd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: relayd.c,v 1.121 2014/04/20 14:48:29 reyk Exp $	*/
+/*	$OpenBSD: relayd.c,v 1.122 2014/04/22 08:04:23 reyk Exp $	*/
 
 /*
  * Copyright (c) 2007 - 2014 Reyk Floeter <reyk@openbsd.org>
@@ -195,6 +195,9 @@ main(int argc, char *argv[])
 	relayd_env = env;
 	env->sc_ps = ps;
 	ps->ps_env = env;
+log_warnx("batman");
+	TAILQ_INIT(&ps->ps_rcsocks);
+log_warnx("robin");
 	env->sc_conffile = conffile;
 	env->sc_opts = opts;
 
