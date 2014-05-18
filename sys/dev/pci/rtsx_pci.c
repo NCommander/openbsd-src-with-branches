5e4e9cad19b147bc1422b24e641ef260a1954ab7
stsp <stsp>
2014-05-18 10:52:17 +0000

    Make rtsx(4) attach to RTL8402 devices. Reported as working by Eric Huiban.

--- sys/dev/pci/rtsx_pci.c
+++ sys/dev/pci/rtsx_pci.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rtsx_pci.c,v 1.4 2013/11/06 13:51:02 stsp Exp $	*/
+/*	$OpenBSD: rtsx_pci.c,v 1.5 2014/04/18 09:34:05 claudio Exp $	*/
 
 /*
  * Copyright (c) 2006 Uwe Stuehler <uwe@openbsd.org>
@@ -59,7 +59,8 @@ rtsx_pci_match(struct device *parent, void *match, void *aux)
 
 	if (PCI_PRODUCT(pa->pa_id) == PCI_PRODUCT_REALTEK_RTS5209 ||
 	    PCI_PRODUCT(pa->pa_id) == PCI_PRODUCT_REALTEK_RTS5227 ||
-	    PCI_PRODUCT(pa->pa_id) == PCI_PRODUCT_REALTEK_RTS5229)
+	    PCI_PRODUCT(pa->pa_id) == PCI_PRODUCT_REALTEK_RTS5229 ||
+	    PCI_PRODUCT(pa->pa_id) == PCI_PRODUCT_REALTEK_RTL8402)
 		return 1;
 
 	return 0;
