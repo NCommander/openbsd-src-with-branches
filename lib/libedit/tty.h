e84b82ee31ce9461c2b20df8a1eeba1abb0fd894
yasuoka <yasuoka>
2014-05-20 22:28:07 +0000

    Merge the fixes from the upstream.  Factor out some common code in tty.c and fix EL_SETTY to work.
    ok nicm

--- lib/libedit/tty.h
+++ lib/libedit/tty.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: tty.h,v 1.8 2010/06/30 00:05:35 nicm Exp $	*/
 /*	$NetBSD: tty.h,v 1.12 2009/12/30 22:37:40 christos Exp $	*/
 
 /*-
@@ -443,6 +443,7 @@
 #define	QU_IO	2	/* used only for quoted chars	*/
 #define	NN_IO	3	/* The number of entries	*/
 
+/* Don't re-order */
 #define	MD_INP	0
 #define	MD_OUT	1
 #define	MD_CTL	2
