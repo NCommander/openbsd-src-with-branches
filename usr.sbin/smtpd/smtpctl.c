43d5df8d32c76cc0f52e6f63996858c543a4eaa9
espie <espie>
2014-05-23 13:28:32 +0000

    use reallocarray okay gilles@

--- usr.sbin/smtpd/smtpctl.c
+++ usr.sbin/smtpd/smtpctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: smtpctl.c,v 1.119 2014/04/22 13:57:58 gilles Exp $	*/
+/*	$OpenBSD: smtpctl.c,v 1.120 2014/05/20 18:47:01 eric Exp $	*/
 
 /*
  * Copyright (c) 2013 Eric Faurot <eric@openbsd.org>
@@ -317,9 +317,10 @@ srv_iter_evpids(uint32_t msgid, uint64_t *evpid, int *offset)
 		while (srv_iter_envelopes(msgid, &evp)) {
 			if (n == alloc) {
 				alloc += 256;
-				evpids = realloc(evpids, alloc * sizeof(*evpids));
+				evpids = reallocarray(evpids, alloc,
+				    sizeof(*evpids));
 				if (evpids == NULL)
-					err(1, "realloc");
+					err(1, "reallocarray");
 			}
 			evpids[n++] = evp.id;
 		}
