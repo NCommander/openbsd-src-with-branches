5520c2c85cbbc7800c6f0bcdecf69bd175ffa42a
stsp <stsp>
2014-06-03 14:41:04 +0000

    Fix zyd(4)'s frame length adjustment in RX path. Instead of snipping off HW-specific data and CRC trailer as intended, the driver was telling the net80211 layer frames were longer than they actually are. Math is hard. ok krw@ kettenis@ mpi@

--- sys/dev/usb/if_zyd.c
+++ sys/dev/usb/if_zyd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_zyd.c,v 1.96 2014/03/07 18:39:02 mpi Exp $	*/
+/*	$OpenBSD: if_zyd.c,v 1.97 2014/03/19 10:09:19 mpi Exp $	*/
 
 /*-
  * Copyright (c) 2006 by Damien Bergamini <damien.bergamini@free.fr>
@@ -1913,7 +1913,7 @@ zyd_rx_data(struct zyd_softc *sc, const uint8_t *buf, uint16_t len)
 	}
 
 	/* compute actual frame length */
-	len -= sizeof (*plcp) - sizeof (*stat) - IEEE80211_CRC_LEN;
+	len -= (sizeof (*plcp) + sizeof (*stat) + IEEE80211_CRC_LEN);
 
 	if (len > MCLBYTES) {
 		DPRINTFN(2, ("frame too large (length=%d)\n", len));
