57c8b3c4e7c3d0495bc021ef9d4a11d243248685
jsg <jsg>
2014-06-08 16:01:00 +0000

    Remove an incorrect bzero() that was zeroing the size of the pointer instead of the size of the buffer. Removal suggested by deraadt@ as no code uses the buffer after the zeroing.

--- sys/arch/sparc64/stand/ofwboot/ofdev.c
+++ sys/arch/sparc64/stand/ofwboot/ofdev.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ofdev.c,v 1.20 2013/11/05 00:51:58 krw Exp $	*/
+/*	$OpenBSD: ofdev.c,v 1.21 2014/06/08 15:34:05 jsg Exp $	*/
 /*	$NetBSD: ofdev.c,v 1.1 2000/08/20 14:58:41 mrg Exp $	*/
 
 /*
@@ -444,7 +444,6 @@ search_label(struct of_dev *devp, u_long off, char *buf, struct disklabel *lp,
 		return (NULL);
 	}
 
-	bzero(buf, sizeof(buf));
 	return ("no disk label");
 }
 
