a879b8754678109a9fd01f5fe4be4cc5ceef1c82
dlg <dlg>
2014-06-18 11:09:58 +0000

    trailing tabs arent needed.

--- sys/kern/uipc_mbuf.c
+++ sys/kern/uipc_mbuf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uipc_mbuf.c,v 1.184 2014/05/04 19:27:08 sf Exp $	*/
+/*	$OpenBSD: uipc_mbuf.c,v 1.185 2014/06/13 07:28:13 mpi Exp $	*/
 /*	$NetBSD: uipc_mbuf.c,v 1.15.4.1 1996/06/13 17:11:44 cgd Exp $	*/
 
 /*
@@ -451,7 +451,7 @@ m_clget(struct mbuf *m, int how, struct ifnet *ifp, u_int pktlen)
 			return (NULL);
 		}
 		m = m0;
-	}			
+	}
 	m->m_ext.ext_buf = pool_get(&mclpools[pi],
 	    how == M_WAIT ? PR_WAITOK : PR_NOWAIT);
 	if (!m->m_ext.ext_buf) {
