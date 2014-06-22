9285d4a05d31af0f076afaaebbf579dc0f0b0548
bluhm <bluhm>
2014-06-22 14:18:01 +0000

    Fix the relayd regression tests.  Now they pass with and without an obj directory.  The relayd process can also run on a remote machine for different timing.

--- regress/usr.sbin/relayd/funcs.pl
+++ regress/usr.sbin/relayd/funcs.pl
@@ -1,4 +1,4 @@
-#	$OpenBSD: funcs.pl,v 1.9 2014/04/24 09:05:10 bluhm Exp $
+#	$OpenBSD: funcs.pl,v 1.10 2014/05/02 14:10:03 andre Exp $
 
 # Copyright (c) 2010-2013 Alexander Bluhm <bluhm@openbsd.org>
 #
@@ -16,7 +16,7 @@
 
 use strict;
 use warnings;
-#no warnings 'experimental::smartmatch';
+no warnings 'experimental::smartmatch';
 use feature 'switch';
 use Errno;
 use Digest::MD5;
