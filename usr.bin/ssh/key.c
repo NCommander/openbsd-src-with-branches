4ed2155f9184ab197491256d12a762cd6b345e17
djm <djm>
2014-06-30 12:54:39 +0000

    suppress spurious error message when loading key with a passphrase; reported by kettenis@ ok markus@

--- usr.bin/ssh/key.c
+++ usr.bin/ssh/key.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: key.c,v 1.117 2014/04/29 18:01:49 markus Exp $ */
+/* $OpenBSD: key.c,v 1.118 2014/06/24 01:13:21 djm Exp $ */
 /*
  * placed in the public domain
  */
@@ -417,7 +417,8 @@ key_load_private_type(int type, const char *filename, const char *passphrase,
 	if ((r = sshkey_load_private_type(type, filename, passphrase,
 	    &ret, commentp, perm_ok)) != 0) {
 		fatal_on_fatal_errors(r, __func__, SSH_ERR_LIBCRYPTO_ERROR);
-		if (r == SSH_ERR_SYSTEM_ERROR && errno == ENOENT)
+		if ((r == SSH_ERR_SYSTEM_ERROR && errno == ENOENT) ||
+		    (r == SSH_ERR_KEY_WRONG_PASSPHRASE))
 			debug("%s: %s", __func__, ssh_err(r));
 		else
 			error("%s: %s", __func__, ssh_err(r));
