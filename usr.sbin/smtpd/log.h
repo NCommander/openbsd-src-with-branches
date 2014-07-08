b1446b3ce300a9435175566aac56b1345937a6c9
eric <eric>
2014-07-08 10:30:52 +0000

    Change fatal/fatalx to use a format string.  Expand in a static buffer to cope with low-memory situations.
    ok gilles@ chl@

--- usr.sbin/smtpd/log.h
+++ usr.sbin/smtpd/log.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: log.h,v 1.2 2011/10/22 00:16:33 eric Exp $	*/
+/*	$OpenBSD$	*/
 
 /*
  * Copyright (c) 2010 Gilles Chehade <gilles@poolp.org>
@@ -28,5 +28,7 @@ void		log_debug(const char *, ...)
     __attribute__((format (printf, 1, 2)));
 void		log_trace(int, const char *, ...)
     __attribute__((format (printf, 2, 3)));
-__dead void	fatal(const char *);
-__dead void	fatalx(const char *);
+__dead void	fatal(const char *, ...)
+    __attribute__((format (printf, 1, 2)));
+__dead void	fatalx(const char *, ...)
+    __attribute__((format (printf, 1, 2)));
