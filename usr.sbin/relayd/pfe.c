5e4cf35600d9354e1491dbd328b7c2f6a5352f23
reyk <reyk>
2014-07-09 16:42:05 +0000

    Replace the protocol directives for HTTP with a new generic filtering language.  The grammar is inspired by pf and allows to write versatile last-matching filter rules in protocol sections starting with the "pass", "block" or "match" keywords.  This work was started almost two years ago and replaces large parts of relayd(8)'s HTTP and filtering code.  The initial version reimplements and extends HTTP filtering, but will be improved to support generic TCP and other protocols later.
    With some testing, feedback, and help from benno@ and andre@.
    
    OK benno@

--- usr.sbin/relayd/pfe.c
+++ usr.sbin/relayd/pfe.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pfe.c,v 1.73 2012/10/03 08:33:31 reyk Exp $	*/
+/*	$OpenBSD: pfe.c,v 1.74 2013/03/10 23:32:53 reyk Exp $	*/
 
 /*
  * Copyright (c) 2006 Pierre-Yves Ritschard <pyr@openbsd.org>
@@ -193,8 +193,6 @@ pfe_dispatch_parent(int fd, struct privsep_proc *p, struct imsg *imsg)
 		break;
 	case IMSG_CFG_PROTO:
 		config_getproto(env, imsg);
-		break;
-	case IMSG_CFG_PROTONODE:
 		break;
 	case IMSG_CFG_RELAY:
 		config_getrelay(env, imsg);
