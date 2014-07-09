cf3c23a6cadb0c4e32049194a30fb000e686816d
guenther <guenther>
2014-07-09 07:29:00 +0000

    Fix boot -d.  refreshcreds() should be called when trapping from userspace, but I flipped the test on i386/amd64, thus breaking kernel traps before enough proc0 bits were set up.  In theory, this could have resulted in a NFS read for a page fault being done with a process's old credentials.
    pointed out by Patrick Wildt of bitrig

--- sys/arch/amd64/amd64/trap.c
+++ sys/arch/amd64/amd64/trap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: trap.c,v 1.39 2014/05/11 00:12:43 guenther Exp $	*/
+/*	$OpenBSD: trap.c,v 1.40 2014/06/15 11:43:24 sf Exp $	*/
 /*	$NetBSD: trap.c,v 1.2 2003/05/04 23:51:56 fvdl Exp $	*/
 
 /*-
@@ -175,8 +175,8 @@ trap(struct trapframe *frame)
 	if (!KERNELMODE(frame->tf_cs, frame->tf_rflags)) {
 		type |= T_USER;
 		p->p_md.md_regs = frame;
-	} else /* if (type != T_NMI) */
 		refreshcreds(p);
+	}
 
 	switch (type) {
 
