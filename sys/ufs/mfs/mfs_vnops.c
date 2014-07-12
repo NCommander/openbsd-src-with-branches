5012e43a0585e307ec344f4f610c9fa581a38064
tedu <tedu>
2014-07-12 18:44:01 +0000

    add a size argument to free. will be used soon, but for now default to 0. after discussions with beck deraadt kettenis.

--- sys/ufs/mfs/mfs_vnops.c
+++ sys/ufs/mfs/mfs_vnops.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mfs_vnops.c,v 1.43 2012/12/29 14:54:11 beck Exp $	*/
+/*	$OpenBSD: mfs_vnops.c,v 1.44 2013/01/15 11:20:55 jsing Exp $	*/
 /*	$NetBSD: mfs_vnops.c,v 1.8 1996/03/17 02:16:32 christos Exp $	*/
 
 /*
@@ -250,7 +250,7 @@ mfs_reclaim(void *v)
 	struct vop_reclaim_args *ap = v;
 	struct vnode *vp = ap->a_vp;
 
-	free(vp->v_data, M_MFSNODE);
+	free(vp->v_data, M_MFSNODE, 0);
 	vp->v_data = NULL;
 	return (0);
 }
