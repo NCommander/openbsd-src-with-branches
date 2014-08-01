719b9679a2af7d81a1de63f3acb7af9860d156ea
reyk <reyk>
2014-08-01 21:59:56 +0000

    remove the global "log updates/all" option that came from relayd.

--- usr.sbin/httpd/httpd.c
+++ usr.sbin/httpd/httpd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: httpd.c,v 1.9 2014/07/25 21:29:58 reyk Exp $	*/
+/*	$OpenBSD: httpd.c,v 1.10 2014/07/26 09:59:14 reyk Exp $	*/
 
 /*
  * Copyright (c) 2014 Reyk Floeter <reyk@openbsd.org>
@@ -193,9 +193,6 @@ main(int argc, char *argv[])
 
 	if (parse_config(env->sc_conffile, env) == -1)
 		exit(1);
-
-	if (debug)
-		env->sc_opts |= HTTPD_OPT_LOGUPDATE;
 
 	if (geteuid())
 		errx(1, "need root privileges");
