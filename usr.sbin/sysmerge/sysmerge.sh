3e2653616c5f5e243c644bdffdc161a8cb3448cb
jmc <jmc>
2014-08-10 17:15:18 +0000

    - add -p to SYNOPSIS - the argument to -s is not optional
    ok ajacoutot

--- usr.sbin/sysmerge/sysmerge.sh
+++ usr.sbin/sysmerge/sysmerge.sh
@@ -1,6 +1,6 @@
 #!/bin/ksh -
 #
-# $OpenBSD: sysmerge.sh,v 1.147 2014/08/09 12:45:03 ajacoutot Exp $
+# $OpenBSD: sysmerge.sh,v 1.148 2014/08/09 12:46:51 ajacoutot Exp $
 #
 # Copyright (c) 2008-2014 Antoine Jacoutot <ajacoutot@openbsd.org>
 # Copyright (c) 1998-2003 Douglas Barton <DougB@FreeBSD.org>
@@ -64,7 +64,7 @@ restore_sum() {
 }
 
 usage() {
-	echo "usage: ${0##*/} [-bdS] [-p | [-s [src | etcXX.tgz]] [-x xetcXX.tgz]]" >&2
+	echo "usage: ${0##*/} [-bdS] [-p | [-s src | etcXX.tgz] [-x xetcXX.tgz]]" >&2
 }
 
 warn() {
