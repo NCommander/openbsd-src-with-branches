0ee68d111932bf35012f4ff91f2ada4bac8a3131
guenther <guenther>
2014-08-10 02:44:26 +0000

    Replace F_DUPFD followed by setting FD_CLOEXEC with just F_DUPFD_CLOEXEC
    ok matthew@ millert@

--- bin/ksh/io.c
+++ bin/ksh/io.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: io.c,v 1.22 2006/03/17 16:30:13 millert Exp $	*/
+/*	$OpenBSD: io.c,v 1.23 2013/12/17 16:37:06 deraadt Exp $	*/
 
 /*
  * shell buffered IO and formatted output
@@ -237,7 +237,7 @@ savefd(int fd)
 	int nfd;
 
 	if (fd < FDBASE) {
-		nfd = fcntl(fd, F_DUPFD, FDBASE);
+		nfd = fcntl(fd, F_DUPFD_CLOEXEC, FDBASE);
 		if (nfd < 0) {
 			if (errno == EBADF)
 				return -1;
@@ -246,7 +246,6 @@ savefd(int fd)
 		}
 	} else
 		nfd = fd;
-	fcntl(nfd, F_SETFD, FD_CLOEXEC);
 	return nfd;
 }
 
