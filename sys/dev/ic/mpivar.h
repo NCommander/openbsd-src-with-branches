e00cb600474ff7cb14ab183dc8aec0e609e4ec38
dlg <dlg>
2014-09-03 00:46:04 +0000

    tasks dont need a mutex and a semaphore to protect against multiple uses of the task structure like workq tasks did.
    tested on an fc929

--- sys/dev/ic/mpivar.h
+++ sys/dev/ic/mpivar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mpivar.h,v 1.37 2014/04/16 01:19:28 dlg Exp $ */
+/*	$OpenBSD: mpivar.h,v 1.38 2014/09/01 07:52:30 blambert Exp $ */
 
 /*
  * Copyright (c) 2005 David Gwynne <dlg@openbsd.org>
@@ -152,8 +152,6 @@ struct mpi_softc {
 	struct scsi_iohandler	sc_evt_scan_handler;
 
 	struct task		sc_evt_rescan;
-	struct mutex		sc_evt_rescan_mtx;
-	u_int			sc_evt_rescan_sem;
 
 	size_t			sc_fw_len;
 	struct mpi_dmamem	*sc_fw;
