1cbec65740ae5558318090e81b27631ceb4cb854
schwarze <schwarze>
2014-09-07 03:08:42 +0000

    Do not report a page as arch=any merely because .Dt lacks the third argument. Pages found outside arch-specific dirs still get arch=any, of course. Issue reported by justinhenryhaynes at gmail dot com on misc@, thanks!

--- usr.bin/mandoc/mandocdb.c
+++ usr.bin/mandoc/mandocdb.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mandocdb.c,v 1.117 2014/09/03 23:20:33 schwarze Exp $ */
+/*	$OpenBSD: mandocdb.c,v 1.118 2014/09/07 03:00:03 schwarze Exp $ */
 /*
  * Copyright (c) 2011, 2012 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2011, 2012, 2013, 2014 Ingo Schwarze <schwarze@openbsd.org>
@@ -1186,8 +1186,8 @@ mpages_merge(struct mchars *mc, struct mparse *mp)
 		if (mpage->mlinks->gzip)
 			mpage->form |= FORM_GZ;
 		putkey(mpage, mpage->sec, TYPE_sec);
-		putkey(mpage, '\0' == *mpage->arch ?
-		    any : mpage->arch, TYPE_arch);
+		if (*mpage->arch != '\0')
+			putkey(mpage, mpage->arch, TYPE_arch);
 
 		for (mlink = mpage->mlinks; mlink; mlink = mlink->next) {
 			if ('\0' != *mlink->dsec)
