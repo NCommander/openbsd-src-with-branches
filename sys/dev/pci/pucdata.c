71931212a1719513262a6f2ad351292601055aaf
sasano <sasano>
2014-09-22 21:16:20 +0000

    add Winchiphead CH382 support.
    CH382 has 2S/2S1P/1P configuration, the diff supports 2S/2S1P only
    (2S1P works as 2S).
    
    ok by deraadt@ and kettenis@

--- sys/dev/pci/pucdata.c
+++ sys/dev/pci/pucdata.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pucdata.c,v 1.92 2014/02/02 19:25:41 miod Exp $	*/
+/*	$OpenBSD: pucdata.c,v 1.93 2014/08/13 07:45:37 jsg Exp $	*/
 /*	$NetBSD: pucdata.c,v 1.6 1999/07/03 05:55:23 cgd Exp $	*/
 
 /*
@@ -2078,6 +2078,22 @@ const struct puc_device_description puc_devs[] = {
 	    {
 		{ PUC_COM_POW2(0), 0x10, 0x0000 },
 		{ PUC_COM_POW2(0), 0x14, 0x0000 },
+	    },
+	},
+	{   /* "WinChipHead CH382 (2S)", */
+	    {	PCI_VENDOR_WCH2, PCI_PRODUCT_WCH2_CH382_1,	0, 0	},
+	    {	0xffff,	0xffff,					0, 0	},
+	    {
+		{ PUC_COM_POW2(0), 0x10, 0x00c0 },
+		{ PUC_COM_POW2(0), 0x10, 0x00c8 },
+	    },
+	},
+	{   /* "WinChipHead CH382 (2S1P)", */
+	    {	PCI_VENDOR_WCH2, PCI_PRODUCT_WCH2_CH382_2,	0, 0	},
+	    {	0xffff,	0xffff,					0, 0	},
+	    {
+		{ PUC_COM_POW2(0), 0x10, 0x00c0 },
+		{ PUC_COM_POW2(0), 0x10, 0x00c8 },
 	    },
 	},
 	{   /* "NetMos NM9820 UART" */
