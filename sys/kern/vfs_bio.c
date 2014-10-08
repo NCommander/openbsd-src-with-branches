7050b8a40819c13ac5592c9b838ade67d5764f6d
blambert <blambert>
2014-10-08 07:33:14 +0000

    Now that the cleaner yields the cpu, we can stop checking to see if we've hogged the cpu for >1 second.
    okay miod@

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vfs_bio.c,v 1.161 2014/08/31 21:08:48 tedu Exp $	*/
+/*	$OpenBSD: vfs_bio.c,v 1.162 2014/09/09 07:07:39 blambert Exp $	*/
 /*	$NetBSD: vfs_bio.c,v 1.44 1996/06/11 11:15:36 pk Exp $	*/
 
 /*
@@ -998,7 +998,6 @@ buf_get(struct vnode *vp, daddr_t blkno, size_t size)
 void
 buf_daemon(struct proc *p)
 {
-	struct timeval starttime, timediff;
 	struct buf *bp = NULL;
 	int s, pushed = 0;
 
@@ -1021,10 +1020,7 @@ buf_daemon(struct proc *p)
 			tsleep(&bd_req, PRIBIO - 7, "cleaner", 0);
 		}
 
-		getmicrouptime(&starttime);
-
 		while ((bp = bufcache_getdirtybuf())) {
-			struct timeval tv;
 
 			if (UNCLEAN_PAGES < lodirtypages &&
 			    bcstats.kvaslots_avail > 2 * RESERVE_SLOTS &&
@@ -1059,13 +1055,7 @@ buf_daemon(struct proc *p)
 
 			sched_pause();
 
-			/* Never allow processing to run for more than 1 sec */
-			getmicrouptime(&tv);
-			timersub(&tv, &starttime, &timediff);
 			s = splbio();
-			if (timediff.tv_sec)
-				break;
-
 		}
 	}
 }
