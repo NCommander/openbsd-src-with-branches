6106abc406dd17d53ef900441076b0bfed4a6987
schwarze <schwarze>
2014-10-09 15:59:08 +0000

    parse and render "from" and "to" clauses in eqn, and render matrices; written by kristaps@ during EuroBSDCon

--- usr.bin/mandoc/html.h
+++ usr.bin/mandoc/html.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: html.h,v 1.25 2014/10/07 18:17:05 schwarze Exp $ */
+/*	$OpenBSD: html.h,v 1.26 2014/10/09 15:49:09 schwarze Exp $ */
 /*
  * Copyright (c) 2008-2011, 2014 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -63,6 +63,9 @@ enum	htmltag {
 	TAG_MTABLE,
 	TAG_MTR,
 	TAG_MTD,
+	TAG_MUNDEROVER,
+	TAG_MUNDER,
+	TAG_MOVER,
 	TAG_MAX
 };
 
