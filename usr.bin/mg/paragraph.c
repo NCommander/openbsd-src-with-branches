4c041e8e315a74bc34eab0199aa307095c72c751
lum <lum>
2014-10-13 21:01:05 +0000

    gotoeop() does not behave as expected when there is no '\n' at the end of the buffer. ok florian@

--- usr.bin/mg/paragraph.c
+++ usr.bin/mg/paragraph.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: paragraph.c,v 1.31 2014/03/27 09:30:55 florian Exp $	*/
+/*	$OpenBSD: paragraph.c,v 1.32 2014/10/12 18:09:41 lum Exp $	*/
 
 /* This file is in the public domain. */
 
@@ -92,6 +92,10 @@ gotoeop(int f, int n)
 			curwp->w_dotline++;
 		}
 	}
+	/* covers corner case of no '\n' at end of buffer */
+	if (lforw(curwp->w_dotp) == curbp->b_headp)
+		gotoeol(FFRAND, 1);
+
 	/* force screen update */
 	curwp->w_rflag |= WFMOVE;
 	return (TRUE);
