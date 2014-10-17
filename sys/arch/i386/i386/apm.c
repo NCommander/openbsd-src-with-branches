4805970fda51b5a42d6193a237fdece8098bd078
deraadt <deraadt>
2014-10-17 15:35:31 +0000

    revert previous.  It isn't just i386 which cares about the setperf hiding, it is a lot of other systems too.

--- sys/arch/i386/i386/apm.c
+++ sys/arch/i386/i386/apm.c
@@ -241,9 +241,7 @@ apm_perror(const char *str, struct apmregs *regs)
 void
 apm_suspend(int state)
 {
-#ifndef SMALL_KERNEL
 	extern int perflevel;
-#endif
 	int s;
 
 #if NWSDISPLAY > 0
@@ -278,11 +276,9 @@ apm_suspend(int state)
 	enable_intr();
 	splx(s);
 
-#ifndef SMALL_KERNEL
 	/* restore hw.setperf */
 	if (cpu_setperf != NULL)
 		cpu_setperf(perflevel);
-#endif
 	bufq_restart();
 
 	config_suspend_all(DVACT_WAKEUP);
