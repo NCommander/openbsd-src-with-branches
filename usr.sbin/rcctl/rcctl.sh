80daa71fdc8d90d809977c465cc6c0e024e6afcb
ajacoutot <ajacoutot>
2014-10-31 09:07:48 +0000

    Make default output matches status. Hackish but we are not allowed to use svc_default_enabled_flags like any other function...

--- usr.sbin/rcctl/rcctl.sh
+++ usr.sbin/rcctl/rcctl.sh
@@ -100,7 +100,9 @@ svc_get_defaults()
 	local _i _svc=$1
 
 	if [ -n "${_svc}" ]; then
-		print -r -- "$(svc_default_enabled_flags ${_svc})"
+		# XXX would be much simpler to just do that:
+		#svc_default_enabled_flags ${_svc}
+		print -r -- "$(svc_default_enabled_flags ${_svc})" | sed '/^$/d'
 		svc_default_enabled ${_svc}
 	else
 		for _i in $(ls_rcscripts); do
