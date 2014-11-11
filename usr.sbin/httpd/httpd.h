29a101477c187972aec2ddeec1a3e3aa694fb4fc
beck <beck>
2014-11-11 15:54:45 +0000

    Allow the log directory to be configurable in the config file, rather than fixed as /logs within the chroot.  As this httpd is properly privesp'ed this has the nice property of allowing us to put the logs outside the chroot if we want to. ok reyk@

--- usr.sbin/httpd/httpd.h
+++ usr.sbin/httpd/httpd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: httpd.h,v 1.61 2014/10/31 13:49:52 jsing Exp $	*/
+/*	$OpenBSD: httpd.h,v 1.62 2014/11/03 18:43:24 bluhm Exp $	*/
 
 /*
  * Copyright (c) 2006 - 2014 Reyk Floeter <reyk@openbsd.org>
@@ -433,6 +433,7 @@ struct httpd {
 	u_int16_t		 sc_id;
 	int			 sc_paused;
 	char			*sc_chroot;
+	char			*sc_logdir;
 
 	struct serverlist	*sc_servers;
 	struct mediatypes	*sc_mediatypes;
