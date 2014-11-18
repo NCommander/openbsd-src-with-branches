8374fc04742cafc11e87e9f24efdbfece719ad3c
dlg <dlg>
2014-11-18 12:41:16 +0000

    udf needs siphash to build.
    i dont know if this is the right fix, but the tree needs to build.

--- lib/libkvm/kvm_udf.c
+++ lib/libkvm/kvm_udf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kvm_udf.c,v 1.6 2013/10/22 16:40:27 guenther Exp $	*/
+/*	$OpenBSD: kvm_udf.c,v 1.7 2013/11/16 00:37:11 guenther Exp $	*/
 
 /*
  * Copyright (c) 2001, 2002 Scott Long <scottl@freebsd.org>
@@ -34,6 +34,8 @@
 #include <sys/vnode.h>
 #include <sys/sysctl.h>
 #include <sys/specdev.h>
+
+#include <crypto/siphash.h>
 
 #include <isofs/udf/ecma167-udf.h>
 #include <isofs/udf/udf.h>
