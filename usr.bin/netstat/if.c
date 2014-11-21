fd5b9579bbb79496e20fd34bc70edc0b6d9bd0bd
mikeb <mikeb>
2014-11-21 17:49:00 +0000

    remove pointless if_var.h and socketvar.h includes; ok deraadt

--- usr.bin/netstat/if.c
+++ usr.bin/netstat/if.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if.c,v 1.67 2013/11/21 17:32:13 mikeb Exp $	*/
+/*	$OpenBSD: if.c,v 1.68 2014/06/23 03:46:17 guenther Exp $	*/
 /*	$NetBSD: if.c,v 1.16.4.2 1996/06/07 21:46:46 thorpej Exp $	*/
 
 /*
@@ -38,7 +38,6 @@
 #include <sys/sysctl.h>
 
 #include <net/if.h>
-#include <net/if_var.h>
 #include <net/if_dl.h>
 #include <net/if_types.h>
 #include <net/route.h>
--- usr.bin/netstat/net80211.c
+++ usr.bin/netstat/net80211.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: net80211.c,v 1.10 2013/11/21 17:32:13 mikeb Exp $	*/
+/*	$OpenBSD: net80211.c,v 1.11 2014/08/14 12:55:50 mpi Exp $	*/
 
 /*
  * Copyright (c) 2005 Reyk Floeter <reyk@openbsd.org>
@@ -23,7 +23,6 @@
 #include <sys/ioctl.h>
 
 #include <net/if.h>
-#include <net/if_var.h>
 
 #include <netinet/in.h>
 #include <netinet/if_ether.h>
