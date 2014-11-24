ba1922c7954ec2c0f327e1fd71d6009ee9c9fd7c
jsg <jsg>
2014-11-24 05:48:25 +0000

    remove a surplus break statement

--- usr.sbin/relayd/snmp.c
+++ usr.sbin/relayd/snmp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: snmp.c,v 1.17 2014/06/25 11:05:15 reyk Exp $	*/
+/*	$OpenBSD: snmp.c,v 1.18 2014/11/19 10:24:40 blambert Exp $	*/
 
 /*
  * Copyright (c) 2008 - 2014 Reyk Floeter <reyk@openbsd.org>
@@ -532,7 +532,6 @@ snmp_agentx_process(struct agentx_handle *h, struct agentx_pdu *pdu, void *arg)
 			if (snmp_agentx_response(h, pdu) == -1)
 				break;
 			break;
-		break;
 
 		case AGENTX_UNREGISTER:
 			if (snmp_agentx_response(h, pdu) == -1)
