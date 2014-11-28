48ab64d794e929bd4884a1d2aa69fd4cbf4256ec
schwarze <schwarze>
2014-11-28 05:51:29 +0000

    Simplify by making many functions in the man(7) parser void, and some cleanup; no functional change, minus 70 lines.

--- usr.bin/mandoc/man.h
+++ usr.bin/mandoc/man.h
@@ -1,4 +1,4 @@
-/*	$Id: man.h,v 1.44 2014/06/20 22:58:41 schwarze Exp $ */
+/*	$Id: man.h,v 1.45 2014/11/27 14:31:29 deraadt Exp $ */
 /*
  * Copyright (c) 2009, 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2014 Ingo Schwarze <schwarze@openbsd.org>
@@ -66,7 +66,6 @@ enum	man_type {
 	MAN_BLOCK,
 	MAN_HEAD,
 	MAN_BODY,
-	MAN_TAIL,
 	MAN_TBL,
 	MAN_EQN
 };
