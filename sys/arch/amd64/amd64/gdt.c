21931a360bd2e4a48e3eff58f07d7431b874aa59
tedu <tedu>
2014-12-15 01:53:45 +0000

    change bcopy to memcpy. ok deraadt

--- sys/arch/amd64/amd64/gdt.c
+++ sys/arch/amd64/amd64/gdt.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: gdt.c,v 1.20 2014/09/14 14:17:23 jsg Exp $	*/
+/*	$OpenBSD: gdt.c,v 1.21 2014/11/16 12:30:56 deraadt Exp $	*/
 /*	$NetBSD: gdt.c,v 1.1 2003/04/26 18:39:28 fvdl Exp $	*/
 
 /*-
@@ -67,7 +67,7 @@ gdt_alloc_cpu(struct cpu_info *ci)
 			panic("gdt_init: no pages");
 		pmap_kenter_pa(va, VM_PAGE_TO_PHYS(pg), PROT_READ | PROT_WRITE);
 	}
-	bcopy(gdtstore, ci->ci_gdt, GDT_SIZE);
+	memcpy(ci->ci_gdt, gdtstore, GDT_SIZE);
 	bzero(ci->ci_tss, sizeof(*ci->ci_tss));
 }
 
