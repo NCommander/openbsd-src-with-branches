7c5c93222c1e2eae33fdd20300d695dd80d294be
kurt <kurt>
2014-12-22 21:58:25 +0000

    Self-relocation code for i386.

--- libexec/ld.so/i386/archdep.h
+++ libexec/ld.so/i386/archdep.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: archdep.h,v 1.10 2004/05/24 20:16:12 drahn Exp $ */
+/*	$OpenBSD: archdep.h,v 1.11 2010/01/02 12:16:35 kettenis Exp $ */
 
 /*
  * Copyright (c) 1998 Per Fogelstrom, Opsycon AB
@@ -67,6 +67,8 @@ RELOC_REL(Elf32_Rel *r, const Elf32_Sym *s, Elf32_Addr *p, unsigned long v)
 		*p += v;
 	} else if (ELF32_R_TYPE(r->r_info) == RELOC_GLOB_DAT) {
 		*p += v + s->st_value;
+	} else if (ELF32_R_TYPE(r->r_info) == RELOC_32) {
+		*p += v + s->st_value;
 	} else {
 		_dl_printf("unknown bootstrap relocation\n");
 		_dl_exit(6);
@@ -83,6 +85,7 @@ RELOC_RELA(Elf32_Rela *r, const Elf32_Sym *s, Elf32_Addr *p, unsigned long v,
 		*p = v + r->r_addend;
 	} else if (ELF32_R_TYPE(r->r_info) == RELOC_GLOB_DAT) {
 		*p = v + s->st_value + r->r_addend;
+	} else if (ELF32_R_TYPE(r->r_info) == RELOC_NONE) {
 	} else {
 		_dl_printf("unknown bootstrap relocation\n");
 		_dl_exit(6);
