6f184df9090e4f79fa08945a641b48bb33c1d97e
bluhm <bluhm>
2014-12-28 11:11:01 +0000

    With revision 1.93 a space character got lost when printing the signal action.  Print the space again. OK jsg@

--- usr.bin/kdump/kdump.c
+++ usr.bin/kdump/kdump.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kdump.c,v 1.94 2014/12/15 01:48:54 guenther Exp $	*/
+/*	$OpenBSD: kdump.c,v 1.95 2014/12/15 07:01:12 jmc Exp $	*/
 
 /*-
  * Copyright (c) 1988, 1993
@@ -1292,6 +1292,7 @@ static void
 ktrpsig(struct ktr_psig *psig)
 {
 	signame(psig->signo);
+	printf(" ");
 	if (psig->action == SIG_DFL)
 		(void)printf("SIG_DFL");
 	else {
