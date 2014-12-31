819a9d16111256c73027aa53c8e0291227c7df21
aoyama <aoyama>
2014-12-31 11:38:27 +0000

    Correct evcount_attach argument on cbus(4).
    The third argument of evcount_attach() should point some permanent
    variable, not an automatic variable.  Now "vmstat -i" correctly prints
    C-bus interrupt level number as irq, on C-bus devices.
    
    ok miod@

--- sys/arch/luna88k/cbus/cbusvar.h
+++ sys/arch/luna88k/cbus/cbusvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cbusvar.h,v 1.1 2014/12/08 13:24:04 aoyama Exp $	*/
+/*	$OpenBSD: cbusvar.h,v 1.2 2014/12/28 13:03:18 aoyama Exp $	*/
 
 /*
  * Copyright (c) 2014 Kenji Aoyama.
@@ -34,6 +34,7 @@
 struct cbus_isr_t {
 	int		(*isr_func)(void *);
 	void		*isr_arg;
+	int		isr_intlevel;
 	struct evcount	isr_count;
 };
 
