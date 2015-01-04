cbb69aa65c86202adc97cec4fd4099a65000aa9a
jsg <jsg>
2015-01-04 02:53:19 +0000

    avoid the use of an uninitialised variable in one of the codepaths in pool_setlowat()
    ok dlg@ tedu@

--- sys/kern/subr_pool.c
+++ sys/kern/subr_pool.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: subr_pool.c,v 1.173 2014/12/22 00:33:40 dlg Exp $	*/
+/*	$OpenBSD: subr_pool.c,v 1.174 2014/12/22 02:59:53 tedu Exp $	*/
 /*	$NetBSD: subr_pool.c,v 1.61 2001/09/26 07:14:56 chs Exp $	*/
 
 /*-
@@ -863,7 +863,7 @@ pool_update_curpage(struct pool *pp)
 void
 pool_setlowat(struct pool *pp, int n)
 {
-	int prime;
+	int prime = 0;
 
 	mtx_enter(&pp->pr_mtx);
 	pp->pr_minitems = n;
