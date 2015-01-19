5c27445f34a98be0eb74133ab3991e3ff3dcd1c0
deraadt <deraadt>
2015-01-19 01:19:17 +0000

    unneccessary cmask variable; ok guenther

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: init_main.c,v 1.231 2014/12/31 15:32:05 jsing Exp $	*/
+/*	$OpenBSD: init_main.c,v 1.232 2015/01/13 18:51:27 kettenis Exp $	*/
 /*	$NetBSD: init_main.c,v 1.84.4.1 1996/06/02 09:08:06 mrg Exp $	*/
 
 /*
@@ -123,7 +123,6 @@ extern struct timeout setperf_to;
 void setperf_auto(void *);
 #endif
 
-int	cmask = CMASK;
 extern	struct user *proc0paddr;
 
 struct	vnode *rootvp, *swapdev_vp;
