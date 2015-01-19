43428411e61c59203447a150c2134c1f14abec42
miod <miod>
2015-01-19 20:25:36 +0000

    Missing initialization; tjenahej@speedmail.se

--- usr.bin/nm/elf.c
+++ usr.bin/nm/elf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: elf.c,v 1.23 2013/11/26 13:19:07 deraadt Exp $	*/
+/*	$OpenBSD: elf.c,v 1.24 2015/01/16 06:40:10 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2003 Michael Shalayeff
@@ -550,7 +550,7 @@ elf_symload(const char *name, FILE *fp, off_t foff, Elf_Ehdr *eh,
 	}
 
 	stab = NULL;
-	*pnames = NULL; *psnames = NULL;
+	*pnames = NULL; *psnames = NULL; *pnrawnames = 0;
 	elf_symloadx(name, fp, foff, eh, shdr, shstr, pnames,
 	    psnames, pstabsize, pnrawnames, ELF_STRTAB, ELF_SYMTAB);
 	if (stab == NULL) {
