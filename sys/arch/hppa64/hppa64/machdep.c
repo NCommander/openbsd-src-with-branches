c2306d617e6871a60798f16d423a65d0c97fc7b6
kettenis <kettenis>
2015-01-20 19:43:21 +0000

    Move ps_strings "after" the random stackgap.  This makes its location a per-process value, and therefpore turns the VM_PSSTRINGS sysctl into a per-process one as well.  This gets rid of a pointer to the bottom of the stack at a fixed location.  Also clears the road for unmapping the stackgap.
    ok deraadt@

--- sys/arch/hppa64/hppa64/machdep.c
+++ sys/arch/hppa64/hppa64/machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: machdep.c,v 1.75 2014/10/25 17:31:26 kettenis Exp $	*/
+/*	$OpenBSD: machdep.c,v 1.76 2014/12/10 15:29:53 mikeb Exp $	*/
 
 /*
  * Copyright (c) 2005 Michael Shalayeff
@@ -808,7 +808,7 @@ setregs(struct proc *p, struct exec_package *pack, u_long stack,
 	tf->tf_iioq[1] = 4 +
 	    (tf->tf_iioq[0] = pack->ep_entry | HPPA_PC_PRIV_USER);
 	tf->tf_rp = 0;
-	tf->tf_args[0] = (u_long)PS_STRINGS;
+	tf->tf_args[0] = p->p_p->ps_strings;
 	tf->tf_args[1] = tf->tf_args[2] = 0; /* XXX dynload stuff */
 
 	/* setup terminal stack frame */
