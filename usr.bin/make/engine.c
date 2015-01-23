4bc4c4c8d595cf5379abbc8fa261c5ae2d79592a
espie <espie>
2015-01-23 13:18:40 +0000

    remove a bunch of dangerous casts (useless casts from void * to something else, in some cases by adding extra temporary variables. IMO, it's much better practice to do
    void *a;
    int *p = a;
    *p = 42;
    
    rather than
    void *a;
    *(int *)a = 42;
    
    okay miod@... to be revisited for some possible const additions later.

--- usr.bin/make/engine.c
+++ usr.bin/make/engine.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: engine.c,v 1.48 2014/05/30 21:19:57 espie Exp $ */
+/*	$OpenBSD: engine.c,v 1.49 2014/10/31 13:29:42 gsoares Exp $ */
 /*
  * Copyright (c) 2012 Marc Espie.
  *
@@ -427,7 +427,7 @@ Make_DoAllVar(GNode *gn)
 static void
 MakeTimeStamp(void *parent, void *child)
 {
-    Make_TimeStamp((GNode *)parent, (GNode *)child);
+    Make_TimeStamp(parent, child);
 }
 
 bool
