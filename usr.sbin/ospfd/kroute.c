97e92b4e3c8b07b68e822cac1b4a03e7d02d372d
claudio <claudio>
2015-02-10 08:26:47 +0000

    Forgot the EAGAIN or EINTR check on read.

--- usr.sbin/ospfd/kroute.c
+++ usr.sbin/ospfd/kroute.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kroute.c,v 1.95 2015/01/16 06:40:19 deraadt Exp $ */
+/*	$OpenBSD: kroute.c,v 1.96 2015/02/10 05:24:48 claudio Exp $ */
 
 /*
  * Copyright (c) 2004 Esben Norby <norby@openbsd.org>
@@ -1307,6 +1307,8 @@ dispatch_rtmsg(void)
 	ssize_t			 n;
 
 	if ((n = read(kr_state.fd, &buf, sizeof(buf))) == -1) {
+		if (errno == EAGAIN || errno == EINTR)
+			return (0);
 		log_warn("dispatch_rtmsg: read error");
 		return (-1);
 	}
