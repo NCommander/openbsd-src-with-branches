e6271c7cd9d55465bfafdfb0065cf734a40947b2
dlg <dlg>
2015-02-11 06:39:24 +0000

    this doesnt need lockmgr. we dont need sys/lock.h

--- sys/arch/amd64/amd64/lock_machdep.c
+++ sys/arch/amd64/amd64/lock_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: lock_machdep.c,v 1.5 2014/03/14 02:08:57 dlg Exp $	*/
+/*	$OpenBSD: lock_machdep.c,v 1.6 2014/07/10 12:14:48 mlarkin Exp $	*/
 
 /*
  * Copyright (c) 2007 Artur Grabowski <art@openbsd.org>
@@ -18,7 +18,6 @@
 
 
 #include <sys/param.h>
-#include <sys/lock.h>
 #include <sys/systm.h>
 
 #include <machine/atomic.h>
