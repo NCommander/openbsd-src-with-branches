9c2eb4d34a048aae0558585dee5e6938ead4c1c5
uebayasi <uebayasi>
2015-02-12 05:09:46 +0000

    Don't panic even if parent port doesn't exist.

--- sys/dev/usb/dwc2/dwc2.c
+++ sys/dev/usb/dwc2/dwc2.c
@@ -1677,8 +1677,10 @@ void dwc2_host_hub_info(struct dwc2_hsotg *hsotg, void *context, int *hub_addr,
 	struct dwc2_pipe *dpipe = DWC2_XFER2DPIPE(xfer);
 	struct usbd_device *dev = dpipe->pipe.device;
 
-	*hub_addr = dev->myhsport->parent->address;
- 	*hub_port = dev->myhsport->portno;
+	if (dev->myhsport != NULL) {
+		*hub_addr = dev->myhsport->parent->address;
+ 		*hub_port = dev->myhsport->portno;
+	}
 }
 
 int dwc2_host_get_speed(struct dwc2_hsotg *hsotg, void *context)
