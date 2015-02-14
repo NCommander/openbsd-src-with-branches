2c96a864a9daf166208c06e739a466e943ed4a5a
uebayasi <uebayasi>
2015-02-14 06:18:58 +0000

    Define UPS_PORT_TEST as FreeBSD/NetBSD do.

--- sys/dev/usb/usb.h
+++ sys/dev/usb/usb.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: usb.h,v 1.48 2014/11/01 14:04:27 mpi Exp $ */
+/*	$OpenBSD$ */
 /*	$NetBSD: usb.h,v 1.69 2002/09/22 23:20:50 augustss Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/usb.h,v 1.14 1999/11/17 22:33:46 n_hibma Exp $	*/
 
@@ -430,6 +430,7 @@ typedef struct {
 #define UPS_LOW_SPEED			0x0200
 #define UPS_HIGH_SPEED			0x0400
 #define UPS_SUPER_SPEED			0x0800
+#define UPS_PORT_TEST			0x0800
 #define UPS_PORT_INDICATOR		0x1000
 
 	uWord		wPortChange;
