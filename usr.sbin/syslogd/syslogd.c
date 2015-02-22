f64ff3d6a14eee8c87f5d265c6a416c0ab2e184c
jsing <jsing>
2015-02-22 15:09:54 +0000

    Rename tls_config_insecure_noverifyhost() to tls_config_insecure_noverifyname(), so that it is more accurate and keeps inline with the distinction between DNS hostname and server name.
    Requested by tedu@ during s2k15.

--- usr.sbin/syslogd/syslogd.c
+++ usr.sbin/syslogd/syslogd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: syslogd.c,v 1.157 2015/02/20 00:56:32 bluhm Exp $	*/
+/*	$OpenBSD: syslogd.c,v 1.158 2015/02/22 14:55:41 jsing Exp $	*/
 
 /*
  * Copyright (c) 1983, 1988, 1993, 1994
@@ -518,8 +518,8 @@ main(int argc, char *argv[])
 	} else if ((tlsconfig = tls_config_new()) == NULL) {
 		logerror("tls_config_new");
 	} else if (NoVerify) {
-		tls_config_insecure_noverifyhost(tlsconfig);
 		tls_config_insecure_noverifycert(tlsconfig);
+		tls_config_insecure_noverifyname(tlsconfig);
 	} else {
 		struct stat sb;
 
