fc6db1728d690c808dfbb8d233c62a684e058ac2
aoyama <aoyama>
2015-03-03 23:50:37 +0000

    Add pcic(4) support on luna88k.
    This is basically glue codes attaching pcic(4) at cbus(4/luna88k),
    based on src/sys/dev/isa/i82365_isa{,subr}.c.
    
    Tested on some ne(4) network interface cards, wdc(4) with a CF memory
    card, and com(4) with an old XJACK modem card.  Currently wi(4) does
    not work yet.
    
    suggestions and ok miod@

--- sys/arch/m88k/m88k/softintr.c
+++ sys/arch/m88k/m88k/softintr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: softintr.c,v 1.4 2014/07/12 18:44:42 tedu Exp $	*/
+/*	$OpenBSD: softintr.c,v 1.5 2014/12/24 22:48:27 miod Exp $	*/
 /*	$NetBSD: softintr.c,v 1.2 2003/07/15 00:24:39 lukem Exp $	*/
 
 /*
@@ -113,6 +113,7 @@ softintr_establish(int ipl, void (*func)(void *), void *arg)
 	case IPL_SOFTNET:
 		si = SI_SOFTNET;
 		break;
+	case IPL_TTY:
 	case IPL_SOFTTTY:
 		si = SI_SOFTTTY;
 		break;
