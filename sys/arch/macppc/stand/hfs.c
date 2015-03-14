004eb4c23def79c6b14dce5dd1c0af8f9cfa0d61
miod <miod>
2015-03-14 20:52:41 +0000

    Allow for multiple concurrent devopen() calls, and fill the .readdir member of fs_ops; makes the `ls' command finally work in the boot loader.

--- sys/arch/macppc/stand/hfs.c
+++ sys/arch/macppc/stand/hfs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: hfs.c,v 1.4 2005/09/26 19:55:47 kettenis Exp $	*/
+/*	$OpenBSD: hfs.c,v 1.5 2011/11/19 16:15:11 mpi Exp $	*/
 /*	$NetBSD: hfs.c,v 1.1 2000/11/14 11:25:35 tsubai Exp $	*/
 
 /*-
@@ -103,4 +103,10 @@ int
 hfs_stat(struct open_file *f, struct stat *sb)
 {
 	return 0;
+}
+
+int
+hfs_readdir(struct open_file *f, char *name)
+{
+	return ENXIO;
 }
