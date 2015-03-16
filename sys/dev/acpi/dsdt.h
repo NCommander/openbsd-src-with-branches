bf517bb6806b81fc18d4cca815e3b68e144d5fcb
deraadt <deraadt>
2015-03-16 20:31:47 +0000

    Reduce use of SMALL_KERNEL in acpi, bringing acpiec driver and more gpe handling into RAMDISK.  This is now possible because the install media has ample room.  The goal is to reduce special cases where we may be skipping (unknown) important operations... ok mlarkin kettenis

--- sys/dev/acpi/dsdt.h
+++ sys/dev/acpi/dsdt.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: dsdt.h,v 1.61 2013/01/18 04:07:06 pirofti Exp $ */
+/* $OpenBSD: dsdt.h,v 1.62 2014/05/02 14:10:15 kettenis Exp $ */
 /*
  * Copyright (c) 2005 Marco Peereboom <marco@openbsd.org>
  *
@@ -277,8 +277,8 @@ int			aml_rdpciaddr(struct aml_node *pcidev,
 #ifndef SMALL_KERNEL
 void			acpi_getdevlist(struct acpi_devlist_head *,
 			    struct aml_node *, struct aml_value *, int);
-void			aml_notify_dev(const char *, int);
 #endif
+void			aml_notify_dev(const char *, int);
 
 void			acpi_freedevlist(struct acpi_devlist_head *);
 
