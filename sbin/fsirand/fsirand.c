5f55977289f2e5acdaadb8f9fc2e0e402668d0b2
millert <millert>
2015-03-20 01:53:05 +0000

    Fix cut & pasto in warning message; from Andre Smagin

--- sbin/fsirand/fsirand.c
+++ sbin/fsirand/fsirand.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fsirand.c,v 1.32 2015/01/20 18:22:21 deraadt Exp $	*/
+/*	$OpenBSD: fsirand.c,v 1.33 2015/02/07 02:09:13 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1997 Todd C. Miller <Todd.Miller@courtesan.com>
@@ -75,7 +75,7 @@ main(int argc, char *argv[])
 	if (getrlimit(RLIMIT_DATA, &rl) == 0) {
 		rl.rlim_cur = rl.rlim_max;
 		if (setrlimit(RLIMIT_DATA, &rl) < 0)
-			warn("Can't get resource limit to max data size");
+			warn("Can't set resource limit to max data size");
 	} else
 		warn("Can't get resource limit for data size");
 
