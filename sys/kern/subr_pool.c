5ae29cea1bc6877a923cb77a18e5fa3bee6c25f5
dlg <dlg>
2015-03-20 11:33:17 +0000

    reintroduce r1.173:
    > if we're able to use large page allocators, try and place at least
    > 8 items on a page. this reduces the number of allocator operations
    > we have to do per item on large items.
    
    this was backed out because of fallout on landisk which has since
    been fixed. putting this in again early in the cycle so we can look
    for more fallout. hopefully it will stick.
    
    ok deraadt@

--- sys/kern/subr_pool.c
+++ sys/kern/subr_pool.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: subr_pool.c,v 1.180 2015/02/10 06:16:13 dlg Exp $	*/
+/*	$OpenBSD: subr_pool.c,v 1.181 2015/03/14 03:38:50 jsg Exp $	*/
 /*	$NetBSD: subr_pool.c,v 1.61 2001/09/26 07:14:56 chs Exp $	*/
 
 /*-
@@ -233,7 +233,7 @@ pool_init(struct pool *pp, size_t size, u_int align, u_int ioff, int flags,
 	size = roundup(size, align);
 
 	if (palloc == NULL) {
-		while (size > pgsize)
+		while (size * 8 > pgsize)
 			pgsize <<= 1;
 
 		if (pgsize > PAGE_SIZE) {
