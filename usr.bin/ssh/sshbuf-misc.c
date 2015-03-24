5f6da784c3d0491cfcaf8a41dfb1921b79d113aa
markus <markus>
2015-03-24 20:03:44 +0000

    correct fmt-string for size_t as noted by Nicholas Lemonias; ok djm@

--- usr.bin/ssh/sshbuf-misc.c
+++ usr.bin/ssh/sshbuf-misc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sshbuf-misc.c,v 1.2 2014/06/24 01:13:21 djm Exp $	*/
+/*	$OpenBSD: sshbuf-misc.c,v 1.3 2015/02/05 12:59:57 millert Exp $	*/
 /*
  * Copyright (c) 2011 Damien Miller
  *
@@ -38,7 +38,7 @@ sshbuf_dump_data(const void *s, size_t len, FILE *f)
 	const u_char *p = (const u_char *)s;
 
 	for (i = 0; i < len; i += 16) {
-		fprintf(f, "%.4zd: ", i);
+		fprintf(f, "%.4zu: ", i);
 		for (j = i; j < i + 16; j++) {
 			if (j < len)
 				fprintf(f, "%02x ", p[j]);
