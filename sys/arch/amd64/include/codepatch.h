8c7cb2894103d5b2b5cea7ab2bb5aae7c38c8dc1
sf <sf>
2015-04-19 19:45:21 +0000

    Add support for x2apic mode
    This is currently only enabled on hypervisors because on real hardware, it
    requires interrupt remapping which we don't support yet. But on virtualization
    it reduces the number of vmexits required per IPI from 4 to 1, causing a
    significant speed-up for MP guests.
    
    ok kettenis@

--- sys/arch/amd64/include/codepatch.h
+++ sys/arch/amd64/include/codepatch.h
@@ -1,4 +1,4 @@
-/*      $OpenBSD: codepatch.h,v Exp $    */
+/*      $OpenBSD: codepatch.h,v 1.1 2015/01/16 10:17:51 sf Exp $    */
 /*
  * Copyright (c) 2014-2015 Stefan Fritsch <sf@sfritsch.de>
  *
@@ -48,5 +48,6 @@ void codepatch_call(uint16_t tag, void *func);
 
 #define CPTAG_STAC		1
 #define CPTAG_CLAC		2
+#define CPTAG_EOI		3
 
 #endif /* _MACHINE_CODEPATCH_H_ */
