e2b16ba23f0f8ec9cc345e73f8893bbd499d7297
dtucker <dtucker>
2015-04-23 05:01:19 +0000

    Add a simple regression test for sshd's configuration parser.  Right now, all it does is run the output of sshd -T back through itself and ensure the output is valid and invariant.

---
+++ regress/usr.bin/ssh/cfgparse.sh
@@ -0,0 +1,16 @@
+#	$OpenBSD$
+#	Placed in the Public Domain.
+
+tid="config parse"
+
+verbose "reparse default config"
+($SUDO ${SSHD} -T -f /dev/null >$OBJ/sshd_config.1 &&
+ $SUDO ${SSHD} -T -f $OBJ/sshd_config.1 >$OBJ/sshd_config.2 &&
+ diff $OBJ/sshd_config.1 $OBJ/sshd_config.2) || fail "reparse default config"
+
+verbose "reparse regress config"
+($SUDO ${SSHD} -T -f $OBJ/sshd_config >$OBJ/sshd_config.1 &&
+ $SUDO ${SSHD} -T -f $OBJ/sshd_config.1 >$OBJ/sshd_config.2 &&
+ diff $OBJ/sshd_config.1 $OBJ/sshd_config.2) || fail "reparse regress config"
+
+rm -f $OBJ/sshd_config.[12]
