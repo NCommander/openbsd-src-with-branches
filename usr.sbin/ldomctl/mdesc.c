9d4a2c327d3cf39ed5b9fa313c43ab559613a38f
jsg <jsg>
2015-05-23 14:26:06 +0000

    fix a memory leak in an error path

--- usr.sbin/ldomctl/mdesc.c
+++ usr.sbin/ldomctl/mdesc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mdesc.c,v 1.7 2012/11/26 20:08:15 kettenis Exp $	*/
+/*	$OpenBSD: mdesc.c,v 1.8 2015/02/09 04:27:25 jsg Exp $	*/
 
 /*
  * Copyright (c) 2012 Mark Kettenis
@@ -622,6 +622,7 @@ md_read(const char *path)
 	buf = xmalloc(size);
 	if (fread(buf, size, 1, fp) != 1) {
 		fclose(fp);
+		free(buf);
 		return NULL;
 	}
 
