2800a0c721ca47a978296ad4ecfd8de738bb3f76
nicm <nicm>
2015-05-27 13:28:04 +0000

    Move the jobs output cache into the formats code so that #() work more generally (for example, again working in set-titles-string).

--- usr.bin/tmux/server.c
+++ usr.bin/tmux/server.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: server.c,v 1.121 2015/04/22 15:30:11 nicm Exp $ */
+/* $OpenBSD: server.c,v 1.122 2015/04/24 23:17:11 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -484,6 +484,8 @@ server_second_callback(unused int fd, unused short events, unused void *arg)
 	}
 
 	server_client_status_timer();
+
+	format_clean();
 
 	evtimer_del(&server_ev_second);
 	memset(&tv, 0, sizeof tv);
