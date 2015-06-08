f9813bbf8abf49e56ab5fc2184c9ca6ed16cdbb2
claudio <claudio>
2015-06-08 15:47:51 +0000

    Introduce a state on the ctl_relay_event struct. This makes it possible to better track the connection state of a session and stops doing double opens in certain situations using http relays. Using a state field to simplify the logic since relay_connect() is called multiple times. OK benno@, bluhm@ and running in production for more than a week

--- usr.sbin/relayd/relayd.h
+++ usr.sbin/relayd/relayd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: relayd.h,v 1.208 2015/03/09 17:20:38 reyk Exp $	*/
+/*	$OpenBSD: relayd.h,v 1.209 2015/05/02 13:15:24 claudio Exp $	*/
 
 /*
  * Copyright (c) 2006 - 2015 Reyk Floeter <reyk@openbsd.org>
@@ -200,6 +200,7 @@ struct ctl_relay_event {
 	int			 line;
 	int			 done;
 	int			 timedout;
+	enum { INIT, PENDING, PRECONNECT, CONNECTED } state;
 	enum direction		 dir;
 
 	u_int8_t		*buf;
