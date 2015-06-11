45aaee9f9b131647ad8a4d4d25e2a8dce7ead906
mikeb <mikeb>
2015-06-11 16:03:04 +0000

    Move hzto(9) to the attic;  OK dlg

--- sys/sys/systm.h
+++ sys/sys/systm.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: systm.h,v 1.106 2014/12/10 15:29:53 mikeb Exp $	*/
+/*	$OpenBSD: systm.h,v 1.107 2015/02/10 21:56:10 miod Exp $	*/
 /*	$NetBSD: systm.h,v 1.50 1996/06/09 04:55:09 briggs Exp $	*/
 
 /*-
@@ -222,7 +222,6 @@ u_int32_t arc4random_uniform(u_int32_t);
 
 struct timeval;
 struct timespec;
-int	hzto(const struct timeval *);
 int	tvtohz(const struct timeval *);
 int	tstohz(const struct timespec *);
 void	realitexpire(void *);
