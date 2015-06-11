5a723483e64579b23671559bb15b8ba91bd3d2c2
reyk <reyk>
2015-06-11 18:49:09 +0000

    Use "compliant" header guards by avoiding the reserved '_' namespace.
    Pointed out by Markus Elfring
    
    OK mikeb@ millert@

--- usr.sbin/relayd/http.h
+++ usr.sbin/relayd/http.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: http.h,v 1.5 2014/08/14 10:30:52 reyk Exp $	*/
+/*	$OpenBSD: http.h,v 1.6 2015/05/30 09:38:11 claudio Exp $	*/
 
 /*
  * Copyright (c) 2012 - 2014 Reyk Floeter <reyk@openbsd.org>
@@ -16,8 +16,8 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#ifndef _HTTP_H
-#define _HTTP_H
+#ifndef HTTP_H
+#define HTTP_H
 
 #define HTTP_PORT	80
 #define HTTPS_PORT	443
@@ -200,4 +200,4 @@ struct http_descriptor {
 	struct kvtree		 http_headers;
 };
 
-#endif /* _HTTP_H */
+#endif /* HTTP_H */
--- usr.sbin/snmpd/snmp.h
+++ usr.sbin/snmpd/snmp.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: snmp.h,v 1.12 2014/07/21 01:51:11 guenther Exp $	*/
+/*	$OpenBSD: snmp.h,v 1.13 2014/11/19 10:19:00 blambert Exp $	*/
 
 /*
  * Copyright (c) 2007, 2008, 2012 Reyk Floeter <reyk@openbsd.org>
@@ -16,8 +16,8 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#ifndef SNMP_HEADER
-#define SNMP_HEADER
+#ifndef SNMPD_SNMP_H
+#define SNMPD_SNMP_H
 
 #include <sys/types.h>
 #include <endian.h>
@@ -450,4 +450,4 @@ snmp_agentx_byteorder_native(struct agentx_hdr *h)
 #error "Unknown host byte order"
 #endif
 
-#endif /* SNMP_HEADER */
+#endif /* SNMPD_SNMP_H */
