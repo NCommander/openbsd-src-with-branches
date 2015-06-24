f41d7e26e39cb6e0ad46f2beee38a9022b40f4b3
miod <miod>
2015-06-24 21:35:01 +0000

    Make ARM_KERNEL_BASE explicitely unsigned

--- sys/arch/armv7/include/vmparam.h
+++ sys/arch/armv7/include/vmparam.h
@@ -1,4 +1,4 @@
-/* 	$OpenBSD: vmparam.h,v 1.2 2015/05/10 15:56:28 jsg Exp $	*/
+/* 	$OpenBSD: vmparam.h,v 1.3 2015/05/12 04:31:10 jsg Exp $	*/
 /*	$NetBSD: vmparam.h,v 1.23 2003/05/22 05:47:07 thorpej Exp $	*/
 
 /*
@@ -33,7 +33,7 @@
 #ifndef	_MACHINE_VMPARAM_H_
 #define	_MACHINE_VMPARAM_H_
 
-#define	ARM_KERNEL_BASE		0xc0000000
+#define	ARM_KERNEL_BASE		0xc0000000U
 
 /* Allow armv7 to have bigger DSIZ than generic arm, allow user to override */
 #ifndef	MAXDSIZ
