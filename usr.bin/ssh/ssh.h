eded993545470231cbe517df29e315a73a1abea8
djm <djm>
2015-07-03 03:49:45 +0000

    refuse to generate or accept RSA keys smaller than 1024 bits; feedback and ok dtucker@

--- usr.bin/ssh/ssh.h
+++ usr.bin/ssh/ssh.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssh.h,v 1.78 2006/08/03 03:34:42 deraadt Exp $ */
+/* $OpenBSD: ssh.h,v 1.79 2010/06/25 07:14:46 djm Exp $ */
 
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
@@ -91,7 +91,7 @@
 #define SSH_PRIVSEP_USER		"sshd"
 
 /* Minimum modulus size (n) for RSA keys. */
-#define SSH_RSA_MINIMUM_MODULUS_SIZE	768
+#define SSH_RSA_MINIMUM_MODULUS_SIZE	1024
 
 /* Listen backlog for sshd, ssh-agent and forwarding sockets */
 #define SSH_LISTEN_BACKLOG		128
