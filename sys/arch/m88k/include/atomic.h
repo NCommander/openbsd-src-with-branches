fad569c320e127ac0c03c07c51370fe27cc6e31c
miod <miod>
2015-07-03 12:47:30 +0000

    Fix cast in atomic_swap_ptr()

--- sys/arch/m88k/include/atomic.h
+++ sys/arch/m88k/include/atomic.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: atomic.h,v 1.11 2014/07/13 08:13:07 miod Exp $	*/
+/*	$OpenBSD: atomic.h,v 1.12 2014/07/18 10:40:14 dlg Exp $	*/
 
 /* Public Domain */
 
@@ -142,7 +142,7 @@ atomic_clear_int(volatile unsigned int *uip)
 #define	atomic_swap_ulong(p,o) \
 	((unsigned long)atomic_swap_uint((unsigned int *)p, (unsigned int)o)
 #define	atomic_swap_ptr(p,o) \
-	((void *)atomic_swap_uint((void *)p, (void *)o))
+	((void *)atomic_swap_uint((void *)p, (unsigned int)o))
 
 static inline void
 __sync_synchronize(void)
