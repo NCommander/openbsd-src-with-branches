d4910b9979254c6d0384d8b0702fd5bc482ae8de
doug <doug>
2015-07-23 02:57:51 +0000

    Replace TIOCSWINSZ with TIOCGWINSZ in tame(2).
    ok deraadt@

--- sys/kern/kern_tame.c
+++ sys/kern/kern_tame.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_tame.c,v 1.12 2015/07/21 16:17:17 guenther Exp $	*/
+/*	$OpenBSD: kern_tame.c,v 1.13 2015/07/22 05:32:44 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2015 Nicholas Marriott <nicm@openbsd.org>
@@ -724,7 +724,7 @@ tame_ioctl_check(struct proc *p, long com, void *v)
 		return (EPERM);
 
 	/* tty subsystem */
-	case TIOCSWINSZ:	/* various programs */
+	case TIOCGWINSZ:	/* various programs */
 	case TIOCSTI:		/* ksh? csh? */
 		if (fp->f_type == DTYPE_VNODE && (vp->v_flag & VISTTY))
 			return (0);
