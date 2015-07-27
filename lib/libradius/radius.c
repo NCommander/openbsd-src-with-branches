bcca6cbfd2d65cb1df1d3afb4bec3bad1df18a1f
yasuoka <yasuoka>
2015-07-27 03:01:22 +0000

    Add a description about radius_get_eap_msk().

--- lib/libradius/radius.c
+++ lib/libradius/radius.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: radius+.c,v 1.6 2012/05/08 13:15:12 yasuoka Exp $ */
+/*	$OpenBSD: radius.c,v 1.1 2015/07/20 23:52:29 yasuoka Exp $ */
 
 /*-
  * Copyright (c) 2009 Internet Initiative Japan Inc.
@@ -45,8 +45,7 @@
 static uint8_t radius_id_counter = 0;
 
 static int
-radius_check_packet_data(const RADIUS_PACKET_DATA * pdata,
-    size_t length)
+radius_check_packet_data(const RADIUS_PACKET_DATA * pdata, size_t length)
 {
 	const RADIUS_ATTRIBUTE	*attr;
 	const RADIUS_ATTRIBUTE	*end;
