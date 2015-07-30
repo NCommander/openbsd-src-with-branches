6cdae4699c3c4879d4fe2ab71c4920c0bade667d
deraadt <deraadt>
2015-07-30 19:23:02 +0000

    change default: PermitRootLogin without-password matching install script changes coming as well ok djm markus

--- usr.bin/ssh/servconf.c
+++ usr.bin/ssh/servconf.c
@@ -1,5 +1,5 @@
 
-/* $OpenBSD: servconf.c,v 1.276 2015/07/10 06:21:53 markus Exp $ */
+/* $OpenBSD: servconf.c,v 1.277 2015/07/30 00:01:34 djm Exp $ */
 /*
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
  *                    All rights reserved
@@ -79,7 +79,7 @@ initialize_server_options(ServerOptions *options)
 	options->server_key_bits = -1;
 	options->login_grace_time = -1;
 	options->key_regeneration_time = -1;
-	options->permit_root_login = PERMIT_NOT_SET;
+	options->permit_root_login = PERMIT_NO_PASSWD;
 	options->ignore_rhosts = -1;
 	options->ignore_user_known_hosts = -1;
 	options->print_motd = -1;
