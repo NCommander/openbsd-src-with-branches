a95f20bb25fca8e542c1a6d744305f27bdcee90a
uebayasi <uebayasi>
2015-08-13 10:13:55 +0000

    Close an fd.

--- regress/sys/kern/kqueue/kqueue-flock.c
+++ regress/sys/kern/kqueue/kqueue-flock.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kqueue-flock.c,v 1.1 2012/07/08 12:31:03 guenther Exp $	*/
+/*	$OpenBSD$	*/
 /*
  *	Written by Philip Guenther <guenther@openbsd.org> 2012 Public Domain
  */
@@ -44,6 +44,7 @@ check_lock(int fd, const char *msg)
 			printf("lock not found %s\n", msg);
 			_exit(1);
 		}
+		close(fd);
 		_exit(0);
 	}
 
