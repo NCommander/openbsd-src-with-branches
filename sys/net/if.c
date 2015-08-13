9f12e66dbf22ac90d961b4aa5b9373872e373f61
mpi <mpi>
2015-08-13 07:19:58 +0000

    If no handler consumed a mbuf, free it.  This also apply if an interface does not have any registered handler.
    Plug a mbuf leak found by sthen@ with gif(4) in a bridge.
    
    ok sthen@, claudio@

--- sys/net/if.c
+++ sys/net/if.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if.c,v 1.355 2015/07/21 04:21:50 jca Exp $	*/
+/*	$OpenBSD: if.c,v 1.356 2015/07/29 00:04:03 rzalamena Exp $	*/
 /*	$NetBSD: if.c,v 1.35 1996/05/07 05:26:04 thorpej Exp $	*/
 
 /*
@@ -538,6 +538,8 @@ if_input_process(void *xmq)
 			if ((*ifih->ifih_input)(ifp, m))
 				break;
 		}
+		if (ifih == NULL)
+			m_freem(m);
 	}
 	splx(s);
 	KERNEL_UNLOCK();
