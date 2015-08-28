a06eebc2014256c496822cc8f56b9840445fa794
jsing <jsing>
2015-08-28 16:38:19 +0000

    Bump LibreSSL version number to 2.2.3 in 5.8 release branch.
    Requested by bcook@ for libressl portable.

--- regress/gnu/egcs/gcc-bounds/getcwd-1.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-
-int main(int argc, char **argv) {
-	char buf[10];
-	getwd(buf);			/* not safe */
-	getcwd(buf, sizeof buf);	/* safe */
-	return 1;
-}
--- sys/arch/solbourne/include/ieeefp.h
+++
@@ -1,3 +0,0 @@
-/*	$OpenBSD: ieeefp.h,v 1.1 2005/04/19 21:30:17 miod Exp $ */
-
-#include <sparc/ieeefp.h>
