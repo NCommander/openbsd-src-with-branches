b54660243ac1310bd4c4fa85e3db56d1e9082d30
mpi <mpi>
2015-08-30 09:48:39 +0000

    Map the whole config1 space based on the size read from the device-tree.
    With this change, supplementary PCIe cards are now properly
    detected.
    
    This should prevents the kernel from faulting when reading
    unmapped PCI addresses as reported by Donovan Watteau on ppc@.
    
    ok kettenis@

--- sys/arch/macppc/pci/ht.c
+++ sys/arch/macppc/pci/ht.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ht.c,v 1.16 2013/08/07 07:29:19 mpi Exp $	*/
+/*	$OpenBSD: ht.c,v 1.17 2015/06/25 18:08:53 miod Exp $	*/
 
 /*
  * Copyright (c) 2005 Mark Kettenis
@@ -109,8 +109,8 @@ ht_attach(struct device *parent, struct device *self, void *aux)
 		return;
 	}
 
-	if (bus_space_map(sc->sc_memt, regs[1] + 0x01000000, 0x80000, 0,
-	    &sc->sc_config1_memh)) {
+	if (bus_space_map(sc->sc_memt, regs[1] + 0x01000000,
+	    regs[2] - 0x01000000, 0, &sc->sc_config1_memh)) {
 		printf(": can't map PCI config1 memory\n");
 		return;
 	}
