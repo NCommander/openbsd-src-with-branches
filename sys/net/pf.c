003e521033abf065e6d78a6ce7e5efc1c8e271ec
sashan <sashan>
2015-09-01 19:12:25 +0000

    - route-to, dup-to, reply-to should not override the block action
      Spotted by Dilli Paudel <dilli ! paudel at oracle ! com>
    
    ok jung@, ok mikeb@

--- sys/net/pf.c
+++ sys/net/pf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf.c,v 1.935 2015/07/21 02:32:04 sashan Exp $ */
+/*	$OpenBSD: pf.c,v 1.936 2015/08/19 21:22:41 sashan Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -6622,6 +6622,10 @@ done:
 		action = PF_PASS;
 		break;
 #endif /* INET6 */
+	case PF_DROP:
+		m_freem(*m0);
+		*m0 = NULL;
+		break;
 	default:
 		/* pf_route can free the mbuf causing *m0 to become NULL */
 		if (r->rt) {
