08b2849152671e21a0f42a3f5568d4cc57f9c3b9
yasuoka <yasuoka>
2015-09-02 08:24:29 +0000

    Add missing copyright.

--- sys/arch/amd64/stand/efiboot/efidev.c
+++ sys/arch/amd64/stand/efiboot/efidev.c
@@ -1,8 +1,9 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: efidev.c,v 1.1 2015/09/02 01:52:25 yasuoka Exp $	*/
 
 /*
  * Copyright (c) 1996 Michael Shalayeff
  * Copyright (c) 2003 Tobias Weingartner
+ * Copyright (c) 2015 YASUOKA Masahiko <yasuoka@yasuoka.net>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
