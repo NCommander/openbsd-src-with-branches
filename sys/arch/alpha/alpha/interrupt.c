e212f955b1808b8f5c0af2e0c8cccb56a347702f
deraadt <deraadt>
2015-09-02 14:07:43 +0000

    sizes for free()

--- sys/arch/alpha/alpha/interrupt.c
+++ sys/arch/alpha/alpha/interrupt.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: interrupt.c,v 1.34 2014/11/18 20:51:00 krw Exp $ */
+/* $OpenBSD: interrupt.c,v 1.35 2015/05/19 20:28:14 miod Exp $ */
 /* $NetBSD: interrupt.c,v 1.46 2000/06/03 20:47:36 thorpej Exp $ */
 
 /*-
@@ -574,7 +574,7 @@ softintr_disestablish(void *arg)
 	}
 	mtx_leave(&asi->softintr_mtx);
 
-	free(sih, M_DEVBUF, 0);
+	free(sih, M_DEVBUF, sizeof *sih);
 }
 
 /*
