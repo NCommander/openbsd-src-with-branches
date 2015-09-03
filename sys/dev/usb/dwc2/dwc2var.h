8e3e62c3930c2fa973086b040463c140b34ff9db
visa <visa>
2015-09-03 14:22:27 +0000

    Remove the abort_task field in struct dwc2_xfer. There is an identical field in struct usbd_xfer which is part of dwc2_xfer.
    ok mpi@

--- sys/dev/usb/dwc2/dwc2var.h
+++ sys/dev/usb/dwc2/dwc2var.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dwc2var.h,v 1.14 2015/02/12 11:38:42 uebayasi Exp $	*/
+/*	$OpenBSD: dwc2var.h,v 1.15 2015/09/03 12:55:54 visa Exp $	*/
 /*	$NetBSD: dwc2var.h,v 1.3 2013/10/22 12:57:40 skrll Exp $	*/
 
 /*-
@@ -43,7 +43,6 @@ struct dwc2_qtd;
 
 struct dwc2_xfer {
 	struct usbd_xfer xfer;			/* Needs to be first */
-	struct usb_task	abort_task;
 
 	struct dwc2_hcd_urb *urb;
 	int packet_count;
