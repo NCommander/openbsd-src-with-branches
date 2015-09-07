b46d85876383a78898d60a3e03c5233dac756eaf
gilles <gilles>
2015-09-07 15:36:53 +0000

    when bypassing the enqueuer, insert Message-Id header if none was found and the client has connected from a loopback interface.
    ok millert@ eric@

--- usr.sbin/smtpd/smtpd.h
+++ usr.sbin/smtpd/smtpd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: smtpd.h,v 1.473 2015/01/20 17:37:54 deraadt Exp $	*/
+/*	$OpenBSD: smtpd.h,v 1.474 2015/04/19 20:29:12 gilles Exp $	*/
 
 /*
  * Copyright (c) 2008 Gilles Chehade <gilles@poolp.org>
@@ -529,6 +529,8 @@ struct listener {
 	char			 hostname[HOST_NAME_MAX+1];
 	char			 hostnametable[PATH_MAX];
 	TAILQ_ENTRY(listener)	 entry;
+
+	int			 local;		/* there must be a better way */
 };
 
 struct smtpd {
