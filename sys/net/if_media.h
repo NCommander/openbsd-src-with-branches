223a3552036398a20ab690233fc30d085743e726
stsp <stsp>
2015-09-09 15:57:15 +0000

    Remove IFM_MAKEMODE. It is unused in base and ports and there doesn't seem to be a way of feeding it with a valid argument without peeking into the internals of ifmedia macros. ok mpi miod deraadt

--- sys/net/if_media.h
+++ sys/net/if_media.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_media.h,v 1.31 2010/02/09 13:18:04 claudio Exp $	*/
+/*	$OpenBSD: if_media.h,v 1.32 2014/12/05 15:50:04 mpi Exp $	*/
 /*	$NetBSD: if_media.h,v 1.22 2000/02/17 21:53:16 sommerfeld Exp $	*/
 
 /*-
@@ -331,8 +331,7 @@ u_int64_t	ifmedia_baudrate(int);
  */
 #define	IFM_MAKEWORD(type, subtype, options, instance)			\
 	((type) | (subtype) | (options) | ((instance) << IFM_ISHIFT))
-#define IFM_MAKEMODE(mode)						\
-	(((mode) << IFM_MSHIFT) & IFM_MMASK)
+
 /*
  * NetBSD extension not defined in the BSDI API.  This is used in various
  * places to get the canonical description for a given type/subtype.
