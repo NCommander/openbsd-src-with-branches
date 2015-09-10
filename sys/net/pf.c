25080125d77080c8ca9d689ef9cca3782f63aa05
mpi <mpi>
2015-09-10 08:28:31 +0000

    Kill two simple in6_ifstat_inc().

--- sys/net/pf.c
+++ sys/net/pf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf.c,v 1.937 2015/09/01 19:12:25 sashan Exp $ */
+/*	$OpenBSD: pf.c,v 1.938 2015/09/09 20:03:56 dlg Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -5722,7 +5722,6 @@ pf_route6(struct mbuf **m, struct pf_rule *r, int dir, struct ifnet *oifp,
 	} else if ((u_long)m0->m_pkthdr.len <= ifp->if_mtu) {
 		nd6_output(ifp, m0, dst, NULL);
 	} else {
-		in6_ifstat_inc(ifp, ifs6_in_toobig);
 		icmp6_error(m0, ICMP6_PACKET_TOO_BIG, 0, ifp->if_mtu);
 	}
 
--- sys/net/pf_norm.c
+++ sys/net/pf_norm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf_norm.c,v 1.180 2015/07/19 01:58:19 sashan Exp $ */
+/*	$OpenBSD: pf_norm.c,v 1.181 2015/08/19 21:22:41 sashan Exp $ */
 
 /*
  * Copyright 2001 Niels Provos <provos@citi.umich.edu>
@@ -753,7 +753,6 @@ pf_refragment6(struct mbuf **m0, struct m_tag *mtag, struct sockaddr_in6 *dst,
 			} else if ((u_long)m->m_pkthdr.len <= ifp->if_mtu) {
 				nd6_output(ifp, m, dst, NULL);
 			} else {
-				in6_ifstat_inc(ifp, ifs6_in_toobig);
 				icmp6_error(m, ICMP6_PACKET_TOO_BIG, 0,
 				    ifp->if_mtu);
 			}
