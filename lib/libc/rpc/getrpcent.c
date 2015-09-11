53c0ee98f45af9d5404f627f6687572971dba3a5
deraadt <deraadt>
2015-09-11 11:33:03 +0000

    rpcdata should not be exported; ok guenther

--- lib/libc/rpc/getrpcent.c
+++ lib/libc/rpc/getrpcent.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: getrpcent.c,v 1.18 2015/04/25 21:38:22 miod Exp $ */
+/*	$OpenBSD: getrpcent.c,v 1.19 2015/08/20 21:49:29 deraadt Exp $ */
 
 /*
  * Copyright (c) 2010, Oracle America, Inc.
@@ -41,7 +41,7 @@
 /*
  * Internet version.
  */
-struct rpcdata {
+static struct rpcdata {
 	FILE	*rpcf;
 	int	stayopen;
 #define	MAXALIASES	35
