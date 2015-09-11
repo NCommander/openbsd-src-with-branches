f1824af03a55b8df0464f5d9b0b99f762487999d
mpi <mpi>
2015-09-11 08:59:48 +0000

    FOREACH macro is not safe to use when removing elements on a list.
    Should fix a NULL dereference reported by guenther@.
    
    ok dlg@

--- sys/net/bpf.c
+++ sys/net/bpf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bpf.c,v 1.123 2015/09/01 04:50:27 dlg Exp $	*/
+/*	$OpenBSD: bpf.c,v 1.124 2015/09/09 11:55:37 dlg Exp $	*/
 /*	$NetBSD: bpf.c,v 1.33 1997/02/21 23:59:35 thorpej Exp $	*/
 
 /*
@@ -1498,7 +1498,7 @@ bpfdetach(struct ifnet *ifp)
 				if (cdevsw[maj].d_open == bpfopen)
 					break;
 
-			SRPL_FOREACH_LOCKED(bd, &bp->bif_dlist, bd_next) {
+			while ((bd = SRPL_FIRST_LOCKED(&bp->bif_dlist))) {
 				struct bpf_d *d;
 
 				/*
