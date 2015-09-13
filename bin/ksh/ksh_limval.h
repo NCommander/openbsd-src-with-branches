b7fb8838326c3680dcfc2ced2f094d4a11bbd150
tedu <tedu>
2015-09-13 19:43:42 +0000

    zap unused FP support. from Michael McConville. ok nicm

--- bin/ksh/ksh_limval.h
+++ bin/ksh/ksh_limval.h
@@ -1,12 +1,8 @@
-/*	$OpenBSD: ksh_limval.h,v 1.1.1.1 1996/08/14 06:19:11 downsj Exp $	*/
+/*	$OpenBSD: ksh_limval.h,v 1.2 2004/12/18 20:55:52 millert Exp $	*/
 
 /* Wrapper around the values.h/limits.h includes/ifdefs */
 
 /* limits.h is included in sh.h */
-
-#ifndef DMAXEXP
-# define DMAXEXP	128	/* should be big enough */
-#endif
 
 #ifndef BITS
 # define BITS(t)	(CHAR_BIT * sizeof(t))
