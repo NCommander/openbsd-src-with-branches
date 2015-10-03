586abbbae54a2274d476f48fc84fe8a83fdf0adc
deraadt <deraadt>
2015-10-03 02:35:56 +0000

    patch appears to work fully with tame "stdio rpath wpath cpath tmppath fattr". in case of exploitation, no more network access, fork, execve, etc. I wonder if we could use whitepath lists here - if it is reasonable to limit operation in directories known early on?

--- usr.bin/patch/patch.c
+++ usr.bin/patch/patch.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: patch.c,v 1.53 2014/12/08 21:59:36 deraadt Exp $	*/
+/*	$OpenBSD: patch.c,v 1.54 2014/12/13 10:31:07 tobias Exp $	*/
 
 /*
  * patch - a program to apply diffs to original files
@@ -146,6 +146,9 @@ main(int argc, char *argv[])
 	LINENUM	where = 0, newwhere, fuzz, mymaxfuzz;
 	const	char *tmpdir;
 	char	*v;
+
+	if (tame("stdio rpath wpath cpath tmppath fattr", NULL) == -1)
+		perror("tame");
 
 	setvbuf(stdout, NULL, _IOLBF, 0);
 	setvbuf(stderr, NULL, _IOLBF, 0);
