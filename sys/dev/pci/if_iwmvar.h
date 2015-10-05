2f063b492703ba5ccb03d7263e174e5b50d3c176
stsp <stsp>
2015-10-05 13:05:08 +0000

    Simplify iwm(4) newstate task by only queuing one state transition at a time. The newstate task now always transitions to the most recently requested state, rather than hopping along with every request.
    This allows us get rid of the silly newstate generation counter, and
    we can now task_del() a pending transition when the interface goes down.
    While several issues with this driver remain, I believe this change
    does not introduce new problems.
    
    Tested by myself, jasper@, and zhuk@

--- sys/dev/pci/if_iwmvar.h
+++ sys/dev/pci/if_iwmvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_iwmvar.h,v 1.8 2015/06/15 07:50:44 stsp Exp $	*/
+/*	$OpenBSD: if_iwmvar.h,v 1.9 2015/06/15 08:06:12 stsp Exp $	*/
 
 /*
  * Copyright (c) 2014 genua mbh <info@genua.de>
@@ -364,6 +364,9 @@ struct iwm_softc {
 	struct timeout sc_led_blink_to;
 
 	struct task		init_task;
+	struct task		newstate_task;
+	enum ieee80211_state	ns_nstate;
+	int			ns_arg;
 
 	bus_space_tag_t sc_st;
 	bus_space_handle_t sc_sh;
