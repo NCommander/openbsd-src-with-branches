099bef55364ed8bfd1e4cd495181db5fc5d9eaf8
gilles <gilles>
2015-10-06 06:04:46 +0000

    fix values passed to umask(), they should be octal. the permissions are even more restrictive than they should.
    misc bug reported by qualys

--- usr.sbin/smtpd/makemap.c
+++ usr.sbin/smtpd/makemap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: makemap.c,v 1.51 2015/01/16 06:40:20 deraadt Exp $	*/
+/*	$OpenBSD: makemap.c,v 1.52 2015/01/20 17:37:54 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2008 Gilles Chehade <gilles@poolp.org>
@@ -185,7 +185,7 @@ main(int argc, char *argv[])
 
 	if (! bsnprintf(dbname, sizeof(dbname), "%s.XXXXXXXXXXX", oflag))
 		errx(1, "path too long");
-	omode = umask(7077);
+	omode = umask(07077);
 	if (mkstemp(dbname) == -1)
 		err(1, "mkstemp");
 	umask(omode);
--- usr.sbin/smtpd/smtpd.c
+++ usr.sbin/smtpd/smtpd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: smtpd.c,v 1.239 2015/06/03 02:24:36 millert Exp $	*/
+/*	$OpenBSD: smtpd.c,v 1.240 2015/10/02 00:37:53 gilles Exp $	*/
 
 /*
  * Copyright (c) 2008 Gilles Chehade <gilles@poolp.org>
@@ -942,7 +942,7 @@ forkmda(struct mproc *p, uint64_t id, struct deliver *deliver)
 
 	/* prepare file which captures stdout and stderr */
 	(void)strlcpy(sfn, "/tmp/smtpd.out.XXXXXXXXXXX", sizeof(sfn));
-	omode = umask(7077);
+	omode = umask(07077);
 	allout = mkstemp(sfn);
 	umask(omode);
 	if (allout < 0) {
