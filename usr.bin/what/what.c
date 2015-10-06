adb0ef2585020b9271da4f225151b52494537202
deraadt <deraadt>
2015-10-06 13:47:08 +0000

    simple tame "stdio rpath"

--- usr.bin/what/what.c
+++ usr.bin/what/what.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: what.c,v 1.12 2009/10/27 23:59:50 deraadt Exp $	*/
+/*	$OpenBSD: what.c,v 1.13 2015/01/22 19:10:17 krw Exp $	*/
 /*	$NetBSD: what.c,v 1.4 1994/12/20 16:01:03 jtc Exp $	*/
 
 /*
@@ -57,6 +57,9 @@ main(int argc, char *argv[])
 	struct utsname utsn;
 	char match[256];
 	int c;
+
+	if (tame("stdio rpath", NULL) == -1)
+		err(1, "tame");
 
 	matches = sflag = 0;
 	while ((c = getopt(argc, argv, "s")) != -1) {
