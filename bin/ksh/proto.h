e073b638ea7808a56973e15cf864b93ab5b748f8
nicm <nicm>
2015-10-06 21:19:06 +0000

    Move tree.c protos into tree.h, from Michael McConville, ok millert

--- bin/ksh/proto.h
+++ bin/ksh/proto.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: proto.h,v 1.35 2013/09/04 15:49:19 millert Exp $	*/
+/*	$OpenBSD: proto.h,v 1.36 2015/09/14 16:08:50 nicm Exp $	*/
 
 /*
  * prototypes for PD-KSH
@@ -232,14 +232,6 @@ int	block_pipe(void);
 void	restore_pipe(int);
 int	setsig(Trap *, sig_t, int);
 void	setexecsig(Trap *, int);
-/* tree.c */
-void	fptreef(struct shf *, int, const char *, ...);
-char *	snptreef(char *, int, const char *, ...);
-struct op *	tcopy(struct op *, Area *);
-char *	wdcopy(const char *, Area *);
-char *	wdscan(const char *, int);
-char *	wdstrip(const char *);
-void	tfree(struct op *, Area *);
 /* var.c */
 void	newblock(void);
 void	popblock(void);
--- bin/ksh/tree.h
+++ bin/ksh/tree.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tree.h,v 1.9 2004/12/18 20:55:52 millert Exp $	*/
+/*	$OpenBSD: tree.h,v 1.10 2005/03/28 21:28:22 deraadt Exp $	*/
 
 /*
  * command trees for compile/execute
@@ -139,3 +139,11 @@ struct ioword {
 #define DB_AND	3		/* && -> -a conversion */
 #define DB_BE	4		/* an inserted -BE */
 #define DB_PAT	5		/* a pattern argument */
+
+void	fptreef(struct shf *, int, const char *, ...);
+char *	snptreef(char *, int, const char *, ...);
+struct op *	tcopy(struct op *, Area *);
+char *	wdcopy(const char *, Area *);
+char *	wdscan(const char *, int);
+char *	wdstrip(const char *);
+void	tfree(struct op *, Area *);
