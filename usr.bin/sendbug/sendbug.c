bc41fbd979392d554dcb899d8c3e1b38b03203e6
deraadt <deraadt>
2015-10-10 19:10:20 +0000

    pledge "stdio getpw rpath wpath cpath tmppath proc exec". doug pointed out the need for getpw.  Not sure if I see ways to improve this program. ok doug

--- usr.bin/sendbug/sendbug.c
+++ usr.bin/sendbug/sendbug.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sendbug.c,v 1.68 2014/03/18 22:36:37 miod Exp $	*/
+/*	$OpenBSD: sendbug.c,v 1.69 2015/01/16 06:40:11 deraadt Exp $	*/
 
 /*
  * Written by Ray Lai <ray@cyth.net>.
@@ -85,6 +85,9 @@ main(int argc, char *argv[])
 	char *pr_form;
 	time_t mtime;
 	FILE *fp;
+
+	if (pledge("stdio getpw rpath wpath cpath tmppath proc exec", NULL) == -1)
+		err(1, "pledge");
 
 	while ((ch = getopt(argc, argv, "DEPV")) != -1)
 		switch (ch) {
