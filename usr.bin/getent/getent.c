b3a45d55758ff61586a1ca594938186d4ebb1f73
doug <doug>
2015-10-10 21:50:05 +0000

    Fix YP user and group support in getent(1).
    These should have been "stdio getpw" before, but they worked for non-YP
    environments.  With YP, it won't work without "getpw".
    
    Reported by semarie@ and confirmed as a problem by miod@.
    
    ok deraadt@

--- usr.bin/getent/getent.c
+++ usr.bin/getent/getent.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: getent.c,v 1.10 2015/10/10 05:26:57 doug Exp $	*/
+/*	$OpenBSD: getent.c,v 1.11 2015/10/10 14:33:02 deraadt Exp $	*/
 /*	$NetBSD: getent.c,v 1.7 2005/08/24 14:31:02 ginsbach Exp $	*/
 
 /*-
@@ -79,9 +79,9 @@ static struct getentdb {
 	const char	*pledge;
 } databases[] = {
 	{	"ethers",	ethers,		"stdio rpath"	},
-	{	"group",	group,		"stdio rpath"	},
+	{	"group",	group,		"stdio getpw"	},
 	{	"hosts",	hosts,		"stdio dns"	},
-	{	"passwd",	passwd,		"stdio rpath"	},
+	{	"passwd",	passwd,		"stdio getpw"	},
 	{	"protocols",	protocols,	"stdio rpath"	},
 	{	"rpc",		rpc,		"stdio rpath"	},
 	{	"services",	services,	"stdio rpath"	},
