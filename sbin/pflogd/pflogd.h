85c041dbc12fafcd657a8ab83445c060ac0bf359
deraadt <deraadt>
2015-10-10 22:36:06 +0000

    pflogd contained the same "privsep error" as tcpdump -- assuming that it can ioctl()'s against a bpf device node.  Privsep that operation via a message to the parent process.  Unfortunately "rpath wpath cpath" is still needed due to SIGHUP handling, but I have asked canacar the expert to look into this.

--- sbin/pflogd/pflogd.h
+++ sbin/pflogd/pflogd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pflogd.h,v 1.3 2006/01/15 16:38:04 canacar Exp $ */
+/*	$OpenBSD: pflogd.h,v 1.4 2010/09/21 05:56:58 henning Exp $ */
 
 /*
  * Copyright (c) 2003 Can Erkin Acar
@@ -38,6 +38,7 @@ int	priv_init(void);
 int	priv_set_snaplen(int snaplen);
 int	priv_open_log(void);
 int	priv_move_log(void);
+int	priv_pcap_stats(struct pcap_stat *);
 pcap_t *pcap_open_live_fd(int fd, int snaplen, char *ebuf);
 
 void set_pcap_filter(void);
