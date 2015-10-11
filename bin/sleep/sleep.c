4bb15ca655dee7409810f51f01153dc4a66b9b3c
guenther <guenther>
2015-10-11 20:17:49 +0000

    Don't return errno from main()
    ok beck@ doug@ deraadt@ tedu@

--- bin/sleep/sleep.c
+++ bin/sleep/sleep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sleep.c,v 1.22 2015/10/03 03:10:38 deraadt Exp $	*/
+/*	$OpenBSD: sleep.c,v 1.23 2015/10/09 01:37:06 deraadt Exp $	*/
 /*	$NetBSD: sleep.c,v 1.8 1995/03/21 09:11:11 cgd Exp $	*/
 
 /*
@@ -107,7 +107,7 @@ main(int argc, char *argv[])
 
 	if ((secs > 0) || (nsecs > 0))
 		if (nanosleep(&rqtp, NULL))
-			return (errno);
+			err(1, NULL);
 	return (0);
 }
 
