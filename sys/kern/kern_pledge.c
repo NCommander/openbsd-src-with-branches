1bf3c020345490c7477a60e1ffe5bc8f0a154acd
deraadt <deraadt>
2015-10-11 15:25:39 +0000

    In pledge "tty", allow TIOCSWINSZ.  stty(1) is the obvious silly use. The more important use will be tmux(1) and other active window size controlling programs. There seems little risk in exposing this small tty setting alongside the tcsetattr() family. ok millert

--- sys/kern/kern_pledge.c
+++ sys/kern/kern_pledge.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_pledge.c,v 1.10 2015/10/10 19:15:53 deraadt Exp $	*/
+/*	$OpenBSD: kern_pledge.c,v 1.11 2015/10/11 01:56:56 guenther Exp $	*/
 
 /*
  * Copyright (c) 2015 Nicholas Marriott <nicm@openbsd.org>
@@ -1031,6 +1031,7 @@ pledge_ioctl_check(struct proc *p, long com, void *v)
 			return (ENOTTY);
 		case TIOCGPGRP:
 		case TIOCGWINSZ:	/* various programs */
+		case TIOCSWINSZ:
 			if (fp->f_type == DTYPE_VNODE && (vp->v_flag & VISTTY))
 				return (0);
 			break;
