05bb355afb134a42c46a1d032ead6010530b00f4
deraadt <deraadt>
2015-10-12 05:05:24 +0000

    preservation modes can adjust setugid bits, so no pledge is possible. Otherwise, lay the groundwork for whether a gzip program may be run or not. After such a gzip program is started, pledge the program will not exec again.  Took a few iterations to get this going... it is looking good. with guenther.

--- bin/pax/ar_io.c
+++ bin/pax/ar_io.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ar_io.c,v 1.49 2015/03/09 04:25:28 guenther Exp $	*/
+/*	$OpenBSD: ar_io.c,v 1.50 2015/03/22 03:15:00 guenther Exp $	*/
 /*	$NetBSD: ar_io.c,v 1.5 1996/03/26 23:54:13 mrg Exp $	*/
 
 /*-
@@ -1260,6 +1260,10 @@ ar_start_gzip(int fd, const char *path, int wr)
 			dup2(fds[0], fd);
 		close(fds[0]);
 		close(fds[1]);
+
+		if (pledge("stdio rpath wpath cpath fattr getpw ioctl proc",
+		    NULL) == -1)
+			err(1, "pledge");
 	} else {
 		if (wr) {
 			dup2(fds[0], STDIN_FILENO);
