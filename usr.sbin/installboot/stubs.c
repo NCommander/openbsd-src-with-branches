4abadebf64e6097e0de5567cf66c66e4d368e4a4
deraadt <deraadt>
2015-10-14 17:54:46 +0000

    worse, need even more includes...

--- usr.sbin/installboot/stubs.c
+++ usr.sbin/installboot/stubs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: stubs.c,v 1.1 2014/01/18 01:44:57 jsing Exp $	*/
+/*	$OpenBSD: stubs.c,v 1.2 2014/01/18 01:49:30 jsing Exp $	*/
 
 /*
  * Copyright (c) 2014 Joel Sing <jsing@openbsd.org>
@@ -16,6 +16,7 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include <stdlib.h>
 #include <err.h>
 
 #include "installboot.h"
