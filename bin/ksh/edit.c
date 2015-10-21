bdb7544bd1ea37007d9b5fd9a5d2dd4a3121b6c8
mmcc <mmcc>
2015-10-21 14:31:28 +0000

    Assign pointer to NULL rather than 0.
    ok nicm@

--- bin/ksh/edit.c
+++ bin/ksh/edit.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: edit.c,v 1.47 2015/10/19 02:15:45 mmcc Exp $	*/
+/*	$OpenBSD: edit.c,v 1.48 2015/10/19 14:42:16 mmcc Exp $	*/
 
 /*
  * Command line editing - common code
@@ -470,7 +470,7 @@ x_command_glob(int flags, const char *str, int slen, char ***wordsp)
 	if (flags & XCF_FULLPATH) {
 		/* Sort by basename, then path order */
 		struct path_order_info *info;
-		struct path_order_info *last_info = 0;
+		struct path_order_info *last_info = NULL;
 		char **words = (char **) XPptrv(w);
 		int path_order = 0;
 		int i;
