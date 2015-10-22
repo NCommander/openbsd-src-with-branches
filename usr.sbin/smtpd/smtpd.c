1bdaf3ab70ef085151202755bd5c0b510c54b7c9
gilles <gilles>
2015-10-22 08:46:31 +0000

    delivery to maildir needs pledge fattr
    from Gregor Best <gbe@unobtanium.de>

--- usr.sbin/smtpd/smtpd.c
+++ usr.sbin/smtpd/smtpd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: smtpd.c,v 1.249 2015/10/17 04:36:10 deraadt Exp $	*/
+/*	$OpenBSD: smtpd.c,v 1.250 2015/10/17 16:03:20 sunil Exp $	*/
 
 /*
  * Copyright (c) 2008 Gilles Chehade <gilles@poolp.org>
@@ -690,7 +690,7 @@ main(int argc, char *argv[])
 
 	purge_task();
 
-	if (pledge("stdio rpath wpath cpath flock tmppath "
+	if (pledge("stdio rpath wpath cpath fattr flock tmppath "
 	    "getpw sendfd proc exec id inet unix", NULL) == -1)
 		err(1, "pledge");
 	
