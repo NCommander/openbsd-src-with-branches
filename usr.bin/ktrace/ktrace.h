756e4c3040a5d3a07c6c230a99f3e8372a473e00
deraadt <deraadt>
2015-10-25 20:39:54 +0000

    Fold "malloc" into "stdio" and -- recognizing that no program so far has used less than "stdio" -- include all the "self" operations.  Instead of different defines, use regular PLEDGE_* in the "p_pledgenote" variable (which indicates the operation subtype a system call is performing).  Many checks before easier to understand.  p_pledgenote can often be passed directly to ktrace, so that kdump says:  15565 test     CALL  pledge(0xa9a3f804c51,0)  15565 test     STRU  pledge request="stdio"  15565 test     RET   pledge 0  15565 test     CALL  open(0xa9a3f804c57,0x2<O_RDWR>)  15565 test     NAMI  "/tmp/testfile"  15565 test     PLDG  open, "wpath", errno 1 Operation not permitted with help from semarie, ok guenther

--- usr.bin/ktrace/ktrace.h
+++ usr.bin/ktrace/ktrace.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ktrace.h,v 1.7 2015/09/07 15:38:45 guenther Exp $ */
+/*	$OpenBSD: ktrace.h,v 1.8 2015/10/02 05:07:41 guenther Exp $ */
 /*-
  * Copyright (c) 1988, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -32,7 +32,7 @@
 
 #define DEF_POINTS (KTRFAC_SYSCALL | KTRFAC_SYSRET | KTRFAC_NAMEI | \
 		  KTRFAC_GENIO | KTRFAC_PSIG | KTRFAC_EMUL | KTRFAC_STRUCT | \
-		  KTRFAC_USER | KTRFAC_EXECARGS)
+		  KTRFAC_USER | KTRFAC_EXECARGS | KTRFAC_PLEDGE)
 
 /* any KTRFAC_* not included in DEF_POINTS should be added here */
 #define ALL_POINTS (DEF_POINTS | KTRFAC_EXECENV)
