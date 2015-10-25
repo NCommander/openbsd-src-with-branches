770b719510f142e937728ad6f1f512aefb8a7905
stsp <stsp>
2015-10-25 21:21:15 +0000

    Fix installboot(8) on i386/amd64 when softraid is on top of GPT. ok krw deraadt

--- usr.sbin/installboot/i386_softraid.c
+++ usr.sbin/installboot/i386_softraid.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: i386_softraid.c,v 1.3 2015/01/16 00:05:12 deraadt Exp $	*/
+/*	$OpenBSD: i386_softraid.c,v 1.4 2015/10/03 16:56:52 krw Exp $	*/
 /*
  * Copyright (c) 2012 Joel Sing <jsing@openbsd.org>
  *
@@ -86,6 +86,12 @@ sr_install_bootblk(int devfd, int vol, int disk)
 	/* Warn on unknown disklabel types. */
 	if (dl.d_type == 0)
 		warnx("disklabel type unknown");
+
+	part = findgptefisys(diskfd, &dl);
+	if (part != -1) {
+		write_efisystem(&dl, (char)part);
+		return;
+	}
 
 	/* Determine poffset and set symbol value. */
 	pp = &dl.d_partitions[part - 'a'];
