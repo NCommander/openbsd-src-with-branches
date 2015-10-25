06ce6bb194b2acec06155b4626e77ee0ee4ff24a
jca <jca>
2015-10-25 21:23:42 +0000

    Stop writing down a pidfile.
    pid files aren't useful on OpenBSD, the rc.d(8) framework doesn't use
    them.  In rtadvd, not writing a pid file means that we can set tighter
    pledge(2) settings.  Also the pid file handling is pretty basic and the
    atexit handler can't remove the pid file since rtadvd did a chroot - beurk.
    
    ok florian@, "perfect" deraadt@ (!)

--- usr.sbin/rtadvd/rtadvd.c
+++ usr.sbin/rtadvd/rtadvd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rtadvd.c,v 1.54 2015/08/18 20:25:28 miod Exp $	*/
+/*	$OpenBSD: rtadvd.c,v 1.55 2015/08/20 22:39:29 deraadt Exp $	*/
 /*	$KAME: rtadvd.c,v 1.66 2002/05/29 14:18:36 itojun Exp $	*/
 
 /*
@@ -55,7 +55,6 @@
 #include <err.h>
 #include <errno.h>
 #include <string.h>
-#include <util.h>
 #include <pwd.h>
 
 #include "rtadvd.h"
@@ -223,10 +222,6 @@ main(argc, argv)
 		daemon(1, 0);
 
 	sock_open();
-
-	/* record the current PID */
-	if (pidfile(NULL) < 0)
-		log_warnx("failed to open the pid log file, run anyway.");
 
 	if (sflag == 0) {
 		rtsock_open();
