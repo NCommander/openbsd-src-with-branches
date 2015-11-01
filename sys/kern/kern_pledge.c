d088d520d4f3af7a1e0d2748a3cd306ce05ee2f0
deraadt <deraadt>
2015-11-01 15:43:50 +0000

    Do not need to check the pledge control bits for system calls that are PLEDGE_ALWAYS. ok semarie

--- sys/kern/kern_pledge.c
+++ sys/kern/kern_pledge.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_pledge.c,v 1.94 2015/10/31 21:53:53 deraadt Exp $	*/
+/*	$OpenBSD: kern_pledge.c,v 1.95 2015/11/01 13:01:58 semarie Exp $	*/
 
 /*
  * Copyright (c) 2015 Nicholas Marriott <nicm@openbsd.org>
@@ -533,7 +533,7 @@ pledge_check(struct proc *p, int code, int *tval)
 	if (code < 0 || code > SYS_MAXSYSCALL - 1)
 		return (EINVAL);
 
-	if ((p->p_p->ps_pledge == 0) && (pledge_syscalls[code] == PLEDGE_ALWAYS))
+	if (pledge_syscalls[code] == PLEDGE_ALWAYS)
 		return (0);
 
 	if (p->p_p->ps_pledge & pledge_syscalls[code])
