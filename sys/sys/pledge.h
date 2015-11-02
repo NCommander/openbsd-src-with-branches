7ffc77374a776d1f9bdb570166156d6981ba368c
semarie <semarie>
2015-11-02 16:31:55 +0000

    move the pledgenote annotation from `struct proc' to `struct nameidata'
    pledgenote is used for annotate the policy for a namei context. So make it
    tracking the nameidata.
    
    It is expected for the caller to explicitly define the policy. It is a kernel
    bug to not do so.
    
    ok deraadt@

--- sys/sys/pledge.h
+++ sys/sys/pledge.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pledge.h,v 1.15 2015/11/01 13:01:58 semarie Exp $	*/
+/*	$OpenBSD: pledge.h,v 1.16 2015/11/01 19:03:33 semarie Exp $	*/
 
 /*
  * Copyright (c) 2015 Nicholas Marriott <nicm@openbsd.org>
@@ -92,9 +92,10 @@ static struct {
 
 #ifdef _KERNEL
 
+struct nameidata;
 int	pledge_syscall(struct proc *, int, int *);
 int	pledge_fail(struct proc *, int, int);
-int	pledge_namei(struct proc *, char *);
+int	pledge_namei(struct proc *, struct nameidata *, char *);
 void	pledge_aftersyscall(struct proc *, int, int);
 
 struct mbuf;
--- sys/sys/proc.h
+++ sys/sys/proc.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: proc.h,v 1.209 2015/10/20 06:40:01 semarie Exp $	*/
+/*	$OpenBSD: proc.h,v 1.210 2015/10/25 20:39:54 deraadt Exp $	*/
 /*	$NetBSD: proc.h,v 1.44 1996/04/22 01:23:21 christos Exp $	*/
 
 /*-
@@ -327,8 +327,7 @@ struct proc {
 	char	p_comm[MAXCOMLEN+1];
 
 	int	p_pledge_syscall;	/* Cache of current syscall */
-	int	p_pledgenote;	/* Observance during syscall */
-	int	p_pledgeafter;
+	int	p_pledgeafter;	/* Observance during syscall */
 
 #ifndef	__HAVE_MD_TCB
 	void	*p_tcb;		/* user-space thread-control-block address */
