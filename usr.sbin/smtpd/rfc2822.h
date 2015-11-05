9713c79174a86552e3702bfe6d0d977b07525bc9
gilles <gilles>
2015-11-05 08:55:09 +0000

    when a message consists solely of headers and does not end them with an empty line, the message parser gets confused, and forgets to flush last header to message file.
    detect if we're still in headers when hitting EOM, and flush if that is
    the case.
    
    reported by Philipp Takacs <philipp@bureaucracy.de>
    
    ok millert@, jung@, sunil@, eric@

--- usr.sbin/smtpd/rfc2822.h
+++ usr.sbin/smtpd/rfc2822.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rfc2822.h,v 1.2 2015/08/15 16:49:23 gilles Exp $	*/
+/*	$OpenBSD: rfc2822.h,v 1.3 2015/09/07 15:36:53 gilles Exp $	*/
 
 /*
  * Copyright (c) 2014 Gilles Chehade <gilles@poolp.org>
@@ -68,6 +68,7 @@ struct rfc2822_parser {
 
 void	rfc2822_parser_init(struct rfc2822_parser *);
 int	rfc2822_parser_feed(struct rfc2822_parser *, const char *);
+void	rfc2822_parser_flush(struct rfc2822_parser *);
 void	rfc2822_parser_reset(struct rfc2822_parser *);
 void	rfc2822_parser_release(struct rfc2822_parser *);
 int	rfc2822_header_callback(struct rfc2822_parser *, const char *,
