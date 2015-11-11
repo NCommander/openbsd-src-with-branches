aeaae07029c92ac69f855de57bdf841eee5fadff
deraadt <deraadt>
2015-11-11 15:10:20 +0000

    pledge "stdio rpath wpath cpath fattr proc exec tty" seems to work. there are a few system, popen, fork... ok florian

--- usr.bin/mg/main.c
+++ usr.bin/mg/main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: main.c,v 1.76 2015/10/29 19:46:47 lum Exp $	*/
+/*	$OpenBSD: main.c,v 1.77 2015/11/08 19:31:57 jasper Exp $	*/
 
 /* This file is in the public domain. */
 
@@ -55,6 +55,9 @@ main(int argc, char **argv)
 	int	 	 o, i, nfiles;
 	int	  	 nobackups = 0;
 	struct buffer	*bp = NULL;
+
+	if (pledge("stdio rpath wpath cpath fattr proc exec tty", NULL) == -1)
+		err(1, "pledge");
 
 	while ((o = getopt(argc, argv, "nf:")) != -1)
 		switch (o) {
