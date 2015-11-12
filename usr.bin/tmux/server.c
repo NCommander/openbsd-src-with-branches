079d62683bd17ee4e8250dba15dd3131cc610c5b
nicm <nicm>
2015-11-12 12:19:57 +0000

    Tidy utf8.c a little: build table on first use, and make utf8_width take a u_int rather than splitting and then combining again in utf8_split.

--- usr.bin/tmux/server.c
+++ usr.bin/tmux/server.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: server.c,v 1.145 2015/10/27 15:58:42 nicm Exp $ */
+/* $OpenBSD: server.c,v 1.146 2015/10/31 13:12:03 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -186,7 +186,6 @@ server_start(struct event_base *base, int lockfd, char *lockfile)
 	TAILQ_INIT(&session_groups);
 	mode_key_init_trees();
 	key_bindings_init();
-	utf8_build();
 
 	start_time = time(NULL);
 
