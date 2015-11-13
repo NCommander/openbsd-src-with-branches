0872f55fa898fb7444e7d7bb5119d2976f8dedbb
deraadt <deraadt>
2015-11-13 01:23:59 +0000

    _exit() in the child; as a result, must use syslog() directly.

--- libexec/fingerd/fingerd.c
+++ libexec/fingerd/fingerd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fingerd.c,v 1.36 2012/12/04 02:24:47 deraadt Exp $	*/
+/*	$OpenBSD: fingerd.c,v 1.37 2015/01/16 06:39:50 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1983, 1993
@@ -196,7 +196,8 @@ main(int argc, char *argv[])
 			(void) close(p[1]);
 		}
 		execv(prog, comp);
-		logerr("execv: %s: %s", prog, strerror(errno));
+		syslog(LOG_ERR, "execv: %s: %s", prog, strerror(errno));
+		_exit(1);
 	case -1:
 		logerr("fork: %s", strerror(errno));
 	}
