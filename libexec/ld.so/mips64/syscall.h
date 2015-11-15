672abe422fd9e2ba405ebc14ac7379f133de2c16
deraadt <deraadt>
2015-11-15 03:41:24 +0000

    ldd(1) sets environment variable LD_TRACE_LOADED_OBJECTS to tell ld.so that it should show information about the program it loads, rather than run it.  In that specific case, ld.so can pledge to "stdio rpath" to ensure that code path in ld.so has no bugs. Yes, a pledge in ld.so.... who'd have thought! ok guenther

--- libexec/ld.so/mips64/syscall.h
+++ libexec/ld.so/mips64/syscall.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: syscall.h,v 1.18 2014/09/01 05:09:53 doug Exp $ */
+/*	$OpenBSD: syscall.h,v 1.19 2015/09/19 20:56:47 guenther Exp $ */
 
 /*
  * Copyright (c) 1998-2002 Opsycon AB, Sweden.
@@ -51,6 +51,7 @@ int	_dl_munmap(const void *, size_t);
 int	_dl_open(const char *, int);
 ssize_t	_dl_read(int, const char *, size_t);
 ssize_t	_dl_readlink(const char *, char *, size_t);
+int	_dl_pledge(const char *, const char **);
 long	_dl__syscall(quad_t, ...);
 int	_dl_sysctl(const int *, u_int, void *, size_t *, void *, size_t);
 int	_dl_utrace(const char *, const void *, size_t);
