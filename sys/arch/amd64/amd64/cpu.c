f72856694b890a048db0b6a17607184f5c1f9a90
mpi <mpi>
2015-11-16 10:08:41 +0000

    Some minor tweaks:
    - Add $OpenBSD$ tag,
    - constify "struct cfattach",
    - Use <uvm/uvm_extern.h> rather than <uvm/uvm.h>, it's enough.
    - Keep the "struct vm" private.  This allows us to not pull <uvm/uvm_extern.h>
      in <macine/vmmvar.h>
    - Prefer DPRINTF() for debug macro as dprintf(3) is a standard function name.
    - Add vmm_debug and fix VMM_DEBUG build
    - Remove unneeded <sys/rwlock.h> from <machine/vmmvar.h>
    - Kill whitespaces
    
    ok mlarkin@

--- sys/arch/amd64/amd64/cpu.c
+++ sys/arch/amd64/amd64/cpu.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cpu.c,v 1.88 2015/07/18 19:21:02 sf Exp $	*/
+/*	$OpenBSD: cpu.c,v 1.89 2015/11/13 07:52:20 mlarkin Exp $	*/
 /* $NetBSD: cpu.c,v 1.1 2003/04/26 18:39:26 fvdl Exp $ */
 
 /*-
@@ -542,7 +542,7 @@ cpu_init_vmm(struct cpu_info *ci)
 		    M_DEVBUF, M_WAITOK | M_ZERO);
 		if (!pmap_extract(pmap_kernel(), (vaddr_t)ci->ci_vmxon_region,
 		    &ci->ci_vmxon_region_pa))
-			panic("Can't locate VMXON region in phys mem\n");	
+			panic("Can't locate VMXON region in phys mem\n");
 	}
 }
 #endif /* VMM */
