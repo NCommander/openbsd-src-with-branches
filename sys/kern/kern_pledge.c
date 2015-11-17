1303b66f058ce9b9384fb7972ba53c83f5be360f
sthen <sthen>
2015-11-17 15:03:53 +0000

    backout removal of SYS_break from stdio, suggested by deraadt@
    ports using base gcc with PCH include: boost, keepassx, wxWidgets, jdk

--- sys/kern/kern_pledge.c
+++ sys/kern/kern_pledge.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_pledge.c,v 1.112 2015/11/16 21:20:20 deraadt Exp $	*/
+/*	$OpenBSD: kern_pledge.c,v 1.113 2015/11/17 04:09:35 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2015 Nicholas Marriott <nicm@openbsd.org>
@@ -114,6 +114,7 @@ const u_int pledge_syscalls[SYS_MAXSYSCALL] = {
 	[SYS_mprotect] = PLEDGE_STDIO,
 	[SYS_mquery] = PLEDGE_STDIO,
 	[SYS_munmap] = PLEDGE_STDIO,
+	[SYS_break] = PLEDGE_STDIO,
 
 	[SYS_umask] = PLEDGE_STDIO,
 
