e76a5212745610fa3322ecf1f8dacae8e9c0c8fa
dlg <dlg>
2015-11-20 05:31:24 +0000

    dont check for IFF_OACTIVE
    only a driver sets or clears this flag. pppx never sets it, so it
    will never be set when it checks it.

--- sys/net/if_pppx.c
+++ sys/net/if_pppx.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_pppx.c,v 1.44 2015/10/25 11:58:11 mpi Exp $ */
+/*	$OpenBSD: if_pppx.c,v 1.45 2015/11/03 12:02:59 dlg Exp $ */
 
 /*
  * Copyright (c) 2010 Claudio Jeker <claudio@openbsd.org>
@@ -972,8 +972,6 @@ pppx_if_start(struct ifnet *ifp)
 	struct mbuf *m;
 	int proto, s;
 
-	if (ISSET(ifp->if_flags, IFF_OACTIVE))
-		return;
 	if (!ISSET(ifp->if_flags, IFF_RUNNING))
 		return;
 
