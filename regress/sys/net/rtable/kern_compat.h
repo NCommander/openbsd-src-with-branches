de5cdf2f62ae0623fda3a0778863e427d18597dd
mpi <mpi>
2015-12-04 12:30:57 +0000

    Sync after rt_mask() removal.

--- regress/sys/net/rtable/kern_compat.h
+++ regress/sys/net/rtable/kern_compat.h
@@ -1,4 +1,4 @@
-/* 	$OpenBSD: kern_compat.h,v 1.1 2015/11/04 09:45:52 mpi Exp $ */
+/* 	$OpenBSD: kern_compat.h,v 1.2 2015/12/03 15:15:04 mpi Exp $ */
 
 #ifndef _KERN_COMPAT_H_
 #define _KERN_COMPAT_H_
@@ -24,6 +24,8 @@
 
 #define KASSERT(x)		assert(x)
 #define KERNEL_ASSERT_LOCKED()	/* nothing */
+#define KERNEL_LOCK()		/* nothing */
+#define KERNEL_UNLOCK()		/* nothing */
 
 #define panic(x...) errx(1, x)
 
