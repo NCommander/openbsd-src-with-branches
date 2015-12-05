9e33c0478d6e2e70610d1ec54dee4a98df152c04
mmcc <mmcc>
2015-12-05 19:10:19 +0000

    strings.h -> string.h to prevent an implicit declaration. Also removes two NULL-checks before free().

--- usr.sbin/ypldap/ber.c
+++ usr.sbin/ypldap/ber.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ber.c,v 1.8 2015/01/16 06:40:22 deraadt Exp $ */
+/*	$OpenBSD: ber.c,v 1.9 2015/02/12 00:30:38 pelikan Exp $ */
 
 /*
  * Copyright (c) 2007 Reyk Floeter <reyk@vantronix.net>
@@ -25,7 +25,7 @@
 #include <stdlib.h>
 #include <err.h>	/* XXX for debug output */
 #include <stdio.h>	/* XXX for debug output */
-#include <strings.h>
+#include <string.h>
 #include <unistd.h>
 #include <stdarg.h>
 
@@ -1217,8 +1217,7 @@ ber_set_application(struct ber *b, unsigned long (*cb)(struct ber_element *))
 void
 ber_free(struct ber *b)
 {
-	if (b->br_wbuf != NULL)
-		free (b->br_wbuf);
+	free(b->br_wbuf);
 }
 
 static ssize_t
