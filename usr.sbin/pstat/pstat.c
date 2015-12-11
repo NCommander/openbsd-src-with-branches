1e3efa1a2686a81bb49ac4298fa82e3457fbb008
tedu <tedu>
2015-12-11 11:53:52 +0000

    dformat is pointer, don't compare with 0

--- usr.sbin/pstat/pstat.c
+++ usr.sbin/pstat/pstat.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pstat.c,v 1.99 2015/03/11 14:59:04 deraadt Exp $	*/
+/*	$OpenBSD: pstat.c,v 1.100 2015/08/28 04:38:47 guenther Exp $	*/
 /*	$NetBSD: pstat.c,v 1.27 1996/10/23 22:50:06 cgd Exp $	*/
 
 /*-
@@ -187,7 +187,7 @@ main(int argc, char *argv[])
 	if (dformat && getuid())
 		errx(1, "Only root can use -d");
 
-	if ((dformat == 0 && argc > 0) || (dformat && argc == 0))
+	if ((dformat == NULL && argc > 0) || (dformat && argc == 0))
 		usage();
 
 	need_nlist = vnodeflag || dformat;
