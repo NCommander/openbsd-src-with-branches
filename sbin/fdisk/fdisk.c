fb42c964e54d67fa3af95f2e073cff49601d0f85
krw <krw>
2015-12-12 04:14:36 +0000

    In olden times (i + u + e) was considered a clever way to 'or' together three boolean values. In today's englightened world we know about '||'.
    Pointed out by naddy@

--- sbin/fdisk/fdisk.c
+++ sbin/fdisk/fdisk.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fdisk.c,v 1.97 2015/12/11 21:57:31 krw Exp $	*/
+/*	$OpenBSD: fdisk.c,v 1.98 2015/12/12 02:49:50 krw Exp $	*/
 
 /*
  * Copyright (c) 1997 Tobias Weingartner
@@ -168,7 +168,7 @@ main(int argc, char *argv[])
 		usage();
 
 	disk.name = argv[0];
-	DISK_open(i_flag + u_flag + e_flag);
+	DISK_open(i_flag || u_flag || e_flag);
 
 	error = MBR_read(0, &dos_mbr);
 	if (error)
@@ -185,7 +185,7 @@ main(int argc, char *argv[])
 			    (unsigned long long)DL_GETDSIZE(&dl));
 	}
 
-	if ((i_flag + u_flag + e_flag) == 0) {
+	if (!(i_flag || u_flag || e_flag)) {
 		if (pledge("stdio", NULL) == -1)
 			err(1, "pledge");
 		USER_print_disk(verbosity);
