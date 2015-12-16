2b629850f519ba1de0f8857d14634e6bf062d190
tb <tb>
2015-12-16 01:39:11 +0000

    Replace "tame" by "pledge" in a comment.

--- bin/pax/pax.c
+++ bin/pax/pax.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pax.c,v 1.42 2015/10/12 05:05:24 deraadt Exp $	*/
+/*	$OpenBSD: pax.c,v 1.43 2015/12/06 16:57:45 deraadt Exp $	*/
 /*	$NetBSD: pax.c,v 1.5 1996/03/26 23:54:20 mrg Exp $	*/
 
 /*-
@@ -258,7 +258,7 @@ main(int argc, char **argv)
 
 	/*
 	 * pmode needs to restore setugid bits when extracting or copying,
-	 * so can't tame at all then.
+	 * so can't pledge at all then.
 	 */
 	if (pmode == 0 || (act != EXTRACT && act != COPY)) {
 		if (pledge("stdio rpath wpath cpath dpath fattr getpw ioctl proc exec",
