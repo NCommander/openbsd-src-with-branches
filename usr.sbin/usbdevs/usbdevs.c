e86bbc856f9534f6088951ee608376fa48c48e76
mmcc <mmcc>
2015-12-22 08:36:40 +0000

    Initialize a char* to NULL rather than 0.

--- usr.sbin/usbdevs/usbdevs.c
+++ usr.sbin/usbdevs/usbdevs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: usbdevs.c,v 1.23 2015/02/09 23:00:15 deraadt Exp $	*/
+/*	$OpenBSD: usbdevs.c,v 1.24 2015/03/31 13:38:27 mpi Exp $	*/
 /*	$NetBSD: usbdevs.c,v 1.19 2002/02/21 00:34:31 christos Exp $	*/
 
 /*
@@ -182,7 +182,7 @@ main(int argc, char **argv)
 {
 	int ch, i, f;
 	char buf[50];
-	char *dev = 0;
+	char *dev = NULL;
 	const char *errstr;
 	int addr = 0;
 	int ncont;
