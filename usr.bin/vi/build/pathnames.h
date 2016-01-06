39851178373273991298dd3004ae07eceeb6ae07
millert <millert>
2016-01-06 22:34:45 +0000

    Remove the actual message catalogs.  From Martijn van Duren

--- usr.bin/vi/build/pathnames.h
+++ usr.bin/vi/build/pathnames.h
@@ -1,8 +1,7 @@
-/*	$OpenBSD: pathnames.h,v 1.3 1997/07/27 22:27:54 downsj Exp $	*/
+/*	$OpenBSD: pathnames.h,v 1.4 2014/11/14 20:27:03 tedu Exp $	*/
 /* @(#)pathnames.h.in	8.4 (Berkeley) 6/26/96 */
 
 #define	_PATH_EXRC	".exrc"
-#define	_PATH_MSGCAT	"/usr/share/vi/catalog/"
 #define	_PATH_NEXRC	".nexrc"
 #define	_PATH_PRESERVE	"/var/tmp/vi.recover"
 #define	_PATH_SYSEXRC	"/etc/vi.exrc"
