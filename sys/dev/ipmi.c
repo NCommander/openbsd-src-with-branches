564f14b39a5adcf90a03753b545925856a953a75
uebayasi <uebayasi>
2016-01-10 14:44:09 +0000

    Print a message to console when ipmi(4) watchdog is either enabled or disabled.  Replace a magic number while here.

--- sys/dev/ipmi.c
+++ sys/dev/ipmi.c
@@ -1749,6 +1749,8 @@ ipmi_activate(struct device *self, int act)
 	return (0);
 }
 
+#define		MIN_PERIOD	10
+
 int
 ipmi_watchdog(void *arg, int period)
 {
@@ -1770,8 +1772,8 @@ ipmi_watchdog(void *arg, int period)
 		return (period);
 	}
 
-	if (period < 10 && period > 0)
-		period = 10;
+	if (period < MIN_PERIOD && period > 0)
+		period = MIN_PERIOD;
 
 	s = splsoftclock();
 	sc->sc_poll = 1;
@@ -1798,5 +1800,7 @@ ipmi_watchdog(void *arg, int period)
 	splx(s);
 
 	sc->sc_wdog_period = period;
+	printf("%s: watchdog %sabled\n", DEVNAME(sc),
+	    (period == 0) ? "dis" : "en");
 	return (period);
 }
