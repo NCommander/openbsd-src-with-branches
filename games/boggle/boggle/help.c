2a03b064869e6cf3f3775a7d1c956749d0982494
mestre <mestre>
2016-01-10 14:10:39 +0000

    Remove needless casts to (char *) NULL. Some were kept, namely for execl(3) last arguments since "The list of arguments must be terminated by a null null pointer" of type char *
    Suggested by and OK tb@

--- games/boggle/boggle/help.c
+++ games/boggle/boggle/help.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: help.c,v 1.5 2009/10/27 23:59:24 deraadt Exp $	*/
+/*	$OpenBSD: help.c,v 1.6 2016/01/10 13:18:07 mestre Exp $	*/
 /*	$NetBSD: help.c,v 1.2 1995/03/21 12:14:38 cgd Exp $	*/
 
 /*-
@@ -60,7 +60,7 @@ help(void)
 
 	while (!eof) {
 		for (i = 0; i < nlines - 3; i++) {
-			if (fgets(buf, sizeof(buf), fp) == (char *) NULL) {
+			if (fgets(buf, sizeof(buf), fp) == NULL) {
 				eof = 1;
 				break;
 			}
