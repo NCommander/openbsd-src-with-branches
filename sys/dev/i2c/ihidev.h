0f1a18404cf199f811bb3b826dafc38a4a78aee5
kettenis <kettenis>
2016-01-13 10:25:31 +0000

    Don't confuse the HID descriptor address (which really is an office into the register space of the i2c device) with the i2c address.  For i2c busses enumerated by ACPI we get the address from the I2C Serial Bus Connection Resource Descriptor returned by the _CRS methide of the i2c device.  Pass the HID descriptor address in the ia_size member.
    ok jcs@

--- sys/dev/i2c/ihidev.h
+++ sys/dev/i2c/ihidev.h
@@ -1,4 +1,4 @@
-/* $OpenBSD$ */
+/* $OpenBSD: ihidev.h,v 1.1 2016/01/12 01:11:15 jcs Exp $ */
 /*
  * HID-over-i2c driver
  *
@@ -71,6 +71,7 @@ struct ihidev_softc {
 	i2c_addr_t	sc_addr;
 	void		*sc_ih;
 
+	u_int		sc_hid_desc_addr;
 	union {
 		uint8_t	hid_desc_buf[sizeof(struct i2c_hid_desc)];
 		struct i2c_hid_desc hid_desc;
