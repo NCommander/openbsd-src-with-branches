438f4bb5608d116b3fdff8eee838baae7ceed0a6
krw <krw>
2016-01-14 04:17:40 +0000

    Nuke 'HFS_name' field which was not referenced now other than setting it and freeing it. Unhook hfs_misc.c from build since it now longer contains anything being used.

--- sbin/pdisk/partition_map.h
+++ sbin/pdisk/partition_map.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: partition_map.h,v 1.4 2016/01/11 07:54:07 jasper Exp $	*/
+/*	$OpenBSD: partition_map.h,v 1.5 2016/01/12 15:32:08 krw Exp $	*/
 
 //
 // partition_map.h - partition map routines
@@ -71,7 +71,6 @@ struct partition_map {
     int contains_driver;
     DPME *data;
     int HFS_kind;
-    char *HFS_name;
 };
 typedef struct partition_map partition_map;
 
