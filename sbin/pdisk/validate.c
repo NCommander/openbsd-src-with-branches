6375710d4a4069e82de15f3b13eaef5efef7601b
krw <krw>
2016-01-16 21:41:41 +0000

    There can be only one. 'grain' that is. i.e. DEV_BSIZE. No need to track or store. Just use.
    No functional change.

--- sbin/pdisk/validate.c
+++ sbin/pdisk/validate.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: validate.c,v 1.12 2016/01/16 14:49:28 krw Exp $	*/
+/*	$OpenBSD: validate.c,v 1.13 2016/01/16 20:00:50 krw Exp $	*/
 
 //
 // validate.c -
@@ -355,10 +355,7 @@ validate_map(partition_map_header *map)
 	    free(name);
 	    return;
 	}
-	g = media_granularity(the_media);
-	if (g < DEV_BSIZE) {
-	    g = DEV_BSIZE;
-	}
+	g = DEV_BSIZE;
 
 	buffer = malloc(DEV_BSIZE);
 	if (buffer == NULL) {
