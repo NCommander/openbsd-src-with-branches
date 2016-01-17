8cc72e92d8f13e6b8e9b37020d2ae08e6406ed50
krw <krw>
2016-01-17 16:07:06 +0000

    The great de-typedef'ification continues. DPME -> struct dpme.

--- sbin/pdisk/pdisk.c
+++ sbin/pdisk/pdisk.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pdisk.c,v 1.36 2016/01/16 22:28:14 krw Exp $	*/
+/*	$OpenBSD: pdisk.c,v 1.37 2016/01/17 15:57:12 krw Exp $	*/
 
 //
 // pdisk - an editor for Apple format partition tables
@@ -121,10 +121,10 @@ main(int argc, char **argv)
 {
     int name_index;
 
-    if (sizeof(DPME) != DEV_BSIZE) {
+    if (sizeof(struct dpme) != DEV_BSIZE) {
 	errx(1, "Size of partition map entry (%zu) "
 		"is not equal to block size (%d)\n",
-		sizeof(DPME), DEV_BSIZE);
+		sizeof(struct dpme), DEV_BSIZE);
     }
     if (sizeof(Block0) != DEV_BSIZE) {
 	errx(1, "Size of block zero structure (%zu) "
