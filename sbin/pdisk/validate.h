033399b28c2a9d529c27a4508e18531433191263
krw <krw>
2016-01-17 17:44:05 +0000

    The great de-typedef'ification continues.
    partition_map -> struct partition_map
    partition_map_header -> struct partition_map_header

--- sbin/pdisk/validate.h
+++ sbin/pdisk/validate.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: validate.h,v 1.4 2016/01/11 07:54:07 jasper Exp $	*/
 
 //
 // validate.h -
@@ -56,6 +56,6 @@
 //
 // Forward declarations
 //
-void validate_map(partition_map_header *map);
+void validate_map(struct partition_map_header *map);
 
 #endif /* __validate__ */
