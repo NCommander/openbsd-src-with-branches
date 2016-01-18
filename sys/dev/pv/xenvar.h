0c759436efbe0eb5cd547367e864fab4ec73cb27
mikeb <mikeb>
2016-01-18 19:06:48 +0000

    Provide a Xen v3 API compatible fallback for event channel hypercalls

--- sys/dev/pv/xenvar.h
+++ sys/dev/pv/xenvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: xenvar.h,v 1.20 2016/01/15 14:27:08 mikeb Exp $	*/
+/*	$OpenBSD: xenvar.h,v 1.21 2016/01/15 18:20:41 mikeb Exp $	*/
 
 /*
  * Copyright (c) 2015 Mike Belopuhov
@@ -103,6 +103,7 @@ struct xen_attach_args {
  *  Hypercalls
  */
 #define XC_MEMORY		12
+#define XC_OEVTCHN		16
 #define XC_VERSION		17
 #define XC_GNTTAB		20
 #define XC_EVTCHN		32
