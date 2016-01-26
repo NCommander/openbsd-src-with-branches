18f1a6ae4bdea66a5c00d62d14ceb7ad9f9919f4
krw <krw>
2016-01-26 16:13:09 +0000

    Whitespace, line wrapping fixes.

--- sbin/pdisk/file_media.c
+++ sbin/pdisk/file_media.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: file_media.c,v 1.40 2016/01/25 22:12:22 krw Exp $	*/
+/*	$OpenBSD: file_media.c,v 1.41 2016/01/25 23:43:20 krw Exp $	*/
 
 /*
  * file_media.c -
@@ -155,7 +155,8 @@ read_block0(int fd, struct block0 *block0)
 		    sizeof(ddmap_ondisk));
 		memcpy(&block0->sbDDMap[i].ddBlock, &ddmap_ondisk.ddBlock,
 		    sizeof(block0->sbDDMap[i].ddBlock));
-		block0->sbDDMap[i].ddBlock = betoh32(block0->sbDDMap[i].ddBlock);
+		block0->sbDDMap[i].ddBlock =
+		    betoh32(block0->sbDDMap[i].ddBlock);
 		memcpy(&block0->sbDDMap[i].ddSize, &ddmap_ondisk.ddSize,
 		    sizeof(block0->sbDDMap[i].ddSize));
 		block0->sbDDMap[i].ddSize = betoh16(block0->sbDDMap[i].ddSize);
