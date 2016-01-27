b9a1090d716c0b22a8acf4e769c25cac70599fa3
beck <beck>
2016-01-27 02:09:51 +0000

    deprecate SSL_OP_SINGLE_DH_USE ok jsing@

--- lib/libc/arch/hppa64/gen/nan.c
+++
@@ -1,9 +0,0 @@
-/*	$OpenBSD$	*/
-
-/* Written by Martynas Venckus.  Public Domain. */
-
-#include <math.h>
-
-/* bytes for qNaN on a hppa64 (IEEE single format) */
-char __nan[] __attribute__((__aligned__(sizeof(float)))) =
-					{ 0x7f, 0xa0, 0, 0 };
--- regress/gnu/egcs/gcc-bounds/strlcpy-5.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int main(int argc, char **argv) {
-	char buf[10];
-	char *buf2;
-	strlcpy(buf, "foo", sizeof buf2);
-	return 1;
-}
