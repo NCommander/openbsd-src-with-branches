c7fc14652fb25bbc67cbd80bd08aac873677debd
mlarkin <mlarkin>
2016-02-26 02:23:07 +0000

    SYMTAB_SPACE is no longer used (last used with a.out ddb)

--- sys/arch/i386/i386/locore.s
+++ sys/arch/i386/i386/locore.s
@@ -1,4 +1,4 @@
-/*	$OpenBSD: locore.s,v 1.160 2015/08/12 06:19:25 mlarkin Exp $	*/
+/*	$OpenBSD: locore.s,v 1.161 2015/08/25 04:57:31 mlarkin Exp $	*/
 /*	$NetBSD: locore.s,v 1.145 1996/05/03 19:41:19 christos Exp $	*/
 
 /*-
@@ -507,7 +507,7 @@ try586:	/* Use the `cpuid' instruction. */
 
 	/* Find end of kernel image. */
 	movl	$RELOC(_C_LABEL(end)),%edi
-#if (defined(DDB) || NKSYMS > 0) && !defined(SYMTAB_SPACE)
+#if (NKSYMS || defined(DDB))
 	/* Save the symbols (if loaded). */
 	movl	RELOC(_C_LABEL(esym)),%eax
 	testl	%eax,%eax
