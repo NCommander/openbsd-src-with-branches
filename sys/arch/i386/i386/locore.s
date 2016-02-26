9aba598d9c20fe479a69c84374e55b1064a5dd4b
mlarkin <mlarkin>
2016-02-26 02:25:09 +0000

    pctr.h is not used here, so no need to include it.

--- sys/arch/i386/i386/locore.s
+++ sys/arch/i386/i386/locore.s
@@ -1,4 +1,4 @@
-/*	$OpenBSD: locore.s,v 1.161 2015/08/25 04:57:31 mlarkin Exp $	*/
+/*	$OpenBSD: locore.s,v 1.162 2016/02/26 02:23:07 mlarkin Exp $	*/
 /*	$NetBSD: locore.s,v 1.145 1996/05/03 19:41:19 christos Exp $	*/
 
 /*-
@@ -41,7 +41,6 @@
 #include "apm.h"
 #include "lapic.h"
 #include "ioapic.h"
-#include "pctr.h"
 #include "ksyms.h"
 #include "acpi.h"
 
