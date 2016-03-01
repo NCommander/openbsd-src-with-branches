5059734c3748376dae20e62d5e653aaa45f17f49
jca <jca>
2016-03-01 12:52:43 +0000

    Pointless 'return;' in void functions

--- usr.sbin/rtadvd/config.c
+++ usr.sbin/rtadvd/config.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: config.c,v 1.53 2016/02/29 06:37:55 jca Exp $	*/
+/*	$OpenBSD: config.c,v 1.54 2016/02/29 20:25:46 jca Exp $	*/
 /*	$KAME: config.c,v 1.62 2002/05/29 10:13:10 itojun Exp $	*/
 
 /*
@@ -892,8 +892,6 @@ make_packet(struct rainfo *rainfo)
 		while (((uintptr_t)buf) % 8 != 0)
 			*buf++ = '\0';
 	}
-
-	return;
 }
 
 static int
