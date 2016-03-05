c795ce1ff4f0393cf12fae6abbe46d908125e380
nicm <nicm>
2016-03-05 07:47:52 +0000

    Do not use c->cwd or s->cwd if it is NULL, found by Ben Boeckel.

--- usr.bin/tmux/cmd-new-session.c
+++ usr.bin/tmux/cmd-new-session.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: cmd-new-session.c,v 1.85 2016/01/31 09:52:01 nicm Exp $ */
+/* $OpenBSD: cmd-new-session.c,v 1.86 2016/02/04 14:11:20 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -142,7 +142,7 @@ cmd_new_session_exec(struct cmd *self, struct cmd_q *cmdq)
 		format_defaults(ft, c, NULL, NULL, NULL);
 		to_free = cwd = format_expand(ft, args_get(args, 'c'));
 		format_free(ft);
-	} else if (c != NULL && c->session == NULL)
+	} else if (c != NULL && c->session == NULL && c->cwd != NULL)
 		cwd = c->cwd;
 	else
 		cwd = ".";
