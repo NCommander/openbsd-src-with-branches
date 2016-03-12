c7140843395d0a5a8dbe7432d4f57daf4d17b1a7
bcook <bcook>
2016-03-12 20:50:31 +0000

    bump for LibreSSL 2.3.3

--- regress/usr.bin/m4/translit.m4
+++
@@ -1,3 +0,0 @@
-dnl $OpenBSD$
-dnl first one should match, not second one
-translit(`onk*', `**', `p_')
--- sys/arch/armish/include/_float.h
+++
@@ -1,3 +0,0 @@
-/*	$OpenBSD: float.h,v 1.1 2006/05/29 17:13:19 drahn Exp $	*/
-
-#include <arm/_float.h>
