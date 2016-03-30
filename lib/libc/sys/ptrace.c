c7cb42a9af938cea3aca72e4b963f02dbef8c9b4
guenther <guenther>
2016-03-30 07:55:36 +0000

    Turd polish: use HIDDEN= instead of PSEUDO= for ptrace syscall stub, to make its underlying symbol name look like others.

--- lib/libc/sys/ptrace.c
+++ lib/libc/sys/ptrace.c
@@ -1,17 +1,16 @@
-/*	$OpenBSD: ptrace.c,v 1.3 2003/06/11 21:03:10 deraadt Exp $	*/
+/*	$OpenBSD: ptrace.c,v 1.4 2005/12/21 01:40:23 millert Exp $	*/
 /* David Leonard <d@openbsd.org>, 1999. Public domain. */
 
 #include <sys/types.h>
 #include <sys/ptrace.h>
 #include <errno.h>
 
-int _ptrace(int, pid_t, caddr_t, int);
-
 int
-ptrace(int request, pid_t pid, caddr_t addr, int data)
+WRAP(ptrace)(int request, pid_t pid, caddr_t addr, int data)
 {
 
 	/* ptrace(2) is documented to clear errno on success: */
 	errno = 0;
-	return (_ptrace(request, pid, addr, data));
+	return (ptrace(request, pid, addr, data));
 }
+DEF_WRAP(ptrace);
