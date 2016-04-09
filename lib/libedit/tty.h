0c5fbf1ac5405f9e24ce9e2fe45e73ecf5d824ab
schwarze <schwarze>
2016-04-09 18:42:49 +0000

    Reset the terminal to its initial state before exiting a program using libedit, using code from NetBSD tty.c rev. 1.42, 1.48, 1.49, 1.58, and 1.59.  Code mostly by Christos Zoulas, one bug report by John Hein, one additional bugfix by me (rev. 1.59). While here, switch t_mode to unsigned char, which is sufficient, also from NetBSD.
    OK martijn@.
    Also checked by Christian Heckendorf <mbie at ulmus dot me>.

--- lib/libedit/tty.h
+++ lib/libedit/tty.h
@@ -1,5 +1,5 @@
-/*	$OpenBSD: tty.h,v 1.9 2014/05/20 22:28:07 yasuoka Exp $	*/
-/*	$NetBSD: tty.h,v 1.12 2009/12/30 22:37:40 christos Exp $	*/
+/*	$OpenBSD: tty.h,v 1.10 2016/03/20 22:57:59 schwarze Exp $	*/
+/*	$NetBSD: tty.h,v 1.19 2016/02/27 18:13:21 christos Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -469,12 +469,13 @@ protected void	tty_bind_char(EditLine *, int);
 typedef struct {
     ttyperm_t t_t;
     ttychar_t t_c;
-    struct termios t_ex, t_ed, t_ts;
+    struct termios t_or, t_ex, t_ed, t_ts;
     int t_tabs;
     int t_eight;
     speed_t t_speed;
-    int t_mode;
+    unsigned char t_mode;
     unsigned char t_vdisable;
+    unsigned char t_initialized;
 } el_tty_t;
 
 
