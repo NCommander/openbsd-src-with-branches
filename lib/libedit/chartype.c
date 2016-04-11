84d6168b9c2634624bfeae13aa793b12d1909535
schwarze <schwarze>
2016-04-11 20:54:05 +0000

    Clean up chartype.h:
    * not used in tokenizer.c
    * only use it for !NARROWCHAR
    * no need for underscores before ct_{de,en}code_string()
    * make the conversion buffer resize function private
    
    OK martijn@

--- lib/libedit/chartype.c
+++ lib/libedit/chartype.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: chartype.c,v 1.12 2016/04/09 20:15:26 schwarze Exp $	*/
+/*	$OpenBSD: chartype.c,v 1.13 2016/04/11 20:43:33 schwarze Exp $	*/
 /*	$NetBSD: chartype.c,v 1.6 2011/07/28 00:48:21 christos Exp $	*/
 
 /*-
@@ -40,7 +40,9 @@
 
 #define CT_BUFSIZ 1024
 
-protected void
+private void ct_conv_buff_resize(ct_buffer_t *, size_t, size_t);
+
+private void
 ct_conv_buff_resize(ct_buffer_t *conv, size_t mincsize, size_t minwsize)
 {
 	void *p;
