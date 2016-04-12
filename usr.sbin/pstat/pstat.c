de5c0803bb2fe8a8703f4af1e7ed08c7b78c2bda
millert <millert>
2016-04-12 16:53:42 +0000

    The -T flag consumes the same data as the -f flag so it requires nlist too.  Fixes a crash when the -T flag is specified due to calling kvm_read() with a NULL kvm_t.  From Rob Pierce

--- usr.sbin/pstat/pstat.c
+++ usr.sbin/pstat/pstat.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pstat.c,v 1.100 2015/08/28 04:38:47 guenther Exp $	*/
+/*	$OpenBSD: pstat.c,v 1.101 2015/12/11 11:53:52 tedu Exp $	*/
 /*	$NetBSD: pstat.c,v 1.27 1996/10/23 22:50:06 cgd Exp $	*/
 
 /*-
@@ -193,7 +193,7 @@ main(int argc, char *argv[])
 	need_nlist = vnodeflag || dformat;
 
 	if (nlistf != NULL || memf != NULL) {
-		if (fileflag)
+		if (fileflag || totalflag)
 			need_nlist = 1;
 	}
 
