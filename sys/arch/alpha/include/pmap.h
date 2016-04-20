6eea30c479b71f511fe89709f9a0f6940e1b6782
landry <landry>
2016-04-20 05:24:18 +0000

    Move back #include <sys/mutex.h> outside #ifdef _KERNEL as it was originally intended in r1.38. (ie more or less revert r1.39)
    (re-)Fixes at least net/net-snmp, sysutils/gkrellm/gkrellm,-client,
    sysutils/xuvmstat and sysutils/bubblemon-dockapp on alpha.
    
    From miod@

--- sys/arch/alpha/include/pmap.h
+++ sys/arch/alpha/include/pmap.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: pmap.h,v 1.38 2016/02/22 07:13:47 landry Exp $ */
+/* $OpenBSD: pmap.h,v 1.39 2016/02/22 07:50:37 deraadt Exp $ */
 /* $NetBSD: pmap.h,v 1.37 2000/11/19 03:16:35 thorpej Exp $ */
 
 /*-
@@ -70,12 +70,12 @@
 #ifndef	_PMAP_MACHINE_
 #define	_PMAP_MACHINE_
 
+#include <sys/mutex.h>
 #include <machine/pte.h>
 
 #ifdef _KERNEL
 
 #include <sys/queue.h>
-#include <sys/mutex.h>
 
 /*
  * Machine-dependent virtual memory state.
