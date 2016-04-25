ce0a9647e317a993c94b04de089bc101a87cf478
tedu <tedu>
2016-04-25 19:12:07 +0000

    remove references to systrace

--- bin/ps/print.c
+++ bin/ps/print.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: print.c,v 1.65 2015/12/30 14:59:10 tedu Exp $	*/
+/*	$OpenBSD: print.c,v 1.66 2016/01/10 14:04:16 schwarze Exp $	*/
 /*	$NetBSD: print.c,v 1.27 1995/09/29 21:58:12 cgd Exp $	*/
 
 /*-
@@ -264,8 +264,6 @@ printstate(const struct kinfo_proc *kp, VARENT *ve)
 		*cp++ = 'N';
 	if (kp->p_psflags & PS_TRACED)
 		*cp++ = 'X';
-	if (flag & P_SYSTRACE)
-		*cp++ = 'x';
 	if ((kp->p_psflags & (PS_EXITING | PS_ZOMBIE)) == PS_EXITING)
 		*cp++ = 'E';
 	if (kp->p_psflags & PS_ISPWAIT)
