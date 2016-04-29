92c9e3fd896b1b445bb93878e6641e5597750af7
nicm <nicm>
2016-04-29 15:00:48 +0000

    Add option to include status text in the pane borders. If pane-border-status is set to "top" or "bottom" (rather than "off"), every pane has a permanent top or bottom border containing the text from pane-border-format.
    Based on a diff sent long ago by Jonathan Slenders, mostly rewritten and
    simplified by me.

--- usr.bin/tmux/cmd-set-option.c
+++ usr.bin/tmux/cmd-set-option.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: cmd-set-option.c,v 1.93 2016/01/19 15:59:12 nicm Exp $ */
+/* $OpenBSD: cmd-set-option.c,v 1.94 2016/03/03 14:15:22 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -200,6 +200,12 @@ cmd_set_option_exec(struct cmd *self, struct cmd_q *cmdq)
 		status_timer_start_all();
 	if (strcmp(oe->name, "monitor-silence") == 0)
 		alerts_reset_all();
+
+	/* When the pane-border-status option has been changed, resize panes. */
+	if (strcmp(oe->name, "pane-border-status") == 0) {
+		RB_FOREACH(w, windows, &windows)
+			layout_fix_panes(w, w->sx, w->sy);
+	}
 
 	/* Update sizes and redraw. May not need it but meh. */
 	recalculate_sizes();
