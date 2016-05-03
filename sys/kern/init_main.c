59dbc345adeda22de9bb4c35adce94cb0305cf8f
mpi <mpi>
2016-05-03 14:52:39 +0000

    Stop using a soft-interrupt context to process incoming network packets.
    Use a new task that runs holding the KERNEL_LOCK to execute mp-unsafe
    code.  Our current goal is to progressively move input functions to the
    unlocked task.
    
    This gives a small performance boost confirmed by Hrvoje Popovski's
    IPv4 forwarding measurement:
    
    before:					after:
    
    send           receive			send           receive
    400kpps        400kpps                  400kpps        400kpps
    500kpps        500kpps                  500kpps        500kpps
    600kpps        600kpps                  600kpps        600kpps
    650kpps        650kpps                  650kpps        640kpps
    700kpps        700kpps                  700kpps        700kpps
    720kpps        640kpps                  720kpps        710kpps
    800kpps        640kpps                  800kpps        650kpps
    1.4Mpps        570kpps                  1.4Mpps        590kpps
    14Mpps         570kpps                  14Mpps         590kpps
    
    
    ok kettenis@, bluhm@, dlg@

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: init_main.c,v 1.248 2016/01/03 00:15:59 jsg Exp $	*/
+/*	$OpenBSD: init_main.c,v 1.249 2016/03/19 12:04:15 natano Exp $	*/
 /*	$NetBSD: init_main.c,v 1.84.4.1 1996/06/02 09:08:06 mrg Exp $	*/
 
 /*
@@ -394,7 +394,6 @@ main(void *framep)
 	 * until everything is ready.
 	 */
 	s = splnet();
-	netisr_init();
 	domaininit();
 	splx(s);
 
