c6f03e4e36662b1c9a6f85f05663d6db0fcfd75e
guenther <guenther>
2016-05-10 04:04:34 +0000

    In a signal handler use snprintf()+local buffer instead of asprintf+free

--- regress/lib/libpthread/pthread_kill/pthread_kill.c
+++ regress/lib/libpthread/pthread_kill/pthread_kill.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: pthread_kill.c,v 1.3 2003/06/19 00:59:54 pvalchev Exp $ */
+/* $OpenBSD: pthread_kill.c,v 1.4 2003/07/31 21:48:05 deraadt Exp $ */
 /* PUBLIC DOMAIN Oct 2002 <marc@snafu.org> */
 
 /*
@@ -18,15 +18,15 @@ static void
 act_handler(int signal, siginfo_t *siginfo, void *context)
 {
 	struct sigaction sa;
-	char *str;
+	char buf[200];
 
 	CHECKe(sigaction(SIGUSR1, NULL, &sa));
 	ASSERT(sa.sa_handler == SIG_DFL);
 	ASSERT(siginfo != NULL);
-	asprintf(&str, "act_handler: signal %d, siginfo %p, context %p\n",
-		 signal, siginfo, context);
-	write(STDOUT_FILENO, str, strlen(str));
-	free(str);
+	snprintf(buf, sizeof buf,
+	    "act_handler: signal %d, siginfo %p, context %p\n",
+	    signal, siginfo, context);
+	write(STDOUT_FILENO, buf, strlen(buf));
 }
  
 static void *
