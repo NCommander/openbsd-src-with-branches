a8026aefd90f89d143d91652869e722529bd52b1
gilles <gilles>
2016-05-16 19:25:05 +0000

    fix logic in error code path of smtp state machine that can lead to an invalid state ending in fatal(), while at it plug a file pointer leak.
    ok eric@, millert@

--- usr.sbin/smtpd/filter.c
+++ usr.sbin/smtpd/filter.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: filter.c,v 1.16 2016/02/03 11:14:08 eric Exp $	*/
+/*	$OpenBSD: filter.c,v 1.17 2016/03/25 15:06:58 krw Exp $	*/
 
 /*
  * Copyright (c) 2011 Gilles Chehade <gilles@poolp.org>
@@ -297,6 +297,8 @@ filter_event(uint64_t id, int event)
 	if (event == EVENT_DISCONNECT) {
 		io_clear(&s->iev);
 		iobuf_clear(&s->ibuf);
+		if (s->ofile)
+			fclose(s->ofile);
 		free(s);
 	}
 }
