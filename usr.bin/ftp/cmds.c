ca79752792d7c87e7cf80cf2681387058e99b37f
krw <krw>
2016-05-25 15:36:01 +0000

    Avoid a use-after-free.
    Diff from Vladimir Sotirov via tech@. Thanks!
    
    ok millert@

--- usr.bin/ftp/cmds.c
+++ usr.bin/ftp/cmds.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cmds.c,v 1.75 2015/10/18 03:04:11 mmcc Exp $	*/
+/*	$OpenBSD: cmds.c,v 1.76 2016/03/17 19:40:43 krw Exp $	*/
 /*	$NetBSD: cmds.c,v 1.27 1997/08/18 10:20:15 lukem Exp $	*/
 
 /*
@@ -1643,9 +1643,7 @@ void
 newer(int argc, char *argv[])
 {
 
-	if (getit(argc, argv, -1, "w"))
-		fprintf(ttyout, "Local file \"%s\" is newer than remote file \"%s\".\n",
-			argv[2], argv[1]);
+	(void)getit(argc, argv, -1, "w");
 }
 
 /*
