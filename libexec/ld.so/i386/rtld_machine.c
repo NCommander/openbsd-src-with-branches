5f70ea32b5bb65a3be995a3a484d74f75b84904e
deraadt <deraadt>
2016-05-26 17:23:39 +0000

    for textrels (sthen ran into one...): Ignore the listed protection (which may contain X) when making page writeable temporary.

--- libexec/ld.so/i386/rtld_machine.c
+++ libexec/ld.so/i386/rtld_machine.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rtld_machine.c,v 1.32 2015/11/02 07:02:53 guenther Exp $ */
+/*	$OpenBSD: rtld_machine.c,v 1.33 2016/05/22 01:40:57 deraadt Exp $ */
 
 /*
  * Copyright (c) 2002 Dale Rahn
@@ -200,7 +200,7 @@ _dl_md_reloc(elf_object_t *object, int rel, int relsz)
 		for (llist = object->load_list; llist != NULL; llist = llist->next) {
 			if (!(llist->prot & PROT_WRITE))
 				_dl_mprotect(llist->start, llist->size,
-				    llist->prot|PROT_WRITE);
+				    PROT_READ | PROT_WRITE);
 		}
 	}
 
