9d8c0ae2f7d47e0b9e2b0f253b885a18c4b61fe3
guenther <guenther>
2016-05-29 22:42:24 +0000

    _yp_bind(), _ypbindlist, _yp_domain, and _yplib_timeout are no longer exported, so declare them as hidden to avoid pointless GOT relocations
    ok millert@ deraadt@

--- lib/libc/yp/yp_bind.c
+++ lib/libc/yp/yp_bind.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: yp_bind.c,v 1.25 2015/09/14 12:09:35 guenther Exp $ */
+/*	$OpenBSD: yp_bind.c,v 1.26 2015/11/26 07:26:50 deraadt Exp $ */
 /*
  * Copyright (c) 1992, 1993, 1996 Theo de Raadt <deraadt@theos.com>
  * All rights reserved.
@@ -257,7 +257,6 @@ _yp_unbind(struct dom_binding *ypb)
 	ypb->dom_client = NULL;
 	ypb->dom_socket = -1;
 }
-DEF_WEAK(_yp_unbind);
 
 int
 yp_bind(const char *dom)
