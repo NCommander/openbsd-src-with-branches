03925f054b9e600d8f49a3fbc3c1bec8a7f113f6
dlg <dlg>
2016-06-01 23:45:19 +0000

    make nd6_llinfo_settimer take seconds instead of ticks.
    most callers are working in seconds, internally it uses seconds, and
    you can call timeout_add_sec as easily as timeout_add.
    
    this also fixes an issue with an nd_defrouter expire which was
    incorrectly scaled with ticks in a comparison.
    
    ok mpi@

--- sys/netinet6/nd6.h
+++ sys/netinet6/nd6.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: nd6.h,v 1.58 2016/03/30 10:13:14 mpi Exp $	*/
+/*	$OpenBSD: nd6.h,v 1.59 2016/05/30 23:37:37 dlg Exp $	*/
 /*	$KAME: nd6.h,v 1.95 2002/06/08 11:31:06 itojun Exp $	*/
 
 /*
@@ -260,7 +260,7 @@ struct nd_opt_hdr *nd6_option(union nd_opts *);
 int nd6_options(union nd_opts *);
 struct	rtentry *nd6_lookup(struct in6_addr *, int, struct ifnet *, u_int);
 void nd6_setmtu(struct ifnet *);
-void nd6_llinfo_settimer(struct llinfo_nd6 *, long);
+void nd6_llinfo_settimer(struct llinfo_nd6 *, int);
 void nd6_timer(void *);
 void nd6_purge(struct ifnet *);
 void nd6_nud_hint(struct rtentry *);
