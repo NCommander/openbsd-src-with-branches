809d1098f5bf5700ac1baff61f5b30b5b5b30ce2
stefan <stefan>
2016-06-07 16:19:06 +0000

    Allocate RAM for guest VM in vmd(8) and pass it to vmm(4)
    vmm(4) then maps the pages allocated by the vmd(8) user process into the
    address space of the guest. This gives vmm(4) and vmd(8) a shared view of
    the guest RAM. This will allow us to have faster guest<->host data
    exchange by ordinary memory loads/stores later, as well as remove the
    vm_readpage and vm_writepage ioctls next.
    
    ok mlarkin@

--- sys/arch/amd64/include/vmmvar.h
+++ sys/arch/amd64/include/vmmvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vmmvar.h,v 1.12 2016/04/06 06:15:06 mlarkin Exp $	*/
+/*	$OpenBSD: vmmvar.h,v 1.13 2016/04/25 15:24:55 mlarkin Exp $	*/
 /*
  * Copyright (c) 2014 Mike Larkin <mlarkin@openbsd.org>
  *
@@ -179,6 +179,7 @@ struct vcpu_init_state {
 
 struct vm_mem_range {
 	paddr_t	vmr_gpa;
+	vaddr_t vmr_va;
 	size_t	vmr_size;
 };
 
