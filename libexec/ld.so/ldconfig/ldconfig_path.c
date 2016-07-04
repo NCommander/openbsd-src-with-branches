260893fb6dae14e91f2f5edb59b7961dbf775211
kettenis <kettenis>
2016-07-04 20:56:50 +0000

    Nuke prebind support; it's unworkable and we're never going to finish it.
    ok guenther@, deraadt@

--- libexec/ld.so/ldconfig/ldconfig_path.c
+++ libexec/ld.so/ldconfig/ldconfig_path.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: prebind_path.c,v 1.3 2014/06/21 08:00:23 otto Exp $	*/
+/*	$OpenBSD: prebind_path.c,v 1.4 2015/08/23 06:27:32 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2013 Kurt Miller <kurt@intricatesoftware.com>
