ffdd9e1643ac04cfa8b30246040cc58d13125fa8
kettenis <kettenis>
2016-07-12 19:17:49 +0000

    Turn ahci(4) glue code for the i.MX6 platform into its own imxahci(4) driver such that we can don't end up with conflicting attachments of other SoC-specific glue code.

--- sys/arch/armv7/imx/imxahci.c
+++ sys/arch/armv7/imx/imxahci.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: imxahci.c,v 1.3 2014/04/14 04:42:22 dlg Exp $ */
+/* $OpenBSD: imxahci.c,v 1.4 2016/06/09 12:39:17 kettenis Exp $ */
 /*
  * Copyright (c) 2013 Patrick Wildt <patrick@blueri.se>
  *
@@ -95,7 +95,7 @@ struct cfattach imxahci_ca = {
 };
 
 struct cfdriver imxahci_cd = {
-	NULL, "ahci", DV_DULL
+	NULL, "imxahci", DV_DULL
 };
 
 int
