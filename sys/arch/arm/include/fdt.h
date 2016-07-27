18096c428248f60759bbb3890887d56bb1204ec8
patrick <patrick>
2016-07-27 11:45:02 +0000

    Instead of passing the raw reg property to simplebus nodes, pass a pre-processed array of fdt_reg structs.  This means that the drivers don't have to understand the cell properties themselves but can rely on the 64-bit addr/size pairs.
    ok kettenis@

--- sys/arch/arm/include/fdt.h
+++ sys/arch/arm/include/fdt.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: fdt.h,v 1.2 2016/06/09 12:32:42 kettenis Exp $ */
+/* $OpenBSD: fdt.h,v 1.3 2016/07/13 20:42:44 patrick Exp $ */
 /*
  * Copyright (c) 2016 Patrick Wildt <patrick@blueri.se>
  *
@@ -26,7 +26,7 @@ struct fdt_attach_args {
 	int			 fa_node;
 	bus_space_tag_t		 fa_iot;
 	bus_dma_tag_t		 fa_dmat;
-	uint32_t		*fa_reg;
+	struct fdt_reg		*fa_reg;
 	int			 fa_nreg;
 	uint32_t		*fa_intr;
 	int			 fa_nintr;
