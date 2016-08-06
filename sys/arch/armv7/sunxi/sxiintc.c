72cca6c1bb12df35f4132b0fae08e2eda580c186
patrick <patrick>
2016-08-06 18:21:34 +0000

    Always allocate intrhand with M_WAITOK.
    Requested by and ok kettenis@

--- sys/arch/armv7/sunxi/sxiintc.c
+++ sys/arch/armv7/sunxi/sxiintc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sxiintc.c,v 1.8 2016/08/05 19:00:25 kettenis Exp $	*/
+/*	$OpenBSD: sxiintc.c,v 1.1 2016/08/05 20:38:17 kettenis Exp $	*/
 /*
  * Copyright (c) 2007,2009 Dale Rahn <drahn@openbsd.org>
  * Copyright (c) 2013 Artturi Alm
@@ -388,11 +388,7 @@ sxiintc_intr_establish(int irq, int level, int (*func)(void *),
 
 	psw = disable_interrupts(PSR_I);
 
-	/* no point in sleeping unless someone can free memory. */
-	ih = (struct intrhand *)malloc (sizeof *ih, M_DEVBUF,
-	    cold ? M_NOWAIT : M_WAITOK);
-	if (ih == NULL)
-		panic("intr_establish: can't malloc handler info\n");
+	ih = malloc(sizeof(*ih), M_DEVBUF, M_WAITOK);
 	ih->ih_func = func;
 	ih->ih_arg = arg;
 	ih->ih_ipl = level;
