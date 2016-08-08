c249d2fbd0773f88b58b7358951c6dfbf8c3fe4d
deraadt <deraadt>
2016-08-08 17:15:51 +0000

    W^X violations are only permitted for binaries marked "wxneeded" on "wxallowed" filesystems.  mmap(2) & mprotect(2) now return ENOTSUP. (To diagnose buggy programs, consider using sysctl kern.wxabort=1 and looking at the coredumps) ok kettenis tedu naddy

--- sys/uvm/uvm_mmap.c
+++ sys/uvm/uvm_mmap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_mmap.c,v 1.136 2016/07/13 17:49:00 kettenis Exp $	*/
+/*	$OpenBSD: uvm_mmap.c,v 1.137 2016/07/13 17:52:37 kettenis Exp $	*/
 /*	$NetBSD: uvm_mmap.c,v 1.49 2001/02/18 21:19:08 chs Exp $	*/
 
 /*
@@ -334,7 +334,7 @@ uvm_wxcheck(struct proc *p, char *call)
 		setsigvec(p, SIGABRT, &sa);
 		psignal(p, SIGABRT);
 	}
-	return (0);		/* ENOTSUP later */
+	return (ENOTSUP);
 }
 
 /*
