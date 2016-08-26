7d6e96689dd9dbb6c52d1edc7b65fa0ccd7cf5ad
rzalamena <rzalamena>
2016-08-26 12:24:21 +0000

    Kill the ps_ninstances from proc.c.
    We got the same information in ps_instances[proc] (more accurate) and
    we avoid allocating unnecessary memory for pipe storage.
    
    ok reyk@

--- usr.sbin/httpd/httpd.c
+++ usr.sbin/httpd/httpd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: httpd.c,v 1.56 2016/06/10 12:09:48 florian Exp $	*/
+/*	$OpenBSD: httpd.c,v 1.57 2016/08/16 18:41:57 tedu Exp $	*/
 
 /*
  * Copyright (c) 2014 Reyk Floeter <reyk@openbsd.org>
@@ -232,7 +232,6 @@ main(int argc, char *argv[])
 		log_info("startup");
 
 	ps->ps_instances[PROC_SERVER] = env->sc_prefork_server;
-	ps->ps_ninstances = env->sc_prefork_server;
 
 	if (env->sc_chroot == NULL)
 		env->sc_chroot = ps->ps_pw->pw_dir;
