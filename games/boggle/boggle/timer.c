181844517382f0ac0fd98a032af5d85835ccb36d
guenther <guenther>
2016-08-27 02:11:27 +0000

    Pull in <sys/select.h> for fd_set Pull in <time.h> for time()
    ok deraadt@

--- games/boggle/boggle/timer.c
+++ games/boggle/boggle/timer.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: timer.c,v 1.13 2016/01/10 13:18:07 mestre Exp $	*/
+/*	$OpenBSD: timer.c,v 1.14 2016/01/10 13:35:09 mestre Exp $	*/
 /*	$NetBSD: timer.c,v 1.3 1995/04/24 12:22:45 cgd Exp $	*/
 
 /*-
@@ -33,8 +33,10 @@
  * SUCH DAMAGE.
  */
 
+#include <sys/select.h>
 #include <curses.h>
 #include <setjmp.h>
+#include <time.h>
 #include <unistd.h>
 
 #include "extern.h"
