6cc36612760ce20bca3e600b7d499d68e9db4a90
florian <florian>
2016-08-31 07:31:20 +0000

    update to 4.1.11 "Working fine here." millert@ OK dlg, sthen

--- usr.sbin/nsd/xfrd-disk.h
+++ usr.sbin/nsd/xfrd-disk.h
@@ -10,8 +10,8 @@
 #ifndef XFRD_DISK_H
 #define XFRD_DISK_H
 
-#include <config.h>
 struct xfrd_state;
+struct nsd;
 
 /* magic string to identify xfrd state file */
 #define XFRD_FILE_MAGIC "NSDXFRD1"
@@ -20,5 +20,16 @@ struct xfrd_state;
 void xfrd_read_state(struct xfrd_state* xfrd);
 /* write xfrd zone state if possible */
 void xfrd_write_state(struct xfrd_state* xfrd);
+
+/* create temp directory */
+void xfrd_make_tempdir(struct nsd* nsd);
+/* rmdir temp directory */
+void xfrd_del_tempdir(struct nsd* nsd);
+/* open temp file, makes directory if needed */
+FILE* xfrd_open_xfrfile(struct nsd* nsd, uint64_t number, char* mode);
+/* unlink temp file */
+void xfrd_unlink_xfrfile(struct nsd* nsd, uint64_t number);
+/* get temp file size */
+uint64_t xfrd_get_xfrfile_size(struct nsd* nsd, uint64_t number );
 
 #endif /* XFRD_DISK_H */
