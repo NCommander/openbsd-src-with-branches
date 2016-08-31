e369c67c256f0b50cdc860ce327ad0369b8f5f1c
jsg <jsg>
2016-08-31 09:48:26 +0000

    close an opened fd before returning from main ok guenther@

--- usr.bin/audioctl/audioctl.c
+++ usr.bin/audioctl/audioctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: audioctl.c,v 1.32 2016/06/21 21:16:42 ratchov Exp $	*/
+/*	$OpenBSD: audioctl.c,v 1.33 2016/06/21 22:35:39 jmc Exp $	*/
 /*
  * Copyright (c) 2016 Alexandre Ratchov <alex@caoua.org>
  *
@@ -277,5 +277,6 @@ main(int argc, char **argv)
 		print_val(f, f->waddr);
 		printf("\n");
 	}
+	close(fd);
 	return 0;	
 }
