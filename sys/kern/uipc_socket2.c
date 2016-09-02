fa1d87d4a4972d5f6c7826d05dc28023263a0dd1
bluhm <bluhm>
2016-09-02 13:28:21 +0000

    After allocating a single 64 KB mbuf cluster in sosend(), the sending socket buffer had no space anymore.  The default mbuf space limit was only 32 KB.  So no more data from user-land was accepted.  As tcp_output() keeps the mbuf cluster for retransmits, it will be freed only after all ACKs have been received.  That has killed our TCP send performance totally.  To allow cycling through the mbufs periodically, we need space for at least 3 of them. Reported by Andreas Bartelt; testing with mikeb@; OK mikeb@ claudio@

--- sys/kern/uipc_socket2.c
+++ sys/kern/uipc_socket2.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uipc_socket2.c,v 1.63 2015/10/06 14:38:32 claudio Exp $	*/
+/*	$OpenBSD: uipc_socket2.c,v 1.64 2016/06/28 14:47:00 tedu Exp $	*/
 /*	$NetBSD: uipc_socket2.c,v 1.11 1996/02/04 02:17:55 christos Exp $	*/
 
 /*
@@ -397,7 +397,8 @@ sbreserve(struct sockbuf *sb, u_long cc)
 	if (cc == 0 || cc > sb_max)
 		return (1);
 	sb->sb_hiwat = cc;
-	sb->sb_mbmax = min(cc * 2, sb_max + (sb_max / MCLBYTES) * MSIZE);
+	sb->sb_mbmax = max(3 * MAXMCLBYTES,
+	    min(cc * 2, sb_max + (sb_max / MCLBYTES) * MSIZE));
 	if (sb->sb_lowat > sb->sb_hiwat)
 		sb->sb_lowat = sb->sb_hiwat;
 	return (0);
