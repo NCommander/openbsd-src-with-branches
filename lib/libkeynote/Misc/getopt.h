246e1e06b1cafb01a2ce37b3027c0e64dd74aeb4
cvs-fast-export <cvs-fast-export>
2016-09-05 11:36:23 +0000

    Synthetic commit for incomplete tag LIBCOMPILER_RT_3_9_0

--- lib/libkeynote/Misc/getopt.h
+++
@@ -1,6 +0,0 @@
-/* $OpenBSD$ */
-
-extern int   opterr;   /* flag:error message on unrecognzed options */
-extern int   optind;   /* last touched cmdline argument */
-extern char  *optarg;  /* argument to optopt */
-int getopt(int argc, char **argv, char *opts);
--- sys/arch/mvme68k/include/db_machdep.h
+++
@@ -1,6 +0,0 @@
-/*	$NetBSD: db_machdep.h,v 1.2 1994/10/26 02:33:40 cgd Exp $	*/
-
-#define DB_MACHINE_COMMANDS
-
-/* Just use the common m68k definition */
-#include <m68k/db_machdep.h>
