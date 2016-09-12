bd37ba075655ca370a572ac392e6994e8f681c0f
guenther <guenther>
2016-09-12 19:36:26 +0000

    Don't declare select() in <unistd.h>
    ok deraadt@ millert@

--- include/unistd.h
+++ include/unistd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: unistd.h,v 1.101 2016/05/23 00:12:58 guenther Exp $ */
+/*	$OpenBSD: unistd.h,v 1.102 2016/09/09 18:12:37 millert Exp $ */
 /*	$NetBSD: unistd.h,v 1.26.4.1 1996/05/28 02:31:51 mrg Exp $	*/
 
 /*-
@@ -508,11 +508,6 @@ int	 revoke(const char *);
 int	 rresvport(int *);
 int	 rresvport_af(int *, int);
 int	 ruserok(const char *, int, const char *, const char *);
-#ifndef _SELECT_DEFINED_
-#define _SELECT_DEFINED_
-struct timeval;
-int	 select(int, fd_set *, fd_set *, fd_set *, struct timeval *);
-#endif
 int	 setdomainname(const char *, size_t);
 int	 setgroups(int, const gid_t *);
 int	 sethostid(long);
