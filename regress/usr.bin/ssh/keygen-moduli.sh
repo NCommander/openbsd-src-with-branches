e1a4a7ee6bc82e76b1549564153283f9e04c90e0
dtucker <dtucker>
2016-09-12 02:25:46 +0000

    Add testcase for ssh-keygen -j, -J and -K options for moduli screening. Does not currently test generation as that is extremely slow.

---
+++ regress/usr.bin/ssh/keygen-moduli.sh
@@ -0,0 +1,15 @@
+#	$OpenBSD$
+#	Placed in the Public Domain.
+
+tid="keygen moduli"
+
+for i in 0 1 2; do
+	rm -f $OBJ/moduli.out $OBJ/moduli.ckpt
+	${SSHKEYGEN} -T $OBJ/moduli.out -f ${SRC}/moduli.in -j$i -J1 \
+	    -K $OBJ/moduli.ckpt 2>/dev/null || \
+	    fail "keygen screen failed line $i"
+	lines=`wc -l <$OBJ/moduli.out`
+	test "$lines" -eq "1" || fail "expected 1 line, got $lines"
+done
+
+rm -f $OBJ/moduli.out $OBJ/moduli.ckpt
