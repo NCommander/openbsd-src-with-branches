05999ec5710bedd40915cf35d3c7eaa3f3f66e10
jasper <jasper>
2016-09-13 18:09:14 +0000

    when loading the kernel binary, also load the .SUNW_ctf section when present, which holds the CTF data.
    ok mpi@

--- sys/lib/libsa/loadfile_elf.c
+++ sys/lib/libsa/loadfile_elf.c
@@ -1,5 +1,5 @@
 /* $NetBSD: loadfile.c,v 1.10 2000/12/03 02:53:04 tsutsui Exp $ */
-/* $OpenBSD: loadfile_elf.c,v 1.10 2014/10/26 10:33:48 miod Exp $ */
+/* $OpenBSD: loadfile_elf.c,v 1.11 2015/05/19 20:39:12 miod Exp $ */
 
 /*-
  * Copyright (c) 1997 The NetBSD Foundation, Inc.
@@ -233,7 +233,8 @@ ELFNAME(exec)(int fd, Elf_Ehdr *elf, u_long *marks, int flags)
 		for (first = 1, i = 0; i < elf->e_shnum; i++) {
 			if (shp[i].sh_type == SHT_SYMTAB ||
 			    shp[i].sh_type == SHT_STRTAB ||
-			    !strcmp(shstr + shp[i].sh_name, ".debug_line")) {
+			    !strcmp(shstr + shp[i].sh_name, ".debug_line") ||
+			    !strcmp(shstr + shp[i].sh_name, ".SUNW_ctf")) {
 				if (havesyms && (flags & LOAD_SYM)) {
 					PROGRESS(("%s%ld", first ? " [" : "+",
 					    (u_long)shp[i].sh_size));
