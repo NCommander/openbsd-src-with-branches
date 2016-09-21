c34a8e3ca2136aa58f99cb2304e2092e89e29cba
kettenis <kettenis>
2016-09-21 11:33:05 +0000

    Modernize arm assembly in the kernel for clang. Based on a similar diff in bitrig. No binary change when compiled with gcc.
    ok patrick@

--- sys/arch/arm/include/asm.h
+++ sys/arch/arm/include/asm.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: asm.h,v 1.5 2015/08/30 10:19:49 guenther Exp $	*/
+/*	$OpenBSD: asm.h,v 1.6 2016/05/27 16:32:38 deraadt Exp $	*/
 /*	$NetBSD: asm.h,v 1.4 2001/07/16 05:43:32 matt Exp $	*/
 
 /*
@@ -50,7 +50,7 @@
 #endif
 
 #ifndef _ALIGN_TEXT
-# define _ALIGN_TEXT .align 0
+# define _ALIGN_TEXT .align 2
 #endif
 
 /*
