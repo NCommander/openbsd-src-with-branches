78d71e726f5082f52b1a809ec0921c27e4f15a72
mlarkin <mlarkin>
2016-10-03 04:53:54 +0000

    disable PAT and MTRR in guest VMs

--- sys/arch/amd64/include/vmmvar.h
+++ sys/arch/amd64/include/vmmvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vmmvar.h,v 1.18 2016/09/01 16:04:47 stefan Exp $	*/
+/*	$OpenBSD: vmmvar.h,v 1.19 2016/09/04 08:49:18 mlarkin Exp $	*/
 /*
  * Copyright (c) 2014 Mike Larkin <mlarkin@openbsd.org>
  *
@@ -300,7 +300,7 @@ struct vm_rwregs_params {
 #define VMX_FAIL_LAUNCH_INVALID_VMCS 2
 #define VMX_FAIL_LAUNCH_VALID_VMCS 3
 
-#define VMX_NUM_MSR_STORE 7
+#define VMX_NUM_MSR_STORE 6
 
 /* MSR bitmap manipulation macros */
 #define MSRIDX(m) ((m) / 8)
