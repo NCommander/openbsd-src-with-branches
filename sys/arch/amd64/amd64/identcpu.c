e2359e76a2eaf0e7b8c3ee3dbf926ada0d5c6236
martijn <martijn>
2016-10-13 19:36:25 +0000

    Add an extra debug line when virtualization is disabled in the firmware. This line would have saved me about an hour of hairpulling.
    OK mlarkin@

--- sys/arch/amd64/amd64/identcpu.c
+++ sys/arch/amd64/amd64/identcpu.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: identcpu.c,v 1.76 2016/09/27 08:06:22 mlarkin Exp $	*/
+/*	$OpenBSD: identcpu.c,v 1.77 2016/09/30 07:33:06 mlarkin Exp $	*/
 /*	$NetBSD: identcpu.c,v 1.1 2003/04/26 18:39:28 fvdl Exp $	*/
 
 /*
@@ -799,6 +799,8 @@ cpu_check_vmm_cap(struct cpu_info *ci)
 		else {
 			if (msr & IA32_FEATURE_CONTROL_VMX_EN)
 				ci->ci_vmm_flags |= CI_VMM_VMX;
+			else
+				ci->ci_vmm_flags |= CI_VMM_DIS;
 		}
 	}
 
--- sys/arch/amd64/amd64/vmm.c
+++ sys/arch/amd64/amd64/vmm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vmm.c,v 1.91 2016/10/06 07:51:10 mlarkin Exp $	*/
+/*	$OpenBSD: vmm.c,v 1.92 2016/10/06 18:52:09 reyk Exp $	*/
 /*
  * Copyright (c) 2014 Mike Larkin <mlarkin@openbsd.org>
  *
@@ -259,7 +259,7 @@ vmm_probe(struct device *parent, void *match, void *aux)
 	struct cpu_info *ci;
 	CPU_INFO_ITERATOR cii;
 	const char **busname = (const char **)aux;
-	int found_vmx, found_svm;
+	int found_vmx, found_svm, vm_disabled;
 
 	/* Check if this probe is for us */
 	if (strcmp(*busname, vmm_cd.cd_name) != 0)
@@ -274,13 +274,19 @@ vmm_probe(struct device *parent, void *match, void *aux)
 			found_vmx = 1;
 		if (ci->ci_vmm_flags & CI_VMM_SVM)
 			found_svm = 1;
+		if (ci->ci_vmm_flags & CI_VMM_DIS)
+			vm_disabled = 1;
 	}
 
 	/* Don't support both SVM and VMX at the same time */
 	if (found_vmx && found_svm)
 		return (0);
 
-	return (found_vmx || found_svm);
+	if (found_vmx || found_svm)
+		return 1;
+	if (vm_disabled)
+		printf("vmm disabled by firmware\n");
+	return 0;
 }
 
 /*
