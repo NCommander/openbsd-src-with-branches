b664206a8363bbe64338868db008c9711d3007c7
guenther <guenther>
2016-10-18 00:43:57 +0000

    Delete remnants of "traptrace" support
    ok deraadt@

--- sys/arch/sparc64/sparc64/autoconf.c
+++ sys/arch/sparc64/sparc64/autoconf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: autoconf.c,v 1.125 2016/03/07 13:21:51 naddy Exp $	*/
+/*	$OpenBSD: autoconf.c,v 1.126 2016/06/08 17:24:44 tedu Exp $	*/
 /*	$NetBSD: autoconf.c,v 1.51 2001/07/24 19:32:11 eeh Exp $ */
 
 /*
@@ -586,11 +586,6 @@ bootpath_build(void)
 #else
 			printf("kernel has no debugger\n");
 #endif
-		} else if (*cp == 't') {
-			/* turn on traptrace w/o breaking into kdb */
-			extern int trap_trace_dis;
-
-			trap_trace_dis = 0;
 		}
 	}
 }
