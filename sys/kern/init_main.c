59f17b8d2d81ba211b2fc53d3813b05fb6a4b56a
dlg <dlg>
2016-10-24 04:38:44 +0000

    move the mbstat structure to percpu counters
    each cpus counters still have to be protected by splnet, but this
    is better thana single set of counters protected by a global mutex.
    
    ok bluhm@

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: init_main.c,v 1.259 2016/09/22 12:55:24 mpi Exp $	*/
+/*	$OpenBSD: init_main.c,v 1.260 2016/10/21 06:27:50 dlg Exp $	*/
 /*	$NetBSD: init_main.c,v 1.84.4.1 1996/06/02 09:08:06 mrg Exp $	*/
 
 /*
@@ -414,6 +414,8 @@ main(void *framep)
 	/* Initialize kernel profiling. */
 	prof_init();
 #endif
+
+	mbcpuinit();	/* enable per cpu mbuf data */
 
 	/* init exec and emul */
 	init_exec();
