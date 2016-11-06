4382d77dccab4cacf805ab339a14f5ba972697b4
mpi <mpi>
2016-11-06 12:58:01 +0000

    Avoid calling usbd_set_config_no() in *_attach() and let the stack do it instead.
    If anything bad happen due to a malformed descriptor it makes no sense
    to try to attach a driver, and bail before probing.
    
    This is similar to the change to avoid calling usbd_set_config_index().

--- sys/dev/usb/if_ral.c
+++ sys/dev/usb/if_ral.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ral.c,v 1.139 2016/04/13 11:03:37 mpi Exp $	*/
+/*	$OpenBSD: if_ral.c,v 1.140 2016/07/20 10:24:43 stsp Exp $	*/
 
 /*-
  * Copyright (c) 2005, 2006
@@ -196,7 +196,7 @@ ural_match(struct device *parent, void *match, void *aux)
 {
 	struct usb_attach_arg *uaa = aux;
 
-	if (uaa->iface != NULL)
+	if (uaa->iface == NULL || uaa->configno != RAL_CONFIG_NO)
 		return UMATCH_NONE;
 
 	return (usb_lookup(ural_devs, uaa->vendor, uaa->product) != NULL) ?
@@ -216,12 +216,6 @@ ural_attach(struct device *parent, struct device *self, void *aux)
 	int i;
 
 	sc->sc_udev = uaa->device;
-
-	if (usbd_set_config_no(sc->sc_udev, RAL_CONFIG_NO, 0) != 0) {
-		printf("%s: could not set configuration no\n",
-		    sc->sc_dev.dv_xname);
-		return;
-	}
 
 	/* get the first interface handle */
 	error = usbd_device2interface_handle(sc->sc_udev, RAL_IFACE_INDEX,
