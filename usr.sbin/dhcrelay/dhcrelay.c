dc782ae9ab221a061e53d089f0ce225e01287abc
reyk <reyk>
2016-12-07 16:41:17 +0000

    pledge(2) dhcrelay with "stdio route".  route is needed for interface ioctls.
    OK rzalamena@

--- usr.sbin/dhcrelay/dhcrelay.c
+++ usr.sbin/dhcrelay/dhcrelay.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dhcrelay.c,v 1.43 2016/09/26 17:15:19 jca Exp $ */
+/*	$OpenBSD: dhcrelay.c,v 1.44 2016/12/07 13:19:18 rzalamena Exp $ */
 
 /*
  * Copyright (c) 2004 Henning Brauer <henning@cvs.openbsd.org>
@@ -247,6 +247,9 @@ main(int argc, char *argv[])
 			error("rdaemon: %m");
 		log_perror = 0;
 	}
+
+	if (pledge("stdio route", NULL) == -1)
+		error("pledge");
 
 	dispatch();
 	/* not reached */
