157593daab7a8aa3366b33f3a2ef48049bbf7d86
stsp <stsp>
2016-12-18 11:15:02 +0000

    Remove duplicated lines of code introduced in my previous tcpdump commit. Noticed by claudio@

--- usr.sbin/tcpdump/print-802_11.c
+++ usr.sbin/tcpdump/print-802_11.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: print-802_11.c,v 1.35 2016/11/19 19:35:46 stsp Exp $	*/
+/*	$OpenBSD: print-802_11.c,v 1.36 2016/12/18 10:38:01 stsp Exp $	*/
 
 /*
  * Copyright (c) 2005 Reyk Floeter <reyk@openbsd.org>
@@ -1103,9 +1103,6 @@ ieee802_11_radio_if_print(u_char *user, const struct pcap_pkthdr *h,
 
 		if (flags & IEEE80211_CHAN_HT)
 			printf(", 11n");
-		else if (flags & IEEE80211_CHAN_DYN &&
-		    flags & IEEE80211_CHAN_2GHZ)
-			printf(", 11g");
 		else if (flags & IEEE80211_CHAN_DYN &&
 		    flags & IEEE80211_CHAN_2GHZ)
 			printf(", 11g");
