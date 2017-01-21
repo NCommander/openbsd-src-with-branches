86f2dac8e60a399f1d8fa90a91eaf58d2c1110e5
florian <florian>
2017-01-21 08:49:59 +0000

    typo; ok benno

--- usr.sbin/acme-client/main.c
+++ usr.sbin/acme-client/main.c
@@ -1,4 +1,4 @@
-/*	$Id: main.c,v 1.19 2017/01/21 08:47:21 benno Exp $ */
+/*	$Id: main.c,v 1.20 2017/01/21 08:48:11 florian Exp $ */
 /*
  * Copyright (c) 2016 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -175,7 +175,7 @@ main(int argc, char *argv[])
 	}
 
 	if (!(popts & ACME_OPT_NEWACCT) && -1 == access(acctkey, R_OK)) {
-		warnx("%s: -f file must exist", acctkey);
+		warnx("%s: account key file must exist", acctkey);
 		ne++;
 	} else if ((popts & ACME_OPT_NEWACCT) && -1 != access(acctkey, R_OK)) {
 		dodbg("%s: account key exists (not creating)", acctkey);
