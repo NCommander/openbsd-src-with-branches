9636894c7bb7b7432853df3d3357d6453cde431d
nicm <nicm>
2017-01-23 10:09:43 +0000

    Open /dev/ptm before pledge() and save it to be used for PTMGET later (this means inlining forkpty()).
    ok deraadt

--- usr.bin/tmux/client.c
+++ usr.bin/tmux/client.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: client.c,v 1.117 2017/01/16 14:52:25 nicm Exp $ */
+/* $OpenBSD: client.c,v 1.118 2017/01/20 14:02:33 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -303,6 +303,8 @@ client_main(struct event_base *base, int argc, char **argv, int flags,
 		fatal("pledge failed");
 
 	/* Free stuff that is not used in the client. */
+	if (ptm_fd != -1)
+		close(ptm_fd);
 	options_free(global_options);
 	options_free(global_s_options);
 	options_free(global_w_options);
