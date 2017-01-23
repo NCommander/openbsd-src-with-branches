3678dfcc56240ae16892824b4c2c6683e86e3803
dlg <dlg>
2017-01-23 04:25:02 +0000

    add the mfii opcode for passthru commands

--- sys/dev/pci/mfii.c
+++ sys/dev/pci/mfii.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: mfii.c,v 1.28 2016/10/24 05:27:52 yasuoka Exp $ */
+/* $OpenBSD: mfii.c,v 1.29 2017/01/23 01:10:31 dlg Exp $ */
 
 /*
  * Copyright (c) 2012 David Gwynne <dlg@openbsd.org>
@@ -51,6 +51,7 @@
 
 #define MFII_REQ_MFA(_a)	htole64((_a) | MFII_REQ_TYPE_MFA)
 
+#define MFII_FUNCTION_PASSTHRU_IO			(0xf0)
 #define MFII_FUNCTION_LDIO_REQUEST			(0xf1)
 
 struct mfii_request_descr {
