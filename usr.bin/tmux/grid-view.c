3d432d7cacd562773fd083ef307f269a298e8b70
nicm <nicm>
2017-02-08 16:45:18 +0000

    Collect sequences of printable ASCII characters and process them together instead of handling them one by one. This is significantly faster. Sequences are terminated when we reach the end of the line, fill the internal buffer, or a different character is seen by the input parser (an escape sequence, or UTF-8).
    Rather than writing collected sequences out immediately, hold them until
    it is necessary (another screen modification, or we consume all
    available data). This means we can discard changes that would have no
    effect (for example, lines that would just be scrolled off the screen or
    cleared). This reduces the total amount of data we write out to the
    terminal - not important for fast terminals, but a big help with slow
    (like xterm).

--- usr.bin/tmux/grid-view.c
+++ usr.bin/tmux/grid-view.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: grid-view.c,v 1.25 2016/09/02 20:57:20 nicm Exp $ */
+/* $OpenBSD: grid-view.c,v 1.26 2016/10/13 20:27:27 nicm Exp $ */
 
 /*
  * Copyright (c) 2008 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -43,6 +43,15 @@ grid_view_set_cell(struct grid *gd, u_int px, u_int py,
     const struct grid_cell *gc)
 {
 	grid_set_cell(gd, grid_view_x(gd, px), grid_view_y(gd, py), gc);
+}
+
+/* Set cells. */
+void
+grid_view_set_cells(struct grid *gd, u_int px, u_int py,
+    const struct grid_cell *gc, const char *s, size_t slen)
+{
+	grid_set_cells(gd, grid_view_x(gd, px), grid_view_y(gd, py), gc, s,
+	    slen);
 }
 
 /* Clear into history. */
