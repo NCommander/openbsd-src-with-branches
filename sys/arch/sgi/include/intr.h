09698078ce9dddf5160c68dffb0cee6502b31bdd
visa <visa>
2017-02-11 03:44:22 +0000

    Let MP-safe interrupt handlers run without the kernel lock on sgi.
    OK miod@

--- sys/arch/sgi/include/intr.h
+++ sys/arch/sgi/include/intr.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: intr.h,v 1.46 2015/12/25 09:22:00 visa Exp $ */
+/*	$OpenBSD: intr.h,v 1.47 2016/03/06 19:42:27 mpi Exp $ */
 
 /*
  * Copyright (c) 2001-2004 Opsycon AB  (www.opsycon.se / www.opsycon.com)
@@ -62,7 +62,7 @@
 #define IPL_MPFLOOR	IPL_TTY
 
 /* Interrupt priority 'flags'. */
-#define	IPL_MPSAFE	0	/* no "mpsafe" interrupts */
+#define	IPL_MPSAFE	0x100
 
 /* Interrupt sharing types. */
 #define	IST_NONE	0	/* none */
@@ -166,6 +166,7 @@ struct intrhand {
 	struct evcount		 ih_count;
 	int			 ih_flags;
 #define	IH_ALLOCATED		0x01
+#define	IH_MPSAFE		0x02
 };
 
 void	intr_barrier(void *);
