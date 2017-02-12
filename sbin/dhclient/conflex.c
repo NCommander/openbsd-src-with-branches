d37ccd78085f55a36e83988757f80d4bcd16b22a
krw <krw>
2017-02-12 13:15:50 +0000

    Switch from 'legacy' errwarn.c to standard daemon logging functions.
    No objections heard. Feedback from millert@ guenther@

--- sbin/dhclient/conflex.c
+++ sbin/dhclient/conflex.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: conflex.c,v 1.33 2016/02/06 19:30:52 krw Exp $	*/
+/*	$OpenBSD: conflex.c,v 1.34 2016/08/16 21:57:51 krw Exp $	*/
 
 /* Lexical scanner for dhclient config file. */
 
@@ -60,6 +60,7 @@
 #include "dhcp.h"
 #include "dhcpd.h"
 #include "dhctoken.h"
+#include "log.h"
 
 int lexline;
 int lexchar;
