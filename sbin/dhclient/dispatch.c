e2508922a89648cf53a366cc73bb9eda3521b42b
krw <krw>
2017-03-08 15:46:36 +0000

    Stray #endif w/o comment.

--- sbin/dhclient/dispatch.c
+++ sbin/dhclient/dispatch.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dispatch.c,v 1.115 2017/02/12 15:53:15 krw Exp $	*/
+/*	$OpenBSD: dispatch.c,v 1.116 2017/02/14 22:46:53 krw Exp $	*/
 
 /*
  * Copyright 2004 Henning Brauer <henning@openbsd.org>
@@ -281,7 +281,7 @@ interface_status(struct interface_info *ifi)
 #ifdef DEBUG
 		if (errno != EINVAL && errno != ENOTTY)
 			log_debug("ioctl(SIOCGIFMEDIA) on %s", ifi->name);
-#endif
+#endif	/* DEBUG */
 
 		ifi->flags |= IFI_NOMEDIA;
 		goto active;
