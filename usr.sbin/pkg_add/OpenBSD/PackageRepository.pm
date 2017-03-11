2d98da8ff8896264ac8f57d5cba4b5a7ca9bb06f
espie <espie>
2017-03-11 11:25:01 +0000

    tweak %m to expand to %c, and make %c more magical: on a -stable system, %c/packages will expand to two directories in support of packages-stable, with the -stable directory being "silent".
    Also add a -Dsnap option that forces %c to be snapshots
    so that the last weeks of testing of release are less
    painful.
    
    okay aja@

--- usr.sbin/pkg_add/OpenBSD/PackageRepository.pm
+++ usr.sbin/pkg_add/OpenBSD/PackageRepository.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: PackageRepository.pm,v 1.140 2017/02/06 16:12:16 espie Exp $
+# $OpenBSD: PackageRepository.pm,v 1.141 2017/03/07 14:19:32 espie Exp $
 #
 # Copyright (c) 2003-2010 Marc Espie <espie@openbsd.org>
 #
@@ -568,6 +568,10 @@ sub parse_url
 		$$r = $path;
 		my $o = $class->SUPER::parse_url($r, $state);
 		$o->{host} = $host;
+		if (defined $o->{release}) {
+			$o->can_be_empty;
+			$$r = $class->urlscheme."://$o->{host}$o->{release}:$$r";
+		}
 		return $o;
 	} else {
 		return undef;
