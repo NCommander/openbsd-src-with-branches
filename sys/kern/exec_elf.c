e85887b6bf7b69a4adde33cdac843e242a5a532f
kettenis <kettenis>
2017-03-20 00:05:21 +0000

    Initialize the stack buffer used to build the auxiliary vector to zero to avoid leaking the contents of the kernel stack into userspace.
    ok guenther@, deraadt@

--- sys/kern/exec_elf.c
+++ sys/kern/exec_elf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: exec_elf.c,v 1.138 2017/02/11 06:07:03 guenther Exp $	*/
+/*	$OpenBSD: exec_elf.c,v 1.139 2017/03/05 00:45:30 guenther Exp $	*/
 
 /*
  * Copyright (c) 1996 Per Fogelstrom
@@ -802,6 +802,7 @@ exec_elf_fixup(struct proc *p, struct exec_package *epp)
 	 * linked binaries
 	 */
 	if (error == 0) {
+		memset(&ai, 0, sizeof ai);
 		a = ai;
 
 		a->au_id = AUX_phdr;
