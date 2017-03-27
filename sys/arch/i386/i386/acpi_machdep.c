568f4fe4f5839da82110188d36dd54c98486d6f0
mlarkin <mlarkin>
2017-03-27 18:32:53 +0000

    add a newline to an error printf

--- sys/arch/i386/i386/acpi_machdep.c
+++ sys/arch/i386/i386/acpi_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: acpi_machdep.c,v 1.60 2016/05/20 02:30:41 mlarkin Exp $	*/
+/*	$OpenBSD: acpi_machdep.c,v 1.61 2017/03/27 18:24:08 deraadt Exp $	*/
 /*
  * Copyright (c) 2005 Thorsten Lockert <tholo@sigmasoft.com>
  *
@@ -355,7 +355,7 @@ acpi_sleep_cpu(struct acpi_softc *sc, int state)
 #ifdef HIBERNATE
 		if (state == ACPI_STATE_S4) {
 			if (hibernate_suspend()) {
-				printf("%s: hibernate_suspend failed",
+				printf("%s: hibernate_suspend failed\n",
 				    DEVNAME(sc));
 				return (ECANCELED);
 			}
