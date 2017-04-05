f4cc97f01649f9145963ac4a36bb2b63283ad0a8
deraadt <deraadt>
2017-04-05 04:00:41 +0000

    Here at OpenBSD we change ABIs at the fling of a hat.  Just in case a future disk info sysctl has pads in the structures, use M_ZERO when allocating the storage to avoid leaking kernel memory.

--- sys/kern/kern_sysctl.c
+++ sys/kern/kern_sysctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_sysctl.c,v 1.321 2017/01/21 05:42:03 guenther Exp $	*/
+/*	$OpenBSD: kern_sysctl.c,v 1.322 2017/03/07 09:29:40 mpi Exp $	*/
 /*	$NetBSD: kern_sysctl.c,v 1.17 1996/05/20 17:49:05 mrg Exp $	*/
 
 /*-
@@ -2088,9 +2088,9 @@ sysctl_diskinit(int update, struct proc *p)
 		diskstats = NULL;
 		disknames = NULL;
 		diskstats = mallocarray(disk_count, sizeof(struct diskstats),
-		    M_SYSCTL, M_WAITOK);
+		    M_SYSCTL, M_WAITOK|M_ZERO);
 		diskstatslen = disk_count * sizeof(struct diskstats);
-		disknames = malloc(tlen, M_SYSCTL, M_WAITOK);
+		disknames = malloc(tlen, M_SYSCTL, M_WAITOK|M_ZERO);
 		disknameslen = tlen;
 		disknames[0] = '\0';
 
