3e2669b6c698e39be790d59664827c88e708adc7
mlarkin <mlarkin>
2017-04-14 01:02:28 +0000

    SVM: calculate max ASID value and save for later use. This will be used in an upcoming diff to handle ASID/VPID reuse/rollover.

--- sys/arch/amd64/include/cpu.h
+++ sys/arch/amd64/include/cpu.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cpu.h,v 1.109 2017/03/11 11:55:03 mpi Exp $	*/
+/*	$OpenBSD: cpu.h,v 1.110 2017/03/16 10:02:03 mpi Exp $	*/
 /*	$NetBSD: cpu.h,v 1.1 2003/04/26 18:39:39 fvdl Exp $	*/
 
 /*-
@@ -77,6 +77,7 @@ struct vmx {
  * SVM for AMD CPUs
  */
 struct svm {
+	uint32_t	svm_max_asid;
 };
 
 union vmm_cpu_cap {
