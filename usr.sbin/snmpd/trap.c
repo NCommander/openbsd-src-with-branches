801cd13bb679d9b72ddda5f91f9c3eb7adcd61ca
jca <jca>
2017-04-21 13:46:15 +0000

    Fix uninitialized variable that coult result in a crash.
    Fix from Rivo Nurges.

--- usr.sbin/snmpd/trap.c
+++ usr.sbin/snmpd/trap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: trap.c,v 1.27 2016/10/28 08:01:53 rzalamena Exp $	*/
+/*	$OpenBSD: trap.c,v 1.28 2016/11/09 20:31:56 jca Exp $	*/
 
 /*
  * Copyright (c) 2008 Reyk Floeter <reyk@openbsd.org>
@@ -70,6 +70,7 @@ trap_agentx(struct agentx_handle *h, struct agentx_pdu *pdu, int *idx,
 
 	*varcpy = NULL;
 	varbind = NULL;
+	iter = NULL;
 	seensysuptime = seentrapoid = 0;
 
 	if (pdu->hdr->flags & AGENTX_NON_DEFAULT_CONTEXT) {
