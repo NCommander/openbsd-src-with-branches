0e8c21b269325f91f152cdd21e7fe66ab25cbb86
jsing <jsing>
2017-04-30 00:06:09 +0000

    MFC.
    Fix a bug caused by the return value being set early to signal successful
    DTLS cookie validation. This can mask a later failure and result in a
    positive return value being returned from ssl3_get_client_hello(), when
    it should return a negative value to propagate the error.
    
    ok beck@

--- lib/libssl/src/ssl/s3_srvr.c
+++ lib/libssl/src/ssl/s3_srvr.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: s3_srvr.c,v 1.126 2016/05/30 13:42:54 beck Exp $ */
+/* $OpenBSD: ssl_srvr.c,v 1.15 2017/04/29 23:38:49 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -721,7 +721,7 @@ ssl3_send_hello_request(SSL *s)
 int
 ssl3_get_client_hello(SSL *s)
 {
-	int i, j, ok, al, ret = -1;
+	int i, j, ok, al, ret = -1, cookie_valid = 0;
 	unsigned int cookie_len;
 	long n;
 	unsigned long id;
@@ -887,7 +887,7 @@ ssl3_get_client_hello(SSL *s)
 				goto f_err;
 			}
 
-			ret = 2;
+			cookie_valid = 1;
 		}
 
 		p += cookie_len;
@@ -1070,8 +1070,8 @@ ssl3_get_client_hello(SSL *s)
 		goto err;
 	}
 
-	if (ret < 0)
-		ret = 1;
+	ret = cookie_valid ? 2 : 1;
+
 	if (0) {
 truncated:
 		al = SSL_AD_DECODE_ERROR;
