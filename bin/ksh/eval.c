4ddcae74e9cb497669e53ade0c262c35baaef0d4
millert <millert>
2017-05-01 19:05:49 +0000

    Quiet an "implicit conversion from 'int' to 'char' changes value" warning from clang.

--- bin/ksh/eval.c
+++ bin/ksh/eval.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: eval.c,v 1.49 2015/12/30 09:07:00 tedu Exp $	*/
+/*	$OpenBSD: eval.c,v 1.50 2016/03/05 12:30:17 czarkoff Exp $	*/
 
 /*
  * Expansion - quoting, separation, substitution, globbing
@@ -326,7 +326,7 @@ expand(char *cp,	/* input word */
 						 * expected)
 						 */
 						*dp++ = MAGIC;
-						*dp++ = '@' + 0x80;
+						*dp++ = '@' + 0x80U;
 						break;
 					case '=':
 						/* Enabling tilde expansion
