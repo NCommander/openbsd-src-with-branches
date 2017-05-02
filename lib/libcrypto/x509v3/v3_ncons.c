fcd06fcd731cec022694a93d716132e36c34a0ce
deraadt <deraadt>
2017-05-02 04:11:08 +0000

    the XXXfree functions being called accept NULL, so don't check first. ok beck

--- lib/libcrypto/x509v3/v3_ncons.c
+++ lib/libcrypto/x509v3/v3_ncons.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: v3_ncons.c,v 1.10 2016/12/30 15:54:49 jsing Exp $ */
+/* $OpenBSD: v3_ncons.c,v 1.11 2017/01/29 17:49:23 beck Exp $ */
 /* Written by Dr Stephen N Henson (steve@openssl.org) for the OpenSSL
  * project.
  */
@@ -223,11 +223,8 @@ v2i_NAME_CONSTRAINTS(const X509V3_EXT_METHOD *method, X509V3_CTX *ctx,
 memerr:
 	X509V3error(ERR_R_MALLOC_FAILURE);
 err:
-	if (ncons)
-		NAME_CONSTRAINTS_free(ncons);
-	if (sub)
-		GENERAL_SUBTREE_free(sub);
-
+	NAME_CONSTRAINTS_free(ncons);
+	GENERAL_SUBTREE_free(sub);
 	return NULL;
 }
 
