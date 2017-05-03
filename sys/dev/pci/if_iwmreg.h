a448cff483c0410d17f4ec935f7acc50c3527bab
stsp <stsp>
2017-05-03 14:53:12 +0000

    Fix a regression in iwm(4) which made 3165 devices unusable.
    When MIMO support was added several entries in the rate table index enum
    got a wrong value. On most devices the firmware ended up using a Tx rate
    different from the rate net80211 asked for (which is bad, but not fatal).
    But on 3165 devices which do not support MIMO the firmware rightly raised
    a fatal error whenever the driver mistakenly asked for a MIMO Tx rate.
    
    Reported by Georgios Pediaditis, Steve Throckmorton, and Kai Wirt on misc@
    Thanks to benno@ for providing 3165 hardware I could use to debug this.

--- sys/dev/pci/if_iwmreg.h
+++ sys/dev/pci/if_iwmreg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_iwmreg.h,v 1.25 2017/04/04 00:40:52 claudio Exp $	*/
+/*	$OpenBSD: if_iwmreg.h,v 1.26 2017/04/24 09:31:31 stsp Exp $	*/
 
 /******************************************************************************
  *
@@ -3873,8 +3873,8 @@ enum {
 	IWM_RATE_MCS_4_INDEX = IWM_RATE_36M_INDEX,
 	IWM_RATE_MCS_10_INDEX,
 	IWM_RATE_48M_INDEX,
-	IWM_RATE_MCS_11_INDEX,
 	IWM_RATE_MCS_5_INDEX = IWM_RATE_48M_INDEX,
+	IWM_RATE_MCS_11_INDEX,
 	IWM_RATE_54M_INDEX,
 	IWM_RATE_MCS_6_INDEX = IWM_RATE_54M_INDEX,
 	IWM_LAST_NON_HT_RATE = IWM_RATE_54M_INDEX,
