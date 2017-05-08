6927ed20df24cda16ea40ca198a4e605be84d669
rzalamena <rzalamena>
2017-05-08 09:31:34 +0000

    Make netstat(8) always ask for IPv6 multicast routing information. Worst case is that we get an empty response instead of a message saying that there is no multicast support.
    ok mpi@

--- usr.bin/netstat/mroute6.c
+++ usr.bin/netstat/mroute6.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mroute6.c,v 1.21 2016/08/27 04:13:43 guenther Exp $	*/
+/*	$OpenBSD: mroute6.c,v 1.22 2016/12/22 11:04:44 rzalamena Exp $	*/
 
 /*
  * Copyright (C) 1998 WIDE Project.
@@ -105,8 +105,7 @@ mroute6pr(void)
 	}
 	switch (mrtproto) {
 	case 0:
-		printf("no IPv6 multicast routing compiled into this system\n");
-		return;
+		break;
 	default:
 		printf("IPv6 multicast routing protocol %u, unknown\n",
 		    mrtproto);
