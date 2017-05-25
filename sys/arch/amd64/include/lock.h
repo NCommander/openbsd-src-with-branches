57cfde1c0134163770b3d29bf535e243411b6905
visa <visa>
2017-05-25 03:50:10 +0000

    Drop trailing semicolon from a macro.
    OK dlg@

--- sys/arch/amd64/include/lock.h
+++ sys/arch/amd64/include/lock.h
@@ -1,10 +1,10 @@
-/*	$OpenBSD: lock.h,v 1.10 2015/02/11 00:14:11 dlg Exp $	*/
+/*	$OpenBSD: lock.h,v 1.11 2016/04/03 11:05:26 jsg Exp $	*/
 
 /* public domain */
 
 #ifndef _MACHINE_LOCK_H_
 #define	_MACHINE_LOCK_H_
 
-#define SPINLOCK_SPIN_HOOK __asm volatile("pause": : :"memory");
+#define SPINLOCK_SPIN_HOOK __asm volatile("pause": : :"memory")
 
 #endif /* _MACHINE_LOCK_H_ */
