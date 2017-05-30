6c2d8f061f8be2eec4e2646e562f6cea13be79b6
mpi <mpi>
2017-05-30 06:38:10 +0000

    Fix a race by always modifying the 'counter' variable while holding the mutex.
    ok tedu@

--- regress/lib/libpthread/socket/1/socket1.c
+++ regress/lib/libpthread/socket/1/socket1.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: socket1.c,v 1.4 2015/11/04 21:29:20 tedu Exp $	*/
+/*	$OpenBSD: socket1.c,v 1.5 2015/11/19 18:37:49 deraadt Exp $	*/
 /*
  * Copyright (c) 1993, 1994, 1995, 1996 by Chris Provenzano and contributors, 
  * proven@mit.edu All rights reserved.
@@ -78,10 +78,10 @@ sock_connect(void *arg)
 	CHECKe(connect(fd, (struct sockaddr *) &a_sout, sizeof(a_sout)));
 	CHECKe(close(fd));
 
-	CHECKr(pthread_mutex_unlock(&mutex));
-
 	CHECKe(fd = socket(AF_INET, SOCK_STREAM, 0));
 	ASSERT(++counter == 3);
+
+	CHECKr(pthread_mutex_unlock(&mutex));
 	CHECKe(connect(fd, (struct sockaddr *) &a_sout, sizeof(a_sout)));
 
 	/* Ensure sock_read runs again */
