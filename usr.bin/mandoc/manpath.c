58e7d9083acdfae5cde04e94d0c1f9d66bb9e9a9
schwarze <schwarze>
2017-06-13 15:05:22 +0000

    Delete the arbitrary range restriction for -Owidth. We provide users with tools.  We don't attempt to prevent them from using them in stupid ways: depending on the context, not every stupid-looking use is necessarily actually stupid, and not every stupidity can be automatically detected anyway, so don't even try.

--- usr.bin/mandoc/manpath.c
+++ usr.bin/mandoc/manpath.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: manpath.c,v 1.19 2017/01/27 13:47:17 schwarze Exp $	*/
+/*	$OpenBSD: manpath.c,v 1.20 2017/02/10 15:44:31 schwarze Exp $	*/
 /*
  * Copyright (c) 2011, 2014, 2015, 2017 Ingo Schwarze <schwarze@openbsd.org>
  * Copyright (c) 2011 Kristaps Dzonsons <kristaps@bsd.lv>
@@ -298,7 +298,7 @@ manconf_output(struct manoutput *conf, const char *cp, int fromfile)
 			mandoc_asprintf(&oldval, "%zu", conf->width);
 			break;
 		}
-		conf->width = strtonum(cp, 58, 1000, &errstr);
+		conf->width = strtonum(cp, 1, 1000, &errstr);
 		if (errstr == NULL)
 			return 0;
 		warnx("-O width=%s is %s", cp, errstr);
