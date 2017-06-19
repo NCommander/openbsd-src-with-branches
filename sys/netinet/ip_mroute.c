6f33e180473144fdf44f578fa35ea55b01272a6c
bluhm <bluhm>
2017-06-19 17:00:16 +0000

    The IP multicast forward functions return an errno, call the variable error.  Make the ip_mforward() return value consistent.  Simplify the caller logic in ipv6_input() like in IPv4. OK mpi@

--- sys/netinet/ip_mroute.c
+++ sys/netinet/ip_mroute.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_mroute.c,v 1.117 2017/05/16 13:05:07 rzalamena Exp $	*/
+/*	$OpenBSD: ip_mroute.c,v 1.118 2017/05/16 13:09:21 rzalamena Exp $	*/
 /*	$NetBSD: ip_mroute.c,v 1.85 2004/04/26 01:31:57 matt Exp $	*/
 
 /*
@@ -1084,8 +1084,7 @@ ip_mforward(struct mbuf *m, struct ifnet *ifp)
 		if ((srctun++ % 1000) == 0)
 			log(LOG_ERR, "ip_mforward: received source-routed "
 			    "packet from %x\n", ntohl(ip->ip_src.s_addr));
-
-		return (1);
+		return (EOPNOTSUPP);
 	}
 
 	/*
