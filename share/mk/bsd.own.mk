0ad310aa7ac7e7c3fbc23958eca377e117f1d5e2
mpi <mpi>
2017-06-19 08:35:24 +0000

    g/c ASPICFLAG.
    This used to be necessary a long time ago in the... gcc 2.95 days.
    
    from miod@

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.own.mk,v 1.183 2017/04/17 15:53:21 kettenis Exp $
+#	$OpenBSD: bsd.own.mk,v 1.184 2017/04/18 14:03:08 deraadt Exp $
 #	$NetBSD: bsd.own.mk,v 1.24 1996/04/13 02:08:09 thorpej Exp $
 
 # Host-specific overrides
@@ -127,10 +127,6 @@ STATIC?=	-static ${STATICPIE}
 PICFLAG?=-fPIC
 .else
 PICFLAG?=-fpic
-.endif
-
-.if ${MACHINE_ARCH} == "sparc64"
-ASPICFLAG=-KPIC
 .endif
 
 .if ${MACHINE_ARCH} == "alpha" || ${MACHINE_ARCH} == "powerpc" || \
