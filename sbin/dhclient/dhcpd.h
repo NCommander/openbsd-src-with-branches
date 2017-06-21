20df35d11c5bfac287495465859f3aea0e139049
krw <krw>
2017-06-21 15:24:34 +0000

    Consistently use time(&var) idiom rather than tossing in a few var = time(NULL). Flip "struct interface *" to be the first parameter in set_timeout(), as in other functions taking the state info.

--- sbin/dhclient/dhcpd.h
+++ sbin/dhclient/dhcpd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dhcpd.h,v 1.188 2017/06/18 17:01:46 krw Exp $	*/
+/*	$OpenBSD: dhcpd.h,v 1.189 2017/06/18 21:08:15 krw Exp $	*/
 
 /*
  * Copyright (c) 2004 Henning Brauer <henning@openbsd.org>
@@ -210,8 +210,8 @@ ssize_t receive_packet(struct interface_info *, struct sockaddr_in *,
 
 /* dispatch.c */
 void dispatch(struct interface_info *);
-void set_timeout(time_t, void (*)(struct interface_info *),
-    struct interface_info *);
+void set_timeout( struct interface_info *, time_t,
+    void (*)(struct interface_info *));
 void cancel_timeout(struct interface_info *);
 void interface_link_forceup(char *);
 int interface_status(struct interface_info *);
