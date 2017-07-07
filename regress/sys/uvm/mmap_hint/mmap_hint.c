ee01edcdb0ca19b8d2a2b8973e1d2cd33244278a
bluhm <bluhm>
2017-07-07 10:49:12 +0000

    Make mmap_hint.c compile on i386 by adding includes.  Unfortunately test is still failing.

--- regress/sys/uvm/mmap_hint/mmap_hint.c
+++ regress/sys/uvm/mmap_hint/mmap_hint.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mmap_hint.c,v 1.1 2011/10/07 20:16:55 ariane Exp $	*/
+/*	$OpenBSD: mmap_hint.c,v 1.2 2013/03/21 21:59:55 deraadt Exp $	*/
 /*
  * Copyright (c) 2011 Ariane van der Steldt <ariane@stack.nl>
  *
@@ -18,6 +18,8 @@
 #include <sys/param.h>
 #include <sys/types.h>
 #include <sys/mman.h>
+#include <sys/tree.h>
+#include <machine/pmap.h>
 #include <machine/vmparam.h>
 #include <err.h>
 #include <sysexits.h>
