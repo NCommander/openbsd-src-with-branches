69059d916b1ba9f60b5139f997bdf1e37bd4f17c
kettenis <kettenis>
2017-07-19 22:23:54 +0000

    Remove SIGIO support.  The audio(4) driver doesn't implement it either and the preferred method to access MIDI hardware is through sndiod(8) which doesn't use it.
    Fixes a potential issue with a stale struct proc pointer found by Ilja van
    Sprundel.
    
    ok deraadt@, ratchov@

--- sys/dev/midivar.h
+++ sys/dev/midivar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: midivar.h,v 1.9 2015/05/16 09:56:10 ratchov Exp $	*/
 
 /*
  * Copyright (c) 2003, 2004 Alexandre Ratchov
@@ -77,7 +77,6 @@ struct midi_softc {
 	int		    wchan;
 	struct selinfo	    rsel;
 	struct selinfo	    wsel;
-	struct proc 	   *async;
 	struct timeout	    timeo;
 	struct midi_buffer  inbuf;
 	struct midi_buffer  outbuf;
