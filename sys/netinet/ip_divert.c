c22ec2ecc9b96de1097a8b4a34c8c99bee93bbe7
mpi <mpi>
2017-07-27 12:04:42 +0000

    Grab the KERNEL_LOCK() before calling sorwakeup().
    In the forwarding path, pf_test() is executed w/o KERNEL_LOCK() and
    in case of divert end up calling sowakup().  However selwakup() and
    csignal() are not yet ready to be executed w/o KERNEL_LOCK().
    
    ok bluhm@

--- sys/netinet/ip_divert.c
+++ sys/netinet/ip_divert.c
@@ -1,4 +1,4 @@
-/*      $OpenBSD: ip_divert.c,v 1.47 2017/05/30 07:50:37 mpi Exp $ */
+/*      $OpenBSD: ip_divert.c,v 1.48 2017/06/26 09:32:32 mpi Exp $ */
 
 /*
  * Copyright (c) 2009 Michele Marchetto <michele@openbsd.org>
@@ -226,8 +226,11 @@ divert_packet(struct mbuf *m, int dir, u_int16_t divert_port)
 			divstat_inc(divs_fullsock);
 			m_freem(m);
 			return (0);
-		} else
+		} else {
+			KERNEL_LOCK();
 			sorwakeup(inp->inp_socket);
+			KERNEL_UNLOCK();
+		}
 	}
 
 	if (sa == NULL) {
