d9b9a0052f8137f66064af5e3a01eddb2b6aabac
visa <visa>
2017-07-30 16:09:53 +0000

    Bump VM_PHYSSEG_MAX.

--- sys/arch/octeon/include/vmparam.h
+++ sys/arch/octeon/include/vmparam.h
@@ -1,9 +1,9 @@
-/*	$OpenBSD: vmparam.h,v 1.3 2011/03/23 16:54:35 pirofti Exp $ */
+/*	$OpenBSD: vmparam.h,v 1.2 2014/08/12 19:35:28 miod Exp $ */
 /* public domain */
 #ifndef _MACHINE_VMPARAM_H_
 #define _MACHINE_VMPARAM_H_
 
-#define	VM_PHYSSEG_MAX		4 /* Max number of physical memory segments */
+#define	VM_PHYSSEG_MAX		8 /* Max number of physical memory segments */
 #define	VM_PHYSSEG_STRAT	VM_PSTRAT_BIGFIRST
 
 #include <mips64/vmparam.h>
