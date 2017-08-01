2d2669dfec0a5b228d4fe58249bad2c867aa7f15
deraadt <deraadt>
2017-08-01 13:05:55 +0000

    A fix is coming in the future, but in the meantime we should never declare a section(".openbsd.randomdata") variable as static or const, because the compiler optimizes out access.

--- regress/libexec/ld.so/randomdata/prog/prog.c
+++ regress/libexec/ld.so/randomdata/prog/prog.c
@@ -1,10 +1,10 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: prog.c,v 1.2 2015/01/20 04:41:01 krw Exp $	*/
 #include <sys/types.h>
 #include <assert.h>
 
 extern int64_t getaavalue(void);
 
-static int64_t progvalue __attribute__((section(".openbsd.randomdata")));
+int64_t progvalue __attribute__((section(".openbsd.randomdata")));
 
 int
 main()
