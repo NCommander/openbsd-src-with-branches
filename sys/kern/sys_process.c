1569eb67c13e8237f8c4e6383fb479a6d9323b3c
deraadt <deraadt>
2017-08-02 16:52:09 +0000

    Uninitialized variable can leak kernel memory. Found by Ilja Van Sprundel errata 35

--- sys/kern/sys_process.c
+++ sys/kern/sys_process.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sys_process.c,v 1.68 2015/09/24 20:35:18 tedu Exp $	*/
+/*	$OpenBSD: sys_process.c,v 1.69 2016/05/31 22:34:53 jca Exp $	*/
 /*	$NetBSD: sys_process.c,v 1.55 1996/05/15 06:17:47 tls Exp $	*/
 
 /*-
@@ -101,7 +101,7 @@ sys_ptrace(struct proc *p, void *v, register_t *retval)
 	register_t wcookie;
 #endif
 	int error, write;
-	int temp;
+	int temp = 0;
 	int req = SCARG(uap, req);
 	int s;
 
