5bff6e830fc62a3e7183f9b30a4abf676585ac26
bluhm <bluhm>
2017-08-08 14:23:23 +0000

    Kernel sendsyslog(2), libc syslog(3), and syslogd(8) restrict and truncate the length of a syslog message to 8192 bytes.  Use one global define LOG_MAXLINE for all of them. OK deraadt@ millert@

--- sys/kern/subr_log.c
+++ sys/kern/subr_log.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: subr_log.c,v 1.50 2017/03/27 11:45:49 bluhm Exp $	*/
+/*	$OpenBSD: subr_log.c,v 1.51 2017/07/18 22:22:19 bluhm Exp $	*/
 /*	$NetBSD: subr_log.c,v 1.11 1996/03/30 22:24:44 christos Exp $	*/
 
 /*
@@ -418,8 +418,8 @@ dosendsyslog(struct proc *p, const char *buf, size_t nbyte, int flags,
 	size_t i, len;
 	int error;
 
-	if (nbyte > 8192)
-		nbyte = 8192;
+	if (nbyte > LOG_MAXLINE)
+		nbyte = LOG_MAXLINE;
 
 	/* Global variable syslogf may change during sleep, use local copy. */
 	fp = syslogf;
