048bd2048dc3c54644676af1597541c9190ea104
nicm <nicm>
2017-08-09 13:44:36 +0000

    Fix filtering so it works after the change to only show windows if they have multiple panes.

--- usr.bin/tmux/options.c
+++ usr.bin/tmux/options.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: options.c,v 1.34 2017/04/22 06:13:30 nicm Exp $ */
+/* $OpenBSD: options.c,v 1.35 2017/05/31 17:56:48 nicm Exp $ */
 
 /*
  * Copyright (c) 2008 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -477,7 +477,6 @@ options_match_get(struct options *oo, const char *s, int *idx, int only,
 	free(name);
 	return (o);
 }
-
 
 const char *
 options_get_string(struct options *oo, const char *name)
