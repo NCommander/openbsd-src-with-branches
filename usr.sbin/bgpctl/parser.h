2f6ce3dd77b7de44cc7f92887ae2bd61f15c826e
benno <benno>
2017-08-10 14:12:34 +0000

    handle extended communities in bgpctl. From Dennis Fondras, thanks! ok phessler@

--- usr.sbin/bgpctl/parser.h
+++ usr.sbin/bgpctl/parser.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: parser.h,v 1.28 2016/10/14 16:05:35 phessler Exp $ */
+/*	$OpenBSD: parser.h,v 1.29 2017/01/13 18:59:12 phessler Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -63,6 +63,7 @@ struct parse_result {
 	struct filter_as	 as;
 	struct filter_set_head	 set;
 	struct filter_community  community;
+	struct filter_extcommunity extcommunity;
 	struct filter_largecommunity  large_community;
 	char			 peerdesc[PEER_DESCR_LEN];
 	char			 rib[PEER_DESCR_LEN];
