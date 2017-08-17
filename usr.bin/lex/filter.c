2ed4d6a7875ff6ccf3477c2026656d574b31cf30
tedu <tedu>
2017-08-17 19:27:48 +0000

    combine malloc/memset into calloc. from Michael W. Bombardieri

--- usr.bin/lex/filter.c
+++ usr.bin/lex/filter.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: filter.c,v 1.6 2015/11/19 23:28:03 tedu Exp $ */
+/* $OpenBSD: filter.c,v 1.7 2016/12/18 06:11:23 krw Exp $ */
 
 /* filter - postprocessing of flex output through filters */
 
@@ -50,10 +50,9 @@ filter_create_ext(struct filter * chain, const char *cmd,
 	va_list ap;
 
 	/* allocate and initialize new filter */
-	f = malloc(sizeof(struct filter));
+	f = calloc(sizeof(struct filter), 1);
 	if (!f)
-		flexerror(_("malloc failed (f) in filter_create_ext"));
-	memset(f, 0, sizeof(*f));
+		flexerror(_("calloc failed (f) in filter_create_ext"));
 	f->filter_func = NULL;
 	f->extra = NULL;
 	f->next = NULL;
@@ -103,10 +102,9 @@ filter_create_int(struct filter * chain,
 	struct filter *f;
 
 	/* allocate and initialize new filter */
-	f = malloc(sizeof(struct filter));
+	f = calloc(sizeof(struct filter), 1);
 	if (!f)
-		flexerror(_("malloc failed in filter_create_int"));
-	memset(f, 0, sizeof(*f));
+		flexerror(_("calloc failed in filter_create_int"));
 	f->next = NULL;
 	f->argc = 0;
 	f->argv = NULL;
