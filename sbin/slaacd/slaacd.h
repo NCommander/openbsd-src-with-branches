1c3438d62b17c3f96978f01d3b9ce712e2532279
florian <florian>
2017-08-18 07:45:03 +0000

    Timeouts should be time_t to prevent overflows.
    Bug report and fix tested by csszep AT gmail, thanks!
    This was triggered by virtualbox which sets vltime and pltime to
    infinity (0xffffffff).

--- sbin/slaacd/slaacd.h
+++ sbin/slaacd/slaacd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: slaacd.h,v 1.5 2017/07/06 15:02:53 florian Exp $	*/
+/*	$OpenBSD: slaacd.h,v 1.6 2017/07/30 07:41:08 florian Exp $	*/
 
 /*
  * Copyright (c) 2017 Florian Obser <florian@openbsd.org>
@@ -128,7 +128,7 @@ struct ctl_engine_info_ra_dnssl {
 struct ctl_engine_info_address_proposal {
 	int64_t			 id;
 	char			 state[sizeof("PROPOSAL_NEARLY_EXPIRED")];
-	int			 next_timeout;
+	time_t			 next_timeout;
 	int			 timeout_count;
 	struct timespec		 when;
 	struct timespec		 uptime;
@@ -143,7 +143,7 @@ struct ctl_engine_info_address_proposal {
 struct ctl_engine_info_dfr_proposal {
 	int64_t			 id;
 	char			 state[sizeof("PROPOSAL_NEARLY_EXPIRED")];
-	int			 next_timeout;
+	time_t			 next_timeout;
 	int			 timeout_count;
 	struct timespec		 when;
 	struct timespec		 uptime;
