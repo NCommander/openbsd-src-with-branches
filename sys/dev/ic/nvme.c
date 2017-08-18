009fe513027b179887f731df46e2e747312b6faf
jsg <jsg>
2017-08-18 09:26:43 +0000

    Avoid a NULL dereference in an error path.  Coverity CID 1453201.
    ok mikeb@

--- sys/dev/ic/nvme.c
+++ sys/dev/ic/nvme.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: nvme.c,v 1.58 2017/05/29 12:58:37 jmatthew Exp $ */
+/*	$OpenBSD: nvme.c,v 1.59 2017/06/12 04:57:42 deraadt Exp $ */
 
 /*
  * Copyright (c) 2014 David Gwynne <dlg@openbsd.org>
@@ -357,7 +357,7 @@ nvme_attach(struct nvme_softc *sc)
 	sc->sc_hib_q = nvme_q_alloc(sc, NVME_HIB_Q, 4, sc->sc_dstrd);
 	if (sc->sc_hib_q == NULL) {
 		printf("%s: unable to allocate hibernate io queue\n", DEVNAME(sc));
-		goto free_hib_q;
+		goto free_q;
 	}
 
 	nvme_write4(sc, NVME_INTMC, 1);
@@ -381,8 +381,6 @@ nvme_attach(struct nvme_softc *sc)
 
 	return (0);
 
-free_hib_q:
-	nvme_q_free(sc, sc->sc_hib_q);
 free_q:
 	nvme_q_free(sc, sc->sc_q);
 disable:
