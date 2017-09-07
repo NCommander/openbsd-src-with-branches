1ddd01bc943dc110ab6b5e5018848468d2a26234
bluhm <bluhm>
2017-09-07 11:27:17 +0000

    In copycols() remove useless code that modifies the uninitialized sp variable.  Its value is not used, it is overwritten later on. found by clang -Wuninitialized; OK deraadt@

--- sys/dev/rasops/rasops_bitops.h
+++ sys/dev/rasops/rasops_bitops.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rasops_bitops.h,v 1.5 2009/09/05 14:09:35 miod Exp $ */
+/*	$OpenBSD: rasops_bitops.h,v 1.6 2010/08/28 12:48:14 miod Exp $ */
 /* 	$NetBSD: rasops_bitops.h,v 1.6 2000/04/12 14:22:30 pk Exp $	*/
 
 /*-
@@ -237,10 +237,8 @@ NAME(copycols)(void *cookie, int row, int src, int dst, int num)
 		rnum = 32 - lnum;
 		db = dst & 31;
 
-		if ((src -= db) < 0) {
-			sp--;
+		if ((src -= db) < 0)
 			src += 32;
-		}
 
 		while (height--) {
 			sp = srp;
