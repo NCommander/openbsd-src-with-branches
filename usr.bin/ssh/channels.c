da1b4acead2e6fcd067625e2b20be5de2ce5f60e
djm <djm>
2017-09-24 09:50:01 +0000

    write the correct buffer when tunnel forwarding; doesn't matter on OpenBSD (they are the same) but does matter on portable where we use an output filter to translate os-specific tun/tap headers

--- usr.bin/ssh/channels.c
+++ usr.bin/ssh/channels.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: channels.c,v 1.372 2017/09/21 19:16:53 markus Exp $ */
+/* $OpenBSD: channels.c,v 1.373 2017/09/23 22:04:07 djm Exp $ */
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
@@ -1922,7 +1922,7 @@ channel_handle_wfd(struct ssh *ssh, Channel *c,
 
 	if (c->datagram) {
 		/* ignore truncated writes, datagrams might get lost */
-		len = write(c->wfd, data, dlen);
+		len = write(c->wfd, buf, dlen);
 		free(data);
 		if (len < 0 && (errno == EINTR || errno == EAGAIN))
 			return 1;
