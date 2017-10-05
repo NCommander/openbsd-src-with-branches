1bbbba84c8598c8c0ff4f564a4b532461e7ddac1
bluhm <bluhm>
2017-10-05 16:15:24 +0000

    I have touched more than half the source code lines of syslogd(8). Add my copyright explicitly.

--- usr.sbin/syslogd/syslogd.h
+++ usr.sbin/syslogd/syslogd.h
@@ -1,6 +1,7 @@
-/*	$OpenBSD: syslogd.h,v 1.30 2017/04/05 22:15:35 bluhm Exp $ */
+/*	$OpenBSD: syslogd.h,v 1.31 2017/08/08 14:23:23 bluhm Exp $ */
 
 /*
+ * Copyright (c) 2014-2017 Alexander Bluhm <bluhm@genua.de>
  * Copyright (c) 2003 Anil Madhavapeddy <anil@recoil.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
