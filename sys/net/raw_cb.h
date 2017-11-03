dd89cd294d3ab94cdf4d491cd202afa8d7747f2b
florian <florian>
2017-11-03 12:49:42 +0000

    There is no way SS_NOFDREF is set on a raw socket in raw_usrreq for PRU_DISCONNECT or PRU_ABORT.
    So raw_disconnect() and sofree() return immediately so remove the dead
    code.
    Also the following call to soisdisconnected() would be a
    use after free.
    
    This removes the last calls to raw_disconnect() so tedu it.

--- sys/net/raw_cb.h
+++ sys/net/raw_cb.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: raw_cb.h,v 1.12 2017/07/03 19:23:47 claudio Exp $	*/
+/*	$OpenBSD: raw_cb.h,v 1.13 2017/11/02 14:01:18 florian Exp $	*/
 /*	$NetBSD: raw_cb.h,v 1.9 1996/02/13 22:00:41 christos Exp $	*/
 
 /*
@@ -58,7 +58,6 @@ struct rawcb {
 int	 raw_attach(struct socket *, int);
 int	 raw_detach(struct socket *);
 void	 raw_do_detach(struct rawcb *);
-void	 raw_disconnect(struct rawcb *);
 void	 raw_init(void);
 int	 raw_usrreq(struct socket *,
 	    int, struct mbuf *, struct mbuf *, struct mbuf *, struct proc *);
