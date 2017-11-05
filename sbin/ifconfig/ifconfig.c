127308d9d63dbd68f25da741545b0a8e71528b63
benno <benno>
2017-11-05 22:09:26 +0000

    ifconfig <if> giftunnel was deprecated in 2001, it's never too late to remove it ok phessler@ beck@

--- sbin/ifconfig/ifconfig.c
+++ sbin/ifconfig/ifconfig.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ifconfig.c,v 1.348 2017/08/29 21:10:20 deraadt Exp $	*/
+/*	$OpenBSD: ifconfig.c,v 1.349 2017/10/30 10:04:07 mpi Exp $	*/
 /*	$NetBSD: ifconfig.c,v 1.40 1997/10/01 02:19:43 enami Exp $	*/
 
 /*
@@ -424,8 +424,6 @@ const struct	cmd {
 	{ "maxupd",	NEXTARG,	0,		setpfsync_maxupd },
 	{ "defer",	1,		0,		setpfsync_defer },
 	{ "-defer",	0,		0,		setpfsync_defer },
-	/* giftunnel is for backward compat */
-	{ "giftunnel",  NEXTARG2,	0,		NULL, settunnel } ,
 	{ "tunnel",	NEXTARG2,	0,		NULL, settunnel } ,
 	{ "deletetunnel",  0,		0,		deletetunnel } ,
 	{ "tunneldomain", NEXTARG,	0,		settunnelinst } ,
