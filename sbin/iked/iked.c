107a13ac6231e83591b38749688a4db1906f2b0d
patrick <patrick>
2017-11-08 16:57:41 +0000

    Do not accept superfluous arguments.
    From Klemens Nanni.
    
    ok markus@

--- sbin/iked/iked.c
+++ sbin/iked/iked.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: iked.c,v 1.33 2017/01/09 14:49:21 reyk Exp $	*/
+/*	$OpenBSD: iked.c,v 1.34 2017/03/23 05:29:48 jsg Exp $	*/
 
 /*
  * Copyright (c) 2010-2013 Reyk Floeter <reyk@openbsd.org>
@@ -109,6 +109,11 @@ main(int argc, char *argv[])
 			usage();
 		}
 	}
+
+	argc -= optind;
+	argv += optind;
+	if (argc > 0)
+		usage();
 
 	if ((env = calloc(1, sizeof(*env))) == NULL)
 		fatal("calloc: env");
