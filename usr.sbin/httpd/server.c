25651cb1aa5c2455b2c127881aca2495b743e944
beck <beck>
2017-11-29 16:55:08 +0000

    Don't do OCSP stapling only if the staple file is 0 length.
    This allows something external (like ocspcheck) to disable the stapling
    deliberatly if it can not retreive a valid staple by truncating the
    staple file to indicate "do not provide a staple", while the file not
    existin will still be treated as a configuration error
    ok claudio@ florian@, and prompted by @jsing

--- usr.sbin/httpd/server.c
+++ usr.sbin/httpd/server.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: server.c,v 1.111 2017/08/11 18:48:56 jsing Exp $	*/
+/*	$OpenBSD: server.c,v 1.112 2017/11/28 01:21:30 beck Exp $	*/
 
 /*
  * Copyright (c) 2006 - 2015 Reyk Floeter <reyk@openbsd.org>
@@ -189,9 +189,9 @@ server_tls_load_ocsp(struct server *srv)
 	if ((srv->srv_conf.tls_ocsp_staple = tls_load_file(
 	    srv->srv_conf.tls_ocsp_staple_file,
 	    &srv->srv_conf.tls_ocsp_staple_len, NULL)) == NULL) {
-		log_warnx("%s: Failed to load ocsp staple from %s - ignoring", __func__,
+		log_warnx("%s: Failed to load ocsp staple from %s", __func__,
 		    srv->srv_conf.tls_ocsp_staple_file);
-		return (0);
+		return (-1);
 	}
 
 	if (srv->srv_conf.tls_ocsp_staple_len == 0) {
