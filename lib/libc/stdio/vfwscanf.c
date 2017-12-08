608b21aa40b5bed57497ba800c3b61621533fad6
kevlo <kevlo>
2017-12-08 01:03:51 +0000

    Fix the return value of fwscanf(3) when encountering an early matching failure.  This change brings fwscanf(3) back in line with fscanf(3).
    From FreeBSD; ok deraadt@, millert@

--- lib/libc/stdio/vfwscanf.c
+++ lib/libc/stdio/vfwscanf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vfwscanf.c,v 1.5 2015/08/31 02:53:57 guenther Exp $ */
+/*	$OpenBSD: vfwscanf.c,v 1.6 2016/10/30 05:07:06 jsg Exp $ */
 /*-
  * Copyright (c) 1990, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -165,7 +165,7 @@ literal:
 				goto input_failure;
 			if (wi != c) {
 				__ungetwc(wi, fp);
-				goto input_failure;
+				goto match_failure;
 			}
 			nread++;
 			continue;
