c0c1d5cbe5b169b16e7571bf0d420ebdb178f2d4
jsing <jsing>
2017-12-09 16:49:17 +0000

    MFC: Make tls_config_parse_protocols() work correctly when passed a NULL pointer for a protocol string.
    Issue found by semarie@, who also provided the diff.

--- lib/libtls/tls_config.c
+++ lib/libtls/tls_config.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: tls_config.c,v 1.43 2017/08/10 18:18:30 jsing Exp $ */
+/* $OpenBSD: tls_config.c,v 1.44 2017/09/25 18:07:03 jsing Exp $ */
 /*
  * Copyright (c) 2014 Joel Sing <jsing@openbsd.org>
  *
@@ -311,8 +311,10 @@ tls_config_parse_protocols(uint32_t *protocols, const char *protostr)
 	char *s, *p, *q;
 	int negate;
 
-	if (protostr == NULL)
-		return TLS_PROTOCOLS_DEFAULT;
+	if (protostr == NULL) {
+		*protocols = TLS_PROTOCOLS_DEFAULT;
+		return (0);
+	}
 
 	if ((s = strdup(protostr)) == NULL)
 		return (-1);
