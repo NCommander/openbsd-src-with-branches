9447078e8627074a210e15e51e59f6c9f47c58fe
kettenis <kettenis>
2017-12-30 13:34:56 +0000

    The generic clock framework expects frequencies in Hz whereas the imx clock code was providing them in kHz.  Get rid of this discrepancy. Hook up the CPU clock to the generic clock framework and remove the cpuspeed function as it is no longer needed.
    ok patrick@

--- sys/arch/armv7/imx/imxesdhc.c
+++ sys/arch/armv7/imx/imxesdhc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: imxesdhc.c,v 1.36 2017/01/21 05:42:03 guenther Exp $	*/
+/*	$OpenBSD: imxesdhc.c,v 1.37 2017/07/18 18:45:44 patrick Exp $	*/
 /*
  * Copyright (c) 2009 Dale Rahn <drahn@openbsd.org>
  * Copyright (c) 2006 Uwe Stuehler <uwe@openbsd.org>
@@ -338,7 +338,7 @@ imxesdhc_attach(struct device *parent, struct device *self, void *aux)
 	/*
 	 * Determine the base clock frequency. (2.2.24)
 	 */
-	sc->clkbase = clock_get_frequency(faa->fa_node, "per");
+	sc->clkbase = clock_get_frequency(faa->fa_node, "per") / 1000;
 
 	printf("%s: %d MHz base clock\n", DEVNAME(sc), sc->clkbase / 1000);
 
