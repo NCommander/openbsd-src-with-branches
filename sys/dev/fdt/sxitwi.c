e234de5a8b3f6966e99693bf5260aae032277b8d
kettenis <kettenis>
2017-12-30 19:04:00 +0000

    Somewhow I botched rev 1.3, which has a = where a |= is needed.  Fix this.

--- sys/dev/fdt/sxitwi.c
+++ sys/dev/fdt/sxitwi.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: sxitwi.c,v 1.3 2017/11/13 21:24:27 kettenis Exp $ */
+/* $OpenBSD: sxitwi.c,v 1.4 2017/12/28 17:50:57 tom Exp $ */
 /*	$NetBSD: gttwsi_core.c,v 1.2 2014/11/23 13:37:27 jmcneill Exp $	*/
 /*
  * Copyright (c) 2008 Eiji Kawauchi.
@@ -209,7 +209,7 @@ sxitwi_attach(struct device *parent, struct device *self, void *aux)
 	 */
 	sc->sc_twsien_iflg = CONTROL_TWSIEN;
 	if (OF_is_compatible(sc->sc_node, "allwinner,sun6i-a31-i2c"))
-		sc->sc_twsien_iflg = CONTROL_IFLG;
+		sc->sc_twsien_iflg |= CONTROL_IFLG;
 
 	sc->sc_started = 0;
 	sc->sc_ic.ic_cookie = sc;
