03c5789d107676f98f1afcb2484d0e05935adc93
robert <robert>
2018-01-13 12:58:40 +0000

    introduce a filter called EVFILT_DEVICE that can be used to notify listeners of device state changes. currently only supports NOTE_CHANGE that will be used by drm(4)
    ok kettenis@

--- sys/kern/kern_event.c
+++ sys/kern/kern_event.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_event.c,v 1.81 2017/10/11 08:06:56 mpi Exp $	*/
+/*	$OpenBSD: kern_event.c,v 1.83 2017/12/18 10:10:53 mpi Exp $	*/
 
 /*-
  * Copyright (c) 1999,2000,2001 Jonathan Lemon <jlemon@FreeBSD.org>
@@ -137,6 +137,7 @@ struct filterops *sysfilt_ops[] = {
 	&proc_filtops,			/* EVFILT_PROC */
 	&sig_filtops,			/* EVFILT_SIGNAL */
 	&timer_filtops,			/* EVFILT_TIMER */
+	&file_filtops,			/* EVFILT_DEVICE */
 };
 
 void KQREF(struct kqueue *);
