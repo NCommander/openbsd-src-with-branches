61446ddc9dee5939d8d3278c5ca4455f817f7604
kettenis <kettenis>
2018-01-17 10:22:25 +0000

    Defend agains branch predictor target injection (Spectre "variant 2") attacks by flushing the branch predictor cache (BTB) on context switches and page faults in kkernel address space.  Note that this relies on the presence of firmware (such as Arm Trusted Firmware) that provides PSCI services that flush the BTB on entry as described in Arm Trusted Firmware Security Advisory TFV 6.
    ok patrick@, visa@

--- sys/arch/arm64/include/cpu.h
+++ sys/arch/arm64/include/cpu.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: cpu.h,v 1.3 2018/01/05 17:42:35 kettenis Exp $ */
+/* $OpenBSD: cpu.h,v 1.4 2018/01/12 22:20:28 kettenis Exp $ */
 /*
  * Copyright (c) 2016 Dale Rahn <drahn@dalerahn.com>
  *
@@ -99,6 +99,8 @@ struct cpu_info {
 	int			ci_mutex_level;
 #endif
 	int			ci_want_resched;
+
+	void			(*ci_flush_bp)(void);
 
 #ifdef MULTIPROCESSOR
 	struct srp_hazard	ci_srp_hazards[SRP_HAZARD_NUM];
