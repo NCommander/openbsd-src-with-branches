2880e7cca6eb7e727fbd7310b459a182e33a48f5
jcs <jcs>
2018-01-19 18:20:38 +0000

    dwiic: cleanup function prototypes after ACPI/PCI split

--- sys/dev/ic/dwiicvar.h
+++ sys/dev/ic/dwiicvar.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: dwiic.c,v 1.22 2016/10/25 06:48:58 pirofti Exp $ */
+/* $OpenBSD: dwiicvar.h,v 1.1 2017/11/16 18:12:27 jcs Exp $ */
 /*
  * Synopsys DesignWare I2C controller
  *
@@ -98,6 +98,5 @@ uint32_t	dwiic_read(struct dwiic_softc *, int);
 void		dwiic_write(struct dwiic_softc *, int, uint32_t);
 int		dwiic_i2c_exec(void *, i2c_op_t, i2c_addr_t, const void *,
 		    size_t, void *, size_t, int);
-void		dwiic_xfer_msg(struct dwiic_softc *);
 
 int		dwiic_acpi_found_hid(struct aml_node *node, void *arg);
