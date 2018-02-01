af7f01d4cffd5dc76a8b7fef4021289bd506aaf1
bluhm <bluhm>
2018-02-01 22:11:48 +0000

    Done means to stop processing the protocol loop, but IP proto none is protocol 58.  Fix typo in ip6_etherip_input() to properly discard EtherIP tunnel IPv6 packets if the feature is disabled. found by Maxime Villard; with markus@; OK patrick@ deraadt@ millert@ jca@ OpenBSD 6.1 errata 036

--- sys/net/if_etherip.c
+++ sys/net/if_etherip.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_etherip.c,v 1.15 2017/03/07 23:35:06 jca Exp $	*/
+/*	$OpenBSD: if_etherip.c,v 1.16 2017/03/27 23:49:03 jca Exp $	*/
 /*
  * Copyright (c) 2015 Kazuya GODA <goda@openbsd.org>
  *
@@ -582,7 +582,7 @@ ip6_etherip_input(struct mbuf **mp, int *offp, int proto)
 	if (!etherip_allow && (m->m_flags & (M_AUTH|M_CONF)) == 0) {
 		m_freem(m);
 		etheripstat.etherips_pdrops++;
-		return IPPROTO_NONE;
+		return IPPROTO_DONE;
 	}
 
 	ip6 = mtod(m, const struct ip6_hdr *);
