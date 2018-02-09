9e25a84eeea644509a3431d4d5c8a1673dd1a73e
dtucker <dtucker>
2018-02-09 03:42:57 +0000

    Skip sftp-chroot test when SUDO not set instead of fatal().

--- regress/usr.bin/ssh/sftp-chroot.sh
+++ regress/usr.bin/ssh/sftp-chroot.sh
@@ -1,4 +1,4 @@
-#	$OpenBSD: sftp-chroot.sh,v 1.4 2014/01/20 00:00:30 dtucker Exp $
+#	$OpenBSD: sftp-chroot.sh,v 1.5 2016/09/26 21:34:38 bluhm Exp $
 #	Placed in the Public Domain.
 
 tid="sftp in chroot"
@@ -8,7 +8,9 @@ FILENAME=testdata_${USER}
 PRIVDATA=${CHROOT}/${FILENAME}
 
 if [ -z "$SUDO" -a ! -w /var/run ]; then
-	fatal "need SUDO to create file in /var/run, test won't work without"
+	echo "need SUDO to create file in /var/run, test won't work without"
+	echo SKIPPED
+	exit 0
 fi
 
 $SUDO sh -c "echo mekmitastdigoat > $PRIVDATA" || \
