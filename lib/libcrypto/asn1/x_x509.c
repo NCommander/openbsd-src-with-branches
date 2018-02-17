d64c8567e86d49e0328bed4f869faa8e9115fd13
jsing <jsing>
2018-02-17 15:50:42 +0000

    Provide X509_get0_extensions() and X509_get0_signature()

--- lib/libcrypto/asn1/x_x509.c
+++ lib/libcrypto/asn1/x_x509.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: x_x509.c,v 1.24 2015/03/19 14:00:22 tedu Exp $ */
+/* $OpenBSD: x_x509.c,v 1.25 2018/02/14 16:18:10 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -343,6 +343,16 @@ i2d_X509_AUX(X509 *a, unsigned char **pp)
 	if (a)
 		length += i2d_X509_CERT_AUX(a->aux, pp);
 	return length;
+}
+
+void
+X509_get0_signature(const ASN1_BIT_STRING **psig, const X509_ALGOR **palg,
+    const X509 *x)
+{
+	if (psig != NULL)
+		*psig = x->signature;
+	if (palg != NULL)
+		*palg = x->sig_alg;
 }
 
 int
