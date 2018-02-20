74e5d9790989296fcf88ddd5b32b92f5b8bb3a2c
tb <tb>
2018-02-20 17:55:26 +0000

    Provide BIO_{g,s}et_shutdown().
    ok jsing

--- lib/libcrypto/bio/bio_lib.c
+++ lib/libcrypto/bio/bio_lib.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: bio_lib.c,v 1.24 2018/02/18 12:58:25 tb Exp $ */
+/* $OpenBSD: bio_lib.c,v 1.25 2018/02/20 17:15:27 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -167,6 +167,18 @@ void
 BIO_set_init(BIO *a, int init)
 {
 	a->init = init;
+}
+
+int
+BIO_get_shutdown(BIO *a)
+{
+	return (a->shutdown);
+}
+
+void
+BIO_set_shutdown(BIO *a, int shut)
+{
+	a->shutdown = shut;
 }
 
 void
