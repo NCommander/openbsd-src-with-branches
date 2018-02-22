b6993acb7e3939cdf91b9db14119f4556b5d6edd
jsing <jsing>
2018-02-22 16:58:45 +0000

    Provide X509_CRL_get0_extensions() and X509_CRL_get_signature_nid().

--- lib/libcrypto/asn1/x_crl.c
+++ lib/libcrypto/asn1/x_crl.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: x_crl.c,v 1.27 2017/01/29 17:49:22 beck Exp $ */
+/* $OpenBSD: x_crl.c,v 1.28 2018/02/20 17:04:58 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -684,6 +684,18 @@ void *
 X509_CRL_get_meth_data(X509_CRL *crl)
 {
 	return crl->meth_data;
+}
+
+int
+X509_CRL_get_signature_nid(const X509_CRL *crl)
+{
+	return OBJ_obj2nid(crl->sig_alg->algorithm);
+}
+
+const STACK_OF(X509_EXTENSION) *
+X509_CRL_get0_extensions(const X509_CRL *crl)
+{
+	return crl->crl->extensions;
 }
 
 const ASN1_TIME *
