9196f47ebe9f1e94d2d2d9c26fad8c75d6add0de
jsg <jsg>
2018-03-02 03:11:23 +0000

    The UEFI specification states that a watchdog timer should be armed for five minutes.  Disable the timer to make it possible to use efiboot for more than five minutes.
    ok patrick@ kettenis@

--- sys/arch/armv7/stand/efiboot/efiboot.c
+++ sys/arch/armv7/stand/efiboot/efiboot.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: efiboot.c,v 1.18 2017/08/07 19:34:53 kettenis Exp $	*/
+/*	$OpenBSD: efiboot.c,v 1.19 2017/08/21 20:05:32 kettenis Exp $	*/
 
 /*
  * Copyright (c) 2015 YASUOKA Masahiko <yasuoka@yasuoka.net>
@@ -70,6 +70,9 @@ efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *systab)
 	ST = systab;
 	BS = ST->BootServices;
 	IH = image;
+
+	/* disable reset by watchdog after 5 minutes */
+	EFI_CALL(BS->SetWatchdogTimer, 0, 0, 0, NULL);
 
 	status = EFI_CALL(BS->HandleProtocol, image, &imgp_guid,
 	    (void **)&imgp);
