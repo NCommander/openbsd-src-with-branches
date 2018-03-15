6ed7942ecefe84eafcf8fa2b8c67ef82a3b3fa7e
bluhm <bluhm>
2018-03-15 12:34:33 +0000

    When processing IPsec AH packets check that the header fits into the packet length. found by Maxime Villard; from FreeBSD; OK procter@ markus@ OpenBSD 6.2 errata 010

--- sys/netinet/ip_ah.c
+++ sys/netinet/ip_ah.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_ah.c,v 1.131 2017/08/11 21:24:19 mpi Exp $ */
+/*	$OpenBSD: ip_ah.c,v 1.131.4.1 2018/02/01 21:58:25 bluhm Exp $ */
 /*
  * The authors of this code are John Ioannidis (ji@tla.org),
  * Angelos D. Keromytis (kermit@csd.uch.gr) and
@@ -583,7 +583,16 @@ ah_input(struct mbuf *m, struct tdb *tdb, int skip, int protoff)
 		    "in SA %s/%08x\n", hl * sizeof(u_int32_t),
 		    ipsp_address(&tdb->tdb_dst, buf, sizeof(buf)),
 		    ntohl(tdb->tdb_spi)));
-
+		ahstat.ahs_badauthl++;
+		m_freem(m);
+		return EACCES;
+	}
+	if (skip + ahx->authsize + rplen > m->m_pkthdr.len) {
+		DPRINTF(("%s: bad mbuf length %d (expecting %d) "
+		    "for packet in SA %s/%08x\n", __func__,
+		    m->m_pkthdr.len, skip + ahx->authsize + rplen,
+		    ipsp_address(&tdb->tdb_dst, buf, sizeof(buf)),
+		    ntohl(tdb->tdb_spi)));
 		ahstat.ahs_badauthl++;
 		m_freem(m);
 		return EACCES;
