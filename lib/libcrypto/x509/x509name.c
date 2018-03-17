ad1106e9bddd385eceba0b51ec11ee2fc4a44922
tb <tb>
2018-03-17 15:28:27 +0000

    Provide X509_NAME_ENTRY_set()
    ok jsing

--- lib/libcrypto/x509/x509name.c
+++ lib/libcrypto/x509/x509name.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: x509name.c,v 1.13 2014/09/29 04:17:24 miod Exp $ */
+/* $OpenBSD: x509name.c,v 1.14 2017/01/29 17:49:23 beck Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -404,4 +404,10 @@ X509_NAME_ENTRY_get_data(X509_NAME_ENTRY *ne)
 	if (ne == NULL)
 		return (NULL);
 	return (ne->value);
+}
+
+int
+X509_NAME_ENTRY_set(const X509_NAME_ENTRY *ne)
+{
+	return (ne->set);
 }
