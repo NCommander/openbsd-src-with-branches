ae5238135d092f7210ba327ff27dd3bbc1abd9b5
cvs-fast-export <cvs-fast-export>
2018-03-24 19:29:03 +0000

    Synthetic commit for incomplete tag OPENBSD_6_3_BASE

--- usr.sbin/bind/lib/dns/include/dns/rdatalist.h
+++ usr.sbin/bind/lib/dns/include/dns/rdatalist.h
@@ -1,21 +1,21 @@
 /*
+ * Copyright (C) 2004, 2005  Internet Systems Consortium, Inc. ("ISC")
  * Copyright (C) 1999-2001  Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
  *
- * THE SOFTWARE IS PROVIDED "AS IS" AND INTERNET SOFTWARE CONSORTIUM
- * DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
- * INTERNET SOFTWARE CONSORTIUM BE LIABLE FOR ANY SPECIAL, DIRECT,
- * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING
- * FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
- * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
- * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND ISC DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS.  IN NO EVENT SHALL ISC BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+ * OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $ISC: rdatalist.h,v 1.13 2001/01/09 21:53:16 bwelling Exp $ */
+/* $ISC: rdatalist.h,v 1.14.18.2 2005/04/29 00:16:19 marka Exp $ */
 
 #ifndef DNS_RDATALIST_H
 #define DNS_RDATALIST_H 1
@@ -24,32 +24,31 @@
  ***** Module Info
  *****/
 
-/*
- * DNS Rdatalist
- *
+/*! \file
+ * \brief
  * A DNS rdatalist is a list of rdata of a common type and class.
  *
  * MP:
- *	Clients of this module must impose any required synchronization.
+ *\li	Clients of this module must impose any required synchronization.
  *
  * Reliability:
- *	No anticipated impact.
+ *\li	No anticipated impact.
  *
  * Resources:
- *	<TBS>
+ *\li	TBS
  *
  * Security:
- *	No anticipated impact.
+ *\li	No anticipated impact.
  *
  * Standards:
- *	None.
+ *\li	None.
  */
 
 #include <isc/lang.h>
 
 #include <dns/types.h>
 
-/*
+/*%
  * Clients may use this type directly.
  */
 struct dns_rdatalist {
@@ -65,38 +64,38 @@ ISC_LANG_BEGINDECLS
 
 void
 dns_rdatalist_init(dns_rdatalist_t *rdatalist);
-/*
+/*%<
  * Initialize rdatalist.
  *
  * Ensures:
- *	All fields of rdatalist have been initialized to their default
+ *\li	All fields of rdatalist have been initialized to their default
  *	values.
  */
 
 isc_result_t
 dns_rdatalist_tordataset(dns_rdatalist_t *rdatalist,
 			 dns_rdataset_t *rdataset);
-/*
+/*%<
  * Make 'rdataset' refer to the rdata in 'rdatalist'.
  *
  * Note:
- *	The caller must ensure that 'rdatalist' remains valid and unchanged
+ *\li	The caller must ensure that 'rdatalist' remains valid and unchanged
  *	while 'rdataset' is associated with it.
  *
  * Requires:
  *
- *	'rdatalist' is a valid rdatalist.
+ *\li	'rdatalist' is a valid rdatalist.
  *
- *	'rdataset' is a valid rdataset that is not currently associated with
+ *\li	'rdataset' is a valid rdataset that is not currently associated with
  *	any rdata.
  *
- * Ensures:
- *	On success,
+ * Ensures,
+ *	on success,
  *
- *		'rdataset' is associated with the rdata in rdatalist.
+ *\li		'rdataset' is associated with the rdata in rdatalist.
  *
  * Returns:
- *	ISC_R_SUCCESS
+ *\li	#ISC_R_SUCCESS
  */
 
 ISC_LANG_ENDDECLS
