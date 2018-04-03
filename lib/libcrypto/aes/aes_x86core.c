d21e2e57acc7d42f111ee68242173507e00c3bd8
tb <tb>
2018-04-03 21:59:37 +0000

    KNF: move two opening curly braces of function bodies to their own lines

--- lib/libcrypto/aes/aes_x86core.c
+++ lib/libcrypto/aes/aes_x86core.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: aes_x86core.c,v 1.7 2014/10/28 07:35:58 jsg Exp $ */
+/* $OpenBSD: aes_x86core.c,v 1.8 2015/02/10 09:46:30 miod Exp $ */
 /**
  * rijndael-alg-fst.c
  *
@@ -465,7 +465,8 @@ static const u32 rcon[] = {
  * Expand the cipher key into the encryption key schedule.
  */
 int
-AES_set_encrypt_key(const unsigned char *userKey, const int bits, AES_KEY *key) {
+AES_set_encrypt_key(const unsigned char *userKey, const int bits, AES_KEY *key)
+{
 	u32 *rk;
 	int i = 0;
 	u32 temp;
