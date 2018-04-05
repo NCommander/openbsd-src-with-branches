4a80e35b01cfc89839079e65bf645baa51d910a9
lteo <lteo>
2018-04-05 03:47:27 +0000

    Import pcap_set_immediate_mode() from mainline libpcap which allows a libpcap-based program to process packets as soon as they arrive.
    feedback from jasper@
    ok jca@
    (a long time ago)

--- lib/libpcap/pcap.h
+++ lib/libpcap/pcap.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pcap.h,v 1.17 2016/04/02 08:49:49 dlg Exp $	*/
+/*	$OpenBSD: pcap.h,v 1.18 2016/04/06 08:02:56 jasper Exp $	*/
 
 /*
  * Copyright (c) 1993, 1994, 1995, 1996, 1997
@@ -168,6 +168,7 @@ int	pcap_set_promisc(pcap_t *, int);
 int	pcap_can_set_rfmon(pcap_t *);
 int	pcap_set_rfmon(pcap_t *, int);
 int	pcap_set_timeout(pcap_t *, int);
+int	pcap_set_immediate_mode(pcap_t *, int);
 int	pcap_set_buffer_size(pcap_t *, int);
 int	pcap_activate(pcap_t *);
 
