6c73844626582836e37957d157e32007acb593f2
dlg <dlg>
2018-04-08 00:39:34 +0000

    on output, actually attach the mbuf tag to packet.
    this is to suppress loops over the interface, and avoids a leak of
    the tag on every packet.
    
    found and fix testd by christian barthel
    also tested by job snijders
    ok bluhm@

--- sys/net/if_gif.c
+++ sys/net/if_gif.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_gif.c,v 1.112 2018/02/28 23:28:05 dlg Exp $	*/
+/*	$OpenBSD: if_gif.c,v 1.113 2018/03/15 21:01:18 remi Exp $	*/
 /*	$KAME: if_gif.c,v 1.43 2001/02/20 08:51:07 itojun Exp $	*/
 
 /*
@@ -403,6 +403,8 @@ gif_output(struct ifnet *ifp, struct mbuf *m, struct sockaddr *dst,
 		error = ENOBUFS;
 		goto drop;
 	}
+	memcpy((caddr_t)(mtag + 1), &ifp->if_index, sizeof(ifp->if_index));
+	m_tag_prepend(m, mtag);
 
 	m->m_pkthdr.ph_family = dst->sa_family;
 
