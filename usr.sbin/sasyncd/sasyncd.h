0941a1f5a9d1de51897cedd0944f915c1fd49ce3
cheloha <cheloha>
2018-04-10 15:58:21 +0000

    Schedule events against the monotonic clock.
    So that events fire punctually, even if the system clock is changed.
    
    ok jca@

--- usr.sbin/sasyncd/sasyncd.h
+++ usr.sbin/sasyncd/sasyncd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sasyncd.h,v 1.17 2012/09/15 00:46:43 guenther Exp $	*/
+/*	$OpenBSD: sasyncd.h,v 1.18 2016/07/18 21:22:09 benno Exp $	*/
 
 /*
  * Copyright (c) 2005 Håkan Olsson.  All rights reserved.
@@ -37,7 +37,7 @@ enum RUNSTATE		{ INIT = 0, SLAVE, MASTER, FAIL };
 #define CARPSTATES	{ "INIT", "SLAVE", "MASTER", "FAIL" }
 
 struct syncpeer;
-struct timeval;
+struct timespec;
 
 struct cfgstate {
 	enum RUNSTATE	 runstate;
@@ -171,6 +171,6 @@ void	pfkey_snapshot(void *);
 
 /* timer.c */
 void	timer_init(void);
-void	timer_next_event(struct timeval *);
+void	timer_next_event(struct timespec *);
 void	timer_run(void);
 int	timer_add(char *, u_int32_t, void (*)(void *), void *);
