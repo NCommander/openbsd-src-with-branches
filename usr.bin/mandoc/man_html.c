7f2cfc39da740b58bbebebd0c1c0d55152daeb0e
schwarze <schwarze>
2018-04-11 17:10:35 +0000

    preserve comments before .Dd when converting mdoc(7) to man(7) with mandoc -Tman; suggested by Thomas Klausner <wiz at NetBSD>

--- usr.bin/mandoc/man_html.c
+++ usr.bin/mandoc/man_html.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: man_html.c,v 1.97 2017/06/24 14:38:27 schwarze Exp $ */
+/*	$OpenBSD: man_html.c,v 1.98 2017/06/25 07:23:53 bentley Exp $ */
 /*
  * Copyright (c) 2008-2012, 2014 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2013, 2014, 2015, 2017 Ingo Schwarze <schwarze@openbsd.org>
@@ -259,6 +259,8 @@ print_man_node(MAN_ARGS)
 		if (*n->string != '\0')
 			break;
 		print_paragraph(h);
+		return;
+	case ROFFT_COMMENT:
 		return;
 	default:
 		break;
