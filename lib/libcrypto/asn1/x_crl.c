5211dc1e7b6e05784dc1f5752eb2ce777214f53e
tb <tb>
2018-04-23 09:00:19 +0000

    Make X509_CRL_METHOD_free() NULL-safe.
    ok bcook

--- lib/libcrypto/asn1/x_crl.c
+++ lib/libcrypto/asn1/x_crl.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: x_crl.c,v 1.29 2018/02/22 16:58:45 jsing Exp $ */
+/* $OpenBSD: x_crl.c,v 1.30 2018/03/17 14:33:20 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -675,7 +675,7 @@ X509_CRL_METHOD_new(int (*crl_init)(X509_CRL *crl),
 void
 X509_CRL_METHOD_free(X509_CRL_METHOD *m)
 {
-	if (!(m->flags & X509_CRL_METHOD_DYNAMIC))
+	if (m == NULL || !(m->flags & X509_CRL_METHOD_DYNAMIC))
 		return;
 	free(m);
 }
