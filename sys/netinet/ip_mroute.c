87cc82118f581a4920980b711798d20ce39f84bf
tb <tb>
2018-04-30 19:07:44 +0000

    Reduce the scope of the NET_LOCK() in in_control(). Two functions were protected: mrt_ioctl() and in_ioctl(). The former has no other callers and only needs a read lock. The latter will need refactoring to reduce the lock's scope further. In a first step, establish a single exit point and protect most of the function body with the NET_LOCK() while removing the NET_LOCK() from a handful of callers.
    suggested by & ok mpi, ok visa

--- sys/netinet/ip_mroute.c
+++ sys/netinet/ip_mroute.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_mroute.c,v 1.120 2017/06/26 09:32:32 mpi Exp $	*/
+/*	$OpenBSD: ip_mroute.c,v 1.121 2017/09/01 15:05:31 mpi Exp $	*/
 /*	$NetBSD: ip_mroute.c,v 1.85 2004/04/26 01:31:57 matt Exp $	*/
 
 /*
@@ -264,12 +264,16 @@ mrt_ioctl(struct socket *so, u_long cmd, caddr_t data)
 	else
 		switch (cmd) {
 		case SIOCGETVIFCNT:
+			NET_RLOCK();
 			error = get_vif_cnt(inp->inp_rtableid,
 			    (struct sioc_vif_req *)data);
+			NET_RUNLOCK();
 			break;
 		case SIOCGETSGCNT:
+			NET_RLOCK();
 			error = get_sg_cnt(inp->inp_rtableid,
 			    (struct sioc_sg_req *)data);
+			NET_RUNLOCK();
 			break;
 		default:
 			error = ENOTTY;
