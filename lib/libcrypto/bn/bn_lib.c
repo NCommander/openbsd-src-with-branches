c9fa49a185dde0be53895fcfdda1e13afdf56dcd
jsing <jsing>
2018-05-12 17:31:41 +0000

    Use explicit_bzero() rather than memset() when clearing a BIGNUM.
    ok bcook@ tb@

--- lib/libcrypto/bn/bn_lib.c
+++ lib/libcrypto/bn/bn_lib.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: bn_lib.c,v 1.38 2017/05/02 03:59:44 deraadt Exp $ */
+/* $OpenBSD: bn_lib.c,v 1.39 2018/02/20 17:13:14 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -529,7 +529,7 @@ BN_clear(BIGNUM *a)
 {
 	bn_check_top(a);
 	if (a->d != NULL)
-		memset(a->d, 0, a->dmax * sizeof(a->d[0]));
+		explicit_bzero(a->d, a->dmax * sizeof(a->d[0]));
 	a->top = 0;
 	a->neg = 0;
 }
