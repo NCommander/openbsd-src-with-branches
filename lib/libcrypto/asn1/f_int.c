df87bec19e26f3e460b8b959df210025e82c7577
jsing <jsing>
2018-05-13 13:48:08 +0000

    Use recallocarray() instead of OPENSSL_realloc_clean().
    Also place all of the OPENSSL_* memory related prototypes under #ifndef
    LIBRESSL_INTERNAL.
    
    ok beck@ tb@

--- lib/libcrypto/asn1/f_int.c
+++ lib/libcrypto/asn1/f_int.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: f_int.c,v 1.18 2017/01/29 17:49:22 beck Exp $ */
+/* $OpenBSD: f_int.c,v 1.19 2018/04/25 11:48:21 tb Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -158,8 +158,7 @@ a2i_ASN1_INTEGER(BIO *bp, ASN1_INTEGER *bs, char *buf, int size)
 		}
 		i /= 2;
 		if (num + i > slen) {
-			sp = OPENSSL_realloc_clean(s, slen, num + i);
-			if (sp == NULL) {
+			if ((sp = recallocarray(s, slen, num + i, 1)) == NULL) {
 				ASN1error(ERR_R_MALLOC_FAILURE);
 				goto err;
 			}
