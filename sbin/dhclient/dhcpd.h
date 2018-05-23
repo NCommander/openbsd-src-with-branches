1a212fa5672a35071a5cea0736e610bdaa0ce66c
krw <krw>
2018-05-23 12:18:58 +0000

    Some interfaces (e.g. cdce(4)) can't tell if they are connected.  They always report a link status of UNKNOWN.
    Do not use recorded leases to configure such interfaces. In such
    cases only use the address in the recorded lease to ask for a
    new lease.
    
    Avoids debilitating /etc/resolv.conf changes by unplugged higher
    priority interfaces given to the tender mercies of dhclient after a
    working interface has already set /etc/resolv.conf correctly.
    
    Reported for cdce(4) by guenther@ at p2k18.

--- sbin/dhclient/dhcpd.h
+++ sbin/dhclient/dhcpd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dhcpd.h,v 1.254 2018/02/28 22:16:56 krw Exp $	*/
+/*	$OpenBSD: dhcpd.h,v 1.255 2018/05/19 22:10:22 krw Exp $	*/
 
 /*
  * Copyright (c) 2004 Henning Brauer <henning@openbsd.org>
@@ -126,7 +126,7 @@ struct interface_info {
 	size_t			 rbuf_len;
 	int			 errors;
 	uint16_t		 index;
-	int			 linkstat;
+	int			 link_state;
 	int			 rdomain;
 	int			 flags;
 #define	IFI_VALID_LLADDR	0x01
