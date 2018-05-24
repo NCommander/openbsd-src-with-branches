b537d6e59394e3c9fea05caa6e96a6ec06d2c314
nicm <nicm>
2018-05-24 09:42:49 +0000

    Make server_client_get_cwd used (almost) everywhere we need to work out the cwd, and do not fall back to "." as it is pretty useless. GitHub issue 1331.

--- usr.bin/tmux/cmd-source-file.c
+++ usr.bin/tmux/cmd-source-file.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: cmd-source-file.c,v 1.34 2017/02/14 18:13:05 nicm Exp $ */
+/* $OpenBSD: cmd-source-file.c,v 1.35 2017/04/19 16:59:54 nicm Exp $ */
 
 /*
  * Copyright (c) 2008 Tiago Cunha <me@tiagocunha.org>
@@ -61,7 +61,7 @@ cmd_source_file_exec(struct cmd *self, struct cmdq_item *item)
 	if (*path == '/')
 		pattern = xstrdup(path);
 	else {
-		utf8_stravis(&tmp, server_client_get_cwd(c), VIS_GLOB);
+		utf8_stravis(&tmp, server_client_get_cwd(c, NULL), VIS_GLOB);
 		xasprintf(&pattern, "%s/%s", tmp, path);
 		free(tmp);
 	}
