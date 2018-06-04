1e5ad44bd3a7dd2a124f76b87e05458e51509c4f
kettenis <kettenis>
2018-06-04 09:24:49 +0000

    Remove unused include.

--- sys/arch/armv7/sunxi/sxiintc.c
+++ sys/arch/armv7/sunxi/sxiintc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sxiintc.c,v 1.2 2016/08/06 18:21:34 patrick Exp $	*/
+/*	$OpenBSD: sxiintc.c,v 1.3 2017/01/21 08:26:49 patrick Exp $	*/
 /*
  * Copyright (c) 2007,2009 Dale Rahn <drahn@openbsd.org>
  * Copyright (c) 2013 Artturi Alm
@@ -26,7 +26,6 @@
 #include <machine/bus.h>
 #include <machine/fdt.h>
 
-#include <armv7/armv7/armv7var.h>
 #include <dev/fdt/sunxireg.h>
 #include <armv7/sunxi/sxiintc.h>
 
