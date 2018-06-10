ea77f4a4eaca989386025cb30d22d663fd39c705
remi <remi>
2018-06-10 14:39:38 +0000

    memory leak spotted by claudio@
    go ahead jca@
    OK claudio@

--- usr.sbin/ospf6d/rde.c
+++ usr.sbin/ospf6d/rde.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rde.c,v 1.72 2017/08/12 16:27:50 benno Exp $ */
+/*	$OpenBSD: rde.c,v 1.73 2018/06/10 14:31:49 remi Exp $ */
 
 /*
  * Copyright (c) 2004, 2005 Claudio Jeker <claudio@openbsd.org>
@@ -1363,7 +1363,8 @@ prefix_tree_add(struct prefix_tree *tree, struct lsa_link *lsa)
 				old->prefix->options |= new->prefix->options;
 				free(new);
 			}
-		}
+		} else
+			free(new);
 
 		cur_prefix = cur_prefix + len;
 	}
