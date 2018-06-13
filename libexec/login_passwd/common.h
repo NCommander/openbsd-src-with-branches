feca1b7a31b9095f6cda28e9aedb0854fb6edd9c
reyk <reyk>
2018-06-13 14:54:42 +0000

    Remove #ifdef PASSWD, it was always enabled and is a leftover from krb5 days.
    No other uses of -DPASSWD were found in the tree.
    
    OK deraadt@ tb@ brynet@

--- libexec/login_passwd/common.h
+++ libexec/login_passwd/common.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: common.h,v 1.7 2016/09/02 10:36:51 gsoares Exp $ */
+/* $OpenBSD: common.h,v 1.8 2016/09/03 10:50:16 gsoares Exp $ */
 /*-
  * Copyright (c) 2001 Hans Insulander <hin@openbsd.org>.
  * All rights reserved.
@@ -54,8 +54,6 @@
 
 extern FILE *back;
 
-#ifdef PASSWD
 int pwd_login(char *, char *, char *, int, char *);
-#endif
 
 #endif /* !_COMMON_H_ */
