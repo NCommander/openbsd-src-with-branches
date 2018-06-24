82ddfe27860a9036ef21c19a50b7386bfeb0b9a3
espie <espie>
2018-06-24 12:38:03 +0000

    fix need_params

--- usr.sbin/pkg_add/OpenBSD/PackingElement.pm
+++ usr.sbin/pkg_add/OpenBSD/PackingElement.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: PackingElement.pm,v 1.258 2018/06/23 13:13:38 espie Exp $
+# $OpenBSD: PackingElement.pm,v 1.259 2018/06/23 22:46:36 espie Exp $
 #
 # Copyright (c) 2003-2014 Marc Espie <espie@openbsd.org>
 #
@@ -1464,7 +1464,7 @@ sub run_tag
 sub need_params
 {
 	my $self = shift;
-	return $self->{expanded} =~ m/\%[lu]/;
+	return $self->{params} =~ m/\%[lu]/;
 }
 
 package OpenBSD::PackingElement::Exec;
