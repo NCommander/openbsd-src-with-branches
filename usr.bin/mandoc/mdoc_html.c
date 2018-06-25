d88b0ede818567ef35a88e96541dd8c8360b6524
schwarze <schwarze>
2018-06-25 14:06:03 +0000

    Drop explicit, constant style=margin-left attribute on .Bf blocks. Since <div> uses HTML_NLAROUND, it is no longer needed.

--- usr.bin/mandoc/mdoc_html.c
+++ usr.bin/mandoc/mdoc_html.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mdoc_html.c,v 1.181 2018/06/10 16:15:40 schwarze Exp $ */
+/*	$OpenBSD: mdoc_html.c,v 1.182 2018/06/25 13:46:01 schwarze Exp $ */
 /*
  * Copyright (c) 2008-2011, 2014 Kristaps Dzonsons <kristaps@bsd.lv>
  * Copyright (c) 2014,2015,2016,2017,2018 Ingo Schwarze <schwarze@openbsd.org>
@@ -1414,7 +1414,7 @@ mdoc_bf_pre(MDOC_ARGS)
 		cattr = "Bf No";
 
 	/* Cannot use TAG_SPAN because it may contain blocks. */
-	print_otag(h, TAG_DIV, "cshl", cattr, 1);
+	print_otag(h, TAG_DIV, "c", cattr);
 	return 1;
 }
 
