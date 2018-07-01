8fa7c585dddc03a94a0e5e4dd01e786ef365e051
visa <visa>
2018-07-01 16:33:15 +0000

    Lock the file descriptor table when accessing the `fd_ofileflags' array. This prevents the array from being freed too early. In the function unp_internalize(), the locking also ensures the per-fdp flags stay coherent with the file instance.
    OK mpi@

--- sys/kern/kern_descrip.c
+++ sys/kern/kern_descrip.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_descrip.c,v 1.171 2018/06/26 14:43:01 visa Exp $	*/
+/*	$OpenBSD: kern_descrip.c,v 1.172 2018/06/27 16:37:25 visa Exp $	*/
 /*	$NetBSD: kern_descrip.c,v 1.42 1996/03/30 22:24:38 christos Exp $	*/
 
 /*
@@ -433,7 +433,9 @@ restart:
 		return (error);
 
 	case F_GETFD:
+		fdplock(fdp);
 		*retval = fdp->fd_ofileflags[fd] & UF_EXCLOSE ? 1 : 0;
+		fdpunlock(fdp);
 		break;
 
 	case F_SETFD:
