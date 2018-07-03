7a83e561a09e702d36c530ed9322f29ef40f3950
kettenis <kettenis>
2018-07-03 20:40:25 +0000

    Add a new so_seek member to "struct file" such that we can have seekable files that aren't vnodes.  Move the vnode-specific code into its own function.  Add an implementation for the "DMA buffers" that can be used by DRI3/prime code to find out the size of the graphics buffer. This implementation is very limited and only supports offset 0 and only for SEEK_SET and SEEK_END.  This doesn't really make sense; implementing stat(2) would be a more obvious choice.  But this is what Linux does.
    ok guenther@, visa@

--- sys/sys/file.h
+++ sys/sys/file.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: file.h,v 1.50 2018/06/25 22:29:16 kettenis Exp $	*/
+/*	$OpenBSD: file.h,v 1.51 2018/07/02 14:36:33 visa Exp $	*/
 /*	$NetBSD: file.h,v 1.11 1995/03/26 20:24:13 jtc Exp $	*/
 
 /*
@@ -57,6 +57,7 @@ struct	fileops {
 	int	(*fo_kqfilter)(struct file *, struct knote *);
 	int	(*fo_stat)(struct file *, struct stat *, struct proc *);
 	int	(*fo_close)(struct file *, struct proc *);
+	int	(*fo_seek)(struct file *, off_t *, int, struct proc *);
 };
 
 /*
