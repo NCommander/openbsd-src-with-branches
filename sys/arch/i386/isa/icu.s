eadae4c39f85418f865bd52bcfe0077482a99f8e
guenther <guenther>
2018-07-09 19:20:30 +0000

    Delete the VM86 kernel option and i386_vm86(3) API: it's required a custom kernel for over 20 years.
    testing mlarkin@
    ok deraadt@ phessler@ jca@ matthieu@

--- sys/arch/i386/isa/icu.s
+++ sys/arch/i386/isa/icu.s
@@ -1,4 +1,4 @@
-/*	$OpenBSD: icu.s,v 1.33 2015/07/16 05:10:14 guenther Exp $	*/
+/*	$OpenBSD: icu.s,v 1.34 2018/04/11 15:44:08 bluhm Exp $	*/
 /*	$NetBSD: icu.s,v 1.45 1996/01/07 03:59:34 mycroft Exp $	*/
 
 /*-
@@ -96,10 +96,6 @@ KIDTVEC(doreti)
 	movl	%ebx,CPL
 	je	3f
 	testb   $SEL_RPL,TF_CS(%esp)
-#ifdef VM86
-	jnz	4f
-	testl	$PSL_VM,TF_EFLAGS(%esp)
-#endif
 	jz	3f
 4:	CLEAR_ASTPENDING(%ecx)
 	sti
