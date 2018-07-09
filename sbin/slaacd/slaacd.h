42c79fae8fa09e480544faff84c6afd0f780463c
florian <florian>
2018-07-09 07:41:15 +0000

    Remove array of IMSG type names.
    It was helpfull in the beginning to know which IMSG are flying around
    but unusable (and unsused) since slaacd(8) is on the ramdisk.

--- sbin/slaacd/slaacd.h
+++ sbin/slaacd/slaacd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: slaacd.h,v 1.15 2018/05/17 11:51:27 florian Exp $	*/
+/*	$OpenBSD: slaacd.h,v 1.16 2018/05/17 13:39:00 florian Exp $	*/
 
 /*
  * Copyright (c) 2017 Florian Obser <florian@openbsd.org>
@@ -75,8 +75,6 @@ enum imsg_type {
 	IMSG_CONFIGURE_DFR,
 	IMSG_WITHDRAW_DFR,
 };
-
-extern const char* imsg_type_name[];
 
 enum {
 	PROC_MAIN,
