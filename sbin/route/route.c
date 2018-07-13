69a4f5758f32c980ee93978b800539bacf462ea1
kn <kn>
2018-07-13 09:06:58 +0000

    Use "rtable" not "tableid" as argument name for rtable
    Make rtable(4) usage documentation consistent with other programs.
    This is to have `man -k ar~rtable' show the full list without having to
    look for other variations of the same argument type.
    
    OK bluhm

--- sbin/route/route.c
+++ sbin/route/route.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: route.c,v 1.218 2018/07/11 15:25:42 kn Exp $	*/
+/*	$OpenBSD: route.c,v 1.219 2018/07/12 16:08:02 florian Exp $	*/
 /*	$NetBSD: route.c,v 1.16 1996/04/15 18:27:05 cgd Exp $	*/
 
 /*
@@ -130,7 +130,7 @@ usage(char *cp)
 	if (cp)
 		warnx("botched keyword: %s", cp);
 	fprintf(stderr,
-	    "usage: %s [-dnqtv] [-T tableid] command [[modifiers] args]\n",
+	    "usage: %s [-dnqtv] [-T rtable] command [[modifiers] args]\n",
 	    __progname);
 	fprintf(stderr,
 	    "commands: add, change, delete, exec, flush, get, monitor, show\n");
