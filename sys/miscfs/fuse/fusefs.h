e473ca3305a26876a58046eb2fa6f40db3b537f5
helg <helg>
2018-07-16 13:10:53 +0000

    Implement FBT_FSYNC, which is called on fsync(2) and fdatasync(2). Currently ignores the a_waitfor argument and always invokes the file system's fsync implementation synchronously.
    ok mpi@

--- sys/miscfs/fuse/fusefs.h
+++ sys/miscfs/fuse/fusefs.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: fusefs.h,v 1.11 2018/06/21 14:53:36 helg Exp $ */
+/* $OpenBSD: fusefs.h,v 1.12 2018/06/25 12:03:53 helg Exp $ */
 /*
  * Copyright (c) 2012-2013 Sylvestre Gallon <ccna.syl@gmail.com>
  *
@@ -68,6 +68,7 @@ struct fusefs_mnt {
 #define UNDEF_SYMLINK	1<<9
 #define UNDEF_MKNOD	1<<10
 #define UNDEF_FLUSH	1<<11
+#define UNDEF_FSYNC	1<<12
 
 extern struct vops fusefs_vops;
 extern struct pool fusefs_fbuf_pool;
