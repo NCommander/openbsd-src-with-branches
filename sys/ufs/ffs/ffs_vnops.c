c3a54731779ccb670ce6a1fe7d7f0d2fa8b9b1f0
anton <anton>
2018-07-21 09:35:08 +0000

    Include the vnode type in the panic message in ffs_write(), just like ffs_read() does.
    ok deraadt@ kettenis@

--- sys/ufs/ffs/ffs_vnops.c
+++ sys/ufs/ffs/ffs_vnops.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ffs_vnops.c,v 1.90 2018/01/13 15:56:02 millert Exp $	*/
+/*	$OpenBSD: ffs_vnops.c,v 1.91 2018/07/02 20:56:22 bluhm Exp $	*/
 /*	$NetBSD: ffs_vnops.c,v 1.7 1996/05/11 18:27:24 mycroft Exp $	*/
 
 /*
@@ -315,7 +315,7 @@ ffs_write(void *v)
 			panic("ffs_write: nonsync dir write");
 		break;
 	default:
-		panic("ffs_write: type");
+		panic("ffs_write: type %d", vp->v_type);
 	}
 
 	fs = ip->i_fs;
