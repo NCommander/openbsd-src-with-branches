653124da8312bca90e37c82f6ceb140242d8f19b
schwarze <schwarze>
2018-08-05 13:32:25 +0000

    remove duplicate installation of the SIGINT handler; from Martin Kopta <martin at kopta dot eu>

--- games/bs/bs.c
+++ games/bs/bs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bs.c,v 1.38 2016/01/10 14:10:39 mestre Exp $	*/
+/*	$OpenBSD: bs.c,v 1.39 2016/03/07 12:07:55 mestre Exp $	*/
 /*
  * Copyright (c) 1986, Bruce Holloway
  * All rights reserved.
@@ -254,7 +254,6 @@ intro(void)
 {
     char *tmpname;
 
-    (void) signal(SIGINT,uninitgame);
     (void) signal(SIGINT,uninitgame);
     if(signal(SIGQUIT,SIG_IGN) != SIG_IGN)
 	(void)signal(SIGQUIT,uninitgame);
