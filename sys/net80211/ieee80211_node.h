9f96e8587314342e365aca5d3dee3f62d12e7220
stsp <stsp>
2018-08-07 18:13:14 +0000

    Ignore basic MCS set information found in association responses.
    Some APs (Aruba 105) send a bogus basic MCS set in assoc responses
    which prevents us from enabling 11n support with those APs, while
    these APs still behave as if were were an 11n client which renders
    the association unusable.
    
    The basic MSC set is already provided in beacons anyway, and the
    802.11 2012 standard says the basic MSC set is reserved in frames
    other than beacons (see Table 8-130).
    
    ok mpi@

--- sys/net80211/ieee80211_node.h
+++ sys/net80211/ieee80211_node.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ieee80211_node.h,v 1.74 2018/04/28 14:49:07 stsp Exp $	*/
+/*	$OpenBSD: ieee80211_node.h,v 1.75 2018/07/11 20:18:09 phessler Exp $	*/
 /*	$NetBSD: ieee80211_node.h,v 1.9 2004/04/30 22:57:32 dyoung Exp $	*/
 
 /*-
@@ -402,7 +402,7 @@ void ieee80211_setup_htcaps(struct ieee80211_node *, const uint8_t *,
     uint8_t);
 void ieee80211_clear_htcaps(struct ieee80211_node *);
 int ieee80211_setup_htop(struct ieee80211_node *, const uint8_t *,
-    uint8_t);
+    uint8_t, int);
 int ieee80211_setup_rates(struct ieee80211com *,
 	    struct ieee80211_node *, const u_int8_t *, const u_int8_t *, int);
 int ieee80211_iserp_sta(const struct ieee80211_node *);
