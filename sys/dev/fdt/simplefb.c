10ab27e3c0ac9949ecc06b73023f6e9d81af6030
kettenis <kettenis>
2018-08-27 09:30:07 +0000

    Add an interface that allows drivers to claim a framebuffer and check whether another driver has already claimed a framebuffer.  Use this in radeondrm(4) and simplefb(4) to prevent the latter from attaching if radeondrm(4) is attached to the hardware that provides the framebuffer set up by the firmware.
    ok mlarkin@

--- sys/dev/fdt/simplefb.c
+++ sys/dev/fdt/simplefb.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: simplefb.c,v 1.3 2017/12/18 10:13:45 kettenis Exp $	*/
+/*	$OpenBSD: simplefb.c,v 1.4 2018/07/31 17:25:55 fcambus Exp $	*/
 /*
  * Copyright (c) 2016 Mark Kettenis
  *
@@ -109,6 +109,10 @@ int
 simplefb_match(struct device *parent, void *match, void *aux)
 {
 	struct fdt_attach_args *faa = aux;
+
+	/* Don't attach if another driver already claimed our framebuffer. */
+	if (faa->fa_nreg > 0 && rasops_check_framebuffer(faa->fa_reg[0].addr))
+		return 0;
 
 	return OF_is_compatible(faa->fa_node, "simple-framebuffer");
 }
