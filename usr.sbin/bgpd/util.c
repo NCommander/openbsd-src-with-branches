40534e8afecada493dd86fc940a9df4208703944
claudio <claudio>
2018-08-29 11:43:15 +0000

    When checking for AS 0 make sure the full ASPATH is validated and don't return on the first 0. The other AS_ERR_SOFT cases do the same and ensure that an ASPATH with an AS_ERR_SOFT is still valid enough for subsequent manipulation or inspection. OK benno@

--- usr.sbin/bgpd/util.c
+++ usr.sbin/bgpd/util.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: util.c,v 1.29 2018/08/09 21:12:33 claudio Exp $ */
+/*	$OpenBSD: util.c,v 1.30 2018/08/10 11:13:01 claudio Exp $ */
 
 /*
  * Copyright (c) 2006 Claudio Jeker <claudio@openbsd.org>
@@ -464,7 +464,7 @@ aspath_verify(void *data, u_int16_t len, int as4byte)
 			ptr += as_size;
 			memcpy(&as, ptr, as_size);
 			if (as == 0)
-				return (AS_ERR_SOFT);
+				error = AS_ERR_SOFT;
 		}
 	}
 	return (error);	/* aspath is valid but probably not loop free */
