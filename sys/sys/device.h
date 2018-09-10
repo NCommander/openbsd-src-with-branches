9321b510437dd03475ab91fcfa3443d13138590e
sashan <sashan>
2018-09-10 16:18:34 +0000

    - if_cloners list populated at boot time only then becomes immutable,   so we can let go if_cloners_lock.
    OK tb@, claudio@, bluhm@, kn@, henning@

--- sys/sys/device.h
+++ sys/sys/device.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: device.h,v 1.53 2015/09/11 20:43:23 dlg Exp $	*/
+/*	$OpenBSD: device.h,v 1.54 2015/12/11 16:07:02 mpi Exp $	*/
 /*	$NetBSD: device.h,v 1.15 1996/04/09 20:55:24 cgd Exp $	*/
 
 /*
@@ -164,6 +164,11 @@ struct pdevinit {
 };
 
 #ifdef _KERNEL
+
+#ifdef DIAGNOSTIC
+extern int pdevinit_done;
+#endif
+
 extern struct devicelist alldevs;	/* list of all devices */
 
 extern int autoconf_verbose;
