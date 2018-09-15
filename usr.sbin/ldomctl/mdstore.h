912ca0243acd7b90b5ad612f01e27f091dd38904
kettenis <kettenis>
2018-09-15 13:20:16 +0000

    Add support for version 2.0 of the mdstore protocol.

--- usr.sbin/ldomctl/mdstore.h
+++ usr.sbin/ldomctl/mdstore.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mdstore.h,v 1.2 2012/11/04 21:44:20 kettenis Exp $	*/
+/*	$OpenBSD: mdstore.h,v 1.3 2012/11/04 23:30:38 kettenis Exp $	*/
 
 /*
  * Copyright (c) 2012 Mark Kettenis
@@ -30,6 +30,8 @@ struct mdstore_set {
 };
 
 extern TAILQ_HEAD(mdstore_set_head, mdstore_set) mdstore_sets;
+
+void mdstore_register(struct ds_conn *);
 
 void mdstore_download(struct ds_conn *, const char *);
 void mdstore_select(struct ds_conn *, const char *);
