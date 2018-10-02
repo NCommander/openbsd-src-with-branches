9df84ce06801d494c99e651706b454c704ee807d
djm <djm>
2018-10-02 12:40:07 +0000

    Add server support for signalling sessions via the SSH channel/ session protocol. Signalling is only supported to sesssions that are not subsystems and were not started with a forced command.
    Long requested in bz#1424
    
    Based on a patch from markus@ and reworked by dtucker@;
    ok markus@ dtucker@

--- usr.bin/ssh/session.h
+++ usr.bin/ssh/session.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: session.h,v 1.34 2017/08/18 05:36:45 djm Exp $ */
+/* $OpenBSD: session.h,v 1.35 2017/09/12 06:32:07 djm Exp $ */
 
 /*
  * Copyright (c) 2000, 2001 Markus Friedl.  All rights reserved.
@@ -35,6 +35,7 @@ struct Session {
 	struct passwd *pw;
 	Authctxt *authctxt;
 	pid_t	pid;
+	int	forced;
 
 	/* tty */
 	char	*term;
