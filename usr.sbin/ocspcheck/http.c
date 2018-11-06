a84ced504b2b53f83c479b51dd07a866faf6f7d8
jsing <jsing>
2018-11-06 20:41:11 +0000

    Use TLS_CA_CERT_FILE instead of a separate define.
    ok beck@ bluhm@ tb@

--- usr.sbin/ocspcheck/http.c
+++ usr.sbin/ocspcheck/http.c
@@ -1,4 +1,4 @@
-/*	$Id: http.c,v 1.8 2017/02/03 08:08:15 guenther Exp $ */
+/*	$Id: http.c,v 1.9 2017/03/26 18:41:02 deraadt Exp $ */
 /*
  * Copyright (c) 2016 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -34,8 +34,6 @@
 
 #include "http.h"
 #include <tls.h>
-
-#define DEFAULT_CA_FILE "/etc/ssl/cert.pem"
 
 /*
  * A buffer for transferring HTTP/S data.
@@ -137,7 +135,7 @@ http_init()
 		goto err;
 	}
 
-	if (tls_config_set_ca_file(tlscfg, DEFAULT_CA_FILE) == -1) {
+	if (tls_config_set_ca_file(tlscfg, TLS_CA_CERT_FILE) == -1) {
 		warn("tls_config_set_ca_file: %s", tls_config_error(tlscfg));
 		goto err;
 	}
