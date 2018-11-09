3a7c8f8fc6083866e4a5ec45b0e9ff0d705b9840
tb <tb>
2018-11-09 04:35:09 +0000

    Avoid leak: free existing SRTP connection profiles before setting it.
    From Ben L <bobsayshilol () live ! co ! uk>.

--- lib/libssl/d1_srtp.c
+++ lib/libssl/d1_srtp.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: d1_srtp.c,v 1.21 2017/02/07 02:08:38 beck Exp $ */
+/* $OpenBSD: d1_srtp.c,v 1.22 2017/08/27 02:58:04 doug Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -207,6 +207,7 @@ ssl_ctx_make_profiles(const char *profiles_string,
 			ptr = col + 1;
 	} while (col);
 
+	sk_SRTP_PROTECTION_PROFILE_free(*out);
 	*out = profiles;
 
 	return 0;
