5f28623fbd32e86eb910b3a1fa91764f84ba37b6
tb <tb>
2018-11-11 06:53:31 +0000

    Add Ribose Inc's implementation of the SM3 hashing function with tweaks from jsing and myself.  The SM2/SM3/SM4 algorithms are mandatory for legal use of cryptography within China and [are] widely applied in the country, covering identification/financial cards, contactless, TPM 2.0 and PKI.
    ok beck inoguchi jsing

--- lib/libcrypto/evp/evp.h
+++ lib/libcrypto/evp/evp.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: evp.h,v 1.68 2018/08/24 20:22:15 tb Exp $ */
+/* $OpenBSD: evp.h,v 1.69 2018/09/12 06:35:38 djm Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -685,6 +685,9 @@ const EVP_MD *EVP_sha256(void);
 #ifndef OPENSSL_NO_SHA512
 const EVP_MD *EVP_sha384(void);
 const EVP_MD *EVP_sha512(void);
+#endif
+#ifndef OPENSSL_NO_SM3
+const EVP_MD *EVP_sm3(void);
 #endif
 #ifndef OPENSSL_NO_RIPEMD
 const EVP_MD *EVP_ripemd160(void);
