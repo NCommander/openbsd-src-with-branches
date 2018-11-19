9c8a80ba7aba0bd6e5220e00cf64ca5a3b03b378
guenther <guenther>
2018-11-19 22:50:24 +0000

    Fix compilation on alpha, where DEF_WEAK() really must be paired with PROTO_NORMAL().  Problem noted by deraadt@

--- lib/libc/stdlib/malloc.c
+++ lib/libc/stdlib/malloc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: malloc.c,v 1.251 2018/11/06 08:01:43 otto Exp $	*/
+/*	$OpenBSD: malloc.c,v 1.252 2018/11/18 16:15:18 otto Exp $	*/
 /*
  * Copyright (c) 2008, 2010, 2011, 2016 Otto Moerbeek <otto@drijf.net>
  * Copyright (c) 2012 Matthew Dempsky <matthew@openbsd.org>
@@ -1542,7 +1542,6 @@ malloc_usable_size(void *ptr)
 	errno = saved_errno;
 	return sz;
 }
-DEF_WEAK(malloc_usable_size);
 
 static void *
 orealloc(struct dir_info *argpool, void *p, size_t newsz, void *f)
