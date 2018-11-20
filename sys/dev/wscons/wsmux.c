9253c90b9ad90a07464a0dfa322881c914ce2cd0
anton <anton>
2018-11-20 19:00:13 +0000

    No need for wscons to handle FIOSETOWN ioctl requests since sys_ioctl() translates such requests into TIOCSPGRP.
    ok visa@

--- sys/dev/wscons/wsmux.c
+++ sys/dev/wscons/wsmux.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: wsmux.c,v 1.32 2017/06/12 13:45:39 deraadt Exp $	*/
+/*	$OpenBSD: wsmux.c,v 1.33 2018/11/19 19:19:24 anton Exp $	*/
 /*      $NetBSD: wsmux.c,v 1.37 2005/04/30 03:47:12 augustss Exp $      */
 
 /*
@@ -470,12 +470,6 @@ wsmux_do_ioctl(struct device *dv, u_long cmd, caddr_t data, int flag,
 			return (EINVAL);
 		evar->async = *(int *)data != 0;
 		return (0);
-	case FIOSETOWN:
-		DPRINTF(("%s: FIOSETOWN\n", sc->sc_base.me_dv.dv_xname));
-		evar = sc->sc_base.me_evp;
-		if (evar == NULL)
-			return (EINVAL);
-		return (sigio_setown(&evar->sigio, *(int *)data));
 	case TIOCSPGRP:
 		DPRINTF(("%s: TIOCSPGRP\n", sc->sc_base.me_dv.dv_xname));
 		if (*(int *)data < 0)
