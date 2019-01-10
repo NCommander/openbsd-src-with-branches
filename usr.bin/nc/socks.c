3b77c7b5c6ac04f4e0972450f5d89e3835fb78cf
mestre <mestre>
2019-01-10 12:44:54 +0000

    Revert back previous commit and stop including strings.h
    Use memset(3) instead of bzero(3) since POSIX recommends using the former and
    because it's also more portable (conforms to ANSI C standard)
    
    OK tedu@ tb@

--- usr.bin/nc/socks.c
+++ usr.bin/nc/socks.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: socks.c,v 1.25 2018/03/27 16:31:10 deraadt Exp $	*/
+/*	$OpenBSD: socks.c,v 1.26 2019/01/09 12:58:18 inoguchi Exp $	*/
 
 /*
  * Copyright (c) 1999 Niklas Hallqvist.  All rights reserved.
@@ -36,7 +36,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <strings.h>
 #include <unistd.h>
 #include <resolv.h>
 #include <readpassphrase.h>
@@ -66,7 +65,7 @@ decode_addrport(const char *h, const char *p, struct sockaddr *addr,
 	int r;
 	struct addrinfo hints, *res;
 
-	bzero(&hints, sizeof(hints));
+	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = v4only ? PF_INET : PF_UNSPEC;
 	hints.ai_flags = numeric ? AI_NUMERICHOST : 0;
 	hints.ai_socktype = SOCK_STREAM;
