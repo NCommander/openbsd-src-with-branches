25064237d306e0530a068353027232c528572f28
djm <djm>
2019-01-21 09:13:41 +0000

    remove hack to use non-system libcrypto

--- regress/usr.bin/ssh/agent-pkcs11.sh
+++ regress/usr.bin/ssh/agent-pkcs11.sh
@@ -1,4 +1,4 @@
-#	$OpenBSD: agent-pkcs11.sh,v 1.4 2019/01/20 23:25:25 djm Exp $
+#	$OpenBSD: agent-pkcs11.sh,v 1.5 2019/01/20 23:26:44 djm Exp $
 #	Placed in the Public Domain.
 
 tid="pkcs11 agent test"
@@ -58,10 +58,8 @@ openssl genpkey \
 openssl pkcs8 -nocrypt -in $EC |\
     softhsm2-util --slot "$slot" --label 02 --id 02 --pin "$TEST_SSH_PIN" --import /dev/stdin
 
-LIBCRYPTO=${OBJ}/../../../../lib/libcrypto/obj
-
 trace "start agent"
-eval `LD_LIBRARY_PATH=$LIBCRYPTO ${SSHAGENT} -s` > /dev/null
+eval `${SSHAGENT} -s` > /dev/null
 r=$?
 if [ $r -ne 0 ]; then
 	fail "could not start ssh-agent: exit code $r"
