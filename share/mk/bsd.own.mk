41978716a63357a35ee175c224077b032d3b3ab8
naddy <naddy>
2019-01-28 17:42:38 +0000

    Switch i386 to use lld as the default linker. ok deraadt@

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$OpenBSD: bsd.own.mk,v 1.192 2018/11/08 14:13:51 visa Exp $
+#	$OpenBSD: bsd.own.mk,v 1.193 2018/11/11 17:23:18 visa Exp $
 #	$NetBSD: bsd.own.mk,v 1.24 1996/04/13 02:08:09 thorpej Exp $
 
 # Host-specific overrides
@@ -18,7 +18,7 @@ YP?=		yes
 CLANG_ARCH=aarch64 amd64 arm i386 mips64 mips64el sparc64
 GCC4_ARCH=alpha hppa mips64 mips64el powerpc sh sparc64
 GCC3_ARCH=m88k
-LLD_ARCH=aarch64 amd64 arm
+LLD_ARCH=aarch64 amd64 arm i386
 
 # m88k: ?
 PIE_ARCH=alpha amd64 arm hppa i386 mips64 mips64el powerpc sh sparc64
