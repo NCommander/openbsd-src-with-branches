ee2ee883c6b90e287ef8db9052a16852e9a71b8a
anton <anton>
2019-02-15 18:33:44 +0000

    The underlying storage for builtin ddb variables are of type int but referenced to using a pointer to long. When writing to such a variable, cast it to the correct type. Writing would otherwise on 64-bit architectures cause the next variable adjacent in memory to also be modified.
    ok deraadt@ visa@

--- sys/ddb/db_variables.c
+++ sys/ddb/db_variables.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: db_variables.c,v 1.17 2015/03/14 05:48:17 tedu Exp $	*/
+/*	$OpenBSD: db_variables.c,v 1.18 2016/01/25 14:30:30 mpi Exp $	*/
 /*	$NetBSD: db_variables.c,v 1.8 1996/02/05 01:57:19 christos Exp $	*/
 
 /*
@@ -163,7 +163,7 @@ db_var_rw_int(struct db_variable *var, db_expr_t *expr, int mode)
 {
 
 	if (mode == DB_VAR_SET)
-		*var->valuep = *(int *)expr;
+		*(int *)var->valuep = *expr;
 	else
 		*expr = *(int *)var->valuep;
 	return (0);
