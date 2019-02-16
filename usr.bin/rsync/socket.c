695a8dee61530b4bd35a7d36584443dc876b9d15
florian <florian>
2019-02-16 10:44:01 +0000

    Add chown pledge so that root can gift files to other uids.
    "you snooze, you get collisions" deraadt@

--- usr.bin/rsync/socket.c
+++ usr.bin/rsync/socket.c
@@ -1,4 +1,4 @@
-/*	$Id: socket.c,v 1.6 2019/02/12 19:20:15 benno Exp $ */
+/*	$Id: socket.c,v 1.7 2019/02/16 05:06:30 deraadt Exp $ */
 /*
  * Copyright (c) 2019 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -228,9 +228,9 @@ protocol_line(struct sess *sess, const char *host, const char *cp)
 }
 
 /*
- * Pledges: dns, inet, unveil, rpath, cpath, wpath, stdio, fattr.
+ * Pledges: dns, inet, unveil, rpath, cpath, wpath, stdio, fattr, chown.
  *
- * Pledges (dry-run): -cpath, -wpath, -fattr.
+ * Pledges (dry-run): -cpath, -wpath, -fattr, -chown.
  * Pledges (!preserve_times): -fattr.
  */
 int
@@ -265,7 +265,7 @@ rsync_socket(const struct opts *opts, const struct fargs *f)
 
 	/* Drop the DNS pledge. */
 
-	if (pledge("stdio rpath wpath cpath fattr getpw inet unveil", NULL) == -1) {
+	if (pledge("stdio rpath wpath cpath fattr chown getpw inet unveil", NULL) == -1) {
 		ERR(&sess, "pledge");
 		goto out;
 	}
@@ -286,7 +286,7 @@ rsync_socket(const struct opts *opts, const struct fargs *f)
 	}
 
 	/* Drop the inet pledge. */
-	if (pledge("stdio rpath wpath cpath fattr getpw unveil", NULL) == -1) {
+	if (pledge("stdio rpath wpath cpath fattr chown getpw unveil", NULL) == -1) {
 		ERR(&sess, "pledge");
 		goto out;
 	}
