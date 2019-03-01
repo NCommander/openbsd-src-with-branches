338a247e85e44bb89a045dee592676fbd9c14ea5
cheloha <cheloha>
2019-03-01 01:46:18 +0000

    New mmap(2) flag: MAP_CONCEAL.
    MAP_CONCEAL'd memory is not written to disk in the event of a core dump.
    It may grow other qualities in the future.
    
    Wanted by libressl, probably useful elsewhere, too.
    
    Prompted by deraadt@, concept from deraadt@/kettenis@.  With input from
    deraadt@, cjeker@, kettenis@, otto@, bcook@, matthew@, guenther@, djm@,
    and tedu@.
    
    ok otto@ deraadt@

--- sys/sys/mman.h
+++ sys/sys/mman.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mman.h,v 1.32 2018/02/11 05:11:50 deraadt Exp $	*/
+/*	$OpenBSD: mman.h,v 1.33 2019/01/11 18:46:30 deraadt Exp $	*/
 /*	$NetBSD: mman.h,v 1.11 1995/03/26 20:24:23 jtc Exp $	*/
 
 /*-
@@ -60,8 +60,9 @@
 #define	MAP_ANONYMOUS	MAP_ANON	/* alternate POSIX spelling */
 #define	__MAP_NOFAULT	0x2000
 #define	MAP_STACK	0x4000	/* mapping is used for a stack */
+#define	MAP_CONCEAL	0x8000	/* omit from dumps */
 
-#define	MAP_FLAGMASK	0x7ff7
+#define	MAP_FLAGMASK	0xfff7
 
 #ifndef _KERNEL
 /*
