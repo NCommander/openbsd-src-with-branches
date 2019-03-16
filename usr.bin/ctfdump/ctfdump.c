6248258a146e87becd2a2fe10c9292011360f3dc
sunil <sunil>
2019-03-16 16:35:03 +0000

    Fix a buffer over-read while dumping functions.
    A bogus vlen in metadata could cause offset point beyond CTF section
    boundary. Found by afl-fuzz.
    
    Ok mpi@

--- usr.bin/ctfdump/ctfdump.c
+++ usr.bin/ctfdump/ctfdump.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ctfdump.c,v 1.20 2019/03/16 07:36:56 sunil Exp $ */
+/*	$OpenBSD: ctfdump.c,v 1.21 2019/03/16 15:34:58 sunil Exp $ */
 
 /*
  * Copyright (c) 2016 Martin Pieuchot <mpi@openbsd.org>
@@ -476,6 +476,9 @@ ctf_dump_type(struct ctf_header *cth, const char *data, off_t dlen,
 		printf(" returns: %u args: (%u", ctt->ctt_type, *argp);
 		for (i = 1; i < vlen; i++) {
 			argp++;
+			if ((const char *)argp > data + dlen)
+				errx(1, "offset exceeds CTF section");
+
 			printf(", %u", *argp);
 		}
 		printf(")");
