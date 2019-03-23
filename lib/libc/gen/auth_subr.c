097fa6f46da74e5defba81801be1c2a87a425c20
millert <millert>
2019-03-23 17:03:00 +0000

    Remove useless secure_path(3) calls. There is no point in checking permissions of files in root-owned directories.  If it even was a problem, secure_path(3) suffers from unsolvable TOCTOU issues.  OK deraadt@

--- lib/libc/gen/auth_subr.c
+++ lib/libc/gen/auth_subr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: auth_subr.c,v 1.50 2015/12/28 22:08:18 mmcc Exp $	*/
+/*	$OpenBSD: auth_subr.c,v 1.51 2019/01/25 00:19:25 millert Exp $	*/
 
 /*
  * Copyright (c) 2000-2002,2004 Todd C. Miller <millert@openbsd.org>
@@ -847,12 +847,6 @@ auth_call(auth_session_t *as, char *path, ...)
 	}
 
 	argv[argc] = NULL;
-
-	if (secure_path(path) < 0) {
-		syslog(LOG_ERR, "%s: path not secure", path);
-		warnx("invalid script: %s", path);
-		goto fail;
-	}
 
 	if (socketpair(PF_LOCAL, SOCK_STREAM, 0, pfd) < 0) {
 		syslog(LOG_ERR, "unable to create backchannel %m");
