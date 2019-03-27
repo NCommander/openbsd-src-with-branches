d176ed29916f5683cbd24cbe2f37f9f0d81ef26e
tedu <tedu>
2019-03-27 11:12:10 +0000

    remove duplicate set key file call. from alf. ok jsing

--- lib/libtls/tls_config.c
+++ lib/libtls/tls_config.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: tls_config.c,v 1.52 2018/04/07 16:35:34 jsing Exp $ */
+/* $OpenBSD: tls_config.c,v 1.53 2018/11/29 14:24:23 tedu Exp $ */
 /*
  * Copyright (c) 2014 Joel Sing <jsing@openbsd.org>
  *
@@ -616,8 +616,6 @@ tls_config_set_keypair_file_internal(struct tls_config *config,
     const char *cert_file, const char *key_file, const char *ocsp_file)
 {
 	if (tls_config_set_cert_file(config, cert_file) != 0)
-		return (-1);
-	if (tls_config_set_key_file(config, key_file) != 0)
 		return (-1);
 	if (tls_config_set_key_file(config, key_file) != 0)
 		return (-1);
