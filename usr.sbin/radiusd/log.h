18fd9eada1909c4ad64a5050cde5ad55b2555467
yasuoka <yasuoka>
2019-03-31 03:53:42 +0000

    Save errno before doing other things.  Also add __deade for fatal() and fatalx().

--- usr.sbin/radiusd/log.h
+++ usr.sbin/radiusd/log.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: log.h,v 1.1 2015/07/21 04:06:04 yasuoka Exp $	*/
 
 #ifndef _LOG_H
 #define	_LOG_H	1
@@ -22,8 +22,8 @@ void		 log_info(const char *, ...)
 		    __attribute__((__format__ (printf, 1, 2)));
 void		 log_debug(const char *, ...)
 		    __attribute__((__format__ (printf, 1, 2)));
-void		 fatal(const char *);
-void		 fatalx(const char *);
+__dead void	 fatal(const char *);
+__dead void	 fatalx(const char *);
 __END_DECLS
 
 #endif
