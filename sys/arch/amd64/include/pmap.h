780848594c5052f6c7b266e3f7adf48222148375
mlarkin <mlarkin>
2019-04-01 12:45:50 +0000

    vmm(4): flush EPT when uvm removes mappings from a nested page table
    Ensure TLB is flushed to avoid stale entries when uvm removes
    entries from a guest VM's EPT. This is done on VM teardown and when uvm
    pages out pages in low memory situations. Prompted by a conversation with
    Maxime from NetBSD a few months back.
    
    ok deraadt

--- sys/arch/amd64/include/pmap.h
+++ sys/arch/amd64/include/pmap.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pmap.h,v 1.73 2019/01/21 06:18:37 mlarkin Exp $	*/
+/*	$OpenBSD: pmap.h,v 1.74 2019/02/18 08:26:19 yasuoka Exp $	*/
 /*	$NetBSD: pmap.h,v 1.1 2003/04/26 18:39:46 fvdl Exp $	*/
 
 /*
@@ -313,6 +313,7 @@ struct pmap {
 
 	u_int64_t pm_cpus;		/* mask of CPUs using pmap */
 	int pm_type;			/* Type of pmap this is (PMAP_TYPE_x) */
+	uint64_t eptp;			/* cached EPTP (used by vmm) */
 };
 
 /*
