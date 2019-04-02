2af91f91a9a3b4cbc0357f2377f3c8ca56b8e375
krw <krw>
2019-04-02 01:10:29 +0000

    No need for RESTORE (-R) to call DIOCGINFO and mpsave() twice in normal mode.
    And not overwriting any -F/-f file when in -n (donothing) mode is
    likely less surprising.

--- sbin/disklabel/disklabel.c
+++ sbin/disklabel/disklabel.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: disklabel.c,v 1.231 2019/04/01 00:12:02 krw Exp $	*/
+/*	$OpenBSD: disklabel.c,v 1.232 2019/04/02 00:58:00 krw Exp $	*/
 
 /*
  * Copyright (c) 1987, 1993
@@ -269,14 +269,9 @@ main(int argc, char *argv[])
 		if (!(t = fopen(argv[1], "r")))
 			err(4, "%s", argv[1]);
 		error = getasciilabel(t, &lab);
-		memset(&lab.d_uid, 0, sizeof(lab.d_uid));
 		if (error == 0) {
+			memset(&lab.d_uid, 0, sizeof(lab.d_uid));
 			error = writelabel(f, &lab);
-			if (error == 0) {
-				if (ioctl(f, DIOCGDINFO, &lab) < 0)
-					err(4, "ioctl DIOCGDINFO");
-				mpsave(&lab);
-			}
 		}
 		fclose(t);
 		break;
