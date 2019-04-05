3318648560a18bf9b9ff9600f1285b5072cb2ae8
tb <tb>
2019-04-05 20:23:38 +0000

    By design, our state machine is a DAG contrary to the state machine in the spec. To avoid the obvious loop in the RFC's state machine, we added a CLIENT_HELLO_RETRY state which is a second ClientHello with special rules.  There is, however, no state to react to this second client hello. This adds a matching SERVER_HELLO_RETRY state to the handshakes table.
    This means in particular that the WITH_HRR state cannot be set in
    tls13_server_hello_recv(), so remove this now dead check.
    
    ok jsing

--- lib/libssl/tls13_client.c
+++ lib/libssl/tls13_client.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: tls13_client.c,v 1.14 2019/03/25 17:21:18 jsing Exp $ */
+/* $OpenBSD: tls13_client.c,v 1.15 2019/04/04 16:53:57 jsing Exp $ */
 /*
  * Copyright (c) 2018, 2019 Joel Sing <jsing@openbsd.org>
  *
@@ -337,9 +337,6 @@ tls13_server_hello_recv(struct tls13_ctx *ctx)
 
 	/* See if we switched back to the legacy client method. */
 	if (s->method->internal->version < TLS1_3_VERSION)
-		return 1;
-
-	if (ctx->handshake_stage.hs_type & WITH_HRR)
 		return 1;
 
 	/* XXX - handle other key share types. */
