2b8ee1f2dcee2d12aa238b4b4c5ce7b188ece21b
gilles <gilles>
2019-04-08 07:44:45 +0000

    fix parsing of datalines before passing them to filters, the | splitting is done a bit too early
    diff from Martijn van Duren <openbsd+tech@list.imperialat.at>

--- usr.sbin/smtpd/lka_filter.c
+++ usr.sbin/smtpd/lka_filter.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: lka_filter.c,v 1.33 2018/12/26 15:55:09 eric Exp $	*/
+/*	$OpenBSD: lka_filter.c,v 1.34 2019/01/15 04:49:50 sunil Exp $	*/
 
 /*
  * Copyright (c) 2018 Gilles Chehade <gilles@poolp.org>
@@ -458,14 +458,15 @@ lka_filter_process_response(const char *name, const char *line)
 		return 0;
 
 	response = ep+1;
-	if ((ep = strchr(response, '|'))) {
-		parameter = ep + 1;
-		*ep = 0;
-	}
 
 	if (strcmp(kind, "filter-dataline") == 0) {
 		filter_data_next(token, reqid, response);
 		return 1;
+	}
+
+	if ((ep = strchr(response, '|'))) {
+		parameter = ep + 1;
+		*ep = 0;
 	}
 
 	if (strcmp(response, "proceed") != 0 &&
