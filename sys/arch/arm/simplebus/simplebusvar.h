3866e2b42cae20f296ad3d91910b6765069e44c2
kettenis <kettenis>
2019-04-16 13:16:06 +0000

    Prevent attaching drivers to devices for which we attached a driver early.
    ok patrick@, dlg@, visa@

--- sys/arch/arm/simplebus/simplebusvar.h
+++ sys/arch/arm/simplebus/simplebusvar.h
@@ -1,4 +1,4 @@
-/* $OpenBSD$ */
+/* $OpenBSD: simplebusvar.h,v 1.1 2016/10/21 20:09:49 patrick Exp $ */
 /*
  * Copyright (c) 2016 Patrick Wildt <patrick@blueri.se>
  *
@@ -28,6 +28,7 @@ struct simplebus_softc {
 	int			*sc_ranges;
 	int			 sc_rangeslen;
 	int			 sc_early;
+	int			 sc_early_nodes[64];
 };
 
 extern void simplebus_attach(struct device *, struct device *, void *);
