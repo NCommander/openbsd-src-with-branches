2fbe54a8fd9d44776c5bde9d612ca1e515cf5f03
jsing <jsing>
2019-04-20 11:13:15 +0000

    Avoid undefined behaviour that results from negating a signed long with minimum value.
    Fixes oss-fuzz #14354.
    
    ok beck@ bcook@ tb@

--- lib/libcrypto/asn1/x_long.c
+++ lib/libcrypto/asn1/x_long.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: x_long.c,v 1.14 2019/04/01 15:48:50 jsing Exp $ */
+/* $OpenBSD: x_long.c,v 1.15 2019/04/15 17:46:02 jsing Exp $ */
 /* Written by Dr Stephen N Henson (steve@openssl.org) for the OpenSSL
  * project 2000.
  */
@@ -142,7 +142,7 @@ long_i2c(ASN1_VALUE **pval, unsigned char *cont, int *putype,
 	 * octet is set.
 	 */
 	if (ltmp < 0)
-		utmp = -ltmp - 1;
+		utmp = -(ltmp + 1);
 	else
 		utmp = ltmp;
 	clen = BN_num_bits_word(utmp);
