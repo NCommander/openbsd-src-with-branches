93a9d664fb7574b362b5b61fd06f812f6340ad37
nicm <nicm>
2019-04-24 20:32:31 +0000

    Use bg not fg when adjusting for aixterm, from Ailin Nemui.

--- usr.bin/tmux/tty.c
+++ usr.bin/tmux/tty.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: tty.c,v 1.320 2019/04/17 14:43:49 nicm Exp $ */
+/* $OpenBSD: tty.c,v 1.321 2019/04/24 20:29:24 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -2361,7 +2361,7 @@ tty_check_bg(struct tty *tty, struct window_pane *wp, struct grid_cell *gc)
 			if (gc->bg & 8) {
 				gc->bg &= 7;
 				if (colours >= 16)
-					gc->fg += 90;
+					gc->bg += 90;
 			}
 		}
 		return;
