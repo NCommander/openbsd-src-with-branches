dd056b043d208009292b42cd05abd863e078ec95
rob <rob>
2019-04-27 04:28:57 +0000

    Fix comment regarding suspected problem when handling large negative integers.

--- regress/lib/libutil/ber/ber_test_int_i.c
+++ regress/lib/libutil/ber/ber_test_int_i.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ber_test_int_i.c,v 1.1 2019/04/27 03:01:39 rob Exp $
+/* $OpenBSD: ber_test_int_i.c,v 1.2 2019/04/27 03:36:19 rob Exp $
 */
 /*
  * Copyright (c) Rob Pierce <rob@openbsd.org>
@@ -36,7 +36,7 @@ struct test_vector {
 
 /*
  * ber_scanf_int failes on the negative boundary at 2^31.
- * va_arg() looks a little suspect.
+ * There may be a problem with the MSB processing in ber_read_element().
  */
 struct test_vector test_vectors[] = {
 	{
