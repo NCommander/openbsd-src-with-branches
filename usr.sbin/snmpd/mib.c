6e0be596a03e36f8ad7697a2f8f38f926274d9ba
gerhard <gerhard>
2019-05-02 14:04:10 +0000

    Index 0 is out of range for hrStorageIndex.
    ok reyk@

--- usr.sbin/snmpd/mib.c
+++ usr.sbin/snmpd/mib.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mib.c,v 1.91 2018/08/31 05:20:36 jsg Exp $	*/
+/*	$OpenBSD: mib.c,v 1.92 2018/12/10 13:35:54 landry Exp $	*/
 
 /*
  * Copyright (c) 2012 Joel Knight <joel@openbsd.org>
@@ -614,6 +614,8 @@ mib_hrstorage(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 
 	sop = &so[0];
 	switch (idx) {
+	case 0:
+		return (-1);
 	case 1:
 		descr = "Physical memory";
 		units = uvm.pagesize;
