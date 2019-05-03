2bc78cd6b844b59898b7bb395218d7b4821b56fe
tb <tb>
2019-05-03 17:16:27 +0000

    Add a "forwarded" log format that extends the "combined" log format by including the contents of the X-Forwarded-For and X-Forwarded-Port headers.  If httpd(8) runs behind a proxy like relayd(8), this allows tracking the origin of the requests.  The format is compatible with log analyzers such as GoAccess and Webalizer.
    Patch from Bruno Flueckiger
    
    ok benno, reyk

--- usr.sbin/httpd/httpd.h
+++ usr.sbin/httpd/httpd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: httpd.h,v 1.142 2018/10/11 09:52:22 benno Exp $	*/
+/*	$OpenBSD: httpd.h,v 1.143 2019/02/19 11:37:26 pirofti Exp $	*/
 
 /*
  * Copyright (c) 2006 - 2015 Reyk Floeter <reyk@openbsd.org>
@@ -437,7 +437,8 @@ SPLAY_HEAD(client_tree, client);
 enum log_format {
 	LOG_FORMAT_COMMON,
 	LOG_FORMAT_COMBINED,
-	LOG_FORMAT_CONNECTION
+	LOG_FORMAT_CONNECTION,
+	LOG_FORMAT_FORWARDED
 };
 
 struct log_file {
