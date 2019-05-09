894de35b1f9e02609594d47bbb064bf5c4b21b22
deraadt <deraadt>
2019-05-09 15:35:19 +0000

    Nope, the right byte layout is happening, but we still need to figure out a reported baddir panic.  Discussed with guenther tedu kettenis millert..

--- sys/ufs/ufs/ufs_lookup.c
+++ sys/ufs/ufs/ufs_lookup.c
@@ -671,7 +671,7 @@ ufs_makedirentry(struct inode *ip, struct componentname *cnp,
 #endif
 	newdirp->d_ino = ip->i_number;
 	newdirp->d_namlen = cnp->cn_namelen;
-	memset(newdirp->d_name + ((cnp->cn_namelen+1) & ~(DIR_ROUNDUP-1)),
+	memset(newdirp->d_name + (cnp->cn_namelen & ~(DIR_ROUNDUP-1)),
 	    0, DIR_ROUNDUP);
 	memcpy(newdirp->d_name, cnp->cn_nameptr, cnp->cn_namelen);
 	if (OFSFMT(ip)) {
