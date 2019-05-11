05d9a3eb2e30d19f666bcbed901a11781ec0ce6b
mpi <mpi>
2019-05-11 23:36:40 +0000

    Correct a check in if_isconnected().
    Fix a regression introduced by the bridge(4) refactoring.
    
    Found by and ok bluhm@

--- sys/net/if.c
+++ sys/net/if.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if.c,v 1.580 2019/04/22 03:26:16 dlg Exp $	*/
+/*	$OpenBSD: if.c,v 1.581 2019/04/28 22:15:57 mpi Exp $	*/
 /*	$NetBSD: if.c,v 1.35 1996/05/07 05:26:04 thorpej Exp $	*/
 
 /*
@@ -1192,7 +1192,7 @@ if_isconnected(const struct ifnet *ifp0, unsigned int ifidx)
 		connected = 1;
 
 #if NBRIDGE > 0
-	if (ifp0->if_bridgeidx == ifp->if_bridgeidx)
+	if (ifp0->if_bridgeidx != 0 && ifp0->if_bridgeidx == ifp->if_bridgeidx)
 		connected = 1;
 #endif
 #if NCARP > 0
