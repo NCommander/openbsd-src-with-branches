ae2e9bbcb69b788b6863a889fea9f9d8844db7fb
mpi <mpi>
2019-05-25 18:11:10 +0000

    Do not account spinning time as running time when a thread crosses a tick boundary of schedlock().
    This reduces the contention on the SCHED_LOCK() when the current thread
    is already spinning.
    
    Prompted by deraadt@, ok visa@

--- sys/kern/sched_bsd.c
+++ sys/kern/sched_bsd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sched_bsd.c,v 1.49 2019/01/28 11:48:13 mpi Exp $	*/
+/*	$OpenBSD: sched_bsd.c,v 1.50 2019/02/26 14:24:21 visa Exp $	*/
 /*	$NetBSD: kern_synch.c,v 1.37 1996/04/22 01:38:37 christos Exp $	*/
 
 /*-
@@ -542,7 +542,7 @@ schedclock(struct proc *p)
 	struct schedstate_percpu *spc = &ci->ci_schedstate;
 	int s;
 
-	if (p == spc->spc_idleproc)
+	if (p == spc->spc_idleproc || spc->spc_spinning)
 		return;
 
 	SCHED_LOCK(s);
