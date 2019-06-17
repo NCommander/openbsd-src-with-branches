2f40573cdb5cd28f495bf939615dc819139fa117
tedu <tedu>
2019-06-17 19:51:23 +0000

    setusercontext resets PATH (which we want). but then it becomes impossible to access the old PATH. save a copy in case we need it later. bug report from espie.

--- usr.bin/doas/doas.h
+++ usr.bin/doas/doas.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: doas.h,v 1.13 2017/04/06 21:12:06 tedu Exp $ */
+/* $OpenBSD: doas.h,v 1.14 2019/06/16 18:16:34 tedu Exp $ */
 /*
  * Copyright (c) 2015 Ted Unangst <tedu@openbsd.org>
  *
@@ -28,6 +28,8 @@ struct rule {
 extern struct rule **rules;
 extern int nrules;
 extern int parse_errors;
+
+extern const char *formerpath;
 
 struct passwd;
 
