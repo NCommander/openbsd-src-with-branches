a2d6f8e6964229a63d382fa4a5af6e60eca2c83d
claudio <claudio>
2019-06-20 15:26:49 +0000

    Don't use assert to validate user input. assert() is not for that, instead check and error out like it is done one other parsing issues. Agreed by deraadt@ Fixes another afl "crash" found by jsg@

--- usr.sbin/rpki-client/mft.c
+++ usr.sbin/rpki-client/mft.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mft.c,v 1.3 2019/06/19 04:21:43 deraadt Exp $ */
+/*	$OpenBSD: mft.c,v 1.4 2019/06/19 16:30:37 deraadt Exp $ */
 /*
  * Copyright (c) 2019 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -401,7 +401,7 @@ mft_parse(X509 **x509, const char *fn, int force)
 		free(p.res->files);
 		p.res->filesz = 0;
 		p.res->files = NULL;
-	} else if (c < 0)
+	} else if (c == -1)
 		goto out;
 
 	rc = 1;
