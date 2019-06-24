eec679e678db3de87df3ae60f098dd06071ee0ca
visa <visa>
2019-06-24 12:33:36 +0000

    Fix test after kernel interface change.
    From Moritz Buhl

--- regress/sys/net/rtable/fullfeed/main.c
+++ regress/sys/net/rtable/fullfeed/main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: main.c,v 1.2 2015/12/03 15:15:04 mpi Exp $ */
+/*	$OpenBSD: main.c,v 1.3 2016/11/15 10:43:41 mpi Exp $ */
 
 /*
  * Copyright (c) 2015 Martin Pieuchot
@@ -53,7 +53,7 @@ main(int argc, char *argv[])
 	do_from_file(0, af, filename, route_insert);
 	do_from_file(0, af, filename, route_lookup);
 
-	rtable_walk(0, af, rtentry_dump, NULL);
+	rtable_walk(0, af, NULL, rtentry_dump, NULL);
 
 	do_from_file(0, af, filename, route_delete);
 
