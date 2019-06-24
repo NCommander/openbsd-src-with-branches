f39e3af022d7fbb185f8a5ebc2164f6b8ca062c1
kn <kn>
2019-06-24 21:36:53 +0000

    Use timeout_add_sec(9)
    Re-challenge timeouts are made up of single scalar factors which are
    multiplied with the time unit lcp.timeout to compute the timeout period.
    
    Simply reduce that unit of 1 * hz [ticks] to 1 [s] and use the appropiate
    API.
    
    OK mpi

--- sys/net/if_sppp.h
+++ sys/net/if_sppp.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_sppp.h,v 1.25 2016/06/14 20:44:43 sthen Exp $	*/
+/*	$OpenBSD: if_sppp.h,v 1.26 2017/01/24 10:08:30 krw Exp $	*/
 /*	$NetBSD: if_sppp.h,v 1.2.2.1 1999/04/04 06:57:39 explorer Exp $	*/
 
 /*
@@ -110,7 +110,7 @@ struct slcp {
 	u_long	protos;		/* bitmask of protos that are started */
 	u_char  echoid;         /* id of last keepalive echo request */
 	/* restart max values, see RFC 1661 */
-	int	timeout;
+	int	timeout;	/* seconds */
 	int	max_terminate;
 	int	max_configure;
 	int	max_failure;
