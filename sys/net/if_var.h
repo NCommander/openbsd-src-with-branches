d429ad94ab1250c87c254fe4f5b781a608f5f10f
claudio <claudio>
2019-06-26 09:36:06 +0000

    Create IF_WWAN_DEFAULT_PRIORITY which is lower than IF_WIRELESS_DEFAULT_PRIORITY and use it in umb(4) as default prio. OK kettenis@, sthen@

--- sys/net/if_var.h
+++ sys/net/if_var.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_var.h,v 1.98 2019/04/22 03:26:16 dlg Exp $	*/
+/*	$OpenBSD: if_var.h,v 1.99 2019/04/28 22:15:57 mpi Exp $	*/
 /*	$NetBSD: if.h,v 1.23 1996/05/07 02:40:27 thorpej Exp $	*/
 
 /*
@@ -310,6 +310,7 @@ do {									\
 /* default interface priorities */
 #define IF_WIRED_DEFAULT_PRIORITY	0
 #define IF_WIRELESS_DEFAULT_PRIORITY	4
+#define IF_WWAN_DEFAULT_PRIORITY	6
 #define IF_CARP_DEFAULT_PRIORITY	15
 
 /*
