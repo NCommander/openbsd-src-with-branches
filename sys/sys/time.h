a6706b8675653069e7154aed9de1b30d0194e3cb
cheloha <cheloha>
2019-07-03 22:39:33 +0000

    Add tsleep_nsec(9), msleep_nsec(9), and rwsleep_nsec(9).
    Equivalent to their unsuffixed counterparts except that (a) they take
    a timeout in terms of nanoseconds, and (b) INFSLP, aka UINT64_MAX (not
    zero) indicates that a timeout should not be set.
    
    For now, zero nanoseconds is not a strictly valid invocation: we log a
    warning on DIAGNOSTIC kernels if we see such a call.  We still sleep
    until the next tick in such a case, however.  In the future this could
    become some sort of poll... TBD.
    
    To facilitate conversions to these interfaces: add inline conversion
    functions to sys/time.h for turning your timeout into nanoseconds.
    
    Also do a few easy conversions for warmup and to demonstrate how
    further conversions should be done.
    
    Lots of input from mpi@ and ratchov@.  Additional input from tedu@,
    deraadt@, mortimer@, millert@, and claudio@.
    
    Partly inspired by FreeBSD r247787.
    
    positive feedback from deraadt@, ok mpi@

--- sys/sys/time.h
+++ sys/sys/time.h
@@ -340,10 +340,36 @@ NSEC_TO_TIMEVAL(uint64_t ns, struct timeval *tv)
 }
 
 static __inline void
-NSEC_TO_TIMESPEC(uint64_t ns, struct timespec *tv)
+NSEC_TO_TIMESPEC(uint64_t ns, struct timespec *ts)
 {
-	tv->tv_sec = ns / 1000000000L;
-	tv->tv_nsec = ns % 1000000000L;
+	ts->tv_sec = ns / 1000000000L;
+	ts->tv_nsec = ns % 1000000000L;
+}
+
+#include <sys/stdint.h>
+
+static __inline uint64_t
+SEC_TO_NSEC(uint64_t seconds)
+{
+	if (seconds > UINT64_MAX / 1000000000ULL)
+		return UINT64_MAX;
+	return seconds * 1000000000ULL;
+}
+
+static __inline uint64_t
+MSEC_TO_NSEC(uint64_t milliseconds)
+{
+	if (milliseconds > UINT64_MAX / 1000000ULL)
+		return UINT64_MAX;
+	return milliseconds * 1000000ULL;
+}
+
+static __inline uint64_t
+USEC_TO_NSEC(uint64_t microseconds)
+{
+	if (microseconds > UINT64_MAX / 1000ULL)
+		return UINT64_MAX;
+	return microseconds * 1000ULL;
 }
 
 #else /* !_KERNEL */
