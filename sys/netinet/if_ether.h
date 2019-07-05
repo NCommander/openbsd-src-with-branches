af408a6265dd5733bf8ac0f5bdf14603f2115804
dlg <dlg>
2019-07-05 01:23:22 +0000

    add ac_trunkport to arpcom so trunks can coordinate owning an interface
    Ethernet interfaces can be used by trunk(4), and i'm about to commit
    a new aggr(4) driver which should not be able to use an interface
    while trunk owns it and visa versa.

--- sys/netinet/if_ether.h
+++ sys/netinet/if_ether.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ether.h,v 1.73 2016/11/29 10:09:57 reyk Exp $	*/
+/*	$OpenBSD: if_ether.h,v 1.74 2018/12/11 01:27:08 dlg Exp $	*/
 /*	$NetBSD: if_ether.h,v 1.22 1996/05/11 13:00:00 mycroft Exp $	*/
 
 /*
@@ -205,6 +205,7 @@ struct	arpcom {
 	int	 ac_multicnt;			/* length of ac_multiaddrs */
 	int	 ac_multirangecnt;		/* number of mcast ranges */
 
+	void	*ac_trunkport;
 };
 
 extern int arpt_keep;				/* arp resolved cache expire */
