1baba4c09f5765ec60d054e59077753d952a0ef7
guenther <guenther>
2019-07-14 05:08:26 +0000

    Delete obsolete bits in a comment: mcontext is gone and struct reg is no longer required to be layout compatible with struct trapframe
    noted by Benjamin Baier (programmer (at) netzbasis.de)

--- sys/arch/amd64/include/reg.h
+++ sys/arch/amd64/include/reg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: reg.h,v 1.5 2009/12/25 20:54:48 miod Exp $	*/
+/*	$OpenBSD: reg.h,v 1.6 2011/03/23 16:54:34 pirofti Exp $	*/
 /*	$NetBSD: reg.h,v 1.1 2003/04/26 18:39:47 fvdl Exp $	*/
 
 /*-
@@ -75,8 +75,6 @@
 
 /*
  * Registers accessible to ptrace(2) syscall for debugger use.
- * Same as mcontext.__gregs and struct trapframe, they must
- * remain synced (XXX should use common structure).
  */
 struct reg {
 	int64_t	r_rdi;
