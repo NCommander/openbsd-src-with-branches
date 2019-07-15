de36d5482deafa2b010d60c071d89dc2c24457da
bluhm <bluhm>
2019-07-15 12:40:42 +0000

    Initialize struct inpcb pool not on demand, but during initialization. Removes a global variable and avoids MP problems. OK mpi@ visa@

--- sys/netinet/in_proto.c
+++ sys/netinet/in_proto.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: in_proto.c,v 1.91 2018/11/19 10:15:04 claudio Exp $	*/
+/*	$OpenBSD: in_proto.c,v 1.92 2019/06/13 08:12:11 claudio Exp $	*/
 /*	$NetBSD: in_proto.c,v 1.14 1996/02/18 18:58:32 christos Exp $	*/
 
 /*
@@ -441,6 +441,7 @@ const struct protosw inetsw[] = {
 struct domain inetdomain = {
   .dom_family = AF_INET,
   .dom_name = "internet",
+  .dom_init = in_init,
   .dom_protosw = inetsw,
   .dom_protoswNPROTOSW = &inetsw[nitems(inetsw)],
   .dom_sasize = sizeof(struct sockaddr_in),
