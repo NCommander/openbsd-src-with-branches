9f331d19832d9b7a55a8fbe184ac80c4870b3d28
lum <lum>
2019-07-17 18:18:37 +0000

    Add two dobeep functions to make calls to the system bell require fewer lines. While working in extend.c change relevant calls to dobeep.

--- usr.bin/mg/def.h
+++ usr.bin/mg/def.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: def.h,v 1.161 2019/07/02 16:25:39 lum Exp $	*/
+/*	$OpenBSD: def.h,v 1.162 2019/07/03 18:11:07 lum Exp $	*/
 
 /* This file is in the public domain. */
 
@@ -711,6 +711,8 @@ int		 compile(int, int);
 void		 bellinit(void);
 int		 toggleaudiblebell(int, int);
 int		 togglevisiblebell(int, int);
+int		 dobeep_msgs(const char *, const char *);
+int		 dobeep_msg(const char *);
 void		 dobeep(void);
 
 /*
