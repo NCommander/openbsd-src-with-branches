d108bb2299284765cc9c5dd814019f29eea13023
krw <krw>
2019-07-22 17:20:06 +0000

    Cleanup/simplify parse_number():
    1) Restructure to explicitly set a long long result and take the
    desired low/high constraints for strtonum().
    
    2) No need to use a temporary variable when setting long long
    (a.k.a. time_t) fields.
    
    3) Fewer magic numbers when processing integer valued option
    data. Less memcpy()'ing where a cast or two is sufficient to make
    integer assignment work.
    
    No intentional functional change.

--- sbin/dhclient/dhcpd.h
+++ sbin/dhclient/dhcpd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dhcpd.h,v 1.279 2019/07/19 20:50:22 krw Exp $	*/
+/*	$OpenBSD: dhcpd.h,v 1.280 2019/07/21 15:47:02 krw Exp $	*/
 
 /*
  * Copyright (c) 2004 Henning Brauer <henning@openbsd.org>
@@ -182,7 +182,7 @@ int		 parse_semi(FILE *);
 int		 parse_string(FILE *, char **);
 int		 parse_ip_addr(FILE *, struct in_addr *);
 int		 parse_cidr(FILE *, unsigned char *);
-int		 parse_number(FILE *, unsigned char *, char);
+int		 parse_number(FILE *, long long *, long long, long long);
 int		 parse_boolean(FILE *, unsigned char *);
 void		 parse_warn(char *);
 
