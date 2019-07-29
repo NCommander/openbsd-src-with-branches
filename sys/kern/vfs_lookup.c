6dfb7953bfabe6ce212e73ccc19823ea72e7c83d
bluhm <bluhm>
2019-07-29 12:35:19 +0000

    When setting immutable flags on directories with chflags(1), calling unveil(2) on a nonexisting file failed.  Ignore files that cannot be created due to permissions. OK deraadt@

--- sys/kern/vfs_lookup.c
+++ sys/kern/vfs_lookup.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vfs_lookup.c,v 1.80 2019/07/18 18:06:17 kn Exp $	*/
+/*	$OpenBSD: vfs_lookup.c,v 1.81 2019/07/27 21:15:35 bluhm Exp $	*/
 /*	$NetBSD: vfs_lookup.c,v 1.17 1996/02/09 19:00:59 christos Exp $	*/
 
 /*
@@ -577,7 +577,7 @@ dirloop:
 		 * create ourselves.
 		 */
 		if (ndp->ni_pledge == PLEDGE_UNVEIL &&
-		    (error == EACCES || error == EROFS))
+		    (error == EPERM || error == EACCES || error == EROFS))
 			error = EJUSTRETURN;
 
 		if (error != EJUSTRETURN)
