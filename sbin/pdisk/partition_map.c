2eccc62b85d97d4ac1eb19e0a51633a4792c61ee
krw <krw>
2019-07-31 00:14:10 +0000

    Allow sbBlkCount to be less than the number of physical sectors on the disk.
    Fixes dealing with MacOS 9.2.2 formatted disk, allowing dual boot
    again.
    
    Problem reported and fix tested by Donovan Watteau on his B&W
    G3. Thanks!

--- sbin/pdisk/partition_map.c
+++ sbin/pdisk/partition_map.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: partition_map.c,v 1.97 2016/01/31 23:00:11 krw Exp $	*/
+/*	$OpenBSD: partition_map.c,v 1.98 2016/02/01 12:53:37 krw Exp $	*/
 
 /*
  * partition_map.c - partition map routines
@@ -87,7 +87,7 @@ open_partition_map(int fd, char *name, uint64_t mediasz, uint32_t sectorsz)
 	}
 	if (map->sbSig == BLOCK0_SIGNATURE &&
 	    map->sbBlkSize == sectorsz &&
-	    map->sbBlkCount == mediasz) {
+	    map->sbBlkCount <= mediasz) {
 		if (read_partition_map(map) == 0)
 			return map;
 	} else {
@@ -98,8 +98,8 @@ open_partition_map(int fd, char *name, uint64_t mediasz, uint32_t sectorsz)
 		else if (map->sbBlkSize != sectorsz)
 			warnx("Block 0 sbBlkSize (%u) != sector size (%u)",
 			    map->sbBlkSize, sectorsz);
-		else if (map->sbBlkCount != mediasz)
-			warnx("Block 0 sbBlkCount (%u) != media size (%llu)",
+		else if (map->sbBlkCount > mediasz)
+			warnx("Block 0 sbBlkCount (%u) > media size (%llu)",
 			    map->sbBlkCount,
 			    (unsigned long long)mediasz);
 	}
