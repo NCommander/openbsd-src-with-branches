c90522371eb45a8c8ca60a6c7fdba93fea0739cb
cheloha <cheloha>
2019-08-02 02:17:35 +0000

    per-process itimers: itimerval -> itimerspec
    Loongson runs at 128hz.  128 doesn't divide evenly into a million,
    but it does divide evenly into a billion.  So if we do the per-process
    itimer bookkeeping with itimerspec structs we can have error-free
    virtual itimers on loongson just as we do on most other platforms.
    
    This change doesn't fix the virtual itimer error alpha, as 1024 does not
    divide evenly into a billion.  But this doesn't make the situation any
    worse, either.
    
    ok deraadt@

--- sys/sys/kernel.h
+++ sys/sys/kernel.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kernel.h,v 1.18 2019/01/19 01:53:44 cheloha Exp $	*/
+/*	$OpenBSD: kernel.h,v 1.19 2019/03/10 21:16:15 cheloha Exp $	*/
 /*	$NetBSD: kernel.h,v 1.11 1995/03/03 01:24:16 cgd Exp $	*/
 
 /*-
@@ -50,6 +50,7 @@ extern int domainnamelen;
 extern struct timezone tz;			/* XXX */
 
 extern int tick;		/* usec per tick (1000000 / hz) */
+extern int tick_nsec;		/* nsec per tick */
 extern int tickfix;		/* periodic tick adj. tick not integral */
 extern int tickfixinterval;	/* interval at which to apply adjustment */
 extern int tickadj;		/* "standard" clock skew, us./tick */
