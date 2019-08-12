372122bf55df3b94707e3c054be62c459ef81908
kettenis <kettenis>
2019-08-12 19:17:35 +0000

    Make it possible to switch to framebuffer "glass" console in case it isn't the default already.
    ok patrick@

--- sys/arch/arm64/stand/efiboot/efiboot.h
+++ sys/arch/arm64/stand/efiboot/efiboot.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: efiboot.h,v 1.2 2018/01/21 21:35:34 patrick Exp $	*/
+/*	$OpenBSD: efiboot.h,v 1.3 2018/06/25 22:39:14 kettenis Exp $	*/
 
 /*
  * Copyright (c) 2015 YASUOKA Masahiko <yasuoka@yasuoka.net>
@@ -25,3 +25,7 @@ void	efi_cons_probe(struct consdev *);
 void	efi_cons_init(struct consdev *);
 int	efi_cons_getc(dev_t);
 void	efi_cons_putc(dev_t, int);
+void	efi_fb_probe(struct consdev *);
+void	efi_fb_init(struct consdev *);
+int	efi_fb_getc(dev_t);
+void	efi_fb_putc(dev_t, int);
