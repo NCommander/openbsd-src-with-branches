3284a91ed5b0e2812652929dff3c506c1eb9d5b9
anton <anton>
2019-08-14 07:34:49 +0000

    Improve the error message when supplying an invalid template to vmctl start. Favoring 'invalid template' over 'permission denied' should give the user a better hint on what went wrong.
    ok kn@ mlarkin@

--- usr.sbin/vmd/vmd.c
+++ usr.sbin/vmd/vmd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vmd.c,v 1.113 2019/05/20 17:04:24 jasper Exp $	*/
+/*	$OpenBSD: vmd.c,v 1.114 2019/06/28 13:32:51 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2015 Reyk Floeter <reyk@openbsd.org>
@@ -1373,8 +1373,13 @@ vm_instance(struct privsep *ps, struct vmd_vm **vm_parent,
 
 	/* return without error if the parent is NULL (nothing to inherit) */
 	if ((vmc->vmc_flags & VMOP_CREATE_INSTANCE) == 0 ||
-	    (*vm_parent = vm_getbyname(vmc->vmc_instance)) == NULL)
+	    vmc->vmc_instance[0] == '\0')
 		return (0);
+
+	if ((*vm_parent = vm_getbyname(vmc->vmc_instance)) == NULL) {
+		errno = VMD_PARENT_INVALID;
+		return (-1);
+	}
 
 	errno = 0;
 	vmcp = &(*vm_parent)->vm_params;
--- usr.sbin/vmd/vmd.h
+++ usr.sbin/vmd/vmd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vmd.h,v 1.94 2019/05/11 23:07:46 jasper Exp $	*/
+/*	$OpenBSD: vmd.h,v 1.95 2019/07/17 05:51:07 pd Exp $	*/
 
 /*
  * Copyright (c) 2015 Mike Larkin <mlarkin@openbsd.org>
@@ -72,6 +72,7 @@
 #define VMD_VM_STOP_INVALID	1004
 #define VMD_CDROM_MISSING	1005
 #define VMD_CDROM_INVALID	1006
+#define VMD_PARENT_INVALID	1007
 
 /* Image file signatures */
 #define VM_MAGIC_QCOW		"QFI\xfb"
