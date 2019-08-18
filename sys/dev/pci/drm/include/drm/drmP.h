33866cc64ffba46f3f39859111500e3d476fccd9
kettenis <kettenis>
2019-08-18 13:11:47 +0000

    Implement a few Linux compat ACPI interfaces and enable the ACPI support code in radeon(4) and amdgpu(4).
    ok jsg@

--- sys/dev/pci/drm/include/drm/drmP.h
+++ sys/dev/pci/drm/include/drm/drmP.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: drmP.h,v 1.3 2019/07/25 05:48:16 jsg Exp $ */
+/* $OpenBSD: drmP.h,v 1.4 2019/08/14 05:32:56 jsg Exp $ */
 /* drmP.h -- Private header for Direct Rendering Manager -*- linux-c -*-
  * Created: Mon Jan  4 10:05:05 1999 by faith@precisioninsight.com
  */
@@ -242,6 +242,7 @@ struct drm_attach_args {
 #define DRMDEVCF_PRIMARY_UNK	-1
 
 void	drm_linux_init(void);
+int	drm_linux_acpi_notify(struct aml_node *, int, void *);
 
 /* Device setup support (drm_drv.c) */
 int	drm_pciprobe(struct pci_attach_args *, const struct drm_pcidev * );
