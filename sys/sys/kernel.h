a82222715370aec8e6b22771c904771c53799307
cheloha <cheloha>
2019-08-21 20:44:09 +0000

    sysctl(2): add kern.utc_offset: successor to the DST/TIMEZONE options(4)
    The DST and TIMEZONE options(4) are incompatible with KARL, so we need
    some other way to compensate for an RTC running with a known offset.
    
    Enter kern.utc_offset, an offset in minutes East of UTC.  TIMEZONE has
    always been minutes West, but this is inconsistent with how everyone
    else talks about timezones, hence the flip.
    
    TIMEZONE has the advantage of being compiled into the binary.  Our new
    sysctl(2) has no such luck, so it needs to be set as early as possible
    in boot, from sysctl.conf(5), so we can correct the kernel clock from
    the RTC's local time to UTC before daemons like ntpd(8) and cron(8)
    start.  To encourage this, kern.utc_offset is made immutable after the
    securelevel(7) is raised to 1.
    
    Prompted by yasuoka@.  Discussed with deraadt@, kettenis@, yasuoka@.
    Additional testing by yasuoka@.
    
    ok deraadt@, yasuoka@

--- sys/sys/kernel.h
+++ sys/sys/kernel.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kernel.h,v 1.19 2019/03/10 21:16:15 cheloha Exp $	*/
+/*	$OpenBSD: kernel.h,v 1.20 2019/08/02 02:17:35 cheloha Exp $	*/
 /*	$NetBSD: kernel.h,v 1.11 1995/03/03 01:24:16 cgd Exp $	*/
 
 /*-
@@ -48,6 +48,7 @@ extern int domainnamelen;
 
 /* 1.2 */
 extern struct timezone tz;			/* XXX */
+extern int utc_offset;		/* seconds east of UTC */
 
 extern int tick;		/* usec per tick (1000000 / hz) */
 extern int tick_nsec;		/* nsec per tick */
