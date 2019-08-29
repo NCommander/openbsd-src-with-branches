9762a5d4da76ff711f499c003ec0140674fe112e
kettenis <kettenis>
2019-08-29 14:19:27 +0000

    Capitalize my last name in copyright.

--- sys/arch/armv7/exynos/exdwusb.c
+++ sys/arch/armv7/exynos/exdwusb.c
@@ -1,6 +1,6 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: exdwusb.c,v 1.1 2017/03/10 12:05:28 kettenis Exp $	*/
 /*
- * Copyright (c) 2017 Mark kettenis <kettenis@openbsd.org>
+ * Copyright (c) 2017 Mark Kettenis <kettenis@openbsd.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
