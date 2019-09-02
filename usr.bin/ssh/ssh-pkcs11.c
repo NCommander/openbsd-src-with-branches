2af93ca683c36f74a5a9b2f3fe5890c7a64e049f
djm <djm>
2019-09-02 00:19:25 +0000

    downgrade PKCS#11 "provider returned no slots" warning from log level error to debug. This is common when attempting to enumerate keys on smartcard readers with no cards plugged in. bz#3058 ok dtucker@

--- usr.bin/ssh/ssh-pkcs11.c
+++ usr.bin/ssh/ssh-pkcs11.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssh-pkcs11.c,v 1.42 2019/02/04 23:37:54 djm Exp $ */
+/* $OpenBSD: ssh-pkcs11.c,v 1.43 2019/03/08 17:24:43 markus Exp $ */
 /*
  * Copyright (c) 2010 Markus Friedl.  All rights reserved.
  * Copyright (c) 2014 Pedro Martelletto. All rights reserved.
@@ -1498,7 +1498,7 @@ pkcs11_register_provider(char *provider_id, char *pin, struct sshkey ***keyp,
 		goto fail;
 	}
 	if (p->nslots == 0) {
-		error("%s: provider %s returned no slots", __func__,
+		debug("%s: provider %s returned no slots", __func__,
 		    provider_id);
 		ret = -SSH_PKCS11_ERR_NO_SLOTS;
 		goto fail;
