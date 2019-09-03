79b0842d7ac5fd6ee18fde5b59083d71b07eaeef
djm <djm>
2019-09-03 08:37:06 +0000

    only add plain keys to prevent any certs laying around from confusing the test.

--- regress/usr.bin/ssh/agent-timeout.sh
+++ regress/usr.bin/ssh/agent-timeout.sh
@@ -1,4 +1,4 @@
-#	$OpenBSD: agent-timeout.sh,v 1.3 2015/03/03 22:35:19 markus Exp $
+#	$OpenBSD: agent-timeout.sh,v 1.4 2019/01/28 00:08:26 dtucker Exp $
 #	Placed in the Public Domain.
 
 tid="agent timeout test"
@@ -14,7 +14,7 @@ else
 	trace "add keys with timeout"
 	keys=0
 	for t in ${SSH_KEYTYPES}; do
-		${SSHADD} -t ${SSHAGENT_TIMEOUT} $OBJ/$t > /dev/null 2>&1
+		${SSHADD} -kt ${SSHAGENT_TIMEOUT} $OBJ/$t > /dev/null 2>&1
 		if [ $? -ne 0 ]; then
 			fail "ssh-add did succeed exit code 0"
 		fi
