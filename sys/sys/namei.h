940ba1f9bd5cd0177ed1f52d9d6958e0b3ec8c92
beck <beck>
2019-09-11 15:01:40 +0000

    Fix unveil for relative lookups in the non AT_FDCWD case
    Issue spotted by Benjamin Baier <programmer@netzbasis.de> with relative
    path lookups from openat(2). Lacking a current working directory unveil,
    operations using the *at functions could fail when not crossing an unveil,
    since an initial covering unveil was not found.
    
    This corrects this so we walk up from the directory vnode to find a
    covering unveil at the start of non AT_FDCWD lookups, and adds regress
    for this case.
    
    ok bluhm@

--- sys/sys/namei.h
+++ sys/sys/namei.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: namei.h,v 1.41 2019/07/27 21:15:36 bluhm Exp $	*/
+/*	$OpenBSD: namei.h,v 1.42 2019/08/02 08:12:35 bluhm Exp $	*/
 /*	$NetBSD: namei.h,v 1.11 1996/02/09 18:25:20 christos Exp $	*/
 
 /*
@@ -209,7 +209,7 @@ void unveil_removevnode(struct vnode *);
 void unveil_free_traversed_vnodes(struct nameidata *);
 ssize_t unveil_find_cover(struct vnode *, struct proc *);
 struct unveil *unveil_lookup(struct vnode *, struct proc *, ssize_t *);
-void unveil_start_relative(struct proc *, struct nameidata *);
+void unveil_start_relative(struct proc *, struct nameidata *, struct vnode *);
 void unveil_check_component(struct proc *, struct nameidata *, struct vnode *);
 int unveil_check_final(struct proc *, struct nameidata *);
 
