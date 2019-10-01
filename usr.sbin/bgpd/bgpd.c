e437edf9808da0ebc6b2c1c67c9c8c5df4bb22c9
claudio <claudio>
2019-10-01 08:57:48 +0000

    For portable kr_init() returns an fd of -1 which now would end up in an immediate exit of bgpd. Instead pass the fd via pointer arg. OK benno@

--- usr.sbin/bgpd/bgpd.c
+++ usr.sbin/bgpd/bgpd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bgpd.c,v 1.224 2019/08/05 08:46:55 claudio Exp $ */
+/*	$OpenBSD: bgpd.c,v 1.225 2019/08/08 11:33:08 claudio Exp $ */
 
 /*
  * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
@@ -234,7 +234,7 @@ main(int argc, char *argv[])
 	imsg_init(ibuf_se, pipe_m2s[0]);
 	imsg_init(ibuf_rde, pipe_m2r[0]);
 	mrt_init(ibuf_rde, ibuf_se);
-	if ((rfd = kr_init()) == -1)
+	if (kr_init(&rfd) == -1)
 		quit = 1;
 	keyfd = pfkey_init();
 
