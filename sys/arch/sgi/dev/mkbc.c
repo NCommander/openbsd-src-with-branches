b9b68ea5c5c732ebab0fd74fd2c987f5fce619dd
cheloha <cheloha>
2019-10-17 13:42:15 +0000

    sgi: iockbc(4), mkbc(4): tsleep(9) -> tsleep_nsec(9); ok kn@ visa@

--- sys/arch/sgi/dev/mkbc.c
+++ sys/arch/sgi/dev/mkbc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mkbc.c,v 1.13 2015/02/11 07:05:39 dlg Exp $  */
+/*	$OpenBSD: mkbc.c,v 1.14 2018/12/03 13:46:30 visa Exp $  */
 
 /*
  * Copyright (c) 2006, 2007, Joel Sing
@@ -747,7 +747,7 @@ pckbc_enqueue_cmd(pckbc_tag_t self, pckbc_slot_t slot, u_char *cmd, int len,
 	if (q->polling)
 		res = (sync ? nc->status : 0);
 	else if (sync) {
-		if ((res = tsleep(nc, 0, "kbccmd", 1*hz))) {
+		if ((res = tsleep_nsec(nc, 0, "kbccmd", SEC_TO_NSEC(1)))) {
 			TAILQ_REMOVE(&q->cmdqueue, nc, next);
 			mkbc_cleanup(t);
 		} else
