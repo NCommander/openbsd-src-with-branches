3eebc65e03a6554387a19adb36df91b28ad79926
florian <florian>
2019-11-12 15:34:37 +0000

    It is possible to cancel libunbound queries by tracking an id.     We are not using that feature so stop tracking the value.

--- sbin/unwind/unwind.h
+++ sbin/unwind/unwind.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: unwind.h,v 1.24 2019/11/09 16:28:10 florian Exp $	*/
+/*	$OpenBSD: unwind.h,v 1.25 2019/11/11 05:51:06 florian Exp $	*/
 
 /*
  * Copyright (c) 2018 Florian Obser <florian@openbsd.org>
@@ -153,7 +153,6 @@ struct query_imsg {
 	int		 c;
 	int		 err;
 	int		 bogus;
-	int		 async_id;
 	void		*resolver;
 	struct timespec	 tp;
 };
