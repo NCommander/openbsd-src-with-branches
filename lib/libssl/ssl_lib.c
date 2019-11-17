1580f09ede385b7c8c5ff7f02e91bf0fcc6dc518
jsing <jsing>
2019-11-17 19:07:07 +0000

    Bring back the ssl_shutdown internal method pointer.
    For now ssl3_shutdown() is called in all cases, however TLSv1.3 will soon
    get its own version.
    
    ok beck@

--- lib/libssl/ssl_lib.c
+++ lib/libssl/ssl_lib.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssl_lib.c,v 1.205 2019/05/15 09:13:16 bcook Exp $ */
+/* $OpenBSD: ssl_lib.c,v 1.206 2019/11/04 19:17:28 otto Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -1012,7 +1012,7 @@ SSL_shutdown(SSL *s)
 	}
 
 	if (s != NULL && !SSL_in_init(s))
-		return (ssl3_shutdown(s));
+		return (s->method->internal->ssl_shutdown(s));
 
 	return (1);
 }
