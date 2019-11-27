07aa310fa5f843381808337d4cbc59e94888df75
ratchov <ratchov>
2019-11-27 08:18:22 +0000

    Initialize nfds fields of new file structures.
    Fixes a possible crash when a new file structure is added in the
    time-out processing code-path.

--- usr.bin/sndiod/file.c
+++ usr.bin/sndiod/file.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: file.c,v 1.23 2016/10/27 04:37:47 ratchov Exp $	*/
+/*	$OpenBSD: file.c,v 1.24 2019/06/28 13:35:03 deraadt Exp $	*/
 /*
  * Copyright (c) 2008-2012 Alexandre Ratchov <alex@caoua.org>
  *
@@ -234,6 +234,7 @@ file_new(struct fileops *ops, void *arg, char *name, unsigned int nfds)
 	}
 	f = xmalloc(sizeof(struct file));
 	f->max_nfds = nfds;
+	f->nfds = 0;
 	f->ops = ops;
 	f->arg = arg;
 	f->name = name;
