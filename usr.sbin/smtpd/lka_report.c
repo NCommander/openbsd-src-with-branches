c709263ae45be5252afb159ad29ec79aaecdb389
gilles <gilles>
2019-12-09 20:36:22 +0000

    swap two fields in the filter response protocol to match order of fields in the query protocol. this difference was overlooked and is error-prone for a filter developer. bump filter protocol.
    when you update your smtpd, if you use filters, they'll need to be updated.
    
    spotted by Chris Ross <cross+openbsd@distal.com>

--- usr.sbin/smtpd/lka_report.c
+++ usr.sbin/smtpd/lka_report.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: lka_report.c,v 1.33 2019/09/21 08:10:44 semarie Exp $	*/
+/*	$OpenBSD: lka_report.c,v 1.34 2019/10/03 05:42:57 gilles Exp $	*/
 
 /*
  * Copyright (c) 2018 Gilles Chehade <gilles@poolp.org>
@@ -35,7 +35,7 @@
 #include "smtpd.h"
 #include "log.h"
 
-#define	PROTOCOL_VERSION	"0.4"
+#define	PROTOCOL_VERSION	"0.5"
 
 struct reporter_proc {
 	TAILQ_ENTRY(reporter_proc)	entries;
