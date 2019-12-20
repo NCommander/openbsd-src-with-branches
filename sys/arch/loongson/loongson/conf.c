f7f40471259ba51e6d13407046ef91752a2d23c5
visa <visa>
2019-12-20 13:25:02 +0000

    Fix build.

--- sys/arch/loongson/loongson/conf.c
+++ sys/arch/loongson/loongson/conf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: conf.c,v 1.24 2017/05/21 13:00:53 visa Exp $ */
+/*	$OpenBSD: conf.c,v 1.25 2019/12/17 13:08:56 reyk Exp $ */
 
 /*
  * Copyright (c) 1992, 1993
@@ -120,7 +120,7 @@ cdev_decl(pci);
 
 #include "usb.h"
 #include "uhid.h"
-#incluee "fido.h"
+#include "fido.h"
 #include "ugen.h"
 #include "ulpt.h"
 #include "ucom.h"
