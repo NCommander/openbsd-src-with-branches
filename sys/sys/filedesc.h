3ff605040b8937283ba7f79013bbb3dd4ab1b85e
visa <visa>
2020-01-03 05:37:00 +0000

    Fix a file descriptor close race in kqueue_register()
    After inserting a knote, check that the associated file descriptor
    still references the same file. Remove the knote if the descriptor
    has changed because otherwise the kqueue becomes inconsistent with
    the file descriptor table.
    
    There is an analogous race in fcntl(F_SETLK). It is already handled,
    but the code can be simplified by using the same check as in
    kqueue_register().
    
    Fix inspired by DragonFly BSD
    
    OK mpi@, anton@

--- sys/sys/filedesc.h
+++ sys/sys/filedesc.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: filedesc.h,v 1.40 2018/06/25 09:36:28 mpi Exp $	*/
+/*	$OpenBSD: filedesc.h,v 1.41 2018/07/02 14:36:33 visa Exp $	*/
 /*	$NetBSD: filedesc.h,v 1.14 1996/04/09 20:55:28 cgd Exp $	*/
 
 /*
@@ -136,6 +136,7 @@ void	fdcloseexec(struct proc *);
 struct file *fd_iterfile(struct file *, struct proc *);
 struct file *fd_getfile(struct filedesc *, int);
 struct file *fd_getfile_mode(struct filedesc *, int, int);
+int	fd_checkclosed(struct filedesc *, int, struct file *);
 
 int	closef(struct file *, struct proc *);
 int	getsock(struct proc *, int, struct file **);
