e43c4d33c80f268cddc484c72ee84bc45dd8356a
beck <beck>
2020-01-04 16:22:36 +0000

    Call process_worklist_item with LK_NOWAIT to skip locked vnodes from within softdep_process_worklist. When this is called from the syncer a vnode may be legtitimately locked by someone waiting for buffers so we need to skip anything locked.  FreeBSD appears to have a similar change. This avoids a deadlock where the syncer ends up waiting for the inode lock that his held by someone waiting for buffer space.
    Found by bluhm@ and some genua folks
    ok bluhm@

--- sys/ufs/ffs/ffs_softdep.c
+++ sys/ufs/ffs/ffs_softdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ffs_softdep.c,v 1.144 2019/07/19 00:24:31 cheloha Exp $	*/
+/*	$OpenBSD: ffs_softdep.c,v 1.145 2019/11/25 11:33:51 mpi Exp $	*/
 
 /*
  * Copyright 1998, 2000 Marshall Kirk McKusick. All Rights Reserved.
@@ -641,7 +641,7 @@ softdep_process_worklist(struct mount *matchmnt)
 	loopcount = 1;
 	getmicrouptime(&starttime);
 	while (num_on_worklist > 0) {
-		matchcnt += process_worklist_item(matchmnt, 0);
+		matchcnt += process_worklist_item(matchmnt, LK_NOWAIT);
 
 		/*
 		 * If a umount operation wants to run the worklist
