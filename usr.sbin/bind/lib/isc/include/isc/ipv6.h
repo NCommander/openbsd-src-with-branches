efbdfe2803920d259a35a76c84b03829f3d58216
florian <florian>
2020-01-09 13:45:34 +0000

    unifdef network defines: #define ISC_PLATFORM_HAVESALEN 1 #define ISC_PLATFORM_HAVEIPV6 1 #undef ISC_PLATFORM_NEEDIN6ADDRANY #undef ISC_PLATFORM_NEEDIN6ADDRLOOPBACK #define ISC_PLATFORM_HAVEIN6PKTINFO 1 #undef ISC_PLATFORM_HAVEINADDR6 #define ISC_PLATFORM_HAVESCOPEID 1 #undef ISC_PLATFORM_NEEDNTOP #undef ISC_PLATFORM_NEEDPTON #undef ISC_PLATFORM_NEEDPORTT #undef ISC_PLATFORM_HAVELIFCONF #undef ISC_PLATFORM_HAVEIF_LADDRCONF #undef ISC_PLATFORM_HAVEIF_LADDRREQ #define ISC_NET_BSD44MSGHDR 1 #define ISC_PLATFORM_HAVEIFNAMETOINDEX 1 #undef ISC_PLATFORM_FIXIN6ISADDR #define ISC_PLATFORM_HAVESOCKADDRSTORAGE 1 #define ISC_PLATFORM_HAVEKQUEUE 1 #undef ISC_PLATFORM_HAVEEPOLL #undef ISC_PLATFORM_HAVEDEVPOLL
    prodding deraadt@

--- usr.sbin/bind/lib/isc/include/isc/ipv6.h
+++ usr.sbin/bind/lib/isc/include/isc/ipv6.h
@@ -14,7 +14,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: ipv6.h,v 1.2 2019/12/16 16:16:26 deraadt Exp $ */
+/* $Id: ipv6.h,v 1.3 2019/12/17 01:46:35 sthen Exp $ */
 
 #ifndef ISC_IPV6_H
 #define ISC_IPV6_H 1
@@ -78,21 +78,15 @@ LIBISC_EXTERNAL_DATA extern const struct in6_addr in6addr_any;
 LIBISC_EXTERNAL_DATA extern const struct in6_addr in6addr_loopback;
 
 struct sockaddr_in6 {
-#ifdef ISC_PLATFORM_HAVESALEN
 	isc_uint8_t		sin6_len;
 	isc_uint8_t		sin6_family;
-#else
-	isc_uint16_t		sin6_family;
-#endif
 	isc_uint16_t		sin6_port;
 	isc_uint32_t		sin6_flowinfo;
 	struct in6_addr		sin6_addr;
 	isc_uint32_t		sin6_scope_id;
 };
 
-#ifdef ISC_PLATFORM_HAVESALEN
 #define SIN6_LEN 1
-#endif
 
 /*%
  * Unspecified
