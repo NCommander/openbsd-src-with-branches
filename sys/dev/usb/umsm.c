8f04270fcca7ae3d872f395a05d0571664a26f41
deraadt <deraadt>
2020-01-11 22:43:15 +0000

    MC7700 works as umsm

--- sys/dev/usb/umsm.c
+++ sys/dev/usb/umsm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: umsm.c,v 1.114 2018/08/15 14:13:07 mpi Exp $	*/
+/*	$OpenBSD: umsm.c,v 1.115 2020/01/05 00:54:13 jsg Exp $	*/
 
 /*
  * Copyright (c) 2008 Yojiro UO <yuo@nui.org>
@@ -256,6 +256,7 @@ static const struct umsm_type umsm_devs[] = {
 	{{ USB_VENDOR_SIERRA, USB_PRODUCT_SIERRA_AIRCARD_340U}, 0},
 	{{ USB_VENDOR_SIERRA, USB_PRODUCT_SIERRA_AIRCARD_770S}, 0},
 	{{ USB_VENDOR_SIERRA, USB_PRODUCT_SIERRA_MC7455}, 0},
+	{{ USB_VENDOR_SIERRA, USB_PRODUCT_SIERRA_MC7700}, 0},
 
 	{{ USB_VENDOR_SIMCOM, USB_PRODUCT_SIMCOM_SIM7600E}, 0},
 
