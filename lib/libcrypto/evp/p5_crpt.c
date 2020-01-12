8052069d9f59464b85a8e8157c096f49f8a1d11d
inoguchi <inoguchi>
2020-01-12 07:11:13 +0000

    Avoid leak in error path of PKCS5_PBE_keyivgen
    ok jsing@ tb@

--- lib/libcrypto/evp/p5_crpt.c
+++ lib/libcrypto/evp/p5_crpt.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: p5_crpt.c,v 1.17 2016/11/08 20:01:06 miod Exp $ */
+/* $OpenBSD: p5_crpt.c,v 1.18 2017/01/29 17:49:23 beck Exp $ */
 /* Written by Dr Stephen N Henson (steve@openssl.org) for the OpenSSL
  * project 1999.
  */
@@ -108,6 +108,7 @@ PKCS5_PBE_keyivgen(EVP_CIPHER_CTX *cctx, const char *pass, int passlen,
 		iter = 1;
 	else if ((iter = ASN1_INTEGER_get(pbe->iter)) <= 0) {
 		EVPerror(EVP_R_UNSUPORTED_NUMBER_OF_ROUNDS);
+		PBEPARAM_free(pbe);
 		return 0;
 	}
 	salt = pbe->salt->data;
