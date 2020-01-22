d80488638dcf62e898fdcd7ebb6d240bf8881d9a
beck <beck>
2020-01-22 02:21:05 +0000

    Add minimal support for hello retry request for RFC conformance. We currently don't support sending a modified clienthello
    ok jsing@ tb@

--- lib/libssl/tls13_server.c
+++ lib/libssl/tls13_server.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: tls13_server.c,v 1.2 2019/11/17 06:43:46 jsing Exp $ */
+/* $OpenBSD: tls13_server.c,v 1.3 2019/11/17 14:25:03 tb Exp $ */
 /*
  * Copyright (c) 2019 Joel Sing <jsing@openbsd.org>
  *
@@ -95,13 +95,6 @@ tls13_server_hello_retry_recv(struct tls13_ctx *ctx)
 {
 	return 0;
 }
-
-int
-tls13_client_hello_retry_recv(struct tls13_ctx *ctx)
-{
-	return 0;
-}
-
 
 int
 tls13_client_end_of_early_data_send(struct tls13_ctx *ctx)
