639454a29d2bcc1eefaf56d1ce5c582a0cc4c71f
inoguchi <inoguchi>
2020-01-26 12:39:16 +0000

    Fix SSL_CIPHER_description
    ok jsing@

--- lib/libssl/ssl_ciph.c
+++ lib/libssl/ssl_ciph.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssl_ciph.c,v 1.108 2019/04/04 16:44:24 jsing Exp $ */
+/* $OpenBSD: ssl_ciph.c,v 1.109 2020/01/24 04:38:12 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -1384,7 +1384,7 @@ SSL_CIPHER_description(const SSL_CIPHER *cipher, char *buf, int len)
 		kx = "GOST";
 		break;
 	case SSL_kTLS1_3:
-		au = "TLSv1.3";
+		kx = "TLSv1.3";
 		break;
 	default:
 		kx = "unknown";
