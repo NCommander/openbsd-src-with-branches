482587796e5defa11a29778edb8518bd9c4acc0c
patrick <patrick>
2020-02-08 19:08:17 +0000

    On the Lenovo X395 acpivout(4) does not attach since there is no _BQC method.  But we need it to attach, since it provides the events for the brightness keys.  Thus, make acpivout(4) attach enough to register the event handler, but return early so that it doesn't register itself as ws_[gs]et_param.
    ok kettenis@

--- sys/dev/acpi/acpivideo.c
+++ sys/dev/acpi/acpivideo.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: acpivideo.c,v 1.10 2015/03/14 03:38:47 jsg Exp $	*/
+/*	$OpenBSD: acpivideo.c,v 1.11 2017/06/30 06:25:29 mlarkin Exp $	*/
 /*
  * Copyright (c) 2008 Federico G. Schwindt <fgsch@openbsd.org>
  * Copyright (c) 2009 Paul Irofti <pirofti@openbsd.org>
@@ -149,7 +149,7 @@ acpi_foundvout(struct aml_node *node, void *arg)
 	if (node->parent != sc->sc_devnode)
 		return (0);
 
-	if (aml_searchname(node, "_BCM") && aml_searchname(node, "_BQC")) {
+	if (aml_searchname(node, "_BCM")) {
 		memset(&aaa, 0, sizeof(aaa));
 		aaa.aaa_iot = sc->sc_acpi->sc_iot;
 		aaa.aaa_memt = sc->sc_acpi->sc_memt;
