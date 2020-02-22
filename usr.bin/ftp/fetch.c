f5b8920f1b67036495edf6ed3d4060a4696fca41
jca <jca>
2020-02-22 01:00:07 +0000

    Avoid duplication in the code that sends headers
    ok yasuoka@

--- usr.bin/ftp/fetch.c
+++ usr.bin/ftp/fetch.c
@@ -730,19 +730,10 @@ noslash:
 				restart_point = 0;
 		}
 #endif	/* SMALL */
-		if (credentials) {
-			ftp_printf(fin,
-			    "GET /%s HTTP/1.1\r\n"
-			    "Connection: close\r\n"
-			    "Authorization: Basic %s\r\n"
-			    "Host: ", epath, credentials);
-			free(credentials);
-			credentials = NULL;
-		} else
-			ftp_printf(fin,
-			    "GET /%s HTTP/1.1\r\n"
-			    "Connection: close\r\n"
-			    "Host: ", epath);
+		ftp_printf(fin,
+		    "GET /%s HTTP/1.1\r\n"
+		    "Connection: close\r\n"
+		    "Host: ", epath);
 		if (proxyhost) {
 			ftp_printf(fin, "%s", proxyhost);
 			port = NULL;
@@ -776,8 +767,12 @@ noslash:
 		if (port && strcmp(port, "80") != 0)
 			ftp_printf(fin, ":%s", port);
 #endif /* !NOSSL */
-		ftp_printf(fin, "\r\n%s%s\r\n\r\n",
+		ftp_printf(fin, "\r\n%s%s\r\n",
 		    buf ? buf : "", httpuseragent);
+		if (credentials)
+			ftp_printf(fin, "Authorization: Basic %s\r\n",
+			    credentials);
+		ftp_printf(fin, "\r\n");
 	}
 	free(epath);
 
