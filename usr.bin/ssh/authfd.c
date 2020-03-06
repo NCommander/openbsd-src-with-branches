2abfb3dc65e2e11d353f4f429bfbe886c2aa2389
markus <markus>
2020-03-06 18:24:39 +0000

    ssh_fetch_identitylist() returns the return value from ssh_request_reply() so we should also check against != 0 ok djm

--- usr.bin/ssh/authfd.c
+++ usr.bin/ssh/authfd.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: authfd.c,v 1.121 2019/12/21 02:19:13 djm Exp $ */
+/* $OpenBSD: authfd.c,v 1.122 2020/02/26 13:40:09 jsg Exp $ */
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
@@ -341,7 +341,7 @@ ssh_agent_has_key(int sock, struct sshkey *key)
 	size_t i;
 	struct ssh_identitylist *idlist = NULL;
 
-	if ((r = ssh_fetch_identitylist(sock, &idlist)) < 0) {
+	if ((r = ssh_fetch_identitylist(sock, &idlist)) != 0) {
 		return r;
 	}
 
