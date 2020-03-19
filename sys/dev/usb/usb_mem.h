06f1be7dfe79b37b3d00ced63dadd0cc7f72191a
patrick <patrick>
2020-03-19 14:18:38 +0000

    Switch USB to use non-coherent buffers for data transfers.  Since the import in '99 all buffers allocated using usb_allocmem() have been mapped COHERENT.  On some ARM SoCs, where the USB controller is not coherent with the caches, this means the buffers were mapped uncached.  This drastically reduces the performance, especially on reads.
    We already added cache syncs before and after USB transfers, but so
    far those have essentially been no-ops.  Since other drivers make
    use of the same allocation code, and those haven't been proven to
    have correct syncs, we can't just remove the COHERENT flag.
    
    This splits the allocation into coherent and non-coherent blocks.
    All drivers who call usb_allocmem() themselves now pass a flag to
    show they require coherent blocks.  Onced verified that they also
    work fine without coherent, or once they have been refactored, we
    can remove this again.
    
    On a ure(4) connected to an i.MX8MQ, the receive performance is
    10x as fast as before.  The Raspberry Pi's ethernet receive speed
    doubled.
    
    Debugged using dt(4)
    Original diff from Marius Strobl
    Feedback from kettenis@
    Tested on an i.MX8MQ (arm64) by patrick@
    Tested on a Raspberry Pi (arm64) by tobhe@
    Tested on an ERL (octeon) by Janne Johansson
    ok mpi@ gerhard@ tobhe@

--- sys/dev/usb/usb_mem.h
+++ sys/dev/usb/usb_mem.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: usb_mem.h,v 1.14 2013/04/15 09:23:02 mglocker Exp $ */
+/*	$OpenBSD: usb_mem.h,v 1.15 2016/11/30 10:19:18 mpi Exp $ */
 /*	$NetBSD: usb_mem.h,v 1.20 2003/05/03 18:11:42 wiz Exp $	*/
 /*	$FreeBSD: src/sys/dev/usb/usb_mem.h,v 1.9 1999/11/17 22:33:47 n_hibma Exp $	*/
 
@@ -40,6 +40,7 @@ struct usb_dma_block {
         caddr_t kaddr;
         bus_dma_segment_t segs[1];
         int nsegs;
+	int coherent;
         size_t size;
         size_t align;
 	struct usb_frag_dma *frags;
