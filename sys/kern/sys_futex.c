8a950e0f246c965a960c799a8b22de432a5e2cdd
cheloha <cheloha>
2020-03-20 17:17:31 +0000

    futex(2): futex_wait(): ensure timeout is set when calling rwsleep_nsec(9)
    rwsleep_nsec(9) will not set a timeout if the nsecs parameter is
    equal to INFSLP (UINT64_MAX).  We need to limit the duration to
     MAXTSLP (UINT64_MAX - 1) to ensure a timeout is set.

--- sys/kern/sys_futex.c
+++ sys/kern/sys_futex.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sys_futex.c,v 1.13 2019/07/10 15:52:17 mpi Exp $ */
+/*	$OpenBSD: sys_futex.c,v 1.14 2020/01/14 08:52:18 mpi Exp $ */
 
 /*
  * Copyright (c) 2016-2017 Martin Pieuchot
@@ -244,7 +244,7 @@ futex_wait(uint32_t *uaddr, uint32_t val, const struct timespec *timeout,
 #endif
 		if (ts.tv_sec < 0 || !timespecisvalid(&ts))
 			return EINVAL;
-		nsecs = TIMESPEC_TO_NSEC(&ts);
+		nsecs = MIN(TIMESPEC_TO_NSEC(&ts), MAXTSLP);
 	}
 
 	f = futex_get(uaddr, flags | FT_CREATE);
