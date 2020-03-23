a1d6cd2dd5c7793d4bcd460c4a7f4e073a0eca67
uaa <uaa>
2020-03-23 21:40:01 +0000

    UART 1-3 support for Allwinner H3 (maybe H5/H2+).
    - add reset signal definition
    - add reset signal de-assert code
    
    ok kettenis@

--- sys/dev/fdt/com_fdt.c
+++ sys/dev/fdt/com_fdt.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: com_fdt.c,v 1.2 2018/07/20 05:37:06 jsg Exp $ */
+/* $OpenBSD: com_fdt.c,v 1.3 2018/08/06 10:52:30 patrick Exp $ */
 /*
  * Copyright (c) 2016 Patrick Wildt <patrick@blueri.se>
  *
@@ -105,6 +105,7 @@ com_fdt_attach(struct device *parent, struct device *self, void *aux)
 		return;
 
 	clock_enable(faa->fa_node, NULL);
+	reset_deassert_all(faa->fa_node);
 
 	/*
 	 * Determine the clock frequency after enabling the clock.
