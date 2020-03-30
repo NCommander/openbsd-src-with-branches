3ade277be837a60384f22b1da1698b912496de13
martijn <martijn>
2020-03-30 12:52:58 +0000

    "eventually" came and went back in 2004.
    OK schwarze@

--- lib/libc/stdlib/getopt_long.c
+++ lib/libc/stdlib/getopt_long.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: getopt_long.c,v 1.29 2015/10/01 02:32:07 guenther Exp $	*/
+/*	$OpenBSD: getopt_long.c,v 1.30 2019/01/25 00:19:25 millert Exp $	*/
 /*	$NetBSD: getopt_long.c,v 1.15 2002/01/31 22:43:40 tv Exp $	*/
 
 /*
@@ -478,8 +478,6 @@ start:
 /*
  * getopt --
  *	Parse argc/argv argument vector.
- *
- * [eventually this will replace the BSD getopt]
  */
 int
 getopt(int nargc, char * const *nargv, const char *options)
