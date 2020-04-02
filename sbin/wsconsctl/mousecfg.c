a9a369d4f38a0bf64a1a7aaef2b5eca5cebe6f7f
deraadt <deraadt>
2020-04-02 17:17:04 +0000

    avoid sys/param.h poisoning, and replace nitems with a local decl

--- sbin/wsconsctl/mousecfg.c
+++ sbin/wsconsctl/mousecfg.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: mousecfg.c,v 1.5 2018/07/30 15:57:04 jcs Exp $ */
+/* $OpenBSD: mousecfg.c,v 1.6 2019/08/19 21:42:33 bru Exp $ */
 
 /*
  * Copyright (c) 2017 Ulf Brosziewski
@@ -20,8 +20,9 @@
  * Read/write wsmouse parameters for touchpad configuration.
  */
 
+#include <sys/types.h>
 #include <sys/ioctl.h>
-#include <sys/param.h>
+#include <sys/time.h>
 #include <dev/wscons/wsconsio.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,6 +30,10 @@
 #include <err.h>
 #include <errno.h>
 #include "mousecfg.h"
+
+#ifndef nitems
+#define nitems(_a)       (sizeof((_a)) / sizeof((_a)[0]))
+#endif
 
 #define BASE_FIRST		WSMOUSECFG_DX_SCALE
 #define BASE_LAST		WSMOUSECFG_REVERSE_SCROLLING
