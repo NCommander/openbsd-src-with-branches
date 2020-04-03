cd17fdfffe149f676f08bcd9a0c3ab7fe073e078
dtucker <dtucker>
2020-04-03 02:27:12 +0000

    Make with config keywords support which percent_expansions more consistent.  - %C is moved into its own function and added to Match Exec.  - move the common (global) options into a macro.  This is ugly but it's    the least-ugly way I could come up with.  - move IdentityAgent and ForwardAgent percent expansion to before the    config dump to make it regression-testable.  - document all of the above
    ok jmc@ for man page bits, "makes things less terrible" djm@ for the rest.

--- usr.bin/ssh/readconf.h
+++ usr.bin/ssh/readconf.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: readconf.h,v 1.131 2019/12/21 02:19:13 djm Exp $ */
+/* $OpenBSD: readconf.h,v 1.132 2020/01/23 02:46:49 dtucker Exp $ */
 
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
@@ -200,6 +200,8 @@ typedef struct {
 #define SSH_STRICT_HOSTKEY_ASK	3
 
 const char *kex_default_pk_alg(void);
+char	*ssh_connection_hash(const char *thishost, const char *host,
+    const char *portstr, const char *user);
 void     initialize_options(Options *);
 void     fill_default_options(Options *);
 void	 fill_default_options_for_canonicalization(Options *);
