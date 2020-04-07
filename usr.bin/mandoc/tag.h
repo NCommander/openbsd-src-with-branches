88e8f3cb0a11dcbb072a93f85529ebb22c95d41e
schwarze <schwarze>
2020-04-07 22:45:38 +0000

    Separate the place to put the <a href> permalink (now marked with NODE_HREF) from the target element of the link (still marked with NODE_ID).  In many cases, use this to move the target to the beginning of the paragraph, such that readers don't get dropped into the middle of a sentence.

--- usr.bin/mandoc/tag.h
+++ usr.bin/mandoc/tag.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: tag.h,v 1.11 2020/03/13 00:31:05 schwarze Exp $ */
+/* $OpenBSD: tag.h,v 1.12 2020/04/02 22:10:27 schwarze Exp $ */
 /*
  * Copyright (c) 2015, 2018, 2019, 2020 Ingo Schwarze <schwarze@openbsd.org>
  *
@@ -31,4 +31,5 @@
 void		 tag_alloc(void);
 int		 tag_exists(const char *);
 void		 tag_put(const char *, int, struct roff_node *);
+void		 tag_postprocess(struct roff_node *);
 void		 tag_free(void);
