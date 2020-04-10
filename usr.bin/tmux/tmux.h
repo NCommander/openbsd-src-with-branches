0a831dc6964486729cb91575cd50f65ebca231a1
nicm <nicm>
2020-04-10 20:53:54 +0000

    Remove unused define, also a man fix from jmc.

--- usr.bin/tmux/tmux.h
+++ usr.bin/tmux/tmux.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: tmux.h,v 1.978 2020/04/09 14:23:34 nicm Exp $ */
+/* $OpenBSD: tmux.h,v 1.979 2020/04/10 07:44:26 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -843,7 +843,6 @@ struct window_mode {
 	void		 (*formats)(struct window_mode_entry *,
 			     struct format_tree *);
 };
-#define WINDOW_MODE_TIMEOUT 180
 
 /* Active window mode. */
 struct window_mode_entry {
