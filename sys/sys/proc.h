ac0e74328be1b4f6d34f991cf69a5bbd2fad7223
mpi <mpi>
2020-04-28 08:29:40 +0000

    Document that thread credentials are owned by curproc.
    From Vitaliy Makkoveev, ok visa@

--- sys/sys/proc.h
+++ sys/sys/proc.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: proc.h,v 1.293 2020/03/20 08:14:07 claudio Exp $	*/
+/*	$OpenBSD: proc.h,v 1.294 2020/04/06 07:52:12 claudio Exp $	*/
 /*	$NetBSD: proc.h,v 1.44 1996/04/22 01:23:21 christos Exp $	*/
 
 /*-
@@ -392,7 +392,7 @@ struct proc {
 	u_int	p_estcpu;		/* [s] Time averaged val of p_cpticks */
 	int	p_pledge_syscall;	/* Cache of current syscall */
 
-	struct	ucred *p_ucred;		/* cached credentials */
+	struct	ucred *p_ucred;		/* [o] cached credentials */
 	struct	sigaltstack p_sigstk;	/* sp & on stack state variable */
 
 	u_long	p_prof_addr;	/* tmp storage for profiling addr until AST */
