3c0fe7bcd32badfa4f09efe72937795c6f0b1c09
deraadt <deraadt>
2020-05-07 18:46:35 +0000

    post-6.7 development continues

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.173 2020/04/05 06:34:20 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.174 2020/05/04 15:00:35 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -71,9 +71,9 @@ ost="OpenBSD"
 osr="6.7"
 
 cat >vers.c <<eof
-#define STATUS ""			/* release */
-#if 0
 #define STATUS "-current"		/* just after a release */
+#if 0
+#define STATUS ""			/* release */
 #define STATUS "-beta"			/* just before a release */
 #endif
 
