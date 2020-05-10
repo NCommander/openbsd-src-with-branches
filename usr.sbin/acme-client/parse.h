b1488da8967b9b77e6da3c8c6f064dce383f3179
benno <benno>
2020-05-10 12:06:18 +0000

    Allow to have multiple domain ... {} sextions with the same domain name, by adding a new (optional) config option "domain name". This can be used to create a rsa and an ecdsa key for the same domain name. The old domain name in the 'title' line continues to be used as domain name in the abscence of the domain name argument, i.e. the change is backward compatible with current config files. tested by sthen@ ok florian@ sthen@

--- usr.sbin/acme-client/parse.h
+++ usr.sbin/acme-client/parse.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: parse.h,v 1.12 2019/06/14 19:55:08 florian Exp $ */
+/*	$OpenBSD: parse.h,v 1.13 2019/06/17 12:42:52 florian Exp $ */
 /*
  * Copyright (c) 2016 Sebastian Benoit <benno@openbsd.org>
  *
@@ -45,6 +45,7 @@ struct domain_c {
 	TAILQ_HEAD(, altname_c)	 altname_list;
 	int			 altname_count;
 	enum keytype		 keytype;
+	char			*handle;
 	char			*domain;
 	char			*key;
 	char			*cert;
@@ -80,7 +81,7 @@ int			 cmdline_symset(char *);
 /* use these to find a authority or domain by name */
 struct authority_c	*authority_find(struct acme_conf *, char *);
 struct authority_c	*authority_find0(struct acme_conf *);
-struct domain_c		*domain_find(struct acme_conf *, char *);
+struct domain_c		*domain_find_handle(struct acme_conf *, char *);
 
 int			 domain_valid(const char *);
 
