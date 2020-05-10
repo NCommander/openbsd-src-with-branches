cc3c33f7ef1b0ae505aa30884b101659c1a18747
florian <florian>
2020-05-10 15:06:07 +0000

    remove noisy and useless debug line

--- usr.sbin/acme-client/dnsproc.c
+++ usr.sbin/acme-client/dnsproc.c
@@ -1,4 +1,4 @@
-/*	$Id: dnsproc.c,v 1.9 2017/01/24 13:32:55 jsing Exp $ */
+/*	$Id: dnsproc.c,v 1.10 2019/11/06 21:05:45 florian Exp $ */
 /*
  * Copyright (c) 2016 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -160,7 +160,6 @@ dnsproc(int nfd)
 			last = look;
 			look = NULL;
 		} else {
-			doddbg("%s: cached", look);
 			free(look);
 			look = NULL;
 		}
