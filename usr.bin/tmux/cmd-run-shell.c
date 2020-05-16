4c7b9b852597a8bd4b85569f974e62a96d66ca4a
nicm <nicm>
2020-05-16 16:02:24 +0000

    Add a customize mode where keys and options may be browsed and changed, includes adding a brief description of each option. Bound to "C" by default.

--- usr.bin/tmux/cmd-run-shell.c
+++ usr.bin/tmux/cmd-run-shell.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: cmd-run-shell.c,v 1.66 2020/04/13 10:59:58 nicm Exp $ */
+/* $OpenBSD: cmd-run-shell.c,v 1.67 2020/04/13 20:51:57 nicm Exp $ */
 
 /*
  * Copyright (c) 2009 Tiago Cunha <me@tiagocunha.org>
@@ -190,7 +190,8 @@ cmd_run_shell_callback(struct job *job)
 		retcode = WTERMSIG(status);
 		xasprintf(&msg, "'%s' terminated by signal %d", cmd, retcode);
 		retcode += 128;
-	}
+	} else
+		retcode = 0;
 	if (msg != NULL)
 		cmd_run_shell_print(job, msg);
 	free(msg);
