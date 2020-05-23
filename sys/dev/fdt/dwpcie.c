c313147f36ea3c5c62599600a48c7adad0e912ea
patrick <patrick>
2020-05-23 22:16:39 +0000

    Increase chances of getting a successful PCIe link on the i.MX8MM. Hold the reset GPIO for 100ms instead of 20ms, and also directly continue setting up the link instead of waiting for another 20ms. This brings us in line with what Linux is doing and removes the 10% risk of em(4) not showing up on my HummingBoard Pulse.
    ok kettenis@

--- sys/dev/fdt/dwpcie.c
+++ sys/dev/fdt/dwpcie.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dwpcie.c,v 1.19 2020/04/27 11:41:31 patrick Exp $	*/
+/*	$OpenBSD: dwpcie.c,v 1.20 2020/04/27 12:17:29 patrick Exp $	*/
 /*
  * Copyright (c) 2018 Mark Kettenis <kettenis@openbsd.org>
  *
@@ -768,9 +768,8 @@ dwpcie_imx8mq_init(struct dwpcie_softc *sc)
 
 	if (reset_gpiolen > 0) {
 		gpio_controller_set_pin(reset_gpio, 1);
-		delay(20000);
+		delay(100000);
 		gpio_controller_set_pin(reset_gpio, 0);
-		delay(20000);
 	}
 
 	reset_deassert(sc->sc_node, "pciephy");
