ace6863bc8cff22c8da7e933b4f9a198413a808b
jan <jan>
2020-05-23 06:15:09 +0000

    Prevent km_alloc() from returning garbage if pagelist is empty.
    ok bluhm@, visa@

--- sys/uvm/uvm_km.c
+++ sys/uvm/uvm_km.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_km.c,v 1.135 2019/12/30 23:58:38 jsg Exp $	*/
+/*	$OpenBSD: uvm_km.c,v 1.136 2020/02/18 12:13:40 mpi Exp $	*/
 /*	$NetBSD: uvm_km.c,v 1.42 2001/01/14 02:10:01 thorpej Exp $	*/
 
 /* 
@@ -816,7 +816,7 @@ km_alloc(size_t sz, const struct kmem_va_mode *kv,
 	paddr_t pla_align;
 	int pla_flags;
 	int pla_maxseg;
-	vaddr_t va, sva;
+	vaddr_t va, sva = 0;
 
 	KASSERT(sz == round_page(sz));
 
