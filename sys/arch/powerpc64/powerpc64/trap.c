b6a8293b8cc0ae79e2fb73d83717ca4056a57ae0
kettenis <kettenis>
2020-06-14 17:53:03 +0000

    Put a bit more information in the panic message.

--- sys/arch/powerpc64/powerpc64/trap.c
+++ sys/arch/powerpc64/powerpc64/trap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: trap.c,v 1.5 2020/06/12 22:01:01 gkoehler Exp $	*/
+/*	$OpenBSD: trap.c,v 1.6 2020/06/13 22:58:42 kettenis Exp $	*/
 
 /*
  * Copyright (c) 2020 Mark Kettenis <kettenis@openbsd.org>
@@ -54,8 +54,6 @@ trap(struct trapframe *frame)
 #endif
 	}
 
-	if (frame->exc == EXC_DSI)
-		printf("dsisr %lx dar %lx\n", frame->dsisr, frame->dar);
-
-	panic("trap type %lx at lr %lx", frame->exc, frame->lr);
+	panic("trap type %lx srr1 %lx at %lx lr %lx",
+	    frame->exc, frame->srr1, frame->srr0, frame->lr);
 }
