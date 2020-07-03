2c20d4f0ae352594bc00c7f482eaadca1f1d9232
kettenis <kettenis>
2020-07-03 17:54:27 +0000

    Use an LFENCE instruction everywhere where we use RDTSC when we are doing some sort of time measurement.  This is necessary since RDTSC is not a serializing instruction.  We can use LFENCE as the serializing instruction instead of CPUID since all amd64 machines have SSE.
    This considerably reduces the jitter in TSC skew measurements.
    
    ok deraadt@, cheloha@, phessler@

--- sys/arch/amd64/include/cpufunc.h
+++ sys/arch/amd64/include/cpufunc.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cpufunc.h,v 1.33 2019/03/26 19:32:46 mlarkin Exp $	*/
+/*	$OpenBSD: cpufunc.h,v 1.34 2019/06/28 21:54:05 bluhm Exp $	*/
 /*	$NetBSD: cpufunc.h,v 1.3 2003/05/08 10:27:43 fvdl Exp $	*/
 
 /*-
@@ -288,6 +288,15 @@ rdtsc(void)
 	uint32_t hi, lo;
 
 	__asm volatile("rdtsc" : "=d" (hi), "=a" (lo));
+	return (((uint64_t)hi << 32) | (uint64_t) lo);
+}
+
+static __inline u_int64_t
+rdtsc_lfence(void)
+{
+	uint32_t hi, lo;
+
+	__asm volatile("lfence; rdtsc" : "=d" (hi), "=a" (lo));
 	return (((uint64_t)hi << 32) | (uint64_t) lo);
 }
 
