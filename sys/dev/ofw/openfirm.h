6ca47d8960e166713a4f7e28c6efa4f838b89d11
kettenis <kettenis>
2020-07-06 15:18:04 +0000

    IEEE1275 (Open Firmware) defines that parameter name strings can have a length of up to 31 characters.  This limit is also present in the flattened device tree specification/  Unfortunately this limit isn't enforced by the tooling and there are systems in the wild that use longer strings. This includes the device trees used on POWER9 systems and has been seen on some ARM systems as well.
    So bump the buffer size from 32 bytes (31 + terminating NUL) to 64 bytes.
    Centrally define OFMAXPARAM  to this value (in <dev/ofw/openfirm.h>)
    replacing the various OPROMMAXPARAM definition scattered around the tree
    to make sure the FDT implementation of OF_nextprop() uses the same
    buffer size as its consumers.
    
    Eliminate the static buffer in various openprom(4) implementations on
    FDT systems.
    
    Makes it possible to dump the full device tree on POWER9 systems using
    eeprom -p.
    
    ok deraadt@, visa@

--- sys/dev/ofw/openfirm.h
+++ sys/dev/ofw/openfirm.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: openfirm.h,v 1.14 2017/03/12 11:44:42 kettenis Exp $	*/
+/*	$OpenBSD: openfirm.h,v 1.15 2017/12/27 11:40:14 kettenis Exp $	*/
 /*	$NetBSD: openfirm.h,v 1.1 1996/09/30 16:35:10 ws Exp $	*/
 
 /*
@@ -37,6 +37,8 @@
 
 #include <sys/param.h>
 #include <sys/device.h>
+
+#define OFMAXPARAM	64
 
 int openfirmware(void *);
 
