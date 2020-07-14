1e54210bd6017520919b74ed9af2a73721d68285
kettenis <kettenis>
2020-07-14 17:03:13 +0000

    Mark unmanaged kernel mappings as wired and skip them if we need to evict a mapping from the page tables.

--- sys/arch/powerpc64/include/pte.h
+++ sys/arch/powerpc64/include/pte.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pte.h,v 1.3 2020/06/18 21:52:57 kettenis Exp $	*/
+/*	$OpenBSD: pte.h,v 1.4 2020/07/01 16:05:48 kettenis Exp $	*/
 
 /*
  * Copyright (c) 2020 Mark Kettenis <kettenis@openbsd.org>
@@ -32,7 +32,7 @@ struct pte {
 /* High doubleword: */
 #define PTE_VALID		0x0000000000000001ULL
 #define PTE_HID			0x0000000000000002ULL
-#define PTE_WIRED		0x0000000000000004ULL /* SW */
+#define PTE_WIRED		0x0000000000000008ULL /* SW */
 #define PTE_AVPN		0x3fffffffffffff80ULL
 #define PTE_VSID_SHIFT		12
 
