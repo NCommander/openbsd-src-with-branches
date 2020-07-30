a15a214cdda79d7f66fc4cab7cffba11c9fc5ca7
millert <millert>
2020-07-30 17:11:10 +0000

    Improved handling of an unescaped '/' in a bracket expression.

--- usr.bin/awk/lex.c
+++ usr.bin/awk/lex.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: lex.c,v 1.22 2020/06/23 16:54:40 millert Exp $	*/
+/*	$OpenBSD: lex.c,v 1.23 2020/07/13 14:03:52 millert Exp $	*/
 /****************************************************************
 Copyright (C) Lucent Technologies 1997
 All Rights Reserved
@@ -551,7 +551,9 @@ int regexpr(void)
 			 */
 			if (!do_posix) {
 				if (c == '[') {
-					if (openclass == 0 || peek() == ':') {
+					int nextc = peek();
+					if (openclass == 0 || nextc == ':' ||
+					    nextc == '.' || nextc == '=') {
 						if (++openclass == 1)
 							cstart = bp;
 					}
