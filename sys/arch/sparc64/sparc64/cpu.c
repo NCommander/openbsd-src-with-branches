60f7ecb4e2c0da7d2a6a620a194eb132898b50f2
kettenis <kettenis>
2020-07-31 11:19:12 +0000

    On sun4u systems that have the STICK register, clear the NPT bit of the register such that it can be accessed from userland.  With this change we can unconditionally enabled the usertc code again.
    ok naddy@

--- sys/arch/sparc64/sparc64/cpu.c
+++ sys/arch/sparc64/sparc64/cpu.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cpu.c,v 1.69 2017/12/11 00:15:50 kettenis Exp $	*/
+/*	$OpenBSD: cpu.c,v 1.70 2020/05/29 04:42:24 deraadt Exp $	*/
 /*	$NetBSD: cpu.c,v 1.13 2001/05/26 21:27:15 chs Exp $ */
 
 /*
@@ -417,10 +417,8 @@ cpu_init(struct cpu_info *ci)
 	int err;
 #endif
 
-	if (CPU_ISSUN4U || CPU_ISSUN4US) {
-		tick_enable();
+	if (CPU_ISSUN4U || CPU_ISSUN4US)
 		return;
-	}
 
 #ifdef SUN4V
 #define MONDO_QUEUE_SIZE	32
