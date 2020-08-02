058857d29d625a028e9a5d7e2b06f7f98b411324
kn <kn>
2020-08-02 11:15:51 +0000

    Add missing rtable(4) check in rip6_input()
    Copied over from sys/netinet/raw_ip.c:rip_input() where it appeared with
    initial support for multiple routing tables.
    
    This enforces separation between multiple raw sockets in different routing
    tables, i.e. one must not see packets from the other if the rtable differs.
    
    Observed with ping6(8)'s "-v" showing all ICMPv6 packets on its raw socket
    including those produced by another ping6 with "-V1".
    
    florian reported IPv6 route advertisments in one routing table appearing on
    raw sockets in other routing tables as well.
    
    OK claudio florian

--- sys/netinet6/raw_ip6.c
+++ sys/netinet6/raw_ip6.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: raw_ip6.c,v 1.136 2019/04/23 11:01:54 bluhm Exp $	*/
+/*	$OpenBSD: raw_ip6.c,v 1.137 2019/11/29 16:41:02 nayden Exp $	*/
 /*	$KAME: raw_ip6.c,v 1.69 2001/03/04 15:55:44 itojun Exp $	*/
 
 /*
@@ -160,6 +160,10 @@ rip6_input(struct mbuf **mp, int *offp, int proto, int af)
 	TAILQ_FOREACH(in6p, &rawin6pcbtable.inpt_queue, inp_queue) {
 		if (in6p->inp_socket->so_state & SS_CANTRCVMORE)
 			continue;
+		if (rtable_l2(in6p->inp_rtableid) !=
+		    rtable_l2(m->m_pkthdr.ph_rtableid))
+			continue;
+
 		if (!(in6p->inp_flags & INP_IPV6))
 			continue;
 		if ((in6p->inp_ipv6.ip6_nxt || proto == IPPROTO_ICMPV6) &&
