b6f70433c96a2ca9e44656c85286a29c5464371d
kn <kn>
2020-08-05 06:12:43 +0000

    Merge switch_status() into bridge_status()
    This is to reduce duplicate code and prepare for bridge_status() to cover
    all bridge like interfaces: bridge(4), switch(4) and tpmr(4).
    
    OK dlg

--- sbin/ifconfig/ifconfig.h
+++ sbin/ifconfig/ifconfig.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ifconfig.h,v 1.1 2019/10/17 18:37:38 bluhm Exp $	*/
+/*	$OpenBSD: ifconfig.h,v 1.2 2019/10/24 18:54:10 bluhm Exp $	*/
 
 /*
  * Copyright (c) 2009 Claudio Jeker <claudio@openbsd.org>
@@ -69,7 +69,6 @@ void bridge_flushrule(const char *, int);
 int is_bridge(void);
 void bridge_status(void);
 int bridge_rule(int, char **, int);
-void switch_status(void);
 void switch_datapathid(const char *, int);
 void switch_portno(const char *, const char *);
 
