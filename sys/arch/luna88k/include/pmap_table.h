47a46f1a3a42a219a77cb37220efd5c47f3b3089
cvs-fast-export <cvs-fast-export>
2020-08-09 15:51:15 +0000

    Synthetic commit for incomplete tag CLANG_10_0_1

--- sys/arch/luna88k/include/pmap_table.h
+++
@@ -1,3 +0,0 @@
-/* $OpenBSD: pmap_table.h,v 1.1 2004/04/21 15:23:58 aoyama Exp $ */
-/* public domain */
-#include <m88k/pmap_table.h>
--- sys/dev/pci/drm/include/linux/dma-fence-chain.h
+++
@@ -1,9 +0,0 @@
-/* Public domain. */
-
-#ifndef _LINUX_DMA_FENCE_CHAIN_H
-#define _LINUX_DMA_FENCE_CHAIN_H
-
-struct dma_fence_chain {
-};
-
-#endif
