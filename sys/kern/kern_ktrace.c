d5d4a72611a1303a77d3e88876adb5f0bc56abfc
claudio <claudio>
2020-09-13 09:48:39 +0000

    Grep the KERNEL_LOCK in ktrpsig() before calling ktrwrite(). Another little step towards moving signal delivery outside of KERNEL_LOCK. OK mpi@

--- sys/kern/kern_ktrace.c
+++ sys/kern/kern_ktrace.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_ktrace.c,v 1.102 2020/03/23 15:45:39 visa Exp $	*/
+/*	$OpenBSD: kern_ktrace.c,v 1.103 2020/04/03 03:20:12 visa Exp $	*/
 /*	$NetBSD: kern_ktrace.c,v 1.23 1996/02/09 18:59:36 christos Exp $	*/
 
 /*
@@ -291,7 +291,9 @@ ktrpsig(struct proc *p, int sig, sig_t action, int mask, int code,
 	kp.code = code;
 	kp.si = *si;
 
+	KERNEL_LOCK();
 	ktrwrite(p, &kth, &kp, sizeof(kp));
+	KERNEL_UNLOCK();
 	atomic_clearbits_int(&p->p_flag, P_INKTR);
 }
 
