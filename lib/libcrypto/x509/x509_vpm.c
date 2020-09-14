055a9446af83541bdda0b4eb8564dd93eef9314d
beck <beck>
2020-09-14 07:32:05 +0000

    revert previous, need to fix a problem

--- lib/libcrypto/x509/x509_vpm.c
+++ lib/libcrypto/x509/x509_vpm.c
@@ -178,6 +178,8 @@ x509_verify_param_zero(X509_VERIFY_PARAM *param)
 	/*param->inh_flags = X509_VP_FLAG_DEFAULT;*/
 	param->inh_flags = 0;
 	param->flags = 0;
+	/* XXX remove to enable new verifier by default */
+	param->flags |= X509_V_FLAG_LEGACY_VERIFY;
 	param->depth = -1;
 	if (param->policies) {
 		sk_ASN1_OBJECT_pop_free(param->policies, ASN1_OBJECT_free);
