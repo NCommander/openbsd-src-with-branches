8538882de648552205d4d579b2aca02b69079689
jsing <jsing>
2020-09-14 18:34:12 +0000

    Move state initialisation from SSL_clear() to ssl3_clear().
    If we use the default method (now TLSv1.3) and end up talking to a TLSv1.2
    server that gives us a session ticket, then try to resume that session,
    we end up trying to talk TLS without doing a handshake.
    
    This is caused by the state (S3I(s)->hs.state) getting cleared, which
    results in SSL_do_handshake() and others thinking they do not need to do
    anything (as SSL_in_init() and SSL_in_before() are not true).
    
    The reason this occurs is due to SSL_set_ssl_method() calling ssl_free()
    and ssl_new() when switching methods. The end result is that the S3I(s)
    has been freed and reallocated, losing the state in the process.
    
    Since the state is part of the S3I(s) structure, move its initialisation
    into ssl3_clear() - this ensures it gets correctly reinitialised across a
    SSL_set_ssl_method() call.
    
    Issue noticed by sthen@ with nginx and unifi.
    
    ok beck@ tb@

--- lib/libssl/s3_lib.c
+++ lib/libssl/s3_lib.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: s3_lib.c,v 1.195 2020/06/05 18:14:05 jsing Exp $ */
+/* $OpenBSD: s3_lib.c,v 1.196 2020/06/06 01:40:08 beck Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -1648,6 +1648,8 @@ ssl3_clear(SSL *s)
 
 	s->internal->packet_length = 0;
 	s->version = TLS1_VERSION;
+
+	S3I(s)->hs.state = SSL_ST_BEFORE|((s->server) ? SSL_ST_ACCEPT : SSL_ST_CONNECT);
 }
 
 static long
