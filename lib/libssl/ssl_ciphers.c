1bc1d4f9c287365f4431706af1b4c7c7fbff3721
schwarze <schwarze>
2020-09-15 15:28:38 +0000

    Do not destroy an existing cipher list when ssl_parse_ciphersuites() fails, to match the behaviour of ssl_create_cipher_list().  This also agrees with the behaviour of SSL_set_ciphersuites(3) in OpenSSL. Issue found while writing documentation. OK jsing@

--- lib/libssl/ssl_ciphers.c
+++ lib/libssl/ssl_ciphers.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ssl_ciphers.c,v 1.7 2020/09/13 16:49:05 jsing Exp $ */
+/*	$OpenBSD: ssl_ciphers.c,v 1.8 2020/09/14 17:52:38 tb Exp $ */
 /*
  * Copyright (c) 2015-2017 Doug Hogan <doug@openbsd.org>
  * Copyright (c) 2015-2018, 2020 Joel Sing <jsing@openbsd.org>
@@ -220,9 +220,6 @@ ssl_parse_ciphersuites(STACK_OF(SSL_CIPHER) **out_ciphers, const char *str)
 	int i;
 	int ret = 0;
 
-	sk_SSL_CIPHER_free(*out_ciphers);
-	*out_ciphers = NULL;
-
 	if ((ciphers = sk_SSL_CIPHER_new_null()) == NULL)
 		goto err;
 
@@ -255,6 +252,7 @@ ssl_parse_ciphersuites(STACK_OF(SSL_CIPHER) **out_ciphers, const char *str)
 	}
 
  done:
+	sk_SSL_CIPHER_free(*out_ciphers);
 	*out_ciphers = ciphers;
 	ciphers = NULL;
 	ret = 1;
