bd6f828d9dc2bd1dfeaa7ffba6e51ce7c4acc224
kettenis <kettenis>
2020-09-27 16:03:55 +0000

    Make sure acpicpu(4) doesn't attach to Processor nodes that happen to have a _HID.  Should fix the issues with broken AML that Janne Johansson reported. This issue should be fixed in a more generic way, but that is too risky for the upcoming release.
    tested by jmc@
    ok deraadt@

--- sys/dev/acpi/acpicpu.c
+++ sys/dev/acpi/acpicpu.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: acpicpu.c,v 1.87 2020/08/26 17:10:49 kettenis Exp $ */
+/* $OpenBSD: acpicpu.c,v 1.88 2020/08/27 01:08:55 jmatthew Exp $ */
 /*
  * Copyright (c) 2005 Marco Peereboom <marco@openbsd.org>
  * Copyright (c) 2015 Philip Guenther <guenther@openbsd.org>
@@ -654,7 +654,9 @@ acpicpu_match(struct device *parent, void *match, void *aux)
 	struct cfdata		*cf = match;
 	struct acpi_softc	*acpi = (struct acpi_softc *)parent;
 
-	if (acpi_matchhids(aa, acpicpu_hids, cf->cf_driver->cd_name)) {
+	if (acpi_matchhids(aa, acpicpu_hids, cf->cf_driver->cd_name) &&
+	    aa->aaa_node && aa->aaa_node->value &&
+	    aa->aaa_node->value->type == AML_OBJTYPE_DEVICE) {
 		/*
 		 * Record that we've seen a Device() CPU object,
 		 * so we won't attach any Processor() nodes.
