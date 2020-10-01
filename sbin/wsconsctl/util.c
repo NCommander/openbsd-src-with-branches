58ff41c17418ab02d5ec18636a63d963814e22c3
kettenis <kettenis>
2020-10-01 17:29:39 +0000

    Teach wsconsctl about astfb(4).

--- sbin/wsconsctl/util.c
+++ sbin/wsconsctl/util.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: util.c,v 1.67 2018/07/30 15:57:04 jcs Exp $ */
+/*	$OpenBSD: util.c,v 1.68 2020/03/03 22:04:30 kettenis Exp $ */
 /*	$NetBSD: util.c,v 1.8 2000/03/14 08:11:53 sato Exp $ */
 
 /*-
@@ -171,7 +171,8 @@ static const struct nameint dpytype_tab[] = {
 	{ WSDISPLAY_TYPE_INTELDRM,	"inteldrm" },
 	{ WSDISPLAY_TYPE_RADEONDRM,	"radeondrm" },
 	{ WSDISPLAY_TYPE_EFIFB,		"efifb" },
-	{ WSDISPLAY_TYPE_RKDRM,		"rkdrm" }
+	{ WSDISPLAY_TYPE_RKDRM,		"rkdrm" },
+	{ WSDISPLAY_TYPE_ASTFB,		"astfb" }
 };
 
 static const struct nameint kbdenc_tab[] = {
