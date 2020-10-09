189bb96c2797819d1f6a4e2d22c2f5cddc323e08
kn <kn>
2020-10-09 00:04:05 +0000

    Improve error message on missing permission
    In case "cmd" (and "args") in doas.conf(5) mismatch, the log syslog(3)
    message might be read as if the command was executed but failed, i.e.
    returned non-zero.
    
    Be unambiguous and help admins spot execution *attempts* as such:
    
    -Oct  9 01:05:20 eru doas: failed command for kn: echo bar
    +Oct  9 01:05:20 eru doas: command not permitted for kn: echo bar
    
    OK tedu deraadt

--- usr.bin/doas/doas.c
+++ usr.bin/doas/doas.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: doas.c,v 1.81 2019/09/14 17:47:00 semarie Exp $ */
+/* $OpenBSD: doas.c,v 1.82 2019/10/18 17:15:45 tedu Exp $ */
 /*
  * Copyright (c) 2015 Ted Unangst <tedu@openbsd.org>
  *
@@ -396,7 +396,7 @@ main(int argc, char **argv)
 	if (!permit(uid, groups, ngroups, &rule, target, cmd,
 	    (const char **)argv + 1)) {
 		syslog(LOG_AUTHPRIV | LOG_NOTICE,
-		    "failed command for %s: %s", mypw->pw_name, cmdline);
+		    "command not permitted for %s: %s", mypw->pw_name, cmdline);
 		errc(1, EPERM, NULL);
 	}
 
