6f6d6339f32f65fb4863f4438c0d29e0e8692fd4
deraadt <deraadt>
2020-10-14 22:11:52 +0000

    use symbolic SYS_exit rather than 1, and skip using the 0x400 %g2 syscall method ok kettenis

--- lib/csu/sparc64/md_init.h
+++ lib/csu/sparc64/md_init.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: md_init.h,v 1.6 2016/03/20 02:32:39 guenther Exp $ */
+/* $OpenBSD: md_init.h,v 1.7 2017/08/11 20:13:31 guenther Exp $ */
 
 /*-
  * Copyright (c) 2001 Ross Harvey
@@ -73,7 +73,6 @@
 	"	 mov	%g1, %o1		\n" \
 	"	.previous")
 
-
 #define	MD_RCRT0_START				\
 	__asm__(				\
 	".text					\n" \
@@ -103,8 +102,7 @@
 	"					\n" \
 	"	.global	_dl_exit		\n" \
 	"_dl_exit:				\n" \
-	"	mov	0x401, %g1		\n" \
-	"	add	%o7, 8, %g2		\n" \
+	"	mov	" STR(SYS_exit) ", %g1	\n" \
 	"	t	0			\n" \
 	"	retl				\n" \
 	"	 sub %g0, %o0, %o0		\n" \
