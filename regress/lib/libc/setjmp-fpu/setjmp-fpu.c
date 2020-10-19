cbdf3b844e27faff6d9dded3047e564a317ac16a
kettenis <kettenis>
2020-10-19 08:50:35 +0000

    Skip floating-point exception checks on arm64 and armv7 as the hardware (typically) doesn't implement support for these.
    ok patrick@, drahn@

--- regress/lib/libc/setjmp-fpu/setjmp-fpu.c
+++ regress/lib/libc/setjmp-fpu/setjmp-fpu.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: setjmp-fpu.c,v 1.5 2020/01/16 13:04:02 bluhm Exp $	*/
 
 #include <err.h>
 #include <fenv.h>
@@ -42,10 +42,12 @@ TEST_SETJMP(void)
 		rv = fegetround();
 		if (rv != FE_UPWARD)
 			errx(1, "fegetround returned %d, not FE_UPWARD", rv);
+#if !defined(__arm__) && !defined(__aarch64__)
 		rv = fegetexcept();
 		if (rv != FE_DIVBYZERO)
 			errx(1, "fegetexcept returned %d, not FE_DIVBYZERO",
 			    rv);
+#endif
 
 		/* Verify that the FPU exception flags weren't clobbered. */
 		flag = 0;
