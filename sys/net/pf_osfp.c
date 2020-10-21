aad8cb2fd3d90c7613cef1aff10a87e45cedcd76
kettenis <kettenis>
2020-10-21 16:15:32 +0000

    Provide dummy definitions for NET_LOCK() and PF_LOCK() when compiling this file as part of tcpdump(8).  Unbreaks the tree.
    ok deraadt@

--- sys/net/pf_osfp.c
+++ sys/net/pf_osfp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf_osfp.c,v 1.42 2020/10/21 11:36:56 mpi Exp $ */
+/*	$OpenBSD: pf_osfp.c,v 1.43 2020/10/21 15:35:25 sashan Exp $ */
 
 /*
  * Copyright (c) 2003 Mike Frantzen <frantzen@w4g.org>
@@ -59,6 +59,11 @@ typedef struct pool pool_t;
 #define pool_get(pool, flags)	malloc(*(pool))
 #define pool_put(pool, item)	free(item)
 #define pool_init(pool, size, a, ao, f, m, p)	(*(pool)) = (size)
+
+#define NET_LOCK()
+#define NET_UNLOCK()
+#define PF_LOCK()
+#define PF_UNLOCK()
 
 #ifdef PFDEBUG
 #include <sys/stdarg.h>	/* for DPFPRINTF() */
