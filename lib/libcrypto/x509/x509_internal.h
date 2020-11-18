f0790101786c6c749310ae3d1022228fbcbb6814
tb <tb>
2020-11-18 17:00:59 +0000

    KNF (whitespace)

--- lib/libcrypto/x509/x509_internal.h
+++ lib/libcrypto/x509/x509_internal.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: x509_internal.h,v 1.3 2020/09/15 11:55:14 beck Exp $ */
+/* $OpenBSD: x509_internal.h,v 1.4 2020/11/11 18:49:34 jsing Exp $ */
 /*
  * Copyright (c) 2020 Bob Beck <beck@openbsd.org>
  *
@@ -73,8 +73,8 @@ struct x509_verify_ctx {
 	size_t max_depth;		/* Max chain depth for validation */
 	size_t max_sigs;		/* Max number of signature checks */
 	size_t sig_checks;		/* Number of signature checks done */
-	size_t error_depth; 		/* Depth of last error seen */
-	int error; 			/* Last error seen */
+	size_t error_depth;		/* Depth of last error seen */
+	int error;			/* Last error seen */
 };
 
 int ASN1_time_tm_clamp_notafter(struct tm *tm);
