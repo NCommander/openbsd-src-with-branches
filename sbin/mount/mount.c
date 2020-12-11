ded415eec8e781fea7bb7c681484c860868f1431
robert <robert>
2020-12-11 06:29:43 +0000

    pass down the `skip' flag to the mountfs() function in the case of manually mounting a device which is not present in fstab(5) so that the `-s' flag can be used in this case as well.
    ok millert@, deraadt@

--- sbin/mount/mount.c
+++ sbin/mount/mount.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mount.c,v 1.72 2019/06/28 13:32:44 deraadt Exp $	*/
+/*	$OpenBSD: mount.c,v 1.73 2019/07/19 15:13:17 millert Exp $	*/
 /*	$NetBSD: mount.c,v 1.24 1995/11/18 03:34:29 cgd Exp $	*/
 
 /*
@@ -276,7 +276,7 @@ main(int argc, char * const argv[])
 		    mntonname, options, fs->fs_mntops, skip);
 		break;
 	case 2:
-		rval = mountfs(vfstype, argv[0], argv[1], options, NULL, 0);
+		rval = mountfs(vfstype, argv[0], argv[1], options, NULL, skip);
 		break;
 	default:
 		usage();
