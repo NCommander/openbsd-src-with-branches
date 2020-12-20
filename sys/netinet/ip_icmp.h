b1c77c19110af85c44c1d36da11614438819daf5
bluhm <bluhm>
2020-12-20 21:15:47 +0000

    Accept reject and blackhole routes for IPsec PMTU discovery.
    Since revision 1.87 of ip_icmp.c icmp_mtudisc_clone() ignored reject
    routes.  Otherwise TCP would clone these routes for PMTU discovery.
    They will not work, even after dynamic routing has found a better
    route than the reject route.
    
    With IPsec the use case is different.  First you need a route, but
    then the flow handles the packet without routing.  Usually this
    route should be a reject route to avoid sending unencrypted traffic
    if the flow is missing.  But IPsec needs this route for PMTU
    discovery, so use it for that.
    
    OK claudio@ tobhe@

--- sys/netinet/ip_icmp.h
+++ sys/netinet/ip_icmp.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_icmp.h,v 1.30 2017/04/14 20:46:31 bluhm Exp $	*/
+/*	$OpenBSD: ip_icmp.h,v 1.31 2018/11/05 21:50:39 claudio Exp $	*/
 /*	$NetBSD: ip_icmp.h,v 1.10 1996/02/13 23:42:28 christos Exp $	*/
 
 /*
@@ -239,7 +239,7 @@ int	icmp_reflect(struct mbuf *, struct mbuf **, struct in_ifaddr *);
 void	icmp_send(struct mbuf *, struct mbuf *);
 int	icmp_sysctl(int *, u_int, void *, size_t *, void *, size_t);
 struct rtentry *
-	icmp_mtudisc_clone(struct in_addr, u_int);
+	icmp_mtudisc_clone(struct in_addr, u_int, int);
 void	icmp_mtudisc(struct icmp *, u_int);
 int	icmp_do_exthdr(struct mbuf *, u_int16_t, u_int8_t, void *, size_t);
 #endif /* _KERNEL */
