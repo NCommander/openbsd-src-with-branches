4c2f4eaee23eddbbc53a8d2c865c16af76776c4f
cheloha <cheloha>
2020-12-23 20:48:06 +0000

    sigsuspend(2): change wmesg from "pause" to "sigsusp"
    Make it obvious where the thread is blocked.  "pause" is ambiguous.
    
    Tweaked by kettenis@.
    
    Probably ok kettenis@.

--- sys/kern/kern_sig.c
+++ sys/kern/kern_sig.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_sig.c,v 1.267 2020/12/04 15:16:45 mpi Exp $	*/
+/*	$OpenBSD: kern_sig.c,v 1.268 2020/12/07 16:55:28 mpi Exp $	*/
 /*	$NetBSD: kern_sig.c,v 1.54 1996/04/22 01:38:32 christos Exp $	*/
 
 /*
@@ -523,7 +523,7 @@ sys_sigsuspend(struct proc *p, void *v, register_t *retval)
 	struct sigacts *ps = pr->ps_sigacts;
 
 	dosigsuspend(p, SCARG(uap, mask) &~ sigcantmask);
-	while (tsleep_nsec(ps, PPAUSE|PCATCH, "pause", INFSLP) == 0)
+	while (tsleep_nsec(ps, PPAUSE|PCATCH, "sigsusp", INFSLP) == 0)
 		/* void */;
 	/* always return EINTR rather than ERESTART... */
 	return (EINTR);
