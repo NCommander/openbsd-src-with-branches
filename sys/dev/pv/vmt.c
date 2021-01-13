c313fd902f9cfcc61b7874f2ed557d4b8814e1dc
jmatthew <jmatthew>
2021-01-13 23:56:48 +0000

    Make vm_rpc_send_rpci_tx_buf() use the buffer passed in, not the buffer allocated as part of the softc, though it's always been the same buffer so far.
    ok dlg@

--- sys/dev/pv/vmt.c
+++ sys/dev/pv/vmt.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vmt.c,v 1.19 2020/06/24 22:03:40 cheloha Exp $ */
+/*	$OpenBSD: vmt.c,v 1.20 2021/01/13 23:50:21 jmatthew Exp $ */
 
 /*
  * Copyright (c) 2007 David Crawshaw <david@zentus.com>
@@ -1372,7 +1372,7 @@ vm_rpc_send_rpci_tx_buf(struct vmt_softc *sc, const uint8_t *buf,
 		return EIO;
 	}
 
-	if (vm_rpc_send(&rpci, sc->sc_rpc_buf, length) != 0) {
+	if (vm_rpc_send(&rpci, buf, length) != 0) {
 		DPRINTF("%s: unable to send rpci command\n", DEVNAME(sc));
 		result = EIO;
 		goto out;
