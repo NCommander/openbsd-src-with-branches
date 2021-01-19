f40d9cf3a20c4350bf7da8fdde2a182a2afc47c0
florian <florian>
2021-01-19 16:53:27 +0000

    No need for a global rad_process; unbreaks -fno-common. Problem reported by mortimer

--- usr.sbin/rad/engine.c
+++ usr.sbin/rad/engine.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: engine.c,v 1.15 2019/03/15 16:47:19 florian Exp $	*/
+/*	$OpenBSD: engine.c,v 1.16 2020/05/20 10:37:02 claudio Exp $	*/
 
 /*
  * Copyright (c) 2018 Florian Obser <florian@openbsd.org>
@@ -105,9 +105,8 @@ engine(int debug, int verbose)
 	if (chdir("/") == -1)
 		fatal("chdir(\"/\")");
 
-	rad_process = PROC_ENGINE;
-	setproctitle("%s", log_procnames[rad_process]);
-	log_procinit(log_procnames[rad_process]);
+	setproctitle("%s", "engine");
+	log_procinit("engine");
 
 	if (setgroups(1, &pw->pw_gid) ||
 	    setresgid(pw->pw_gid, pw->pw_gid, pw->pw_gid) ||
