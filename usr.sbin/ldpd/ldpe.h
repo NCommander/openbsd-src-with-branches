62740ee3a6c8b58281982e62b090ff930f102556
claudio <claudio>
2021-01-19 15:59:25 +0000

    Adjust the disc_recv_packet() code to not use IBUF_READ_SIZE and to use a local recv_buf that is allocated on first call with malloc(). The memory returned from malloc() is properly aligned which may not be the case for bss or stack memory.

--- usr.sbin/ldpd/ldpe.h
+++ usr.sbin/ldpd/ldpe.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ldpe.h,v 1.77 2019/12/12 00:10:29 yasuoka Exp $ */
+/*	$OpenBSD: ldpe.h,v 1.78 2021/01/19 15:14:35 claudio Exp $ */
 
 /*
  * Copyright (c) 2013, 2016 Renato Westphal <renato@openbsd.org>
@@ -270,8 +270,6 @@ void			 session_close(struct nbr *);
 struct tcp_conn		*tcp_new(int, struct nbr *);
 void			 pending_conn_del(struct pending_conn *);
 struct pending_conn	*pending_conn_find(int, union ldpd_addr *);
-
-char	*pkt_ptr;	/* packet buffer */
 
 /* pfkey.c */
 int	pfkey_read(int, struct sadb_msg *);
