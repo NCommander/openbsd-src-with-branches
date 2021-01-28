fcbbc237bb9619219ad00006feddb34583eb812f
ratchov <ratchov>
2021-01-28 11:15:31 +0000

    Dont attempt to drain disconnected clients
    Clients are always drained before they disconnect, so this change
    affects programs that die unexpectedly or loose thier network
    connection.
    
    Besides the bad style, this change fixes a theoretical bug when the
    disconnected client slot could be recycled and given to another client
    while it's being drained

--- usr.bin/sndiod/sock.c
+++ usr.bin/sndiod/sock.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sock.c,v 1.34 2020/04/25 05:03:54 ratchov Exp $	*/
+/*	$OpenBSD: sock.c,v 1.35 2020/04/26 14:13:22 ratchov Exp $	*/
 /*
  * Copyright (c) 2008-2012 Alexandre Ratchov <alex@caoua.org>
  *
@@ -1141,7 +1141,7 @@ sock_execmsg(struct sock *f)
 				f->ralign = s->round * s->mix.bpf;
 			}
 		}
-		slot_stop(s);
+		slot_stop(s, 1);
 		break;
 	case AMSG_SETPAR:
 #ifdef DEBUG
