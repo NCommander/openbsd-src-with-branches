b9c8d65e9d71add1666ccc49f7c7e9a537c3d20e
espie <espie>
2021-01-30 11:19:01 +0000

    do not print to STDOUT if we're in background, as requested by Theo who pointed at ssh code for process group handling. Thanks

--- usr.sbin/pkg_add/OpenBSD/ProgressMeter.pm
+++ usr.sbin/pkg_add/OpenBSD/ProgressMeter.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: ProgressMeter.pm,v 1.49 2016/06/23 16:11:23 espie Exp $
+# $OpenBSD: ProgressMeter.pm,v 1.50 2021/01/30 10:43:43 espie Exp $
 #
 # Copyright (c) 2010 Marc Espie <espie@openbsd.org>
 #
@@ -88,6 +88,12 @@ sub handle_continue
 {
 	my $self = shift;
 	$self->{continued} = 1;
+}
+
+sub can_output
+{
+	my $self = shift;
+	return $self->{state}->can_output;
 }
 
 # stub class when no actual progressmeter that still prints out.
