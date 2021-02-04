3cee6d81f29a3a8e8cf5e036235f20a8f9a52f4b
tobhe <tobhe>
2021-02-04 20:45:13 +0000

    'struct group' is now called 'struct dh_group'.

--- regress/sbin/iked/dh/dhtest.c
+++ regress/sbin/iked/dh/dhtest.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dhtest.c,v 1.2 2014/08/27 10:28:57 reyk Exp $	*/
+/*	$OpenBSD: dhtest.c,v 1.3 2020/11/03 20:45:58 tobhe Exp $	*/
 /*	$EOM: dhtest.c,v 1.1 1998/07/18 21:14:20 provos Exp $	*/
 
 /*
@@ -56,7 +56,7 @@ main(void)
 	struct ibuf *buf, *buf2;
 	struct ibuf *sec, *sec2;
 	uint8_t *raw, *raw2;
-	struct group *group, *group2;
+	struct dh_group *group, *group2;
 	const char *name[] = { "MODP", "ECP", "CURVE25519" };
 
 	group_init();
