d99891f69c24cad48c96c8ea04f6f0f6ba08ee02
ajacoutot <ajacoutot>
2021-02-18 06:17:35 +0000

    Only pick up files matching alphanumeric and underscore characters under /etc/rc.d. This prevents issues with editor backup files (foo~) or other bogus files.
    ok sthen@

--- usr.sbin/rcctl/rcctl.sh
+++ usr.sbin/rcctl/rcctl.sh
@@ -1,6 +1,6 @@
 #!/bin/ksh
 #
-# $OpenBSD: rcctl.sh,v 1.107 2018/10/21 21:20:40 ajacoutot Exp $
+# $OpenBSD: rcctl.sh,v 1.108 2020/03/31 08:03:44 ajacoutot Exp $
 #
 # Copyright (c) 2014, 2015 Antoine Jacoutot <ajacoutot@openbsd.org>
 # Copyright (c) 2014 Ingo Schwarze <schwarze@openbsd.org>
@@ -54,7 +54,7 @@ ls_rcscripts()
 
 	cd /etc/rc.d && set -- *
 	for _s; do
-		[[ ${_s} = *.* ]] && continue
+		[[ ${_s} == +([[:alnum:]_]) ]] || continue
 		[ ! -d "${_s}" ] && echo "${_s}"
 	done
 }
