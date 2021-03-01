abd15c98b4f9641c03a08e0f53b4ab926bf742ba
bluhm <bluhm>
2021-03-01 11:05:43 +0000

    Refactor ip_fragment() and ip6_fragment().  Use a mbuf list to simplify the handling of the fragment list.  Now the functions ip_fragment() and ip6_fragment() always consume the mbuf.  They free the mbuf and mbuf list in case of an error and take care about the counter.  Adjust the code a bit to make v4 and v6 look similar. Fixes a potential mbuf leak when pf_route6() called pf_refragment6() and it failed.  Now the mbuf is always freed by ip6_fragment(). OK dlg@ mvs@

--- sys/netinet6/ip6_var.h
+++ sys/netinet6/ip6_var.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip6_var.h,v 1.86 2019/12/08 11:08:22 sashan Exp $	*/
+/*	$OpenBSD: ip6_var.h,v 1.87 2021/01/11 13:28:54 bluhm Exp $	*/
 /*	$KAME: ip6_var.h,v 1.33 2000/06/11 14:59:20 jinmei Exp $	*/
 
 /*
@@ -322,7 +322,7 @@ void	ip6_forward(struct mbuf *, struct rtentry *, int);
 void	ip6_mloopback(struct ifnet *, struct mbuf *, struct sockaddr_in6 *);
 int	ip6_output(struct mbuf *, struct ip6_pktopts *, struct route_in6 *, int,
 	    struct ip6_moptions *, struct inpcb *);
-int	ip6_fragment(struct mbuf *, int, u_char, u_long);
+int	ip6_fragment(struct mbuf *, struct mbuf_list *, int, u_char, u_long);
 int	ip6_ctloutput(int, struct socket *, int, int, struct mbuf *);
 int	ip6_raw_ctloutput(int, struct socket *, int, int, struct mbuf *);
 void	ip6_initpktopts(struct ip6_pktopts *);
