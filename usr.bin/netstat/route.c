1e6c540568344dd440fc1a5df005295650611e52
claudio <claudio>
2021-03-17 09:03:51 +0000

    Netstat wants to access kernel internal structures for the kvm walker of the routing table.  Define _KERNEL around the net/route.h include. OK florian@

--- usr.bin/netstat/route.c
+++ usr.bin/netstat/route.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: route.c,v 1.105 2020/06/12 06:22:32 remi Exp $	*/
+/*	$OpenBSD: route.c,v 1.106 2020/12/29 19:51:52 benno Exp $	*/
 /*	$NetBSD: route.c,v 1.15 1996/05/07 02:55:06 thorpej Exp $	*/
 
 /*
@@ -37,7 +37,9 @@
 
 #include <net/if.h>
 #include <net/if_types.h>
+#define _KERNEL
 #include <net/route.h>
+#undef _KERNEL
 #include <netinet/ip_ipsp.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
