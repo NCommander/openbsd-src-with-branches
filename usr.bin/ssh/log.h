d7a5f18ede675627ad3cc60d9b75578c9a0ec00b
djm <djm>
2021-04-06 23:24:30 +0000

    include "ssherr.h" not <ssherr.h>; from Balu Gajjala via bz#3292

--- usr.bin/ssh/log.h
+++ usr.bin/ssh/log.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: log.h,v 1.30 2020/12/04 02:25:13 djm Exp $ */
+/* $OpenBSD: log.h,v 1.31 2021/04/03 05:54:14 djm Exp $ */
 
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
@@ -16,7 +16,7 @@
 #define SSH_LOG_H
 
 #include <stdarg.h> /* va_list */
-#include <ssherr.h> /* ssh_err() */
+#include "ssherr.h" /* ssh_err() */
 
 /* Supported syslog facilities and levels. */
 typedef enum {
