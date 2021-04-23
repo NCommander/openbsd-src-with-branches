3eb2ada45bbfd993d5a51a0408aa6fa88311989c
bluhm <bluhm>
2021-04-23 21:47:32 +0000

    The variable la_hold_total contains the number of packets currently in the arp queue.  So the sysctl net.inet.ip.arpqueued must be read only.  In if_ether.c include the header with the declaration of la_hold_total to ensure that the definition matches. OK mvs@

--- sys/netinet/if_ether.c
+++ sys/netinet/if_ether.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ether.c,v 1.242 2019/11/07 11:23:23 krw Exp $	*/
+/*	$OpenBSD: if_ether.c,v 1.243 2020/06/24 22:03:43 cheloha Exp $	*/
 /*	$NetBSD: if_ether.c,v 1.31 1996/05/11 12:59:58 mycroft Exp $	*/
 
 /*
@@ -60,6 +60,7 @@
 #include <netinet/in.h>
 #include <netinet/in_var.h>
 #include <netinet/if_ether.h>
+#include <netinet/ip_var.h>
 #if NCARP > 0
 #include <netinet/ip_carp.h>
 #endif
