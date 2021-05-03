3c67a9f25d170714e1a4be27f99a5c061ab55e0f
inoguchi <inoguchi>
2021-05-03 23:44:05 +0000

    Use limits.h instead of sys/limits.h in dtlstest.c for portable
    discussed and input from jsing@

--- regress/lib/libssl/dtls/dtlstest.c
+++ regress/lib/libssl/dtls/dtlstest.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: dtlstest.c,v 1.6 2021/02/06 07:34:34 jsing Exp $ */
+/* $OpenBSD: dtlstest.c,v 1.7 2021/02/07 14:52:17 jsing Exp $ */
 /*
  * Copyright (c) 2020 Joel Sing <jsing@openbsd.org>
  *
@@ -16,10 +16,10 @@
  */
 
 #include <netinet/in.h>
-#include <sys/limits.h>
 #include <sys/socket.h>
 
 #include <err.h>
+#include <limits.h>
 #include <poll.h>
 #include <unistd.h>
 
