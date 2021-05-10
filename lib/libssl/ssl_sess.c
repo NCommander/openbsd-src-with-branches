0d332178386893bba556127c8ea0265912fd4ac6
tb <tb>
2021-05-10 17:07:24 +0000

    Provide SSL_SESSION_get0_cipher(3)
    ok jsing

--- lib/libssl/ssl_sess.c
+++ lib/libssl/ssl_sess.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssl_sess.c,v 1.101 2020/10/11 02:22:27 jsing Exp $ */
+/* $OpenBSD: ssl_sess.c,v 1.102 2021/02/20 08:30:52 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -871,6 +871,12 @@ int
 SSL_SESSION_get_protocol_version(const SSL_SESSION *s)
 {
 	return s->ssl_version;
+}
+
+const SSL_CIPHER *
+SSL_SESSION_get0_cipher(const SSL_SESSION *s)
+{
+	return s->cipher;
 }
 
 X509 *
