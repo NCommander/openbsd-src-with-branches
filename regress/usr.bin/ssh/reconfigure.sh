90e2c1d05273d1f4f23daa32909776ad43cc1ea9
dtucker <dtucker>
2021-06-10 09:43:51 +0000

    Use $SUDO when reading sshd's pidfile in case it was created with a very restrictive umask.  This resyncs with -portable.

--- regress/usr.bin/ssh/reconfigure.sh
+++ regress/usr.bin/ssh/reconfigure.sh
@@ -1,4 +1,4 @@
-#	$OpenBSD: reconfigure.sh,v 1.6 2017/04/30 23:34:55 djm Exp $
+#	$OpenBSD: reconfigure.sh,v 1.7 2021/06/10 03:45:31 dtucker Exp $
 #	Placed in the Public Domain.
 
 tid="simple connect after reconfigure"
@@ -12,7 +12,7 @@ if [ $? -ne 0 ]; then
 	fail "ssh connect with failed before reconfigure"
 fi
 
-PID=`cat $PIDFILE`
+PID=`$SUDO cat $PIDFILE`
 rm -f $PIDFILE
 $SUDO kill -HUP $PID
 
