1d6b7193549540d7716b0ad2ed244c78162e34db
claudio <claudio>
2021-06-15 18:42:23 +0000

    Remove the uvshrink logic and keep the unveil list in the order of insertion. unveil_lookup() is now doing a dumb linear search. The problem with the uvshrink logic was that ps_uvpcwd was a pointer into this array and after compation it pointed to the wrong element. Also future unveil caches would suffer from the same issue. OK semarie@

--- sys/sys/proc.h
+++ sys/sys/proc.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: proc.h,v 1.311 2021/03/17 14:06:54 visa Exp $	*/
+/*	$OpenBSD: proc.h,v 1.312 2021/05/12 08:09:33 mvs Exp $	*/
 /*	$NetBSD: proc.h,v 1.44 1996/04/22 01:23:21 christos Exp $	*/
 
 /*-
@@ -230,10 +230,9 @@ struct process {
 
 	struct unveil *ps_uvpaths;	/* unveil vnodes and names */
 	struct unveil *ps_uvpcwd;	/* pointer to unveil of cwd, NULL if none */
-	ssize_t ps_uvvcount;		/* count of unveil vnodes held */
-	size_t ps_uvncount;		/* count of unveil names allocated */
-	int ps_uvshrink;		/* do we need to shrink vnode list */
-	int ps_uvdone;			/* no more unveil is permitted */
+	ssize_t	ps_uvvcount;		/* count of unveil vnodes held */
+	size_t	ps_uvncount;		/* count of unveil names allocated */
+	int	ps_uvdone;		/* no more unveil is permitted */
 
 /* End area that is zeroed on creation. */
 #define	ps_endzero	ps_startcopy
