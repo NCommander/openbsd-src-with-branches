b176bc1f2dc84ff3116740da98ee7c823382f00d
krw <krw>
2021-06-16 15:40:47 +0000

    Ensure that the '-i' command line option and the 'reinit [mbr]' editing commnd completely remove any existing on-disk GPT header when writing the MBR.
    '-ig'/'reinit gpt' must be used to create GPT format.
    
    Previous interactions between '-i', '-b' and overly clever
    'reinit' produced surprising, inconsistent and incorrect
    behaviour.
    
    ok deraadt@ kettenis@

--- sbin/fdisk/gpt.h
+++ sbin/fdisk/gpt.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: gpt.h,v 1.10 2021/05/19 21:49:07 krw Exp $	*/
+/*	$OpenBSD: gpt.h,v 1.11 2021/06/12 17:19:13 krw Exp $	*/
 /*
  * Copyright (c) 2015 Markus Muller <mmu@grummel.net>
  * Copyright (c) 2015 Kenneth R Westerback <krw@openbsd.org>
@@ -22,6 +22,7 @@ int		GPT_get_lba_end(unsigned int);
 
 int		GPT_init(void);
 int		GPT_write(void);
+void		GPT_zap_headers(void);
 void		GPT_print(char *, int);
 void		GPT_print_part(int, char *, int);
 void		GPT_print_parthdr(int);
