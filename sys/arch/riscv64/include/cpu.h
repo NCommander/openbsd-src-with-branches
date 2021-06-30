9e7bcffa2833ef927f9a0d7684fede4002814ed6
kettenis <kettenis>
2021-06-30 22:20:56 +0000

    Simplify the way we track the FPU state, using powerpc64 as a model. The new code still uses the clean/dirty state that the hardware reports to optimize saving/restoring the FPU register, but no longer attempts to keep the FPU registers alive across a context switch.  Fixes panics seen on MP kernels.
    ok drahn@

--- sys/arch/riscv64/include/cpu.h
+++ sys/arch/riscv64/include/cpu.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cpu.h,v 1.7 2021/06/02 00:39:27 cheloha Exp $	*/
+/*	$OpenBSD: cpu.h,v 1.8 2021/06/29 21:27:52 kettenis Exp $	*/
 
 /*
  * Copyright (c) 2019 Mike Larkin <mlarkin@openbsd.org>
@@ -84,7 +84,6 @@ struct cpu_info {
 
 	struct proc		*ci_curproc;
 	struct pmap		*ci_curpm;
-	struct proc		*ci_fpuproc;
 	u_int32_t		ci_randseed;
 
 	struct pcb		*ci_curpcb;
