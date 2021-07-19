84f1b80347d7773728c5f32cbf29e769d0235465
jsing <jsing>
2021-07-19 08:42:24 +0000

    Mop up dtls1_get_ccs_header() and struct ccs_header_st.
    All this code does is read one byte from memory with an unknown length,
    potentially being a one byte overread... and then nothing is actually done
    with the value.
    
    ok tb@

--- lib/libssl/d1_both.c
+++ lib/libssl/d1_both.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: d1_both.c,v 1.75 2021/06/11 17:29:48 jsing Exp $ */
+/* $OpenBSD: d1_both.c,v 1.76 2021/07/01 17:53:39 jsing Exp $ */
 /*
  * DTLS implementation written by Nagendra Modadugu
  * (nagendra@cs.stanford.edu) for the OpenSSL project 2005.
@@ -1201,12 +1201,4 @@ dtls1_get_message_header(unsigned char *data, struct hm_header_st *msg_hdr)
 	msg_hdr->frag_len = frag_len;
 
 	return 1;
-}
-
-void
-dtls1_get_ccs_header(unsigned char *data, struct ccs_header_st *ccs_hdr)
-{
-	memset(ccs_hdr, 0, sizeof(struct ccs_header_st));
-
-	ccs_hdr->type = *(data++);
 }
