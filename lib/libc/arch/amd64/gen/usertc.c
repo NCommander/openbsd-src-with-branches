fc14d8df1f56b6dc2156adee5cbde31e4c025d59
jca <jca>
2021-07-25 22:58:39 +0000

    Consistently use __asm instead of asm/__asm__
    ok kettenis@

--- lib/libc/arch/amd64/gen/usertc.c
+++ lib/libc/arch/amd64/gen/usertc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: usertc.c,v 1.2 2020/07/08 09:17:48 kettenis Exp $ */
+/*	$OpenBSD: usertc.c,v 1.3 2020/08/23 21:38:47 cheloha Exp $ */
 /*
  * Copyright (c) 2020 Paul Irofti <paul@irofti.net>
  *
@@ -22,7 +22,7 @@ static inline u_int
 rdtsc_lfence(void)
 {
 	uint32_t hi, lo;
-	asm volatile("lfence; rdtsc" : "=a"(lo), "=d"(hi));
+	__asm volatile("lfence; rdtsc" : "=a"(lo), "=d"(hi));
 	return ((uint64_t)lo)|(((uint64_t)hi)<<32);
 }
 
