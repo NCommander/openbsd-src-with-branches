46aea6d250a8822c28f6686e86996367357a18b9
dtucker <dtucker>
2021-08-06 09:00:18 +0000

    Make diff invocation more portable.  POSIX does not require diff to have -N, so compare in both directions with just -r, which should catch missing files in either directory.

--- regress/usr.bin/ssh/scp3.sh
+++ regress/usr.bin/ssh/scp3.sh
@@ -1,4 +1,4 @@
-#	$OpenBSD$
+#	$OpenBSD: scp3.sh,v 1.1 2021/08/04 21:28:00 djm Exp $
 #	Placed in the Public Domain.
 
 tid="scp3"
@@ -44,7 +44,8 @@ for mode in scp ; do
 	rm -rf ${DIR2}
 	cp ${DATA} ${DIR}/copy
 	$SCP $scpopts -3r hostA:${DIR} hostB:${DIR2} || fail "copy failed"
-	diff -rN ${DIR} ${DIR2} || fail "corrupted copy"
+	diff -r ${DIR} ${DIR2} || fail "corrupted copy"
+	diff -r ${DIR2} ${DIR} || fail "corrupted copy"
 
 	verbose "$tag: detect non-directory target"
 	scpclean
