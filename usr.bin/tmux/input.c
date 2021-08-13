3deb135e262909dafde1cd77b47db46b16aab40f
nicm <nicm>
2021-08-13 06:52:51 +0000

    Change focus to be driven by events rather than walking all panes at end of event loop, this way the ordering of in and out can be enforced. GitHub issue 2808.

--- usr.bin/tmux/input.c
+++ usr.bin/tmux/input.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: input.c,v 1.189 2021/06/10 07:24:10 nicm Exp $ */
+/* $OpenBSD: input.c,v 1.190 2021/08/11 20:49:55 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -1792,8 +1792,12 @@ input_csi_dispatch_sm_private(struct input_ctx *ictx)
 			if (sctx->s->mode & MODE_FOCUSON)
 				break;
 			screen_write_mode_set(sctx, MODE_FOCUSON);
-			if (wp != NULL)
-				wp->flags |= PANE_FOCUSPUSH; /* force update */
+			if (wp == NULL)
+				break;
+			if (wp->flags & PANE_FOCUSED)
+				bufferevent_write(wp->event, "\033[I", 3);
+			else
+				bufferevent_write(wp->event, "\033[O", 3);
 			break;
 		case 1005:
 			screen_write_mode_set(sctx, MODE_MOUSE_UTF8);
--- usr.bin/tmux/tty-keys.c
+++ usr.bin/tmux/tty-keys.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: tty-keys.c,v 1.147 2021/06/10 07:21:10 nicm Exp $ */
+/* $OpenBSD: tty-keys.c,v 1.148 2021/08/06 09:19:02 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -821,11 +821,13 @@ complete_key:
 
 	/* Check for focus events. */
 	if (key == KEYC_FOCUS_OUT) {
-		tty->client->flags &= ~CLIENT_FOCUSED;
+		c->flags &= ~CLIENT_FOCUSED;
+		window_update_focus(c->session->curw->window);
 		notify_client("client-focus-out", c);
 	} else if (key == KEYC_FOCUS_IN) {
-		tty->client->flags |= CLIENT_FOCUSED;
+		c->flags |= CLIENT_FOCUSED;
 		notify_client("client-focus-in", c);
+		window_update_focus(c->session->curw->window);
 	}
 
 	/* Fire the key. */
