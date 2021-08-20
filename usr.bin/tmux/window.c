47460a793257a394281067818b6dbdb755007f47
nicm <nicm>
2021-08-20 17:36:03 +0000

    Fill colour palette correctly from option for new panes, GitHub issue 2831.

--- usr.bin/tmux/window.c
+++ usr.bin/tmux/window.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: window.c,v 1.273 2021/08/11 20:49:55 nicm Exp $ */
+/* $OpenBSD: window.c,v 1.274 2021/08/13 06:52:51 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -924,6 +924,8 @@ window_pane_create(struct window *w, u_int sx, u_int sy, u_int hlimit)
 	wp->pipe_fd = -1;
 
 	colour_palette_init(&wp->palette);
+	colour_palette_from_option(&wp->palette, wp->options);
+
 	screen_init(&wp->base, sx, sy, hlimit);
 	wp->screen = &wp->base;
 
