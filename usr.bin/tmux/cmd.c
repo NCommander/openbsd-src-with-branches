5177cf7707c53031f16ee279fa484593dc2a95d0
nicm <nicm>
2021-08-21 08:44:59 +0000

    Wrap command argument definitions in their own struct.

--- usr.bin/tmux/cmd.c
+++ usr.bin/tmux/cmd.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: cmd.c,v 1.165 2021/08/20 19:34:51 nicm Exp $ */
+/* $OpenBSD: cmd.c,v 1.166 2021/08/20 19:50:17 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -518,8 +518,7 @@ cmd_parse(int argc, char **argv, const char *file, u_int line, char **cause)
 		return (NULL);
 	cmd_log_argv(argc, argv, "%s: %s", __func__, entry->name);
 
-	args = args_parse(entry->args.template, argc, argv, entry->args.lower,
-	    entry->args.upper);
+	args = args_parse(&entry->args, argc, argv);
 	if (args == NULL)
 		goto usage;
 
