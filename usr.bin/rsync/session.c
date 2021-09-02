097caeef1b18b8454f4cc66ed3f1e9f61580a8a1
deraadt <deraadt>
2021-09-02 21:06:06 +0000

    remove sys/param.h that snuck in, by replacing MAXPATHLEN with PATH_MAX

--- usr.bin/rsync/session.c
+++ usr.bin/rsync/session.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: session.c,v 1.7 2019/05/08 20:00:25 benno Exp $ */
+/*	$OpenBSD: session.c,v 1.8 2021/06/30 13:10:04 claudio Exp $ */
 /*
  * Copyright (c) 2019 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -14,7 +14,6 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-#include <sys/param.h>
 
 #include <assert.h>
 #include <stdint.h>
