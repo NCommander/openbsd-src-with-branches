c501271e4a758c64ed6c0670430814f1435b98c4
job <job>
2021-09-02 12:41:44 +0000

    Lay groundwork to support X.509 v3 extensions for IP Addresses and AS Identifiers
    These extensions are defined in RFC 3779 and used in the RPKI (RFC 6482, RFC 8360).
    Imported from OpenSSL 1.1.1j (aaf2fcb575cdf6491b98ab4829abf78a3dec8402b8b81efc8f23c00d443981bf)
    This changeset is a no-op, as there are 10+ issues and at least 2 security issues.
    Work will continue in-tree.
    
    OK tb@, discussed with beck@

--- lib/libcrypto/x509/ext_dat.h
+++ lib/libcrypto/x509/ext_dat.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: ext_dat.h,v 1.13 2016/12/21 15:49:29 jsing Exp $ */
+/* $OpenBSD: ext_dat.h,v 1.1 2020/06/04 15:19:31 jsing Exp $ */
 /* Written by Dr Stephen N Henson (steve@openssl.org) for the OpenSSL
  * project 1999.
  */
@@ -105,6 +105,10 @@ static const X509V3_EXT_METHOD *standard_exts[] = {
 #endif
 	&v3_sxnet,
 	&v3_info,
+#ifndef OPENSSL_NO_RFC3779
+	&v3_addr,
+	&v3_asid,
+#endif
 #ifndef OPENSSL_NO_OCSP
 	&v3_ocsp_nonce,
 	&v3_ocsp_crlid,
