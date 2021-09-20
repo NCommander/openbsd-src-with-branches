28de7c49deb38ed2ea1d72477dc2c3c831803eb2
djm <djm>
2021-09-20 01:55:42 +0000

    Switch scp back to use the old protocol by default, ahead of release. We'll wait a little longer for people to pick up sftp-server(8) that supports the extension that scp needs for ~user paths to continue working in SFTP protocol mode. Discussed with deraadt@

--- usr.bin/ssh/scp.c
+++ usr.bin/ssh/scp.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: scp.c,v 1.236 2021/09/16 15:22:22 djm Exp $ */
+/* $OpenBSD: scp.c,v 1.237 2021/09/18 02:03:25 djm Exp $ */
 /*
  * scp - secure remote copy.  This is basically patched BSD rcp which
  * uses ssh to do the data transfer (instead of using rcmd).
@@ -422,7 +422,7 @@ main(int argc, char **argv)
 	const char *errstr;
 	extern char *optarg;
 	extern int optind;
-	enum scp_mode_e mode = MODE_SFTP;
+	enum scp_mode_e mode = MODE_SCP;
 	char *sftp_direct = NULL;
 
 	/* Ensure that fds 0, 1 and 2 are open or directed to /dev/null */
