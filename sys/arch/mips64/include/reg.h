b55fd1792bcafb565c1350f1ede0af9024079de9
visa <visa>
2021-10-07 12:32:10 +0000

    Use tabs instead of spaces.

--- sys/arch/mips64/include/reg.h
+++ sys/arch/mips64/include/reg.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: reg.h,v 1.3 2011/03/23 16:54:36 pirofti Exp $	*/
+/*	$OpenBSD: reg.h,v 1.4 2021/10/07 12:31:03 visa Exp $	*/
 
 /*
  * Copyright (c) 1988 University of Utah.
@@ -49,6 +49,6 @@
 #include <machine/regnum.h>
 
 struct reg {
-        register_t     r_regs[NREGS];	/* numbered as above */
+	register_t	r_regs[NREGS];	/* numbered as above */
 };
 #endif /* !_MIPS64_REG_H_ */
