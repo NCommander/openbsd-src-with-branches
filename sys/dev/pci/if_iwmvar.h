b402b236e8c3a525d1dcf181cf27e914f33b4db6
stsp <stsp>
2021-10-07 08:15:04 +0000

    Make our old BSSID available to iwm_newstate() when roaming.
    ic_bss->ni_bssid has already been overwritten once we enter
    iwm_newstate() to perform the state transitions necessary for
    roaming to our new access point (RUN->AUTH->ASSOC->RUN).
    
    We do however use the BSSID in commands sent to firmware.
    Cache our BSSID in struct iwm_node such that firmware commands
    keep using the old BSSID while we are still tearing things down.
    Switch to the new BSSID only once we start back up in iwm_auth().
    This should be consistent from the firmware's point of view.
    
    ok mpi@

--- sys/dev/pci/if_iwmvar.h
+++ sys/dev/pci/if_iwmvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_iwmvar.h,v 1.68 2021/07/08 00:12:49 stsp Exp $	*/
+/*	$OpenBSD: if_iwmvar.h,v 1.69 2021/07/08 17:14:08 stsp Exp $	*/
 
 /*
  * Copyright (c) 2014 genua mbh <info@genua.de>
@@ -654,6 +654,7 @@ struct iwm_softc {
 struct iwm_node {
 	struct ieee80211_node in_ni;
 	struct iwm_phy_ctxt *in_phyctxt;
+	uint8_t in_macaddr[ETHER_ADDR_LEN];
 
 	uint16_t in_id;
 	uint16_t in_color;
