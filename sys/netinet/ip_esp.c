e3970fd850896b48a10364cedf1132c11971e148
tobhe <tobhe>
2021-11-04 14:45:07 +0000

    The authenticator is removed elsewhere.
    ok patrick@

--- sys/netinet/ip_esp.c
+++ sys/netinet/ip_esp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_esp.c,v 1.183 2021/10/24 17:08:27 bluhm Exp $ */
+/*	$OpenBSD: ip_esp.c,v 1.184 2021/10/24 23:33:37 tobhe Exp $ */
 /*
  * The authors of this code are John Ioannidis (ji@tla.org),
  * Angelos D. Keromytis (kermit@csd.uch.gr) and
@@ -682,7 +682,7 @@ esp_input(struct mbuf **mp, struct tdb *tdb, int skip, int protoff)
 		goto drop;
 	}
 
-	/* Trim the mbuf chain to remove the trailing authenticator and padding */
+	/* Trim the mbuf chain to remove the padding */
 	m_adj(m, -(lastthree[1] + 2));
 
 	/* Restore the Next Protocol field */
