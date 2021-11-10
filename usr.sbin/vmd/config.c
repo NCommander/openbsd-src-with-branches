a76056361611a40845b7892045091140f63285c5
sthen <sthen>
2021-11-10 20:49:04 +0000

    Remove switch(4) pieces, feedback/missing piece millert@ ok claudio@ mlarkin@

--- usr.sbin/vmd/config.c
+++ usr.sbin/vmd/config.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: config.c,v 1.62 2021/05/12 02:24:56 mlarkin Exp $	*/
+/*	$OpenBSD: config.c,v 1.63 2021/07/18 11:55:45 dv Exp $	*/
 
 /*
  * Copyright (c) 2015 Reyk Floeter <reyk@openbsd.org>
@@ -40,7 +40,7 @@
 #include "vmd.h"
 
 /* Supported bridge types */
-const char *vmd_descsw[] = { "switch", "bridge", "veb", NULL };
+const char *vmd_descsw[] = { "bridge", "veb", NULL };
 
 static int	 config_init_localprefix(struct vmd_config *);
 
