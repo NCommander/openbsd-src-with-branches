00ac3cf787d895f890be695dd6c7b092ba2037bc
deraadt <deraadt>
2021-11-25 23:03:05 +0000

    move label to fix RAMDISK

--- sys/netinet/tcp_input.c
+++ sys/netinet/tcp_input.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tcp_input.c,v 1.370 2021/08/09 17:03:08 bluhm Exp $	*/
+/*	$OpenBSD: tcp_input.c,v 1.371 2021/11/25 13:46:02 bluhm Exp $	*/
 /*	$NetBSD: tcp_input.c,v 1.23 1996/02/13 23:43:44 christos Exp $	*/
 
 /*
@@ -2275,8 +2275,8 @@ tcp_dooptions(struct tcpcb *tp, u_char *cp, int cnt, struct tcphdr *th,
 
 	return (0);
 
- bad:
 #ifdef TCP_SIGNATURE
+ bad:
 	tdb_unref(tdb);
 #endif /* TCP_SIGNATURE */
 	return (-1);
