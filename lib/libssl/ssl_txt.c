72b6d5783702fcfc91706c25713ccfc595eeb7d9
tb <tb>
2021-11-29 18:36:27 +0000

    Stop using BIO_s_file_inernal() in libssl.
    BIO_s_file_internal() should never have leaked out of libcrypto,
    but it did. As a first step of getting rid of it, stop using it
    internally.
    
    ok jsing

--- lib/libssl/ssl_txt.c
+++ lib/libssl/ssl_txt.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssl_txt.c,v 1.29 2021/06/11 11:13:53 jsing Exp $ */
+/* $OpenBSD: ssl_txt.c,v 1.30 2021/10/23 08:13:02 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -94,7 +94,7 @@ SSL_SESSION_print_fp(FILE *fp, const SSL_SESSION *x)
 	BIO *b;
 	int ret;
 
-	if ((b = BIO_new(BIO_s_file_internal())) == NULL) {
+	if ((b = BIO_new(BIO_s_file())) == NULL) {
 		SSLerrorx(ERR_R_BUF_LIB);
 		return (0);
 	}
