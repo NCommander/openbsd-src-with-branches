aa085fefa25e5cb5563f08af5d41abe735ec9f44
cheloha <cheloha>
2021-12-05 22:00:42 +0000

    kbind(2): disable system call if not initialized before first __tfork(2)
    To unlock kbind(2) we need to protect ps_kbind_addr and
    ps_kbind_cookie.
    
    The simplest way to do this is to disallow kbind(2) initialization
    after the first __tfork(2) call.  If the first thread does not
    initialize the kbind(2) variables before __tfork(2) then we disable
    kbind(2) during that first __tfork(2) call.
    
    This is guenther@'s patch, I'm just committing it.
    
    Discussed with guenther@, deraadt@, kettenis@, and mpi@.
    
    ok kettenis@, positive response from mpi@, "I am busy" guenther@

--- sys/uvm/uvm_mmap.c
+++ sys/uvm/uvm_mmap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_mmap.c,v 1.163 2020/10/07 12:26:20 mpi Exp $	*/
+/*	$OpenBSD: uvm_mmap.c,v 1.164 2021/03/26 13:40:05 mpi Exp $	*/
 /*	$NetBSD: uvm_mmap.c,v 1.49 2001/02/18 21:19:08 chs Exp $	*/
 
 /*
@@ -1073,8 +1073,6 @@ uvm_mmapfile(vm_map_t map, vaddr_t *addr, vsize_t size, vm_prot_t prot,
 	return error;
 }
 
-/* an address that can't be in userspace or kernelspace */
-#define	BOGO_PC	(u_long)-1
 int
 sys_kbind(struct proc *p, void *v, register_t *retval)
 {
