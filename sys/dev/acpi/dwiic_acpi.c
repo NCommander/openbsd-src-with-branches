9516a2300b5a0eddf5b34e2f4b01adacd58ad4a1
kettenis <kettenis>
2021-12-07 10:16:50 +0000

    Fix parsing of SR_IRQ resource descriptors here as well.
    ok patrick@, anton@

--- sys/dev/acpi/dwiic_acpi.c
+++ sys/dev/acpi/dwiic_acpi.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: dwiic_acpi.c,v 1.15 2020/07/23 09:21:51 jsg Exp $ */
+/* $OpenBSD: dwiic_acpi.c,v 1.16 2020/08/22 22:29:28 kettenis Exp $ */
 /*
  * Synopsys DesignWare I2C controller
  *
@@ -204,7 +204,7 @@ dwiic_acpi_parse_crs(int crsidx, union acpi_resource *crs, void *arg)
 	case SR_IRQ:
 		sc_crs->irq_int = ffs(letoh16(crs->sr_irq.irq_mask)) - 1;
 		/* Default is exclusive, active-high, edge triggered. */
-		if (AML_CRSLEN(crs) < 3)
+		if (AML_CRSLEN(crs) < 4)
 			flags = SR_IRQ_MODE;
 		else
 			flags = crs->sr_irq.irq_flags;
