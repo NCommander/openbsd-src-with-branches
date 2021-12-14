bce69210379cca588874ef805fadffc5f6b96da5
jsing <jsing>
2021-12-14 17:35:21 +0000

    Consolidate ASN.1 universal tag type data.
    There are currently three different tables in three different files that
    contain information about ASN.1 universal class tag types. Range checking
    is also implemented in three different places (with different
    implementations).
    
    Consolidate all of this into a single table, provide a lookup function that
    deals with the range checks and wrappers to deal with specific types.
    
    ok inoguchi@ tb@

--- lib/libcrypto/asn1/asn1_locl.h
+++ lib/libcrypto/asn1/asn1_locl.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: asn1_locl.h,v 1.11 2018/08/24 20:22:15 tb Exp $ */
+/* $OpenBSD: asn1_locl.h,v 1.12 2019/10/24 16:36:10 jsing Exp $ */
 /* Written by Dr Stephen N Henson (steve@openssl.org) for the OpenSSL
  * project 2006.
  */
@@ -154,5 +154,7 @@ struct x509_crl_method_st {
 
 int UTF8_getc(const unsigned char *str, int len, unsigned long *val);
 int UTF8_putc(unsigned char *str, int len, unsigned long value);
+
+int asn1_tag2charwidth(int tag);
 
 __END_HIDDEN_DECLS
