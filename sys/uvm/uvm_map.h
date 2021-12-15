d5c8938cb4350dbd989ec0cb2cd11f622ab05fab
mpi <mpi>
2021-12-15 12:53:53 +0000

    Use a per-UVM object lock to serialize the lower part of the fault handler.
    Like the per-amap lock the `vmobjlock' is principally used to serialized
    access to objects in the fault handler to allow faults occurring on
    different CPUs and different objects to be processed in parallel.
    
    The fault handler now acquires the `vmobjlock' of a given UVM object as
    soon as it finds one.  For now a write-lock is always acquired even if
    some operations could use a read-lock.
    
    Every pager, corresponding to a different kind of UVM object, now expect
    the UVM object to be locked and some operations, like *_get() return it
    unlocked.  This is enforced by assertions checking for rw_write_held().
    
    The KERNEL_LOCK() is now pushed to the VFS boundary in the vnode pager.
    
    To ensure the correct amap or object lock is held when modifying a page
    many uvm_page* operations are now asserting for the "owner" lock.
    However, fields of the "struct vm_page" are still being protected by the
    global `pageqlock'.  To prevent lock ordering issues with the new
    `vmobjlock' and to reduce differences with NetBSD this lock is now taken
    and released for each page instead of around the whole loop.
    
    This commit does not remove the KERNEL_LOCK/UNLOCK() dance.  Unlocking
    will follow if there is no fallout.
    
    Ported from NetBSD, tested by many, thanks!
    
    ok kettenis@, kn@

--- sys/uvm/uvm_map.h
+++ sys/uvm/uvm_map.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_map.h,v 1.69 2021/03/12 14:15:49 jsg Exp $	*/
+/*	$OpenBSD: uvm_map.h,v 1.70 2021/05/22 08:38:29 mpi Exp $	*/
 /*	$NetBSD: uvm_map.h,v 1.24 2001/02/18 21:19:08 chs Exp $	*/
 
 /*
@@ -441,6 +441,9 @@ void		vm_map_unbusy_ln(struct vm_map*, char*, int);
 #define vm_map_busy(map)	vm_map_busy_ln(map, NULL, 0)
 #define vm_map_unbusy(map)	vm_map_unbusy_ln(map, NULL, 0)
 #endif
+
+void		uvm_map_lock_entry(struct vm_map_entry *);
+void		uvm_map_unlock_entry(struct vm_map_entry *);
 
 #endif /* _KERNEL */
 
