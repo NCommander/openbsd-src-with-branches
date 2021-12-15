40f258dda7ba4b07300269c6892c3189c5283599
deraadt <deraadt>
2021-12-15 17:21:08 +0000

    structure pads can leak uninitialized memory to userland via copyout, therefore the mandatory idiom is completely clearing structs before building them for copyout -- that means ALMOST ALL STRUCTS, because we never know when some architecture will pad a struct..  In two more cases, the clearing wasn't performed. from Reno Robert ZDI ok millert bluhm

--- sys/netinet/ip_mroute.c
+++ sys/netinet/ip_mroute.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_mroute.c,v 1.129 2020/03/15 05:34:13 visa Exp $	*/
+/*	$OpenBSD: ip_mroute.c,v 1.130 2020/05/27 11:19:29 mpi Exp $	*/
 /*	$NetBSD: ip_mroute.c,v 1.85 2004/04/26 01:31:57 matt Exp $	*/
 
 /*
@@ -353,6 +353,7 @@ mrt_sysctl_vif(void *oldp, size_t *oldlenp)
 
 	given = *oldlenp;
 	needed = 0;
+	memset(&vinfo, 0, sizeof vinfo);
 	TAILQ_FOREACH(ifp, &ifnet, if_list) {
 		if ((vifp = (struct vif *)ifp->if_mcast) == NULL)
 			continue;
