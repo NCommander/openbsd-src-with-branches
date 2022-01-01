c30de357d8169cc077c654955b2d175921875778
kettenis <kettenis>
2022-01-01 18:52:37 +0000

    Rewrite the kernel FPU handling code.  The new code saves the FPU state in cpu_switch() instead of at the kernel edge and gets rid of the FPU state tracking in struct cpu_info and struct pcb.  This fixes the random crashes seen with SMP kernels on Apple M1.
    ok patrick@

--- sys/arch/arm64/include/cpu.h
+++ sys/arch/arm64/include/cpu.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: cpu.h,v 1.22 2021/07/06 09:34:06 kettenis Exp $ */
+/* $OpenBSD: cpu.h,v 1.23 2021/11/22 19:22:59 kettenis Exp $ */
 /*
  * Copyright (c) 2016 Dale Rahn <drahn@dalerahn.com>
  *
@@ -99,7 +99,6 @@ struct cpu_info {
 
 	struct proc		*ci_curproc;
 	struct pmap		*ci_curpm;
-	struct proc		*ci_fpuproc;
 	u_int32_t		ci_randseed;
 
 	struct pcb		*ci_curpcb;
