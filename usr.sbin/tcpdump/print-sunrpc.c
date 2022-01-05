49b19b26d9ee27cd9db1dd6a3565a9c081d3a683
dlg <dlg>
2022-01-05 05:29:54 +0000

    silence a -Wunused-but-set-variable

--- usr.sbin/tcpdump/print-sunrpc.c
+++ usr.sbin/tcpdump/print-sunrpc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: print-sunrpc.c,v 1.20 2015/11/16 00:16:39 mmcc Exp $	*/
+/*	$OpenBSD: print-sunrpc.c,v 1.21 2021/12/01 18:28:46 deraadt Exp $	*/
 
 /*
  * Copyright (c) 1992, 1993, 1994, 1995, 1996
@@ -63,11 +63,9 @@ void
 sunrpcrequest_print(const u_char *bp, u_int length, const u_char *bp2)
 {
 	const struct rpc_msg *rp;
-	const struct ip *ip;
 	u_int32_t x;
 
 	rp = (struct rpc_msg *)bp;
-	ip = (struct ip *)bp2;
 
 	printf("xid 0x%x %d", (u_int32_t)ntohl(rp->rm_xid), length);
 	printf(" %s", tok2str(proc2str, " proc #%u",
