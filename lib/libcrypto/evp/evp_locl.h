e97853311b9d7b44e8905c6faa47db720a46cc52
tb <tb>
2022-01-09 15:15:25 +0000

    Prepare to provide EVP_MD_CTX{,_set}_pkey_ctx()
    This API with very strange ownership handling is used by Ruby 3.1,
    unfortunately.
    
    For unclear reasons, it was decided that the caller retains ownership of
    the pctx passed in.  EVP_PKEY_CTX aren't refcounted, so a flag was added to
    make sure that md_ctx->pctx is not freed in EVP_MD_CTX_{cleanup,reset}().
    Since EVP_MD_CTX_copy_ex() duplicates the md_ctx->pctx, the flag also needs
    to be unset on the duplicated EVP_MD_CTX.
    
    ok inoguchi jsing

--- lib/libcrypto/evp/evp_locl.h
+++ lib/libcrypto/evp/evp_locl.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: evp_locl.h,v 1.16 2019/10/29 07:52:17 jsing Exp $ */
+/* $OpenBSD: evp_locl.h,v 1.17 2021/12/12 21:21:58 tb Exp $ */
 /* Written by Dr Stephen N Henson (steve@openssl.org) for the OpenSSL
  * project 2000.
  */
@@ -60,6 +60,12 @@
 #define HEADER_EVP_LOCL_H
 
 __BEGIN_HIDDEN_DECLS
+
+/*
+ * Don't free md_ctx->pctx in EVP_MD_CTX_cleanup().  Needed for ownership
+ * handling in EVP_MD_CTX_set_pkey_ctx().
+ */
+#define EVP_MD_CTX_FLAG_KEEP_PKEY_CTX   0x0400
 
 /* Macros to code block cipher wrappers */
 
