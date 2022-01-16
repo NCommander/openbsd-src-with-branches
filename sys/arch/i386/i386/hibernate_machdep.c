7ca09d341404e7283b8c7c0c6276e9521e4a3c4e
mlarkin <mlarkin>
2022-01-16 22:27:46 +0000

    Remove trailing whitespace in a few places. No code change.

--- sys/arch/i386/i386/hibernate_machdep.c
+++ sys/arch/i386/i386/hibernate_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: hibernate_machdep.c,v 1.55 2018/07/30 14:19:12 kettenis Exp $	*/
+/*	$OpenBSD: hibernate_machdep.c,v 1.56 2020/09/11 09:27:10 mpi Exp $	*/
 
 /*
  * Copyright (c) 2011 Mike Larkin <mlarkin@openbsd.org>
@@ -205,7 +205,7 @@ get_hibernate_info_md(union hibernate_info *hiber_info)
 		if (hiber_info->nranges >= VM_PHYSSEG_MAX)
 			return (1);
 
-		i = hiber_info->nranges;	
+		i = hiber_info->nranges;
 		hiber_info->ranges[i].base = round_page(bmp->addr);
 		hiber_info->ranges[i].end = trunc_page(bmp->addr + bmp->size);
 		hiber_info->image_size += hiber_info->ranges[i].end -
