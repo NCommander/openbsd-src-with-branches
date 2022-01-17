c27332d0e49ba4b20264bc0c57e2708b91a81fcd
guenther <guenther>
2022-01-17 01:35:36 +0000

    Nothing depends on archdep.h pulling in other #includes anymore, so delete the #includes and hide the RELOC_* functions that are only used by lib/csu behind "#ifdef RCRT0"
    these are the ones I tested; kettenis@ was on board with the concept

--- libexec/ld.so/sh/archdep.h
+++ libexec/ld.so/sh/archdep.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: archdep.h,v 1.12 2019/10/23 19:55:09 guenther Exp $ */
+/*	$OpenBSD: archdep.h,v 1.13 2021/11/14 22:07:39 guenther Exp $ */
 
 /*
  * Copyright (c) 1998 Per Fogelstrom, Opsycon AB
@@ -32,15 +32,11 @@
 #define	RELOC_TAG	DT_RELA
 #define	MACHID		EM_SH		/* ELF e_machine ID value checked */
 
-#include <elf.h>
-#include <machine/reloc.h>
-#include "syscall.h"
-#include "util.h"
-
-
 /* Only used in lib/csu/boot.h */
+#ifdef RCRT0
+
 static inline void
-RELOC_DYN(Elf_RelA *r, const Elf_Sym *s, Elf_Addr *p, unsigned long v)
+RELOC_DYN(const Elf_RelA *r, const Elf_Sym *s, Elf_Addr *p, unsigned long v)
 {
 	if (ELF_R_TYPE(r->r_info) == R_SH_RELATIVE) {
 		*p = v + r->r_addend;
@@ -51,4 +47,5 @@ RELOC_DYN(Elf_RelA *r, const Elf_Sym *s, Elf_Addr *p, unsigned long v)
 	}
 }
 
+#endif /* RCRT0 */
 #endif /* _SH_ARCHDEP_H_ */
