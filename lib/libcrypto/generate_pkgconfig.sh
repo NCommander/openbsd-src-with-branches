f8996b16e188e59a8fd7c0e6600280ae39cce662
tb <tb>
2022-02-04 16:42:15 +0000

    Bump libressl's version in pkgconfig to 2.0.0
    LibreSSL's pc files effectively hardcode the version to 1.0.0 since
    LibreSSL exists. That probably never made much sense. This causes
    some pain for ports that "need 'openssl' ['>= +1.1.0'] found '1.0.0'"
    or similar while they would build perfectly fine with LibreSSL.
    This only affects OpenBSD. We do not put the actual LibreSSL version
    in there since it may cause trouble
    
    Discussed with sthen, millert, inoguchi, beck over the past year.
    
    Diff from/ok sthen

--- lib/libcrypto/generate_pkgconfig.sh
+++ lib/libcrypto/generate_pkgconfig.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $OpenBSD: generate_pkgconfig.sh,v 1.2 2016/09/03 12:42:46 beck Exp $
+# $OpenBSD: generate_pkgconfig.sh,v 1.3 2021/06/08 11:19:39 inoguchi Exp $
 #
 # Copyright (c) 2010,2011 Jasper Lievisse Adriaanse <jasper@openbsd.org>
 #
@@ -53,7 +53,8 @@ fi
 version_re="s/^#define[[:blank:]]+SHLIB_VERSION_NUMBER[[:blank:]]+\"(.*)\".*/\1/p"
 #version_file=${curdir}/src/crypto/opensslv.h
 version_file=${curdir}/opensslv.h
-lib_version=$(sed -nE ${version_re} ${version_file})
+#lib_version=$(sed -nE ${version_re} ${version_file})
+lib_version=2.0.0
 
 # Put -I${includedir} into Cflags so configure script tests like
 #   test -n "`pkg-config --cflags openssl`"
--- lib/libssl/generate_pkgconfig.sh
+++ lib/libssl/generate_pkgconfig.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $OpenBSD: generate_pkgconfig.sh,v 1.9 2016/09/03 12:42:42 beck Exp $
+# $OpenBSD: generate_pkgconfig.sh,v 1.10 2021/06/08 11:19:39 inoguchi Exp $
 #
 # Copyright (c) 2010,2011 Jasper Lievisse Adriaanse <jasper@openbsd.org>
 #
@@ -52,7 +52,8 @@ fi
 
 version_re="s/^#define[[:blank:]]+SHLIB_VERSION_NUMBER[[:blank:]]+\"(.*)\".*/\1/p"
 version_file=${curdir}/../libcrypto/opensslv.h
-lib_version=$(sed -nE ${version_re} ${version_file})
+#lib_version=$(sed -nE ${version_re} ${version_file})
+lib_version=2.0.0
 
 # Put -I${includedir} into Cflags so configure script tests like
 #   test -n "`pkg-config --cflags openssl`"
