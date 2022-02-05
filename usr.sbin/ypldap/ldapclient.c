9747a427a1959606a4b75997c61e7fbd6f28a04b
naddy <naddy>
2022-02-05 22:59:58 +0000

    ypldap: fix -Wunused-but-set-variable warnings
    * wrlen has been write-only since the code was imported
    * removing "dns_pid" mirrors ntpd/ntp.c 1.122
    * ifdef out unfinished code in yp_check()
    
    ok millert@ deraadt@

--- usr.sbin/ypldap/ldapclient.c
+++ usr.sbin/ypldap/ldapclient.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ldapclient.c,v 1.42 2018/11/27 12:06:39 martijn Exp $ */
+/* $OpenBSD: ldapclient.c,v 1.43 2019/06/28 13:32:52 deraadt Exp $ */
 
 /*
  * Copyright (c) 2008 Alexander Schrijver <aschrijver@openbsd.org>
@@ -357,7 +357,7 @@ client_shutdown(void)
 pid_t
 ldapclient(int pipe_main2client[2])
 {
-	pid_t            pid, dns_pid;
+	pid_t            pid;
 	int              pipe_dns[2];
 	struct passwd	*pw;
 	struct event	 ev_sigint;
@@ -382,7 +382,7 @@ ldapclient(int pipe_main2client[2])
 
 	if (socketpair(AF_UNIX, SOCK_STREAM, PF_UNSPEC, pipe_dns) == -1)
 		fatal("socketpair");
-	dns_pid = ypldap_dns(pipe_dns, pw);
+	ypldap_dns(pipe_dns, pw);
 	close(pipe_dns[1]);
 
 #ifndef DEBUG
