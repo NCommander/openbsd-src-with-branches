5db01f24cc626ab5e9d13655c0f4654c626b6c3d
sashan <sashan>
2022-02-09 11:42:58 +0000

    let pfattach() to also initialize pf_default_rule_new to avoid div-by-zero in pf_purge()
    Reported-by: syzbot+e720e3bab51366d7b667@syzkaller.appspotmail.com
    
    OK deraadt@

--- sys/net/pf_ioctl.c
+++ sys/net/pf_ioctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf_ioctl.c,v 1.370 2022/01/11 09:00:17 sashan Exp $ */
+/*	$OpenBSD: pf_ioctl.c,v 1.371 2022/01/24 22:49:48 bluhm Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -250,6 +250,8 @@ pfattach(int num)
 
 	/* XXX do our best to avoid a conflict */
 	pf_status.hostid = arc4random();
+
+	pf_default_rule_new = pf_default_rule;
 }
 
 int
