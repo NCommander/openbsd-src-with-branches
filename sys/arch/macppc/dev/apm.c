f6da769792544cc745dca8b04a09a98071596c84
deraadt <deraadt>
2022-02-15 16:54:48 +0000

    when the MI suspend code encounters problems, we need a way to reset the MD state before bailing out.  New MD function sleep_abort() does that.

--- sys/arch/macppc/dev/apm.c
+++ sys/arch/macppc/dev/apm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: apm.c,v 1.28 2022/02/12 16:22:40 deraadt Exp $	*/
+/*	$OpenBSD: apm.c,v 1.29 2022/02/15 02:38:17 deraadt Exp $	*/
 
 /*-
  * Copyright (c) 2001 Alexander Guy.  All rights reserved.
@@ -367,6 +367,11 @@ sleep_setstate(void *v)
 {
 	printf("TODO sleep_setstate\n");
 	return 0;
+}
+
+void
+sleep_abort(void *v)
+{
 }
 
 int
