785c408ebcd2c9f4ce318f8a608a65a2f82f0144
jsg <jsg>
2022-02-21 08:27:51 +0000

    onbard -> onboard

--- sys/dev/tc/if_le_tc.c
+++ sys/dev/tc/if_le_tc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_le_tc.c,v 1.11 2013/09/24 20:11:05 miod Exp $	*/
+/*	$OpenBSD: if_le_tc.c,v 1.12 2014/12/22 02:28:52 tedu Exp $	*/
 /*	$NetBSD: if_le_tc.c,v 1.12 2001/11/13 06:26:10 lukem Exp $	*/
 
 /*
@@ -98,7 +98,7 @@ le_tc_attach(struct device *parent, struct device *self, void *aux)
 
 	/*
 	 * TC lance boards have onboard SRAM buffers.  DMA
-	 * between the onbard RAM and main memory is not possible,
+	 * between the onboard RAM and main memory is not possible,
 	 * so  DMA setup is not required.
 	 */
 
