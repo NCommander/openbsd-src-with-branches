74b18c6fe6e05357ece35b1e14b8b03b78ef18bd
hastings <hastings>
2022-03-10 07:12:13 +0000

    Inform fw_update(8) about mtw(4) pattern.
    ok jsg@

--- usr.sbin/fw_update/patterns.c
+++ usr.sbin/fw_update/patterns.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: patterns.c,v 1.1 2022/01/05 16:24:29 deraadt Exp $	*/
+/*	$OpenBSD: patterns.c,v 1.2 2022/01/29 22:33:43 afresh1 Exp $	*/
 
 /*
  * Copyright (c) 1995, 1996 Christopher G. Demetriou.  All rights reserved.
@@ -103,6 +103,7 @@ main(void)
 	printf("%s\n", "iwn");
 	printf("%s\n", "iwx");
 	printf("%s\n", "malo");
+	printf("%s\n", "mtw");
 	printf("%s\n", "ogx");
 	printf("%s\n", "otus");
 	printf("%s\n", "pgt");
