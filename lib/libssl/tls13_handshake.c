8f737e567b22481a18bb5bff95dd037d9d75470e
tb <tb>
2022-04-19 17:01:43 +0000

    Drop unused KeyUpdate from debug printf
    The handshake state machine does not handle key updates since that's a
    post-handshake handshake message. This is code under #ifdef TLS13_DEBUG
    and if it is ever to be reused in tls13_handshake_msg.c, that will have
    to be revisited.
    
    ok inoguchi jsing

--- lib/libssl/tls13_handshake.c
+++ lib/libssl/tls13_handshake.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tls13_handshake.c,v 1.69 2021/07/01 17:53:39 jsing Exp $	*/
+/*	$OpenBSD: tls13_handshake.c,v 1.70 2021/09/16 19:25:30 jsing Exp $	*/
 /*
  * Copyright (c) 2018-2021 Theo Buehler <tb@openbsd.org>
  * Copyright (c) 2019 Joel Sing <jsing@openbsd.org>
@@ -291,8 +291,6 @@ tls13_handshake_message_name(uint8_t msg_type)
 		return "CertificateVerify";
 	case TLS13_MT_FINISHED:
 		return "Finished";
-	case TLS13_MT_KEY_UPDATE:
-		return "KeyUpdate";
 	}
 	return "Unknown";
 }
