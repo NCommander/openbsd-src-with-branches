d3c86d4b621c29bc06bf4ba182de7dfdbc36cf10
krw <krw>
2022-04-20 15:49:56 +0000

    Simpify code manipulating GPT partition names by coalescing logic into GPT_get_name(), string_to_name() and name_to_string() functions. Remove unnecessarily abstract functions ask_string(), utf16le_to_string() and string_to_utf16le().
    No intentional functional change.

--- sbin/fdisk/misc.h
+++ sbin/fdisk/misc.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: misc.h,v 1.46 2021/09/13 15:07:51 krw Exp $	*/
+/*	$OpenBSD: misc.h,v 1.47 2021/10/25 13:51:25 krw Exp $	*/
 
 /*
  * Copyright (c) 1997 Tobias Weingartner
@@ -36,5 +36,3 @@ void		 string_from_line(char *, const size_t, const int);
 int		 ask_yn(const char *);
 int		 hex_octet(char *);
 uint64_t	 getuint64(const char *, uint64_t, const uint64_t, const uint64_t);
-char		*utf16le_to_string(const uint16_t *);
-uint16_t	*string_to_utf16le(const char *);
