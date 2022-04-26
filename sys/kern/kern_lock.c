23cb85fb4d247db6dfe8c09d934866c848c5542c
dv <dv>
2022-04-26 15:31:14 +0000

    Bump __mp_lock_spinout to INT_MAX.
    The previous value set years ago was causing amd64 kernels to spin
    out when run with MP_LOCKDEBUG during boot.
    
    ok kettenis@

--- sys/kern/kern_lock.c
+++ sys/kern/kern_lock.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_lock.c,v 1.70 2019/06/04 15:41:02 visa Exp $	*/
+/*	$OpenBSD: kern_lock.c,v 1.71 2020/03/05 09:28:31 claudio Exp $	*/
 
 /*
  * Copyright (c) 2017 Visa Hankala
@@ -33,7 +33,7 @@
 #endif
 
 /* CPU-dependent timing, this needs to be settable from ddb. */
-int __mp_lock_spinout = 200000000;
+int __mp_lock_spinout = INT_MAX;
 #endif /* MP_LOCKDEBUG */
 
 #ifdef MULTIPROCESSOR
