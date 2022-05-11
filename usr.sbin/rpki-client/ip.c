c8d64f160fd843ff2a6e922d992f73c37df6016c
tb <tb>
2022-05-11 18:48:35 +0000

    Zap stray space

--- usr.sbin/rpki-client/ip.c
+++ usr.sbin/rpki-client/ip.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip.c,v 1.20 2021/11/10 09:15:29 claudio Exp $ */
+/*	$OpenBSD: ip.c,v 1.21 2021/12/26 12:32:28 tb Exp $ */
 /*
  * Copyright (c) 2019 Kristaps Dzonsons <kristaps@bsd.lv>
  *
@@ -227,7 +227,7 @@ ip_addr_parse(const ASN1_BIT_STRING *p,
 		return 0;
 	}
 
-	memset (addr, 0, sizeof(struct ip_addr));
+	memset(addr, 0, sizeof(struct ip_addr));
 	addr->prefixlen = p->length * 8 - unused;
 	memcpy(addr->addr, p->data, p->length);
 	return 1;
