7eff54778ea7b4d87235e6a9fa3625123495d306
kevlo <kevlo>
2022-05-19 00:52:37 +0000

    Force maximum payload size to 128 bytes for AR816X/AR817x as it triggers dma write error rendering the network unusable.
    Bug reported and patch tested by Scott C. MacCallum.

--- sys/dev/pci/if_alc.c
+++ sys/dev/pci/if_alc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_alc.c,v 1.54 2020/07/10 13:26:37 patrick Exp $	*/
+/*	$OpenBSD: if_alc.c,v 1.55 2022/03/11 18:00:45 mpi Exp $	*/
 /*-
  * Copyright (c) 2009, Pyun YongHyeon <yongari@FreeBSD.org>
  * All rights reserved.
@@ -1354,10 +1354,11 @@ alc_attach(struct device *parent, struct device *self, void *aux)
 			sc->alc_dma_wr_burst = 3;
 		/*
 		 * Force maximum payload size to 128 bytes for
-		 * E2200/E2400/E2500.
+		 * E2200/E2400/E2500/AR8162/AR8171/AR8172.
 		 * Otherwise it triggers DMA write error.
 		 */
-		if ((sc->alc_flags & ALC_FLAG_E2X00) != 0)
+		if ((sc->alc_flags &
+		    (ALC_FLAG_E2X00 | ALC_FLAG_AR816X_FAMILY)) != 0)
 			sc->alc_dma_wr_burst = 0;
 		alc_init_pcie(sc, base);
 	}
