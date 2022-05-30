5b530ff377339c5623e04f75cf375d603b98c1f0
nicm <nicm>
2022-05-30 12:57:31 +0000

    iTerm2 has OSC 7, from Gregory Anders.

--- usr.bin/tmux/tty-features.c
+++ usr.bin/tmux/tty-features.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: tty-features.c,v 1.21 2021/12/21 14:57:28 nicm Exp $ */
+/* $OpenBSD: tty-features.c,v 1.22 2022/03/24 09:05:57 nicm Exp $ */
 
 /*
  * Copyright (c) 2020 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -376,7 +376,7 @@ tty_default_features(int *feat, const char *name, u_int version)
 		},
 		{ .name = "iTerm2",
 		  .features = TTY_FEATURES_BASE_MODERN_XTERM
-			      ",cstyle,extkeys,margins,usstyle,sync"
+			      ",cstyle,extkeys,margins,usstyle,sync,osc7"
 		},
 		{ .name = "XTerm",
 		  /*
