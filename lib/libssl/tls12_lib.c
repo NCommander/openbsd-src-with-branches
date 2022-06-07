ce087acacee177690de12dd35ff6896ee4d7c246
tb <tb>
2022-06-07 17:19:09 +0000

    The master_key_length can no longer be < 0
    ok jsing

--- lib/libssl/tls12_lib.c
+++ lib/libssl/tls12_lib.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tls12_lib.c,v 1.3 2021/05/02 15:57:29 jsing Exp $ */
+/*	$OpenBSD: tls12_lib.c,v 1.4 2022/02/05 14:54:10 jsing Exp $ */
 /*
  * Copyright (c) 2021 Joel Sing <jsing@openbsd.org>
  *
@@ -27,7 +27,7 @@ tls12_finished_verify_data(SSL *s, const char *finished_label,
 
 	*out_len = 0;
 
-	if (s->session->master_key_length <= 0)
+	if (s->session->master_key_length == 0)
 		return 0;
 
 	if (verify_data_len < TLS1_FINISH_MAC_LENGTH)
