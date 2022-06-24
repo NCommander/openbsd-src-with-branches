ea8b99eff159eb1cfb89527f32e2ebd8202991a9
dtucker <dtucker>
2022-06-24 10:45:06 +0000

    Roll back previous KEX changes as they aren't safe until compat_pkalg_proposal and friends always allocate their returned strings.  Reported by Qualys.

--- usr.bin/ssh/sshconnect2.c
+++ usr.bin/ssh/sshconnect2.c
@@ -109,7 +109,6 @@ first_alg(const char *algs)
 	return ret;
 }
 
-/* Returns an allocated string that the caller must free. */
 static char *
 order_hostkeyalgs(char *host, struct sockaddr *hostaddr, u_short port,
     const struct ssh_conn_info *cinfo)
@@ -213,7 +212,7 @@ ssh_kex2(struct ssh *ssh, char *host, struct sockaddr *hostaddr, u_short port,
     const struct ssh_conn_info *cinfo)
 {
 	char *myproposal[PROPOSAL_MAX] = { KEX_CLIENT };
-	char *s, *all_key, *hostkeyalgs = NULL;
+	char *s, *all_key;
 	int r, use_known_hosts_order = 0;
 
 	xxx_host = host;
@@ -251,10 +250,9 @@ ssh_kex2(struct ssh *ssh, char *host, struct sockaddr *hostaddr, u_short port,
 	    myproposal[PROPOSAL_MAC_ALGS_STOC] = options.macs;
 	if (use_known_hosts_order) {
 		/* Query known_hosts and prefer algorithms that appear there */
-		hostkeyalgs = order_hostkeyalgs(host, hostaddr, port, cinfo);
 		myproposal[PROPOSAL_SERVER_HOST_KEY_ALGS] =
-		    compat_pkalg_proposal(ssh, hostkeyalgs);
-		free(hostkeyalgs);
+		    compat_pkalg_proposal(ssh,
+		    order_hostkeyalgs(host, hostaddr, port, cinfo));
 	} else {
 		/* Use specified HostkeyAlgorithms exactly */
 		myproposal[PROPOSAL_SERVER_HOST_KEY_ALGS] =
