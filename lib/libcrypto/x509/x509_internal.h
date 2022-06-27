4fb725704ff89f4d4bac1734c10e5dbaa3202180
tb <tb>
2022-06-27 14:10:22 +0000

    Allow security_level to mestastasize into the verifier
    The tentacles are everywhere.  This checks that all certs in a chain
    have keys and signature algorithms matching the requirements of the
    security_level configured in the verify parameters.
    
    ok beck jsing

--- lib/libcrypto/x509/x509_internal.h
+++ lib/libcrypto/x509/x509_internal.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: x509_internal.h,v 1.17 2022/03/13 17:08:04 tb Exp $ */
+/* $OpenBSD: x509_internal.h,v 1.18 2022/03/14 21:15:49 tb Exp $ */
 /*
  * Copyright (c) 2020 Bob Beck <beck@openbsd.org>
  *
@@ -135,6 +135,7 @@ int x509_constraints_check(struct x509_constraints_names *names,
 int x509_constraints_chain(STACK_OF(X509) *chain, int *error,
     int *depth);
 void x509_verify_cert_info_populate(X509 *cert);
+int x509_vfy_check_security_level(X509_STORE_CTX *ctx);
 
 __END_HIDDEN_DECLS
 
