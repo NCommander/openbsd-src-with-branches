9ec1224b45f832b259edc818fd9f98514d296574
claudio <claudio>
2022-06-27 13:26:51 +0000

    Add support for RFC 9234 - Route Leak Prevention and Detection Using Roles
    With this it is possible to send a role in the OPEN message and if that
    was successful the RDE will add the new OTC attribute if necessary.
    OK tb@

--- usr.sbin/bgpd/rde_peer.c
+++ usr.sbin/bgpd/rde_peer.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rde_peer.c,v 1.15 2022/03/22 10:53:08 claudio Exp $ */
+/*	$OpenBSD: rde_peer.c,v 1.16 2022/05/23 13:40:12 deraadt Exp $ */
 
 /*
  * Copyright (c) 2019 Claudio Jeker <claudio@openbsd.org>
@@ -59,6 +59,13 @@ peer_has_add_path(struct rde_peer *peer, uint8_t aid, int mode)
 	if (aid > AID_MAX)
 		return 0;
 	return (peer->capa.add_path[aid] & mode);
+}
+
+int
+peer_has_open_policy(struct rde_peer *peer, uint8_t *role)
+{
+	*role = peer->capa.role;
+	return (peer->capa.role_ena != 0);
 }
 
 int
