8b1474282b46135145060c82e0f5ac08321b3330
tb <tb>
2022-06-28 20:42:22 +0000

    Set up the default callback in SSL_CERT
    ok beck jsing sthen

--- lib/libssl/ssl_cert.c
+++ lib/libssl/ssl_cert.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssl_cert.c,v 1.94 2022/01/28 13:14:48 inoguchi Exp $ */
+/* $OpenBSD: ssl_cert.c,v 1.95 2022/02/05 14:54:10 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -170,6 +170,13 @@ ssl_cert_new(void)
 	}
 	ret->key = &(ret->pkeys[SSL_PKEY_RSA]);
 	ret->references = 1;
+#if defined(LIBRESSL_HAS_SECURITY_LEVEL)
+	ret->security_cb = ssl_security_default_cb;
+#else
+	ret->security_cb = ssl_security_dummy_cb;
+#endif
+	ret->security_level = OPENSSL_TLS_SECURITY_LEVEL;
+	ret->security_ex_data = NULL;
 	return (ret);
 }
 
