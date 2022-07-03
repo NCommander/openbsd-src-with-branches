8618c49d336e5f197936fe1809247a8e9414086c
deraadt <deraadt>
2022-07-03 15:06:06 +0000

    thou shalt not include sys/cdefs.h because it is not standardized, the file to be used is sys/types.h

--- usr.bin/ts/ts.c
+++ usr.bin/ts/ts.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ts.c,v 1.3 2022/06/30 07:42:50 claudio Exp $	*/
+/*	$OpenBSD: ts.c,v 1.4 2022/06/30 07:55:48 job Exp $	*/
 /*
  * Copyright (c) 2022 Job Snijders <job@openbsd.org>
  * Copyright (c) 2022 Claudio Jeker <claudio@openbsd.org>
@@ -16,7 +16,7 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include <sys/cdefs.h>
+#include <sys/types.h>
 #include <sys/time.h>
 
 #include <err.h>
