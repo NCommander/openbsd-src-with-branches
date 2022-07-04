aa44493132c98eda82e8cb6f6c3db374c54928b3
cheloha <cheloha>
2022-07-04 17:29:03 +0000

    ts(1): the first argument to clock_gettime(2) is a clockid_t, not an int

--- usr.bin/ts/ts.c
+++ usr.bin/ts/ts.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ts.c,v 1.4 2022/06/30 07:55:48 job Exp $	*/
+/*	$OpenBSD: ts.c,v 1.5 2022/07/03 15:06:06 deraadt Exp $	*/
 /*
  * Copyright (c) 2022 Job Snijders <job@openbsd.org>
  * Copyright (c) 2022 Claudio Jeker <claudio@openbsd.org>
@@ -42,7 +42,7 @@ main(int argc, char *argv[])
 	int ch, prev;
 	struct timespec roff, start, now;
 	struct tm *tm;
-	int clock = CLOCK_REALTIME;
+	clockid_t clock = CLOCK_REALTIME;
 
 	if (pledge("stdio", NULL) == -1)
 		err(1, "pledge");
