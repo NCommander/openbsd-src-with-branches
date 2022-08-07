cca32b0a5ce7ae89c843a88dcbdd8a2448ae7cf8
guenther <guenther>
2022-08-07 23:56:06 +0000

    Start to add annotations to the cpu_info members, doing I/a/o for immutable/atomic/owned ala <sys/proc.h>.  Move CPUF_USERSEGS and CPUF_USERXSTATE, which really are private to the CPU, into a new ci_pflags and rename s/CPUF_/CPUPF_/.  Make all (remaining) ci_flags alterations via atomic_{set,clear}bits_int(), so its annotation isn't a lie.  Delete ci_info member as unused all the way from rev 1.1
    ok jsg@ mlarkin@

--- sys/arch/amd64/amd64/ipifuncs.c
+++ sys/arch/amd64/amd64/ipifuncs.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ipifuncs.c,v 1.35 2020/09/13 11:53:16 jsg Exp $	*/
+/*	$OpenBSD: ipifuncs.c,v 1.36 2021/08/31 17:40:59 dv Exp $	*/
 /*	$NetBSD: ipifuncs.c,v 1.1 2003/04/26 18:39:28 fvdl Exp $ */
 
 /*-
@@ -124,7 +124,7 @@ x86_64_ipi_halt(struct cpu_info *ci)
 	intr_disable();
 	lapic_disable();
 	wbinvd();
-	ci->ci_flags &= ~CPUF_RUNNING;
+	atomic_clearbits_int(&ci->ci_flags, CPUF_RUNNING);
 	wbinvd();
 
 	for(;;) {
