733eb624c279c4fe1d38bc3136234129bfff08db
kn <kn>
2022-08-31 19:40:37 +0000

    Log copy of /ofwboot
    Another step towards more consistent behaviour across platforms.
    
    This leaves only hppa and landisk **not** logging such copies,
    but I can't test on those.
    
    OK miod

--- usr.sbin/installboot/sparc64_installboot.c
+++ usr.sbin/installboot/sparc64_installboot.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sparc64_installboot.c,v 1.8 2019/06/28 13:32:48 deraadt Exp $	*/
+/*	$OpenBSD: sparc64_installboot.c,v 1.9 2021/07/20 14:51:56 kettenis Exp $	*/
 
 /*
  * Copyright (c) 2012, 2013 Joel Sing <jsing@openbsd.org>
@@ -103,6 +103,9 @@ md_installboot(int devfd, char *dev)
 	bootldr = fileprefix(root, bootldr);
 	if (bootldr == NULL)
 		exit(1);
+	if (verbose)
+		fprintf(stderr, "%s %s to %s\n",
+		    (nowrite ? "would copy" : "copying"), stage2, bootldr);
 	if (!nowrite)
 		if (filecopy(stage2, bootldr) == -1)
 			exit(1);
