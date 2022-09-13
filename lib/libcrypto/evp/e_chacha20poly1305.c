b3695f944a3485b26b02aa6822afdac836424fc4
jsing <jsing>
2022-09-13 04:59:18 +0000

    Stop pretending that EVP_CIPHER cleanup can fail.
    Now that EVP_CIPHER is opaque, stop pretending that EVP_CIPHER cleanup can
    fail.
    
    ok tb@

--- lib/libcrypto/evp/e_chacha20poly1305.c
+++ lib/libcrypto/evp/e_chacha20poly1305.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: e_chacha20poly1305.c,v 1.24 2022/08/21 10:47:09 tb Exp $ */
+/* $OpenBSD: e_chacha20poly1305.c,v 1.25 2022/08/30 19:33:26 tb Exp $ */
 
 /*
  * Copyright (c) 2022 Joel Sing <jsing@openbsd.org>
@@ -530,14 +530,12 @@ chacha20_poly1305_cipher(EVP_CIPHER_CTX *ctx, unsigned char *out,
 	return len;
 }
 
-static int
+static void
 chacha20_poly1305_cleanup(EVP_CIPHER_CTX *ctx)
 {
 	struct chacha20_poly1305_ctx *cpx = ctx->cipher_data;
 
 	explicit_bzero(cpx, sizeof(*cpx));
-
-	return 1;
 }
 
 static int
