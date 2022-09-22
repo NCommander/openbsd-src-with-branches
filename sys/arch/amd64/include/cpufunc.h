aaefd47abde0e524d5aa32fa8a7a1384fa07055a
robert <robert>
2022-09-22 04:57:08 +0000

    use the always serializing RDTSCP instruction in tsc and usertc if available
    tweaks from cheloha@; ok deraadt@, sthen@, cheloha@

--- sys/arch/amd64/include/cpufunc.h
+++ sys/arch/amd64/include/cpufunc.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cpufunc.h,v 1.35 2020/07/03 17:54:27 kettenis Exp $	*/
+/*	$OpenBSD: cpufunc.h,v 1.36 2020/09/13 11:53:16 jsg Exp $	*/
 /*	$NetBSD: cpufunc.h,v 1.3 2003/05/08 10:27:43 fvdl Exp $	*/
 
 /*-
@@ -299,6 +299,15 @@ rdtsc(void)
 	uint32_t hi, lo;
 
 	__asm volatile("rdtsc" : "=d" (hi), "=a" (lo));
+	return (((uint64_t)hi << 32) | (uint64_t) lo);
+}
+
+static __inline u_int64_t
+rdtscp(void)
+{
+	uint32_t hi, lo;
+
+	__asm volatile("rdtscp" : "=d" (hi), "=a" (lo) : : "ecx");
 	return (((uint64_t)hi << 32) | (uint64_t) lo);
 }
 
--- sys/arch/amd64/include/timetc.h
+++ sys/arch/amd64/include/timetc.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: timetc.h,v 1.1 2020/07/06 13:33:06 pirofti Exp $ */
+/*	$OpenBSD: timetc.h,v 1.2 2020/07/08 09:17:48 kettenis Exp $ */
 /*
  * Copyright (c) 2020 Paul Irofti <paul@irofti.net>
  *
@@ -18,6 +18,7 @@
 #ifndef _MACHINE_TIMETC_H_
 #define _MACHINE_TIMETC_H_
 
-#define TC_TSC	1
+#define TC_TSC_LFENCE	1
+#define TC_TSC_RDTSCP	2
 
 #endif	/* _MACHINE_TIMETC_H_ */
