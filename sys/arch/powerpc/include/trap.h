4105711e963192145fdca33a9b780648fcdbce06
gkoehler <gkoehler>
2022-10-22 00:58:56 +0000

    AltiVec assist for subnormal floats in vectors
    The old CPU in a macppc traps AltiVec instructions when they encounter
    denormal or subnormal floats.  Emulate most of them.  They operate on
    vectors of 4 single-precision floats.  The emulations either use
    scalar operations (so vmaddfp becomes 4 of fmadds) or a formula (like
    vrsqrtefp's 1 / sqrt(b) = 1 / sqrt(b * 2**126) * 2**63).
    
    I am forgetting to emulate some instructions (at least vrfin, vrfiz,
    vrfip, vrfim).  If I don't emulate it, it will still cause SIGFPE.
    
    Mac OS never emulated these instructions, but set AltiVec's "non-Java"
    NJ bit (which changes all subnormal floats to zero).  FreeBSD also
    sets NJ; NetBSD does SIGFPE; Linux emulates them.  The POWER9 running
    OpenBSD/powerpc64 does them in hardware (without trapping).
    
    ok kettenis@ miod@

--- sys/arch/powerpc/include/trap.h
+++ sys/arch/powerpc/include/trap.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: trap.h,v 1.7 2007/04/26 21:36:32 kettenis Exp $	*/
+/*	$OpenBSD: trap.h,v 1.8 2022/05/19 05:43:48 miod Exp $	*/
 /*	$NetBSD: trap.h,v 1.1 1996/09/30 16:34:35 ws Exp $	*/
 
 /*
@@ -51,7 +51,8 @@
 #define	EXC_VEC		0x0f20		/* AltiVec Unavailable */
 #define	EXC_BPT		0x1300		/* Instruction Breakpoint */
 #define	EXC_SMI		0x1400		/* System Management Interrupt */
-#define	EXC_VECAST	0x1600		/* AltiVec Assist */
+#define	EXC_VECAST_G4	0x1600		/* AltiVec Assist */
+#define	EXC_VECAST_G5	0x1700		/* AltiVec Assist */
 
 /* And these are only on the 603: */
 #define	EXC_IMISS	0x1000		/* Instruction translation miss */
