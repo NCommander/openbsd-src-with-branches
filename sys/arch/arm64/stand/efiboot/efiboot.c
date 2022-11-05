58163ecfc2ab63d477c77d265cef8d48320d92b0
patrick <patrick>
2022-11-05 19:00:31 +0000

    Not every device tree we're getting passed has an /aliases or /chosen node, but our code assumes so.  It's useful to have them, so create them if they do not exist.
    ok kettenis@

--- sys/arch/arm64/stand/efiboot/efiboot.c
+++ sys/arch/arm64/stand/efiboot/efiboot.c
@@ -604,10 +604,15 @@ efi_makebootargs(char *bootargs, int howto)
 	if (!fdt_init(fdt))
 		return NULL;
 
+	/* Create common nodes which might not exist when using mach dtb */
+	node = fdt_find_node("/aliases");
+	if (node == NULL)
+		fdt_node_add_node(fdt_find_node("/"), "aliases", &node);
 	node = fdt_find_node("/chosen");
-	if (!node)
-		return NULL;
+	if (node == NULL)
+		fdt_node_add_node(fdt_find_node("/"), "chosen", &node);
 
+	node = fdt_find_node("/chosen");
 	len = strlen(bootargs) + 1;
 	fdt_node_add_property(node, "bootargs", bootargs, len);
 	fdt_node_add_property(node, "openbsd,boothowto",
