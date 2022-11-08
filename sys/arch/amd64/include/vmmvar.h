50f38744ce6ac39e1516d2eff3fc939546f2a07d
dlg <dlg>
2022-11-08 19:18:47 +0000

    further speed up delivery of interrupts to a running vcpu.
    this records which physical cpu a vcpu is running on. this is used
    by the code that marks a vcpu as having a pending interrupt to check
    if the vcpu is currently running. if it thinks the vcpu is running,
    it sends a nop IPI to the physical cpu it is running on to trigger
    a vmexit, which in turn runs interrupt handling in the guest.
    
    ok mlarkin@

--- sys/arch/amd64/include/vmmvar.h
+++ sys/arch/amd64/include/vmmvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vmmvar.h,v 1.80 2022/08/30 17:09:21 dv Exp $	*/
+/*	$OpenBSD: vmmvar.h,v 1.81 2022/09/01 22:01:40 dv Exp $	*/
 /*
  * Copyright (c) 2014 Mike Larkin <mlarkin@openbsd.org>
  *
@@ -934,6 +934,7 @@ struct vcpu {
 	struct rwlock vc_lock;
 	struct refcnt vc_refcnt;		/* [a] */
 
+	struct cpu_info *vc_curcpu;		/* [a] */
 	struct cpu_info *vc_last_pcpu;		/* [v] */
 	struct vm_exit vc_exit;			/* [v] */
 
