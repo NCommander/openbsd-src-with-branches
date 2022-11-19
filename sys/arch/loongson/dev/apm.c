987328c0cde9ef43ce9f2e5ebbc9f9205825b80d
cheloha <cheloha>
2022-11-19 16:23:48 +0000

    mips64, loongson, octeon: switch to clockintr
    - Remove mips64-specific clock interrupt scheduling bits from cpu_info.
    - Add missing tick_nsec initialization to cpu_initclocks().
    - Disable the glxclk interrupt clock on loongson.  visa@/miod@ say it
      can be removed later if it isn't useful for anything else.
    - Wire up cp0_intrclock.
    
    Notes:
    
    - The loongson apm_suspend() changes are untested, but deraadt@ claims
      APM suspend/resume on loongson doesn't work anyway.
    - loongson and octeon now have a randomized statclock(), stathz = hz.
    
    With input from miod@, visa@.  Tested by miod@, visa@.
    
    Link: https://marc.info/?l=openbsd-tech&m=166776379603497&w=2
    
    ok visa@ mlarkin@

--- sys/arch/loongson/dev/apm.c
+++ sys/arch/loongson/dev/apm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: apm.c,v 1.39 2020/12/25 12:59:51 visa Exp $	*/
+/*	$OpenBSD: apm.c,v 1.40 2022/04/06 18:59:26 naddy Exp $	*/
 
 /*-
  * Copyright (c) 2001 Alexander Guy.  All rights reserved.
@@ -38,6 +38,7 @@
 #include <sys/systm.h>
 #include <sys/kernel.h>
 #include <sys/proc.h>
+#include <sys/clockintr.h>
 #include <sys/device.h>
 #include <sys/fcntl.h>
 #include <sys/ioctl.h>
@@ -415,7 +416,13 @@ apm_suspend(int state)
 		if (rv == 0)
 			rv = sys_platform->resume();
 	}
+
 	inittodr(gettime());	/* Move the clock forward */
+#ifdef __HAVE_CLOCKINTR
+	clockintr_cpu_init(NULL);
+	clockintr_trigger();
+#endif
+
 	config_suspend_all(DVACT_RESUME);
 
 	cold = 0;
