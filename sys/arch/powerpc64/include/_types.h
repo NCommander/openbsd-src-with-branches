8a64723dff6a30acc5366618313487cc6b45db4b
cheloha <cheloha>
2022-11-29 01:04:44 +0000

    powerpc64: switch to clockintr
    - Remove powerpc64-specific clock interrupt scheduling bits from
      cpu_info.
    - Remove powerpc64-specific randomized statclock() bits from
      powerpc64/clock.c.
    - Remove the 'stat_count' evcount.  All clock interrupts are now counted
      via the 'clock_count' evcount.
    - Wire up dec_intrclock.
    
    Bringup help from gkoehler@.  Tested by gkoehler@: this patch has
    survived four kernel-release-upgrade cycles on a Raptor Talos II
    T2P9S01 sporting a quad-core POWER9 CPU.
    
    Link: https://marc.info/?l=openbsd-tech&m=166776404803622&w=2
    
    ok gkoehler@ mlarkin@

--- sys/arch/powerpc64/include/_types.h
+++ sys/arch/powerpc64/include/_types.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: _types.h,v 1.1 2020/05/16 17:11:14 kettenis Exp $ */
+/* $OpenBSD: _types.h,v 1.2 2020/05/27 22:22:04 gkoehler Exp $ */
 /*-
  * Copyright (c) 1990, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -33,6 +33,8 @@
 
 #ifndef _MACHINE__TYPES_H_
 #define _MACHINE__TYPES_H_
+
+#define	__HAVE_CLOCKINTR
 
 #if defined(_KERNEL)
 typedef struct label_t {
