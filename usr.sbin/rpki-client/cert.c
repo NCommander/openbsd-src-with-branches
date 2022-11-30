4f880ee068051d94b5b9e3d132b2671811dff2f0
job <job>
2022-11-30 09:12:34 +0000

    Remove unused includes
    OK claudio@

--- usr.sbin/rpki-client/cert.c
+++ usr.sbin/rpki-client/cert.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cert.c,v 1.99 2022/11/29 20:41:32 job Exp $ */
+/*	$OpenBSD: cert.c,v 1.100 2022/11/30 08:17:21 job Exp $ */
 /*
  * Copyright (c) 2022 Theo Buehler <tb@openbsd.org>
  * Copyright (c) 2021 Job Snijders <job@openbsd.org>
@@ -17,10 +17,8 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include <arpa/inet.h>
 #include <assert.h>
 #include <err.h>
-#include <inttypes.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
