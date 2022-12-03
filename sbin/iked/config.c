45dc3ab84678c3ac15790682571e65320ef37646
tobhe <tobhe>
2022-12-03 22:34:35 +0000

    Consistently use uintXX_t from <stdint.h> instead of u_intXX_t.

--- sbin/iked/config.c
+++ sbin/iked/config.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: config.c,v 1.89 2022/11/13 21:19:40 mbuhl Exp $	*/
+/*	$OpenBSD: config.c,v 1.90 2022/11/18 18:56:39 mbuhl Exp $	*/
 
 /*
  * Copyright (c) 2019 Tobias Heider <tobias.heider@stusta.de>
@@ -950,11 +950,11 @@ config_setocsp(struct iked *env)
 int
 config_getocsp(struct iked *env, struct imsg *imsg)
 {
-	size_t			 have, need;
-	u_int8_t		*ptr;
+	size_t		 have, need;
+	uint8_t		*ptr;
 
 	free(env->sc_ocsp_url);
-	ptr = (u_int8_t *)imsg->data;
+	ptr = (uint8_t *)imsg->data;
 	have = IMSG_DATA_SIZE(imsg);
 
 	/* get tolerate */
--- sbin/iked/dh.c
+++ sbin/iked/dh.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dh.c,v 1.30 2021/11/29 06:43:42 deraadt Exp $	*/
+/*	$OpenBSD: dh.c,v 1.31 2021/12/13 18:06:56 tb Exp $	*/
 
 /*
  * Copyright (c) 2010-2014 Reyk Floeter <reyk@openbsd.org>
@@ -838,8 +838,8 @@ kemsx_create_shared2(struct dh_group *group, struct ibuf **sharedp,
 	struct kemsx_key	*kemsx = group->kemsx;
 	struct ibuf		*buf = NULL;
 	EVP_MD_CTX		*ctx = NULL;
-	u_int8_t		*cp;
-	u_int8_t		 shared[CURVE25519_SIZE];
+	uint8_t			*cp;
+	uint8_t			 shared[CURVE25519_SIZE];
 	size_t			 have, need;
 	u_int			 len;
 
