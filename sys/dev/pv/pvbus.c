47c5062ba41f91d0d808ae35c1c9a23295006061
yasuoka <yasuoka>
2022-12-08 05:45:36 +0000

    Fix pvbus to specify M_ZERO properly.
    ok kn mvs mlarkin asou deraadt

--- sys/dev/pv/pvbus.c
+++ sys/dev/pv/pvbus.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pvbus.c,v 1.24 2021/11/05 11:38:29 mpi Exp $	*/
+/*	$OpenBSD: pvbus.c,v 1.25 2022/08/25 17:38:16 cheloha Exp $	*/
 
 /*
  * Copyright (c) 2015 Reyk Floeter <reyk@openbsd.org>
@@ -408,7 +408,7 @@ pvbusgetstr(size_t srclen, const char *src, char **dstp)
 	else if (srclen > PAGE_SIZE)
 		return (ENAMETOOLONG);
 
-	*dstp = dst = malloc(srclen + 1, M_TEMP|M_ZERO, M_WAITOK);
+	*dstp = dst = malloc(srclen + 1, M_TEMP, M_WAITOK | M_ZERO);
 	if (src != NULL) {
 		error = copyin(src, dst, srclen);
 		dst[srclen] = '\0';
