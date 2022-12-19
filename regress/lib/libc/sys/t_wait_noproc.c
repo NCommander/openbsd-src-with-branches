42a28b5d60e8b041e74035f0f66c2aeea7a0dcdb
guenther <guenther>
2022-12-19 22:44:54 +0000

    WTRAPPED is now supported by waitid(2) Don't test waitid(WUNTRACED) as that's not portable and only 'works' due to an implementation decision

--- regress/lib/libc/sys/t_wait_noproc.c
+++ regress/lib/libc/sys/t_wait_noproc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: t_wait_noproc.c,v 1.2 2021/12/13 16:56:48 deraadt Exp $	*/
+/*	$OpenBSD: t_wait_noproc.c,v 1.3 2022/10/26 23:18:01 kettenis Exp $	*/
 /* $NetBSD: t_wait_noproc.c,v 1.5 2016/11/09 17:50:19 kamil Exp $ */
 
 /*-
@@ -145,11 +145,11 @@ get_options6(size_t pos)
 	const int matrix[] = {
 		WNOWAIT,	// First in order to blacklist it easily
 		WEXITED,
-		WUNTRACED,
-		WSTOPPED,	// SUS compatibility, equal to WUNTRACED
 #ifndef __OpenBSD__
-		WTRAPPED,
+		WUNTRACED,
 #endif
+		WSTOPPED,	// SUS compatibility, equal to WUNTRACED
+		WTRAPPED,
 		WCONTINUED
 	};
 
