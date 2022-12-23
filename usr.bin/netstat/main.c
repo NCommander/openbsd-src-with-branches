5c76877be79ab7ca8d0d1f858721072cefb1090a
jmc <jmc>
2022-12-23 07:18:46 +0000

    remove "-p protocol" from the -r synopsis: it no longer makes sense
    pointed out by pguemther
    ok pguenther kn claudio

--- usr.bin/netstat/main.c
+++ usr.bin/netstat/main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: main.c,v 1.122 2022/09/08 13:18:47 kn Exp $	*/
+/*	$OpenBSD: main.c,v 1.123 2022/12/04 23:50:48 cheloha Exp $	*/
 /*	$NetBSD: main.c,v 1.9 1996/05/07 02:55:02 thorpej Exp $	*/
 
 /*
@@ -485,8 +485,7 @@ usage(void)
 	    "       netstat -s [-gru] [-f address_family] [-p protocol]\n"
 	    "       netstat -g [-lnu] [-f address_family]\n"
 	    "       netstat -R\n"
-	    "       netstat -r [-AFu] [-f address_family] [-M core] [-N system] [-p protocol]\n"
-	    "               [-T rtable]\n"
+	    "       netstat -r [-AFu] [-f address_family] [-M core] [-N system] [-T rtable]\n"
 	    "       netstat -P pcbaddr [-v] [-M core] [-N system]\n");
 	exit(1);
 }
