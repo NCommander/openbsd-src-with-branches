96a51b3719e536a1ecfaa211cf680b642f7fd922
jsg <jsg>
2023-01-04 10:59:34 +0000

    Leo Weppelman agreed to rescind clause 3 and 4 in NetBSD kvm_dump.3 rev 1.15 kcore.h rev 1.3
    https://mail-index.netbsd.org/source-changes/2009/10/20/msg002169.html

--- sys/arch/mips64/include/kcore.h
+++ sys/arch/mips64/include/kcore.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kcore.h,v 1.1 2004/08/06 20:56:02 pefo Exp $	*/
+/*	$OpenBSD: kcore.h,v 1.2 2011/03/23 16:54:36 pirofti Exp $	*/
 /*	$NetBSD: kcore.h,v 1.1 1996/03/10 21:55:18 leo Exp $	*/
 
 /*
@@ -13,11 +13,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Leo Weppelman.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
