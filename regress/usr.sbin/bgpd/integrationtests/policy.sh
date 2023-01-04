f35d2e9b9b1fc82f720628383adc2b8f80cc755a
claudio <claudio>
2023-01-04 14:34:50 +0000

    Adjust open policy integration test to the change of config in bgpd.

--- regress/usr.sbin/bgpd/integrationtests/policy.sh
+++ regress/usr.sbin/bgpd/integrationtests/policy.sh
@@ -1,5 +1,5 @@
 #!/bin/ksh
-#	$OpenBSD: maxprefix.sh,v 1.2 2022/03/08 17:20:52 claudio Exp $
+#	$OpenBSD: policy.sh,v 1.1 2022/06/27 13:29:40 claudio Exp $
 
 set -e
 
@@ -107,7 +107,7 @@ ifconfig lo${RDOMAIN2} inet 127.0.0.1/8
 
 echo test1: no policy
 test_bgpd "Last error sent: error in OPEN message, role mismatch" \
-    "no" "no" "no" "no" "no"
+    "none" "none" "none" "none" "none"
 
 echo test2: wrong policy
 test_bgpd "Last error sent: error in OPEN message, role mismatch" \
