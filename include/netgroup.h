1276bb52db3c6ddee56d3c5971b7ab6f781658f3
jsg <jsg>
2023-01-04 13:00:11 +0000

    Christos Zoulas agreed to rescind clause 3 and 4 in NetBSD fsck.8 rev 1.35 fsutil.h rev 1.14 pathnames.h rev 1.2 netgroup_mkdb.8 rev 1.9 netgroup_mkdb.c rev 1.18 str.c rev 1.7 str.h rev 1.4 rdate.8 rev 1.11 rdate.c rev 1.19 extern.h rev 1.14 getnetgrent.c rev 1.41 netgroup.h rev 1.10 fparseln.3 rev 1.4 fparseln.c rev 1.10
    our stringlist.c/stringlist.h are derived from getnetgrent.c
    rfc868time.c from rdate.c
    newfs/pathnames.h from fsck/pathnames.h
    
    https://mail-index.netbsd.org/source-changes/2009/10/21/msg002182.html
    Not all files are covered as some had copyright assigned to TNF in 1998.

--- include/netgroup.h
+++ include/netgroup.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: netgroup.h,v 1.7 2015/01/19 15:30:52 krw Exp $ */
+/*	$OpenBSD: netgroup.h,v 1.8 2015/09/10 18:59:34 deraadt Exp $ */
 /*
  * Copyright (c) 1994 Christos Zoulas
  * All rights reserved.
@@ -11,11 +11,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Christos Zoulas.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
--- usr.sbin/netgroup_mkdb/stringlist.c
+++ usr.sbin/netgroup_mkdb/stringlist.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: stringlist.c,v 1.2 2015/10/01 13:04:42 jsg Exp $	*/
+/*	$OpenBSD: stringlist.c,v 1.3 2015/12/19 20:37:11 mmcc Exp $	*/
 
 /*
  * Copyright (c) 1994 Christos Zoulas
@@ -12,11 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Christos Zoulas.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
