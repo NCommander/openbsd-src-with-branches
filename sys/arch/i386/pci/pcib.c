5e849834e1c3c726676c389599faa66f7965bb22
jsg <jsg>
2023-01-17 23:39:30 +0000

    include isadma.h not isa.h missed in rev 1.5 when #if NISA became #if NISADMA not a problem in practice as isavar.h includes isadma.h
    ok miod@ deraadt@ millert@

--- sys/arch/i386/pci/pcib.c
+++ sys/arch/i386/pci/pcib.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pcib.c,v 1.24 2013/05/30 16:15:01 deraadt Exp $	*/
+/*	$OpenBSD: pcib.c,v 1.25 2022/02/21 10:24:28 mpi Exp $	*/
 /*	$NetBSD: pcib.c,v 1.6 1997/06/06 23:29:16 thorpej Exp $	*/
 
 /*-
@@ -42,7 +42,7 @@
 
 #include <dev/pci/pcidevs.h>
 
-#include "isa.h"
+#include "isadma.h"
 #include "pcibios.h"
 #if NPCIBIOS > 0
 #include <i386/pci/pcibiosvar.h>
