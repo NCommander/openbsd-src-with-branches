8c41b1603edf26c6a19f4dee463ea6327fdd34fd
jsing <jsing>
2023-01-23 10:31:03 +0000

    Move bn_add_words() and bn_sub_words from bn_asm.c to bn_add.c.
    These are wrapped with #ifndef HAVE_BN_ADD_WORDS/HAVE_BN_SUB_WORDS, which
    are defined for architectures that provide their own assembly versions.

--- lib/libcrypto/bn/arch/sparc/bn_arch.h
+++ lib/libcrypto/bn/arch/sparc/bn_arch.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bn_arch.h,v 1.1 2023/01/20 10:04:34 jsing Exp $ */
+/*	$OpenBSD: bn_arch.h,v 1.2 2023/01/20 17:31:52 jsing Exp $ */
 /*
  * Copyright (c) 2023 Joel Sing <jsing@openbsd.org>
  *
@@ -20,11 +20,15 @@
 
 #ifndef OPENSSL_NO_ASM
 
+#define HAVE_BN_ADD_WORDS
+
 #define HAVE_BN_MUL_COMBA4
 #define HAVE_BN_MUL_COMBA8
 
 #define HAVE_BN_SQR_COMBA4
 #define HAVE_BN_SQR_COMBA8
+
+#define HAVE_BN_SUB_WORDS
 
 #endif
 #endif
