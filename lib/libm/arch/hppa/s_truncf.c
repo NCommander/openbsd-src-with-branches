e54608492e279e0001b5d26ce381220d02e72661
miod <miod>
2023-01-27 16:43:33 +0000

    Use the same inline assembly constraints as for libc fp*(), when operating on the floating-point control register.

--- lib/libm/arch/hppa/s_truncf.c
+++ lib/libm/arch/hppa/s_truncf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD$	*/
+/*	$OpenBSD: s_truncf.c,v 1.5 2015/01/20 04:41:01 krw Exp $	*/
 /*
  * Written by Michael Shalayeff. Public Domain
  */
@@ -12,13 +12,13 @@ truncf(float x)
 {
 	u_int64_t ofpsr, fpsr;
 
-	__asm__ volatile("fstds %%fr0,0(%0)" :: "r" (&ofpsr) : "memory");
+	__asm__ volatile("fstds %%fr0,0(%1)" : "=m" (ofpsr) : "r" (&ofpsr));
 	fpsr = (ofpsr & ~((u_int64_t)FP_RM << (9 + 32))) |
 	    ((u_int64_t)FP_RZ << (9 + 32));
-	__asm__ volatile("fldds 0(%0), %%fr0" :: "r" (&fpsr) : "memory");
+	__asm__ volatile("fldds 0(%0), %%fr0" :: "r" (&fpsr), "m" (fpsr));
 
 	__asm__ volatile("frnd,sgl %0,%0" : "+f" (x));
 
-	__asm__ volatile("fldds 0(%0), %%fr0" :: "r" (&ofpsr) : "memory");
+	__asm__ volatile("fldds 0(%0), %%fr0" :: "r" (&ofpsr), "m" (ofpsr));
 	return (x);
 }
