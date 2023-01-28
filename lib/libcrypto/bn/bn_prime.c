811cd86cbaeea1b31aedb7400beb64971bbd4594
jsing <jsing>
2023-01-28 17:13:26 +0000

    Remove comment referencing bn_depr.c.

--- lib/libcrypto/bn/bn_prime.c
+++ lib/libcrypto/bn/bn_prime.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: bn_prime.c,v 1.28 2022/11/26 16:08:51 tb Exp $ */
+/* $OpenBSD: bn_prime.c,v 1.29 2023/01/28 17:09:00 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -115,11 +115,6 @@
 #include <openssl/err.h>
 
 #include "bn_local.h"
-
-/* NB: these functions have been "upgraded", the deprecated versions (which are
- * compatibility wrappers using these functions) are in bn_depr.c.
- * - Geoff
- */
 
 /* The quick sieve algorithm approach to weeding out primes is
  * Philip Zimmermann's, as implemented in PGP.  I have had a read of
