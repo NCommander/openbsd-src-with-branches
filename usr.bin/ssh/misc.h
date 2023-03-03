e978d5c7f448f75560b3dd2edf70faeb5aed8c04
dtucker <dtucker>
2023-03-03 02:37:58 +0000

    Use time_t for x11_refuse_time timeout.  We need SSH_TIME_T_MAX for this, so move from misc.c to misc.h so it's available.  Fixes a Coverity warning for 64bit time_t safety, ok djm@

--- usr.bin/ssh/misc.h
+++ usr.bin/ssh/misc.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: misc.h,v 1.100 2022/06/03 04:30:47 djm Exp $ */
+/* $OpenBSD: misc.h,v 1.101 2023/01/06 02:37:04 djm Exp $ */
 
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
@@ -237,5 +237,8 @@ void	notify_complete(struct notifier_ctx *, const char *, ...)
 
 typedef void (*sshsig_t)(int);
 sshsig_t ssh_signal(int, sshsig_t);
+
+/* On OpenBSD time_t is int64_t which is long long. */
+#define SSH_TIME_T_MAX LLONG_MAX
 
 #endif /* _MISC_H */
