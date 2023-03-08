36926936de76e4bac6ee991060ea2e966ecdc767
jsing <jsing>
2023-03-08 04:50:27 +0000

    Remove EC_FLAGS_DEFAULT_OCT.
    The EC code has an amazing array of function pointer hooks, such that a
    method can hook into almost any operation... and then there is the
    EC_FLAGS_DEFAULT_OCT flag, which adds a bunch of complex code and #ifdef
    so you can avoid setting three of those function pointers!
    
    Remove EC_FLAGS_DEFAULT_OCT, the now unused flags field from EC_METHOD,
    along with the various code that was wrapped in EC_FLAGS_DEFAULT_OCT,
    setting the three function pointers that need to be set in each of the
    EC_METHODs.
    
    ok beck@ tb@

--- lib/libcrypto/ec/ec_local.h
+++ lib/libcrypto/ec/ec_local.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: ec_local.h,v 1.8 2023/03/07 05:45:14 jsing Exp $ */
+/* $OpenBSD: ec_local.h,v 1.9 2023/03/07 05:50:59 jsing Exp $ */
 /*
  * Originally written by Bodo Moeller for the OpenSSL project.
  */
@@ -86,17 +86,7 @@ __BEGIN_HIDDEN_DECLS
 # endif
 #endif
 
-/* Use default functions for poin2oct, oct2point and compressed coordinates */
-#define EC_FLAGS_DEFAULT_OCT	0x1
-
 struct ec_method_st {
-
-	/*
-	 * Methods and members exposed directly by the public API.
-	 */
-
-	int flags;
-
 	int field_type;
 
 	int (*group_init)(EC_GROUP *);
