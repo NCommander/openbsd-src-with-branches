91ce56815225dc2006cfa88438ed64ae456effac
deraadt <deraadt>
2023-03-17 22:52:22 +0000

    remove -beta tag

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.192 2022/09/27 02:39:24 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.193 2023/03/04 14:49:37 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -71,10 +71,10 @@ ost="OpenBSD"
 osr="7.3"
 
 cat >vers.c <<eof
-#define STATUS "-beta"			/* just before a release */
-#if 0
-#define STATUS "-current"		/* just after a release */
 #define STATUS ""			/* release */
+#if 0
+#define STATUS "-beta"			/* just before a release */
+#define STATUS "-current"		/* just after a release */
 #define STATUS "-stable"		/* stable branch */
 #endif
 
