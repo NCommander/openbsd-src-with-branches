3b70a99c5216a9dc89cbecb97d235ce5d3d92b9e
tb <tb>
2023-04-18 08:46:08 +0000

    Sigh cvs... Also drop OPENSSL_NO_DEPRECATED from ec.h

--- lib/libcrypto/ec/ec.h
+++ lib/libcrypto/ec/ec.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: ec.h,v 1.31 2023/03/08 05:45:31 jsing Exp $ */
+/* $OpenBSD: ec.h,v 1.33 2023/04/16 08:55:44 tb Exp $ */
 /*
  * Originally written by Bodo Moeller for the OpenSSL project.
  */
@@ -83,9 +83,7 @@
 #endif
 
 #include <openssl/asn1.h>
-#ifndef OPENSSL_NO_DEPRECATED
 #include <openssl/bn.h>
-#endif
 
 #ifdef  __cplusplus
 extern "C" {
