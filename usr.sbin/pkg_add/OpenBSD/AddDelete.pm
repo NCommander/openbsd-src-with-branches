6d209b2c136c6522807aab636c27b6548717585e
sthen <sthen>
2023-04-19 18:07:43 +0000

    Force a standard umask before adding/deleting packages. If not done and the user has a restrictive mask, various items (new directories, @sample'd files, /var/db/pkg entries, mandoc databases) end up not readable.
    feedback/ok espie@

--- usr.sbin/pkg_add/OpenBSD/AddDelete.pm
+++ usr.sbin/pkg_add/OpenBSD/AddDelete.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: AddDelete.pm,v 1.95 2021/05/04 10:19:28 espie Exp $
+# $OpenBSD: AddDelete.pm,v 1.96 2022/05/16 14:13:19 espie Exp $
 #
 # Copyright (c) 2007-2010 Marc Espie <espie@openbsd.org>
 #
@@ -72,6 +72,8 @@ sub do_the_main_work
 	if ($state->{bad}) {
 		return;
 	}
+
+	umask 0022;
 
 	my $handler = sub { $state->fatal("Caught SIG#1", shift); };
 	local $SIG{'INT'} = $handler;
