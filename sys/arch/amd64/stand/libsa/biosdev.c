8f3ec75a75606ac2f20ba78e5020dcf8ed841a21
kn <kn>
2023-04-25 10:06:12 +0000

    Do not attempt to write to read-only softraid
    Bootloaders have no write support for softraid volumes, which manifests in,
    e.g. /bsd.upgrade not being stripped of its 'x' permission bit to prevent
    sysupgrade loops in case of upgrade failure.
    
    Set a no-write flag handled by libsa to bail out early in write calls.
    There should be no real behaviour change, writes just fail earlier now.
    
    amd64 BIOS bits first, rest to come.  Crank minor.
    
    "seems sensible" jsing

--- sys/arch/amd64/stand/libsa/biosdev.c
+++ sys/arch/amd64/stand/libsa/biosdev.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: biosdev.c,v 1.34 2020/12/09 18:10:18 krw Exp $	*/
+/*	$OpenBSD: biosdev.c,v 1.35 2022/08/12 20:17:46 stsp Exp $	*/
 
 /*
  * Copyright (c) 1996 Michael Shalayeff
@@ -533,6 +533,8 @@ biosopen(struct open_file *f, ...)
 #ifdef SOFTRAID
 	/* Intercept softraid disks. */
 	if (strncmp("sr", dev, 2) == 0) {
+		/* We only support read-only softraid. */
+		f->f_flags |= F_NOWRITE;
 
 		/* Create a fake diskinfo for this softraid volume. */
 		SLIST_FOREACH(bv, &sr_volumes, sbv_link)
