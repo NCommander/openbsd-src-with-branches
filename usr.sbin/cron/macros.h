0ed2c9e3dbff6ae30be0dc8a682b4239a424a655
millert <millert>
2023-05-06 23:06:27 +0000

    Support random offsets when using ranges with a step value in cron. This extends the random range syntax to support step values.  Instead of choosing a random number between the high and low values, the field is treated as a range with a random offset less than the step value.  This can be used to avoid thundering herd problems where multiple machines contact a server all at the same time via cron jobs.
    The syntax is similar to the existing range/step syntax but uses a
    random range.  For example, instead of "0-59/10" in the minutes
    field, "0~59/10" can be used to run a command every 10 minutes where
    the first command starts at a random offset in the range [0,9].
    The high and low numbers are optional, "~/10" can be used instead.
    
    Requested by job@, OK phessler@

--- usr.sbin/cron/macros.h
+++ usr.sbin/cron/macros.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: macros.h,v 1.14 2015/11/04 20:28:17 millert Exp $	*/
+/*	$OpenBSD: macros.h,v 1.15 2015/11/12 21:12:05 millert Exp $	*/
 
 /*
  * Copyright (c) 2004 by Internet Systems Consortium, Inc. ("ISC")
@@ -28,6 +28,8 @@
 #define	MAX_ENVSTR	1000	/* max length of envvar=value\0 strings */
 #define	MAX_TEMPSTR	100	/* obvious */
 #define	MAX_UNAME	(_PW_NAME_LEN+1)	/* max length of username, should be overkill */
+
+#define	MINIMUM(a, b)	(((a) < (b)) ? (a) : (b))
 
 #define	Skip_Blanks(c, f) \
 			while (c == '\t' || c == ' ') \
