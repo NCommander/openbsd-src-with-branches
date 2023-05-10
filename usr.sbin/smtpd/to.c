d92d8347b448b589220d387e786c621f9d6d809e
op <op>
2023-05-10 08:02:10 +0000

    constify day, month and tz
    spotted while diffing with -portable, where tz is marked as const.
    ok tb@

--- usr.sbin/smtpd/to.c
+++ usr.sbin/smtpd/to.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: to.c,v 1.47 2021/05/26 18:08:55 eric Exp $	*/
+/*	$OpenBSD: to.c,v 1.48 2021/06/14 17:58:16 eric Exp $	*/
 
 /*
  * Copyright (c) 2009 Jacek Masiulaniec <jacekm@dobremiasto.net>
@@ -140,10 +140,10 @@ time_to_text(time_t when)
 {
 	struct tm *lt;
 	static char buf[40];
-	char *day[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
-	char *month[] = {"Jan","Feb","Mar","Apr","May","Jun",
+	const char *day[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+	const char *month[] = {"Jan","Feb","Mar","Apr","May","Jun",
 			 "Jul","Aug","Sep","Oct","Nov","Dec"};
-	char *tz;
+	const char *tz;
 	long offset;
 
 	lt = localtime(&when);
