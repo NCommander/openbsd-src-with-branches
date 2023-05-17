1e59ef7d23fa51c4833d71376f44f19b5bb0e5e3
tb <tb>
2023-05-17 07:42:38 +0000

    Use crypto_internal.h's CTASSERT()
    Now that this macro is available in a header, let's use that version
    rather than copies in several .c files.
    
    discussed with jsing

--- lib/libcrypto/bn/bn_isqrt.c
+++ lib/libcrypto/bn/bn_isqrt.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bn_isqrt.c,v 1.6 2022/12/17 15:56:25 jsing Exp $ */
+/*	$OpenBSD: bn_isqrt.c,v 1.7 2023/03/27 10:25:02 tb Exp $ */
 /*
  * Copyright (c) 2022 Theo Buehler <tb@openbsd.org>
  *
@@ -22,9 +22,7 @@
 #include <openssl/err.h>
 
 #include "bn_local.h"
-
-#define CTASSERT(x)	extern char  _ctassert[(x) ? 1 : -1 ]   \
-			    __attribute__((__unused__))
+#include "crypto_internal.h"
 
 /*
  * Calculate integer square root of |n| using a variant of Newton's method.
