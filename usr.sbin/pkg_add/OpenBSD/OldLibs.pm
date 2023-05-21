6c2510338a1e1a98a8b729dfa9cc4989b024a54f
espie <espie>
2023-05-21 16:07:35 +0000

    move the interface to SharedLibs to be somewhat object oriented
    accordingly, load it "just in time" in State.
    
    Most calls get simplified, and we can save more state for later.

--- usr.sbin/pkg_add/OpenBSD/OldLibs.pm
+++ usr.sbin/pkg_add/OpenBSD/OldLibs.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: OldLibs.pm,v 1.13 2022/04/27 15:04:11 espie Exp $
+# $OpenBSD: OldLibs.pm,v 1.14 2023/05/16 14:31:26 espie Exp $
 #
 # Copyright (c) 2004-2010 Marc Espie <espie@openbsd.org>
 #
@@ -214,9 +214,8 @@ sub do_save_libs
 
 
 	if ($state->{not}) {
-		require OpenBSD::SharedLibs;
 
-		OpenBSD::SharedLibs::add_libs_from_plist($stub_list, $state);
+		$state->shlibs->add_libs_from_plist($stub_list);
 		$stub_list->to_cache;
 		$o->plist->to_cache;
 	} else {
