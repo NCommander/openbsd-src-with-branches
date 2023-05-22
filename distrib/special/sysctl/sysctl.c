6d98436bc47a5d20d75ca2a8ee3de120426e91d5
kn <kn>
2023-05-22 09:29:54 +0000

    fix usage, name arg is optional

--- distrib/special/sysctl/sysctl.c
+++ distrib/special/sysctl/sysctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sysctl.c,v 1.13 2021/06/03 17:05:41 kettenis Exp $	*/
+/*	$OpenBSD: sysctl.c,v 1.14 2021/06/04 00:29:15 deraadt Exp $	*/
 
 /*
  * Copyright (c) 2009 Theo de Raadt <deraadt@openbsd.org>
@@ -143,7 +143,7 @@ main(int argc, char *argv[])
 			nflag = 1;
 			break;
 		default:
-			fprintf(stderr, "usage: sysctl [-n] name\n");
+			fprintf(stderr, "usage: sysctl [-n] [name]\n");
 			exit(1);
 		}
 	}
