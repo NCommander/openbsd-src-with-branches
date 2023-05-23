8f0747ce630f5386eeb6745fddcfa9ffd9ade9fa
claudio <claudio>
2023-05-23 13:12:19 +0000

    Replace ibuf_release() with ibuf_free() since the former just calls the latter OK kn@ tb@

--- sbin/iked/policy.c
+++ sbin/iked/policy.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: policy.c,v 1.92 2022/10/10 11:33:55 tobhe Exp $	*/
+/*	$OpenBSD: policy.c,v 1.93 2023/02/08 19:59:10 tb Exp $	*/
 
 /*
  * Copyright (c) 2020-2021 Tobias Heider <tobhe@openbsd.org>
@@ -784,8 +784,8 @@ childsa_free(struct iked_childsa *csa)
 		csb->csa_bundled = NULL;
 	if ((csb = csa->csa_peersa) != NULL)
 		csb->csa_peersa = NULL;
-	ibuf_release(csa->csa_encrkey);
-	ibuf_release(csa->csa_integrkey);
+	ibuf_free(csa->csa_encrkey);
+	ibuf_free(csa->csa_integrkey);
 	free(csa);
 }
 
