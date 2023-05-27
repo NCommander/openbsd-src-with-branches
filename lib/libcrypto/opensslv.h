d19ce98974ffa5d2299cf2fe5a1f20d9c232cb03
tb <tb>
2023-05-27 13:54:46 +0000

    Bump LibreSSL version

--- lib/libcrypto/opensslv.h
+++ lib/libcrypto/opensslv.h
@@ -1,11 +1,11 @@
-/* $OpenBSD: opensslv.h,v 1.74 2023/04/25 16:15:37 tb Exp $ */
+/* $OpenBSD: opensslv.h,v 1.75 2023/04/27 10:43:47 tb Exp $ */
 #ifndef HEADER_OPENSSLV_H
 #define HEADER_OPENSSLV_H
 
 /* These will change with each release of LibreSSL-portable */
-#define LIBRESSL_VERSION_NUMBER 0x3080000fL
+#define LIBRESSL_VERSION_NUMBER 0x3080100fL
 /*                                    ^ Patch starts here   */
-#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.8.0"
+#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.8.1"
 
 /* These will never change */
 #define OPENSSL_VERSION_NUMBER	0x20000000L
