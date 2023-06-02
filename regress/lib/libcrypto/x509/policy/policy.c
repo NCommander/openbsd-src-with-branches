ce89c9b5269a6828980dbdb46374b16698d0ce19
tb <tb>
2023-06-02 08:35:10 +0000

    fix typo

--- regress/lib/libcrypto/x509/policy/policy.c
+++ regress/lib/libcrypto/x509/policy/policy.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: policy.c,v 1.10 2023/04/28 16:50:16 beck Exp $ */
+/* $OpenBSD: policy.c,v 1.11 2023/05/29 15:52:46 beck Exp $ */
 /*
  * Copyright (c) 2020 Joel Sing <jsing@openbsd.org>
  * Copyright (c) 2020-2023 Bob Beck <beck@openbsd.org>
@@ -279,7 +279,7 @@ struct verify_cert_test verify_cert_tests[] = {
 	},
 	/*  The policy extension cannot be parsed. */
 	{
-		.id = "1 in invalid intermediate poicy",
+		.id = "1 in invalid intermediate policy",
 		.root_file = CERTSDIR "/" "policy_root.pem",
 		.intermediate_file = CERTSDIR "/" "policy_intermediate_invalid.pem",
 		.leaf_file = CERTSDIR "/" "policy_leaf.pem",
