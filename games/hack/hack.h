8e2bd1be6818fc019f378d778df3c3eeedb5db09
op <op>
2023-06-03 15:19:38 +0000

    drop `uptodate()' check from hack(6)
    hack(6) scrapes $PATH to find its executable and compare the mtime to
    the save file and bone file.  If the game is newer than those, they're
    not loaded.
    
    Drop this feature.  /usr/games is not in the default $PATH anymore, and
    the format for those file didn't change since the import.
    
    Diff from Anton Konyahin (me [at] konyahin [dot] xyz)

--- games/hack/hack.h
+++ games/hack/hack.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: hack.h,v 1.12 2015/12/26 00:26:39 mestre Exp $*/
+/*	$OpenBSD: hack.h,v 1.13 2016/01/09 18:33:15 mestre Exp $*/
 /*	$NetBSD: hack.h,v 1.3 1995/03/23 08:30:21 cgd Exp $*/
 
 /*
@@ -681,7 +681,6 @@ int  phase_of_the_moon(void);
 int  night(void);
 int  midnight(void);
 void gethdate(char *);
-int  uptodate(int);
 void getlock(void);
 #ifdef MAIL
 void getmailstatus(void);
