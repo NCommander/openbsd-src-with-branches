68a7432bece9ff89efd7e14f94e3b1e21f1a8fe9
sashan <sashan>
2023-06-05 08:37:27 +0000

    pf_remove_state() should not attempt to remove state which is already removed.
    OK dlg@

--- sys/net/pf.c
+++ sys/net/pf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf.c,v 1.1179 2023/05/13 13:35:17 bluhm Exp $ */
+/*	$OpenBSD: pf.c,v 1.1180 2023/05/15 16:34:56 bluhm Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -1713,6 +1713,9 @@ void
 pf_remove_state(struct pf_state *st)
 {
 	PF_ASSERT_LOCKED();
+
+	if (st->timeout == PFTM_UNLINKED)
+		return;
 
 	/* handle load balancing related tasks */
 	pf_postprocess_addr(st);
