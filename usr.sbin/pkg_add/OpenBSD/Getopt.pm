42bd1df609651c66aa0bb4c8f4bf3818476ad7f2
espie <espie>
2023-06-16 06:44:14 +0000

    fix default parm, noticed by aja@ on clean-old-distfiles

--- usr.sbin/pkg_add/OpenBSD/Getopt.pm
+++ usr.sbin/pkg_add/OpenBSD/Getopt.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: Getopt.pm,v 1.15 2023/05/21 13:44:21 espie Exp $
+# $OpenBSD: Getopt.pm,v 1.16 2023/06/13 09:07:17 espie Exp $
 #
 # Copyright (c) 2006 Marc Espie <espie@openbsd.org>
 #
@@ -45,9 +45,8 @@ sub handle_option($opt, $hash, @params)
 	}
 }
 
-sub getopts($args, $hash)
+sub getopts($args, $hash = {})
 {
-    $hash = {} unless defined $hash;
     local @EXPORT;
 
     while ($_ = shift @ARGV) {
