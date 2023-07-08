6fd57a108fb32e5b8068714d694e3f765f6b4491
beck <beck>
2023-07-08 12:27:51 +0000

    Hide symbols in objects
    ok tb@

--- lib/libcrypto/objects/obj_err.c
+++ lib/libcrypto/objects/obj_err.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: obj_err.c,v 1.12 2017/01/29 17:49:23 beck Exp $ */
+/* $OpenBSD: obj_err.c,v 1.13 2022/07/12 14:42:49 kn Exp $ */
 /* ====================================================================
  * Copyright (c) 1999-2006 The OpenSSL Project.  All rights reserved.
  *
@@ -88,3 +88,4 @@ ERR_load_OBJ_strings(void)
 	}
 #endif
 }
+LCRYPTO_ALIAS(ERR_load_OBJ_strings);
