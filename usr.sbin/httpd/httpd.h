a6ce94eb82f5715bb7f675e625d4c034700919d5
tb <tb>
2023-07-12 12:46:01 +0000

    Work around use after free in httpd(8)
    A malformed HTTP request can cause httpd in fastcgi mode to crash due to a
    use-after-free. This is an awful hack, but it's good enough until someone
    figures out the correct way of dealing with server_close() here.
    
    Reported by Jesper Wallin <jesper () ifconfig.se>
    
    "this will do the trick for now" claudio
    ok beck deraadt

--- usr.sbin/httpd/httpd.h
+++ usr.sbin/httpd/httpd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: httpd.h,v 1.161 2022/08/15 12:29:17 claudio Exp $	*/
+/*	$OpenBSD: httpd.h,v 1.162 2022/10/24 15:02:01 jmc Exp $	*/
 
 /*
  * Copyright (c) 2006 - 2015 Reyk Floeter <reyk@openbsd.org>
@@ -352,6 +352,7 @@ struct client {
 	int			 clt_inflight;
 	struct range_data	 clt_ranges;
 	struct fcgi_data	 clt_fcgi;
+	const char		*clt_fcgi_error;
 	char			*clt_remote_user;
 	struct evbuffer		*clt_srvevb;
 
