8ed5d2d5f987721508834c13eac17ff85ecdd34c
kettenis <kettenis>
2023-07-13 08:33:36 +0000

    Use the deep idle state available on Apple M1/M2 cores in the idle loop and for suspend.  This state makes the CPU lose some of its register state so we need to save these registers before putting the core to sleep and restore them when we wake up.  This deep idle state has a higher wakeup latency than the normal WFI idle state.  Use similar logic as acpucpu(4) to decide which idle state to pick.
    If some cores of a cluster are in this deep idle state, turbo states become
    available to the cores that remain active.  So stop skipping these states.
    This improves single-core performance a little bit.
    
    The main win is in power savings when running in a state with a high clock
    frequency.  My M2 Pro mini goes from 14W to 6.5W when idle at the maximum
    clock frequency.  But event at the lowest clock frequency there are small
    but significant power savings.
    
    ok deraadt@, tobhe@

--- sys/arch/arm64/include/cpu.h
+++ sys/arch/arm64/include/cpu.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: cpu.h,v 1.35 2023/02/19 17:16:13 kettenis Exp $ */
+/* $OpenBSD: cpu.h,v 1.36 2023/06/10 19:30:48 kettenis Exp $ */
 /*
  * Copyright (c) 2016 Dale Rahn <drahn@dalerahn.com>
  *
@@ -153,6 +153,9 @@ struct cpu_info {
 	volatile int		ci_opp_idx;
 	volatile int		ci_opp_max;
 	uint32_t		ci_cpu_supply;
+
+	u_long			ci_prev_sleep;
+	u_long			ci_last_itime;
 
 #ifdef MULTIPROCESSOR
 	struct srp_hazard	ci_srp_hazards[SRP_HAZARD_NUM];
@@ -343,6 +346,11 @@ void	cpu_halt(void);
 void	cpu_startclock(void);
 int	cpu_suspend_primary(void);
 void	cpu_resume_secondary(struct cpu_info *);
+
+extern void (*cpu_idle_cycle_fcn)(void);
+extern void (*cpu_suspend_cycle_fcn)(void);
+
+void	cpu_wfi(void);
 
 void	delay (unsigned);
 #define	DELAY(x)	delay(x)
