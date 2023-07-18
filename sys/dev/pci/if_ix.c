07a041129325ec3df218e22ac00f13ae291d1cc3
bluhm <bluhm>
2023-07-18 16:01:20 +0000

    Enable LRO for TCP per default in the network drivers.
    Large Receive Offload allows to receive aggregated packets larger
    than the MTU.  Receiving TCP streams becomes much faster.  As the
    network hardware is not aware whether a packet is received locally
    or to be forwarded, everything is aggregated.  In case of forwarding
    it is split on output to packets not larger than the original
    packets.  So path MTU discovery should still work.  If the outgoing
    interface supports TSO, the packet is chopped in hardware by TCP
    Segmentation Offload.
    
    Currently only ix(4) and lo(4) devices support LRO, and ix(4) is
    limited to IPv4 and hardware newer than the old 82598 model.  If
    the interface is added to a tpmr(4), bridge(4) or veb(4), LRO is
    automatically disabled.  All ix(4) devices support outgoing TSO for
    IPv4 and IPv6.  Enabling LRO on lo(4) automatically enables TSO and
    TCP packets larger than the MTU pass the loopback interface.
    
    LRO can be turned off per interface with ifconfig -tcplro.
    
    OK jan@

--- sys/dev/pci/if_ix.c
+++ sys/dev/pci/if_ix.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ix.c,v 1.198 2023/07/08 09:01:30 jmatthew Exp $	*/
+/*	$OpenBSD: if_ix.c,v 1.199 2023/07/10 19:36:54 jan Exp $	*/
 
 /******************************************************************************
 
@@ -1931,8 +1931,10 @@ ixgbe_setup_interface(struct ix_softc *sc)
 	ifp->if_capabilities |= IFCAP_CSUM_IPv4;
 
 	ifp->if_capabilities |= IFCAP_TSOv4 | IFCAP_TSOv6;
-	if (sc->hw.mac.type != ixgbe_mac_82598EB)
+	if (sc->hw.mac.type != ixgbe_mac_82598EB) {
+		ifp->if_xflags |= IFXF_LRO;
 		ifp->if_capabilities |= IFCAP_LRO;
+	}
 
 	/*
 	 * Specify the media types supported by this sc and register
