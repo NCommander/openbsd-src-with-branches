43bed3bda2f1f0d61c6238c118838d3f8a9b473c
tb <tb>
2023-07-28 09:53:55 +0000

    Set OPENSSL_NO_ENGINE, remove engine code
    ENGINE was special. It's horrible code even by the low standards of this
    library. Some ports may now try to use the stubs which will fail, but
    the fallout from this should be minimal. Of course there are various
    language bindings that expose the ENGINE API. OpenSSL 3 disabling ENGINE
    by default will likely help fixing this at some point.
    
    ok jsing

--- lib/libcrypto/opensslfeatures.h
+++ lib/libcrypto/opensslfeatures.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: opensslfeatures.h,v 1.39 2023/07/06 06:38:01 beck Exp $ */
+/* $OpenBSD: opensslfeatures.h,v 1.40 2023/07/28 09:46:36 tb Exp $ */
 /*
  * Feature flags for LibreSSL... so you can actually tell when things
  * are enabled, rather than not being able to tell when things are
@@ -63,7 +63,7 @@
 /* #define OPENSSL_NO_EC */
 #define OPENSSL_NO_EC_NISTP_64_GCC_128
 #define OPENSSL_NO_EGD
-/* #define OPENSSL_NO_ENGINE */
+#define OPENSSL_NO_ENGINE
 /* #define OPENSSL_NO_ERR */
 /* #define OPENSSL_NO_FUZZ_AFL */
 /* #define OPENSSL_NO_FUZZ_LIBFUZZER */
