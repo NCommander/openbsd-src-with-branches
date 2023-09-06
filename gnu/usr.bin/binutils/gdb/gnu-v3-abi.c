6750be0123b0b72dad3903210481d24107f4d6fb
jca <jca>
2023-09-06 15:02:32 +0000

    Use casts to force two function pointers assignements and fix build with clang-16
    "looks fine to me" robert@, ok kettenis@

--- gnu/usr.bin/binutils/gdb/gnu-v3-abi.c
+++ gnu/usr.bin/binutils/gdb/gnu-v3-abi.c
@@ -422,13 +422,13 @@ gnuv3_baseclass_offset (struct type *type, int index, char *valaddr,
 static void
 init_gnuv3_ops (void)
 {
-  vtable_type_gdbarch_data = register_gdbarch_data (build_gdb_vtable_type);
+  vtable_type_gdbarch_data = gdbarch_data_register_post_init (build_gdb_vtable_type);
 
   gnu_v3_abi_ops.shortname = "gnu-v3";
   gnu_v3_abi_ops.longname = "GNU G++ Version 3 ABI";
   gnu_v3_abi_ops.doc = "G++ Version 3 ABI";
-  gnu_v3_abi_ops.is_destructor_name = is_gnu_v3_mangled_dtor;
-  gnu_v3_abi_ops.is_constructor_name = is_gnu_v3_mangled_ctor;
+  gnu_v3_abi_ops.is_destructor_name = (enum dtor_kinds (*)(const char *))is_gnu_v3_mangled_dtor;
+  gnu_v3_abi_ops.is_constructor_name = (enum ctor_kinds (*)(const char *))is_gnu_v3_mangled_ctor;
   gnu_v3_abi_ops.is_vtable_name = gnuv3_is_vtable_name;
   gnu_v3_abi_ops.is_operator_name = gnuv3_is_operator_name;
   gnu_v3_abi_ops.rtti_type = gnuv3_rtti_type;
