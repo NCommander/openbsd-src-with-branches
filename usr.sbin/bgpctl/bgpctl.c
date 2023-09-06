59def94b2bc847e90e4b4e281778bef2b465ae25
claudio <claudio>
2023-09-06 09:52:26 +0000

    Add missing <sys/time.h> include for timespecsub()

--- usr.sbin/bgpctl/bgpctl.c
+++ usr.sbin/bgpctl/bgpctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bgpctl.c,v 1.294 2023/04/23 11:39:51 claudio Exp $ */
+/*	$OpenBSD: bgpctl.c,v 1.295 2023/04/28 13:24:25 claudio Exp $ */
 
 /*
  * Copyright (c) 2003 Henning Brauer <henning@openbsd.org>
@@ -19,6 +19,7 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
