86132513f2cc2a58276b1441b8711166ad7e1fec
jsg <jsg>
2023-09-10 06:25:09 +0000

    don't error on WSDISPLAYIO_SVIDEO/WSDISPLAYIO_GVIDEO in drm drivers it is handled by wsdisplay(4) ok dlg@

--- sys/dev/fdt/rkdrm.c
+++ sys/dev/fdt/rkdrm.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: rkdrm.c,v 1.14 2022/07/15 17:57:26 kettenis Exp $ */
+/* $OpenBSD: rkdrm.c,v 1.15 2023/01/01 01:34:33 jsg Exp $ */
 /* $NetBSD: rk_drm.c,v 1.3 2019/12/15 01:00:58 mrg Exp $ */
 /*-
  * Copyright (c) 2019 Jared D. McNeill <jmcneill@invisible.ca>
@@ -282,6 +282,9 @@ rkdrm_wsioctl(void *v, u_long cmd, caddr_t data, int flag, struct proc *p)
 		return 0;
 	case WSDISPLAYIO_LINEBYTES:
 		*(u_int *)data = ri->ri_stride;
+		return 0;
+	case WSDISPLAYIO_SVIDEO:
+	case WSDISPLAYIO_GVIDEO:
 		return 0;
 	}
 
