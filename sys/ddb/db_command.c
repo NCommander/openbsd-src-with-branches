f0c808df90c3712274de1e95d0de09f67b83b896
claudio <claudio>
2023-09-19 11:35:30 +0000

    Improve the output of ddb "show proc" command
    Include missing fields -- like the sleep channel and message -- and
    show both the PID and TID of the proc.
    Also add '/t' as an argument that can be used to specify a proc by TID
    instead of by address.
    OK mpi@

--- sys/ddb/db_command.c
+++ sys/ddb/db_command.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: db_command.c,v 1.98 2023/03/08 04:43:07 guenther Exp $	*/
+/*	$OpenBSD: db_command.c,v 1.99 2023/07/02 19:02:27 cheloha Exp $	*/
 /*	$NetBSD: db_command.c,v 1.20 1996/03/30 22:30:05 christos Exp $	*/
 
 /*
@@ -541,6 +541,13 @@ db_proc_print_cmd(db_expr_t addr, int have_addr, db_expr_t count, char *modif)
 {
 	if (!have_addr)
 		addr = (db_expr_t)curproc;
+	if (modif[0] == 't') {
+		addr = (db_expr_t)tfind((pid_t)addr);
+		if (addr == 0) {
+			db_printf("not found\n");
+			return;
+		}
+	}
 
 	proc_printit((struct proc *)addr, modif, db_printf);
 }
