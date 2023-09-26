994d2f26853d5a3dd73a7318bb864e4ef01c5ee8
deraadt <deraadt>
2023-09-26 13:27:32 +0000

    we are heading out of -beta

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.195 2023/03/25 05:49:50 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.196 2023/09/18 13:16:13 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -71,10 +71,10 @@ ost="OpenBSD"
 osr="7.4"
 
 cat >vers.c <<eof
-#define STATUS "-beta"			/* just before a release */
+#define STATUS ""			/* release */
 #if 0
 #define STATUS "-current"		/* just after a release */
-#define STATUS ""			/* release */
+#define STATUS "-beta"			/* just before a release */
 #define STATUS "-stable"		/* stable branch */
 #endif
 
