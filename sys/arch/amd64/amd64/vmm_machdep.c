08d1fa73f53e2e09bbb156fc8c51505294619e51
jasper <jasper>
2023-11-13 19:15:01 +0000

    include function name in warning printf in vmx_handle_np_fault() and svm_handle_np_fault() more clearer output
    ok mlarkin@

--- sys/arch/amd64/amd64/vmm_machdep.c
+++ sys/arch/amd64/amd64/vmm_machdep.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: vmm_machdep.c,v 1.7 2023/09/05 14:00:40 mlarkin Exp $ */
+/* $OpenBSD: vmm_machdep.c,v 1.8 2023/09/06 03:35:57 dv Exp $ */
 /*
  * Copyright (c) 2014 Mike Larkin <mlarkin@openbsd.org>
  *
@@ -4922,8 +4922,8 @@ svm_handle_np_fault(struct vcpu *vcpu)
 		ret = EAGAIN;
 		break;
 	default:
-		printf("unknown memory type %d for GPA 0x%llx\n",
-		    gpa_memtype, gpa);
+		printf("%s: unknown memory type %d for GPA 0x%llx\n",
+		    __func__, gpa_memtype, gpa);
 		return (EINVAL);
 	}
 
@@ -5020,8 +5020,8 @@ vmx_handle_np_fault(struct vcpu *vcpu)
 		}
 		break;
 	default:
-		printf("unknown memory type %d for GPA 0x%llx\n",
-		    gpa_memtype, gpa);
+		printf("%s: unknown memory type %d for GPA 0x%llx\n",
+		    __func__, gpa_memtype, gpa);
 		return (EINVAL);
 	}
 
