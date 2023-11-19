97c7e58a3ffd3dceacc4fec227a32a198b798f0f
tb <tb>
2023-11-19 13:11:06 +0000

    Unifdef OPENSSL_NO_ENGINE in libcrypto regress

--- regress/lib/libcrypto/dsa/dsatest.c
+++ regress/lib/libcrypto/dsa/dsatest.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dsatest.c,v 1.8 2023/08/12 06:23:59 tb Exp $	*/
+/*	$OpenBSD: dsatest.c,v 1.9 2023/08/20 22:22:55 tb Exp $	*/
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -137,12 +137,10 @@ main(int argc, char **argv)
 	if ((dsa = DSA_new()) == NULL)
 		goto end;
 
-#ifdef OPENSSL_NO_ENGINE
 	if (DSA_get0_engine(dsa) != NULL) {
 		BIO_printf(bio_err, "ENGINE was not NULL\n");
 		goto end;
 	}
-#endif
 
 	if (!DSA_generate_parameters_ex(dsa, 512, seed, 20, &counter, &h, cb))
 		goto end;
