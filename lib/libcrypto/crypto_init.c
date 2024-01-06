6bf3a2c14db7f67d814ad054494d924c8167376b
tb <tb>
2024-01-06 17:43:39 +0000

    Remove X509_PURPOSE_cleanup() call in OPENSSL_cleanup()
    Since x509_purp.c r1.34 this is a noop since there is nothing to clean up
    anymore. Remove the last caller.

--- lib/libcrypto/crypto_init.c
+++ lib/libcrypto/crypto_init.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: crypto_init.c,v 1.12 2023/11/19 15:46:09 tb Exp $ */
+/*	$OpenBSD: crypto_init.c,v 1.13 2023/12/16 12:36:14 tb Exp $ */
 /*
  * Copyright (c) 2018 Bob Beck <beck@openbsd.org>
  *
@@ -84,7 +84,6 @@ OPENSSL_cleanup(void)
 	EVP_cleanup();
 
 	X509V3_EXT_cleanup();
-	X509_PURPOSE_cleanup();
 	X509_TRUST_cleanup();
 	X509_VERIFY_PARAM_table_cleanup();
 
