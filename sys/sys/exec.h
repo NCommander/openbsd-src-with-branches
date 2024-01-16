9a26b3e626dcceea7e9a7f35d2fbd127462a1cbf
deraadt <deraadt>
2024-01-16 19:05:01 +0000

    The kernel will now read pinsyscall tables out of PT_OPENBSD_SYSCALLS in the main program or ld.so, and accept a submission of that information for libc.so from ld.so via pinsyscalls(2).  At system call invocation, the syscall number is matched to the specific address it must come from. ok kettenis, gnezdo, testing of variations by many people

--- sys/sys/exec.h
+++ sys/sys/exec.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: exec.h,v 1.51 2022/10/30 17:43:40 guenther Exp $	*/
+/*	$OpenBSD: exec.h,v 1.52 2023/04/19 15:37:36 kettenis Exp $	*/
 /*	$NetBSD: exec.h,v 1.59 1996/02/09 18:25:09 christos Exp $	*/
 
 /*-
@@ -131,6 +131,9 @@ struct exec_package {
 	struct	elf_args *ep_args;	/* ELF info */
 	void	*ep_auxinfo;		/* userspace auxinfo address */
 	char	*ep_interp;		/* name of interpreter if any */
+	vaddr_t	ep_pinstart, ep_pinend;	/* executable region */
+	u_int	*ep_pins;		/* array of system call offsets */
+	int	ep_npins;		/* entries in array */
 };
 #define	EXEC_INDIR	0x0001		/* script handling already done */
 #define	EXEC_HASFD	0x0002		/* holding a shell script */
