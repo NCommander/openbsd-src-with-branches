debc10cdbace1b09442cad64f9a9dbed9748d246
claudio <claudio>
2024-01-18 14:49:59 +0000

    Use imsg_get_fd() in vmd.
    vmd uses a lot of fd passing and does it sometimes via extra abstraction
    so this just tries to convert the code without any optimisations.
    
    ok dv@

--- usr.sbin/vmd/proc.c
+++ usr.sbin/vmd/proc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: proc.c,v 1.20 2023/02/15 20:44:01 tobhe Exp $	*/
+/*	$OpenBSD: proc.c,v 1.21 2023/09/26 01:53:54 dv Exp $	*/
 
 /*
  * Copyright (c) 2010 - 2016 Reyk Floeter <reyk@openbsd.org>
@@ -661,7 +661,7 @@ proc_dispatch(int fd, short event, void *arg)
 		case IMSG_CTL_PROCFD:
 			IMSG_SIZE_CHECK(&imsg, &pf);
 			memcpy(&pf, imsg.data, sizeof(pf));
-			proc_accept(ps, imsg.fd, pf.pf_procid,
+			proc_accept(ps, imsg_get_fd(&imsg), pf.pf_procid,
 			    pf.pf_instance);
 			break;
 		default:
@@ -792,7 +792,8 @@ proc_forward_imsg(struct privsep *ps, struct imsg *imsg,
     enum privsep_procid id, int n)
 {
 	return (proc_compose_imsg(ps, id, n, imsg->hdr.type,
-	    imsg->hdr.peerid, imsg->fd, imsg->data, IMSG_DATA_SIZE(imsg)));
+	    imsg->hdr.peerid, imsg_get_fd(imsg), imsg->data,
+	    IMSG_DATA_SIZE(imsg)));
 }
 
 struct imsgbuf *
