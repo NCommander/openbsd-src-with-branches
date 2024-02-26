71da7bb9ba77c9271f3cf06f8a8990c67a839b56
kettenis <kettenis>
2024-02-26 18:57:50 +0000

    Add RK3588 support.  Rework the RK3568 support to take advantage of the PHY mode enum like the new RK3588 code.
    ok jmatthew@

--- sys/dev/ic/dwqevar.h
+++ sys/dev/ic/dwqevar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: dwqevar.h,v 1.9 2023/10/11 12:52:00 stsp Exp $	*/
+/*	$OpenBSD: dwqevar.h,v 1.10 2023/11/11 16:50:25 stsp Exp $	*/
 /*
  * Copyright (c) 2008, 2019 Mark Kettenis <kettenis@openbsd.org>
  * Copyright (c) 2017, 2022 Patrick Wildt <patrick@blueri.se>
@@ -18,6 +18,7 @@
 
 enum dwqe_phy_mode {
 	DWQE_PHY_MODE_UNKNOWN,
+	DWQE_PHY_MODE_RMII,
 	DWQE_PHY_MODE_RGMII,
 	DWQE_PHY_MODE_RGMII_ID,
 	DWQE_PHY_MODE_RGMII_TXID,
