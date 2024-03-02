1a2b34828ab8825a98f7127bfcfbafecfe1dd07f
tb <tb>
2024-03-02 13:39:28 +0000

    Remove some GOST relics

--- lib/libcrypto/err/err_all.c
+++ lib/libcrypto/err/err_all.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: err_all.c,v 1.32 2023/07/28 09:46:36 tb Exp $ */
+/* $OpenBSD: err_all.c,v 1.33 2023/11/19 15:46:09 tb Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -93,9 +93,6 @@
 #endif
 #ifndef OPENSSL_NO_RSA
 #include <openssl/rsa.h>
-#endif
-#ifndef OPENSSL_NO_GOST
-#include <openssl/gost.h>
 #endif
 
 void ERR_load_ERR_strings_internal(void);
