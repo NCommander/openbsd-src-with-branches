cbf9d3a8d98ab86b96e7e13f4ccf7a23b3e83d17
tb <tb>
2024-03-02 09:36:40 +0000

    Remove EVP_add_{cipher,digest}() from public API
    Ciphers and digests are now handled in a static lookup table and no
    longer by the associative array that used to underlie the OBJ_NAME API.
    Adding ciphers is no longer possible. What uses this API does so for
    historic reasons coming from a time where SHA-2 and some AES variants
    needed to be enabled explicitly. Ports doing this (PHP and DANE code)
    were fixed.
    
    ok jsing

--- lib/libcrypto/evp/evp_names.c
+++ lib/libcrypto/evp/evp_names.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: evp_names.c,v 1.7 2024/01/13 12:18:52 tb Exp $ */
+/*	$OpenBSD: evp_names.c,v 1.8 2024/01/27 18:12:27 tb Exp $ */
 /*
  * Copyright (c) 2023 Theo Buehler <tb@openbsd.org>
  *
@@ -1918,18 +1918,6 @@ EVP_cleanup(void)
 /*
  * XXX - Remove all the garbage below in the next bump.
  */
-
-int
-EVP_add_cipher(const EVP_CIPHER *c)
-{
-	return 1;
-}
-
-int
-EVP_add_digest(const EVP_MD *md)
-{
-	return 1;
-}
 
 int
 OBJ_NAME_init(void)
