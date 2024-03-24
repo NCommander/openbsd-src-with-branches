6748c6688330d79a4323a56affb97ff1ffef1d3a
yasuoka <yasuoka>
2024-03-24 00:05:01 +0000

    Allow zero-length identity response
    ok tobhe

--- sbin/iked/eap.c
+++ sbin/iked/eap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: eap.c,v 1.24 2023/05/23 13:57:14 claudio Exp $	*/
+/*	$OpenBSD: eap.c,v 1.25 2023/07/18 15:07:41 claudio Exp $	*/
 
 /*
  * Copyright (c) 2010-2013 Reyk Floeter <reyk@openbsd.org>
@@ -71,7 +71,12 @@ eap_validate_id_response(struct eap_message *eap)
 	len = betoh16(eap->eap_length) - sizeof(*eap);
 	ptr += sizeof(*eap);
 
-	if (len == 0 || (str = get_string(ptr, len)) == NULL) {
+	if (len == 0) {
+		if ((str = strdup("")) == NULL) {
+			log_warn("%s: strdup failed", __func__);
+			return (NULL);
+		}
+	} else if ((str = get_string(ptr, len)) == NULL) {
 		log_info("%s: invalid identity response, length %zu",
 		    __func__, len);
 		return (NULL);
