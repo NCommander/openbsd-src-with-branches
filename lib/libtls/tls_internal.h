d3cc68003e2840a8eb46c48b527253218af46b82
joshua <joshua>
2024-03-26 00:50:22 +0000

    Use errno_value instead of num for readability
    ok beck@ jsing@

--- lib/libtls/tls_internal.h
+++ lib/libtls/tls_internal.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: tls_internal.h,v 1.82 2023/06/18 11:43:03 op Exp $ */
+/* $OpenBSD: tls_internal.h,v 1.83 2023/06/27 18:19:59 tb Exp $ */
 /*
  * Copyright (c) 2014 Jeremie Courreges-Anglas <jca@openbsd.org>
  * Copyright (c) 2014 Joel Sing <jsing@openbsd.org>
@@ -46,7 +46,7 @@ union tls_addr {
 
 struct tls_error {
 	char *msg;
-	int num;
+	int errno_value;
 	int tls;
 };
 
