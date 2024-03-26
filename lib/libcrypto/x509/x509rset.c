39d976097b38d303fe9860fe6e876aafbfcdb97e
tb <tb>
2024-03-26 11:09:37 +0000

    Reject setting invalid versions for certs, CRLs and CSRs
    The toolkit aspect bites again. Lots of invalid CRLs and CSRs are produced
    because people neither read the RFCs nor does the toolkit check anything it
    is fed. Reviewers apparently also aren't capable of remembering that they
    have three copy-pasted versions of the same API and that adding a version
    check to one of the might suggest adding one for the other two.
    
    This requires ruby-openssl 20240326p0 to pass
    
    ok beck job jsing

--- lib/libcrypto/x509/x509rset.c
+++ lib/libcrypto/x509/x509rset.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: x509rset.c,v 1.12 2023/02/16 08:38:17 tb Exp $ */
+/* $OpenBSD: x509rset.c,v 1.14 2024/03/25 12:10:57 jsing Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -69,6 +69,9 @@ int
 X509_REQ_set_version(X509_REQ *x, long version)
 {
 	if (x == NULL)
+		return (0);
+	/* RFC 2986 section 4.1 only specifies version 1, encoded as a 0. */
+	if (version != 0)
 		return (0);
 	x->req_info->enc.modified = 1;
 	return (ASN1_INTEGER_set(x->req_info->version, version));
