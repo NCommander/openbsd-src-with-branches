6fcd48242ba810cd3f22f00f6be397c60d71829c
tb <tb>
2024-03-27 01:22:30 +0000

    Unify *_up_ref() implementations
    No need for an inconsistently named local variable and a ternary operator.
    
    ok jsing

--- lib/libcrypto/rsa/rsa_lib.c
+++ lib/libcrypto/rsa/rsa_lib.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: rsa_lib.c,v 1.48 2023/07/28 10:05:16 tb Exp $ */
+/* $OpenBSD: rsa_lib.c,v 1.49 2023/11/19 15:46:10 tb Exp $ */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -192,8 +192,7 @@ LCRYPTO_ALIAS(RSA_free);
 int
 RSA_up_ref(RSA *r)
 {
-	int i = CRYPTO_add(&r->references, 1, CRYPTO_LOCK_RSA);
-	return i > 1 ? 1 : 0;
+	return CRYPTO_add(&r->references, 1, CRYPTO_LOCK_RSA) > 1;
 }
 LCRYPTO_ALIAS(RSA_up_ref);
 
