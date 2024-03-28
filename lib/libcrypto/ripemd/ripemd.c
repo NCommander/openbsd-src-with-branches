27d5fb8c72ab38b1d4432319b00e1f14c6e42897
jsing <jsing>
2024-03-28 07:29:41 +0000

    Remove now unnecessary do { } while (0);

--- lib/libcrypto/ripemd/ripemd.c
+++ lib/libcrypto/ripemd/ripemd.c
@@ -485,7 +485,6 @@ RIPEMD160_Final(unsigned char *md, RIPEMD160_CTX *c)
 	c->num = 0;
 	memset(p, 0, RIPEMD160_CBLOCK);
 
-	do {
 	ll = c->A;
 	HOST_l2c(ll, md);
 	ll = c->B;
@@ -496,7 +495,6 @@ RIPEMD160_Final(unsigned char *md, RIPEMD160_CTX *c)
 	HOST_l2c(ll, md);
 	ll = c->E;
 	HOST_l2c(ll, md);
-	} while (0);
 
 	return 1;
 }
