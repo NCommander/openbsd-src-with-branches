7478fa6cac4935e414805b2d9e90c25bd8fe2311
miod <miod>
2024-03-29 21:18:19 +0000

    send_softint() was designed as being able to target a particular cpu, but the code for this was never written and all uses target the running cpu anyway, so stop pretending it may do things it won't do and drop that cpu argument.
    ok claudio@ kettenis@

--- sys/arch/sparc64/dev/vpci.c
+++ sys/arch/sparc64/dev/vpci.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vpci.c,v 1.32 2020/10/24 05:07:47 jmatthew Exp $	*/
+/*	$OpenBSD: vpci.c,v 1.33 2020/10/27 21:01:33 kettenis Exp $	*/
 /*
  * Copyright (c) 2008 Mark Kettenis <kettenis@openbsd.org>
  *
@@ -769,7 +769,7 @@ vpci_msi_eq_intr(void *arg)
 		if (err != H_EOK)
 			printf("%s: pci_msi_setstate: %d\n", __func__, err);
 
-		send_softint(-1, ih->ih_pil, ih);
+		send_softint(ih->ih_pil, ih);
 
 		head += sizeof(struct vpci_msi_msg);
 		head &= eq->eq_mask;
