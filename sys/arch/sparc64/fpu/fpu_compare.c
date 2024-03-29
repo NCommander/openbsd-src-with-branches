c217157fb1cf668c08365600a288ee49d41378d6
miod <miod>
2024-03-29 21:08:11 +0000

    No longer include <machine/fsr.h> from <machine/reg.h>, and have the few users which need both explicitly include both.
    ok claudio@ kettenis@

--- sys/arch/sparc64/fpu/fpu_compare.c
+++ sys/arch/sparc64/fpu/fpu_compare.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: fpu_compare.c,v 1.3 2003/06/02 23:27:55 millert Exp $	*/
+/*	$OpenBSD: fpu_compare.c,v 1.4 2024/03/29 21:07:11 miod Exp $	*/
 /*	$NetBSD: fpu_compare.c,v 1.3 2001/08/26 05:46:31 eeh Exp $ */
 
 /*
@@ -50,6 +50,7 @@
 
 #include <sys/types.h>
 
+#include <machine/fsr.h>
 #include <machine/reg.h>
 
 #include <sparc64/fpu/fpu_arith.h>
