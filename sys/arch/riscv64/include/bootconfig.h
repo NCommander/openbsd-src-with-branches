05c10bb0a46abad5a4abf0e7020a05cdd771d37d
kettenis <kettenis>
2024-04-06 18:33:54 +0000

    Now that we support RISC-V CPUs that have MMUs with memory cachability attributes, the "direct map" becomes problematic as it results in mappings for the same physical memory pages with different cachability addresses.  The RISC-V specification of the "Svpbmt" extension doesn't outright state that this is "verboten" like on some other architectures that we support.  But it does say that it may result in access with the wrong attributes.  So restrict the use of the direct map to just mapping the 64MB block that the bootloader loaded us into. To make this possible map the device tree later like we do on arm64. This allows us to get rid of some assembly code in locore.S as a bonus!
    ok miod@, jca@

--- sys/arch/riscv64/include/bootconfig.h
+++ sys/arch/riscv64/include/bootconfig.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bootconfig.h,v 1.3 2021/05/12 01:20:52 jsg Exp $	*/
+/*	$OpenBSD: bootconfig.h,v 1.4 2021/07/02 10:42:22 kettenis Exp $	*/
 
 /*-
  * Copyright (c) 2015-2017 Ruslan Bukin <br@bsdpad.com>
@@ -41,7 +41,6 @@ struct riscv_bootparams {
 	vaddr_t		kern_l1pt;	/* L1 page table for the kernel */
 	paddr_t		kern_phys;
 	vaddr_t		kern_stack;
-	vaddr_t		dtbp_virt;	/* Device tree blob virtual addr */
 	paddr_t		dtbp_phys;	/* Device tree blob physical addr */
 };
 
