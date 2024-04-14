21842a09fb518e3a267ba6c633923b3027d6d538
bluhm <bluhm>
2024-04-14 20:46:27 +0000

    Run raw IP input in parallel.
    Running raw IPv4 input with shared net lock in parallel is less
    complex than UDP.  Especially there is no socket splicing.
    
    New ip_deliver() may run with shared or exclusive net lock.  The
    last parameter indicates the mode.  If is is running with shared
    netlock and encounters a protocol that needs exclusive lock, the
    packet is queued.  Old ip_ours() always queued the packet.  Now it
    calls ip_deliver() with shared net lock, and if that cannot handle
    the packet completely, the packet is queued and later processed
    with exclusive net lock.
    
    In case of an IPv6 header chain, that switches from shared to
    exclusive processing, the next protocol and mbuf offset are stored
    in a mbuf tag.
    
    OK mvs@

--- sys/sys/protosw.h
+++ sys/sys/protosw.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: protosw.h,v 1.64 2024/01/11 14:15:12 bluhm Exp $	*/
+/*	$OpenBSD: protosw.h,v 1.65 2024/02/03 22:50:09 mvs Exp $	*/
 /*	$NetBSD: protosw.h,v 1.10 1996/04/09 20:55:32 cgd Exp $	*/
 
 /*-
@@ -132,6 +132,7 @@ struct protosw {
 #define PR_ABRTACPTDIS	0x0020		/* abort on accept(2) to disconnected
 					   socket */
 #define PR_SPLICE	0x0040		/* socket splicing is possible */
+#define PR_MPINPUT	0x0080		/* input runs with shared netlock */
 
 /*
  * The arguments to usrreq are:
