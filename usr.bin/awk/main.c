5841cb22244cac3dbfea3d69c593f18a8ef0eaee
millert <millert>
2024-04-25 18:33:53 +0000

    Update awk to the Apr 22, 2024 version.
     * fixed regex engine gototab reallocation issue that was introduced
       during the Nov 24 rewrite.
    
     * fixed use-after-free bug in fnematch due to adjbuf invalidating
       the pointers to buf.

--- usr.bin/awk/main.c
+++ usr.bin/awk/main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: main.c,v 1.67 2023/11/28 20:54:38 millert Exp $	*/
+/*	$OpenBSD: main.c,v 1.68 2024/01/25 16:40:51 millert Exp $	*/
 /****************************************************************
 Copyright (C) Lucent Technologies 1997
 All Rights Reserved
@@ -23,7 +23,7 @@ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 THIS SOFTWARE.
 ****************************************************************/
 
-const char	*version = "version 20240122";
+const char	*version = "version 20240422";
 
 #define DEBUG
 #include <stdio.h>
